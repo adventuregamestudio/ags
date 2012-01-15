271732cc6c74b093eda6cdeab7acd9f3e0ccdd50
JJS <jjs@jjs.at>
2012-01-15 08:56:16 +0100

    Engine: Fixed building.

--- Android/app/jni/Application.mk
+++ Android/app/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_PROJECT_PATH := $(call my-dir)/..
-APP_MODULES      := agsengine pe ags_snowrain agsblend AGSflashlight
+APP_MODULES      := agsengine pe ags_snowrain agsblend agsflashlight
 APP_STL          := gnustl_static
 APP_OPTIM        := release
 APP_ABI          := armeabi armeabi-v7a

fd30feb3c43f11c30165142e74a41120289d0a8b
JJS <jjs@jjs.at>
2012-01-25 08:58:46 +0100

    Android: Fixed issue 43: Theora video crashing on Tegra 2 devices.

--- Android/buildlibs/armeabi-v7a/theora-svn.sh
+++ Android/buildlibs/armeabi-v7a/theora-svn.sh
@@ -21,6 +21,6 @@ rm ./config.guess
 wget http://git.savannah.gnu.org/cgit/config.git/plain/config.sub -O config.sub
 wget http://git.savannah.gnu.org/cgit/config.git/plain/config.guess -O config.guess
 
-LDFLAGS="-Wl,--fix-cortex-a8,-rpath-link=$NDK_PLATFORM_ROOT/lib,-L$NDK_ADDITIONAL_LIBRARY_PATH/lib,-L$NDK_PLATFORM_ROOT/lib" CFLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=neon -marm -nostdlib -I$NDK_ADDITIONAL_LIBRARY_PATH/include -I$NDK_PLATFORM_ROOT/include" LIBS="-lc" ./configure --host=$NDK_HOST_NAME --prefix=$NDK_ADDITIONAL_LIBRARY_PATH --disable-examples
+LDFLAGS="-Wl,--fix-cortex-a8,-rpath-link=$NDK_PLATFORM_ROOT/lib,-L$NDK_ADDITIONAL_LIBRARY_PATH/lib,-L$NDK_PLATFORM_ROOT/lib" CFLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16 -marm -nostdlib -I$NDK_ADDITIONAL_LIBRARY_PATH/include -I$NDK_PLATFORM_ROOT/include" LIBS="-lc" ./configure --host=$NDK_HOST_NAME --prefix=$NDK_ADDITIONAL_LIBRARY_PATH --disable-examples
 make
 make install

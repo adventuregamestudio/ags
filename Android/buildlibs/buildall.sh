60ef5f03796b84a07833cb5441692d7dde3938e5
JJS <jjs@jjs.at>
2012-01-25 12:47:09 +0100

    Android: Added shell script to build all libraries automatically.

---
+++ Android/buildlibs/buildall.sh
@@ -0,0 +1,23 @@
+cd armeabi
+chmod +x *.sh
+./png.sh
+./freetype.sh
+./ogg.sh
+./tremor.sh
+./theora-svn.sh
+./allegro.sh
+./dumb.sh
+
+cd ..
+
+cd armeabi-v7a
+chmod +x *.sh
+./png.sh
+./freetype.sh
+./ogg.sh
+./tremor.sh
+./theora-svn.sh
+./allegro.sh
+./dumb.sh
+
+cd ..

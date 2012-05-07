dc35c516888b095802fc654d4346f1bac5e85bb2
JJS <jjs@jjs.at>
2012-05-07 11:11:30 +0200

    iOS: Added library headers.

---
+++ iOS/nativelibs/fat/include/allegro/platform/aintios.h
@@ -0,0 +1,26 @@
+/*         ______   ___    ___
+ *        /\  _  \ /\_ \  /\_ \
+ *        \ \ \L\ \\//\ \ \//\ \      __     __   _ __   ___
+ *         \ \  __ \ \ \ \  \ \ \   /'__`\ /'_ `\/\`'__\/ __`\
+ *          \ \ \/\ \ \_\ \_ \_\ \_/\  __//\ \L\ \ \ \//\ \L\ \
+ *           \ \_\ \_\/\____\/\____\ \____\ \____ \ \_\\ \____/
+ *            \/_/\/_/\/____/\/____/\/____/\/___L\ \/_/ \/___/
+ *                                           /\____/
+ *                                           \_/__/
+ *
+ *      Internal header file for the Android Allegro library port.
+ *
+ *      By JJS for the Adventure Game Studio runtime port.
+ *      Based on the Allegro PSP port.
+ *
+ *      See readme.txt for copyright information.
+ */
+
+
+#ifndef AINTIOS_H
+#define AINTIOS_H
+
+void ios_mouse_setup(int left, int right, int top, int bottom, float scaling_x, float scaling_y);
+
+#endif
+

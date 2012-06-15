e756fd41d64a598cabe324b8b8986d284da26067
Ivan Mogilko <ikm_spb@yahoo.com>
2012-06-15 17:34:00 +0400

    Re-Engine: ac.cpp: rich media header

--- Engine/ac.cpp
+++ Engine/ac.cpp
@@ -211,30 +211,6 @@ extern "C" HWND allegro_wnd;
 
 
 
-// Windows Vista Rich Save Games, modified to be platform-agnostic
-
-#define RM_MAXLENGTH    1024
-#define RM_MAGICNUMBER  "RGMH"
-
-#pragma pack(push)
-#pragma pack(1)
-typedef struct _RICH_GAME_MEDIA_HEADER
-{
-    long       dwMagicNumber;
-    long       dwHeaderVersion;
-    long       dwHeaderSize;
-    long       dwThumbnailOffsetLowerDword;
-    long       dwThumbnailOffsetHigherDword;
-    long       dwThumbnailSize;
-    unsigned char guidGameId[16];
-    unsigned short szGameName[RM_MAXLENGTH];
-    unsigned short szSaveName[RM_MAXLENGTH];
-    unsigned short szLevelName[RM_MAXLENGTH];
-    unsigned short szComments[RM_MAXLENGTH];
-} RICH_GAME_MEDIA_HEADER;
-#pragma pack(pop)
-
-
 #ifdef WINDOWS_VERSION
 int wArgc;
 LPWSTR *wArgv;
---
+++ Engine/acmain/ac_richgamemedia.h
@@ -0,0 +1,27 @@
+#ifndef __AC_RICHGAMEMEDIA_H
+#define __AC_RICHGAMEMEDIA_H
+
+// Windows Vista Rich Save Games, modified to be platform-agnostic
+
+#define RM_MAXLENGTH    1024
+#define RM_MAGICNUMBER  "RGMH"
+
+#pragma pack(push)
+#pragma pack(1)
+typedef struct _RICH_GAME_MEDIA_HEADER
+{
+    long       dwMagicNumber;
+    long       dwHeaderVersion;
+    long       dwHeaderSize;
+    long       dwThumbnailOffsetLowerDword;
+    long       dwThumbnailOffsetHigherDword;
+    long       dwThumbnailSize;
+    unsigned char guidGameId[16];
+    unsigned short szGameName[RM_MAXLENGTH];
+    unsigned short szSaveName[RM_MAXLENGTH];
+    unsigned short szLevelName[RM_MAXLENGTH];
+    unsigned short szComments[RM_MAXLENGTH];
+} RICH_GAME_MEDIA_HEADER;
+#pragma pack(pop)
+
+#endif // __AC_RICHGAMEMEDIA_H

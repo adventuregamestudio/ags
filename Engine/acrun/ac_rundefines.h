43aa3017bb449dc3b28a22725a5b13741e5c4b60
Tobias Hansen <tobias.han@gmx.de>
2012-06-22 01:42:36 +0200

    Compile on Linux: Define HWND also in /acrun/ac_rundefines.h.

--- Engine/acrun/ac_rundefines.h
+++ Engine/acrun/ac_rundefines.h
@@ -126,6 +126,10 @@
 #define Sleep(x) usleep(1000*x)
 #endif
 
+#if defined(LINUX_VERSION) || defined(MAC_VERSION)
+#define HWND long
+#endif
+
 #define BASEWIDTH usetup.base_width
 #define BASEHEIGHT usetup.base_height
 #define TRANS_ALPHA_CHANNEL 20000

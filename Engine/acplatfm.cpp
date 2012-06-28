2a24e0e4b604c2c60ade39ed2b45c24ea806d879
jjsat <jjs@jjs.at>
2012-06-28 15:58:31 +0200

    Engine: Don't try to unload the library for a built-in plugin.

--- Engine/acplatfm.cpp
+++ Engine/acplatfm.cpp
@@ -169,6 +169,7 @@ struct EnginePlugin {
   void      (*initGfxHook) (const char *driverName, void *data);
   int       (*debugHook) (const char * whichscript, int lineNumber, int reserved);
   IAGSEngine  eiface;
+  bool        builtin;
  
   EnginePlugin() {
     filename[0] = 0;
@@ -176,6 +177,7 @@ struct EnginePlugin {
     wantHook = 0;
     invalidatedRegion = 0;
     savedata = NULL;
+    builtin = false;
   }
 };
 #define MAXPLUGINS 20
@@ -799,11 +801,13 @@ void pl_stop_plugins() {
         free(plugins[a].savedata);
         plugins[a].savedata = NULL;
       }
+      if (!plugins[a].builtin) {
 #ifdef PSP_VERSION
-      sceKernelUnloadModule(plugins[a].dllHandle);
+        sceKernelUnloadModule(plugins[a].dllHandle);
 #else
-      FreeLibrary (plugins[a].dllHandle);
+        FreeLibrary (plugins[a].dllHandle);
 #endif
+      }
     }
   }
   numPlugins = 0;
@@ -865,6 +869,7 @@ bool pl_use_builtin_plugin(EnginePlugin* apl)
     apl->debugHook = agsflashlight::AGS_EngineDebugHook;
     apl->initGfxHook = agsflashlight::AGS_EngineInitGfx;
     apl->dllHandle = (HINSTANCE)1;
+    apl->builtin = true;
     return true;
   }
   else if (strncmp(apl->filename, "agsblend", strlen("agsblend")) == 0)
@@ -875,6 +880,7 @@ bool pl_use_builtin_plugin(EnginePlugin* apl)
     apl->debugHook = agsblend::AGS_EngineDebugHook;
     apl->initGfxHook = agsblend::AGS_EngineInitGfx;
     apl->dllHandle = (HINSTANCE)1;
+    apl->builtin = true;
     return true;
   }
   else if (strncmp(apl->filename, "ags_snowrain", strlen("ags_snowrain")) == 0)
@@ -885,6 +891,7 @@ bool pl_use_builtin_plugin(EnginePlugin* apl)
     apl->debugHook = ags_snowrain::AGS_EngineDebugHook;
     apl->initGfxHook = ags_snowrain::AGS_EngineInitGfx;
     apl->dllHandle = (HINSTANCE)1;
+    apl->builtin = true;
     return true;
   }
 #endif

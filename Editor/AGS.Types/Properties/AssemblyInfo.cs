6786f11e0b0bc7e36fab2a20bb4035d3672bad8c
Ivan Mogilko <ikm_spb@yahoo.com>
2014-01-27 22:06:00 +0400

    Editor: remove "beta version" tag

--- Editor/AGS.Types/Properties/AssemblyInfo.cs
+++ Editor/AGS.Types/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ namespace AGS.Types
 {
 	public class Version
 	{
-		public static readonly bool IS_BETA_VERSION = true;
+		public static readonly bool IS_BETA_VERSION = false;
 		public const string AGS_EDITOR_DATE = "January 2014";
 		public const string AGS_EDITOR_FRIENDLY_VERSION = "3.3.0";
         public const string AGS_EDITOR_VERSION = "3.3.0.1154";

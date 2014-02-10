adventuregamestudio/ags

commit 1c8164d3b11d28a68f9cddcdab521e8be445ecf3

Getting rid of direct reading int32s into arrays of pointers

Editor/AGS.Native/agsnative.cpp
@@ -4659,6 +4659,13 @@ void serialize_room_interactions(Stream *ooo)
 	}
 }
 
+void WriteGameSetupStructBase_Aligned(Stream *out)
+{
+    GameSetupStructBase *gameBase = (GameSetupStructBase *)&thisgame;
+    AlignedStream align_s(out, Common::kAligned_Write);
+    gameBase->WriteToFile(&align_s);
+}
+
 void save_thisgame_to_file(const char *fileName, Game ^game)
 {
     Common::String ags_version;
@@ -4678,7 +4685,7 @@ void save_thisgame_to_file(const char *fileName, Game ^game)
   ooo->WriteInt32(ags_version.GetLength());
   ooo->Write(ags_version, ags_version.GetLength());
 
-  ooo->WriteArray(&thisgame, sizeof (GameSetupStructBase), 1);
+  WriteGameSetupStructBase_Aligned(ooo);
   ooo->Write(&thisgame.guid[0], MAX_GUID_LENGTH);
   ooo->Write(&thisgame.saveGameFileExtension[0], MAX_SG_EXT_LENGTH);
   ooo->Write(&thisgame.saveGameFolderName[0], MAX_SG_FOLDER_LEN);

Common/ac/gamesetupstruct.cpp
@@ -306,7 +306,7 @@ void GameSetupStruct::read_lipsync(Common::Stream *in, GAME_STRUCT_READ_DATA &re
 void GameSetupStruct::read_messages(Common::Stream *in, GAME_STRUCT_READ_DATA &read_data)
 {
     for (int ee=0;ee<MAXGLOBALMES;ee++) {
-        if (messages[ee]==NULL) continue;
+        if (!load_messages[ee]) continue;
         messages[ee]=(char*)malloc(500);
 
         if (read_data.filever < kGameVersion_261) // Global messages are not encrypted on < 2.61
@@ -324,6 +324,8 @@ void GameSetupStruct::read_messages(Common::Stream *in, GAME_STRUCT_READ_DATA &r
         else
             read_string_decrypt(in, messages[ee]);
     }
+    delete [] load_messages;
+    load_messages = NULL;
 }
 
 void GameSetupStruct::ReadCharacters_Aligned(Stream *in)

Common/ac/gamesetupstructbase.cpp
@@ -17,6 +17,23 @@
 
 using AGS::Common::Stream;
 
+GameSetupStructBase::GameSetupStructBase()
+    : dict(NULL)
+    , globalscript(NULL)
+    , chars(NULL)
+    , compiled_script(NULL)
+    , load_messages(NULL)
+    , load_dictionary(false)
+    , load_compiled_script(false)
+{
+    memset(messages, 0, sizeof(messages));
+}
+
+GameSetupStructBase::~GameSetupStructBase()
+{
+    delete [] load_messages;
+}
+
 void GameSetupStructBase::ReadFromFile(Stream *in)
 {
     in->Read(&gamename[0], 50);
@@ -44,12 +61,8 @@ void GameSetupStructBase::ReadFromFile(Stream *in)
     default_lipsync_frame = in->ReadInt32();
     invhotdotsprite = in->ReadInt32();
     in->ReadArrayOfInt32(reserved, 17);
-    // read the final ptrs so we know to load dictionary, scripts etc
-    // 64 bit: Read 4 byte values into array of 8 byte
-    in->ReadArrayOfIntPtr32((intptr_t*)messages, MAXGLOBALMES);
-    //int i;
-    //for (i = 0; i < MAXGLOBALMES; i++)
-    //  messages[i] = (char*)in->ReadInt32();
+    load_messages = new int32_t[MAXGLOBALMES];
+    in->ReadArrayOfInt32(load_messages, MAXGLOBALMES);
 
     // - GameSetupStruct::read_words_dictionary() checks load_dictionary
     // - load_game_file() checks load_compiled_script
@@ -86,8 +99,10 @@ void GameSetupStructBase::WriteToFile(Stream *out)
     out->WriteInt32(default_lipsync_frame);
     out->WriteInt32(invhotdotsprite);
     out->WriteArrayOfInt32(reserved, 17);
-    // write the final ptrs so we know to load dictionary, scripts etc
-    out->WriteArrayOfIntPtr32((intptr_t*)messages, MAXGLOBALMES);
+    for (int i = 0; i < MAXGLOBALMES; ++i)
+    {
+        out->WriteInt32(messages[i] ? 1 : 0);
+    }
     out->WriteInt32(dict ? 1 : 0);
     out->WriteInt32(0); // globalscript
     out->WriteInt32(0); // chars

Common/ac/gamesetupstructbase.h
@@ -28,11 +28,6 @@
 namespace AGS { namespace Common { class Stream; } }
 using namespace AGS; // FIXME later
 
-//
-// This struct is still written by copying plain byte data in
-// AGS.Native (the editor); it must not be changed any way
-// until the game compilation was improved.
-//
 struct GameSetupStructBase {
     static const int  MAX_OPTIONS = 100;
 
@@ -63,6 +58,8 @@ struct GameSetupStructBase {
     char             *globalscript;
     CharacterInfo    *chars;
     ccScript         *compiled_script;
+
+    int32_t          *load_messages;
     bool             load_dictionary;
     bool             load_compiled_script;
     // [IKM] 2013-03-30
@@ -70,6 +67,8 @@ struct GameSetupStructBase {
     // to store actual script data anytime; 'ccScript* gamescript' global
     // pointer is used for that instead.
 
+    GameSetupStructBase();
+    virtual ~GameSetupStructBase();
     void ReadFromFile(Common::Stream *in);
     void WriteToFile(Common::Stream *out);
 };

Common/gui/guimain.cpp
@@ -102,11 +102,8 @@ void GUIMain::ReadFromFile(Stream *in, GuiVersion gui_version)
   in->Read(vtext, 40);
   in->ReadArrayOfInt32(&x, 27);
 
-  // 64 bit fix: Read 4 byte int values into array of 8 byte long ints
-  in->ReadArrayOfIntPtr32((intptr_t*)objs, MAX_OBJS_ON_GUI);
-  //int i;
-  //for (i = 0; i < MAX_OBJS_ON_GUI; i++)
-  //  objs[i] = (GUIObject*)in->ReadInt32();
+  // array of 32-bit pointers; these values are unused
+  in->Seek(AGS::Common::kSeekCurrent, MAX_OBJS_ON_GUI * sizeof(int32_t));
 
   in->ReadArrayOfInt32(objrefptr, MAX_OBJS_ON_GUI);
 }
@@ -116,11 +113,9 @@ void GUIMain::WriteToFile(Stream *out)
   out->Write(vtext, 40);
   out->WriteArrayOfInt32(&x, 27);
 
-  // 64 bit fix: Write 4 byte int values from array of 8 byte long ints
-  out->WriteArrayOfIntPtr32((intptr_t*)objs, MAX_OBJS_ON_GUI);
-  //int i;
-  //for (i = 0; i < MAX_OBJS_ON_GUI; i++)
-  //  out->WriteArray(&objs[i], 4, 1);
+  // array of dummy 32-bit pointers
+  int32_t dummy_arr[MAX_OBJS_ON_GUI];
+  out->WriteArrayOfInt32(dummy_arr, MAX_OBJS_ON_GUI);
 
   out->WriteArrayOfInt32((int32_t*)&objrefptr, MAX_OBJS_ON_GUI);
 }

commit 93d13bdabc1d15f142fa7b0d8e0276137de0f92f

AGS.Native: fixed import from 2.72 projects
Was broken by: 1c8164d3b11d28a68f9cddcdab521e8be445ecf3

Editor/AGS.Native/agsnative.cpp
@@ -1624,10 +1624,12 @@ const char *load_dta_file_into_thisgame(const char *fileName)
   iii->ReadArray (&thisgame.lipSyncFrameLetters[0][0], 20, 50);
 
   for (bb=0;bb<MAXGLOBALMES;bb++) {
-    if (thisgame.messages[bb]==NULL) continue;
+    if (!thisgame.load_messages[bb]) continue;
     thisgame.messages[bb]=(char*)malloc(500);
     read_string_decrypt(iii, thisgame.messages[bb]);
   }
+  delete [] thisgame.load_messages;
+  thisgame.load_messages = NULL;
 
   read_dialogs(iii, filever, true);
   read_gui(iii,&guis[0],&thisgame, &guis);
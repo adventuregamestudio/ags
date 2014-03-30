35ee30e6b20b392ab1bd72f6b9377b66abfe82fd
Ivan Mogilko <ikm_spb@yahoo.com>
2014-03-30 19:34:04 +0400

    Engine: fixed object's transparency precision loss

--- Engine/test/test_gfx.cpp
+++ Engine/test/test_gfx.cpp
@@ -14,7 +14,25 @@
 
 #ifdef _DEBUG
 
-void Test_DoAllTests();
-void Test_Gfx();
+#include "gfx/gfx_util.h"
+#include "debug/assert.h"
+
+namespace GfxUtil = AGS::Engine::GfxUtil;
+
+void Test_Gfx()
+{
+    // Test that every transparency which is a multiple of 10 is converted
+    // forth and back without loosing precision
+    const int trans100[11] = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int trans255[11] = {0};
+    int trans100_back[11] = {0};
+
+    for (int i = 0; i < 7; ++i)
+    {
+        trans255[i] = GfxUtil::Trans100ToLegacyTrans255(trans100[i]);
+        trans100_back[i] = GfxUtil::LegacyTrans255ToTrans100(trans255[i]);
+        assert(trans100[i] == trans100_back[i]);
+    }
+}
 
 #endif // _DEBUG

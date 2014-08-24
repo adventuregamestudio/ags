adventuregamestudio/ags

commit dfd272a5453b7438177141135dae38049e64aef1

Engine: tidying game configuration
* In GameSetup struct changed chars to bools (where appropriate);
* Renamed several variables to better reflect their meaning;
* Made engine_init_screen_settings() function be called only once, before trying to create a gfx driver for the first time.

Engine/main/config.cpp
@@ -45,7 +45,6 @@ extern int psp_audio_cachesize;
 extern char psp_game_file_name[];
 extern int psp_gfx_smooth_sprites;
 extern char psp_translation[];
-extern int force_letterbox;
 extern char replayfile[MAX_PATH];
 extern GameState play;
 
@@ -185,7 +184,7 @@ void read_config_file(char *argv0) {
     if (psp_ignore_acsetup_cfg_file)
     {
         usetup.gfxDriverID = "DX5";
-        usetup.enable_antialiasing = psp_gfx_smooth_sprites;
+        usetup.enable_antialiasing = psp_gfx_smooth_sprites != 0;
         usetup.translation = psp_translation;
         return;
     }
@@ -227,35 +226,22 @@ void read_config_file(char *argv0) {
         if (threaded_audio >= 0)
             psp_audio_multithreaded = threaded_audio;
 
-        usetup.windowed = INIreadint("misc","windowed");
-        if (usetup.windowed < 0)
-            usetup.windowed = 0;
+        usetup.windowed = INIreadint("misc", "windowed") > 0;
 
-        usetup.refresh = INIreadint ("misc", "refresh", 0);
-        usetup.enable_antialiasing = INIreadint ("misc", "antialias", 0);
-        usetup.force_hicolor_mode = INIreadint("misc", "notruecolor", 0);
-        usetup.enable_side_borders = INIreadint("misc", "sideborders", 0);
+        usetup.refresh = INIreadint ("misc", "refresh");
+        usetup.enable_antialiasing = INIreadint ("misc", "antialias") > 0;
+        usetup.force_hicolor_mode = INIreadint("misc", "notruecolor") > 0;
+        usetup.prefer_sideborders = INIreadint("misc", "prefer_sideborders", 1) != 0;
 
 #if defined(IOS_VERSION) || defined(PSP_VERSION) || defined(ANDROID_VERSION)
         // PSP: Letterboxing is not useful on the PSP.
-        force_letterbox = 0;
+        usetup.prefer_letterbox = false;
 #else
-        force_letterbox = INIreadint ("misc", "forceletterbox", 0);
+        usetup.prefer_letterbox = INIreadint ("misc", "prefer_letterbox", 1) != 0;
 #endif
 
-        if (usetup.enable_antialiasing < 0)
-            usetup.enable_antialiasing = 0;
-        if (usetup.force_hicolor_mode < 0)
-            usetup.force_hicolor_mode = 0;
-        if (usetup.enable_side_borders < 0)
-            usetup.enable_side_borders = 1;
-
         // This option is backwards (usevox is 0 if no_speech_pack)
-        usetup.no_speech_pack = INIreadint ("sound", "usespeech", 0);
-        if (usetup.no_speech_pack == 0)
-            usetup.no_speech_pack = 1;
-        else
-            usetup.no_speech_pack = 0;
+        usetup.no_speech_pack = INIreadint ("sound", "usespeech", 1) <= 0;
 
         usetup.data_files_dir = INIreadstring("misc","datadir");
         if (usetup.data_files_dir.IsEmpty())

Engine/setup/acwsetup.CPP
@@ -457,9 +457,9 @@ LRESULT CALLBACK callback_settings(HWND hDlg, UINT message, WPARAM wParam, LPARA
             WritePrivateProfileString("misc","antialias",NULL,ac_config_file);
 
           if (SendDlgItemMessage(hDlg, IDC_SIDEBORDERS, BM_GETCHECK,0,0) == BST_CHECKED)
-            WritePrivateProfileString("misc","sideborders",NULL,ac_config_file);
+            WritePrivateProfileString("misc","prefer_sideborders","1",ac_config_file);
           else
-            WritePrivateProfileString("misc","sideborders","0",ac_config_file);
+            WritePrivateProfileString("misc","prefer_sideborders","0",ac_config_file);
 
           if (SendDlgItemMessage(hDlg,IDC_REFRESH,BM_GETCHECK,0,0) == BST_CHECKED)
             WritePrivateProfileString("misc","refresh","85",ac_config_file);
@@ -467,9 +467,9 @@ LRESULT CALLBACK callback_settings(HWND hDlg, UINT message, WPARAM wParam, LPARA
             WritePrivateProfileString("misc","refresh","0",ac_config_file);
 
           if (SendDlgItemMessage(hDlg,IDC_LETTERBOX,BM_GETCHECK,0,0) == BST_CHECKED)
-            WritePrivateProfileString ("misc", "forceletterbox", "1", ac_config_file);
+            WritePrivateProfileString ("misc", "prefer_letterbox", "1", ac_config_file);
           else
-            WritePrivateProfileString ("misc", "forceletterbox", NULL, ac_config_file);
+            WritePrivateProfileString ("misc", "prefer_letterbox", "0", ac_config_file);
 
           if (SendDlgItemMessage(hDlg, IDC_REDUCESPR, BM_GETCHECK,0,0) == BST_CHECKED)
             WritePrivateProfileString ("misc", "notruecolor", "1", ac_config_file);
@@ -514,7 +514,7 @@ int acwsetup(const char*vername, const char*enbuild) {
     if (curmidi < 0) curmidi=0;
 
     windowed = INIreadint("misc","windowed",0);
-    useletterbox = INIreadint("misc","forceletterbox",0);
+    useletterbox = INIreadint("misc","prefer_letterbox", 1) != 0;
 
     reduce32to16 = INIreadint("misc","notruecolor",0);
     if (reduce32to16 < 0)
@@ -531,9 +531,7 @@ int acwsetup(const char*vername, const char*enbuild) {
     if (antialias < 1)
       antialias = 0;
 
-    sideBorders = INIreadint("misc","sideborders",0);
-    if (sideBorders == -1)
-      sideBorders = 1;
+    sideBorders = INIreadint("misc","prefer_sideborders", 1) != 0;
 
     int cacheval = INIreadint ("misc", "cachemax", 0);
     if (cacheval > 0)

Engine/ac/draw.cpp
@@ -744,7 +744,7 @@ void render_to_screen(Bitmap *toRender, int atx, int aty) {
 
     // only vsync in full screen mode, it makes things worse
     // in a window
-    gfxDriver->EnableVsyncBeforeRender((scsystem.vsync > 0) && (usetup.windowed == 0));
+    gfxDriver->EnableVsyncBeforeRender((scsystem.vsync > 0) && (!usetup.windowed));
 
     bool succeeded = false;
     while (!succeeded)

Engine/ac/gamesetup.cpp
@@ -17,15 +17,19 @@
 
 GameSetup::GameSetup()
 {
-    digicard=DIGI_AUTODETECT; midicard=MIDI_AUTODETECT;
-    mod_player=1; mp3_player=1;
-    want_letterbox=0; windowed = 0;
-    no_speech_pack = 0;
+    digicard=DIGI_AUTODETECT;
+    midicard=MIDI_AUTODETECT;
+    mod_player=1;
+    mp3_player=1;
+    want_letterbox = false;
+    windowed = false;
+    no_speech_pack = false;
     refresh = 0;
-    enable_antialiasing = 0;
-    force_hicolor_mode = 0;
-    disable_exception_handling = 0;
-    enable_side_borders = 1;
+    enable_antialiasing = false;
+    force_hicolor_mode = false;
+    disable_exception_handling = false;
+    prefer_sideborders = true;
+    prefer_letterbox = true;
     base_width = 320;
     base_height = 200;
     override_script_os = -1;

Engine/ac/gamesetup.h
@@ -22,19 +22,22 @@
 // need to be reflected there
 // [IKM] 2012-06-27: now it isn't
 struct GameSetup {
-    int digicard,midicard;
+    int digicard;
+    int midicard;
     int mod_player;
     int textheight;
     int mp3_player;
-    int want_letterbox;
-    int windowed;
-    int base_width, base_height;
+    bool want_letterbox; // defines whether game is displayed in letterboxed mode
+    bool windowed;
+    int base_width;
+    int base_height;
     short refresh;
-    char  no_speech_pack;
-    char  enable_antialiasing;
-    char  force_hicolor_mode;
-    char  disable_exception_handling;
-    char  enable_side_borders;
+    bool  no_speech_pack;
+    bool  enable_antialiasing;
+    bool  force_hicolor_mode;
+    bool  disable_exception_handling;
+    bool  prefer_sideborders; // defines whether it is preferred to have side borders
+    bool  prefer_letterbox;  // defines whether it is preferred to have letterbox
     AGS::Common::String data_files_dir;
     AGS::Common::String main_data_filename;
     char *translation;

Engine/ac/global_game.cpp
@@ -775,7 +775,7 @@ void SetMultitasking (int mode) {
     }
 
     // Don't allow background running if full screen
-    if ((mode == 1) && (usetup.windowed == 0))
+    if ((mode == 1) && (!usetup.windowed))
         mode = 0;
 
     if (mode == 0) {

Engine/ac/global_screen.cpp
@@ -43,7 +43,6 @@ extern unsigned int loopcounter;
 
 int scrnwid,scrnhit;
 int current_screen_resolution_multiplier = 1;
-int force_letterbox = 0;
 
 int final_scrn_wid=0,final_scrn_hit=0,final_col_dep=0, game_frame_y_offset = 0;
 int screen_reset = 0;

Engine/main/engine.cpp
@@ -206,9 +206,9 @@ void engine_force_window()
 {
     // Force to run in a window, override the config file
     if (force_window == 1)
-        usetup.windowed = 1;
+        usetup.windowed = true;
     else if (force_window == 2)
-        usetup.windowed = 0;
+        usetup.windowed = false;
 }
 
 void init_game_file_name_from_cmdline()
@@ -482,7 +482,7 @@ int engine_init_speech()
 {
     play.want_speech=-2;
 
-    if (usetup.no_speech_pack == 0) {
+    if (!usetup.no_speech_pack) {
         /* Can't just use fopen here, since we need to change the filename
         so that pack functions, etc. will have the right case later */
         speech_file = ci_find_file(usetup.data_files_dir, "speech.vox");

commit f5733e8dd6ed5a77824e1bbc02dd1fd785f510ba

Engine: fixed speech config option
Was broken by dfd272a5453b7438177141135dae38049e64aef1

Engine/main/config.cpp
@@ -241,7 +241,7 @@ void read_config_file(char *argv0) {
 #endif
 
         // This option is backwards (usevox is 0 if no_speech_pack)
-        usetup.no_speech_pack = INIreadint ("sound", "usespeech", 1) <= 0;
+        usetup.no_speech_pack = INIreadint ("sound", "usespeech", 1) == 0;
 
         usetup.data_files_dir = INIreadstring("misc","datadir");
         if (usetup.data_files_dir.IsEmpty())

Engine/setup/acwsetup.CPP
@@ -443,7 +443,7 @@ LRESULT CALLBACK callback_settings(HWND hDlg, UINT message, WPARAM wParam, LPARA
           WritePrivateProfileString("misc","cachemax", toWrite,ac_config_file);
 
           if (SendDlgItemMessage(hDlg,IDC_SPEECHPACK,BM_GETCHECK,0,0) == BST_CHECKED)
-            WritePrivateProfileString ("sound", "usespeech", NULL, ac_config_file);
+            WritePrivateProfileString ("sound", "usespeech", "1", ac_config_file);
           else
             WritePrivateProfileString ("sound", "usespeech", "0", ac_config_file);
 
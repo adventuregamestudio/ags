adventuregamestudio/ags

commit b08134492adb78a08d14a04c6b801f88530328e1

Game resolution is now enum

Common/ac/gamesetupstructbase.cpp
@@ -57,7 +57,7 @@ void GameSetupStructBase::ReadFromFile(Stream *in)
     uniqueid = in->ReadInt32();
     numgui = in->ReadInt32();
     numcursors = in->ReadInt32();
-    default_resolution = in->ReadInt32();
+    default_resolution = (GameResolutionType)in->ReadInt32();
     default_lipsync_frame = in->ReadInt32();
     invhotdotsprite = in->ReadInt32();
     in->ReadArrayOfInt32(reserved, 17);

Common/ac/gamestructdefines.h
@@ -106,4 +106,24 @@
 
 #define MAX_DIALOG        500
 
+enum GameResolutionType
+{
+    kGameResolution_Undefined   = 0,
+    kGameResolution_320x200     = 1,
+    kGameResolution_320x240     = 2,
+    kGameResolution_640x400     = 3,
+    kGameResolution_640x480     = 4,
+    kGameResolution_800x600     = 5,
+    kGameResolution_1024x768    = 6,
+    kNumGameResolutions,
+
+    kGameResolution_LastLoRes   = kGameResolution_320x240,
+    kGameResolution_FirstHiRes  = kGameResolution_640x400
+};
+
+inline bool IsHiRes(GameResolutionType resolution)
+{
+    return resolution > kGameResolution_LastLoRes;
+}
+
 #endif // __AGS_CN_AC__GAMESTRUCTDEFINES_H

Engine/main/graphics_mode.cpp
@@ -131,7 +131,7 @@ void adjust_sizes_for_resolution(int filever)
     }
 
     if ((filever >= 37) && (game.options[OPT_NATIVECOORDINATES] == 0) &&
-        (game.default_resolution > 2))
+        game.IsHiRes())
     {
         // New 3.1 format game file, but with Use Native Coordinates off
 
@@ -195,69 +195,45 @@ void engine_init_screen_settings()
     usetup.base_width = 320;
     usetup.base_height = 200;
 
-    if (game.default_resolution >= 5)
+    switch (game.default_resolution)
     {
-        if (game.default_resolution >= 6)
-        {
-            // 1024x768
-            scrnwid = 1024;
-            scrnhit = 768;
-        }
-        else
-        {
-            // 800x600
-            scrnwid = 800;
-            scrnhit = 600;
-        }
-        // don't allow letterbox mode
-        wtext_multiply = 2;
-    }
-    else if ((game.default_resolution == 4) ||
-        (game.default_resolution == 3))
-    {
-        if (game.default_resolution == 4)
-        {
-            scrnwid = 640;
-            scrnhit = 480;
-        }
-        else
-        {
-            scrnwid = 640;
-            scrnhit = 400;
-        }
-        wtext_multiply = 2;
-    }
-    else if ((game.default_resolution == 2) ||
-        (game.default_resolution == 1))
-    {
-        if ((game.default_resolution == 2))
-        {
-            scrnwid = 320;
-            scrnhit = 240;
-        }
-        else
-        {
-            scrnwid = 320;
-            scrnhit = 200;
-        }
-        wtext_multiply = 1;
-    }
-    else
-    {
-        scrnwid = usetup.base_width;
-        scrnhit = usetup.base_height;
-        wtext_multiply = 1;
+    case kGameResolution_320x200:
+        scrnwid = 320;
+        scrnhit = 200;
+        break;
+    case kGameResolution_320x240:
+        scrnwid = 320;
+        scrnhit = 240;
+        break;
+    case kGameResolution_640x400:
+        scrnwid = 640;
+        scrnhit = 400;
+        break;
+    case kGameResolution_640x480:
+        scrnwid = 640;
+        scrnhit = 480;
+        break;
+    case kGameResolution_800x600:
+        scrnwid = 800;
+        scrnhit = 600;
+        break;
+    case kGameResolution_1024x768:
+        scrnwid = 1024;
+        scrnhit = 768;
+        break;
     }
 
-    if (game.default_resolution > 2)
+    if (game.IsHiRes())
     {
         usetup.base_width = scrnwid / 2;
         usetup.base_height = scrnhit / 2;
+        wtext_multiply = 2;
     }
     else
     {
         usetup.base_width = scrnwid;
         usetup.base_height = scrnhit;
+        wtext_multiply = 1;
     }
 
     usetup.textheight = wgetfontheight(0) + 1;
@@ -265,7 +241,7 @@ void engine_init_screen_settings()
     vesa_xres=scrnwid; vesa_yres=scrnhit;
     current_screen_resolution_multiplier = scrnwid / BASEWIDTH;
 
-    if ((game.default_resolution > 2) &&
+    if (game.IsHiRes() &&
         (game.options[OPT_NATIVECOORDINATES]))
     {
         usetup.base_width *= 2;

Common/ac/gamesetupstructbase.h
@@ -49,7 +49,7 @@ struct GameSetupStructBase {
     int32             uniqueid;    // random key identifying the game
     int32             numgui;
     int32             numcursors;
-    int32             default_resolution; // 0=undefined, 1=320x200, 2=320x240, 3=640x400 etc
+    GameResolutionType default_resolution;
     int32             default_lipsync_frame; // used for unknown chars
     int32             invhotdotsprite;
     int32             reserved[17];
@@ -71,6 +71,11 @@ struct GameSetupStructBase {
     virtual ~GameSetupStructBase();
     void ReadFromFile(Common::Stream *in);
     void WriteToFile(Common::Stream *out);
+
+    inline bool IsHiRes() const
+    {
+        return ::IsHiRes(default_resolution);
+    }
 };
 
 #endif // __AGS_CN_AC__GAMESETUPSTRUCTBASE_H

Editor/AGS.Native/agsnative.cpp
@@ -941,7 +941,7 @@ void update_font_sizes() {
   // scale up fonts if necessary
   wtext_multiply = 1;
   if ((thisgame.options[OPT_NOSCALEFNT] == 0) &&
-      (thisgame.default_resolution >= 3)) {
+      thisgame.IsHiRes()) {
     wtext_multiply = 2;
   }
 
@@ -951,7 +951,7 @@ void update_font_sizes() {
       reload_font (bb);
   }
 
-  if (thisgame.default_resolution >= 3) {
+  if (thisgame.IsHiRes()) {
     sxmult = 2;
     symult = 2;
   }
@@ -1038,8 +1038,8 @@ void drawFontAt (int hdc, int fontnum, int x,int y) {
 
   update_font_sizes();
 
-  int doubleSize = (thisgame.default_resolution < 3) ? 2 : 1;
-  int blockSize = (thisgame.default_resolution < 3) ? 1 : 2;
+  int doubleSize = (!thisgame.IsHiRes()) ? 2 : 1;
+  int blockSize = (!thisgame.IsHiRes()) ? 1 : 2;
   antiAliasFonts = thisgame.options[OPT_ANTIALIASFONTS];
 
   // we can't antialias font because changing col dep to 16 here causes
@@ -1417,10 +1417,10 @@ bool reload_font(int curFont)
   int fsize = thisgame.fontflags[curFont] & FFLG_SIZEMASK;
   // if the font is designed for 640x400, half it
   if (thisgame.options[OPT_NOSCALEFNT]) {
-    if (thisgame.default_resolution <= 2)
+    if (!thisgame.IsHiRes())
       fsize /= 2;
   }
-  else if (thisgame.default_resolution >= 3) {
+  else if (thisgame.IsHiRes()) {
     // designed for 320x200, double it up
     fsize *= 2;
   }
@@ -1900,7 +1900,7 @@ void copy_global_palette_to_room_palette()
 
 const char* load_room_file(const char*rtlo) {
 
-  load_room((char*)rtlo, &thisroom, (thisgame.default_resolution > 2));
+  load_room((char*)rtlo, &thisroom, thisgame.IsHiRes());
 
   if (thisroom.wasversion < kRoomVersion_250b) 
   {
@@ -2667,7 +2667,7 @@ void DrawSpriteToBuffer(int sprNum, int x, int y, int scaleFactor) {
 	  todraw = spriteset[0];
 
 	if (((thisgame.spriteflags[sprNum] & SPF_640x400) == 0) &&
-		(thisgame.default_resolution > 2))
+		thisgame.IsHiRes())
 	{
 		scaleFactor *= 2;
 	}
@@ -2736,7 +2736,7 @@ void UpdateSpriteFlags(SpriteFolder ^folder)
 
 void GameUpdated(Game ^game) {
   thisgame.color_depth = (int)game->Settings->ColorDepth;
-  thisgame.default_resolution = (int)game->Settings->Resolution;
+  thisgame.default_resolution = (GameResolutionType)game->Settings->Resolution;
 
   thisgame.options[OPT_NOSCALEFNT] = game->Settings->FontsForHiRes;
   thisgame.options[OPT_ANTIALIASFONTS] = game->Settings->AntiAliasFonts;
@@ -3071,7 +3071,7 @@ int SetNewSpriteFromBitmap(int slot, System::Drawing::Bitmap^ bmp, int spriteImp
 	}
 
 	thisgame.spriteflags[slot] = 0;
-	if (thisgame.default_resolution > 2)
+	if (thisgame.IsHiRes())
 	{
 		thisgame.spriteflags[slot] |= SPF_640x400;
 	}
@@ -3091,7 +3091,7 @@ int SetNewSpriteFromBitmap(int slot, System::Drawing::Bitmap^ bmp, int spriteImp
 
 	SetNewSprite(slot, tempsprite);
 
-	return (thisgame.default_resolution > 2) ? 1 : 0;
+	return thisgame.IsHiRes() ? 1 : 0;
 }
 
 void SetBitmapPaletteFromGlobalPalette(System::Drawing::Bitmap ^bmp)
@@ -4875,7 +4875,7 @@ void save_game_to_dta_file(Game^ game, const char *fileName)
 	thisgame.options[OPT_DIALOGNUMBERED] = game->Settings->NumberDialogOptions;
 	thisgame.options[OPT_PIXPERFECT] = game->Settings->PixelPerfect;
 	thisgame.options[OPT_SCORESOUND] = 0; // saved elsewhere now to make it 32-bit
-	thisgame.default_resolution = (int)game->Settings->Resolution;
+	thisgame.default_resolution = (GameResolutionType)game->Settings->Resolution;
 	thisgame.options[OPT_FADETYPE] = (int)game->Settings->RoomTransition;
 	thisgame.options[OPT_RUNGAMEDLGOPTS] = game->Settings->RunGameLoopsWhileDialogOptionsDisplayed;
 	thisgame.options[OPT_SAVESCREENSHOT] = game->Settings->SaveScreenshots;

Engine/ac/character.cpp
@@ -615,12 +615,12 @@ void Character_LockViewFrame(CharacterInfo *chaa, int view, int loop, int frame)
 void Character_LockViewOffset(CharacterInfo *chap, int vii, int xoffs, int yoffs) {
     Character_LockView(chap, vii);
 
-    if ((current_screen_resolution_multiplier == 1) && (game.default_resolution >= 3)) {
+    if ((current_screen_resolution_multiplier == 1) && (game.IsHiRes())) {
         // running a 640x400 game at 320x200, adjust
         xoffs /= 2;
         yoffs /= 2;
     }
-    else if ((current_screen_resolution_multiplier > 1) && (game.default_resolution <= 2)) {
+    else if ((current_screen_resolution_multiplier > 1) && (!game.IsHiRes())) {
         // running a 320x200 game at 640x400, adjust
         xoffs *= 2;
         yoffs *= 2;
@@ -1563,7 +1563,7 @@ void walk_character(int chac,int tox,int toy,int ignwal, bool autoWalkAnims) {
         mls[mslot].direct = ignwal;
 
         if ((game.options[OPT_NATIVECOORDINATES] != 0) &&
-            (game.default_resolution > 2))
+            game.IsHiRes())
         {
             convert_move_path_to_high_res(&mls[mslot]);
         }

Engine/ac/dynobj/scriptdrawingsurface.cpp
@@ -117,7 +117,7 @@ ScriptDrawingSurface::ScriptDrawingSurface()
     highResCoordinates = 0;
 
     if ((game.options[OPT_NATIVECOORDINATES] != 0) &&
-        (game.default_resolution > 2))
+        (game.IsHiRes()))
     {
         highResCoordinates = 1;
     }

Engine/ac/global_drawingsurface.cpp
@@ -209,12 +209,12 @@ void RawDrawImageTrans(int xx, int yy, int slot, int alpha) {
 
 void RawDrawImageOffset(int xx, int yy, int slot) {
 
-    if ((current_screen_resolution_multiplier == 1) && (game.default_resolution >= 3)) {
+    if ((current_screen_resolution_multiplier == 1) && game.IsHiRes()) {
         // running a 640x400 game at 320x200, adjust
         xx /= 2;
         yy /= 2;
     }
-    else if ((current_screen_resolution_multiplier > 1) && (game.default_resolution <= 2)) {
+    else if ((current_screen_resolution_multiplier > 1) && !game.IsHiRes()) {
         // running a 320x200 game at 640x400, adjust
         xx *= 2;
         yy *= 2;

commit 4a333e575b562482a9db5875176b67df4a7a659d

Engine: restored support for old defintion of 320x200 games
This fixes very old (pre 2.56 at least) AGS games which used a different property value for 320x200 game resolution.
The correct behavior was previously broken by b08134492adb78a08d14a04c6b801f88530328e1

Common/ac/gamesetupstructbase.cpp
@@ -113,6 +113,7 @@ Size ResolutionTypeToSize(GameResolutionType resolution, bool letterbox)
 {
     switch (resolution)
     {
+    case kGameResolution_Default:
     case kGameResolution_320x200:
         return letterbox ? Size(320, 240) : Size(320, 200);
     case kGameResolution_320x240:

Common/ac/gamestructdefines.h
@@ -110,7 +110,9 @@
 
 enum GameResolutionType
 {
-    kGameResolution_Undefined   = 0,
+    kGameResolution_Undefined   = -1,
+    // definition of 320x200 in very old versions of the engine (somewhere pre-2.56)
+    kGameResolution_Default     = 0,
     kGameResolution_320x200     = 1,
     kGameResolution_320x240     = 2,
     kGameResolution_640x400     = 3,

Engine/main/graphics_mode.cpp
@@ -191,10 +191,10 @@ void engine_init_screen_settings(Size &game_size, Size &screen_size)
     _rgb_b_shift_15 = 0;
 #endif
 
-    usetup.base_width = 320;
-    usetup.base_height = 200;
-
     GameSize = ResolutionTypeToSize(game.default_resolution);
+    if (GameSize.IsNull())
+        quit("Unable to define native game resolution, could be unsupported game format.");
+
     scrnwid = GameSize.Width;
     scrnhit = GameSize.Height;
 

Engine/setup/acwsetup.CPP
@@ -505,7 +505,7 @@ int acwsetup(const char*vername, const char*enbuild) {
       curscrn = 1;
     defaultRes = (GameResolutionType)INIreadint ("misc", "defaultres", 0);
     if (defaultRes < 1)
-      defaultRes = kGameResolution_Undefined;
+      defaultRes = kGameResolution_Default;
 
     curdigi=INIreadint("sound","digiwinindx");
     curmidi=INIreadint("sound","midiwinindx");
0a165efdc0ad5e740c86a12d75b06d2176fedee4
Michael Rittenhouse <monkey.05.06@gmail.com>
2014-11-06 00:12:54 -0600

    Added interface for unified build process

---
+++ Editor/AGS.Types/BuildTargetsInfo.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+
+namespace AGS.Types
+{
+    public class BuildTargetsInfo
+    {
+        public const string DATAFILE_TARGET_NAME = "DataFile";
+        private static IList<IBuildTarget> _buildTargets;
+
+        static BuildTargetsInfo()
+        {
+            _buildTargets = new List<IBuildTarget>();
+        }
+
+        /// <summary>
+        /// Includes an IBuildTarget in the list of potential target build platforms.
+        /// </summary>
+        public static void RegisterBuildTarget(IBuildTarget target)
+        {
+            if (target == null) throw new ArgumentNullException("Build targets cannot be null!");
+            if (string.IsNullOrEmpty(target.Name)) throw new ArgumentException("Build target names cannot be null or empty!");
+            foreach (IBuildTarget bt in _buildTargets)
+            {
+                if (bt == target) return;   // build target already registered, ignore it
+                if (bt.Name == target.Name) // otherwise, if two targets share the same name
+                {
+                    throw new ArgumentException("Another build target with the same name ('" + target.Name + "') already exists.");
+                }
+            }
+            _buildTargets.Add(target);
+        }
+
+        /// <summary>
+        /// Returns a read-only list of the IBuildTargets that are currently registered,
+        /// regardless of whether they are selected or available for building.
+        /// </summary>
+        public static IList<IBuildTarget> GetRegisteredBuildTargets(bool onlyNormalTargets)
+        {
+            if (!onlyNormalTargets)
+            {
+                return new ReadOnlyCollection<IBuildTarget>(_buildTargets);
+            }
+            IList<IBuildTarget> result = new List<IBuildTarget>(_buildTargets);
+            foreach (IBuildTarget target in _buildTargets)
+            {
+                if (!target.IsNormalBuildTarget) result.Remove(target);
+            }
+            return new ReadOnlyCollection<IBuildTarget>(result);
+        }
+
+        public static IList<IBuildTarget> GetRegisteredBuildTargets()
+        {
+            return GetRegisteredBuildTargets(false);
+        }
+
+        /// <summary>
+        /// Returns a list of the names of all registered build targets.
+        /// </summary>
+        public static string[] GetRegisteredBuildTargetNames(bool onlyNormalTargets)
+        {
+            List<string> names = new List<string>(_buildTargets.Count);
+            for (int i = 0; i < _buildTargets.Count; ++i)
+            {
+                if (onlyNormalTargets ? _buildTargets[i].IsNormalBuildTarget : true)
+                {
+                    names.Add(_buildTargets[i].Name);
+                }
+            }
+            return names.ToArray();
+        }
+
+        public static string[] GetRegisteredBuildTargetNames()
+        {
+            return GetRegisteredBuildTargetNames(false);
+        }
+
+        /// <summary>
+        /// Returns a list of the IBuildTargets that are currently available and selected
+        /// for building.
+        /// </summary>
+        public static IList<IBuildTarget> GetSelectedBuildTargets()
+        {
+            List<IBuildTarget> targets = new List<IBuildTarget>(_buildTargets.Count);
+            foreach (IBuildTarget target in _buildTargets)
+            {
+                if (target.IsTargetedForBuild)
+                {
+                    targets.Add(target);
+                }
+            }
+            return targets;
+        }
+
+        /// <summary>
+        /// Locates a registered IBuildTarget by its name, (e.g, "Windows").
+        /// </summary>
+        public static IBuildTarget FindBuildTargetByName(string name)
+        {
+            if (string.IsNullOrEmpty(name)) return null;
+            foreach (IBuildTarget target in _buildTargets)
+            {
+                if (target.Name == name)
+                {
+                    return target;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Editor/AGS.Types/Interfaces/IBuildTarget.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AGS.Types
+{
+    public interface IBuildTarget
+    {
+        /// <summary>
+        /// Returns a set of the required library names and paths.
+        /// The VALUE may or may not end with the KEY string, but
+        /// the two joined together should result in the full path
+        /// to the library file (usually in the editor directory).
+        /// </summary>
+        IDictionary<string, string> GetRequiredLibraryPaths();
+        /// <summary>
+        /// Helper function to join together parts of a full path
+        /// to the Compiled folder. For normal build targets this
+        /// automatically adds the Compiled folder and target
+        /// OutputDirectory (e.g., GetCompiledPath("MyGame.exe")
+        /// would return the full path to "Compiled/Windows/MyGame.exe"
+        /// for a Windows build target).
+        /// </summary>
+        string GetCompiledPath(params string[] parts);
+        /// <summary>
+        /// Returns a list of the standard folders which are created
+        /// when this target is built. This always includes the
+        /// OutputDirectory, but may contain other subfolders as
+        /// well.
+        /// </summary>
+        string[] GetPlatformStandardSubfolders();
+        /// <summary>
+        /// Returns a list of the files required for this target
+        /// to be built. This is the same as the Key list returned
+        /// by GetRequiredLibraryPaths, so if you need full paths
+        /// use that function instead.
+        /// </summary>
+        string[] GetRequiredLibraryNames();
+        /// <summary>
+        /// Calls GetPlatformStandardSubfolders() and then makes
+        /// sure that each of those folders exist. This is always
+        /// called as part of the normal Build process.
+        /// </summary>
+        void EnsureStandardSubfoldersExist();
+        /// <summary>
+        /// Attempts to build the target. Compilation errors will
+        /// be added to the ERRORS collection. Returns whether or
+        /// not the target was successfully built. You should not
+        /// normally need to call this yourself.
+        /// </summary>
+        bool Build(CompileMessages errors, bool forceRebuild);
+
+        /// <summary>
+        /// Returns whether all necessary files are available for
+        /// this target to be successfully built.
+        /// </summary>
+        bool IsAvailable
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Returns whether this build target is currently selected
+        /// for building in the General Settings pane. See the
+        /// Settings.BuildTargets property to check all selected
+        /// targets.
+        /// </summary>
+        bool IsTargetedForBuild
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Returns whether this is the only selected build
+        /// target in the General Settings pane (excludes the
+        /// DataFile target unless all other targets are
+        /// deselected). See the Settings.BuildTargets property
+        /// to check all selected targets.
+        /// </summary>
+        bool IsOnlyBuildTarget
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Returns whether this is a normal build target. A
+        /// normal build target is one which can be selected
+        /// from the General Settings pane, and builds to the
+        /// Compiled folder. An example of a non-normal build
+        /// target would be the built-in "_Debug" target,
+        /// whose files are built to the "_Debug" folder
+        /// instead of the "Compiled" folder, and is not
+        /// listed in the General Settings pane.
+        /// </summary>
+        bool IsNormalBuildTarget
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the name of this build target (e.g., "Windows").
+        /// </summary>
+        string Name
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the subfolder where this target's files are
+        /// built (relative to Compiled folder, see
+        /// OutputDirectoryFullPath for a full path).
+        /// </summary>
+        string OutputDirectory
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the full path to this target's OutputDirectory.
+        /// </summary>
+        string OutputDirectoryFullPath
+        {
+            get;
+        }
+    }
+}

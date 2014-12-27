495ec26190523a6f22a8d9cf5ff34848dc19d814
persn <perolavflaten@live.no>
2014-12-28 00:33:13 +0100

    Color Theme Switcher
     - Added the abstract class "ColorTheme" which provides a default set of function heads for coloring GUI controls.
     - Added the class "VanillaTheme" which loads default coloring. It mostly contains empty functions since default colors are stored by default in the GUI controls, and as thus we want this solution to do nothing.
     - Added the class "DraconianTheme" which will act as the controller for coloring controls in dark theme.
     - Added various helper classes that were needed to override color choices on several GUI controls.

---
+++ Editor/AGS.Editor/ColorTheme/Draconian/DraconianMenuRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace AGS.Editor
+{
+    public class DraconianMenuRenderer : ToolStripProfessionalRenderer
+    {
+        public DraconianMenuRenderer(ProfessionalColorTable pct) : base(pct) { }
+
+        protected override void OnRenderItemCheck(ToolStripItemImageRenderEventArgs e)
+        {
+            if (e.Item.Selected)
+                e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(62, 62, 64)), new Rectangle(3, 1, 20, 20));
+            else
+                e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(45, 45, 48)), new Rectangle(3, 1, 20, 20));
+            e.Graphics.DrawImage(this.InvertImage(e.Image), new Point(5, 3));
+        }
+
+        protected override void OnRenderItemText(ToolStripItemTextRenderEventArgs e)
+        {
+            e.TextColor = Color.FromArgb(241, 241, 241);
+            base.OnRenderItemText(e);
+        }
+
+        private Image InvertImage(Image image)
+        {
+            Bitmap result = new Bitmap(image);
+            int A, R, G, B;
+            for (int y = 0; y < result.Height; y++)
+            {
+                for (int x = 0; x < result.Width; x++)
+                {
+                    Color pixel = result.GetPixel(x, y);
+                    A = pixel.A;
+                    R = 255 - pixel.R;
+                    G = 255 - pixel.G;
+                    B = 255 - pixel.B;
+                    result.SetPixel(x, y, Color.FromArgb(A, R, G, B));
+                }
+            }
+            return result;
+        }
+    }
+}

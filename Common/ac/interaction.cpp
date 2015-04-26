adventuregamestudio/ags

commit f8a6177aa8cb648740d46e26cc2f9ab570ede6fe

Common: proper copying of interaction structs
This implements safer copying functions and copy constructors for NewInteraction and supplemental types, that do not copy raw pointers, but allocate clones of dynamic objects.
This also removes practically redundant NewInteractionAction parent class.

Common/ac/interaction.cpp
@@ -27,7 +27,8 @@ using AGS::Common::String;
 InteractionVariable globalvars[MAX_GLOBAL_VARIABLES] = {{"Global 1", 0, 0}};
 int numGlobalVars = 1;
 
-NewInteractionValue::NewInteractionValue() {
+NewInteractionValue::NewInteractionValue()
+{
     valType = VALTYPE_LITERALINT;
     val = 0;
     extra = 0;
@@ -63,27 +64,41 @@ void InteractionVariable::WriteToFile(Common::Stream *out)
     out->WriteInt32(value);
 }
 
-NewInteractionCommand::NewInteractionCommand() {
+NewInteractionCommand::NewInteractionCommand()
+{
     type = 0;
+    memset(data, 0, sizeof(data));
     children = NULL;
     parent = NULL;
 }
 
-NewInteractionCommandList *NewInteractionCommand::get_child_list() {
+NewInteractionCommandList *NewInteractionCommand::get_child_list()
+{
     return (NewInteractionCommandList*)children;
 }
 
-void NewInteractionCommand::remove () {
-    if (children != NULL) {
-        children->reset();
-        delete children;
-    }
+NewInteractionCommand::~NewInteractionCommand()
+{
+    reset();
+}
+
+void NewInteractionCommand::assign(const NewInteractionCommand &nic, NewInteractionCommandList *new_parent)
+{
+    reset();
+    type = nic.type;
+    memcpy(data, nic.data, sizeof(data));
+    children = nic.children ? new NewInteractionCommandList(*nic.children) : NULL;
+    parent = new_parent;
+}
+
+void NewInteractionCommand::reset()
+{
+    delete children;
     children = NULL;
     parent = NULL;
     type = 0;
+    memset(data, 0, sizeof(data));
 }
-
-void NewInteractionCommand::reset() { remove(); }
 
 void NewInteractionCommand::ReadNewInteractionValues_Aligned(Stream *in)
 {
@@ -101,7 +116,7 @@ void NewInteractionCommand::ReadFromFile_v321(Stream *in)
     type = in->ReadInt32();
     ReadNewInteractionValues_Aligned(in);
     // all that matters is whether or not these are null...
-    children = (NewInteractionAction *) (long)in->ReadInt32();
+    children = (NewInteractionCommandList *) (long)in->ReadInt32();
     parent = (NewInteractionCommandList *) (long)in->ReadInt32();
 }
 
@@ -124,25 +139,37 @@ void NewInteractionCommand::WriteToFile_v321(Stream *out)
     out->WriteInt32((long)parent);
 }
 
-NewInteractionCommandList::NewInteractionCommandList () {
+NewInteractionCommandList::NewInteractionCommandList()
+{
     numCommands = 0;
     timesRun = 0;
 }
 
-void NewInteractionCommandList::reset () {
-  int j;
-  for (j = 0; j < numCommands; j++) {
-    if (command[j].children != NULL) {
-      // using this Reset crashes it for some reason
-      //command[j].reset ();
-      command[j].get_child_list()->reset();
-      delete command[j].children;
-      command[j].children = NULL;
+NewInteractionCommandList::NewInteractionCommandList(const NewInteractionCommandList &nicmd_list)
+{
+    reset();
+    numCommands = nicmd_list.numCommands;
+    timesRun = nicmd_list.timesRun;
+    for (int i = 0; i < numCommands; ++i)
+    {
+        command[i].assign(nicmd_list.command[i], this);
     }
-    command[j].remove();
-  }
-  numCommands = 0;
-  timesRun = 0;
+}
+
+NewInteractionCommandList::~NewInteractionCommandList()
+{
+    reset();
+}
+
+void NewInteractionCommandList::reset()
+{
+    int j;
+    for (j = 0; j < numCommands; j++)
+    {
+        command[j].reset();
+    }
+    numCommands = 0;
+    timesRun = 0;
 }
 
 void NewInteractionCommandList::ReadInteractionCommands_Aligned(Stream *in)
@@ -165,29 +192,55 @@ void NewInteractionCommandList::WriteInteractionCommands_Aligned(Stream *out)
     }
 }
 
-NewInteraction::NewInteraction() { 
+NewInteraction::NewInteraction()
+{ 
     numEvents = 0;
-    // NULL all the pointers
-    memset (response, 0, sizeof(NewInteractionCommandList*) * MAX_NEWINTERACTION_EVENTS);
-    memset (&timesRun[0], 0, sizeof(int) * MAX_NEWINTERACTION_EVENTS);
+    memset(eventTypes, 0, sizeof(eventTypes));
+    memset(timesRun, 0, sizeof(timesRun));
+    memset(response, 0, sizeof(response));
 }
 
-
-void NewInteraction::copy_timesrun_from (NewInteraction *nifrom) {
-    memcpy (&timesRun[0], &nifrom->timesRun[0], sizeof(int) * MAX_NEWINTERACTION_EVENTS);
+NewInteraction::NewInteraction(const NewInteraction &ni)
+{
+    *this = ni;
 }
-void NewInteraction::reset() {
-    for (int i = 0; i < MAX_NEWINTERACTION_EVENTS; i++) {
-        if (response[i] != NULL) {
-            response[i]->reset();
-            delete response[i];
-            response[i] = NULL;
+
+NewInteraction::~NewInteraction()
+{
+    reset();
+}
+
+NewInteraction &NewInteraction::operator =(const NewInteraction &ni)
+{
+    reset();
+    numEvents = ni.numEvents;
+    memcpy(eventTypes, ni.eventTypes, sizeof(eventTypes));
+    memcpy(timesRun, ni.timesRun, sizeof(timesRun));
+    for (int i = 0; i < numEvents; i++)
+    {
+        if (ni.response[i] != NULL)
+        {
+            response[i] = new NewInteractionCommandList(*ni.response[i]);
         }
     }
-    numEvents = 0;
+    return *this;
 }
-NewInteraction::~NewInteraction() {
-    reset();
+
+void NewInteraction::copy_timesrun_from(NewInteraction *nifrom)
+{
+    memcpy(&timesRun[0], &nifrom->timesRun[0], sizeof(int) * MAX_NEWINTERACTION_EVENTS);
+}
+
+void NewInteraction::reset()
+{
+    for (int i = 0; i < numEvents; i++)
+    {
+        delete response[i];
+        response[i] = NULL;
+    }
+    numEvents = 0;
+    memset(eventTypes, 0, sizeof(eventTypes));
+    memset(timesRun, 0, sizeof(timesRun));
 }
 
 void NewInteraction::ReadFromFile(Stream *in)

Common/ac/interaction.h
@@ -43,7 +43,8 @@ using namespace AGS; // FIXME later
 #define VALTYPE_BOOLEAN    3
 #define VALTYPE_CHARNUM    4
 
-struct NewInteractionValue {
+struct NewInteractionValue
+{
     unsigned char valType;
     int  val;
     int  extra;
@@ -54,21 +55,21 @@ struct NewInteractionValue {
     void WriteToFile(Common::Stream *out);
 };
 
-struct NewInteractionAction {
-    virtual void reset() = 0;
-};
+
 struct NewInteractionCommandList;
 
-struct NewInteractionCommand: public NewInteractionAction {
+struct NewInteractionCommand
+{
     int32 type;
     NewInteractionValue data[MAX_ACTION_ARGS];
-    NewInteractionAction * children;
+    NewInteractionCommandList *children;
     NewInteractionCommandList *parent;
 
     NewInteractionCommand();
+    ~NewInteractionCommand();
     NewInteractionCommandList *get_child_list();
-    void remove();
 
+    void assign(const NewInteractionCommand &nic, NewInteractionCommandList *new_parent);
     void reset();
 
     void ReadFromFile_v321(Common::Stream *in);
@@ -77,19 +78,23 @@ struct NewInteractionCommand: public NewInteractionAction {
     void WriteNewInteractionValues_Aligned(Common::Stream *out);
 };
 
-struct NewInteractionCommandList : public NewInteractionAction {
+struct NewInteractionCommandList
+{
     int32 numCommands;
     NewInteractionCommand command[MAX_COMMANDS_PER_LIST];
     int32 timesRun;   // used by engine to track score changes
 
-    NewInteractionCommandList ();
+    NewInteractionCommandList();
+    NewInteractionCommandList(const NewInteractionCommandList &nicmd_list);
+    ~NewInteractionCommandList();
     void reset();
 
     void ReadInteractionCommands_Aligned(Common::Stream *in);
     void WriteInteractionCommands_Aligned(Common::Stream *out);
 };
 
-struct NewInteraction {
+struct NewInteraction
+{
     int numEvents;
     // the first few event types depend on the item - ID's of 100+ are
     // custom events (used for subroutines)
@@ -97,16 +102,17 @@ struct NewInteraction {
     int timesRun[MAX_NEWINTERACTION_EVENTS];
     NewInteractionCommandList *response[MAX_NEWINTERACTION_EVENTS];
 
-
     NewInteraction();
-
+    NewInteraction(const NewInteraction &ni);
+    ~NewInteraction();
 
     void copy_timesrun_from (NewInteraction *nifrom);
     void reset();
-    ~NewInteraction();
 
     void ReadFromFile(Common::Stream *in);
     void WriteToFile(Common::Stream *out);
+
+    NewInteraction &operator =(const NewInteraction &ni);
 };
 
 

Engine/ac/roomstatus.cpp
@@ -128,22 +128,8 @@ void resetRoomStatuses()
         {
             if ((room_statuses[i]->tsdata != NULL) && (room_statuses[i]->tsdatasize > 0))
                 free(room_statuses[i]->tsdata);
-
-            // Don't delete the status on 2.x. The status struct contains NewInteraction
-            // pointer that are also referenced in the current room struct.
-            // If they are freed here this will lead to an access violation when the
-            // room unloading function tries to frees them.
-            if (loaded_game_file_version <= kGameVersion_272)
-            {
-                room_statuses[i]->tsdatasize = 0;
-                room_statuses[i]->tsdata = 0;
-                room_statuses[i]->beenhere = 0;
-            }
-            else
-            {
-                delete room_statuses[i];
-                room_statuses[i] = NULL;
-            }
+            delete room_statuses[i];
+            room_statuses[i] = NULL;
         }
     }
 }

Engine/main/quit.cpp
@@ -192,9 +192,7 @@ void quit_message_on_exit(char *qmsg, char *alertis)
 
 void quit_release_data()
 {
-    // wipe all the interaction structs so they don't de-alloc the children twice
     resetRoomStatuses();
-    memset (&troom, 0, sizeof(RoomStatus));
 
     /*  _CrtMemState memstart;
     _CrtMemCheckpoint(&memstart);

commit 7d1d2ac4ab714896a9787f2b57dd79f29fa9c4c2

Common: fixed NewInteractionCommandList's copy constructor
Reset() call is redundant, and actually causes invalid memory access, because some of the class members were not assigned yet.
Error was introduced in f8a6177aa8cb648740d46e26cc2f9ab570ede6fe

Common/ac/interaction.cpp
@@ -147,7 +147,6 @@ NewInteractionCommandList::NewInteractionCommandList()
 
 NewInteractionCommandList::NewInteractionCommandList(const NewInteractionCommandList &nicmd_list)
 {
-    reset();
     numCommands = nicmd_list.numCommands;
     timesRun = nicmd_list.timesRun;
     for (int i = 0; i < numCommands; ++i)
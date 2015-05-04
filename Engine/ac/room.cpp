adventuregamestudio/ags

commit 9f55259bd353ce50b462f3c528fb8f6e7a52a7ef

Engine: corrections for screen size designation on room load
This ammends previous fix for letterboxed mode; fixes situation when there's a room which size is smaller than game's size, which was technically acceptable in 320x240 and 640x480 games.

Engine/ac/room.cpp
@@ -61,11 +61,14 @@
 #include "core/assetmanager.h"
 #include "ac/dynobj/all_dynamicclasses.h"
 #include "gfx/bitmap.h"
+#include "util/math.h"
+#include "main/graphics_mode.h"
 
 using AGS::Common::Bitmap;
 using AGS::Common::Stream;
 using AGS::Common::String;
 namespace BitmapHelper = AGS::Common::BitmapHelper;
+namespace Math = AGS::Common::Math;
 namespace Out = AGS::Common::Out;
 
 #if !defined (WINDOWS_VERSION)
@@ -494,18 +497,26 @@ void load_new_room(int newnum, CharacterInfo*forchar) {
     update_polled_stuff_if_runtime();
 
     our_eip=202;
-    if (usetup.want_letterbox) {
+    const int real_room_height = multiply_up_coordinate(thisroom.height);
+    // Frame size is updated when letterbox mode is on, or when room's size is smaller than game's size.
+    if (usetup.want_letterbox ||
+            real_room_height < GameSize.Height || scrnhit < GameSize.Height) {
         int abscreen=0;
 
         Bitmap *ds = GetVirtualScreen();
         if (ds==BitmapHelper::GetScreenBitmap()) abscreen=1;
         else if (ds==virtual_screen) abscreen=2;
-        // if this is a 640x480 room and we're in letterbox mode, full-screen it
         int newScreenHeight = final_scrn_hit;
-        if (scrnhit < final_scrn_hit) {
+        const int viewport_height = Math::Min(real_room_height, GameSize.Height);
+        if (viewport_height < final_scrn_hit) {
             clear_letterbox_borders();
-            newScreenHeight = scrnhit;
+            newScreenHeight = viewport_height;
         }
+
+        // If the game is run not in letterbox mode, but there's a random room smaller than the game size,
+        // then the sub_screen does not exist at this point; so we create it here.
+        if (!_sub_screen)
+            _sub_screen = BitmapHelper::CreateSubBitmap(_old_screen, RectWH(final_scrn_wid / 2 - scrnwid / 2, final_scrn_hit / 2-newScreenHeight/2, scrnwid, newScreenHeight));
 
         if (newScreenHeight == _sub_screen->GetHeight())
         {
@@ -525,6 +536,7 @@ void load_new_room(int newnum, CharacterInfo*forchar) {
 
 		scrnhit = BitmapHelper::GetScreenBitmap()->GetHeight();
         vesa_yres = scrnhit;
+        game_frame_y_offset = (final_scrn_hit - scrnhit) / 2;
 
         filter->SetMouseArea(0,0, scrnwid-1, vesa_yres-1);
 

Engine/ac/global_screen.cpp
@@ -45,7 +45,7 @@ int scrnwid,scrnhit;
 int current_screen_resolution_multiplier = 1;
 int force_letterbox = 0;
 
-int final_scrn_wid=0,final_scrn_hit=0,final_col_dep=0, game_frame_y_offset = 0, game_frame_borders = 0;
+int final_scrn_wid=0,final_scrn_hit=0,final_col_dep=0, game_frame_y_offset = 0;
 int screen_reset = 0;
 
 int GetMaxScreenHeight () {

Engine/main/graphics_mode.cpp
@@ -59,7 +59,6 @@ extern int force_letterbox;
 extern AGSPlatformDriver *platform;
 extern int force_16bit;
 extern IGraphicsDriver *gfxDriver;
-extern int final_scrn_wid,final_scrn_hit,final_col_dep, game_frame_y_offset, game_frame_borders;
 extern volatile int timerloop;
 extern IDriverDependantBitmap *blankImage;
 extern IDriverDependantBitmap *blankSidebarImage;
@@ -69,6 +68,7 @@ extern int _places_r, _places_g, _places_b;
 
 const int MaxScalingFactor = 8; // we support up to x8 scaling now
 
+Size GameSize;
 int firstDepth, secondDepth;
 String GfxFilterRequest;
 
@@ -195,9 +195,9 @@ void engine_init_screen_settings()
     usetup.base_width = 320;
     usetup.base_height = 200;
 
-    Size res_size = ResolutionTypeToSize(game.default_resolution);
-    scrnwid = res_size.Width;
-    scrnhit = res_size.Height;
+    GameSize = ResolutionTypeToSize(game.default_resolution);
+    scrnwid = GameSize.Width;
+    scrnhit = GameSize.Height;
 
     if (game.IsHiRes())
     {
@@ -731,8 +731,7 @@ bool init_gfx_mode(const Size &game_size, const Size &screen_size, int cdep)
     final_scrn_wid = game_size.Width;
     final_scrn_hit = game_size.Height;
     final_col_dep = cdep;
-    game_frame_borders = final_scrn_hit - scrnhit;
-    game_frame_y_offset = game_frame_borders / 2;
+    game_frame_y_offset = (final_scrn_hit - scrnhit) / 2;
     usetup.want_letterbox = (final_scrn_hit > scrnhit) ? 1 : 0;
 
     if (game.color_depth == 1) {

Engine/main/graphics_mode.h
@@ -21,4 +21,7 @@
 int  graphics_mode_init();
 void graphics_mode_shutdown();
 
+extern Size GameSize;
+extern int final_scrn_wid, final_scrn_hit, final_col_dep, game_frame_y_offset;
+
 #endif // __AGS_EE_MAIN__GRAPHICSMODE_H

commit d714bb1fa6c08bd97504bac2b9cefd8055f94bb6

Engine: restored letterboxed game's behavior for larger rooms
Was broken by 9f55259bd353ce50b462f3c528fb8f6e7a52a7ef

Engine/ac/room.cpp
@@ -499,6 +499,7 @@ void load_new_room(int newnum, CharacterInfo*forchar) {
     our_eip=202;
     const int real_room_height = multiply_up_coordinate(thisroom.height);
     // Frame size is updated when letterbox mode is on, or when room's size is smaller than game's size.
+    // NOTE: if "want_letterbox" is false, GameSize.Height = final_scrn_hit always.
     if (usetup.want_letterbox ||
             real_room_height < GameSize.Height || scrnhit < GameSize.Height) {
         int abscreen=0;
@@ -507,7 +508,11 @@ void load_new_room(int newnum, CharacterInfo*forchar) {
         if (ds==BitmapHelper::GetScreenBitmap()) abscreen=1;
         else if (ds==virtual_screen) abscreen=2;
         int newScreenHeight = final_scrn_hit;
-        const int viewport_height = Math::Min(real_room_height, GameSize.Height);
+        // [IKM] 2015-05-04: in original engine the letterbox feature only allowed viewports of
+        // either 200 or 240 (400 and 480) pixels, if the room height was equal or greater than 200 (400).
+        const int viewport_height = real_room_height < GameSize.Height ? real_room_height :
+            (real_room_height >= GameSize.Height && real_room_height < final_scrn_hit) ? GameSize.Height :
+            final_scrn_hit;
         if (viewport_height < final_scrn_hit) {
             clear_letterbox_borders();
             newScreenHeight = viewport_height;
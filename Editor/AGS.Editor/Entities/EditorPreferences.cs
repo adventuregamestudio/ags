35828361e4577c71a037c5e959e726992d5dfe34
Per Olav Flaten <perolavflaten@live.no>
2015-05-21 17:13:22 +0200

    Prompt dialog when "closing all tabs" and "closing all other tabs"
     - Added a checkmark in Preferences for if a dialog should show on closing multiple tabs
     - Added a dialog on closing multiple tabs

--- Editor/AGS.Editor/Entities/EditorPreferences.cs
+++ Editor/AGS.Editor/Entities/EditorPreferences.cs
@@ -26,6 +26,7 @@ namespace AGS.Editor
         private bool _remapPalettizedBackgrounds = true;
         private List<string> _previousSearches = new List<string>();
         private bool _keepHelpOnTop = true;
+        private bool _dialogOnMultibleTabsClose = true;
         private bool _useLegacyCompiler = false;
 
         private string _registryKey;
@@ -54,6 +55,7 @@ namespace AGS.Editor
                 _lastBackupWarning = ReadDateFromRegistry(key, "LastBackupWarning", _lastBackupWarning);
                 _remapPalettizedBackgrounds = Convert.ToInt32(key.GetValue("RemapPaletteBackgrounds", _remapPalettizedBackgrounds)) != 0;
                 _keepHelpOnTop = Convert.ToInt32(key.GetValue("KeepHelpOnTop", _keepHelpOnTop)) != 0;
+                _dialogOnMultibleTabsClose = Convert.ToInt32(key.GetValue("DialogOnMultipleTabsClose", _dialogOnMultibleTabsClose)) != 0;
                 _useLegacyCompiler = Convert.ToInt32(key.GetValue("UseLegacyCompiler", _useLegacyCompiler)) != 0;
                 ReadRecentSearchesList(key);
                 key.Close();
@@ -129,6 +131,7 @@ namespace AGS.Editor
                 key.SetValue("LastBackupWarning", _lastBackupWarning.ToString("u"));
                 key.SetValue("RemapPaletteBackgrounds", _remapPalettizedBackgrounds ? "1" : "0");
                 key.SetValue("KeepHelpOnTop", _keepHelpOnTop ? "1" : "0");
+                key.SetValue("DialogOnMultipleTabsClose", _dialogOnMultibleTabsClose ? "1" : "0");
                 key.SetValue("UseLegacyCompiler", _useLegacyCompiler ? "1" : "0");
                 WriteRecentSearchesList(key);
                 key.Close();
@@ -255,7 +258,13 @@ namespace AGS.Editor
         public bool KeepHelpOnTop
         {
             get { return _keepHelpOnTop; }
-            set { _keepHelpOnTop = value; }
+            set { _keepHelpOnTop = value; }            
+        }
+
+        public bool DialogOnMultibleTabsClose
+        {
+            get { return _dialogOnMultibleTabsClose; }
+            set { _dialogOnMultibleTabsClose = value; }
         }
 
         public bool UseLegacyCompiler
--- Editor/AGS.Editor/GUI/PreferencesEditor.cs
+++ Editor/AGS.Editor/GUI/PreferencesEditor.cs
@@ -45,6 +45,7 @@ namespace AGS.Editor
             udBackupInterval.Enabled = chkBackupReminders.Checked;
             chkRemapBgImport.Checked = _preferences.RemapPalettizedBackgrounds;
             chkKeepHelpOnTop.Checked = _preferences.KeepHelpOnTop;
+            chkPromptDialogOnTabsClose.Checked = _preferences.DialogOnMultibleTabsClose;
             chkUseLegacyCompiler.Checked = _preferences.UseLegacyCompiler;
             Utilities.CheckLabelWidthsOnForm(this);
 		}
@@ -82,6 +83,7 @@ namespace AGS.Editor
             _preferences.BackupWarningInterval = (chkBackupReminders.Checked ? (int)udBackupInterval.Value : 0);
             _preferences.RemapPalettizedBackgrounds = chkRemapBgImport.Checked;
             _preferences.KeepHelpOnTop = chkKeepHelpOnTop.Checked;
+            _preferences.DialogOnMultibleTabsClose = chkPromptDialogOnTabsClose.Checked;
             _preferences.UseLegacyCompiler = chkUseLegacyCompiler.Checked;
 		}
 

adventuregamestudio/ags

commit 7bc96e6a45fe7eb94494c53dab8e18d1331fadc8

Editor: updated default game configuration

Editor/AGS.Editor/AGSEditor.cs
@@ -1536,25 +1536,16 @@ namespace AGS.Editor
 			if (buffer.ToString() != resolution.ToString())
 			{
 				NativeProxy.WritePrivateProfileString("misc", "defaultres", resolution.ToString(), configFilePath);
-
-				int screenRes = 1;
-				if ((_game.Settings.Resolution == GameResolutions.R320x200) ||
-					(_game.Settings.Resolution == GameResolutions.R320x240)) 
-				{
-					screenRes = 0;
-				}
-				NativeProxy.WritePrivateProfileString("misc", "screenres", screenRes.ToString(), configFilePath);
 			}
 
             int letterbox = _game.Settings.LetterboxMode ? 1 : 0;
             NativeProxy.WritePrivateProfileString("misc", "letterbox", letterbox.ToString(), configFilePath);
 
-			NativeProxy.GetPrivateProfileString("misc", "defaultgfxdriver", "NULL", buffer, buffer.Capacity, configFilePath);
-			if (buffer.ToString() != _game.Settings.GraphicsDriver.ToString())
-			{
-				NativeProxy.WritePrivateProfileString("misc", "defaultgfxdriver", _game.Settings.GraphicsDriver.ToString(), configFilePath);
-				NativeProxy.WritePrivateProfileString("misc", "gfxdriver", _game.Settings.GraphicsDriver.ToString(), configFilePath);
-			}
+            NativeProxy.GetPrivateProfileString("graphics", "driver", "NULL", buffer, buffer.Capacity, configFilePath);
+            if (buffer.ToString() != _game.Settings.GraphicsDriver.ToString())
+            {
+                NativeProxy.WritePrivateProfileString("graphics", "driver", _game.Settings.GraphicsDriver.ToString(), configFilePath);
+            }
             NativeProxy.WritePrivateProfileString("misc", "titletext", _game.Settings.GameName + " Setup", configFilePath);
         }
 

commit 396365836b73edfb3b2461ccc88856967c713428

Editor: restored condition of gfx driver option update in config file
Broken by 7bc96e6a45fe7eb94494c53dab8e18d1331fadc8

Editor/AGS.Editor/AGSEditor.cs
@@ -1402,9 +1402,10 @@ namespace AGS.Editor
 			NativeProxy.WritePrivateProfileString("misc", "gamecolordepth", (((int)_game.Settings.ColorDepth) * 8).ToString(), configFilePath);
 
             StringBuilder buffer = new StringBuilder(100);
-            NativeProxy.GetPrivateProfileString("graphics", "driver", "NULL", buffer, buffer.Capacity, configFilePath);
+            NativeProxy.GetPrivateProfileString("graphics", "defaultdriver", "NULL", buffer, buffer.Capacity, configFilePath);
             if (buffer.ToString() != _game.Settings.GraphicsDriver.ToString())
             {
+                NativeProxy.WritePrivateProfileString("graphics", "defaultdriver", _game.Settings.GraphicsDriver.ToString(), configFilePath);
                 NativeProxy.WritePrivateProfileString("graphics", "driver", _game.Settings.GraphicsDriver.ToString(), configFilePath);
             }
             NativeProxy.WritePrivateProfileString("misc", "titletext", _game.Settings.GameName + " Setup", configFilePath);
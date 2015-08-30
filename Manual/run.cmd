760657b894f0dec4ee0e6c674b306cf89e1ea39f
Nick Sonneveld <nick@sonneveld.name>
2015-08-30 21:26:56 +1000

    Manual: add additional error checking to run.cmd

--- Manual/run.cmd
+++ Manual/run.cmd
@@ -1,11 +1,29 @@
-start /WAIT tex2rtf.exe ags.tex ags.html -html -sync
-python striphdr.py htmlfiles\actutor.htm ags29.htm
-python striphdr.py htmlfiles\actutor2.htm ags30.htm
-python procdocs.py
-cd htmlfiles
-for %%i in (acintro*.htm) do python ..\striphdr.py %%i ..\%%i
-cd ..
-for %%i in (acintro*.htm) do echo %%i >>ags.hhp
+@echo off
+
+tex2rtf.exe ags.tex ags.html -html -sync
+if %errorlevel% neq -1 goto error
+
+python procdocs.py || goto error
+
+python striphdr.py htmlfiles\actutor.htm ags29.htm || goto error
+python striphdr.py htmlfiles\actutor2.htm ags30.htm || goto error
+pushd htmlfiles
+for %%i in (acintro*.htm) do (
+    python ..\striphdr.py %%i ..\%%i || goto error
+)
+popd
+for %%i in (acintro*.htm) do (
+    echo %%i >>ags.hhp
+)
+
 echo images\*.jpg >>ags.hhp
 echo images\*.gif >>ags.hhp
+
 hhc ags.hhp
+if %errorlevel% neq 1 goto error
+
+goto :EOF
+
+:error
+echo Failed with error level %errorlevel%
+exit /b 1

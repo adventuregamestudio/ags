ad5637331d6f32a87f7509207fc2548231822820
Nick Sonneveld <nick@sonneveld.name>
2015-08-30 20:08:21 +1000

    Manual: replace striphdr with python equiv.

--- Manual/run.cmd
+++ Manual/run.cmd
@@ -1,12 +1,11 @@
 start /WAIT tex2rtf.exe ags.tex ags.html -html -sync
-striphdr htmlfiles\actutor.htm ags29.htm
-striphdr htmlfiles\actutor2.htm ags30.htm
+python striphdr.py htmlfiles\actutor.htm ags29.htm
+python striphdr.py htmlfiles\actutor2.htm ags30.htm
 procdocs
 cd htmlfiles
-for %%i in (acintro*.htm) do ..\striphdr %%i ..\%%i
+for %%i in (acintro*.htm) do python ..\striphdr.py %%i ..\%%i
 cd ..
 for %%i in (acintro*.htm) do echo %%i >>ags.hhp
 echo images\*.jpg >>ags.hhp
 echo images\*.gif >>ags.hhp
 hhc ags.hhp
-pause

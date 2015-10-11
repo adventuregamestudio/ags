d2c4c51ab3e4f7ea6fcd5a002f5e6abcb92ce1e0
Nick Sonneveld <nick@sonneveld.name>
2015-10-10 18:31:56 -0700

    iOS: Add armv7s to nativelib archs

--- iOS/buildlibs/buildall.sh
+++ iOS/buildlibs/buildall.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for arch in armv7 i386 # future: armv7s x86_64
+for arch in armv7 armv7s i386 # future: arm64 x86_64
 do
 	rm -rf ../nativelibs/$arch
 	pushd $arch
--- iOS/buildlibs/makefatlibs.sh
+++ iOS/buildlibs/makefatlibs.sh
@@ -2,12 +2,11 @@
 
 set -e
 
-mkdir -p ../nativelibs/fat
 mkdir -p ../nativelibs/fat/lib
 
 for i in dumb aldmb alleg freetype ogg theora vorbisidec lua
 do
-	lipo -create -output ../nativelibs/fat/lib/lib$i.a ../nativelibs/i386/lib/lib$i.a ../nativelibs/armv7/lib/lib$i.a
+	lipo -create -output ../nativelibs/fat/lib/lib$i.a ../nativelibs/i386/lib/lib$i.a ../nativelibs/armv7/lib/lib$i.a ../nativelibs/armv7s/lib/lib$i.a
 	ranlib ../nativelibs/fat/lib/lib$i.a
 done
 

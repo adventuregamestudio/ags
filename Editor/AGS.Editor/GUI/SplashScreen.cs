405d7a3b3386b017922a69c9ff4d7b8f76b82a62
morganwillcock <morganwillcock@users.noreply.github.com>
2015-12-13 17:56:35 +0000

    Make splash screen tag lines gender neutral.
    Remove the "girl next door" tag line from splash screen.

--- Editor/AGS.Editor/GUI/SplashScreen.cs
+++ Editor/AGS.Editor/GUI/SplashScreen.cs
@@ -30,7 +30,6 @@ namespace AGS.Editor
             "Best served with nostalgia",
             "Please do not feed the scripters",
             "This area intentionally left blank",
-            "More fun than the girl next door",
             "Insert Disk 2 to continue",
             "The partner that keeps you up all night",
             "Can help weight loss only as part of a balanced diet",

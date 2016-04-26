3bfa71591f8b6be278675d19536c00b5bcfa20a5
Ivan Mogilko <ikm_spb@yahoo.com>
2016-04-26 16:44:59 +0300

    Updated build version (3.3.5.5)

--- Common/core/def_version.h
+++ Common/core/def_version.h
@@ -1,9 +1,9 @@
 #ifndef __AGS_CN_CORE__DEFVERSION_H
 #define __AGS_CN_CORE__DEFVERSION_H
 
-#define ACI_VERSION_STR      "3.3.5.4"
+#define ACI_VERSION_STR      "3.3.5.5"
 #if defined (RC_INVOKED) // for MSVC resource compiler
-#define ACI_VERSION_MSRC_DEF  3,3,5,4
+#define ACI_VERSION_MSRC_DEF  3,3,5,5
 #endif
 
 #ifdef NO_MP3_PLAYER

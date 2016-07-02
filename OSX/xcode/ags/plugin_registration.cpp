037b3a9a15a98400c4b820a1b46ab136b4afd81e
Nick Sonneveld <nick@sonneveld.name>
2016-07-02 21:44:39 +1000

    osx: add AGSKit and example app.

---
+++ OSX/xcode/ags/plugin_registration.cpp
@@ -0,0 +1,34 @@
+//
+//  plugin_registration.cpp
+//  Shivah
+//
+//  Created by Nick Sonneveld on 26/06/2016.
+//
+//
+
+#include "plugin_registration.hpp"
+
+#include "agsplugin.h"
+//#include "example.h"
+
+typedef void (*t_engine_pre_init_callback)(void);
+extern void engine_set_pre_init_callback(t_engine_pre_init_callback callback);
+
+static void pl_register_builtin_plugin() {
+
+//  pl_register_builtin_plugin((InbuiltPluginDetails){
+//    .filename = "example",
+//    .engineStartup = example::AGS_EngineStartup,
+//    .engineShutdown = example::AGS_EngineShutdown,
+//    .onEvent = example::AGS_EngineOnEvent,
+//    .initGfxHook = example::AGS_EngineInitGfx,
+//    .debugHook = example::AGS_EngineDebugHook,
+//  });
+
+}
+
+
+__attribute__((constructor))
+static void oninit_register_builtin_plugins() {
+  engine_set_pre_init_callback(&pl_register_builtin_plugin);
+}

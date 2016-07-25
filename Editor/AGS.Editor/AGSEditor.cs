e4cdc5d9fae1b2d5d4ba7540167ea77d7dee6a35
Ivan Mogilko <ikm_spb@yahoo.com>
2016-07-25 21:22:05 +0300

    Editor: corrected project format index

--- Editor/AGS.Editor/AGSEditor.cs
+++ Editor/AGS.Editor/AGSEditor.cs
@@ -65,9 +65,10 @@ namespace AGS.Editor
          *  9: 3.4.0.1    - Settings.CustomResolution
          * 10: 3.4.0.2    - Settings.BuildTargets
          * 11: 3.4.0.4    - Region.TintLuminance
-         * 12: 3.4.0.8    - Settings.UseOldCustomDialogOptionsAPI
+         * 12: 3.4.0.8    - Settings.UseOldCustomDialogOptionsAPI & ScriptAPILevel
+         * 13: 3.4.0.9    - Settings.ScriptCompatLevel
         */
-        public const int    LATEST_XML_VERSION_INDEX = 12;
+        public const int    LATEST_XML_VERSION_INDEX = 13;
         /*
          * LATEST_USER_DATA_VERSION is the last version of the user data file that used a
          * 4-point-4-number string to identify the version of AGS that saved the file.

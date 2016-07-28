8383c83230f8877a83075c79e0b1a28533ba3125
Nick Sonneveld <nick@sonneveld.name>
2016-07-28 21:25:44 +1000

    packaging: include changelog in zip docs

--- Script/build_packages.py
+++ Script/build_packages.py
@@ -134,6 +134,7 @@ def transfer_installer(output_path, staging_path, ver_str):
 def build_ags_zip(output_path, staging_path, ver_str):
 
     shutil.copytree(workspace_rel("Windows/Installer/Source/Docs"), os.path.join(staging_path,"Docs"))
+    shutil.copy2(workspace_rel("Changes.txt"), os.path.join(staging_path,"Docs"))
 
     extract_into(staging_path, workspace_rel("editor.zip"))
     extract_into(os.path.join(staging_path,"Templates"), workspace_rel("templates.zip"))

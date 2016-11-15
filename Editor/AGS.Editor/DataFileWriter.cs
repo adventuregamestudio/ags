d32c7fff98c11e158ebc19c11ce582e39d8d2528
Ivan Mogilko <ikm_spb@yahoo.com>
2016-11-15 23:11:34 +0300

    Editor: implemented optional vertical offset for fonts

--- Editor/AGS.Editor/DataFileWriter.cs
+++ Editor/AGS.Editor/DataFileWriter.cs
@@ -1317,6 +1317,10 @@ namespace AGS.Editor
                     writer.Write((byte)game.Fonts[i].OutlineFont);
                 }
             }
+            for (int i = 0; i < game.Fonts.Count; ++i)
+            {
+                writer.Write(game.Fonts[i].VerticalOffset);
+            }
             writer.Write(NativeConstants.MAX_SPRITES);
             byte[] spriteFlags = new byte[NativeConstants.MAX_SPRITES];
             UpdateSpriteFlags(game.RootSpriteFolder, spriteFlags);

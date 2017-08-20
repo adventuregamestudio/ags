a0d666d82e242952533b39bc81de9941bcab22b6
Ivan Mogilko <ikm_spb@yahoo.com>
2017-08-21 01:13:18 +0300

    Updated build version (3.4.1.7)

--- Editor/AGS.Types/Properties/AssemblyInfo.cs
+++ Editor/AGS.Types/Properties/AssemblyInfo.cs
@@ -22,9 +22,9 @@ namespace AGS.Types
 	public class Version
 	{
 		public static readonly bool IS_BETA_VERSION = true;
-		public const string AGS_EDITOR_DATE = "June 2017";
+		public const string AGS_EDITOR_DATE = "August 2017";
 		public const string AGS_EDITOR_FRIENDLY_VERSION = "3.4.1";
-        public const string AGS_EDITOR_VERSION = "3.4.1.6";
+        public const string AGS_EDITOR_VERSION = "3.4.1.7";
         public const string AGS_EDITOR_COPYRIGHT = "Copyright © 2006-2011 Chris Jones and 2011-2017 others.";
 	}
 }

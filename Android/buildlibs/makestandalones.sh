ac94fc29a3c7714d613554530a60184bfa7edb90
ericoporto <ericoporto2008@gmail.com>
2018-02-14 09:20:08 -0200

    updates the Android port to use ndk-bundle/platforms/android-14
    Commands I used to build:
    
        export PATH=$PATH:~/Android/Sdk/ndk-bundle/
        cd git/ags/Android/buildlibs/
        ./makestandalones.sh
        ./buildall.sh
        cd ../library/
        ndk-build

--- Android/buildlibs/makestandalones.sh
+++ Android/buildlibs/makestandalones.sh
@@ -5,18 +5,19 @@
 set -e
 
 # Last committed pre-compiled libs were with R8
-NDK_HOME=/opt/android-ndk-r10e
+NDK_HOME=~/Android/Sdk/ndk-bundle
 
 # android-9 is minimum to support mips and x86
-PLATFORM=android-9
+PLATFORM=android-14
 
 # standalone toolchains cannot share same directory
-NDK_STANDALONE=/opt/android-ndk-r10e-standalone/$PLATFORM
+NDK_STANDALONE=~/Android/Sdk/ndk-bundle/platforms/$PLATFORM
 
+# if you don't pass force, the directory is not rewritten
 for arch in arm x86 mips
 do
     INSTALL_DIR=$NDK_STANDALONE/$arch
     mkdir -p $INSTALL_DIR
-    $NDK_HOME/build/tools/make-standalone-toolchain.sh --platform=$PLATFORM --install-dir=$INSTALL_DIR --arch=$arch
+    $NDK_HOME/build/tools/make-standalone-toolchain.sh --force --platform=$PLATFORM --install-dir=$INSTALL_DIR --arch=$arch
 done
 

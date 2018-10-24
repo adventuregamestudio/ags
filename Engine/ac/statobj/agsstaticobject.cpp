a411f34729f0bcf4e159bb1056c72454cc61e117
Ivan Mogilko <ikm_spb@yahoo.com>
2018-10-24 04:34:52 +0300

    Engine: fixed ReadScriptAlignment() was applied in a wrong operation

--- Engine/ac/statobj/agsstaticobject.cpp
+++ Engine/ac/statobj/agsstaticobject.cpp
@@ -58,21 +58,15 @@ void AGSStaticObject::WriteFloat(const char *address, intptr_t offset, float val
 }
 
 
-int32_t StaticGame::ReadInt32(const char *address, intptr_t offset)
-{
-    int32_t val = *(int32_t*)(address + offset);
-    if (offset == 99 * sizeof(int32_t) || offset == 112 * sizeof(int32_t))
-    { // game.text_align, game.speech_text_align
-        return ReadScriptAlignment(val);
-    }
-    return val;
-}
-
 void StaticGame::WriteInt32(const char *address, intptr_t offset, int32_t val)
 {
     if (offset == 4 * sizeof(int32_t))
     { // game.debug_mode
         set_debug_mode(val != 0);
+    }
+    else if (offset == 99 * sizeof(int32_t) || offset == 112 * sizeof(int32_t))
+    { // game.text_align, game.speech_text_align
+        *(int32_t*)(address + offset) = ReadScriptAlignment(val);
     }
     else
     {

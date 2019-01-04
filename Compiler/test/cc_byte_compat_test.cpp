b2a6740bdb0a51bcd33606b56a758950c7390479
Peter G Bouillon [fernewelten] <fernewelten@fantasymail.de>
2019-01-05 00:21:56 +0100

    Snapshot

--- Compiler/test/cc_byte_compat_test.cpp
+++ Compiler/test/cc_byte_compat_test.cpp
@@ -1672,6 +1672,7 @@ TEST(Compatibility, ArrayOfPointers2) {
     EXPECT_EQ(numfixups, scrip->numfixups);
 }
 
+
 TEST(Compatibility, ArrayOfPointers2a) {
     ccCompiledScript *scrip = newScriptFixture();
 
@@ -1685,9 +1686,8 @@ TEST(Compatibility, ArrayOfPointers2a) {
     int main()                           \n\
     {                                    \n\
         Struct *arr2[5];                 \n\
-        for (int i = 0; i < 4; i++) {    \n\
-                arr2[i] = new Struct;    \n\
-        }                                \n\    }                                    \n\
+        arr2[5].Float = 2.2;             \n\
+    }                                    \n\
     ";
 
     last_seen_cc_error = 0;
@@ -1698,25 +1698,21 @@ TEST(Compatibility, ArrayOfPointers2a) {
     // run the test, comment out the previous line 
     // and append its output below.
     // Then run the test in earnest after changes have been made to the code
-
-    const size_t codesize = 111;
+    const size_t codesize = 80;
     EXPECT_EQ(codesize, scrip->codesize);
 
     intptr_t code[] = {
       38,    0,    3,    1,            2,   63,   20,    1,    // 7
-       1,   20,    6,    3,            0,    3,    1,    2,    // 15
-       8,    3,    1,    1,            4,   51,    4,    7,    // 23
-       3,   29,    3,    6,            3,    4,   30,    4,    // 31
-      18,    4,    3,    3,            4,    3,   28,   42,    // 39
-      73,    3,    8,   29,            3,   51,    8,    7,    // 47
-       3,   46,    3,    5,            3,    3,    7,   30,    // 55
-       3,   51,   24,   32,            7,    4,   11,    2,    // 63
-       7,   29,    2,   30,            2,   47,    3,   51,    // 71
-       4,    7,    3,    1,            3,    1,    8,    3,    // 79
-      31,  -61,    2,    1,            4,    6,    3,    0,    // 87
-      51,   20,   69,    1,            2,    4,   69,    1,    // 95
-       2,    4,   69,    1,            2,    4,   69,    1,    // 103
-       2,    4,   69,    2,            1,   20,    5,  -999
+       1,   20,    6,    3,         1074580685,   29,    3,    6,    // 15
+       3,    5,   46,    3,            5,    3,    3,    7,    // 23
+      30,    3,   51,   20,           32,    7,    4,   11,    // 31
+       2,    7,   29,    2,           30,    2,   29,    3,    // 39
+      48,    3,   30,    4,           29,    3,    3,    4,    // 47
+       3,   30,    2,   52,            8,    3,    6,    3,    // 55
+       0,   51,   20,   69,            1,    2,    4,   69,    // 63
+       1,    2,    4,   69,            1,    2,    4,   69,    // 71
+       1,    2,    4,   69,            2,    1,   20,    5,    // 79
+     -999
     };
 
     for (size_t idx = 0; idx < codesize; idx++)

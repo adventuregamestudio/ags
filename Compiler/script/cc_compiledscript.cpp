bd28b53534b783a080879055726ac14bd2eb9536
Peter G Bouillon [fernewelten] <fernewelten@fantasymail.de>
2019-01-06 00:08:38 +0100

    Snapshot

--- Compiler/script/cc_compiledscript.cpp
+++ Compiler/script/cc_compiledscript.cpp
@@ -368,7 +368,7 @@ void ccCompiledScript::flush_line_numbers()
 void ccCompiledScript::write_code(intptr_t byy) 
 {
     // DEBUGGING [fw]
-    if (codesize == 49)
+    if (codesize == 48)
     {
         int iii = 0;
     }

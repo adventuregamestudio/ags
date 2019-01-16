a186cb626424b72fec1d7f6a2ed0b3a04fc2df66
Ivan Mogilko <ikm_spb@yahoo.com>
2019-01-16 18:06:08 +0300

    Editor: added missing audio options to Default Setup

---
+++ Editor/AGS.Types/Enums/RuntimeAudioDriver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AGS.Types
+{
+    // Problem is that every platform has their own, so we try to be as universal as possible here
+    public enum RuntimeAudioDriver
+    {
+        Default = -1,
+        Disabled = 0
+    }
+}

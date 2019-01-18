ea895ae77f6c7bceafe8e8c50df81cc740fbb8b7
Peter G Bouillon [fernewelten] <fernewelten@fantasymail.de>
2019-01-18 14:09:17 +0100

    Warn that the C++ preprocessing functions aren't used any longer

--- Compiler/script/cs_prepro.h
+++ Compiler/script/cs_prepro.h
@@ -5,6 +5,11 @@
 //  Should be used only internally by cs_compiler.cpp
 //-----------------------------------------------------------------------------
 
+
+//-----------------------------------------------------------------------------
+// NOTE: All macro processing is currently done within the Editor.
+//       These functions aren't called any more.
+
 #ifndef __CS_PREPRO_H
 #define __CS_PREPRO_H
 

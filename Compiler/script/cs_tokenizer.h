907f9339b422b353d2b6f0350921057fdfee3515
Peter G Bouillon [fernewelten] <fernewelten@fantasymail.de>
2019-02-02 10:53:59 +0100

    Blow up cs_parser.cpp, cs_parser.h, cs_scanner_test.cpp to shorten cs_parser.cpp which had 8k lines before

---
+++ Compiler/script/cs_tokenizer.h
@@ -0,0 +1,135 @@
+#ifndef __CS_TOKENIZER_H
+#define __CS_TOKENIZER_H
+
+#include <vector>
+#include <string>
+
+#include "cc_compiledscript.h"
+#include "cc_internallist.h"
+#include "cc_symboltable.h"
+
+#include "cs_scanner.h"
+
+
+namespace AGS
+{
+// Scans symstrings as necessary (through the scanner), converts them to tokens and returns tokens one-by-one.
+// Strings are passed to the string collector.
+class Tokenizer
+{
+public:
+    enum Modes
+    {
+        kMode_Standard = 0, // Tokenizer isn't in any special mode
+        kMode_StructDecl,   // Tokenizer is in a struct declaration
+    };
+
+    // ctors
+    Tokenizer();
+
+    // Note: The tokenizer does not "own" the objects below: 
+    // When it destructs, it will make no attempt to destruct *scanner etc.
+    Tokenizer(
+        Scanner *scanner,
+        struct ::ccInternalList *token_list,
+        struct ::SymbolTable *symbol_table,
+        struct ::ccCompiledScript *string_collector);
+
+    // setters
+    void SetScanner(Scanner *scanner);
+    void SetTokenList(struct ::ccInternalList *token_list);
+    void SetSymbolTable(struct ::SymbolTable *symbol_table);
+    void SetStringCollector(struct ::ccCompiledScript * string_collector);
+
+    /// Initialize internal variables
+    void Reset();
+
+    /// Get next token, reading symstrings as necessary
+    void GetNextToken(
+        int &token,                 // The token that is read
+        bool &eof_encountered,      // Is true if symstring couldn't be tokenized; details in GetLastError()
+        bool &error_encountered);   // Is true if the scanner should reset the line numbering
+
+    /// If Tokenizer couldn't tokenize a symstring, explain why
+    const std::string GetLastError();
+
+    /// The tokenizer gives some symbols a temporary type; reset those types
+    void ResetTemporaryTypesInSymbolTable();
+
+
+protected:
+    // Does the real work processing the scanner symstring
+    void ProcessScannerSymstring(Scanner::ScanType symbol_type, std::string & symstring, int last_token, int &token, bool & eof_encountered, bool & error_encountered);
+
+    void TokenizeStringLiteral(int token, std::string const &symstring);
+
+    // Collect parentheses, brackets, braces as they come up; check whether they match
+    void CheckMatcherNesting(int token, bool &error_encountered);
+
+    // Given the token of a struct name and a member name, generate a string denoting the full name of the member
+    std::string FullNameFromStructAndMember(int struct_name_token, int member_name_token);
+
+    int ConvertSymstringToTokenIndex(std::string symstring);
+
+private:
+    // Collect a sequence of opening ("([{") and closing (")]}") symbols; check matching
+    class OpenCloseMatcher
+    {
+    public:
+        // c'tor
+        OpenCloseMatcher();
+
+        // Parameters: an opening thing, the thing that is expected to close it later, the current line number
+        // Pushes that onto a stack
+        void Push(std::string const &opener, std::string const &expected_closer, int lineno);
+
+        // Input a closing thing; this is matched against the symbol of the "put" method earlier.
+        // If they don't match, an error is generated.
+        void PopAndCheck(std::string const &closer, int lineno, bool &error_encountered);
+
+        // Get the last error encountered.
+        std::string GetLastError();
+
+        // Reset the matcher.
+        void Reset();
+
+
+    private:
+        struct OpenInfo
+        {
+            std::string _opener;
+            std::string _closer;
+            int _lineno;  // of the Opener symbol
+        };
+        std::vector<OpenInfo> _openInfoStack;
+        std::string _lastError;
+    };
+
+private:
+    Scanner *_scanner;
+    struct ::ccInternalList *_tokenList;
+    struct ::SymbolTable *_symbolTable;
+    struct ::ccCompiledScript *_stringCollector;
+
+    std::string _lastError;
+    OpenCloseMatcher _ocMatcher;
+
+    Modes _currentMode;
+    bool _inTypeSubmode;
+    int _braceNestingDepthInStructDecl; // only used in StructDeclMode
+    int _structBeingDeclared;
+    int _parenthesisNestingDepth;
+    std::deque<int> _tokenBuffer;
+
+    inline Symbol TokenType(int token) { return _symbolTable->entries[token].stype; }
+    inline std::string TokenName(int token) { return _symbolTable->entries[token].sname; }
+    inline void SetTokenType(int token, Symbol value) { _symbolTable->entries[token].stype = value; }
+    inline void SetTokenOffsetInStrings(int token, int value) { _symbolTable->entries[token].soffs = value; }
+    inline void SetTokenVartype(int token, Symbol value) { _symbolTable->entries[token].vartype = value; }
+
+
+};
+
+} // namespace AGS
+
+#endif
---
+++ Compiler/test/cs_tokenizer_test.cpp
@@ -0,0 +1,332 @@
+#include "gtest/gtest.h" 
+
+#include <string>
+#include <iostream>
+#include "script/cs_parser.h"
+#include "script/cc_symboltable.h"
+#include "script/cc_internallist.h"
+
+#include "script/cs_tokenizer.h"
+
+extern ccCompiledScript *newScriptFixture(); // cs_parser_test.cpp
+
+TEST(Tokenize, UnknownKeywordAfterReadonly) {
+    ccCompiledScript *scrip = newScriptFixture();
+
+    // This incorrect code would crash the tokenizer.
+    char *inpl = "struct MyStruct \
+        {\
+          readonly int2 a; \
+          readonly int2 b; \
+        };";
+
+    ccInternalList targ;
+    int tokenizeResult = cc_tokenize(inpl, &targ, scrip);
+
+    ASSERT_EQ(0, tokenizeResult);
+}
+
+TEST(Tokenize, BasicStruct)
+{
+    sym.reset();
+
+    // Basic function of the Tokenizer
+    // Jibbling the struct name with the struct components
+    std::string Input = "\
+        int A; \
+        struct B { \
+            String B; \
+            float A; \
+        }; \
+        int C; \
+";
+
+    struct ccInternalList TokenList;
+    struct ccCompiledScript StringCollect;
+
+    AGS::Scanner scanner(Input, 1, &TokenList);
+    AGS::Tokenizer tokenizer(&scanner, &TokenList, &sym, &StringCollect);
+
+    int token;
+    bool eof_encountered;
+    bool error_encountered;
+    std::string token_str;
+    int token_type;
+
+    // int
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_str = sym.get_name_string(token);
+    ASSERT_EQ(0, token_str.compare("int"));
+
+    // A
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_str = sym.get_name_string(token);
+    ASSERT_EQ(0, token_str.compare("A"));
+
+    // ;
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_str = sym.get_name_string(token);
+    ASSERT_EQ(0, token_str.compare(";"));
+
+
+    // struct 
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_str = sym.get_name_string(token);
+    ASSERT_EQ(0, token_str.compare("struct"));
+    token_type = sym.get_type(token);
+    ASSERT_EQ(SYM_STRUCT, token_type);
+
+    // B 
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_str = sym.get_name_string(token);
+    ASSERT_EQ(0, token_str.compare("B"));
+    token_type = sym.get_type(token);
+    ASSERT_EQ(0, token_type); // new, unknown token
+
+    // {
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_type = sym.get_type(token);
+    ASSERT_EQ(SYM_OPENBRACE, token_type);
+
+    // String 
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_str = sym.get_name_string(token);
+    ASSERT_EQ(0, token_str.compare("String"));
+
+
+    // B
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_str = sym.get_name_string(token);
+    ASSERT_EQ(0, token_str.compare("B::B"));  // Important: B must be jibbled to B
+
+    // ;
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_type = sym.get_type(token);
+    ASSERT_EQ(SYM_SEMICOLON, token_type);
+
+    // float
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_str = sym.get_name_string(token);
+    ASSERT_EQ(0, token_str.compare("float"));
+
+    // A
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_str = sym.get_name_string(token);
+    ASSERT_EQ(0, token_str.compare("B::A"));  // Important: B must be jibbled to A
+
+    // ;
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_type = sym.get_type(token);
+    ASSERT_EQ(SYM_SEMICOLON, token_type);
+
+    // }
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_type = sym.get_type(token);
+    ASSERT_EQ(SYM_CLOSEBRACE, token_type);
+
+    // ;
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+
+    // int 
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+
+    // C
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_str = sym.get_name_string(token);
+    ASSERT_EQ(0, token_str.compare("C"));
+    token_type = sym.get_type(token);
+
+    // ;
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_type = sym.get_type(token);
+    ASSERT_EQ(SYM_SEMICOLON, token_type);
+
+    // EOF
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_TRUE(eof_encountered);
+}
+
+TEST(Tokenize, SectionChange)
+{
+    sym.reset();
+
+    // Basic function of the Tokenizer
+    // Jibbling the struct name with the struct components
+    std::string Input = "\
+        \n\
+        String A = \"__NEWSCRIPTSTART_Foo\"; \n\
+     ";
+
+    struct ccInternalList TokenList;
+    struct ccCompiledScript StringCollect;
+
+    AGS::Scanner scanner(Input, 1, &TokenList);
+    AGS::Tokenizer tokenizer(&scanner, &TokenList, &sym, &StringCollect);
+
+    int token;
+    bool eof_encountered;
+    bool error_encountered;
+    std::string token_str;
+    int token_type;
+
+    // String
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_str = sym.get_name_string(token);
+    ASSERT_EQ(0, token_str.compare("String"));
+
+    // A
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_str = sym.get_name_string(token);
+    ASSERT_EQ(0, token_str.compare("A"));
+
+    // =
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    token_str = sym.get_name_string(token);
+    ASSERT_EQ(0, token_str.compare("="));
+
+
+    // ((Section change))
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    ASSERT_FALSE(eof_encountered);
+    ASSERT_FALSE(error_encountered);
+    ASSERT_EQ(0, scanner.GetLineno());
+}
+
+
+
+TEST(Tokenize, MatchBraceParen1)
+{
+    // The tokenizer checks that nested (), [], {} match.
+
+    // This closing ']' does not match the '{' on line 99
+    std::string Input = "   \r\n\
+    struct B {          \r\n\
+        String B;       \r\n\
+        float A;        \r\n\
+    ];                  \r\n\
+";
+
+    struct ccInternalList TokenList;
+    struct ccCompiledScript StringCollect;
+    sym.reset();
+    AGS::Scanner scanner(Input, 1, &TokenList);
+    AGS::Tokenizer tokenizer(&scanner, &TokenList, &sym, &StringCollect);
+
+    int token;
+    bool eof_encountered;
+    bool error_encountered;
+
+    for (size_t count = 0; count < 9; count++)
+    {
+        tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+        if (eof_encountered || error_encountered) break;
+    }
+
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    EXPECT_TRUE(error_encountered);
+    std::string errorstring = tokenizer.GetLastError();
+    size_t ret = errorstring.find("line 2");
+    EXPECT_NE(std::string::npos, ret);
+}
+
+TEST(Tokenize, MatchBraceParen2)
+{
+    // The tokenizer checks that nested (), [], {} match.
+
+    struct ccInternalList TokenList;
+    struct ccCompiledScript StringCollect;
+
+    // This closing ')' does not match the '[' on this line
+    std::string Input = "f(a[bb.ccc * (d + e - ( f - g)))";
+    sym.reset();
+    AGS::Scanner scanner(Input, 1, &TokenList);
+    AGS::Tokenizer tokenizer(&scanner, &TokenList, &sym, &StringCollect);
+    bool eof_encountered;
+    bool error_encountered;
+    int token;
+
+    for (size_t count = 0; count < 19; count++)
+    {
+        tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+        if (eof_encountered || error_encountered) break;
+    }
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    EXPECT_TRUE(error_encountered);
+    std::string errorstring = tokenizer.GetLastError();
+    size_t ret = errorstring.find("this line");
+}
+
+TEST(Tokenize, MatchBraceParen3)
+{
+    // The tokenizer checks that nested (), [], {} match.
+
+    // This closing ']' does not match the '{' on line 99
+    std::string Input = "   \r\n\
+    struct B [          \r\n\
+        String B;       \r\n\
+        float A;        \r\n\
+    };                  \r\n\
+";
+
+    struct ccInternalList TokenList;
+    struct ccCompiledScript StringCollect;
+    sym.reset();
+    AGS::Scanner scanner(Input, 1, &TokenList);
+    AGS::Tokenizer tokenizer(&scanner, &TokenList, &sym, &StringCollect);
+
+    int token;
+    bool eof_encountered;
+    bool error_encountered;
+
+    for (size_t count = 0; count < 9; count++)
+    {
+        tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+        if (eof_encountered || error_encountered) break;
+    }
+
+    tokenizer.GetNextToken(token, eof_encountered, error_encountered);
+    EXPECT_TRUE(error_encountered);
+    std::string errorstring = tokenizer.GetLastError();
+    size_t ret = errorstring.find("line 2");
+    EXPECT_NE(std::string::npos, ret);
+}

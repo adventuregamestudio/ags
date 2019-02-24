adventuregamestudio/ags

commit 9b7c8196bf1ebf80dd579306e1e0ef75f5f881eb

Common: proper ctor and dtor for GameSetupStruct

Common/game/main_game_file.cpp
@@ -489,10 +489,10 @@ void UpgradeAudio(GameSetupStruct &game, GameDataVersion data_ver)
     audioClipCount = 1000;
     audioClipTypeCount = 4;
 
-    audioClipTypes = (AudioClipType*)malloc(audioClipTypeCount * sizeof(AudioClipType));
+    audioClipTypes = new AudioClipType[audioClipTypeCount];
     memset(audioClipTypes, 0, audioClipTypeCount * sizeof(AudioClipType));
 
-    audioClips = (ScriptAudioClip*)malloc(audioClipCount * sizeof(ScriptAudioClip));
+    audioClips = new ScriptAudioClip[audioClipCount];
     memset(audioClips, 0, audioClipCount * sizeof(ScriptAudioClip));
 
     // TODO: find out what is 4
@@ -503,7 +503,6 @@ void UpgradeAudio(GameSetupStruct &game, GameDataVersion data_ver)
         audioClipTypes[i].volume_reduction_while_speech_playing = 10;
     }
     audioClipTypes[3].reservedChannels = 0;
-
 
     audioClipCount = 0;
 
@@ -519,7 +518,10 @@ void UpgradeAudio(GameSetupStruct &game, GameDataVersion data_ver)
     if (game_lib)
         BuildAudioClipArray(game, *game_lib);
 
-    audioClips = (ScriptAudioClip*)realloc(audioClips, audioClipCount * sizeof(ScriptAudioClip));
+    ScriptAudioClip *real_clips = new ScriptAudioClip[audioClipCount];
+    memcpy(real_clips, audioClips, audioClipCount);
+    delete[] audioClips;
+    audioClips = real_clips;
     game.scoreClipID = -1;
 }
 

Common/ac/gamesetupstruct.cpp
@@ -23,6 +23,82 @@
 
 using namespace AGS::Common;
 
+GameSetupStruct::GameSetupStruct()
+    : filever(0)
+    , intrChar(NULL)
+    , charScripts(NULL)
+    , invScripts(NULL)
+    , roomCount(0)
+    , roomNumbers(NULL)
+    , roomNames(NULL)
+    , audioClipCount(0)
+    , audioClips(NULL)
+    , audioClipTypeCount(0)
+    , audioClipTypes(NULL)
+    , scoreClipID(0)
+{
+    memset(intrInv, 0, sizeof(intrInv));
+    memset(lipSyncFrameLetters, 0, sizeof(lipSyncFrameLetters));
+    memset(guid, 0, sizeof(guid));
+    memset(saveGameFileExtension, 0, sizeof(saveGameFileExtension));
+    memset(saveGameFolderName, 0, sizeof(saveGameFolderName));
+}
+
+GameSetupStruct::~GameSetupStruct()
+{
+    Free();
+}
+
+void GameSetupStruct::Free()
+{
+    GameSetupStructBase::Free();
+
+    if (intrChar)
+    {
+        for (int i = 0; i < numcharacters; ++i)
+            delete intrChar[i];
+        delete[] intrChar;
+        intrChar = NULL;
+    }
+
+    if (charScripts)
+    {
+        for (int i = 0; i < numcharacters; ++i)
+            delete charScripts[i];
+        delete[] charScripts;
+        charScripts = NULL;
+    }
+    numcharacters = 0;
+
+    for (int i = 1; i < numinvitems; i++)
+        delete intrInv[i];
+
+    if (invScripts)
+    {
+        for (int i = 1; i < numinvitems; i++)
+            delete invScripts[i];
+        delete invScripts;
+        invScripts = NULL;
+    }
+    numinvitems = 0;
+
+    for (int i = 0; i < roomCount; i++)
+        delete roomNames[i];
+    delete[] roomNames;
+    delete[] roomNumbers;
+    roomCount = 0;
+
+    delete[] audioClips;
+    audioClips = NULL;
+    delete[] audioClipTypes;
+    audioClipTypes = NULL;
+    audioClipCount = 0;
+    audioClipTypeCount = 0;
+
+    charProps.clear();
+    viewNames.clear();
+}
+
 // Assigns font info parameters using flags value read from the game data
 void SetFontInfoFromSerializedFlags(FontInfo &finfo, char flags)
 {
@@ -148,7 +224,7 @@ void GameSetupStruct::read_interaction_scripts(Common::Stream *in, GameDataVersi
 void GameSetupStruct::read_words_dictionary(Common::Stream *in)
 {
     if (load_dictionary) {
-        dict = (WordsDictionary*)malloc(sizeof(WordsDictionary));
+        dict = new WordsDictionary();
         read_dictionary (dict, in);
     }
 }
@@ -178,7 +254,7 @@ void GameSetupStruct::WriteMouseCursors_Aligned(Stream *out)
 
 void GameSetupStruct::read_characters(Common::Stream *in, GameDataVersion data_ver)
 {
-    chars=(CharacterInfo*)calloc(1,sizeof(CharacterInfo)*numcharacters+5);
+    chars = new CharacterInfo[numcharacters + 5]; // TODO: why +5, is this really needed?
 
     ReadCharacters_Aligned(in);
 }
@@ -193,7 +269,7 @@ void GameSetupStruct::read_messages(Common::Stream *in, GameDataVersion data_ver
 {
     for (int ee=0;ee<MAXGLOBALMES;ee++) {
         if (!load_messages[ee]) continue;
-        messages[ee]=(char*)malloc(GLOBALMESLENGTH);
+        messages[ee] = new char[GLOBALMESLENGTH];
 
         if (data_ver < kGameVersion_261) // Global messages are not encrypted on < 2.61
         {
@@ -304,14 +380,14 @@ void GameSetupStruct::read_room_names(Stream *in, GameDataVersion data_ver)
     if ((data_ver >= kGameVersion_301) && (options[OPT_DEBUGMODE] != 0))
     {
         roomCount = in->ReadInt32();
-        roomNumbers = (int*)malloc(roomCount * sizeof(int));
-        roomNames = (char**)malloc(roomCount * sizeof(char*));
+        roomNumbers = new int[roomCount];
+        roomNames = new char*[roomCount];
         String pexbuf;
         for (int bb = 0; bb < roomCount; bb++)
         {
             roomNumbers[bb] = in->ReadInt32();
             pexbuf.Read(in, STD_BUFFER_SIZE);
-            roomNames[bb] = (char*)malloc(pexbuf.GetLength() + 1);
+            roomNames[bb] = new char[pexbuf.GetLength() + 1];
             strcpy(roomNames[bb], pexbuf);
         }
     }

Common/ac/gamesetupstruct.h
@@ -90,6 +90,11 @@ struct GameSetupStruct: public GameSetupStructBase {
     inline int GetColorDepth() const { return color_depth * 8; }
 
 
+    GameSetupStruct();
+    ~GameSetupStruct();
+
+    void Free();
+
     // [IKM] Game struct loading code is moved here from Engine's load_game_file
     // function; for now it is not supposed to be called by Editor; although it
     // is possible that eventually will be.

Common/ac/gamesetupstructbase.cpp
@@ -32,7 +32,26 @@ GameSetupStructBase::GameSetupStructBase()
 
 GameSetupStructBase::~GameSetupStructBase()
 {
-    delete [] load_messages;
+    Free();
+}
+
+void GameSetupStructBase::Free()
+{
+    for (int i = 0; i < MAXGLOBALMES; ++i)
+    {
+        delete[] messages[i];
+        messages[i] = NULL;
+    }
+    delete[] load_messages;
+    load_messages = NULL;
+    delete dict;
+    dict = NULL;
+    delete globalscript;
+    globalscript = NULL;
+    delete compiled_script;
+    compiled_script = NULL;
+    delete[] chars;
+    chars = NULL;
 }
 
 void GameSetupStructBase::SetDefaultResolution(GameResolutionType resolution_type)

Common/ac/gamesetupstructbase.h
@@ -72,6 +72,7 @@ struct GameSetupStructBase {
 
     GameSetupStructBase();
     virtual ~GameSetupStructBase();
+    void Free();
     void SetDefaultResolution(GameResolutionType resolution_type);
     void SetCustomResolution(Size game_res);
     void ReadFromFile(Common::Stream *in);

Common/ac/wordsdictionary.cpp
@@ -23,27 +23,40 @@
 
 using AGS::Common::Stream;
 
+WordsDictionary::WordsDictionary()
+    : num_words(0)
+    , word(NULL)
+    , wordnum(NULL)
+{
+}
+
+WordsDictionary::~WordsDictionary()
+{
+    free_memory();
+}
+
 void WordsDictionary::allocate_memory(int wordCount)
 {
     num_words = wordCount;
     if (num_words > 0)
     {
-        word = (char**)malloc(wordCount * sizeof(char*));
-        word[0] = (char*)malloc(wordCount * MAX_PARSER_WORD_LENGTH);
-        wordnum = (short*)malloc(wordCount * sizeof(short));
+        word = new char*[wordCount];
+        word[0] = new char[wordCount * MAX_PARSER_WORD_LENGTH];
+        wordnum = new short[wordCount];
         for (int i = 1; i < wordCount; i++)
         {
             word[i] = word[0] + MAX_PARSER_WORD_LENGTH * i;
         }
     }
 }
+
 void WordsDictionary::free_memory()
 {
     if (num_words > 0)
     {
-        free(word[0]);
-        free(word);
-        free(wordnum);
+        delete [] word[0];
+        delete [] word;
+        delete [] wordnum;
         word = NULL;
         wordnum = NULL;
         num_words = 0;

Common/ac/wordsdictionary.h
@@ -29,6 +29,8 @@ struct WordsDictionary {
     char**word;
     short*wordnum;
 
+    WordsDictionary();
+    ~WordsDictionary();
     void allocate_memory(int wordCount);
     void free_memory();
     void  sort();

Editor/AGS.Native/agsnative.cpp
@@ -1541,23 +1541,10 @@ void free_script_modules() {
 void free_old_game_data()
 {
   int bb;
-  for (bb=0;bb<MAXGLOBALMES;bb++) {
-    if (thisgame.messages[bb] != NULL)
-      free(thisgame.messages[bb]);
-  }
   for (bb = 0; bb < thisgame.numdialog; bb++) 
   {
 	  if (dialog[bb].optionscripts != NULL)
 		  free(dialog[bb].optionscripts);
-  }
-  thisgame.charProps.clear();
-  if (thisgame.intrChar != NULL)
-  {
-    for (bb = 0; bb < thisgame.numcharacters; bb++)
-      delete thisgame.intrChar[bb];
-    
-    free(thisgame.intrChar);
-    thisgame.intrChar = NULL;
   }
   for (bb = 0; bb < numNewViews; bb++)
   {
@@ -1567,14 +1554,13 @@ void free_old_game_data()
     }
     newViews[bb].Dispose();
   }
-  thisgame.viewNames.clear();
   free(newViews);
   guis.clear();
-  free(thisgame.chars);
-  thisgame.dict->free_memory();
-  free(thisgame.dict);
   free(dialog);
   free_script_modules();
+
+  // free game struct last because it contains object counts
+  thisgame.Free();
 }
 
 // remap the scene, from its current palette oldpale to palette

commit 57acecbfb77764b378323a92194d46a391b2328a

Common: refactored UpgradeAudio() and BuildAudioClipArray()
Also this fixes a memcpy bug introduced by 9b7c8196bf1ebf80dd579306e1e0ef75f5f881eb

Common/game/main_game_file.cpp
@@ -389,61 +389,57 @@ HGameFileError ReadPlugins(std::vector<PluginInfo> &infos, Stream *in)
 // Create the missing audioClips data structure for 3.1.x games.
 // This is done by going through the data files and adding all music*.*
 // and sound*.* files to it.
-void BuildAudioClipArray(GameSetupStruct &game, const AssetLibInfo &lib)
+void BuildAudioClipArray(const AssetLibInfo &lib, std::vector<ScriptAudioClip> &audioclips)
 {
     char temp_name[30];
     int temp_number;
     char temp_extension[10];
-
-    // TODO: this was done to simplify code transition; ideally we should be
-    // working with GameSetupStruct's getters and setters here
-    int &audioClipCount            = game.audioClipCount;
-    ScriptAudioClip *&audioClips   = game.audioClips;
 
     size_t number_of_files = lib.AssetInfos.size();
     for (size_t i = 0; i < number_of_files; ++i)
     {
         if (sscanf(lib.AssetInfos[i].FileName, "%5s%d.%3s", temp_name, &temp_number, temp_extension) == 3)
         {
+            audioclips.push_back(ScriptAudioClip());
+            ScriptAudioClip &clip = audioclips.back();
             if (stricmp(temp_name, "music") == 0)
             {
-                sprintf(audioClips[audioClipCount].scriptName, "aMusic%d", temp_number);
-                sprintf(audioClips[audioClipCount].fileName, "music%d.%s", temp_number, temp_extension);
-                audioClips[audioClipCount].bundlingType = (stricmp(temp_extension, "mid") == 0) ? AUCL_BUNDLE_EXE : AUCL_BUNDLE_VOX;
-                audioClips[audioClipCount].type = 2;
-                audioClips[audioClipCount].defaultRepeat = 1;
+                sprintf(clip.scriptName, "aMusic%d", temp_number);
+                sprintf(clip.fileName, "music%d.%s", temp_number, temp_extension);
+                clip.bundlingType = (stricmp(temp_extension, "mid") == 0) ? AUCL_BUNDLE_EXE : AUCL_BUNDLE_VOX;
+                clip.type = 2;
+                clip.defaultRepeat = 1;
             }
             else if (stricmp(temp_name, "sound") == 0)
             {
-                sprintf(audioClips[audioClipCount].scriptName, "aSound%d", temp_number);
-                sprintf(audioClips[audioClipCount].fileName, "sound%d.%s", temp_number, temp_extension);
-                audioClips[audioClipCount].bundlingType = AUCL_BUNDLE_EXE;
-                audioClips[audioClipCount].type = 3;
+                sprintf(clip.scriptName, "aSound%d", temp_number);
+                sprintf(clip.fileName, "sound%d.%s", temp_number, temp_extension);
+                clip.bundlingType = AUCL_BUNDLE_EXE;
+                clip.type = 3;
             }
             else
             {
+                audioclips.pop_back();
                 continue;
             }
 
-            audioClips[audioClipCount].defaultVolume = 100;
-            audioClips[audioClipCount].defaultPriority = 50;
-            audioClips[audioClipCount].id = audioClipCount;
+            clip.defaultVolume = 100;
+            clip.defaultPriority = 50;
+            clip.id = audioclips.size() - 1;
 
             if (stricmp(temp_extension, "mp3") == 0)
-                audioClips[audioClipCount].fileType = eAudioFileMP3;
+                clip.fileType = eAudioFileMP3;
             else if (stricmp(temp_extension, "wav") == 0)
-                audioClips[audioClipCount].fileType = eAudioFileWAV;
+                clip.fileType = eAudioFileWAV;
             else if (stricmp(temp_extension, "voc") == 0)
-                audioClips[audioClipCount].fileType = eAudioFileVOC;
+                clip.fileType = eAudioFileVOC;
             else if (stricmp(temp_extension, "mid") == 0)
-                audioClips[audioClipCount].fileType = eAudioFileMIDI;
+                clip.fileType = eAudioFileMIDI;
             else if ((stricmp(temp_extension, "mod") == 0) || (stricmp(temp_extension, "xm") == 0)
                 || (stricmp(temp_extension, "s3m") == 0) || (stricmp(temp_extension, "it") == 0))
-                audioClips[audioClipCount].fileType = eAudioFileMOD;
+                clip.fileType = eAudioFileMOD;
             else if (stricmp(temp_extension, "ogg") == 0)
-                audioClips[audioClipCount].fileType = eAudioFileOGG;
-
-            audioClipCount++;
+                clip.fileType = eAudioFileOGG;
         }
     }
 }
@@ -471,43 +467,22 @@ void UpgradeAudio(GameSetupStruct &game, GameDataVersion data_ver)
     // To make the modern engine compatible with pre-3.2 games, we have
     // to scan game data packages for audio files, and enumerate them
     // ourselves, then add this information to game struct.
-    //
-    // Some things below can be classified as "dirty hack" and should be
-    // fixed in the future.
-
-    // TODO: this was done to simplify code transition; ideally we should be
-    // working with GameSetupStruct's getters and setters here
-    int &audioClipCount            = game.audioClipCount;
-    ScriptAudioClip *&audioClips   = game.audioClips;
-    int &audioClipTypeCount        = game.audioClipTypeCount;
-    AudioClipType *&audioClipTypes = game.audioClipTypes;
 
     // Create soundClips and audioClipTypes structures.
-    //
-    // TODO: 1000 is a maximal number of clip entries this code is
-    // supporting, but it is not clear whether that limit is covering
-    // all possibilities. In any way, this array should be replaced
-    // with vector.
-    audioClipCount = 1000;
-    audioClipTypeCount = 4;
+    std::vector<AudioClipType> audiocliptypes;
+    std::vector<ScriptAudioClip> audioclips;
 
-    audioClipTypes = new AudioClipType[audioClipTypeCount];
-    memset(audioClipTypes, 0, audioClipTypeCount * sizeof(AudioClipType));
-
-    audioClips = new ScriptAudioClip[audioClipCount];
-    memset(audioClips, 0, audioClipCount * sizeof(ScriptAudioClip));
-
-    // TODO: find out what is 4
+    // TODO: find out what is 4 (maybe music, sound, ambient sound, voice?)
+    audiocliptypes.resize(4);
     for (int i = 0; i < 4; i++)
     {
-        audioClipTypes[i].reservedChannels = 1;
-        audioClipTypes[i].id = i;
-        audioClipTypes[i].volume_reduction_while_speech_playing = 10;
+        audiocliptypes[i].reservedChannels = 1;
+        audiocliptypes[i].id = i;
+        audiocliptypes[i].volume_reduction_while_speech_playing = 10;
     }
-    audioClipTypes[3].reservedChannels = 0;
+    audiocliptypes[3].reservedChannels = 0;
 
-    audioClipCount = 0;
-
+    audioclips.reserve(1000);
     // Read audio clip names from "music.vox", then from main library
     // TODO: this may become inconvenient that this code has to know about
     // "music.vox"; there might be better ways to handle this.
@@ -515,15 +490,23 @@ void UpgradeAudio(GameSetupStruct &game, GameDataVersion data_ver)
     // resolve this (as well as make it unnecessary to switch between them)
     AssetLibInfo music_lib;
     if (AssetManager::ReadDataFileTOC("music.vox", music_lib) == kAssetNoError)
-        BuildAudioClipArray(game, music_lib);
+        BuildAudioClipArray(music_lib, audioclips);
     const AssetLibInfo *game_lib = AssetManager::GetLibraryTOC();
     if (game_lib)
-        BuildAudioClipArray(game, *game_lib);
+        BuildAudioClipArray(*game_lib, audioclips);
 
-    ScriptAudioClip *real_clips = new ScriptAudioClip[audioClipCount];
-    memcpy(real_clips, audioClips, audioClipCount);
-    delete[] audioClips;
-    audioClips = real_clips;
+    // Copy gathered data over to game
+    delete[] game.audioClipTypes;
+    game.audioClipTypeCount = audiocliptypes.size();
+    game.audioClipTypes = new AudioClipType[game.audioClipTypeCount];
+    memcpy(game.audioClipTypes, &audiocliptypes.front(), sizeof(AudioClipType) * game.audioClipTypeCount);
+
+    delete[] game.audioClips;
+    game.audioClipCount = audioclips.size();
+    game.audioClips = new ScriptAudioClip[game.audioClipCount];
+    memcpy(game.audioClips, &audioclips.front(), sizeof(ScriptAudioClip) * game.audioClipCount);
+    
+    // Setup sound clip played on score event
     game.scoreClipID = -1;
 }
 
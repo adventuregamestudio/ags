76155b7f6bba7347206bb61b3c2b138ba981bf82
Peter G Bouillon [fernewelten] <fernewelten@fantasymail.de>
2019-03-12 14:22:09 +0100

    Whitespace redistribution

--- Compiler/script/cs_compiler.cpp
+++ Compiler/script/cs_compiler.cpp
@@ -1,11 +1,12 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "cs_compiler.h"
 #include "cc_macrotable.h"
 #include "cc_compiledscript.h"
 #include "cc_symboltable.h"
+
 #include "script/cc_error.h"
 #include "script/cc_options.h"
 #include "script/script_common.h"
@@ -15,20 +16,20 @@
 
 const char *ccSoftwareVersion = "1.0";
 
-char**defaultheaders = NULL;
-char**defaultHeaderNames = NULL;
-static int numheaders=0;
-static int capacityHeaders=0;
+char **defaultheaders = NULL;
+char **defaultHeaderNames = NULL;
+static int numheaders = 0;
+static int capacityHeaders = 0;
 
 MacroTable predefinedMacros;
 
-int ccAddDefaultHeader(char* nhead, char *nName)
+int ccAddDefaultHeader(char *nhead, char *nName)
 {
     if (numheaders >= capacityHeaders)
     {
         capacityHeaders += 50;
-        defaultheaders = (char**)realloc(defaultheaders, sizeof(char*) * capacityHeaders);
-        defaultHeaderNames = (char**)realloc(defaultHeaderNames, sizeof(char*) * capacityHeaders);
+        defaultheaders = (char **)realloc(defaultheaders, sizeof(char *) * capacityHeaders);
+        defaultHeaderNames = (char **)realloc(defaultHeaderNames, sizeof(char *) * capacityHeaders);
     }
 
     defaultheaders[numheaders] = nhead;
@@ -37,24 +38,29 @@ int ccAddDefaultHeader(char* nhead, char *nName)
     return 0;
 }
 
-void ccRemoveDefaultHeaders() {
+void ccRemoveDefaultHeaders()
+{
     numheaders = 0;
 }
 
-void ccDefineMacro(const char *macro, const char *definition) {
-    predefinedMacros.add((char*)macro, (char*)definition);
+void ccDefineMacro(const char *macro, const char *definition)
+{
+    predefinedMacros.add((char *)macro, (char *)definition);
 }
 
-void ccClearAllMacros() {
+void ccClearAllMacros()
+{
     predefinedMacros.shutdown();
     predefinedMacros.init();
 }
 
-void ccSetSoftwareVersion(const char *versionNumber) {
+void ccSetSoftwareVersion(const char *versionNumber)
+{
     ccSoftwareVersion = versionNumber;
 }
 
-ccScript* ccCompileText(const char *texo, const char *scriptName) {
+ccScript *ccCompileText(const char *texo, const char *scriptName)
+{
     int t;
     ccCompiledScript *cctemp = new ccCompiledScript();
     cctemp->init();
@@ -68,41 +74,48 @@ ccScript* ccCompileText(const char *texo, const char *scriptName) {
     ccError = 0;
     ccErrorLine = 0;
 
-    for (t=0;t<numheaders;t++) {
+    for (t = 0; t < numheaders; t++)
+    {
         if (defaultHeaderNames[t] != NULL)
             ccCurScriptName = defaultHeaderNames[t];
         else
             ccCurScriptName = "Internal header file";
 
         cctemp->start_new_section(ccCurScriptName);
-        cc_compile(defaultheaders[t],cctemp);
+        cc_compile(defaultheaders[t], cctemp);
         if (ccError) break;
     }
 
-    if (!ccError) {
+    if (!ccError)
+    {
         ccCurScriptName = scriptName;
         cctemp->start_new_section(ccCurScriptName);
-        cc_compile(texo,cctemp);
+        cc_compile(texo, cctemp);
     }
     preproc_shutdown();
 
-    if (ccError) {
+    if (ccError)
+    {
         cctemp->shutdown();
         delete cctemp;
         return NULL;
     }
 
-    for (t=0;t<sym.entries.size();t++) {
+    for (t = 0; t < sym.entries.size(); t++)
+    {
         int stype = sym.get_type(t);
         // blank out the name for imports that are not used, to save space
         // in the output file
-        if (((sym.entries[t].flags & SFLG_IMPORTED)!=0) && ((sym.entries[t].flags & SFLG_ACCESSED)==0)) {
+        if (((sym.entries[t].flags & SFLG_IMPORTED) != 0) && ((sym.entries[t].flags & SFLG_ACCESSED) == 0))
+        {
 
-            if ((stype == SYM_FUNCTION) || (stype == SYM_GLOBALVAR)) {
+            if ((stype == SYM_FUNCTION) || (stype == SYM_GLOBALVAR))
+            {
                 // unused func/variable
                 cctemp->imports[sym.entries[t].soffs][0] = 0;
             }
-            else if (sym.entries[t].flags & SFLG_ATTRIBUTE) {
+            else if (sym.entries[t].flags & SFLG_ATTRIBUTE)
+            {
                 // unused attribute -- get rid of the getter and setter
                 int attr_get = sym.entries[t].get_attrget();
                 if (attr_get > 0)
@@ -114,22 +127,28 @@ ccScript* ccCompileText(const char *texo, const char *scriptName) {
         }
 
         if ((sym.get_type(t) != SYM_GLOBALVAR) &&
-            (sym.get_type(t) != SYM_LOCALVAR)) continue;
+            (sym.get_type(t) != SYM_LOCALVAR))
+            continue;
 
-        if (sym.entries[t].flags & SFLG_IMPORTED) continue;
-        if (ccGetOption(SCOPT_SHOWWARNINGS)==0) ;
-        else if ((sym.entries[t].flags & SFLG_ACCESSED)==0) {
-            printf("warning: variable '%s' is never used\n",sym.get_friendly_name(t).c_str());
+        if (sym.entries[t].flags & SFLG_IMPORTED)
+            continue;
+        if (ccGetOption(SCOPT_SHOWWARNINGS) == 0);
+        else if ((sym.entries[t].flags & SFLG_ACCESSED) == 0)
+        {
+            printf("warning: variable '%s' is never used\n", sym.get_friendly_name(t).c_str());
         }
     }
 
-    if (ccGetOption(SCOPT_EXPORTALL)) {
+    if (ccGetOption(SCOPT_EXPORTALL))
+    {
         // export all functions
-        for (t=0;t<cctemp->numfunctions;t++) {
-            if (cctemp->add_new_export(cctemp->functions[t],EXPORT_FUNCTION,
-                cctemp->funccodeoffs[t], cctemp->funcnumparams[t]) == -1) {
-                    cctemp->shutdown();
-                    return NULL;
+        for (t = 0; t < cctemp->numfunctions; t++)
+        {
+            if (cctemp->add_new_export(cctemp->functions[t], EXPORT_FUNCTION,
+                cctemp->funccodeoffs[t], cctemp->funcnumparams[t]) == -1)
+            {
+                cctemp->shutdown();
+                return NULL;
             }
 
         }
--- Compiler/script/cs_parser_common.cpp
+++ Compiler/script/cs_parser_common.cpp
@@ -3,12 +3,12 @@
 
 int is_whitespace(char cht) {
     // space, tab, EOF, VT (dunno, Chrille had this char appearing)
-    if ((cht==' ') || (cht==9) || (cht == 26) || (cht == 11)) return 1;
+    if ((cht == ' ') || (cht == 9) || (cht == 26) || (cht == 11)) return 1;
     return 0;
 }
 
-void skip_whitespace(char**pttt) {
-    char*mpt=pttt[0];
+void skip_whitespace(char **pttt) {
+    char *mpt=pttt[0];
     while (is_whitespace(mpt[0])) mpt++;
     pttt[0]=mpt;
 }

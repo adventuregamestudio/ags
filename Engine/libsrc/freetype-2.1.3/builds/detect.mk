ce615c850f43eeb3e60e9460a26a5a7c683275ee
rofl0r <retnyg@gmx.net>
2019-03-25 00:00:02 +0000

    freetype: replace corrupted makefiles from lib-alfont ags port
    the only difference of the upstream files to the lib-alfont version was
    that 3 makefile snippets were cut off in the middle.
    the originals contain bogus \xff characters, however it seems make is able
    to handle them just like it would handle a space character.
    this must have caused the corruption in the lib-alfont version.
    i now replace the \xff characters with spaces in builds/detect.mk and
    builds/win32/detect.mk, which makes it possible to run configure successfully.
    the ags version, due to being cut off, fails immediately with a syntax error.
    
    the builds/mac/freetype.make file still contains the original \xff, as i don't
    know how to fix it. the file seems to use a different buildsystem i'm unfamiliar
    with. also, it's only for powerpc macs which are very rare these days, so this
    code is likely unused and will stay so.

--- Engine/libsrc/freetype-2.1.3/builds/detect.mk
+++ Engine/libsrc/freetype-2.1.3/builds/detect.mk
@@ -140,4 +140,16 @@ dos_setup:
 	@type builds\newline
 	@echo The following settings are used:
 	@type builds\newline
-	@echo   platform
+	@echo   platform                     $(PLATFORM)
+	@echo   compiler                     $(CC)
+	@echo   configuration directory      $(BUILD)
+	@echo   configuration rules          $(CONFIG_RULES)
+	@type builds\newline
+	@echo If this does not correspond to your system or settings please remove the file
+	@echo '$(CONFIG_MK)' from this directory then read the INSTALL file for help.
+	@type builds\newline
+	@echo Otherwise, simply type 'make' again to build the library.
+	@type builds\newline
+	@$(COPY) $(subst /,\,$(CONFIG_RULES) $(CONFIG_MK)) > nul
+
+# EOF

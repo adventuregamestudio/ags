6c1fd4a7145249b887d190d6914b4bbbebbdc532
Nick Sonneveld <nick@sonneveld.name>
2019-04-04 14:55:00 +1100

    endian fixup

--- Common/core/endianness.h
+++ Common/core/endianness.h
@@ -15,28 +15,31 @@
 #ifndef __AGS_CN_CORE__ENDIANNESS_H
 #define __AGS_CN_CORE__ENDIANNESS_H
 
-#ifndef AGS_BIG_ENDIAN
+#if !defined(AGS_BIG_ENDIAN) && !defined(AGS_LITTLE_ENDIAN)
 
-#if !defined (WINDOWS_VERSION)
+#if defined(WINDOWS_VERSION)
+#define AGS_LITTLE_ENDIAN
 
-#if defined (LINUX_VERSION)
+#elif defined(LINUX_VERSION)
 #include <endian.h>
-#endif
-
-// Detect endianess on Linux
-// The logic is inverted on purpose so that it assumes
-// little endian if the defines have not been set
 #if !(__BYTE_ORDER == __LITTLE_ENDIAN)
 #define AGS_BIG_ENDIAN
+#else
+#define AGS_LITTLE_ENDIAN
 #endif
 
-// Detect endianess on Mac
-#if defined (__BIG_ENDIAN__) && !defined (AGS_BIG_ENDIAN)
+#elif defined(MAC_VERSION)
+#include <machine/endian.h>
+#if !(BYTE_ORDER == LITTLE_ENDIAN)
 #define AGS_BIG_ENDIAN
+#else
+#define AGS_LITTLE_ENDIAN
 #endif
 
-#endif // !WINDOWS_VERSION
+#else
+#error Unsupported platform.
+#endif
 
-#endif // ifndef AGS_BIG_ENDIAN
+#endif // !AGS_BIG_ENDIAN && !AGS_LITTLE_ENDIAN
 
 #endif // __AGS_CN_CORE__ENDIANNESS_H

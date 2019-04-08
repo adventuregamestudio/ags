607d71fba828981b0b52c30bcce00757943f2907
Nick Sonneveld <nick@sonneveld.name>
2019-04-08 17:07:21 +1000

    cmake: fix some issues around allegro static linking requirements.

--- CMake/CheckLoadMidiPf.cmake
+++ CMake/CheckLoadMidiPf.cmake
@@ -1,13 +1,4 @@
 include(CheckCXXSourceCompiles)
-find_package(Allegro)
-
-if(LINUX)
-    # check_symbol_exists seems to have a problem with aliased aliases :)
-    set(CMAKE_REQUIRED_LIBRARIES PkgConfig::ALLEGRO)
-else()
-    set(CMAKE_REQUIRED_LIBRARIES Allegro::Allegro)
-endif()
-
+set(CMAKE_REQUIRED_LIBRARIES allegro)
 set(CMAKE_REQUIRED_DEFINITIONS -DALLEGRO_NO_MAGIC_MAIN)
-
 check_symbol_exists(load_midi_pf allegro.h HAVE_LOAD_MIDI_PF)
--- CMake/FindAllegro.cmake
+++ CMake/FindAllegro.cmake
@@ -3,11 +3,12 @@ if (TARGET Allegro::Allegro)
 endif()
 
 if (WIN32)
-    add_library(Allegro::Allegro STATIC IMPORTED)
-    set_property(TARGET Allegro::Allegro PROPERTY IMPORTED_LOCATION ${PROJECT_SOURCE_DIR}/Solutions/.lib/alleg-static.lib)
-    set_property(TARGET Allegro::Allegro PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${PROJECT_SOURCE_DIR}/Windows/include)
+    add_library(allegro STATIC IMPORTED GLOBAL)
+    set_property(TARGET allegro PROPERTY IMPORTED_LOCATION ${PROJECT_SOURCE_DIR}/Solutions/.lib/alleg-static.lib)
+    target_compile_definitions(allegro INTERFACE ALLEGRO_STATICLINK)
+    set_property(TARGET allegro PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${PROJECT_SOURCE_DIR}/Windows/include)
 
-    set_property(TARGET Allegro::Allegro PROPERTY INTERFACE_LINK_LIBRARIES 
+    set_property(TARGET allegro PROPERTY INTERFACE_LINK_LIBRARIES 
 		winmm
 		DirectX::DirectDraw
         DirectX::DirectInput
@@ -19,8 +20,22 @@ if (WIN32)
 endif()
 
 if (LINUX)
-    pkg_check_modules(ALLEGRO REQUIRED IMPORTED_TARGET GLOBAL allegro)
-    add_library(Allegro::Allegro ALIAS PkgConfig::ALLEGRO)
+    pkg_check_modules(ALLEGRO REQUIRED allegro)
+
+    add_library(allegro INTERFACE IMPORTED GLOBAL)
+
+    if(ALLEGRO_INCLUDE_DIRS)
+        set_property(TARGET allegro PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${ALLEGRO_INCLUDE_DIRS}")
+    endif()
+    if(ALLEGRO_LINK_LIBRARIES)
+        set_property(TARGET allegro PROPERTY INTERFACE_LINK_LIBRARIES "${ALLEGRO_LINK_LIBRARIES}")
+    endif()
+    if(ALLEGRO_LDFLAGS_OTHER)
+        set_property(TARGET allegro PROPERTY INTERFACE_LINK_OPTIONS "${ALLEGRO_LDFLAGS_OTHER}")
+    endif()
+    if(ALLEGRO_CFLAGS_OTHER)
+        set_property(TARGET allegro PROPERTY INTERFACE_COMPILE_OPTIONS "${ALLEGRO_CFLAGS_OTHER}")
+    endif()
 endif ()
 
 if (MACOS)
@@ -31,10 +46,11 @@ if (MACOS)
     find_library(FW_IO_KIT IOKit)
     find_library(FW_OPEN_GL OpenGL)
 
-    add_library(Allegro::Allegro STATIC IMPORTED)
-    set_property(TARGET Allegro::Allegro PROPERTY IMPORTED_LOCATION ${PROJECT_SOURCE_DIR}/OSX/lib/liballeg.a)
-    set_property(TARGET Allegro::Allegro PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${PROJECT_SOURCE_DIR}/OSX/include)
-    set_property(TARGET Allegro::Allegro PROPERTY INTERFACE_LINK_LIBRARIES 
+    add_library(allegro STATIC IMPORTED GLOBAL)
+    set_property(TARGET allegro PROPERTY IMPORTED_LOCATION ${PROJECT_SOURCE_DIR}/OSX/lib/liballeg.a)
+    target_compile_definitions(allegro INTERFACE ALLEGRO_STATICLINK)
+    set_property(TARGET allegro PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${PROJECT_SOURCE_DIR}/OSX/include)
+    set_property(TARGET allegro PROPERTY INTERFACE_LINK_LIBRARIES 
         ${FW_AUDIO_TOOLBOX}
         ${FW_CORE_AUDIO}
         ${FW_COCOA}
@@ -43,3 +59,13 @@ if (MACOS)
         ${FW_OPEN_GL}
         )
 endif()
+
+
+target_compile_definitions (allegro
+    INTERFACE 
+        ALLEGRO_NO_COMPATIBILITY 
+        ALLEGRO_NO_FIX_ALIASES 
+        ALLEGRO_NO_FIX_CLASS
+)
+
+add_library(Allegro::Allegro ALIAS allegro)

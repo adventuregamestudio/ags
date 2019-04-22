94c57ff0f0abc34d6390860d5ffa329beeeba9fd
Nick Sonneveld <nick@sonneveld.name>
2019-04-22 19:10:08 +1000

    pluginobjreader: forward declare instead of changing type to void

--- Engine/plugin/pluginobjectreader.h
+++ Engine/plugin/pluginobjectreader.h
@@ -18,17 +18,11 @@
 #ifndef __AGS_EE_PLUGIN__PLUGINOBJECTREADER_H
 #define __AGS_EE_PLUGIN__PLUGINOBJECTREADER_H
 
-#ifndef _AGS_PLUGIN_H
-#define IAGSManagedObjectReader void
-#endif
+class IAGSManagedObjectReader;
 
 struct PluginObjectReader {
     IAGSManagedObjectReader *reader;
     const char *type;
 };
-
-#ifndef _AGS_PLUGIN_H
-#undef IAGSManagedObjectReader
-#endif
 
 #endif // __AGS_EE_PLUGIN__PLUGINOBJECTREADER_H

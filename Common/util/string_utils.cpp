adventuregamestudio/ags

commit 32a0b368db8fcab7b32af9ef56f3cc1fe31bb3f1

Restructured string_utils unit in order to simplify ties between code

Common/util/string_utils.cpp
@@ -11,57 +11,13 @@
 // http://www.opensource.org/licenses/artistic-license-2.0.php
 //
 //=============================================================================
-
-#include <errno.h>
-#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include "core/platform.h"
-#include "gui/guidefines.h" // MAXLINE
 #include "util/math.h"
 #include "util/string_utils.h"
 #include "util/stream.h"
 
 using namespace AGS::Common;
-
-#define STD_BUFFER_SIZE 3000
-
-extern "C" char *ags_strlwr(char *s) {
-    for (auto p = s; *p; p++) {
-        *p = tolower(*p);
-    }
-   return s;
-}
-
-extern "C" char *ags_strupr(char *s) {
-    for (auto p = s; *p; p++) {
-        *p = toupper(*p);
-    }
-    return s;
-}
-
-extern "C" int ags_stricmp(const char *s1, const char *s2) {
-#if AGS_PLATFORM_OS_WINDOWS
-    return stricmp(s1, s2);
-#else
-    return strcasecmp(s1, s2);
-#endif
-}
-
-extern "C" int ags_strnicmp(const char *s1, const char *s2, size_t n) {
-#if AGS_PLATFORM_OS_WINDOWS
-    return strnicmp(s1, s2, n);
-#else
-    return strncasecmp(s1, s2, n);
-#endif
-}
-
-extern "C" char *ags_strdup(const char *s) {
-    char *result = (char *)malloc(strlen(s) + 1);
-    strcpy(result, s);
-    return result;
-}
-
 
 // Turn [ into \n and turn \[ into [
 void unescape(char *buffer) {
@@ -81,85 +37,6 @@ void unescape(char *buffer) {
         else
             memmove(offset - 1, offset, strlen(offset) + 1);
         offset++;
-    }
-}
-
-char lines[MAXLINE][200];
-int  numlines;
-
-// Project-dependent implementation
-extern int wgettextwidth_compensate(const char *tex, int font);
-
-// Break up the text into lines
-void split_lines(const char *todis, int wii, int fonnt) {
-    // v2.56.636: rewrote this function because the old version
-    // was crap and buggy
-    int i = 0;
-    int nextCharWas;
-    int splitAt;
-    char *theline;
-    // make a copy, since we change characters in the original string
-    // and this might be in a read-only bit of memory
-    char textCopyBuffer[STD_BUFFER_SIZE];
-    strcpy(textCopyBuffer, todis);
-    theline = textCopyBuffer;
-    unescape(theline);
-
-    while (1) {
-        splitAt = -1;
-
-        if (theline[i] == 0) {
-            // end of the text, add the last line if necessary
-            if (i > 0) {
-                strcpy(lines[numlines], theline);
-                numlines++;
-            }
-            break;
-        }
-
-        // temporarily terminate the line here and test its width
-        nextCharWas = theline[i + 1];
-        theline[i + 1] = 0;
-
-        // force end of line with the \n character
-        if (theline[i] == '\n')
-            splitAt = i;
-        // otherwise, see if we are too wide
-        else if (wgettextwidth_compensate(theline, fonnt) >= wii) {
-            int endline = i;
-            while ((theline[endline] != ' ') && (endline > 0))
-                endline--;
-
-            // single very wide word, display as much as possible
-            if (endline == 0)
-                endline = i - 1;
-
-            splitAt = endline;
-        }
-
-        // restore the character that was there before
-        theline[i + 1] = nextCharWas;
-
-        if (splitAt >= 0) {
-            // add this line
-            nextCharWas = theline[splitAt];
-            theline[splitAt] = 0;
-            strcpy(lines[numlines], theline);
-            numlines++;
-            theline[splitAt] = nextCharWas;
-            if (numlines >= MAXLINE) {
-                strcat(lines[numlines-1], "...");
-                break;
-            }
-            // the next line starts from here
-            theline += splitAt;
-            // skip the space or new line that caused the line break
-            if ((theline[0] == ' ') || (theline[0] == '\n'))
-                theline++;
-            i = -1;
-        }
-
-        i++;
     }
 }
 

Common/ac/wordsdictionary.cpp
@@ -16,7 +16,7 @@
 #include <string.h>
 #include "ac/wordsdictionary.h"
 #include "util/stream.h"
-#include "util/string_utils.h" // ags_stricmp
+#include "util/string_compat.h"
 
 using AGS::Common::Stream;
 

Common/font/fonts.cpp
@@ -20,6 +20,10 @@
 #include "font/ttffontrenderer.h"
 #include "font/wfnfontrenderer.h"
 #include "gfx/bitmap.h"
+#include "gui/guidefines.h" // MAXLINE
+#include "util/string_utils.h"
+
+#define STD_BUFFER_SIZE 3000
 
 using namespace AGS::Common;
 
@@ -182,6 +186,85 @@ bool use_default_linespacing(size_t fontNumber)
     if (fontNumber >= fonts.size())
         return false;
     return fonts[fontNumber].Info.LineSpacing == 0;
+}
+
+char lines[MAXLINE][200];
+int  numlines;
+
+// Project-dependent implementation
+extern int wgettextwidth_compensate(const char *tex, int font);
+
+// Break up the text into lines
+void split_lines(const char *todis, int wii, int fonnt) {
+    // v2.56.636: rewrote this function because the old version
+    // was crap and buggy
+    int i = 0;
+    int nextCharWas;
+    int splitAt;
+    char *theline;
+    // make a copy, since we change characters in the original string
+    // and this might be in a read-only bit of memory
+    char textCopyBuffer[STD_BUFFER_SIZE];
+    strcpy(textCopyBuffer, todis);
+    theline = textCopyBuffer;
+    unescape(theline);
+
+    while (1) {
+        splitAt = -1;
+
+        if (theline[i] == 0) {
+            // end of the text, add the last line if necessary
+            if (i > 0) {
+                strcpy(lines[numlines], theline);
+                numlines++;
+            }
+            break;
+        }
+
+        // temporarily terminate the line here and test its width
+        nextCharWas = theline[i + 1];
+        theline[i + 1] = 0;
+
+        // force end of line with the \n character
+        if (theline[i] == '\n')
+            splitAt = i;
+        // otherwise, see if we are too wide
+        else if (wgettextwidth_compensate(theline, fonnt) >= wii) {
+            int endline = i;
+            while ((theline[endline] != ' ') && (endline > 0))
+                endline--;
+
+            // single very wide word, display as much as possible
+            if (endline == 0)
+                endline = i - 1;
+
+            splitAt = endline;
+        }
+
+        // restore the character that was there before
+        theline[i + 1] = nextCharWas;
+
+        if (splitAt >= 0) {
+            // add this line
+            nextCharWas = theline[splitAt];
+            theline[splitAt] = 0;
+            strcpy(lines[numlines], theline);
+            numlines++;
+            theline[splitAt] = nextCharWas;
+            if (numlines >= MAXLINE) {
+                strcat(lines[numlines - 1], "...");
+                break;
+            }
+            // the next line starts from here
+            theline += splitAt;
+            // skip the space or new line that caused the line break
+            if ((theline[0] == ' ') || (theline[0] == '\n'))
+                theline++;
+            i = -1;
+        }
+
+        i++;
+    }
 }
 
 void wouttextxy(Common::Bitmap *ds, int xxx, int yyy, size_t fontNumber, color_t text_color, const char *texx)

Common/font/fonts.h
@@ -43,6 +43,7 @@ bool font_supports_extended_characters(size_t fontNumber);
 // Need to check whether it is safe to completely remove it.
 void ensure_text_valid_for_font(char *text, size_t fontnum);
 int get_font_scaling_mul(size_t fontNumber);
+// Calculate actual width of a line of text
 int wgettextwidth(const char *texx, size_t fontNumber);
 // Calculates actual height of a line of text
 int wgettextheight(const char *text, size_t fontNumber);
@@ -56,6 +57,9 @@ int  get_font_outline(size_t font_number);
 void set_font_outline(size_t font_number, int outline_type);
 // Outputs a single line of text on the defined position on bitmap, using defined font, color and parameters
 int getfontlinespacing(size_t fontNumber);
+// Break up the text into lines restricted by the given width
+void split_lines(const char *texx, int width, int fontNumber);
+// Print text on a surface using a given font
 void wouttextxy(Common::Bitmap *ds, int xxx, int yyy, size_t fontNumber, color_t text_color, const char *texx);
 // Assigns FontInfo to the font
 void set_fontinfo(size_t fontNumber, const FontInfo &finfo);

Common/game/main_game_file.cpp
@@ -26,6 +26,7 @@
 #include "script/cc_error.h"
 #include "util/alignedstream.h"
 #include "util/path.h"
+#include "util/string_compat.h"
 #include "util/string_utils.h"
 
 namespace AGS

Common/script/cc_script.cpp
@@ -18,7 +18,7 @@
 #include "script/cc_script.h"
 #include "script/script_common.h"
 #include "util/stream.h"
-#include "util/string_utils.h"
+#include "util/string_compat.h"
 
 using AGS::Common::Stream;
 

Common/util/string.cpp
@@ -11,15 +11,13 @@
 // http://www.opensource.org/licenses/artistic-license-2.0.php
 //
 //=============================================================================
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include "util/math.h"
 #include "util/stream.h"
 #include "util/string.h"
-#include "util/string_utils.h" // ags_stricmp
+#include "util/string_compat.h"
 
 namespace AGS
 {

Common/util/string_compat.c
@@ -1,0 +1,56 @@
+//=============================================================================
+//
+// Adventure Game Studio (AGS)
+//
+// Copyright (C) 1999-2011 Chris Jones and 2011-20xx others
+// The full list of copyright holders can be found in the Copyright.txt
+// file, which is part of this source code distribution.
+//
+// The AGS source code is provided under the Artistic License 2.0.
+// A copy of this license can be found in the file License.txt and at
+// http://www.opensource.org/licenses/artistic-license-2.0.php
+//
+//=============================================================================
+#include "util/string_compat.h"
+#include <ctype.h>
+#include <string.h>
+#include "core/platform.h"
+
+char *ags_strlwr(char *s)
+{
+    for (char *p = s; *p; p++)
+        *p = tolower(*p);
+    return s;
+}
+
+char *ags_strupr(char *s)
+{
+    for (char *p = s; *p; p++)
+        *p = toupper(*p);
+    return s;
+}
+
+int ags_stricmp(const char *s1, const char *s2)
+{
+#if AGS_PLATFORM_OS_WINDOWS
+    return stricmp(s1, s2);
+#else
+    return strcasecmp(s1, s2);
+#endif
+}
+
+int ags_strnicmp(const char *s1, const char *s2, size_t n)
+{
+#if AGS_PLATFORM_OS_WINDOWS
+    return strnicmp(s1, s2, n);
+#else
+    return strncasecmp(s1, s2, n);
+#endif
+}
+
+char *ags_strdup(const char *s)
+{
+    char *result = (char *)malloc(strlen(s) + 1);
+    strcpy(result, s);
+    return result;
+}

commit f4fa7bcec21194d786864b768665517d31f2a898

Fixed string_utils.cpp
was broken by 32a0b368db8fcab7b32af9ef56f3cc1fe31bb3f1

Common/util/string_utils.cpp
@@ -11,6 +11,7 @@
 // http://www.opensource.org/licenses/artistic-license-2.0.php
 //
 //=============================================================================
+#include <errno.h>
 #include <string.h>
 #include "core/platform.h"
 #include "util/math.h"
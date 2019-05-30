66d2cedc291fcb00726a58f4066ff4ea06969c0d
Nick Sonneveld <nick@sonneveld.name>
2019-05-30 11:46:40 +1000

    multifilelib: adjust style to be the same as rest of file?

--- Common/util/mutifilelib.cpp
+++ Common/util/mutifilelib.cpp
@@ -132,15 +132,18 @@ MFLUtil::MFLError MFLUtil::ReadSigsAndVersion(Stream *in, MFLVersion *p_lib_vers
         soff_t abs_offset_32 = in->ReadInt32();
 
         // test for header signature again, with 64-bit and 32-bit offsets if necessary
-        if (abs_offset > 0 && abs_offset < (tail_abs_offset - HeadSig.GetLength())) {
+        if (abs_offset > 0 && abs_offset < (tail_abs_offset - HeadSig.GetLength())) 
+        {
             in->Seek(abs_offset, kSeekBegin);
             sig.ReadCount(in, HeadSig.GetLength());
         }
 
         // try again with 32-bit offset
-        if (HeadSig.Compare(sig) != 0) {
+        if (HeadSig.Compare(sig) != 0) 
+        {
             abs_offset = abs_offset_32;
-            if (abs_offset > 0 && abs_offset < (tail_abs_offset - HeadSig.GetLength())) {
+            if (abs_offset > 0 && abs_offset < (tail_abs_offset - HeadSig.GetLength())) 
+            {
                 in->Seek(abs_offset, kSeekBegin);
                 sig.ReadCount(in, HeadSig.GetLength());
             }

516aa11da99c78facab2d3661570edc5889661e2
Per Olav Flaten <perolavflaten@live.no>
2020-01-27 00:20:03 +0100

    Added a few more tests that demoes the mocking framework more

---
+++ Editor/AGS.Editor.Tests/ColorThemes/ColorThemesTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using AGS.Editor.Preferences;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace AGS.Editor
+{
+    [TestFixture]
+    public class ColorThemesTests
+    {
+        private IAppSettings _settings;
+        private IColorThemes _themes;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _settings = Substitute.For<IAppSettings>();
+            _themes = new ColorThemes(_settings);
+        }
+
+        [Test]
+        public void SettingCurrentToNullWillThrowNullReferenceException()
+        {
+            Assert.Throws<NullReferenceException>(() => _themes.Current = null);
+        }
+
+        [Test]
+        public void SettingCurrentWillUpdateSettings()
+        {
+            _themes.Current = ColorThemeStub.DEFAULT;
+
+            Assert.That(_settings.ColorTheme, Is.EqualTo(ColorThemeStub.DEFAULT.Name));
+            _settings.Received().Save();
+        }
+    }
+}

6ba396caf8f5b31247e882d041ad32234f3373ec
ericoporto <ericoporto2008@gmail.com>
2020-02-14 02:21:52 +0000

    enables WS_EX_COMPOSITED This renders the control renders into an off-screen buffer, and only when rendering is complete is the result copied to the screen. This avoids flicker because only a completely-drawn control is put on the screen; you never see the control in a partially-drawn state.

--- Editor/AGS.Editor/Panes/BufferedPanel.cs
+++ Editor/AGS.Editor/Panes/BufferedPanel.cs
@@ -7,6 +7,19 @@ namespace AGS.Editor
 {
     public class BufferedPanel : Panel
     {
+        // Enabling Composite makes scrolling more smooth
+        private const int WS_EX_COMPOSITED = 0x02000000;
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= WS_EX_COMPOSITED;
+                return cp;
+            }
+        }
+
         public BufferedPanel()
         {
             this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);

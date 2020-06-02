0f835dac67c2b066995c95f59b30606b263249a0
Per Olav Flaten <perolavflaten@live.no>
2020-06-02 23:02:14 +0200

    Added global setup to tests
    Needed to make sure certain files that AGS relies on were available to run code properly

---
+++ Editor/AGS.Editor.Tests/SetUp.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace AGS
+{
+    [SetUpFixture]
+    public class SetUp
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            // Running directory is different from test assembly directory, and there are files there we need to
+            // execute certain code. We set running directory to assembly directory, if that doesn't work we may
+            // have to copy the files over to the original running directory
+            Directory.SetCurrentDirectory(TestContext.CurrentContext.TestDirectory);
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+        }
+    }
+}

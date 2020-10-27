2ec0db1b5c77f108fb3df8072691d3b610b6ceed
Per Olav Flaten <perolavflaten@live.no>
2020-10-27 23:15:41 +0100

    Added test coverage to Room

---
+++ Editor/AGS.Editor.Tests/Types/RoomTests.cs
@@ -0,0 +1,170 @@
+﻿using NUnit.Framework;
+
+namespace AGS.Types
+{
+    [TestFixture]
+    public class RoomTests
+    {
+        private Room _room;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _room = new Room(0);
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void GetsAndSetsModified(bool modified)
+        {
+            _room.Modified = modified;
+            Assert.That(_room.Modified, Is.EqualTo(modified));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(int.MaxValue)]
+        public void GetsAndSetsMaskResolution(int maskResolution)
+        {
+            _room.MaskResolution = maskResolution;
+            Assert.That(_room.MaskResolution, Is.EqualTo(maskResolution));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(int.MaxValue)]
+        public void GetsAndSetsBackgroundCount(int backgroundCount)
+        {
+            _room.BackgroundCount = backgroundCount;
+            Assert.That(_room.BackgroundCount, Is.EqualTo(backgroundCount));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(int.MaxValue)]
+        public void GetsAndSetsGameID(int gameID)
+        {
+            _room.GameID = gameID;
+            Assert.That(_room.GameID, Is.EqualTo(gameID));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(int.MaxValue)]
+        public void GetsAndSetsColorDepth(int colorDepth)
+        {
+            _room.ColorDepth = colorDepth;
+            Assert.That(_room.ColorDepth, Is.EqualTo(colorDepth));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(int.MaxValue)]
+        public void GetsAndSetsWidth(int width)
+        {
+            _room.Width = width;
+            Assert.That(_room.Width, Is.EqualTo(width));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(int.MaxValue)]
+        public void GetsAndSetsHeight(int height)
+        {
+            _room.Height = height;
+            Assert.That(_room.Height, Is.EqualTo(height));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(int.MaxValue)]
+        public void GetsAndSetsBackgroundAnimationDelay(int backgroundAnimationDelay)
+        {
+            _room.BackgroundAnimationDelay = backgroundAnimationDelay;
+            Assert.That(_room.BackgroundAnimationDelay, Is.EqualTo(backgroundAnimationDelay));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(int.MaxValue)]
+        public void GetsAndSetsPlayMusicOnRoomLoad(int playMusicOnRoomLoad)
+        {
+            _room.PlayMusicOnRoomLoad = playMusicOnRoomLoad;
+            Assert.That(_room.PlayMusicOnRoomLoad, Is.EqualTo(playMusicOnRoomLoad));
+        }
+
+        [TestCase(false)]
+        [TestCase(true)]
+        public void GetsAndSetsSaveLoadEnabled(bool saveLoadEnabled)
+        {
+            _room.SaveLoadEnabled = saveLoadEnabled;
+            Assert.That(_room.SaveLoadEnabled, Is.EqualTo(saveLoadEnabled));
+        }
+
+        [TestCase(false)]
+        [TestCase(true)]
+        public void GetsAndSetsShowPlayerCharacter(bool showPlayerCharacter)
+        {
+            _room.ShowPlayerCharacter = showPlayerCharacter;
+            Assert.That(_room.ShowPlayerCharacter, Is.EqualTo(showPlayerCharacter));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(int.MaxValue)]
+        public void GetsAndSetsPlayerCharacterView(int playerCharacterView)
+        {
+            _room.PlayerCharacterView = playerCharacterView;
+            Assert.That(_room.PlayerCharacterView, Is.EqualTo(playerCharacterView));
+        }
+
+        [TestCase(RoomVolumeAdjustment.Loud)]
+        [TestCase(RoomVolumeAdjustment.Louder)]
+        [TestCase(RoomVolumeAdjustment.Loudest)]
+        [TestCase(RoomVolumeAdjustment.Normal)]
+        [TestCase(RoomVolumeAdjustment.Quiet)]
+        [TestCase(RoomVolumeAdjustment.Quieter)]
+        [TestCase(RoomVolumeAdjustment.Quietest)]
+        public void GetsAndSetsPlayerCharacterView(RoomVolumeAdjustment musicVolumeAdjustment)
+        {
+            _room.MusicVolumeAdjustment = musicVolumeAdjustment;
+            Assert.That(_room.MusicVolumeAdjustment, Is.EqualTo(musicVolumeAdjustment));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(int.MaxValue)]
+        public void GetsAndSetsLeftEdgeX(int leftEdgeX)
+        {
+            _room.LeftEdgeX = leftEdgeX;
+            Assert.That(_room.LeftEdgeX, Is.EqualTo(leftEdgeX));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(int.MaxValue)]
+        public void GetsAndSetsRightEdgeX(int rightEdgeX)
+        {
+            _room.RightEdgeX = rightEdgeX;
+            Assert.That(_room.RightEdgeX, Is.EqualTo(rightEdgeX));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(int.MaxValue)]
+        public void GetsAndSetsTopEdgeY(int topEdgeY)
+        {
+            _room.TopEdgeY = topEdgeY;
+            Assert.That(_room.TopEdgeY, Is.EqualTo(topEdgeY));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(int.MaxValue)]
+        public void GetsAndSetsBottomEdgeY(int bottomEdgeY)
+        {
+            _room.BottomEdgeY = bottomEdgeY;
+            Assert.That(_room.BottomEdgeY, Is.EqualTo(bottomEdgeY));
+        }
+    }
+}

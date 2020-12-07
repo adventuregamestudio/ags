aefdab3b8d2edd0bb42e88be95c61f0e4c215ab9
Per Olav Flaten <perolavflaten@live.no>
2020-12-07 03:35:14 +0100

    Fixed full serialization and de-serialization of RoomMessage with tests

--- Editor/AGS.Editor.Tests/Types/RoomMessageTests.cs
+++ Editor/AGS.Editor.Tests/Types/RoomMessageTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Xml;
 
 namespace AGS.Types
 {
@@ -79,6 +80,48 @@ namespace AGS.Types
         {
             _roomMessage.AutoRemoveAfterTime = autoRemoveAfterTime;
             Assert.That(_roomMessage.AutoRemoveAfterTime, Is.EqualTo(autoRemoveAfterTime));
+        }
+
+        [TestCase(4, "Test room message.", false, 1, true, false)]
+        [TestCase(10, "Another test room message.", true, 5, false, true)]
+        public void DeserializesFromXml(int id, string text, bool showAsSpeech, int characterId, bool displayNextMessageAfter, bool autoRemoveAfterTime)
+        {
+            string xml = $@"
+            <RoomMessage>
+                <Text xml:space=""preserve"">{text}</Text>
+                <ShowAsSpeech>{showAsSpeech}</ShowAsSpeech>
+                <CharacterID>{characterId}</CharacterID>
+                <DisplayNextMessageAfter>{displayNextMessageAfter}</DisplayNextMessageAfter>
+                <AutoRemoveAfterTime>{autoRemoveAfterTime}</AutoRemoveAfterTime>
+            </RoomMessage>";
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+            _roomMessage = new RoomMessage(id, doc.SelectSingleNode("RoomMessage"));
+
+            Assert.That(_roomMessage.ID, Is.EqualTo(id));
+            Assert.That(_roomMessage.Text, Is.EqualTo(text));
+            Assert.That(_roomMessage.ShowAsSpeech, Is.EqualTo(showAsSpeech));
+            Assert.That(_roomMessage.CharacterID, Is.EqualTo(characterId));
+            Assert.That(_roomMessage.DisplayNextMessageAfter, Is.EqualTo(displayNextMessageAfter));
+            Assert.That(_roomMessage.AutoRemoveAfterTime, Is.EqualTo(autoRemoveAfterTime));
+        }
+
+        [TestCase("Test room message.", false, 1, true, false)]
+        [TestCase("Another test room message.", true, 5, false, true)]
+        public void SerializesToXml(string text, bool showAsSpeech, int characterId, bool displayNextMessageAfter, bool autoRemoveAfterTime)
+        {
+            _roomMessage.Text = text;
+            _roomMessage.ShowAsSpeech = showAsSpeech;
+            _roomMessage.CharacterID = characterId;
+            _roomMessage.DisplayNextMessageAfter = displayNextMessageAfter;
+            _roomMessage.AutoRemoveAfterTime = autoRemoveAfterTime;
+            XmlDocument doc = _roomMessage.ToXmlDocument();
+
+            Assert.That(doc.SelectSingleNode("/RoomMessage/Text").InnerText, Is.EqualTo(text));
+            Assert.That(doc.SelectSingleNode("/RoomMessage/ShowAsSpeech").InnerText, Is.EqualTo(showAsSpeech.ToString()));
+            Assert.That(doc.SelectSingleNode("/RoomMessage/CharacterID").InnerText, Is.EqualTo(characterId.ToString()));
+            Assert.That(doc.SelectSingleNode("/RoomMessage/DisplayNextMessageAfter").InnerText, Is.EqualTo(displayNextMessageAfter.ToString()));
+            Assert.That(doc.SelectSingleNode("/RoomMessage/AutoRemoveAfterTime").InnerText, Is.EqualTo(autoRemoveAfterTime.ToString()));
         }
     }
 }

adventuregamestudio/ags

commit 5f870fe57a4cb1fde55127e3ed160c22b17b99d8

Engine: rewrote custom PACKFILE without use of normal member
This complements a1329085d94e3723e4cbb960a8b1818c1d9a00dc

I made a mistake by keep using normal.todo member of PACKFILE, it appears you cannot do this if you create one with a custom vtable. Also, custom pack functions were not checking for remaining bytes in the asset segment.
This commit fixes these issues by adding more fields to PACKFILE's userdata and making custom pack functions rely on these.

Engine/media/audio/sound.cpp
@@ -64,8 +64,8 @@ MYWAVE *thiswave;
 SOUNDCLIP *my_load_wave(const AssetPath &asset_name, int voll, int loop)
 {
     // Load via soundcache.
-    long dummy;
-    SAMPLE *new_sample = (SAMPLE*)get_cached_sound(asset_name, true, &dummy);
+    size_t dummy;
+    SAMPLE *new_sample = (SAMPLE*)get_cached_sound(asset_name, true, dummy);
 
     if (new_sample == nullptr)
         return nullptr;
@@ -85,7 +85,8 @@ PACKFILE *mp3in;
 MYMP3 *thistune;
 SOUNDCLIP *my_load_mp3(const AssetPath &asset_name, int voll)
 {
-    mp3in = PackfileFromAsset(asset_name);
+    size_t asset_size;
+    mp3in = PackfileFromAsset(asset_name, asset_size);
     if (mp3in == nullptr)
         return nullptr;
 
@@ -97,11 +98,11 @@ SOUNDCLIP *my_load_mp3(const AssetPath &asset_name, int voll)
     thistune = new MYMP3();
     thistune->in = mp3in;
     thistune->chunksize = MP3CHUNKSIZE;
-    thistune->filesize = mp3in->normal.todo;
+    thistune->filesize = asset_size;
     thistune->vol = voll;
 
-    if (thistune->chunksize > mp3in->normal.todo)
-        thistune->chunksize = mp3in->normal.todo;
+    if (thistune->chunksize > thistune->filesize)
+        thistune->chunksize = thistune->filesize;
 
     pack_fread(tmpbuffer, thistune->chunksize, mp3in);
 
@@ -109,7 +110,7 @@ SOUNDCLIP *my_load_mp3(const AssetPath &asset_name, int voll)
 
     {
         AGS::Engine::MutexLock _lockMp3(_mp3_mutex);
-        thistune->stream = almp3_create_mp3stream(tmpbuffer, thistune->chunksize, (mp3in->normal.todo < 1));
+        thistune->stream = almp3_create_mp3stream(tmpbuffer, thistune->chunksize, (thistune->filesize < 1));
     }
 
     if (thistune->stream == nullptr) {
@@ -128,8 +129,8 @@ MYSTATICMP3 *thismp3;
 SOUNDCLIP *my_load_static_mp3(const AssetPath &asset_name, int voll, bool loop)
 {
     // Load via soundcache.
-    long muslen = 0;
-    char* mp3buffer = get_cached_sound(asset_name, false, &muslen);
+    size_t muslen = 0;
+    char* mp3buffer = get_cached_sound(asset_name, false, muslen);
     if (mp3buffer == nullptr)
         return nullptr;
 
@@ -179,8 +180,8 @@ MYSTATICOGG *thissogg;
 SOUNDCLIP *my_load_static_ogg(const AssetPath &asset_name, int voll, bool loop)
 {
     // Load via soundcache.
-    long muslen = 0;
-    char* mp3buffer = get_cached_sound(asset_name, false, &muslen);
+    size_t muslen = 0;
+    char* mp3buffer = get_cached_sound(asset_name, false, muslen);
     if (mp3buffer == nullptr)
         return nullptr;
 
@@ -205,7 +206,8 @@ SOUNDCLIP *my_load_static_ogg(const AssetPath &asset_name, int voll, bool loop)
 MYOGG *thisogg;
 SOUNDCLIP *my_load_ogg(const AssetPath &asset_name, int voll)
 {
-    mp3in = PackfileFromAsset(asset_name);
+    size_t asset_size;
+    mp3in = PackfileFromAsset(asset_name, asset_size);
     if (mp3in == nullptr)
         return nullptr;
 
@@ -223,13 +225,13 @@ SOUNDCLIP *my_load_ogg(const AssetPath &asset_name, int voll)
     thisogg->last_ms_offs = 0;
     thisogg->last_but_one_but_one = 0;
 
-    if (thisogg->chunksize > mp3in->normal.todo)
-        thisogg->chunksize = mp3in->normal.todo;
+    if (thisogg->chunksize > asset_size)
+        thisogg->chunksize = asset_size;
 
     pack_fread(tmpbuffer, thisogg->chunksize, mp3in);
 
     thisogg->buffer = (char *)tmpbuffer;
-    thisogg->stream = alogg_create_oggstream(tmpbuffer, thisogg->chunksize, (mp3in->normal.todo < 1));
+    thisogg->stream = alogg_create_oggstream(tmpbuffer, thisogg->chunksize, (asset_size < 1));
 
     if (thisogg->stream == nullptr) {
         free(tmpbuffer);
@@ -250,7 +252,8 @@ SOUNDCLIP *my_load_midi(const AssetPath &asset_name, int repet)
     if (!thismidi && psp_midi_preload_patches)
         load_midi_patches();
 
-    PACKFILE *pf = PackfileFromAsset(asset_name);
+    size_t asset_size;
+    PACKFILE *pf = PackfileFromAsset(asset_name, asset_size);
     if (!pf)
         return nullptr;
 
@@ -299,7 +302,8 @@ void remove_mod_player() {
 MYMOD *thismod = nullptr;
 SOUNDCLIP *my_load_mod(const AssetPath &asset_name, int repet)
 {
-    DUMBFILE *df = DUMBfileFromAsset(asset_name);
+    size_t asset_size;
+    DUMBFILE *df = DUMBfileFromAsset(asset_name, asset_size);
     if (!df)
         return nullptr;
 

Engine/ac/asset_helper.h
@@ -17,7 +17,7 @@
 //=============================================================================
 #ifndef __AGS_EE_AC__ASSETHELPER_H
 #define __AGS_EE_AC__ASSETHELPER_H
-
+#include <memory>
 #include <utility>
 #include "util/string.h"
 
@@ -47,12 +47,21 @@ AssetPath get_audio_clip_assetpath(int bundling_type, const String &filename);
 // Returns the path to the voice-over asset
 AssetPath get_voice_over_assetpath(const String &filename);
 
+// Custom AGS PACKFILE user object
+// TODO: it is preferrable to let our Stream define custom readable window instead,
+// keeping this as simple as possible for now (we may require a stream classes overhaul).
+struct AGS_PACKFILE_OBJ
+{
+    std::unique_ptr<Stream> stream;
+    size_t asset_size = 0u;
+    size_t remains = 0u;
+};
 // Creates PACKFILE stream from AGS asset.
 // This function is supposed to be used only when you have to create Allegro
 // object, passing PACKFILE stream to constructor.
-PACKFILE *PackfileFromAsset(const AssetPath &path);
+PACKFILE *PackfileFromAsset(const AssetPath &path, size_t &asset_size);
 // Creates DUMBFILE stream from AGS asset. Used for creating DUMB objects
-DUMBFILE *DUMBfileFromAsset(const AssetPath &path);
+DUMBFILE *DUMBfileFromAsset(const AssetPath &path, size_t &asset_size);
 bool DoesAssetExistInLib(const AssetPath &assetname);
 
 #endif // __AGS_EE_AC__ASSETHELPER_H

Engine/ac/file.cpp
@@ -401,13 +401,19 @@ Stream *LocateAsset(const AssetPath &path, size_t &asset_size)
 //
 static int ags_pf_fclose(void *userdata)
 {
-    delete (Stream*)userdata;
+    delete (AGS_PACKFILE_OBJ*)userdata;
     return 0;
 }
 
 static int ags_pf_getc(void *userdata)
 {
-    return ((Stream*)userdata)->ReadByte();
+    AGS_PACKFILE_OBJ* obj = (AGS_PACKFILE_OBJ*)userdata;
+    if (obj->remains > 0)
+    {
+        obj->remains--;
+        return obj->stream->ReadByte();
+    }
+    return -1;
 }
 
 static int ags_pf_ungetc(int c, void *userdata)
@@ -417,7 +423,14 @@ static int ags_pf_ungetc(int c, void *userdata)
 
 static long ags_pf_fread(void *p, long n, void *userdata)
 {
-    return ((Stream*)userdata)->Read(p, n);
+    AGS_PACKFILE_OBJ* obj = (AGS_PACKFILE_OBJ*)userdata;
+    if (obj->remains > 0)
+    {
+        size_t read = Math::Min(obj->remains, (size_t)n);
+        obj->remains -= read;
+        return obj->stream->Read(p, read);
+    }
+    return -1;
 }
 
 static int ags_pf_putc(int c, void *userdata)
@@ -432,20 +445,20 @@ static long ags_pf_fwrite(AL_CONST void *p, long n, void *userdata)
 
 static int ags_pf_fseek(void *userdata, int offset)
 {
-    return ((Stream*)userdata)->Seek(offset) ? 0 : -1;
+    return -1; // don't support seek
 }
 
 static int ags_pf_feof(void *userdata)
 {
-    return ((Stream*)userdata)->EOS() ? 1 : 0;
+    return ((AGS_PACKFILE_OBJ*)userdata)->remains == 0;
 }
 
 static int ags_pf_ferror(void *userdata)
 {
-    return ((Stream*)userdata)->HasErrors() ? 1 : 0;
+    return ((AGS_PACKFILE_OBJ*)userdata)->stream->HasErrors() ? 1 : 0;
 }
 
-// Custom AGS PACKFILE callback table
+// Custom PACKFILE callback table
 static PACKFILE_VTABLE ags_packfile_vtable = {
     ags_pf_fclose,
     ags_pf_getc,
@@ -459,23 +472,23 @@ static PACKFILE_VTABLE ags_packfile_vtable = {
 };
 //
 
-PACKFILE *PackfileFromAsset(const AssetPath &path)
+PACKFILE *PackfileFromAsset(const AssetPath &path, size_t &asset_size)
 {
-    size_t asset_size = 0;
     Stream *asset_stream = LocateAsset(path, asset_size);
     if (asset_stream && asset_size > 0)
     {
-        PACKFILE *pf = pack_fopen_vtable(&ags_packfile_vtable, asset_stream);
-        if (pf) // We still must set normal.todo, because some of the AGS code relies on it
-            pf->normal.todo = asset_size;
-        return pf;
+        AGS_PACKFILE_OBJ* obj = new AGS_PACKFILE_OBJ;
+        obj->stream.reset(asset_stream);
+        obj->asset_size = asset_size;
+        obj->remains = asset_size;
+        return pack_fopen_vtable(&ags_packfile_vtable, obj);
     }
     return nullptr;
 }
 
-DUMBFILE *DUMBfileFromAsset(const AssetPath &path)
+DUMBFILE *DUMBfileFromAsset(const AssetPath &path, size_t &asset_size)
 {
-    PACKFILE *pf = PackfileFromAsset(path);
+    PACKFILE *pf = PackfileFromAsset(path, asset_size);
     if (pf)
         return dumbfile_open_packfile(pf);
     return nullptr;

Engine/media/audio/clip_mymp3.cpp
@@ -19,6 +19,7 @@
 #include "media/audio/clip_mymp3.h"
 #include "media/audio/audiointernaldefs.h"
 #include "ac/common.h"               // quit()
+#include "ac/asset_helper.h"
 #include "util/mutex_lock.h"
 
 #include "platform/base/agsplatformdriver.h"
@@ -36,9 +37,10 @@ void MYMP3::poll()
     }
 
     if (tempbuf != nullptr) {
+        AGS_PACKFILE_OBJ* obj = (AGS_PACKFILE_OBJ*)in->userdata;
         int free_val = -1;
-        if (chunksize >= in->normal.todo) {
-            chunksize = in->normal.todo;
+        if (chunksize >= obj->remains) {
+            chunksize = obj->remains;
             free_val = chunksize;
         }
         pack_fread(tempbuf, chunksize, in);

Engine/media/audio/clip_mymp3.h
@@ -24,7 +24,7 @@ struct MYMP3:public SOUNDCLIP
 {
     ALMP3_MP3STREAM *stream;
     PACKFILE *in;
-    long  filesize;
+    size_t filesize;
     char *buffer;
     int chunksize;
 

Engine/media/audio/clip_myogg.cpp
@@ -16,6 +16,7 @@
 #include "media/audio/clip_myogg.h"
 #include "media/audio/audiointernaldefs.h"
 #include "ac/common.h"               // quit()
+#include "ac/asset_helper.h"
 #include "util/mutex_lock.h"
 
 #include "platform/base/agsplatformdriver.h"
@@ -31,16 +32,17 @@ void MYOGG::poll()
 {
     if (state_ != SoundClipPlaying) { return; }
 
-    if (in->normal.todo > 0)
+    AGS_PACKFILE_OBJ* obj = (AGS_PACKFILE_OBJ*)in->userdata;
+    if (obj->remains > 0)
     {
         // update the buffer
         char *tempbuf = (char *)alogg_get_oggstream_buffer(stream);
         if (tempbuf != nullptr)
         {
             int free_val = -1;
-            if (chunksize >= in->normal.todo)
+            if (chunksize >= obj->remains)
             {
-                chunksize = in->normal.todo;
+                chunksize = obj->remains;
                 free_val = chunksize;
             }
             pack_fread(tempbuf, chunksize, in);

Engine/media/audio/soundcache.cpp
@@ -94,7 +94,7 @@ void sound_cache_free(char* buffer, bool is_wave)
 }
 
 
-char* get_cached_sound(const AssetPath &asset_name, bool is_wave, long* size)
+char* get_cached_sound(const AssetPath &asset_name, bool is_wave, size_t &size)
 {
 	AGS::Engine::MutexLock _lock(_sound_cache_mutex);
 
@@ -102,7 +102,7 @@ char* get_cached_sound(const AssetPath &asset_name, bool is_wave, long* size)
     Debug::Printf("get_cached_sound(%s %d)\n", asset_name.first.GetCStr(), (unsigned int)is_wave);
 #endif
 
-    *size = 0;
+    size = 0;
 
     int i;
     for (i = 0; i < psp_audio_cachesize; i++)
@@ -117,7 +117,7 @@ char* get_cached_sound(const AssetPath &asset_name, bool is_wave, long* size)
 #endif
             sound_cache_entries[i].reference++;
             sound_cache_entries[i].last_used = sound_cache_counter++;
-            *size = sound_cache_entries[i].size;
+            size = sound_cache_entries[i].size;
 
             return sound_cache_entries[i].data;
         }
@@ -129,7 +129,7 @@ char* get_cached_sound(const AssetPath &asset_name, bool is_wave, long* size)
 
     if (is_wave)
     {
-        PACKFILE *wavin = PackfileFromAsset(asset_name);
+        PACKFILE *wavin = PackfileFromAsset(asset_name, size);
         if (wavin != nullptr)
         {
             wave = load_wav_pf(wavin);
@@ -138,7 +138,7 @@ char* get_cached_sound(const AssetPath &asset_name, bool is_wave, long* size)
     }  
     else
     {
-        mp3in = PackfileFromAsset(asset_name);
+        mp3in = PackfileFromAsset(asset_name, size);
         if (mp3in == nullptr)
         {
             return nullptr;
@@ -178,13 +178,12 @@ char* get_cached_sound(const AssetPath &asset_name, bool is_wave, long* size)
 
     if (is_wave)
     {
-        *size = 0;
+        size = 0; // ??? CHECKME
         newdata = (char*)wave;
     }
     else
     {
-        *size = mp3in->normal.todo;
-        newdata = (char *)malloc(*size);
+        newdata = (char *)malloc(size);
 
         if (newdata == nullptr)
         {
@@ -192,7 +191,7 @@ char* get_cached_sound(const AssetPath &asset_name, bool is_wave, long* size)
             return nullptr;
         }
 
-        pack_fread(newdata, *size, mp3in);
+        pack_fread(newdata, size, mp3in);
         pack_fclose(mp3in);
     }
 
@@ -218,7 +217,7 @@ char* get_cached_sound(const AssetPath &asset_name, bool is_wave, long* size)
                 free(sound_cache_entries[i].data);
 	}
 	
-        sound_cache_entries[i].size = *size;
+        sound_cache_entries[i].size = size;
         sound_cache_entries[i].data = newdata;
 
         if (sound_cache_entries[i].file_name)

Engine/media/audio/soundcache.h
@@ -42,7 +42,7 @@ extern int psp_midi_preload_patches;
 
 void clear_sound_cache();
 void sound_cache_free(char* buffer, bool is_wave);
-char* get_cached_sound(const AssetPath &asset_name, bool is_wave, long* size);
+char* get_cached_sound(const AssetPath &asset_name, bool is_wave, size_t &size);
 
 
 #endif // __AC_SOUNDCACHE_H

commit ca75033bdcef373e4d75f92e15e255b68238b256

Engine: fixed short OGG sounds don't stop properly
Regression caused by 5f870fe57a4cb1fde55127e3ed160c22b17b99d8

Mistake was caused by taking remaining length value from initial variable instead of dynamically updated struct member.

Engine/media/audio/sound.cpp
@@ -90,6 +90,7 @@ SOUNDCLIP *my_load_mp3(const AssetPath &asset_name, int voll)
     if (mp3in == nullptr)
         return nullptr;
 
+    AGS_PACKFILE_OBJ* obj = (AGS_PACKFILE_OBJ*)mp3in->userdata;
     char *tmpbuffer = (char *)malloc(MP3CHUNKSIZE);
     if (tmpbuffer == nullptr) {
         pack_fclose(mp3in);
@@ -98,11 +99,11 @@ SOUNDCLIP *my_load_mp3(const AssetPath &asset_name, int voll)
     thistune = new MYMP3();
     thistune->in = mp3in;
     thistune->chunksize = MP3CHUNKSIZE;
-    thistune->filesize = asset_size;
+    thistune->filesize = obj->remains;
     thistune->vol = voll;
 
-    if (thistune->chunksize > thistune->filesize)
-        thistune->chunksize = thistune->filesize;
+    if (thistune->chunksize > obj->remains)
+        thistune->chunksize = obj->remains;
 
     pack_fread(tmpbuffer, thistune->chunksize, mp3in);
 
@@ -110,7 +111,7 @@ SOUNDCLIP *my_load_mp3(const AssetPath &asset_name, int voll)
 
     {
         AGS::Engine::MutexLock _lockMp3(_mp3_mutex);
-        thistune->stream = almp3_create_mp3stream(tmpbuffer, thistune->chunksize, (thistune->filesize < 1));
+        thistune->stream = almp3_create_mp3stream(tmpbuffer, thistune->chunksize, (obj->remains < 1));
     }
 
     if (thistune->stream == nullptr) {
@@ -211,6 +212,7 @@ SOUNDCLIP *my_load_ogg(const AssetPath &asset_name, int voll)
     if (mp3in == nullptr)
         return nullptr;
 
+    AGS_PACKFILE_OBJ* obj = (AGS_PACKFILE_OBJ*)mp3in->userdata;
     char *tmpbuffer = (char *)malloc(MP3CHUNKSIZE);
     if (tmpbuffer == nullptr) {
         pack_fclose(mp3in);
@@ -225,13 +227,13 @@ SOUNDCLIP *my_load_ogg(const AssetPath &asset_name, int voll)
     thisogg->last_ms_offs = 0;
     thisogg->last_but_one_but_one = 0;
 
-    if (thisogg->chunksize > asset_size)
-        thisogg->chunksize = asset_size;
+    if (thisogg->chunksize > obj->remains)
+        thisogg->chunksize = obj->remains;
 
     pack_fread(tmpbuffer, thisogg->chunksize, mp3in);
 
     thisogg->buffer = (char *)tmpbuffer;
-    thisogg->stream = alogg_create_oggstream(tmpbuffer, thisogg->chunksize, (asset_size < 1));
+    thisogg->stream = alogg_create_oggstream(tmpbuffer, thisogg->chunksize, (obj->remains < 1));
 
     if (thisogg->stream == nullptr) {
         free(tmpbuffer);
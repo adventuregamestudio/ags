ee4bcfb4379975fac784dd79a4f8dbbd5d411644
Ivan Mogilko <ikm_spb@yahoo.com>
2021-02-03 03:42:26 +0300

    Editor: added HandleKeyRelease to EditorContentPanel

--- Editor/AGS.Editor/GUI/TabbedDocumentManager.cs
+++ Editor/AGS.Editor/GUI/TabbedDocumentManager.cs
@@ -362,6 +362,10 @@ namespace AGS.Editor
                 }
                 _flipThroughPanesIndex = 0;
             }
+            else if (_currentPane != null)
+            {
+                return _currentPane.Control.KeyReleased(key);
+            }
             return false;
         }
 

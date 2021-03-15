adventuregamestudio/ags

commit 2eef9fd0181756d67a73f2012c0d00cd7caaf77d

Script API: Speech.PortraitOverlay for accessing speech portrait overlay

Engine/ac/overlay.cpp
@@ -234,21 +234,24 @@ static void dispose_overlay(ScreenOverlay &over)
 void remove_screen_overlay_index(size_t over_idx)
 {
     ScreenOverlay &over = screenover[over_idx];
+    // TODO: move these custom settings outside of this function
     if (over.type == play.complete_overlay_on)
     {
         play.complete_overlay_on = 0;
     }
-    else if (over.type == play.text_overlay_on)
+    else if (over.type == play.text_overlay_on || over.type == OVER_PICTURE)
     {
         play.text_overlay_on = 0;
         if (play.speech_text_scover)
             invalidate_and_subref(over, play.speech_text_scover);
+        if (play.speech_face_scover)
+            invalidate_and_subref(over, play.speech_face_scover);
     }
     dispose_overlay(over);
     screenover.erase(screenover.begin() + over_idx);
-    // if an overlay before the sierra-style speech one is removed, update the index
-    if (face_talking >= 0 && (size_t)face_talking > over_idx)
-        face_talking--;
+    // TODO: this is bad, need more generic system to store overlay references
+    if (face_talking >= 0)
+        face_talking = find_overlay_of_type(OVER_PICTURE);
 }
 
 void remove_screen_overlay(int type)
@@ -298,6 +301,7 @@ size_t add_screen_overlay(int x, int y, int type, Common::Bitmap *piccy, int pic
     over.hasAlphaChannel = alphaChannel;
     over.positionRelativeToScreen = true;
     over.blendMode = blendMode;
+    // TODO: move these custom settings outside of this function
     if (type == OVER_COMPLETE) play.complete_overlay_on = type;
     else if (type == OVER_TEXTMSG || type == OVER_TEXTSPEECH)
     {
@@ -306,6 +310,10 @@ size_t add_screen_overlay(int x, int y, int type, Common::Bitmap *piccy, int pic
         // and therefore cannot be accessed, so no practical reason for that atm
         if (type == OVER_TEXTSPEECH)
             play.speech_text_scover = create_scriptobj_addref(over);
+    }
+    else if (type == OVER_PICTURE)
+    {
+        play.speech_face_scover = create_scriptobj_addref(over);
     }
     screenover.push_back(over);
     return screenover.size() - 1;

Editor/AGS.Editor/Resources/agsdefns.sh
@@ -2292,6 +2292,8 @@ builtin struct Speech {
 #ifdef SCRIPT_API_v399
   /// Gets the overlay representing displayed blocking text, or null if no such text none is displayed at the moment.
   import static readonly attribute Overlay* TextOverlay;
+  /// Gets the overlay representing displayed portrait, or null if it is not displayed at the moment.
+  import static readonly attribute Overlay* PortraitOverlay;
 #endif
 };
 

Engine/ac/gamestate.cpp
@@ -41,6 +41,7 @@ extern ScriptSystem scsystem;
 GameState::GameState()
 {
     speech_text_scover = nullptr;
+    speech_face_scover = nullptr;
     _isAutoRoomViewport = true;
     _mainViewportHasChanged = false;
 }

Engine/ac/gamestate.h
@@ -241,6 +241,8 @@ struct GameState {
     int  text_overlay_on;
     // Blocking speech overlay managed object, for accessing in scripts
     ScriptOverlay *speech_text_scover;
+    // Speech portrait overlay managed object
+    ScriptOverlay *speech_face_scover;
 
     int shake_screen_yoff; // y offset of the shaking screen
 

Engine/ac/speech.cpp
@@ -101,6 +101,11 @@ ScriptOverlay* Speech_GetTextOverlay()
     return play.speech_text_scover;
 }
 
+ScriptOverlay* Speech_GetPortraitOverlay()
+{
+    return play.speech_face_scover;
+}
+
 RuntimeScriptValue Sc_Speech_GetAnimationStopTimeMargin(const RuntimeScriptValue *params, int32_t param_count)
 {
     API_VARGET_INT(play.close_mouth_speech_time);
@@ -229,6 +234,11 @@ RuntimeScriptValue Sc_Speech_GetTextOverlay(const RuntimeScriptValue *params, in
     API_SCALL_OBJAUTO(ScriptOverlay, Speech_GetTextOverlay);
 }
 
+RuntimeScriptValue Sc_Speech_GetPortraitOverlay(const RuntimeScriptValue *params, int32_t param_count)
+{
+    API_SCALL_OBJAUTO(ScriptOverlay, Speech_GetPortraitOverlay);
+}
+
 extern RuntimeScriptValue Sc_SetVoiceMode(const RuntimeScriptValue *params, int32_t param_count);
 
 void RegisterSpeechAPI(ScriptAPIVersion base_api, ScriptAPIVersion compat_api)
@@ -241,6 +251,7 @@ void RegisterSpeechAPI(ScriptAPIVersion base_api, ScriptAPIVersion compat_api)
     ccAddExternalStaticFunction("Speech::set_DisplayPostTimeMs",      Sc_Speech_SetDisplayPostTimeMs);
 	ccAddExternalStaticFunction("Speech::get_GlobalSpeechAnimationDelay", Sc_Speech_GetGlobalSpeechAnimationDelay);
 	ccAddExternalStaticFunction("Speech::set_GlobalSpeechAnimationDelay", Sc_Speech_SetGlobalSpeechAnimationDelay);
+    ccAddExternalStaticFunction("Speech::get_PortraitOverlay",        Sc_Speech_GetPortraitOverlay);
     ccAddExternalStaticFunction("Speech::get_PortraitXOffset",        Sc_Speech_GetPortraitXOffset);
     ccAddExternalStaticFunction("Speech::set_PortraitXOffset",        Sc_Speech_SetPortraitXOffset);
     ccAddExternalStaticFunction("Speech::get_PortraitY",              Sc_Speech_GetPortraitY);

commit d79f8b5e581cb7d31dd27842c7cdb1b3bc159b10

Engine: fixed incorrect portrait overlay disposal
Was broken in 2eef9fd0181756d67a73f2012c0d00cd7caaf77d

Engine/ac/overlay.cpp
@@ -239,19 +239,24 @@ void remove_screen_overlay_index(size_t over_idx)
     {
         play.complete_overlay_on = 0;
     }
-    else if (over.type == play.text_overlay_on || over.type == OVER_PICTURE)
+    else if (over.type == play.text_overlay_on)
     {
-        play.text_overlay_on = 0;
         if (play.speech_text_scover)
             invalidate_and_subref(over, play.speech_text_scover);
+        play.text_overlay_on = 0;
+    }
+    else if (over.type == OVER_PICTURE)
+    {
         if (play.speech_face_scover)
             invalidate_and_subref(over, play.speech_face_scover);
+        face_talking = -1;
     }
     dispose_overlay(over);
     screenover.erase(screenover.begin() + over_idx);
+    // if an overlay before the sierra-style speech one is removed, update the index
     // TODO: this is bad, need more generic system to store overlay references
-    if (face_talking >= 0)
-        face_talking = find_overlay_of_type(OVER_PICTURE);
+    if (face_talking >= 0 && (size_t)face_talking > over_idx)
+        face_talking--;
 }
 
 void remove_screen_overlay(int type)
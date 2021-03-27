adventuregamestudio/ags

commit 6341cbae35b381e7016fcdab1ffdc4a25f437378

Engine: tidied coordinate conversion on startup a little

Engine/main/engine_setup.cpp
@@ -45,80 +45,74 @@ extern IGraphicsDriver *gfxDriver;
 
 int convert_16bit_bgr = 0;
 
-int ff; // whatever!
-
-int adjust_pixel_size_for_loaded_data(int size, int filever)
+// Convert guis position and size to proper game screen coordinates.
+// Necessary for pre 3.1.0 games only.
+void convert_gui_to_screen_coordinates(GameDataVersion filever)
 {
-    if (filever < kGameVersion_310)
+    if (filever > kGameVersion_310)
+        return;
+
+    for (int i = 0; i < game.numcursors; ++i)
     {
-        return multiply_up_coordinate(size);
-    }
-    return size;
-}
-
-void adjust_pixel_sizes_for_loaded_data(int *x, int *y, int filever)
-{
-    x[0] = adjust_pixel_size_for_loaded_data(x[0], filever);
-    y[0] = adjust_pixel_size_for_loaded_data(y[0], filever);
-}
-
-void adjust_sizes_for_resolution(int filever)
-{
-    int ee;
-    for (ee = 0; ee < game.numcursors; ee++) 
-    {
-        game.mcurs[ee].hotx = adjust_pixel_size_for_loaded_data(game.mcurs[ee].hotx, filever);
-        game.mcurs[ee].hoty = adjust_pixel_size_for_loaded_data(game.mcurs[ee].hoty, filever);
+        game.mcurs[i].hotx *= current_screen_resolution_multiplier;
+        game.mcurs[i].hoty *= current_screen_resolution_multiplier;
     }
 
-    for (ee = 0; ee < game.numinvitems; ee++) 
+    for (int i = 0; i < game.numinvitems; ++i)
     {
-        adjust_pixel_sizes_for_loaded_data(&game.invinfo[ee].hotx, &game.invinfo[ee].hoty, filever);
+        game.invinfo[i].hotx *= current_screen_resolution_multiplier;
+        game.invinfo[i].hoty *= current_screen_resolution_multiplier;
     }
 
-    for (ee = 0; ee < game.numgui; ee++) 
+    for (int i = 0; i < game.numgui; ++i)
     {
-        GUIMain*cgp=&guis[ee];
-        adjust_pixel_sizes_for_loaded_data(&cgp->X, &cgp->Y, filever);
+        GUIMain*cgp = &guis[i];
+        cgp->X *= current_screen_resolution_multiplier;
+        cgp->Y *= current_screen_resolution_multiplier;
         if (cgp->Width < 1)
             cgp->Width = 1;
         if (cgp->Height < 1)
             cgp->Height = 1;
-        // Temp fix for older games
         if (cgp->Width == play.GetNativeSize().Width - 1)
             cgp->Width = play.GetNativeSize().Width;
 
-        adjust_pixel_sizes_for_loaded_data(&cgp->Width, &cgp->Height, filever);
+        cgp->Width *= current_screen_resolution_multiplier;
+        cgp->Height *= current_screen_resolution_multiplier;
 
-        cgp->PopupAtMouseY = adjust_pixel_size_for_loaded_data(cgp->PopupAtMouseY, filever);
+        cgp->PopupAtMouseY *= current_screen_resolution_multiplier;
 
-        for (ff = 0; ff < cgp->GetControlCount(); ff++)
+        for (int j = 0; j < cgp->GetControlCount(); ++j)
         {
-            GUIObject *guio = cgp->GetControl(ff);
-            adjust_pixel_sizes_for_loaded_data(&guio->X, &guio->Y, filever);
-            adjust_pixel_sizes_for_loaded_data(&guio->Width, &guio->Height, filever);
+            GUIObject *guio = cgp->GetControl(j);
+            guio->X *= current_screen_resolution_multiplier;
+            guio->Y *= current_screen_resolution_multiplier;
+            guio->Width *= current_screen_resolution_multiplier;
+            guio->Height *= current_screen_resolution_multiplier;
             guio->IsActivated = false;
         }
     }
+}
 
-    if ((filever >= 37) && (game.options[OPT_NATIVECOORDINATES] == 0) &&
-        game.IsHiRes())
+// Convert objects position and size to proper room coordinates.
+// Necessary for 3.1.0 and above games with legacy "low-res coordinates" setting.
+void convert_objects_to_room_coordinates(GameDataVersion filever)
+{
+    if (! (filever >= kGameVersion_310 && (game.options[OPT_NATIVECOORDINATES] == 0) && game.IsHiRes()) )
+        return;
+
+    for (int i = 0; i < game.numcharacters; ++i) 
     {
-        // New 3.1 format game file, but with Use Native Coordinates off
-
-        for (ee = 0; ee < game.numcharacters; ee++) 
-        {
-            game.chars[ee].x /= 2;
-            game.chars[ee].y /= 2;
-        }
-
-        for (ee = 0; ee < numguiinv; ee++)
-        {
-            guiinv[ee].ItemWidth /= 2;
-            guiinv[ee].ItemHeight /= 2;
-        }
+        game.chars[i].x /= current_screen_resolution_multiplier;
+        game.chars[i].y /= current_screen_resolution_multiplier;
     }
 
+    // TODO: frankly doing this to inventory window props makes little sense,
+    // find out if this is correct and why; fix or add comment here.
+    for (int i = 0; i < numguiinv; ++i)
+    {
+        guiinv[i].ItemWidth /= current_screen_resolution_multiplier;
+        guiinv[i].ItemHeight /= current_screen_resolution_multiplier;
+    }
 }
 
 void engine_setup_system_gamesize()
@@ -163,7 +157,9 @@ void engine_init_resolution_settings(const Size game_size)
     Debug::Printf(kDbgMsg_Init, "Game native resolution: %d x %d (%d bit)%s", game_size.Width, game_size.Height, game.color_depth * 8,
         game.IsLegacyLetterbox() ? " letterbox-by-design" : "");
 
-    adjust_sizes_for_resolution(loaded_game_file_version);
+    convert_gui_to_screen_coordinates(loaded_game_file_version);
+    convert_objects_to_room_coordinates(loaded_game_file_version);
+
     engine_setup_system_gamesize();
 }
 

Engine/ac/draw.h
@@ -123,8 +123,6 @@ void setpal();
 // This conversion is done *before* scaling game's frame further in the window
 // (which is a separate task done by graphics renderer and its filters).
 extern AGS_INLINE int get_fixed_pixel_size(int pixels);
-extern AGS_INLINE int convert_to_low_res(int coord);
-extern AGS_INLINE int convert_back_to_high_res(int coord);
 // coordinate conversion game,script ---> screen
 extern AGS_INLINE int multiply_up_coordinate(int coord);
 extern AGS_INLINE void multiply_up_coordinates(int *x, int *y);
@@ -133,6 +131,17 @@ extern AGS_INLINE void multiply_up_coordinates_round_up(int *x, int *y);
 extern AGS_INLINE int divide_down_coordinate(int coord);
 extern AGS_INLINE void divide_down_coordinates(int &x, int &y);
 extern AGS_INLINE int divide_down_coordinate_round_up(int coord);
+// These functions are separate, they help to convert from room coordinates
+// to the region mask coordinates. They were necessary because in hi-res
+// games region masks are 1:2 of the room background size.
+// This is why their conversion condition is also opposite. Above functions
+// apply conversion when OPT_NATIVECOORDINATES == 0, but functions below
+// apply conversion when OPT_NATIVECOORDINATES != 0.
+// TODO: they really should depend only on room mask setting, not on global game setting.
+// coordinate conversion room ---> mask
+extern AGS_INLINE int convert_to_low_res(int coord);
+// coordinate conversion mask ---> room
+extern AGS_INLINE int convert_back_to_high_res(int coord);
 
 // Checks if the bitmap needs to be converted and **deletes original** if a new bitmap
 // had to be created (by default).

Engine/ac/room.cpp
@@ -367,7 +367,10 @@ void unload_old_room() {
 }
 
 
-// TODO: merge this into UpdateRoomData?
+// The coordinates are divided by 2, because in the past there were only two
+// room resolution "types": low-res and hi-res, and the difference between
+// them was equal to the difference between 320x200 and 640x400 (i.e. x2).
+// TODO: merge this into UpdateRoomData? or this is required for engine only?
 void convert_room_coordinates_to_low_res(RoomStruct *rstruc)
 {
     for (size_t i = 0; i < rstruc->ObjectCount; ++i)

Engine/ac/room.h
@@ -41,7 +41,6 @@ RuntimeScriptValue Sc_Room_GetProperty(const RuntimeScriptValue *params, int32_t
 
 void  save_room_data_segment ();
 void  unload_old_room();
-void  convert_room_coordinates_to_low_res(AGS::Common::RoomStruct *room);
 void  load_new_room(int newnum,CharacterInfo*forchar);
 void  new_room(int newnum,CharacterInfo*forchar);
 int   find_highest_room_entered();

Engine/main/engine.cpp
@@ -1354,6 +1354,9 @@ bool engine_do_config(const String &exe_path)
     return engine_check_run_setup(exe_path, cfg);
 }
 
+// TODO: this function is still a big mess, engine/system-related initialization
+// is mixed with game-related data adjustments. Divide it in parts, move game
+// data init into either InitGameState() or other game method as appropriate.
 int initialize_engine(int argc,char*argv[])
 {
     if (engine_pre_init_callback) {

Engine/main/engine_setup.h
@@ -17,12 +17,8 @@
 #include "util/geometry.h"
 #include "gfx/gfxdefines.h"
 
-struct ColorDepthOption;
-
-// Get color depth settings depending on game settings and user config
-ColorDepthOption engine_get_color_depth();
-// Sets up game viewport and object scaling parameters depending on game and
-// user config; fills in color depth options.
+// Sets up game viewport and object scaling parameters depending on game.
+// TODO: this is part of the game init, not engine init, move it later
 void engine_init_resolution_settings(const Size game_size);
 // Setup engine after the graphics mode has changed
 void engine_post_gfxmode_setup(const Size &init_desktop);

commit 50f35df4e8b4983591bd0e452356c44906aff24b

Engine: fixed convert_gui_to_game_resolution() format comparison
Mistake introduced in 6341cbae35b381e7016fcdab1ffdc4a25f437378

Engine/main/engine_setup.cpp
@@ -50,7 +50,7 @@ int convert_16bit_bgr = 0;
 // Necessary for pre 3.1.0 games only to sync with modern engine.
 void convert_gui_to_game_resolution(GameDataVersion filever)
 {
-    if (filever > kGameVersion_310)
+    if (filever >= kGameVersion_310)
         return;
 
     const int mul = game.GetDataUpscaleMult();
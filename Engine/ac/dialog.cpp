adventuregamestudio/ags

commit 15489ff2c97762f7c40a8374d5b50241830bcd74

timing: reimplement timing changes similar to c4907f5d but without the counter

Engine/ac/dialog.cpp
@@ -1044,7 +1044,14 @@ bool DialogOptions::Run()
             return true; // continue running loop
         }
       }
-      PollUntilNextFrame();
+
+      update_polled_stuff_if_runtime();
+
+      if (play.fast_forward == 0)
+      {
+          WaitForNextFrame();
+      }
+
       return true; // continue running loop
 }
 

Engine/ac/timer.h
@@ -28,6 +28,8 @@ using AGS_Clock = std::conditional<
         std::chrono::high_resolution_clock, std::chrono::steady_clock
       >::type;
 
+extern void WaitForNextFrame();
+
 extern void setTimerFps(int new_fps);
 extern bool waitingForNextTick();  // store last tick time.
 extern void skipMissedTicks();  // if more than N frames, just skip all, start a fresh.

Engine/ac/display.cpp
@@ -290,7 +290,14 @@ int _display_main(int xx,int yy,int wii,const char*text,int blocking,int usingfo
                 if (skip_setting & SKIP_KEYPRESS)
                     break;
             }
-            PollUntilNextFrame();
+            
+            update_polled_stuff_if_runtime();
+
+            if (play.fast_forward == 0)
+            {
+                WaitForNextFrame();
+            }
+
             countdown--;
 
             if (play.speech_has_voice) {

Engine/ac/event.cpp
@@ -282,12 +282,12 @@ void process_event(EventHappened*evp) {
                     boxhit = Math::Clamp(boxhit, 0, viewport.GetHeight());
                     int lxp = viewport.GetWidth() / 2 - boxwid / 2;
                     int lyp = viewport.GetHeight() / 2 - boxhit / 2;
+                    gfxDriver->Vsync();
                     temp_scr->Blit(saved_backbuf, lxp, lyp, lxp, lyp, 
                         boxwid, boxhit);
                     render_to_screen(viewport.Left, viewport.Top);
-                    do {
-                        update_polled_stuff_if_runtime();
-                    } while (waitingForNextTick());
+                    update_polled_mp3();
+                    WaitForNextFrame();
                 }
                 gfxDriver->SetMemoryBackBuffer(saved_backbuf, viewport.Left, viewport.Top);
             }
@@ -315,9 +315,8 @@ void process_event(EventHappened*evp) {
                     gfxDriver->DrawSprite(0, 0, ddb);
                 }
                 render_to_screen();
-                do {
-                    update_polled_stuff_if_runtime();
-                } while (waitingForNextTick());
+                update_polled_stuff_if_runtime();
+                WaitForNextFrame();
                 transparency -= 16;
             }
             saved_viewport_bitmap->Release();
@@ -353,9 +352,8 @@ void process_event(EventHappened*evp) {
                 draw_screen_callback();
                 gfxDriver->DrawSprite(0, 0, ddb);
                 render_to_screen();
-                do {
-                    update_polled_stuff_if_runtime();
-                } while (waitingForNextTick());
+                update_polled_stuff_if_runtime();
+                WaitForNextFrame();
             }
             saved_viewport_bitmap->Release();
 

Engine/ac/invwindow.cpp
@@ -474,7 +474,10 @@ bool InventoryScreen::Run()
             //ags_domouse(DOMOUSE_ENABLE);
         }
         wasonitem=isonitem;
-        PollUntilNextFrame();
+
+        update_polled_stuff_if_runtime();
+
+        WaitForNextFrame();
 
     return true; // continue inventory screen loop
 }

Engine/ac/timer.cpp
@@ -27,19 +27,57 @@ namespace {
 
 const auto MAXIMUM_FALL_BEHIND = 3;
 
-auto last_tick_time = AGS_Clock::now();
 auto tick_duration = std::chrono::microseconds(1000000LL/40);
 auto framerate_maxed = false;
 
+auto last_tick_time = AGS_Clock::now();
+auto next_frame_timestamp = AGS_Clock::now();
+
 }
 
-void setTimerFps(int new_fps) {
+std::chrono::microseconds GetFrameDuration()
+{
+    if (framerate_maxed) {
+        return std::chrono::microseconds(0);
+    }
+    return tick_duration;
+}
+
+void setTimerFps(int new_fps) 
+{
     tick_duration = std::chrono::microseconds(1000000LL/new_fps);
-    last_tick_time = AGS_Clock::now();
     framerate_maxed = new_fps >= 1000;
+
+    last_tick_time = AGS_Clock::now();
+    next_frame_timestamp = AGS_Clock::now();
 }
 
-bool waitingForNextTick() {
+void WaitForNextFrame()
+{
+    auto now = AGS_Clock::now();
+    auto frameDuration = GetFrameDuration();
+
+    // early exit if we're trying to maximise framerate
+    if (frameDuration <= std::chrono::milliseconds::zero()) {
+        next_frame_timestamp = now;
+        return;
+    }
+
+    // jump ahead if we're lagging
+    if (next_frame_timestamp < (now - MAXIMUM_FALL_BEHIND*frameDuration)) {
+        next_frame_timestamp = now;
+    }
+
+    auto frame_time_remaining = next_frame_timestamp - now;
+    if (frame_time_remaining > std::chrono::milliseconds::zero()) {
+        std::this_thread::sleep_for(frame_time_remaining);
+    }
+    
+    next_frame_timestamp += frameDuration;
+}
+
+bool waitingForNextTick() 
+{
     auto now = AGS_Clock::now();
 
     if (framerate_maxed) {
@@ -67,11 +105,11 @@ bool waitingForNextTick() {
         return false;
     }
 
-    std::this_thread::sleep_for(next_tick_time - now);
-
     return true;
 }
 
-void skipMissedTicks() {
+void skipMissedTicks() 
+{
     last_tick_time = AGS_Clock::now();
+    next_frame_timestamp = AGS_Clock::now();
 }

Engine/gfx/ali3dogl.cpp
@@ -1918,11 +1918,9 @@ void OGLGraphicsDriver::do_fade(bool fadingOut, int speed, int targetColourRed, 
     d3db->SetTransparency(fadingOut ? a : (255 - a));
     this->_render(flipTypeLastTime, false);
 
-    do {
-      if (_pollingCallback)
-        _pollingCallback();
-    } while (waitingForNextTick());
-
+    if (_pollingCallback)
+      _pollingCallback();
+    WaitForNextFrame();
   }
 
   if (fadingOut)

Engine/gfx/ali3dsw.cpp
@@ -622,12 +622,9 @@ void ALSoftwareGraphicsDriver::highcolor_fade_in(Bitmap *vs, int offx, int offy,
        bmp_buff->TransBlendBlt(bmp_orig, 0, 0);
        this->Vsync();
        _filter->RenderScreen(bmp_buff, 0, 0);
-       do
-       {
-         if (_pollingCallback)
-           _pollingCallback();
-       }
-       while (waitingForNextTick());
+       if (_pollingCallback)
+         _pollingCallback();
+       WaitForNextFrame();
    }
    delete bmp_buff;
    if (bmp_orig != vs)
@@ -651,12 +648,9 @@ void ALSoftwareGraphicsDriver::highcolor_fade_out(Bitmap *vs, int offx, int offy
         bmp_buff->TransBlendBlt(bmp_orig, 0, 0);
         this->Vsync();
         _filter->RenderScreen(bmp_buff, 0, 0);
-        do
-        {
-          if (_pollingCallback)
-            _pollingCallback();
-          platform->Delay(1);
-        } while (waitingForNextTick());
+        if (_pollingCallback)
+          _pollingCallback();
+        WaitForNextFrame();
     }
     delete bmp_buff;
 

commit bac258cac2ba17d84e5c623072de3100a72436b8

Engine: fixed custom dialog options dropped fps twice
This is because game was making TWO time breaks between frames instead of one.
Regression introduced by 15489ff2c97762f7c40a8374d5b50241830bcd74

Engine/ac/dialog.cpp
@@ -1049,8 +1049,8 @@ bool DialogOptions::Run()
 
       update_polled_stuff_if_runtime();
 
-      if (play.fast_forward == 0)
-      {
+      if (!runGameLoopsInBackground && (play.fast_forward == 0))
+      { // note if runGameLoopsInBackground then it's called inside UpdateGameOnce
           WaitForNextFrame();
       }
 

Engine/ac/timer.h
@@ -28,6 +28,7 @@ using AGS_Clock = std::conditional<
         std::chrono::high_resolution_clock, std::chrono::steady_clock
       >::type;
 
+// Sleeps for time remaining until the next game frame, updates next frame timestamp
 extern void WaitForNextFrame();
 
 // Sets real FPS to the given number of frames per second; pass 1000+ for maxed FPS mode
adventuregamestudio/ags

commit 32cd95759d1b649f449cac9fdc1c75b51f96dadb

Engine: factored out FillSaveList() function to avoid excess code

Engine/gui/guidialog.cpp
@@ -11,7 +11,7 @@
 // http://www.opensource.org/licenses/artistic-license-2.0.php
 //
 //=============================================================================
-
+#include <algorithm>
 #include <cstdio>
 #include "gui/guidialog.h"
 
@@ -20,6 +20,7 @@
 #include "ac/game.h"
 #include "ac/gamesetup.h"
 #include "ac/gamesetupstruct.h"
+#include "ac/global_game.h"
 #include "gui/cscidialog.h"
 #include <cctype> //isdigit()
 #include "gfx/bitmap.h"
@@ -298,67 +299,22 @@ int savegamedialog()
 
 void preparesavegamelist(int ctrllist)
 {
-  numsaves = 0;
-  toomanygames = 0;
-  al_ffblk ffb;
-  int bufix = 0;
+  // TODO: find out if limiting to MAXSAVEGAMES is still necessary here
+  std::vector<SaveListItem> saves;
+  FillSaveList(saves, MAXSAVEGAMES);
+  std::sort(saves.rbegin(), saves.rend());
 
-  String svg_dir = get_save_game_directory();
-  String svg_suff = get_save_game_suffix();
-  String searchPath = Path::ConcatPaths(svg_dir, String::FromFormat("agssave.*%s", svg_suff.GetCStr()));
-
-  int don = al_findfirst(searchPath, &ffb, -1);
-  while (!don) {
-    bufix = 0;
-    if (numsaves >= MAXSAVEGAMES) {
-      toomanygames = 1;
-      break;
-    }
-
-    // only list games .000 to .099 (to allow higher slots for other purposes)
-    if (strstr(ffb.name, ".0") == nullptr) {
-      don = al_findnext(&ffb);
-      continue;
-    }
-
-    const char *numberExtension = strstr(ffb.name, ".0") + 1;
-    int sgNumber = atoi(numberExtension);
-
-    String thisGamePath = get_save_game_path(sgNumber);
-
-    // get description
-    String description;
-    read_savedgame_description(thisGamePath, description);
-
-    CSCISendControlMessage(ctrllist, CLB_ADDITEM, 0, (long)description.GetCStr());
-    // Select the first item
-    CSCISendControlMessage(ctrllist, CLB_SETCURSEL, 0, 0);
-    filenumbers[numsaves] = sgNumber;
-    filedates[numsaves] = (long int)ffb.time;
-    numsaves++;
-    don = al_findnext(&ffb);
+  // fill in the list box and global savegameindex[] array for backward compatibilty
+  numsaves = (int)saves.size();
+  toomanygames = (numsaves >= MAXSAVEGAMES) ? 1 : 0;
+  for (const auto &item : saves)
+  {
+      CSCISendControlMessage(ctrllist, CLB_ADDITEM, 0, (long)item.Description.GetCStr());
+      filenumbers[numsaves] = item.Slot;
+      filedates[numsaves] = (long int)item.FileTime;
   }
-
-  al_findclose(&ffb);
-  if (numsaves >= MAXSAVEGAMES)
-    toomanygames = 1;
-
-  for (int nn = 0; nn < numsaves - 1; nn++) {
-    for (int kk = 0; kk < numsaves - 1; kk++) { // Date order the games
-      if (filedates[kk] < filedates[kk + 1]) {  // swap them round
-        CSCISendControlMessage(ctrllist, CLB_GETTEXT, kk, (long)&buff[0]);
-        CSCISendControlMessage(ctrllist, CLB_GETTEXT, kk + 1, (long)&buffer2[0]);
-        CSCISendControlMessage(ctrllist, CLB_SETTEXT, kk + 1, (long)&buff[0]);
-        CSCISendControlMessage(ctrllist, CLB_SETTEXT, kk, (long)&buffer2[0]);
-        int numtem = filenumbers[kk];
-        filenumbers[kk] = filenumbers[kk + 1];
-        filenumbers[kk + 1] = numtem;
-        long numted = filedates[kk];
-        filedates[kk] = filedates[kk + 1];
-        filedates[kk + 1] = numted;
-      }
-    }
-  }
+  // Select the first item
+  CSCISendControlMessage(ctrllist, CLB_SETCURSEL, 0, 0);
 }
 
 void enterstringwindow(const char *prompttext, char *stouse)

Engine/ac/global_game.cpp
@@ -153,16 +153,19 @@ int IsGamePaused() {
     return 0;
 }
 
+bool GetSaveSlotDescription(int slnum, String &description) {
+    if (read_savedgame_description(get_save_game_path(slnum), description))
+        return true;
+    description.Format("INVALID SLOT %d", slnum);
+    return false;
+}
+
 int GetSaveSlotDescription(int slnum, char *desbuf) {
     VALIDATE_STRING(desbuf);
     String description;
-    if (read_savedgame_description(get_save_game_path(slnum), description))
-    {
-        snprintf(desbuf, MAX_MAXSTRLEN, "%s", description.GetCStr());
-        return 1;
-    }
-    snprintf(desbuf, MAX_MAXSTRLEN, "INVALID SLOT %d", slnum);
-    return 0;
+    bool res = GetSaveSlotDescription(slnum, description);
+    snprintf(desbuf, MAX_MAXSTRLEN, "%s", description.GetCStr());
+    return res ? 1 : 0;
 }
 
 int LoadSaveSlotScreenshot(int slnum, int width, int height) {
@@ -191,6 +194,33 @@ int LoadSaveSlotScreenshot(int slnum, int width, int height) {
     add_dynamic_sprite(gotSlot, newPic);
 
     return gotSlot;
+}
+
+void FillSaveList(std::vector<SaveListItem> &saves, size_t max_count)
+{
+    String svg_dir = get_save_game_directory();
+    String svg_suff = get_save_game_suffix();
+    String searchPath = Path::ConcatPaths(svg_dir, String::FromFormat("agssave.*%s", svg_suff.GetCStr()));
+
+    al_ffblk ffb;
+    int don = al_findfirst(searchPath, &ffb, FA_SEARCH);
+    while (!don)
+    {
+        if (saves.size() >= max_count)
+            break;
+        // only list games .000 to .099 (to allow higher slots for other perposes)
+        if (strstr(ffb.name, ".0") == nullptr) {
+            don = al_findnext(&ffb);
+            continue;
+        }
+        const char *numberExtension = strstr(ffb.name, ".0") + 1;
+        int saveGameSlot = atoi(numberExtension);
+        String description;
+        GetSaveSlotDescription(saveGameSlot, description);
+        saves.push_back(SaveListItem(saveGameSlot, description, ffb.time));
+        don = al_findnext(&ffb);
+    }
+    al_findclose(&ffb);
 }
 
 void SetGlobalInt(int index,int valu) {

Engine/ac/global_game.h
@@ -21,12 +21,29 @@
 #include "util/string.h"
 using namespace AGS; // FIXME later
 
+struct SaveListItem
+{
+    int    Slot;
+    Common::String Description;
+    time_t FileTime = 0;
+
+    SaveListItem(int slot, const Common::String &desc, time_t ft)
+        : Slot(slot), Description(desc), FileTime(ft) {}
+
+    inline bool operator < (const SaveListItem &other) const
+    {
+        return FileTime < other.FileTime;
+    }
+};
+
+
 void GiveScore(int amnt);
 void restart_game();
 void RestoreGameSlot(int slnum);
 void DeleteSaveSlot (int slnum);
 int  GetSaveSlotDescription(int slnum,char*desbuf);
 int  LoadSaveSlotScreenshot(int slnum, int width, int height);
+void FillSaveList(std::vector<SaveListItem> &saves, size_t max_count = -1);
 void PauseGame();
 void UnPauseGame();
 int  IsGamePaused();

Engine/ac/listbox.cpp
@@ -11,7 +11,7 @@
 // http://www.opensource.org/licenses/artistic-license-2.0.php
 //
 //=============================================================================
-
+#include <algorithm>
 #include <set>
 #include "ac/listbox.h"
 #include "ac/common.h"
@@ -91,64 +91,29 @@ int ListBox_GetSaveGameSlots(GUIListBox *listbox, int index) {
 }
 
 int ListBox_FillSaveGameList(GUIListBox *listbox) {
+  // TODO: find out if limiting to MAXSAVEGAMES is still necessary here
+  std::vector<SaveListItem> saves;
+  FillSaveList(saves, MAXSAVEGAMES);
+  std::sort(saves.rbegin(), saves.rend());
+
+  // fill in the list box
   listbox->Clear();
-
-  int numsaves=0;
-  int bufix=0;
-  al_ffblk ffb; 
-  long filedates[MAXSAVEGAMES];
-  char buff[200];
-
-  String svg_dir = get_save_game_directory();
-  String searchPath = Path::ConcatPaths(svg_dir, "agssave.*");
-
-  int don = al_findfirst(searchPath, &ffb, FA_SEARCH);
-  while (!don) {
-    bufix=0;
-    if (numsaves >= MAXSAVEGAMES)
-      break;
-    // only list games .000 to .099 (to allow higher slots for other perposes)
-    if (strstr(ffb.name,".0")==nullptr) {
-      don = al_findnext(&ffb);
-      continue;
-    }
-    const char *numberExtension = strstr(ffb.name, ".0") + 1;
-    int saveGameSlot = atoi(numberExtension);
-    GetSaveSlotDescription(saveGameSlot, buff);
-    listbox->AddItem(buff);
-    listbox->SavedGameIndex[numsaves] = saveGameSlot;
-    filedates[numsaves]=(long int)ffb.time;
-    numsaves++;
-    don = al_findnext(&ffb);
-  }
-  al_findclose(&ffb);
-
-  int nn;
-  for (nn=0;nn<numsaves-1;nn++) {
-    for (int kk=0;kk<numsaves-1;kk++) {  // Date order the games
-
-      if (filedates[kk] < filedates[kk+1]) {   // swap them round
-        String tempptr = listbox->Items[kk];
-        listbox->Items[kk] = listbox->Items[kk+1];
-        listbox->Items[kk+1] = tempptr;
-        int numtem = listbox->SavedGameIndex[kk];
-        listbox->SavedGameIndex[kk] = listbox->SavedGameIndex[kk+1];
-        listbox->SavedGameIndex[kk+1] = numtem;
-        long numted=filedates[kk]; filedates[kk]=filedates[kk+1];
-        filedates[kk+1]=numted;
-      }
-    }
+  for (const auto &item : saves)
+  {
+    listbox->AddItem(item.Description);
+    listbox->SavedGameIndex[listbox->ItemCount - 1] = item.Slot;
   }
 
   // update the global savegameindex[] array for backward compatibilty
-  for (nn = 0; nn < numsaves; nn++) {
-    play.filenumbers[nn] = listbox->SavedGameIndex[nn];
+  for (size_t n = 0; n < saves.size(); ++n)
+  {
+    play.filenumbers[n] = saves[n].Slot;
   }
 
   guis_need_update = 1;
   listbox->SetSvgIndex(true);
 
-  if (numsaves >= MAXSAVEGAMES)
+  if (saves.size() >= MAXSAVEGAMES)
     return 1;
   return 0;
 }

Engine/ac/runtime_defines.h
@@ -21,7 +21,7 @@
 #undef MAX_TIMERS
 #endif
 
-// Max script string length
+// Max old-style script string length
 #define MAX_MAXSTRLEN 200
 #define MAXGLOBALVARS 50
 

commit c777edc70cc5e4d92ee7ea63271747d63fef7850

Engine: fixed built-in save/restore dialog list
Regression caused by 32cd95759d1b649f449cac9fdc1c75b51f96dadb

Engine/gui/guidialog.cpp
@@ -305,14 +305,13 @@ void preparesavegamelist(int ctrllist)
   std::sort(saves.rbegin(), saves.rend());
 
   // fill in the list box and global savegameindex[] array for backward compatibilty
-  numsaves = (int)saves.size();
-  toomanygames = (numsaves >= MAXSAVEGAMES) ? 1 : 0;
-  for (const auto &item : saves)
+  for (numsaves = 0; numsaves < (size_t)saves.size(); ++numsaves)
   {
-      CSCISendControlMessage(ctrllist, CLB_ADDITEM, 0, (long)item.Description.GetCStr());
-      filenumbers[numsaves] = item.Slot;
-      filedates[numsaves] = (long int)item.FileTime;
+      CSCISendControlMessage(ctrllist, CLB_ADDITEM, 0, (long)saves[numsaves].Description.GetCStr());
+      filenumbers[numsaves] = saves[numsaves].Slot;
+      filedates[numsaves] = (long int)saves[numsaves].FileTime;
   }
+  toomanygames = (numsaves >= MAXSAVEGAMES) ? 1 : 0;
   // Select the first item
   CSCISendControlMessage(ctrllist, CLB_SETCURSEL, 0, 0);
 }
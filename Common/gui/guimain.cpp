adventuregamestudio/ags

commit 4fa3ef52163d41aeb8bf3c4573d6b971f17be4c0

rata: dont draw fully transparent (like, faded out) guis

Common/gui/guimain.cpp
@@ -164,7 +164,10 @@ bool GUIMain::IsConcealed() const
 
 bool GUIMain::IsDisplayed() const
 {
-    return IsVisible() && !IsConcealed();
+    if(!IsVisible()) return false;
+    if(IsConcealed()) return false;
+    if(Transparency == 255) return false;
+    return true;
 }
 
 bool GUIMain::IsInteractableAt(int x, int y) const
@@ -458,6 +461,7 @@ void GUIMain::SetTextWindow(bool on)
 void GUIMain::SetTransparencyAsPercentage(int percent)
 {
     Transparency = GfxDef::Trans100ToLegacyTrans255(percent);
+    guis_need_update = 1;
 }
 
 void GUIMain::SetVisible(bool on)

commit ce3e8355e5e20b59a6fa73546669cb25b3084a2f

Engine: moved Transparency check out of GUIMain::IsDisplayed()
This was an mistake to have it there, introduced by 4fa3ef52163d41aeb8bf3c4573d6b971f17be4c0

GUIMain::IsDisplayed() is meant to refer strictly to logical state of a GUI, defining its presence in the screen space, so to speak, and should not involve any visual fx.

Common/gui/guimain.cpp
@@ -164,10 +164,7 @@ bool GUIMain::IsConcealed() const
 
 bool GUIMain::IsDisplayed() const
 {
-    if(!IsVisible()) return false;
-    if(IsConcealed()) return false;
-    if(Transparency == 255) return false;
-    return true;
+    return IsVisible() && !IsConcealed();
 }
 
 bool GUIMain::IsInteractableAt(int x, int y) const

Common/gui/guimain.h
@@ -71,9 +71,9 @@ public:
     // Tells if GUI's visibility is overridden and it won't be displayed on
     // screen regardless of Visible property (until concealed mode is off).
     bool        IsConcealed() const;
-    // Tells if gui is actually displayed on screen. Normally Visible property
-    // determines whether GUI is allowed to be seen, but there may be other
-    // settings that override GUI's visibility.
+    // Tells if gui is actually meant to be displayed on screen.
+    // Normally Visible property determines whether GUI is allowed to be seen,
+    // but there may be other settings that override it.
     bool        IsDisplayed() const;
     // Tells if given coordinates are within interactable area of gui
     // NOTE: this currently tests for actual visibility and Clickable property

Engine/ac/draw.cpp
@@ -2172,6 +2172,7 @@ void draw_gui_and_overlays()
             for (aa=0;aa<game.numgui;aa++) {
                 if (!guis[aa].IsDisplayed()) continue;
                 if (!guis[aa].HasChanged()) continue;
+                if (guis[aa].Transparency == 255) continue;
 
                 guis[aa].ClearChanged();
                 if (guibg[aa] == nullptr)
@@ -2212,6 +2213,7 @@ void draw_gui_and_overlays()
         for (gg = 0; gg < game.numgui; gg++) {
             aa = play.gui_draw_order[gg];
             if (!guis[aa].IsDisplayed()) continue;
+            if (guis[aa].Transparency == 255) continue;
 
             // Don't draw GUI if "GUIs Turn Off When Disabled"
             if ((game.options[OPT_DISABLEOFF] == 3) &&

Engine/ac/gui.cpp
@@ -537,7 +537,7 @@ int adjust_x_for_guis (int xx, int yy) {
         if ((guis[aa].X > xx) || (guis[aa].Y > yy) || (guis[aa].Y + guis[aa].Height < yy))
             continue;
         // totally transparent GUI, ignore
-        if ((guis[aa].BgColor == 0) && (guis[aa].BgImage < 1))
+        if (((guis[aa].BgColor == 0) && (guis[aa].BgImage < 1)) || (guis[aa].Transparency == 255))
             continue;
 
         // try to deal with full-width GUIs across the top
@@ -560,7 +560,7 @@ int adjust_y_for_guis ( int yy) {
         if (guis[aa].Y > yy)
             continue;
         // totally transparent GUI, ignore
-        if ((guis[aa].BgColor == 0) && (guis[aa].BgImage < 1))
+        if (((guis[aa].BgColor == 0) && (guis[aa].BgImage < 1)) || (guis[aa].Transparency == 255))
             continue;
 
         // try to deal with full-height GUIs down the left or right
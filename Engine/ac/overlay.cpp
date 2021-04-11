adventuregamestudio/ags

commit cb7ab658acfc16d734f113fbb7f3cde41dab0d9c

Engine: removed Overlays' arbitrary limit

Engine/ac/overlay.cpp
@@ -40,8 +40,7 @@ extern IGraphicsDriver *gfxDriver;
 
 
 
-ScreenOverlay screenover[MAX_SCREEN_OVERLAYS];
-int numscreenover=0;
+std::vector<ScreenOverlay> screenover;
 int is_complete_overlay=0,is_text_overlay=0;
 int crovr_id=2;  // whether using SetTextOverlay or CreateTextOvelay
 
@@ -156,24 +155,22 @@ void dispose_overlay(ScreenOverlay &over)
         ccAttemptDisposeObject(over.associatedOverlayHandle);
 }
 
-void remove_screen_overlay_index(int over_idx)
+void remove_screen_overlay_index(size_t over_idx)
 {
     ScreenOverlay &over = screenover[over_idx];
     dispose_overlay(over);
     if (over.type==OVER_COMPLETE) is_complete_overlay--;
     if (over.type==OVER_TEXTMSG) is_text_overlay--;
-    numscreenover--;
-    for (int i = over_idx; i < numscreenover; ++i)
-        screenover[i] = screenover[i + 1];
+    screenover.erase(screenover.begin() + over_idx);
     // if an overlay before the sierra-style speech one is removed,
     // update the index
-    if (face_talking > over_idx)
+    if (face_talking >= 0 && (size_t)face_talking > over_idx)
         face_talking--;
 }
 
 void remove_screen_overlay(int type)
 {
-    for (int i = 0; i < numscreenover;)
+    for (size_t i = 0; i < screenover.size();)
     {
         if (type < 0 || screenover[i].type == type)
             remove_screen_overlay_index(i);
@@ -184,23 +181,23 @@ void remove_screen_overlay(int type)
 
 int find_overlay_of_type(int type)
 {
-    for (int i = 0; i < numscreenover; ++i)
+    for (size_t i = 0; i < screenover.size(); ++i)
     {
         if (screenover[i].type == type) return i;
     }
     return -1;
 }
 
-int add_screen_overlay(int x,int y,int type,Bitmap *piccy, bool alphaChannel) {
+size_t add_screen_overlay(int x,int y,int type,Bitmap *piccy, bool alphaChannel) {
     if (type==OVER_COMPLETE) is_complete_overlay++;
     if (type==OVER_TEXTMSG) is_text_overlay++;
     if (type==OVER_CUSTOM) {
         // find an unused custom ID; TODO: find a better approach!
-        for (int id = OVER_CUSTOM + 1; id < OVER_CUSTOM + 100; ++id) {
+        for (int id = OVER_CUSTOM + 1; id < screenover.size() + OVER_CUSTOM + 1; ++id) {
             if (find_overlay_of_type(id) == -1) { type=id; break; }
         }
     }
-    ScreenOverlay &over = screenover[numscreenover++];
+    ScreenOverlay over;
     over.pic=piccy;
     over.bmp = gfxDriver->CreateDDBFromBitmap(piccy, alphaChannel);
     over.x=x;
@@ -211,7 +208,8 @@ int add_screen_overlay(int x,int y,int type,Bitmap *piccy, bool alphaChannel) {
     over.associatedOverlayHandle = 0;
     over.hasAlphaChannel = alphaChannel;
     over.positionRelativeToScreen = true;
-    return numscreenover - 1;
+    screenover.push_back(over);
+    return screenover.size() - 1;
 }
 
 
@@ -260,14 +258,14 @@ void get_overlay_position(const ScreenOverlay &over, int *x, int *y) {
 
 void recreate_overlay_ddbs()
 {
-    for (int i = 0; i < numscreenover; ++i)
+    for (auto &over : screenover)
     {
-        if (screenover[i].bmp)
-            gfxDriver->DestroyDDB(screenover[i].bmp);
-        if (screenover[i].pic)
-            screenover[i].bmp = gfxDriver->CreateDDBFromBitmap(screenover[i].pic, false);
+        if (over.bmp)
+            gfxDriver->DestroyDDB(over.bmp);
+        if (over.pic)
+            over.bmp = gfxDriver->CreateDDBFromBitmap(over.pic, false);
         else
-            screenover[i].bmp = nullptr;
+            over.bmp = nullptr;
     }
 }
 

Engine/ac/character.cpp
@@ -2380,14 +2380,13 @@ void _displayspeech(const char*texx, int aschar, int xx, int yy, int widd, int i
 
     said_speech_line = 1;
 
-    int aa;
     if (play.bgspeech_stay_on_display == 0) {
         // remove any background speech
-        for (aa=0;aa<numscreenover;aa++) {
-            if (screenover[aa].timeout > 0) {
-                remove_screen_overlay(screenover[aa].type);
-                aa--;
-            }
+        for (size_t i = 0; i < screenover.size();) {
+            if (screenover[i].timeout > 0)
+                remove_screen_overlay(screenover[i].type);
+            else
+                i++;
         }
     }
     said_text = 1;
@@ -2398,7 +2397,7 @@ void _displayspeech(const char*texx, int aschar, int xx, int yy, int widd, int i
 
     int isPause = 1;
     // if the message is all .'s, don't display anything
-    for (aa = 0; texx[aa] != 0; aa++) {
+    for (int aa = 0; texx[aa] != 0; aa++) {
         if (texx[aa] != '.') {
             isPause = 0;
             break;

Engine/ac/draw.cpp
@@ -2135,20 +2135,18 @@ void draw_fps(const Rect &viewport)
 // Draw GUI and overlays of all kinds, anything outside the room space
 void draw_gui_and_overlays()
 {
-    int gg;
-
     if(pl_any_want_hook(AGSE_PREGUIDRAW))
         add_thing_to_draw(nullptr, AGSE_PREGUIDRAW, 0, TRANS_RUN_PLUGIN, false);
 
     // draw overlays, except text boxes and portraits
-    for (gg=0;gg<numscreenover;gg++) {
+    for (const auto &over : screenover) {
         // complete overlay draw in non-transparent mode
-        if (screenover[gg].type == OVER_COMPLETE)
-            add_thing_to_draw(screenover[gg].bmp, screenover[gg].x, screenover[gg].y, TRANS_OPAQUE, false);
-        else if (screenover[gg].type != OVER_TEXTMSG && screenover[gg].type != OVER_PICTURE) {
+        if (over.type == OVER_COMPLETE)
+            add_thing_to_draw(over.bmp, over.x, over.y, TRANS_OPAQUE, false);
+        else if (over.type != OVER_TEXTMSG && over.type != OVER_PICTURE) {
             int tdxp, tdyp;
-            get_overlay_position(screenover[gg], &tdxp, &tdyp);
-            add_thing_to_draw(screenover[gg].bmp, tdxp, tdyp, 0, screenover[gg].hasAlphaChannel);
+            get_overlay_position(over, &tdxp, &tdyp);
+            add_thing_to_draw(over.bmp, tdxp, tdyp, 0, over.hasAlphaChannel);
         }
     }
 
@@ -2205,7 +2203,7 @@ void draw_gui_and_overlays()
         }
         our_eip = 38;
         // Draw the GUIs
-        for (gg = 0; gg < game.numgui; gg++) {
+        for (int gg = 0; gg < game.numgui; gg++) {
             aa = play.gui_draw_order[gg];
             if (!guis[aa].IsDisplayed()) continue;
 
@@ -2225,13 +2223,13 @@ void draw_gui_and_overlays()
     }
 
     // draw speech and portraits (so that they appear over GUIs)
-    for (gg=0;gg<numscreenover;gg++) 
+    for (const auto &over : screenover) 
     {
-        if (screenover[gg].type == OVER_TEXTMSG || screenover[gg].type == OVER_PICTURE)
+        if (over.type == OVER_TEXTMSG || over.type == OVER_PICTURE)
         {
             int tdxp, tdyp;
-            get_overlay_position(screenover[gg], &tdxp, &tdyp);
-            add_thing_to_draw(screenover[gg].bmp, tdxp, tdyp, 0, false);
+            get_overlay_position(over, &tdxp, &tdyp);
+            add_thing_to_draw(over.bmp, tdxp, tdyp, 0, false);
         }
     }
 

Engine/ac/game.cpp
@@ -1389,20 +1389,20 @@ void restore_game_ambientsounds(Stream *in, RestoredData &r_data)
 void ReadOverlays_Aligned(Stream *in)
 {
     AlignedStream align_s(in, Common::kAligned_Read);
-    for (int i = 0; i < numscreenover; ++i)
+    for (auto &over : screenover)
     {
-        screenover[i].ReadFromFile(&align_s);
+        over.ReadFromFile(&align_s);
         align_s.Reset();
     }
 }
 
 void restore_game_overlays(Stream *in)
 {
-    numscreenover = in->ReadInt32();
+    screenover.resize(in->ReadInt32());
     ReadOverlays_Aligned(in);
-    for (int bb=0;bb<numscreenover;bb++) {
-        if (screenover[bb].hasSerializedBitmap)
-            screenover[bb].pic = read_serialized_bitmap(in);
+    for (auto &over : screenover) {
+        if (over.hasSerializedBitmap)
+            over.pic = read_serialized_bitmap(in);
     }
 }
 

Engine/ac/global_character.cpp
@@ -549,12 +549,11 @@ void DisplaySpeechAt (int xx, int yy, int wii, int aschar, const char*spch) {
 
 int DisplaySpeechBackground(int charid, const char*speel) {
     // remove any previous background speech for this character
-    int cc;
-    for (cc = 0; cc < numscreenover; cc++) {
-        if (screenover[cc].bgSpeechForChar == charid) {
-            remove_screen_overlay_index(cc);
-            cc--;
-        }
+    for (size_t i = 0; i < screenover.size();) {
+        if (screenover[i].bgSpeechForChar == charid)
+            remove_screen_overlay_index(i);
+        else
+            i++;
     }
 
     int ovrl=CreateTextOverlay(OVR_AUTOPLACE,charid,play.GetUIViewport().GetWidth()/2,FONT_SPEECH,

Engine/ac/overlay.h
@@ -17,7 +17,7 @@
 //=============================================================================
 #ifndef __AGS_EE_AC__OVERLAY_H
 #define __AGS_EE_AC__OVERLAY_H
-#include "ac/runtime_defines.h"
+#include <vector>
 #include "ac/screenoverlay.h"
 #include "ac/dynobj/scriptoverlay.h"
 
@@ -38,14 +38,13 @@ int  find_overlay_of_type(int type);
 void remove_screen_overlay(int type);
 // Calculates overlay position in screen coordinates
 void get_overlay_position(const ScreenOverlay &over, int *x, int *y);
-int  add_screen_overlay(int x,int y,int type,Common::Bitmap *piccy, bool alphaChannel = false);
-void remove_screen_overlay_index(int over_idx);
+size_t add_screen_overlay(int x,int y,int type,Common::Bitmap *piccy, bool alphaChannel = false);
+void remove_screen_overlay_index(size_t over_idx);
 void recreate_overlay_ddbs();
 
 extern int is_complete_overlay;
 extern int is_text_overlay;
 
-extern ScreenOverlay screenover[MAX_SCREEN_OVERLAYS];
-extern int numscreenover;
+extern std::vector<ScreenOverlay> screenover;
 
 #endif // __AGS_EE_AC__OVERLAY_H

Engine/ac/runtime_defines.h
@@ -107,7 +107,6 @@ const int LegacyRoomVolumeFactor            = 30;
 #define FOR_SCRIPT    2
 #define FOR_EXITLOOP  3
 #define CHMLSOFFS (MAX_ROOM_OBJECTS+1)    // reserve this many movelists for objects & stuff
-#define MAX_SCREEN_OVERLAYS 20
 #define abort_all_conditions restrict_until
 #define MAX_SCRIPT_AT_ONCE 10
 #define EVENT_NONE       0

Engine/game/savegame_components.cpp
@@ -823,11 +823,11 @@ HSaveError ReadDynamicSprites(PStream in, int32_t cmp_ver, const PreservedParams
 
 HSaveError WriteOverlays(PStream out)
 {
-    out->WriteInt32(numscreenover);
-    for (int i = 0; i < numscreenover; ++i)
+    out->WriteInt32(screenover.size());
+    for (const auto &over : screenover)
     {
-        screenover[i].WriteToFile(out.get());
-        serialize_bitmap(screenover[i].pic, out.get());
+        over.WriteToFile(out.get());
+        serialize_bitmap(over.pic, out.get());
     }
     return HSaveError::None();
 }
@@ -835,15 +835,14 @@ HSaveError WriteOverlays(PStream out)
 HSaveError ReadOverlays(PStream in, int32_t cmp_ver, const PreservedParams &pp, RestoredData &r_data)
 {
     HSaveError err;
-    int over_count = in->ReadInt32();
-    if (!AssertCompatLimit(err, over_count, MAX_SCREEN_OVERLAYS, "overlays"))
-        return err;
-    numscreenover = over_count;
-    for (int i = 0; i < numscreenover; ++i)
+    size_t over_count = in->ReadInt32();
+    for (size_t i = 0; i < over_count; ++i)
     {
-        screenover[i].ReadFromFile(in.get());
-        if (screenover[i].hasSerializedBitmap)
-            screenover[i].pic = read_serialized_bitmap(in.get());
+        ScreenOverlay over;
+        over.ReadFromFile(in.get());
+        if (over.hasSerializedBitmap)
+            over.pic = read_serialized_bitmap(in.get());
+        screenover.push_back(over);
     }
     return err;
 }

commit 0c7acbc48211e9934740b29d8595893757c741d0

Engine: Fix finding unused overlay custom ID
This was a regression from cb7ab658a.

Engine/ac/overlay.cpp
@@ -288,7 +288,7 @@ size_t add_screen_overlay(int x, int y, int type, Common::Bitmap *piccy, int pic
 {
     if (type == OVER_CUSTOM) {
         // find an unused custom ID; TODO: find a better approach!
-        for (int id = OVER_CUSTOM + 1; id < screenover.size() + OVER_CUSTOM + 1; ++id) {
+        for (int id = OVER_CUSTOM + 1; id <= screenover.size() + OVER_CUSTOM + 1; ++id) {
             if (find_overlay_of_type(id) == -1) { type=id; break; }
         }
     }
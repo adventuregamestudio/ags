68391c056b1dbff83d4f8cca185623a2cd61f3c1
Ivan Mogilko <ikm_spb@yahoo.com>
2021-04-23 19:06:37 +0300

    Tool: crm2ash - generates room script header
    crm2ash - stands for "CRM to ASH" - parses the compiled AGS room (*.crm) and generates a script header (*.ash) containing room script declarations necessary for compiling room script.
    
    Input: compiled ags room (*.crm).
    Output: generated script header with room declarations.
    
    Usage: `crm2ash room.crm room.ash`

---
+++ Tools/data/room_utils.h
@@ -0,0 +1,50 @@
+//=============================================================================
+//
+// Adventure Game Studio (AGS)
+//
+// Copyright (C) 1999-2011 Chris Jones and 2011-20xx others
+// The full list of copyright holders can be found in the Copyright.txt
+// file, which is part of this source code distribution.
+//
+// The AGS source code is provided under the Artistic License 2.0.
+// A copy of this license can be found in the file License.txt and at
+// http://www.opensource.org/licenses/artistic-license-2.0.php
+//
+//=============================================================================
+#ifndef __AGS_TOOL_DATA__CRMUTIL_H
+#define __AGS_TOOL_DATA__CRMUTIL_H
+
+#include <vector>
+#include "game/room_file.h"
+
+namespace AGS
+{
+namespace DataUtil
+{
+
+using namespace AGS::Common;
+
+// Script names found in the room data
+struct RoomScNames
+{
+    std::vector<String> ObjectNames;
+    std::vector<String> HotspotNames;
+};
+
+// Following functions parse the binary room file and extract script names
+// Reads only script names from the main room block, ignores other data
+HRoomFileError ReadFromMainBlock(RoomScNames &data, Stream *in, RoomFileVersion data_ver, soff_t block_len);
+// Reads room object script names block
+HRoomFileError ReadObjScNamesBlock(RoomScNames &data, Stream *in, RoomFileVersion data_ver);
+// A room reading callback of type PfnReadRoomBlock, meant to be passed into ReadRoomData();
+// reads only blocks necessary for retrieving script names.
+HRoomFileError ReadRoomScNames(RoomScNames &data, Stream *in, RoomFileBlock block, const String &ext_id,
+    soff_t block_len, RoomFileVersion data_ver);
+
+// Generates room script header out of the room data
+String MakeRoomScriptHeader(const RoomScNames &data);
+
+} // namespace DataUtil
+} // namespace AGS
+
+#endif // __AGS_TOOL_DATA__CRMUTIL_H

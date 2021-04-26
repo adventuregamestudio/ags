adventuregamestudio/ags

commit e5d43dc57a37b6a2a292860e2c8ab6dfc8e3855f

Engine: support resizable SDL window

Engine/main/engine_setup.cpp
@@ -312,10 +312,12 @@ void engine_post_gfxmode_setup(const Size &init_desktop)
     bool has_driver_changed = scsystem.coldepth != dm.ColorDepth;
 
     engine_setup_scsystem_screen(dm);
-    engine_post_gfxmode_driver_setup();
-    engine_post_gfxmode_screen_setup(dm, has_driver_changed);
     if (has_driver_changed)
+    {
+        engine_post_gfxmode_driver_setup();
         engine_post_gfxmode_draw_setup(dm);
+    }
+    engine_post_gfxmode_screen_setup(dm, has_driver_changed);   
     engine_post_gfxmode_mouse_setup(dm, init_desktop);
 
     video_on_gfxmode_changed();

Engine/ac/sys_events.cpp
@@ -22,6 +22,7 @@
 #include "ac/timer.h"
 #include "device/mousew32.h"
 #include "platform/base/agsplatformdriver.h"
+#include "main/engine.h"
 
 using namespace AGS::Common;
 using namespace AGS::Engine;
@@ -481,6 +482,9 @@ void sys_evt_process_one(const SDL_Event &event) {
             if (_on_switchout_callback) {
                 _on_switchout_callback();
             }
+            break;
+        case SDL_WINDOWEVENT_SIZE_CHANGED:
+            engine_on_window_changed(Size(event.window.data1, event.window.data2));
             break;
         }
         break;

Engine/main/engine.cpp
@@ -1448,6 +1448,13 @@ bool engine_try_switch_windowed_gfxmode()
     return res;
 }
 
+void engine_on_window_changed(const Size &sz)
+{
+    graphics_mode_on_window_changed(sz);
+    on_coordinates_scaling_changed();
+    invalidate_screen();
+}
+
 void engine_shutdown_gfxmode()
 {
     if (!gfxDriver)

Engine/main/engine.h
@@ -30,6 +30,8 @@ bool        engine_try_set_gfxmode_any(const ScreenSetup &setup);
 // Tries to switch between fullscreen and windowed mode; uses previously saved
 // setup if it is available, or default settings for the new mode
 bool        engine_try_switch_windowed_gfxmode();
+// Update graphic renderer and render frame when window size changes
+void        engine_on_window_changed(const Size &sz);
 // Shutdown graphics mode (used before shutting down tha application)
 void        engine_shutdown_gfxmode();
 

Engine/main/graphics_mode.cpp
@@ -654,6 +654,14 @@ bool graphics_mode_set_filter(const String &filter_id)
     return true;
 }
 
+void graphics_mode_on_window_changed(const Size &sz)
+{
+    if (!gfxDriver)
+        return; // nothing to update
+    gfxDriver->UpdateDeviceScreen(sz);
+    graphics_mode_update_render_frame();
+}
+
 void graphics_mode_shutdown()
 {
     if (GfxFactory)

Engine/main/graphics_mode.h
@@ -152,6 +152,8 @@ bool graphics_mode_set_render_frame(const GameFrameSetup &frame_setup);
 bool graphics_mode_set_filter_any(const GfxFilterSetup &setup);
 // Set the scaling filter with given ID
 bool graphics_mode_set_filter(const String &filter_id);
+// Update graphic renderer and render frame when window size changes
+void graphics_mode_on_window_changed(const Size &sz);
 // Releases current graphic mode and shuts down renderer
 void graphics_mode_shutdown();
 

Engine/platform/base/sys_main.cpp
@@ -92,8 +92,7 @@ SDL_Window *sys_window_create(const char *window_title, int w, int h, bool windo
     }
     // TODO: support display index selection (?)
     // TODO: support separate fullscreen and desktop (borderless fullscreen window) modes
-    // TODO: support resizable window later, might need callback for engine and/or gfx renderer
-    Uint32 flags = 0u/*SDL_WINDOW_RESIZABLE*/;
+    Uint32 flags = SDL_WINDOW_RESIZABLE;
     if (!windowed) {
         flags |= SDL_WINDOW_FULLSCREEN_DESKTOP/*SDL_WINDOW_FULLSCREEN*/;
     }

commit 8c1ed36a2a5c118c57da7889f890b874c4e90aa3

Engine: fixed gfxdriver loosing callbacks on mode switch
Was broken by e5d43dc57a37b6a2a292860e2c8ab6dfc8e3855f

Engine/main/engine_setup.cpp
@@ -312,9 +312,9 @@ void engine_post_gfxmode_setup(const Size &init_desktop)
     bool has_driver_changed = scsystem.coldepth != dm.ColorDepth;
 
     engine_setup_scsystem_screen(dm);
+    engine_post_gfxmode_driver_setup();
     if (has_driver_changed)
     {
-        engine_post_gfxmode_driver_setup();
         engine_post_gfxmode_draw_setup(dm);
     }
     engine_post_gfxmode_screen_setup(dm, has_driver_changed);   
adventuregamestudio/ags

commit 15489ff2c97762f7c40a8374d5b50241830bcd74

timing: reimplement timing changes similar to c4907f5d but without the counter

Engine/ac/timer.cpp
@@ -27,19 +27,57 @@ namespace {
 
 const auto MAXIMUM_FALL_BEHIND = 3;
 
-auto last_tick_time = AGS_Clock::now();
 auto tick_duration = std::chrono::microseconds(1000000LL/40);
 auto framerate_maxed = false;
 
+auto last_tick_time = AGS_Clock::now();
+auto next_frame_timestamp = AGS_Clock::now();
+
 }
 
-void setTimerFps(int new_fps) {
+std::chrono::microseconds GetFrameDuration()
+{
+    if (framerate_maxed) {
+        return std::chrono::microseconds(0);
+    }
+    return tick_duration;
+}
+
+void setTimerFps(int new_fps) 
+{
     tick_duration = std::chrono::microseconds(1000000LL/new_fps);
-    last_tick_time = AGS_Clock::now();
     framerate_maxed = new_fps >= 1000;
+
+    last_tick_time = AGS_Clock::now();
+    next_frame_timestamp = AGS_Clock::now();
 }
 
-bool waitingForNextTick() {
+void WaitForNextFrame()
+{
+    auto now = AGS_Clock::now();
+    auto frameDuration = GetFrameDuration();
+
+    // early exit if we're trying to maximise framerate
+    if (frameDuration <= std::chrono::milliseconds::zero()) {
+        next_frame_timestamp = now;
+        return;
+    }
+
+    // jump ahead if we're lagging
+    if (next_frame_timestamp < (now - MAXIMUM_FALL_BEHIND*frameDuration)) {
+        next_frame_timestamp = now;
+    }
+
+    auto frame_time_remaining = next_frame_timestamp - now;
+    if (frame_time_remaining > std::chrono::milliseconds::zero()) {
+        std::this_thread::sleep_for(frame_time_remaining);
+    }
+    
+    next_frame_timestamp += frameDuration;
+}
+
+bool waitingForNextTick() 
+{
     auto now = AGS_Clock::now();
 
     if (framerate_maxed) {
@@ -67,11 +105,11 @@ bool waitingForNextTick() {
         return false;
     }
 
-    std::this_thread::sleep_for(next_tick_time - now);
-
     return true;
 }
 
-void skipMissedTicks() {
+void skipMissedTicks() 
+{
     last_tick_time = AGS_Clock::now();
+    next_frame_timestamp = AGS_Clock::now();
 }

Engine/ac/dialog.cpp
@@ -1044,7 +1044,14 @@ bool DialogOptions::Run()
             return true; // continue running loop
         }
       }
-      PollUntilNextFrame();
+
+      update_polled_stuff_if_runtime();
+
+      if (play.fast_forward == 0)
+      {
+          WaitForNextFrame();
+      }
+
       return true; // continue running loop
 }
 

Engine/ac/display.cpp
@@ -290,7 +290,14 @@ int _display_main(int xx,int yy,int wii,const char*text,int blocking,int usingfo
                 if (skip_setting & SKIP_KEYPRESS)
                     break;
             }
-            PollUntilNextFrame();
+            
+            update_polled_stuff_if_runtime();
+
+            if (play.fast_forward == 0)
+            {
+                WaitForNextFrame();
+            }
+
             countdown--;
 
             if (play.speech_has_voice) {

Engine/ac/event.cpp
@@ -282,12 +282,12 @@ void process_event(EventHappened*evp) {
                     boxhit = Math::Clamp(boxhit, 0, viewport.GetHeight());
                     int lxp = viewport.GetWidth() / 2 - boxwid / 2;
                     int lyp = viewport.GetHeight() / 2 - boxhit / 2;
+                    gfxDriver->Vsync();
                     temp_scr->Blit(saved_backbuf, lxp, lyp, lxp, lyp, 
                         boxwid, boxhit);
                     render_to_screen(viewport.Left, viewport.Top);
-                    do {
-                        update_polled_stuff_if_runtime();
-                    } while (waitingForNextTick());
+                    update_polled_mp3();
+                    WaitForNextFrame();
                 }
                 gfxDriver->SetMemoryBackBuffer(saved_backbuf, viewport.Left, viewport.Top);
             }
@@ -315,9 +315,8 @@ void process_event(EventHappened*evp) {
                     gfxDriver->DrawSprite(0, 0, ddb);
                 }
                 render_to_screen();
-                do {
-                    update_polled_stuff_if_runtime();
-                } while (waitingForNextTick());
+                update_polled_stuff_if_runtime();
+                WaitForNextFrame();
                 transparency -= 16;
             }
             saved_viewport_bitmap->Release();
@@ -353,9 +352,8 @@ void process_event(EventHappened*evp) {
                 draw_screen_callback();
                 gfxDriver->DrawSprite(0, 0, ddb);
                 render_to_screen();
-                do {
-                    update_polled_stuff_if_runtime();
-                } while (waitingForNextTick());
+                update_polled_stuff_if_runtime();
+                WaitForNextFrame();
             }
             saved_viewport_bitmap->Release();
 

Engine/ac/invwindow.cpp
@@ -474,7 +474,10 @@ bool InventoryScreen::Run()
             //ags_domouse(DOMOUSE_ENABLE);
         }
         wasonitem=isonitem;
-        PollUntilNextFrame();
+
+        update_polled_stuff_if_runtime();
+
+        WaitForNextFrame();
 
     return true; // continue inventory screen loop
 }

Engine/ac/timer.h
@@ -28,6 +28,8 @@ using AGS_Clock = std::conditional<
         std::chrono::high_resolution_clock, std::chrono::steady_clock
       >::type;
 
+extern void WaitForNextFrame();
+
 extern void setTimerFps(int new_fps);
 extern bool waitingForNextTick();  // store last tick time.
 extern void skipMissedTicks();  // if more than N frames, just skip all, start a fresh.

Engine/gfx/ali3dogl.cpp
@@ -1918,11 +1918,9 @@ void OGLGraphicsDriver::do_fade(bool fadingOut, int speed, int targetColourRed, 
     d3db->SetTransparency(fadingOut ? a : (255 - a));
     this->_render(flipTypeLastTime, false);
 
-    do {
-      if (_pollingCallback)
-        _pollingCallback();
-    } while (waitingForNextTick());
-
+    if (_pollingCallback)
+      _pollingCallback();
+    WaitForNextFrame();
   }
 
   if (fadingOut)

Engine/gfx/ali3dsw.cpp
@@ -622,12 +622,9 @@ void ALSoftwareGraphicsDriver::highcolor_fade_in(Bitmap *vs, int offx, int offy,
        bmp_buff->TransBlendBlt(bmp_orig, 0, 0);
        this->Vsync();
        _filter->RenderScreen(bmp_buff, 0, 0);
-       do
-       {
-         if (_pollingCallback)
-           _pollingCallback();
-       }
-       while (waitingForNextTick());
+       if (_pollingCallback)
+         _pollingCallback();
+       WaitForNextFrame();
    }
    delete bmp_buff;
    if (bmp_orig != vs)
@@ -651,12 +648,9 @@ void ALSoftwareGraphicsDriver::highcolor_fade_out(Bitmap *vs, int offx, int offy
         bmp_buff->TransBlendBlt(bmp_orig, 0, 0);
         this->Vsync();
         _filter->RenderScreen(bmp_buff, 0, 0);
-        do
-        {
-          if (_pollingCallback)
-            _pollingCallback();
-          platform->Delay(1);
-        } while (waitingForNextTick());
+        if (_pollingCallback)
+          _pollingCallback();
+        WaitForNextFrame();
     }
     delete bmp_buff;
 

commit abd03a8fbc672266da721c5281c7c9591f9b0e76

Engine: reintroduced a suspend mode in WaitForNextFrame()
Previously the engine was using allegro timers that were suspended by default when the player switched away from the game.
The commits f2ab428a596c67d950f2aed5fbd335f6ccc72e3f and 15489ff2c97762f7c40a8374d5b50241830bcd74  broke that behavior. Here we reintroduce it, now implementing the timer lock mechanic directly in the engine.

Engine/ac/game.cpp
@@ -101,6 +101,7 @@ RoomStruct thisroom;
 
 volatile int switching_away_from_game = 0;
 volatile bool switched_away = false;
+volatile int game_update_suspend = 0;
 volatile char want_exit = 0, abort_engine = 0;
 GameDataVersion loaded_game_file_version = kGameVersion_Undefined;
 int frames_per_second=40;
@@ -1406,9 +1407,9 @@ void display_switch_out_suspend()
 {
     // this is only called if in SWITCH_PAUSE mode
     //debug_script_warn("display_switch_out");
-    display_switch_out();
-
     switching_away_from_game++;
+    game_update_suspend++;
+    display_switch_out();
 
     platform->PauseApplication();
 
@@ -1438,7 +1439,6 @@ void display_switch_out_suspend()
 // Called whenever game gets input focus
 void display_switch_in()
 {
-    switched_away = false;
     if (gfxDriver)
     {
         DisplayMode mode = gfxDriver->GetDisplayMode();
@@ -1450,6 +1450,7 @@ void display_switch_in()
     // If auto lock option is set, lock mouse to the game window
     if (usetup.mouse_auto_lock && scsystem.windowed)
         Mouse::TryLockToWindow();
+    switched_away = false;
 }
 
 void display_switch_in_resume()
@@ -1471,6 +1472,7 @@ void display_switch_in_resume()
         gfxDriver->ClearRectangle(0, 0, game.GetGameRes().Width - 1, game.GetGameRes().Height - 1, nullptr);
 
     platform->ResumeApplication();
+    game_update_suspend--;
 }
 
 void replace_tokens(const char*srcmes,char*destm, int maxlen) {

Engine/ac/timer.cpp
@@ -11,12 +11,12 @@
 // http://www.opensource.org/licenses/artistic-license-2.0.php
 //
 //=============================================================================
-
 #include "ac/timer.h"
-
 #include "core/platform.h"
 #include <thread>
 #include "platform/base/agsplatformdriver.h"
+
+extern volatile int game_update_suspend;
 
 namespace {
 
@@ -60,6 +60,10 @@ void WaitForNextFrame()
     // early exit if we're trying to maximise framerate
     if (frameDuration <= std::chrono::milliseconds::zero()) {
         next_frame_timestamp = now;
+        // suspend while the game is being switched out
+        while (game_update_suspend > 0) {
+            platform->YieldCPU();
+        }
         return;
     }
 
@@ -74,6 +78,11 @@ void WaitForNextFrame()
     }
     
     next_frame_timestamp += frameDuration;
+
+    // suspend while the game is being switched out
+    while (game_update_suspend > 0) {
+        platform->YieldCPU();
+    }
 }
 
 void skipMissedTicks() 
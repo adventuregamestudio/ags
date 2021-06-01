95c60551ad2b84c2ae327d9226d2afe290e9a49f
Ivan Mogilko <ikm_spb@yahoo.com>
2021-06-02 01:30:28 +0300

    Tool: trac - compiles game translation (TRS -> TRA)
    trac - stands for "TRAnslation Compiler" - parses the translation source file (*.trs) and generates a compiled translation (*.tra) which the engine can use.
    
    Input: translation source (*.trs) and game identification parameters (name, uniqueid).
    Output: runtime translation (*.tra).
    
    Usage: `trac name.trs name.tra --gamename MyGame --uniqueid 12345`

---
+++ Tools/data/tra_utils.h
@@ -0,0 +1,36 @@
+//=============================================================================
+//
+// Adventure Game Studio (AGS)
+//
+// Copyright (C) 1999-2011 Chris Jones and 2011-20xx others
+// The full list of copyright holders can be found in the Copyright.txt
+// file, which is part of this source code distribution.
+//
+// The AGS source code is provided under the Artistic License 2.0.
+// A copy of this license can be found in the file License.txt and at
+// http://www.opensource.org/licenses/artistic-license-2.0.php
+//
+//=============================================================================
+#ifndef __AGS_TOOL_DATA__CRMUTIL_H
+#define __AGS_TOOL_DATA__CRMUTIL_H
+
+#include "game/tra_file.h"
+
+namespace AGS
+{
+namespace DataUtil
+{
+
+using AGS::Common::HError;
+using AGS::Common::Stream;
+using AGS::Common::String;
+using AGS::Common::Translation;
+
+// Parses a TRS format and fills Translation data
+HError ReadTRS(Translation &tra, Stream *in);
+HError WriteTRA(const Translation &tra, Stream *out);
+
+} // namespace DataUtil
+} // namespace AGS
+
+#endif // __AGS_TOOL_DATA__CRMUTIL_H

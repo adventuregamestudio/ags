d1366a50e4a07626a15e0d9375aba8f58f5fc96e
Ivan Mogilko <ikm_spb@yahoo.com>
2021-06-04 22:34:12 +0300

    Removed mention of NO_MP3_PLAYER

--- Common/core/def_version.h
+++ Common/core/def_version.h
@@ -6,12 +6,6 @@
 #define ACI_VERSION_MSRC_DEF  3,6,0,3
 #endif
 
-#ifdef NO_MP3_PLAYER
-#define SPECIAL_VERSION "NMP"
-#else
-#define SPECIAL_VERSION ""
-#endif
-
 #define ACI_COPYRIGHT_YEARS "2011-2021"
 
 #endif // __AGS_CN_CORE__DEFVERSION_H

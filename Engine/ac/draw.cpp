adventuregamestudio/ags

commit ad42b7a5c6dd16d81fb20baf4e10c9ea9f2f3941

Engine: pass sprite's origin coordinates into the renderers
Because there's now rotation supported it no longer makes sense to pass coordinates with only scaling-based offset. For software renderer we will have to provide prepared transformed images, but 3D renderers do transformation themselves. Passing fully transformed coords into 3D renderer will make it more complicated for them to position sprite in the scene.

Knowing sprite's origin, 3D renderer may calculate it's new position after scaling.
At the same time software renderer will need some new method to find top-left coordinates to draw a sprite at, which may be introduced by the following commits.

Engine/ac/draw.cpp
@@ -1473,6 +1473,8 @@ int construct_object_gfx(int aa, int *drawnWidth, int *drawnHeight, bool alwaysU
 void prepare_objects_for_drawing() {
     our_eip=32;
 
+    const bool is_3d_render = gfxDriver->HasAcceleratedTransform();
+
     for (int aa=0; aa<croom->numobj; aa++) {
         if (objs[aa].on != 1) continue;
         // offscreen, don't draw
@@ -1486,9 +1488,15 @@ void prepare_objects_for_drawing() {
         // update the cache for next time
         objcache[aa].xwas = objs[aa].x;
         objcache[aa].ywas = objs[aa].y;
-        int atxp = objs[aa].x;
-        int atyp = objs[aa].y - tehHeight;
+        const int objx = objs[aa].x;
+        const int objy = objs[aa].y;
 
+        //
+        // Sort out walk-behinds
+        //
+        // we must use actual image's top-left position here
+        const int imgx = objx;
+        const int imgy = objy - tehHeight;
         int usebasel = objs[aa].get_baseline();
 
         if (objs[aa].flags & OBJF_NOWALKBEHINDS) {
@@ -1500,11 +1508,11 @@ void prepare_objects_for_drawing() {
         }
         else if (walkBehindMethod == DrawAsSeparateCharSprite) 
         {
-            sort_out_char_sprite_walk_behind(useindx, atxp, atyp, usebasel, objs[aa].zoom, objs[aa].last_width, objs[aa].last_height);
+            sort_out_char_sprite_walk_behind(useindx, imgx, imgy, usebasel, objs[aa].zoom, objs[aa].last_width, objs[aa].last_height);
         }
         else if ((!actspsIntact) && (walkBehindMethod == DrawOverCharSprite))
         {
-            sort_out_walk_behinds(actsps[useindx], atxp, atyp, usebasel);
+            sort_out_walk_behinds(actsps[useindx], imgx, imgy, usebasel);
         }
 
         if ((!actspsIntact) || (actspsbmp[useindx] == nullptr))
@@ -1516,7 +1524,8 @@ void prepare_objects_for_drawing() {
             actspsbmp[useindx] = gfxDriver->CreateDDBFromBitmap(actsps[useindx], hasAlpha);
         }
 
-        if (gfxDriver->HasAcceleratedTransform())
+        actspsbmp[useindx]->SetOrigin(0.f, 1.f);
+        if (is_3d_render)
         {
             actspsbmp[useindx]->SetFlippedLeftRight(objcache[aa].mirroredWas != 0);
             actspsbmp[useindx]->SetStretch(objs[aa].last_width, objs[aa].last_height);
@@ -1537,10 +1546,7 @@ void prepare_objects_for_drawing() {
                 actspsbmp[useindx]->SetLightLevel(0);
         }
 
-        actspsbmp[useindx]->SetTransparency(objs[aa].transparent);
-        actspsbmp[useindx]->SetBlendMode(objs[aa].blend_mode);
-        actspsbmp[useindx]->SetRotation(objs[aa].rotation);
-        add_to_sprite_list(actspsbmp[useindx], atxp, atyp, usebasel, false);
+        add_to_sprite_list(actspsbmp[useindx], objx, objy, usebasel, false);
     }
 }
 
@@ -1599,6 +1605,8 @@ void prepare_characters_for_drawing() {
     int tint_red, tint_green, tint_blue, tint_amount, tint_light = 255;
 
     our_eip=33;
+
+    const bool is_3d_render = gfxDriver->HasAcceleratedTransform();
 
     // draw characters
     for (int aa=0; aa < game.numcharacters; aa++) {
@@ -1706,7 +1714,7 @@ void prepare_characters_for_drawing() {
         }
         else if ((charcache[aa].inUse) && 
             (charcache[aa].sppic == specialpic) &&
-            (gfxDriver->HasAcceleratedTransform()))
+            (is_3d_render))
         {
             usingCachedImage = true;
         }
@@ -1745,7 +1753,7 @@ void prepare_characters_for_drawing() {
             // create the base sprite in actsps[useindx], which will
             // be scaled and/or flipped, as appropriate
             int actspsUsed = 0;
-            if (!gfxDriver->HasAcceleratedTransform())
+            if (!is_3d_render)
             {
                 actspsUsed = scale_and_flip_sprite(
                     useindx, coldept, zoom_level, sppic,
@@ -1760,7 +1768,7 @@ void prepare_characters_for_drawing() {
             our_eip = 335;
 
             if (((light_level != 0) || (tint_amount != 0)) &&
-                (!gfxDriver->HasAcceleratedTransform())) {
+                (!is_3d_render)) {
                     // apply the lightening or tinting
                     Bitmap *comeFrom = nullptr;
                     // if possible, direct read from the source image
@@ -1789,19 +1797,18 @@ void prepare_characters_for_drawing() {
         charextra[aa].width = newwidth;
         charextra[aa].height = newheight;
         charextra[aa].UpdateGraphicSpace(chin);
-        // FIXME: use graphic space to know actual AABB after rotation
-        // Calculate the X & Y co-ordinates of where the sprite will be
-        const int atxp = chin->x - newwidth / 2;
-        const int atyp = chin->y - newheight
-            // adjust the Y positioning for the character's Z co-ord
-            - chin->z;
+        const int charx = chin->x + chin->pic_xoffs;
+        const int chary = chin->y - chin->z + chin->pic_yoffs;
 
+        //
+        // Sort out walk-behinds
+        //
+        // we must use actual image's top-left position here
+        const int imgx = charx - newwidth / 2;
+        const int imgy = chary - newheight;
         int usebasel = chin->get_baseline();
 
         our_eip = 336;
-
-        const int bgX = atxp + chin->pic_xoffs;
-        const int bgY = atyp + chin->pic_yoffs;
 
         if (chin->flags & CHF_NOWALKBEHINDS) {
             // ignore walk-behinds, do nothing
@@ -1812,11 +1819,11 @@ void prepare_characters_for_drawing() {
         }
         else if (walkBehindMethod == DrawAsSeparateCharSprite) 
         {
-            sort_out_char_sprite_walk_behind(useindx, bgX, bgY, usebasel, charextra[aa].zoom, newwidth, newheight);
+            sort_out_char_sprite_walk_behind(useindx, imgx, imgy, usebasel, charextra[aa].zoom, newwidth, newheight);
         }
         else if (walkBehindMethod == DrawOverCharSprite)
         {
-            sort_out_walk_behinds(actsps[useindx], bgX, bgY, usebasel);
+            sort_out_walk_behinds(actsps[useindx], imgx, imgy, usebasel);
         }
 
         if ((!usingCachedImage) || (actspsbmp[useindx] == nullptr))
@@ -1826,9 +1833,11 @@ void prepare_characters_for_drawing() {
             actspsbmp[useindx] = recycle_ddb_bitmap(actspsbmp[useindx], actsps[useindx], hasAlpha);
         }
 
-        if (gfxDriver->HasAcceleratedTransform()) 
+        actspsbmp[useindx]->SetOrigin(0.5f, 1.f);
+        if (is_3d_render)
         {
             actspsbmp[useindx]->SetStretch(newwidth, newheight);
+            actspsbmp[useindx]->SetRotation(charextra[chin->index_id].rotation);
             actspsbmp[useindx]->SetFlippedLeftRight(isMirrored != 0);
             actspsbmp[useindx]->SetTint(tint_red, tint_green, tint_blue, (tint_amount * 256) / 100);
 
@@ -1850,13 +1859,9 @@ void prepare_characters_for_drawing() {
 
         our_eip = 337;
 
-        chin->actx = atxp;
-        chin->acty = atyp;
-
         actspsbmp[useindx]->SetTransparency(chin->transparency);
         actspsbmp[useindx]->SetBlendMode(charextra[chin->index_id].blend_mode);
-        actspsbmp[useindx]->SetRotation(charextra[chin->index_id].rotation);
-        add_to_sprite_list(actspsbmp[useindx], bgX, bgY, usebasel, false);
+        add_to_sprite_list(actspsbmp[useindx], charx, chary, usebasel, false);
     }
 }
 

Engine/gfx/ali3dogl.cpp
@@ -1070,6 +1070,10 @@ void OGLGraphicsDriver::_renderSprite(const OGLDrawListEntry *drawListEntry, con
       // and now shift it over to make it 0..w again
       thisX += width;
     }
+    // Apply sprite origin
+    thisX -= widthToScale * bmpToDraw->_originX;
+    thisY += heightToScale * bmpToDraw->_originY; // inverse axis
+    // Setup rotation and pivot
     float rotZ = bmpToDraw->_rotation;
     float pivotX = -(widthToScale * 0.5), pivotY = (heightToScale * 0.5);
 

Engine/gfx/ali3dogl.h
@@ -116,8 +116,9 @@ public:
         _colDepth = colDepth;
         _flipped = false;
         _hasAlpha = false;
-        _stretchToWidth = 0;
-        _stretchToHeight = 0;
+        _stretchToWidth = width;
+        _stretchToHeight = height;
+        _originX = _originY = 0.f;
         _useResampler = false;
         _rotation = 0;
         _red = _green = _blue = 0;
@@ -128,8 +129,8 @@ public:
         _blendMode = Common::kBlend_Normal;
     }
 
-    int GetWidthToRender() const { return (_stretchToWidth > 0) ? _stretchToWidth : _width; }
-    int GetHeightToRender() const { return (_stretchToHeight > 0) ? _stretchToHeight : _height; }
+    int GetWidthToRender() const { return _stretchToWidth; }
+    int GetHeightToRender() const { return _stretchToHeight; }
 
     void Dispose();
 

Engine/gfx/ali3dsw.h
@@ -50,7 +50,7 @@ public:
     int  GetTransparency() const override { return _transparency; }
     void SetTransparency(int transparency) override { _transparency = transparency; }
     void SetFlippedLeftRight(bool isFlipped) override { _flipped = isFlipped; }
-    void SetStretch(int width, int height, bool useResampler = true) override 
+    void SetStretch(int width, int height, bool /*useResampler*/) override
     {
         _stretchToWidth = width;
         _stretchToHeight = height;
@@ -77,8 +77,8 @@ public:
         _height = bmp->GetHeight();
         _colDepth = bmp->GetColorDepth();
         _flipped = false;
-        _stretchToWidth = 0;
-        _stretchToHeight = 0;
+        _stretchToWidth = _width;
+        _stretchToHeight = _height;
         _rotation = 0;
         _transparency = 0;
         _opaque = opaque;
@@ -86,8 +86,8 @@ public:
         _blendMode = Common::kBlend_Normal;
     }
 
-    int GetWidthToRender() { return (_stretchToWidth > 0) ? _stretchToWidth : _width; }
-    int GetHeightToRender() { return (_stretchToHeight > 0) ? _stretchToHeight : _height; }
+    int GetWidthToRender() const { return _stretchToWidth; }
+    int GetHeightToRender() const { return _stretchToHeight; }
 
     void Dispose()
     {

Engine/gfx/ddb.h
@@ -32,6 +32,10 @@ namespace Engine
 class IDriverDependantBitmap
 {
 public:
+  // Sprite's origin [0,1] is a relative position of texture around sprite's position;
+  // E.g. (0.0, 0.0) means the texture will be aligned to sprite's position by its
+  // left-top corner, (0.5, 0.5) means the texture will be centered around sprite's pos.
+  virtual void SetOrigin(float originx, float originy) = 0;
   virtual int  GetTransparency() const = 0;
   virtual void SetTransparency(int transparency) = 0;  // 0-255
   virtual void SetFlippedLeftRight(bool isFlipped) = 0;

Engine/gfx/gfxdriverbase.h
@@ -163,11 +163,18 @@ struct TextureTile
 class BaseDDB : public IDriverDependantBitmap
 {
 public:
+    void SetOrigin(float originx, float originy) override
+    {
+        _originX = originx;
+        _originY = originy;
+    }
+
     int GetWidth() const override { return _width; }
     int GetHeight() const override { return _height; }
     int GetColorDepth() const override { return _colDepth; }
 
     int _width = 0, _height = 0;
+    float _originX = 0.f, _originY = 0.f;
     int _colDepth = 0;
     bool _hasAlpha = false; // has meaningful alpha channel
     bool _opaque = false; // no mask color

Engine/platform/windows/gfx/ali3dd3d.cpp
@@ -1214,6 +1214,10 @@ void D3DGraphicsDriver::_renderSprite(const D3DDrawListEntry *drawListEntry, con
       // and now shift it over to make it 0..w again
       thisX += width;
     }
+    // Apply sprite origin
+    thisX -= widthToScale * bmpToDraw->_originX;
+    thisY += heightToScale * bmpToDraw->_originY; // inverse axis
+    // Setup rotation and pivot
     float rotZ = bmpToDraw->_rotation;
     float pivotX = -(widthToScale * 0.5), pivotY = (heightToScale * 0.5);
 

Engine/platform/windows/gfx/ali3dd3d.h
@@ -109,8 +109,9 @@ public:
         _colDepth = colDepth;
         _flipped = false;
         _hasAlpha = false;
-        _stretchToWidth = 0;
-        _stretchToHeight = 0;
+        _stretchToWidth = width;
+        _stretchToHeight = height;
+        _originX = _originY = 0.f;
         _useResampler = false;
         _rotation = 0;
         _red = _green = _blue = 0;
@@ -121,8 +122,8 @@ public:
         _blendMode = Common::kBlend_Normal;
     }
 
-    int GetWidthToRender() { return (_stretchToWidth > 0) ? _stretchToWidth : _width; }
-    int GetHeightToRender() { return (_stretchToHeight > 0) ? _stretchToHeight : _height; }
+    int GetWidthToRender() const { return _stretchToWidth; }
+    int GetHeightToRender() const { return _stretchToHeight; }
 
     void Dispose();
 

commit 0ca7dc74e0fbf708c1b90903a69f14547122d01a

Engine: fixed missing Object's render parameters
Was broken by ad42b7a5c6dd16d81fb20baf4e10c9ea9f2f3941

Engine/ac/draw.cpp
@@ -1570,8 +1570,9 @@ void prepare_objects_for_drawing() {
         actspsbmp[useindx]->SetOrigin(0.f, 1.f);
         if (is_3d_render)
         {
-            actspsbmp[useindx]->SetFlippedLeftRight(objcache[aa].mirroredWas != 0);
             actspsbmp[useindx]->SetStretch(objs[aa].last_width, objs[aa].last_height);
+            actspsbmp[useindx]->SetRotation(objs[aa].rotation);
+            actspsbmp[useindx]->SetFlippedLeftRight(objcache[aa].mirroredWas != 0);
             actspsbmp[useindx]->SetTint(objcache[aa].tintredwas, objcache[aa].tintgrnwas, objcache[aa].tintbluwas, (objcache[aa].tintamntwas * 256) / 100);
 
             if (objcache[aa].tintamntwas > 0)
@@ -1589,6 +1590,8 @@ void prepare_objects_for_drawing() {
                 actspsbmp[useindx]->SetLightLevel(0);
         }
 
+        actspsbmp[useindx]->SetTransparency(objs[aa].transparent);
+        actspsbmp[useindx]->SetBlendMode(objs[aa].blend_mode);
         add_to_sprite_list(actspsbmp[useindx], objx, objy, aabb, usebasel, false);
     }
 }
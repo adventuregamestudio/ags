32a3b0d25d71145d3435a8e697b537b506091e58
Érico Vieira Porto <ericoporto2008@gmail.com>
2021-07-06 22:39:07 -0300

    add preprocessor from Editor in cpp version

---
+++ Compiler/preproc/preprocessor.cpp
@@ -0,0 +1,437 @@
+#include <algorithm>
+#include "preproc/preprocessor.h"
+#include "util/textstreamwriter.h"
+#include "util/textreader.h"
+#include "script/cc_error.h"
+
+#define STRINGIFY2(X) #X
+#define STRINGIFY(X) STRINGIFY2(X)
+
+using namespace AGS::Common;
+
+extern int currentline; // in script/script_common
+
+namespace AGS {
+namespace Preprocessor {
+
+    static const size_t NOT_FOUND = -1;
+
+#if AGS_PLATFORM_OS_WINDOWS
+    static const char * li_end = "\r\n";
+#else
+    static const char * li_end = "\n";
+#endif
+
+    class StringBuilder : TextWriter {
+    private:
+        String _str = nullptr;
+    public:
+        explicit StringBuilder(size_t size) {
+            _str.Reserve(size);
+        }
+
+        void WriteLine(const String& str) override { _str.Append(str); _str.Append(li_end); }
+        bool IsValid() const override { return true; }
+        void WriteChar(char c) override {  _str.AppendChar(c); }
+        void WriteString(const String &str) override { _str.Append(str);}
+        void WriteFormat(const char *fmt, ...) override { va_list argptr; va_start(argptr, fmt); _str.Append(String::FromFormatV(fmt, argptr)); va_end(argptr);}
+        void WriteLineBreak() override { _str.Append(li_end); }
+
+        String GetString () {return _str;}
+    };
+
+    class StringReader : TextReader{
+    private:
+        String _str = nullptr;
+        size_t _idx = 0;
+    public:
+        StringReader(const String& str) {
+            _str = str;
+            _idx = 0;
+        }
+
+        bool IsValid() const override { return _idx < _str.GetLength(); }
+        char ReadChar() override { return _str.GetAt(_idx++); }
+        String ReadString(size_t length) override { String res = _str.Mid(_idx, length); _idx+=res.GetLength(); return res;}
+        String ReadLine() override {
+            if(_idx >= _str.GetLength()) return nullptr;
+
+            size_t newPos = _str.FindChar('\n', _idx);
+            if(newPos != NOT_FOUND) {
+                size_t length = newPos - _idx;
+                size_t idx = _idx;
+                _idx = newPos+1;
+                if (length == 0) return String("");
+                return _str.Mid(idx,length);
+            } else {
+                return ReadAll();
+            }
+        }
+        String ReadAll() override { size_t idx = _idx; _idx = _str.GetLength(); return _str.Mid(idx, _str.GetLength() - idx); }
+    };
+
+    static bool Contains(const std::vector<String> &v, const String &s) {
+        return std::find(v.rbegin(), v.rend(), s) != v.rend();
+    }
+
+
+    void Preprocessor::LogError(ErrorCode error, const String& message) {
+        if(message == nullptr) {
+            switch (error) {
+                case ErrorCode::MacroNameMissing:
+                    cc_error("Macro name expected");
+                    break;
+                case ErrorCode::LineTooLong:
+                    cc_error("Line too long (max line length = " STRINGIFY(MAX_LINE_LENGTH) ")");
+                    break;
+                case ErrorCode::EndIfWithoutIf:
+                    cc_error("#endif has no matching #if");
+                    break;
+                case ErrorCode::IfWithoutEndIf:
+                    cc_error("Missing #endif");
+                    break;
+                case ErrorCode::MacroDoesNotExist:
+                case ErrorCode::MacroAlreadyExists:
+                case ErrorCode::MacroNameInvalid:
+                case ErrorCode::UnknownPreprocessorDirective:
+                case ErrorCode::UserDefinedError:
+                case ErrorCode::InvalidVersionNumber:
+                    break;
+                default:
+                    cc_error("Unknown preprocessor error"); // should never be reached
+                    break;
+            }
+        } else {
+            cc_error(message.GetCStr());
+        }
+    }
+
+    void Preprocessor::ProcessConditionalDirective(String &directive, String &line)
+    {
+        String macroName = GetNextWord(line, true, true);
+        if (macroName.GetLength() == 0)
+        {
+            LogError(ErrorCode::MacroNameMissing, String::FromFormat("Expected something after '%s'", directive.GetCStr()));
+            return;
+        }
+
+        bool includeCodeBlock = true;
+
+        if ((!_conditionalStatements.empty()) && !_conditionalStatements.back())
+        {
+            includeCodeBlock = false;
+        }
+        else if (directive.CompareRight("def") == 0)
+        {
+            includeCodeBlock = _macros.contains(macroName.GetCStr());
+            if (directive == "ifndef")
+            {
+                includeCodeBlock = !includeCodeBlock;
+            }
+        }
+        else if (directive == "ifver" || directive == "ifnver")
+        {
+            // Compare provided version number with the current application version
+            Version macroVersion = Version(macroName);
+            if(macroVersion.Major == 0) {
+                LogError(ErrorCode::InvalidVersionNumber, String::FromFormat("Cannot parse version number: %s", macroName.GetCStr()));
+            }
+            includeCodeBlock = _applicationVersion.AsLongNumber() >= macroVersion.AsLongNumber();
+            if(directive == "ifnver" )
+            {
+                includeCodeBlock = !includeCodeBlock;
+            }
+        }
+
+        _conditionalStatements.push_back(includeCodeBlock);
+    }
+
+    bool Preprocessor::DeletingCurrentLine()
+    {
+        return ((!_conditionalStatements.empty()) && !_conditionalStatements.back());
+    }
+
+    String Preprocessor::GetNextWord(String &text, bool trimText, bool includeDots) {
+        int i = 0;
+        while ((i < text.GetLength()) &&
+               (is_alphanum(text[i]) ||
+                (includeDots && (text[i] == '.')))
+                ) {
+            i++;
+        }
+        String word = text.Left(i);
+        text.ClipLeft(i);
+        if (trimText) {
+            text.Trim();
+        }
+        return word;
+    }
+
+    String Preprocessor::RemoveComments(String text)
+    {
+        if (_inMultiLineComment)
+        {
+            size_t commentEnd = text.FindString("*/",0);
+            if (commentEnd == NOT_FOUND)
+            {
+                return String("");
+            }
+            text = text.Mid(commentEnd + 2, text.GetLength() - (commentEnd + 2));
+            _inMultiLineComment = false;
+        }
+
+        StringBuilder output = StringBuilder(text.GetLength());
+        for (int i = 0; i < text.GetLength(); i++)
+        {
+            if (!_inMultiLineComment)
+            {
+                if ((text[i] == '"') || (text[i] == '\''))
+                {
+                    size_t endOfString = text.FindChar(text[i],i);
+                    if (endOfString == NOT_FOUND) //size_t is unsigned but it's alright
+                    {
+                        break;
+                    }
+                    endOfString++;
+                    output.WriteString(text.Mid(i, endOfString - i));
+                    text = text.Mid(endOfString, text.GetLength() - endOfString);
+                    i = -1;
+                }
+                else if ((i < text.GetLength() - 1) && (text[i] == '/') && (text[i + 1] == '/'))
+                {
+                    break;
+                }
+                else if ((i < text.GetLength() - 1) && (text[i] == '/') && (text[i + 1] == '*'))
+                {
+                    _inMultiLineComment = true;
+                    i++;
+                }
+                else
+                {
+                    output.WriteChar(text[i]);
+                }
+            }
+            else if ((i < text.GetLength() - 1) && (text[i] == '*') && (text[i + 1] == '/'))
+            {
+                _inMultiLineComment = false;
+                i++;
+            }
+        }
+
+        String out = output.GetString();
+        out.Trim();
+        return out;
+    }
+
+    String Preprocessor::PreProcessDirective(String &line)
+    {
+        line.ClipLeft(1);
+        String directive = GetNextWord(line);
+
+        if ((directive == "ifdef") || (directive == "ifndef") ||
+            (directive == "ifver") || (directive == "ifnver"))
+        {
+            ProcessConditionalDirective(directive, line);
+        }
+        else if (directive == "endif")
+        {
+            if (!_conditionalStatements.empty())
+            {
+                _conditionalStatements.pop_back();
+            }
+            else
+            {
+                LogError(ErrorCode::EndIfWithoutIf);
+            }
+        }
+        else if (DeletingCurrentLine())
+        {
+            // allow the line to be deleted, we are inside a failed #ifdef
+        }
+        else if (directive == "define")
+        {
+            String macroName = GetNextWord(line);
+            if (macroName.GetLength() == 0)
+            {
+                LogError(ErrorCode::MacroNameMissing);
+                return String("");
+            }
+            else if (is_digit(macroName[0]))
+            {
+                LogError(ErrorCode::MacroNameInvalid, String::FromFormat("Macro name '%s' cannot start with a digit", macroName.GetCStr()));
+            }
+            else if (_macros.contains(macroName.GetCStr()))
+            {
+                LogError(ErrorCode::MacroAlreadyExists, String::FromFormat("Macro '%s' is already defined", macroName.GetCStr()));
+            }
+            else
+            {
+                _macros.add(macroName, line);
+            }
+        }
+        else if (directive == "undef")
+        {
+            String macroName = GetNextWord(line);
+            if (macroName.GetLength() == 0)
+            {
+                LogError(ErrorCode::MacroNameMissing);
+            }
+            else if (!_macros.contains(macroName))
+            {
+                LogError(ErrorCode::MacroDoesNotExist, String::FromFormat("Macro '%s' is not defined", macroName.GetCStr()));
+            }
+            else
+            {
+                _macros.remove(macroName);
+            }
+        }
+        else if (directive == "error")
+        {
+            LogError(ErrorCode::UserDefinedError, String::FromFormat("User error: %s", line.GetCStr()));
+        }
+        else if ((directive == "sectionstart") || (directive == "sectionend"))
+        {
+            // do nothing -- 2.72 put these as markers in the script
+        }
+        else if ((directive == "region") || (directive == "endregion"))
+        {
+            // do nothing -- scintilla can fold it, so it can be used to organize the code
+        }
+        else
+        {
+            LogError(ErrorCode::UnknownPreprocessorDirective, String::FromFormat("Unknown preprocessor directive '%s'", directive.GetCStr()));
+        }
+
+        return String("");  // replace the directive with a blank line
+    }
+
+    void Preprocessor::DefineMacro(const String& name, const String& value)
+    {
+        _macros.add(name, value);
+    }
+
+    String Preprocessor::PreProcessLine(const String& lineToProcess)
+    {
+        if (DeletingCurrentLine())
+        {
+            return String("");
+        }
+
+        std::vector<StringBuilder> previousOutput = std::vector<StringBuilder>();
+        std::vector<String> previousLine = std::vector<String>();
+        StringBuilder output = StringBuilder(lineToProcess.GetLength());
+        String line = lineToProcess;
+        std::vector<String> ignored = std::vector<String>();
+        while (line.GetLength() > 0)
+        {
+            size_t i = 0;
+            while ((i < line.GetLength()) && (!is_alphanum(line[i])))
+            {
+                if ((line[i] == '"') || (line[i] == '\''))
+                {
+                    i = line.FindChar(line[i],i);
+                    if (i == NOT_FOUND)
+                    {
+                        i = line.GetLength();
+                        break;
+                    }
+                }
+                i++;
+            }
+
+            output.WriteString(line.Mid(0, i));
+
+            if (i < line.GetLength())
+            {
+                bool precededByDot = false;
+                if (i > 0) precededByDot = (line[i - 1] == '.');
+
+                line = line.Mid(i, line.GetLength() - i);
+
+                String theWord = GetNextWord(line, false, false);
+
+                if ((!precededByDot) && (!Contains(ignored, theWord)) && (_macros.contains(theWord.GetCStr())))
+                {
+                    previousOutput.push_back(output);
+                    previousLine.push_back(line);
+                    ignored.push_back(theWord);
+                    line = _macros.get_macro(theWord.GetCStr());
+                    output = StringBuilder(line.GetLength());
+                }
+                else
+                {
+                    output.WriteString(theWord);
+                }
+            }
+            else
+                line = "";
+
+            while (line.GetLength() == 0 && !previousOutput.empty())
+            {
+                String result = output.GetString();
+                output = previousOutput.back();
+                previousOutput.pop_back();
+                line = previousLine.back();
+                previousLine.pop_back();
+                ignored.pop_back();
+                output.WriteString(result);
+            }
+
+        }
+
+        return output.GetString();
+    }
+
+
+    String Preprocessor::Preprocess(const String& script, const String& scriptName)
+    {
+        StringBuilder output = StringBuilder(script.GetLength());
+        currentline = _lineNumber = 0;
+        output.WriteLine(String::FromFormat("%s%s\"", NEW_SCRIPT_TOKEN_PREFIX, scriptName.GetCStr()));
+        StringReader reader = StringReader(script);
+        _scriptName = scriptName;
+        while (reader.IsValid())
+        {
+            currentline = ++_lineNumber;
+            String thisLine = reader.ReadLine();
+            thisLine = RemoveComments(thisLine);
+            if (thisLine.GetLength() > 0)
+            {
+                if (thisLine[0] != '#')
+                {
+                    thisLine = PreProcessLine(thisLine);
+                }
+                else
+                {
+                    thisLine = PreProcessDirective(thisLine);
+                }
+            }
+            if (thisLine.GetLength() >= MAX_LINE_LENGTH)
+            {
+                // For compatibility with legacy CSPARSER, which assumes lines
+                // will not be longer than 500 chars. Remove once new compiler
+                // is implemented.
+                LogError(ErrorCode::LineTooLong);
+            }
+            output.WriteLine(thisLine);
+        }
+
+
+        if (!_conditionalStatements.empty()) // count > 0
+        {
+            LogError(ErrorCode::IfWithoutEndIf);
+        }
+
+        return output.GetString();
+    }
+
+    void Preprocessor::MergeMacros(MacroTable &macros) {
+        _macros.merge(macros);
+    }
+
+    void Preprocessor::SetAppVersion(const String &version) {
+        _applicationVersion = Version(version);
+    }
+
+    } // Preprocessor
+} // AGS
---
+++ Compiler/test/preprocessor_test.cpp
@@ -0,0 +1,408 @@
+#include "gtest/gtest.h"
+#include "preproc/preprocessor.h"
+#include <util/string_compat.h>
+#include "test/cc_test_helper.h"
+
+typedef AGS::Common::String AGSString;
+
+namespace AGS {
+namespace Preprocessor {
+
+
+std::vector<AGSString> SplitLines(const AGSString& str)
+{
+    std::vector<AGSString> str_lines = str.Split('\n');
+    for (int i = 0; i < str_lines.size(); i++) {
+        if (str_lines[i].CompareRight("\r", 1) == 0) {
+            str_lines[i].ClipRight(1);
+        }
+    }
+    return str_lines;
+}
+
+
+TEST(Preprocess, Comments) {
+    Preprocessor pp = Preprocessor();
+    const char* inpl = R"EOS(
+// this is a comment
+// this is another comment
+// 1234
+//#define invalid 5
+// invalid
+int i;
+)EOS";
+
+    clear_error();
+    String res = pp.Preprocess(inpl,"ScriptName");
+
+    EXPECT_STREQ(last_seen_cc_error(), "");
+
+    std::vector<AGSString> lines = SplitLines(res);
+    ASSERT_EQ(lines.size(),9);
+    ASSERT_STREQ(lines[0].GetCStr(), "\"__NEWSCRIPTSTART_ScriptName\"");
+    ASSERT_STREQ(lines[1].GetCStr(), "");
+    ASSERT_STREQ(lines[2].GetCStr(), "");
+    ASSERT_STREQ(lines[3].GetCStr(), "");
+    ASSERT_STREQ(lines[4].GetCStr(), "");
+    ASSERT_STREQ(lines[5].GetCStr(), "");
+    ASSERT_STREQ(lines[6].GetCStr(), "");
+    ASSERT_STREQ(lines[7].GetCStr(), "int i;");
+    ASSERT_STREQ(lines[8].GetCStr(), "");
+}
+
+
+TEST(Preprocess, MultilineComments) {
+    Preprocessor pp = Preprocessor();
+    // this test is verifying multiline comments
+    // and documents current behavior of Editor preprocessor
+    const char* inpl = R"EOS(
+int hey /* comment  */= 7;
+// this  
+"this prints";
+//;
+// /*
+Display("this does display!");
+// */
+/* this
+is a real
+multiline comment*/
+int k;
+)EOS";
+
+    clear_error();
+    String res = pp.Preprocess(inpl,"MultiLine");
+
+    EXPECT_STREQ(last_seen_cc_error(), "");
+
+    std::vector<AGSString> lines = SplitLines(res);
+    ASSERT_EQ(lines.size(),14);
+    ASSERT_STREQ(lines[0].GetCStr(), "\"__NEWSCRIPTSTART_MultiLine\"");
+    ASSERT_STREQ(lines[1].GetCStr(), "");
+    ASSERT_STREQ(lines[2].GetCStr(), "int hey = 7;");
+    ASSERT_STREQ(lines[3].GetCStr(), "");
+    ASSERT_STREQ(lines[4].GetCStr(), "\"this prints\";");
+    ASSERT_STREQ(lines[5].GetCStr(), "");
+    ASSERT_STREQ(lines[6].GetCStr(), "");
+    ASSERT_STREQ(lines[7].GetCStr(), "Display(\"this does display!\");");
+    ASSERT_STREQ(lines[8].GetCStr(), "");
+    ASSERT_STREQ(lines[9].GetCStr(), "");
+    ASSERT_STREQ(lines[10].GetCStr(), "");
+    ASSERT_STREQ(lines[11].GetCStr(), "");
+    ASSERT_STREQ(lines[12].GetCStr(),"int k;");
+}
+
+
+TEST(Preprocess, Define) {
+    Preprocessor pp = Preprocessor();
+    const char* inpl = R"EOS(
+#define MACRO2 MACRO3
+#define MACRO3 9
+#define TEST1 5
+Display("a: %d", TEST1);
+Display("b: TEST1");
+Display("d: %d", MACRO3);
+Display("e: %d", MACRO2);
+#define MACRO4 MACRO3
+Display("f: %d", MACRO4);
+)EOS";
+
+    clear_error();
+    String res = pp.Preprocess(inpl, "ScriptDefine");
+
+    EXPECT_STREQ(last_seen_cc_error(), "");
+
+    std::vector<AGSString> lines = SplitLines(res);
+    ASSERT_EQ(lines.size(), 12);
+    ASSERT_STREQ(lines[0].GetCStr(), "\"__NEWSCRIPTSTART_ScriptDefine\"");
+    ASSERT_STREQ(lines[1].GetCStr(), "");
+    ASSERT_STREQ(lines[2].GetCStr(), "");
+    ASSERT_STREQ(lines[3].GetCStr(), "");
+    ASSERT_STREQ(lines[4].GetCStr(), "");
+    ASSERT_STREQ(lines[5].GetCStr(), "Display(\"a: %d\", 5);");
+    ASSERT_STREQ(lines[6].GetCStr(), "Display(\"b: TEST1\");");
+    ASSERT_STREQ(lines[7].GetCStr(), "Display(\"d: %d\", 9);");
+    ASSERT_STREQ(lines[8].GetCStr(), "Display(\"e: %d\", 9);");
+    ASSERT_STREQ(lines[9].GetCStr(), "");
+    ASSERT_STREQ(lines[10].GetCStr(), "Display(\"f: %d\", 9);");
+    ASSERT_STREQ(lines[11].GetCStr(), "");
+}
+
+
+TEST(Preprocess, ReDefine) {
+    Preprocessor pp = Preprocessor();
+    const char* inpl = R"EOS(
+#define TEST1 5
+Display("a: %d", TEST1);
+#define TEST1 10
+Display("c: %d", TEST1);
+)EOS";
+
+    clear_error();
+    String res = pp.Preprocess(inpl, "ScriptReDefine");
+
+    EXPECT_STREQ(last_seen_cc_error(), "Macro 'TEST1' is already defined");
+}
+
+
+TEST(Preprocess, MacroDoesNotExist) {
+    Preprocessor pp = Preprocessor();
+    const char* inpl = R"EOS(
+#define BAR
+#undef FOO
+)EOS";
+
+    clear_error();
+    String res = pp.Preprocess(inpl, "MacroDoesNotExist");
+
+    EXPECT_STREQ(last_seen_cc_error(), "Macro 'FOO' is not defined");
+}
+
+
+TEST(Preprocess, MacroNameMissing) {
+    Preprocessor pp = Preprocessor();
+        const char* inpl = R"EOS(
+#define BAR
+#undef
+)EOS";
+
+    clear_error();
+    String res = pp.Preprocess(inpl, "MacroNameMissing");
+
+    EXPECT_STREQ(last_seen_cc_error(), "Macro name expected");
+}
+
+
+TEST(Preprocess, MacroStartsWithDigit) {
+    Preprocessor pp = Preprocessor();
+    const char* inpl = R"EOS(
+#define 1BAR
+)EOS";
+
+    clear_error();
+    String res = pp.Preprocess(inpl, "MacroStartsWithDigit");
+
+    EXPECT_STREQ(last_seen_cc_error(), "Macro name '1BAR' cannot start with a digit");
+}
+
+
+TEST(Preprocess, UserError) {
+    Preprocessor pp = Preprocessor();
+    const char* inpl = R"EOS(
+#error my error here
+Display("this doesn't display");
+)EOS";
+
+    clear_error();
+    String res = pp.Preprocess(inpl, "ScriptUserError");
+
+    EXPECT_STREQ(last_seen_cc_error(), "User error: my error here");
+}
+
+
+TEST(Preprocess, LineTooLong) {
+    Preprocessor pp = Preprocessor();
+    const char* inpl = R"EOS(
+"this message has more than 500 characters. I will throw a poem here: He with body waged a fight, But body won; it walks upright. Then he struggled with the heart; Innocence and peace depart. Then he struggled with the mind; His proud heart he left behind. Now his wars on God begin; At stroke of midnight God shall win. The poem has now ended and we still have more characters to go... 1/2 cup butter, cubed; 6 medium unpeeled tart red apples, sliced; 3/4 cup sugar, divided; 3/4 teaspoon ground cinnamon; Vanilla ice cream, optional; throw all of these in the oven for at least 20 minutes. End of long text";
+)EOS";
+
+    clear_error();
+    String res = pp.Preprocess(inpl, "ScriptLineTooLong");
+
+    EXPECT_STREQ(last_seen_cc_error(), "Line too long (max line length = 500)");
+}
+
+
+TEST(Preprocess, RemoveEditorDirectives) {
+    Preprocessor pp = Preprocessor();
+    const char* inpl = R"EOS(
+#region THIS_IS_DISPLAY
+Display("Prints normally");
+#endregion THIS_IS_DISPLAY
+#sectionstart
+Display("Prints normally too");
+#sectionend
+)EOS";
+
+    clear_error();
+    String res = pp.Preprocess(inpl, "RemoveEditorDirectives");
+
+    EXPECT_STREQ(last_seen_cc_error(), "");
+
+    std::vector<AGSString> lines = SplitLines(res);
+    ASSERT_EQ(lines.size(), 9);
+    ASSERT_STREQ(lines[0].GetCStr(), "\"__NEWSCRIPTSTART_RemoveEditorDirectives\"");
+    ASSERT_STREQ(lines[1].GetCStr(), "");
+    ASSERT_STREQ(lines[2].GetCStr(), "");
+    ASSERT_STREQ(lines[3].GetCStr(), "Display(\"Prints normally\");");
+    ASSERT_STREQ(lines[4].GetCStr(), "");
+    ASSERT_STREQ(lines[5].GetCStr(), "");
+    ASSERT_STREQ(lines[6].GetCStr(), "Display(\"Prints normally too\");");
+    ASSERT_STREQ(lines[7].GetCStr(), "");
+}
+
+
+TEST(Preprocess, UnknownDirective) {
+    Preprocessor pp = Preprocessor();
+    const char* inpl = R"EOS(
+#valhalla
+)EOS";
+
+    clear_error();
+    String res = pp.Preprocess(inpl, "UnknownDirective");
+
+    EXPECT_STREQ(last_seen_cc_error(), "Unknown preprocessor directive 'valhalla'");
+}
+
+
+TEST(Preprocess, IfDef) {
+    Preprocessor pp = Preprocessor();
+    const char* inpl = R"EOS(
+#define FOO
+#ifdef FOO
+Display("This displays!");
+#endif
+#undef FOO
+#ifdef FOO
+Display("This doesn't");
+#endif
+#ifdef BAR
+Display("This doesn't too");
+#endif
+#define BAR
+#ifdef BAR
+Display("This displays dude");
+Display("and this too");
+#endif
+#ifndef BAR
+Display("This doesn't");
+#endif
+#ifndef BORK
+Display("This does");
+#endif
+)EOS";
+
+    clear_error();
+    String res = pp.Preprocess(inpl, "ScriptIfDef");
+
+    EXPECT_STREQ(last_seen_cc_error(), "");
+
+    std::vector<AGSString> lines = SplitLines(res);
+    ASSERT_EQ(lines.size(), 25);
+
+    ASSERT_STREQ(lines[0].GetCStr(), "\"__NEWSCRIPTSTART_ScriptIfDef\"");
+    ASSERT_STREQ(lines[1].GetCStr(), "");
+    ASSERT_STREQ(lines[2].GetCStr(), "");
+    ASSERT_STREQ(lines[3].GetCStr(), "");
+    ASSERT_STREQ(lines[4].GetCStr(), "Display(\"This displays!\");");
+    ASSERT_STREQ(lines[5].GetCStr(), "");
+    ASSERT_STREQ(lines[6].GetCStr(), "");
+    ASSERT_STREQ(lines[7].GetCStr(), "");
+    ASSERT_STREQ(lines[8].GetCStr(), "");
+    ASSERT_STREQ(lines[9].GetCStr(), "");
+    ASSERT_STREQ(lines[10].GetCStr(), "");
+    ASSERT_STREQ(lines[11].GetCStr(), "");
+    ASSERT_STREQ(lines[12].GetCStr(), "");
+    ASSERT_STREQ(lines[13].GetCStr(), "");
+    ASSERT_STREQ(lines[14].GetCStr(), "");
+    ASSERT_STREQ(lines[15].GetCStr(), "Display(\"This displays dude\");");
+    ASSERT_STREQ(lines[16].GetCStr(), "Display(\"and this too\");");
+    ASSERT_STREQ(lines[17].GetCStr(), "");
+    ASSERT_STREQ(lines[18].GetCStr(), "");
+    ASSERT_STREQ(lines[19].GetCStr(), "");
+    ASSERT_STREQ(lines[20].GetCStr(), "");
+    ASSERT_STREQ(lines[21].GetCStr(), "");
+    ASSERT_STREQ(lines[22].GetCStr(), "Display(\"This does\");");
+    ASSERT_STREQ(lines[23].GetCStr(), "");
+}
+
+
+TEST(Preprocess, IfVer) {
+    Preprocessor pp = Preprocessor();
+    pp.SetAppVersion("3.6.0.5");
+    const char* inpl = R"EOS(
+#ifver 3.6.0
+Display("This displays!");
+#endif
+#ifver 3.7.0
+Display("This doesn't");
+#endif
+#ifver 3.4.0
+Display("This displays yey!");
+#endif
+#ifver 2.7.2
+Display("This displays dude");
+#endif
+#ifver 3.6.0.6
+Display("doesn't display");
+#endif
+#ifnver 3.6.0.6
+Display("But this displays");
+#endif
+)EOS";
+
+    clear_error();
+    String res = pp.Preprocess(inpl, "ScriptIfVer");
+
+    EXPECT_STREQ(last_seen_cc_error(), "");
+
+    std::vector<AGSString> lines = SplitLines(res);
+    ASSERT_EQ(lines.size(), 21);
+
+    ASSERT_STREQ(lines[0].GetCStr(), "\"__NEWSCRIPTSTART_ScriptIfVer\"");
+    ASSERT_STREQ(lines[1].GetCStr(), "");
+    ASSERT_STREQ(lines[2].GetCStr(), "");
+    ASSERT_STREQ(lines[3].GetCStr(), "Display(\"This displays!\");");
+    ASSERT_STREQ(lines[4].GetCStr(), "");
+    ASSERT_STREQ(lines[5].GetCStr(), "");
+    ASSERT_STREQ(lines[6].GetCStr(), "");
+    ASSERT_STREQ(lines[7].GetCStr(), "");
+    ASSERT_STREQ(lines[8].GetCStr(), "");
+    ASSERT_STREQ(lines[9].GetCStr(), "Display(\"This displays yey!\");");
+    ASSERT_STREQ(lines[10].GetCStr(), "");
+    ASSERT_STREQ(lines[11].GetCStr(), "");
+    ASSERT_STREQ(lines[12].GetCStr(), "Display(\"This displays dude\");");
+    ASSERT_STREQ(lines[13].GetCStr(), "");
+    ASSERT_STREQ(lines[14].GetCStr(), "");
+    ASSERT_STREQ(lines[15].GetCStr(), "");
+    ASSERT_STREQ(lines[16].GetCStr(), "");
+    ASSERT_STREQ(lines[17].GetCStr(), "");
+    ASSERT_STREQ(lines[18].GetCStr(), "Display(\"But this displays\");");
+    ASSERT_STREQ(lines[19].GetCStr(), "");
+    ASSERT_STREQ(lines[20].GetCStr(), "");
+}
+
+
+TEST(Preprocess, IfWithoutEndIf) {
+    Preprocessor pp = Preprocessor();
+    const char* inpl = R"EOS(
+#ifdef BAR
+#endif
+#ifdef FOO
+Display("test");
+)EOS";
+
+    clear_error();
+    String res = pp.Preprocess(inpl, "IfWithoutEndIf");
+
+    EXPECT_STREQ(last_seen_cc_error(), "Missing #endif");
+}
+
+
+TEST(Preprocess, EndIfWithoutIf) {
+    Preprocessor pp = Preprocessor();
+    const char* inpl = R"EOS(
+#ifdef BAR
+#endif
+Display("test");
+#endif
+)EOS";
+
+    clear_error();
+    String res = pp.Preprocess(inpl, "EndIfWithoutIf");
+
+    EXPECT_STREQ(last_seen_cc_error(), "#endif has no matching #if");
+}
+
+
+} // Preprocessor
+} // AGS

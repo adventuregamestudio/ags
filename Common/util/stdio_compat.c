adventuregamestudio/ags

commit 5569e1c0617e6ca34491f84ddef4d34f60b7dbf7

stdio/file: less platform header idefs

Common/util/stdio_compat.c
@@ -1,0 +1,94 @@
+//=============================================================================
+//
+// Adventure Game Studio (AGS)
+//
+// Copyright (C) 1999-2011 Chris Jones and 2011-20xx others
+// The full list of copyright holders can be found in the Copyright.txt
+// file, which is part of this source code distribution.
+//
+// The AGS source code is provided under the Artistic License 2.0.
+// A copy of this license can be found in the file License.txt and at
+// http://www.opensource.org/licenses/artistic-license-2.0.php
+//
+//=============================================================================
+
+#include "util/stdio_compat.h"
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#ifdef WINDOWS_VERSION
+#include <windows.h>
+#include <shlwapi.h>
+#endif
+
+int	 ags_fseek(FILE * stream, file_off_t offset, int whence)
+{
+#if defined(HAVE_FSEEKO) // Contemporary POSIX libc
+    return fseeko(stream, offset, whence);
+#elif WINDOWS_VERSION // MSVC
+    return _fseeki64(stream, offset, whence); 
+#else // No distinct interface with off_t
+    return fseek(stream, offset, whence);
+#endif
+}
+
+file_off_t ags_ftell(FILE * stream) 
+{
+    #if defined(HAVE_FSEEKO) // Contemporary POSIX libc
+        return ftello(stream);
+    #elif WINDOWS_VERSION // MSVC
+        return _ftelli64(stream); 
+    #else // No distinct interface with off_t
+        return ftell(stream);
+    #endif
+}
+
+int  ags_file_exists(const char *path) 
+{
+#if WINDOWS_VERSION
+    return PathFileExistsA(path);
+#else
+    struct stat path_stat;
+    if (stat(path, &path_stat) != 0) {
+        return 0;
+    }
+    return S_ISREG(path_stat.st_mode);
+#endif
+}
+
+int ags_directory_exists(const char *path)
+{
+#if defined(WINDOWS_VERSION)
+    return PathIsDirectoryA(path);
+#else
+    struct stat path_stat;
+    if (stat(path, &path_stat) != 0) {
+        return 0;
+    }
+    return S_ISDIR(path_stat.st_mode);
+#endif
+}
+
+int ags_path_exists(const char *path)
+{
+    #if defined(WINDOWS_VERSION)
+        return PathIsDirectoryA(path);
+    #else
+        struct stat path_stat;
+        if (stat(path, &path_stat) != 0) {
+            return 0;
+        }
+        return S_ISREG(path_stat.st_mode) || S_ISDIR(path_stat.st_mode);
+    #endif
+}
+
+file_off_t ags_file_size(const char *path)
+{
+    struct stat path_stat;
+    if (stat(path, &path_stat) != 0) {
+        return -1;
+    }
+    return path_stat.st_size;
+}

Common/CMakeLists.txt
@@ -138,6 +138,7 @@ target_sources(common
     util/path.h
     util/proxystream.cpp
     util/proxystream.h
+    util/stdio_compat.c
     util/stdio_compat.h
     util/stream.cpp
     util/stream.h

Common/ac/spritecache.cpp
@@ -741,7 +741,7 @@ HError SpriteCache::InitFile(const char *filnam)
 
     // failed, delete the index file because it's invalid
     // TODO: refactor loading process and make it NOT delete file running the game!!
-    unlink(spindexfilename);
+    ::remove(spindexfilename);
 
     return RebuildSpriteIndex(_stream.get(), topmost, vers);
 }

Common/util/compress.cpp
@@ -18,6 +18,7 @@
 #endif
 
 #include <stdlib.h>
+#include <stdio.h>
 #include "ac/common.h"	// quit, update_polled_stuff
 #include "gfx/bitmap.h"
 #include "util/compress.h"
@@ -310,7 +311,7 @@ void save_lzw(Stream *out, const Bitmap *bmpp, const color *pall)
 
   // Delete temp file
   delete lz_temp_s;
-  unlink(lztempfnm);
+  ::remove(lztempfnm);
 
   // Seek back to the end of the output stream
   out->Seek(toret, kSeekBegin);

Common/util/file.cpp
@@ -12,11 +12,9 @@
 //
 //=============================================================================
 
-#if defined(WINDOWS_VERSION)
-#include <io.h>
-#else
-#include <unistd.h> // for unlink()
-#endif
+#include "util/file.h"
+
+#include "util/stdio_compat.h"
 #include <errno.h>
 #include "util/filestream.h"
 
@@ -27,10 +25,7 @@ namespace Common
 
 soff_t File::GetFileSize(const String &filename)
 {
-    struct stat_t st;
-    if (stat_fn(filename, &st) == 0)
-        return st.st_size;
-    return -1;
+    return ags_file_size(filename.GetCStr());
 }
 
 bool File::TestReadFile(const String &filename)
@@ -61,7 +56,7 @@ bool File::TestCreateFile(const String &filename)
     if (test_file)
     {
         fclose(test_file);
-        unlink(filename);
+        ::remove(filename);
         return true;
     }
     return false;
@@ -69,7 +64,7 @@ bool File::TestCreateFile(const String &filename)
 
 bool File::DeleteFile(const String &filename)
 {
-    if (unlink(filename) != 0)
+    if (::remove(filename) != 0)
     {
         int err;
 #if defined(WINDOWS_VERSION)

Common/util/filestream.cpp
@@ -14,6 +14,9 @@
 
 #include "util/filestream.h"
 
+#include "util/stdio_compat.h"
+#include "util/string.h"
+
 namespace AGS
 {
 namespace Common
@@ -71,10 +74,10 @@ soff_t FileStream::GetLength() const
 {
     if (IsValid())
     {
-        soff_t pos = (soff_t)ftell(_file);
-        fseek(_file, 0, SEEK_END);
-        soff_t end = (soff_t)ftell(_file);
-        fseek(_file, pos, SEEK_SET);
+        soff_t pos = (soff_t)ags_ftell(_file);
+        ags_fseek(_file, 0, SEEK_END);
+        soff_t end = (soff_t)ags_ftell(_file);
+        ags_fseek(_file, pos, SEEK_SET);
         return end;
     }
 
@@ -85,7 +88,7 @@ soff_t FileStream::GetPosition() const
 {
     if (IsValid())
     {
-        return (soff_t) ftell(_file);
+        return (soff_t) ags_ftell(_file);
     }
     return -1;
 }
@@ -159,7 +162,7 @@ soff_t FileStream::Seek(soff_t offset, StreamSeek origin)
         return -1;
     }
 
-    fseek(_file, (file_off_t)offset, stdclib_origin);
+    ags_fseek(_file, (file_off_t)offset, stdclib_origin);
     return GetPosition();
 }
 

Common/util/filestream.h
@@ -18,9 +18,10 @@
 #ifndef __AGS_CN_UTIL__FILESTREAM_H
 #define __AGS_CN_UTIL__FILESTREAM_H
 
+#include <stdio.h>
+
 #include "util/datastream.h"
 #include "util/file.h" // TODO: extract filestream mode constants
-#include "util/stdio_compat.h"
 
 namespace AGS
 {

Common/util/lzw.cpp
@@ -27,10 +27,6 @@ using namespace AGS::Common;
 #pragma unmanaged
 #endif
 
-#if defined (WINDOWS_VERSION)
-#include <io.h>
-#endif
-
 int insert(int, int);
 void _delete(int);
 

commit 557be45f3600147da8a640a5ed086b3d97fea1f5

Common: fixed ags_file_size() not working with 64-bit file sizes on Win
Was broken by 5569e1c0617e6ca34491f84ddef4d34f60b7dbf7

Common/util/stdio_compat.c
@@ -88,9 +88,17 @@ int ags_path_exists(const char *path)
 
 file_off_t ags_file_size(const char *path)
 {
+#if AGS_PLATFORM_OS_WINDOWS
+    struct _stat64 path_stat;
+    if (_stati64(path, &path_stat) != 0) {
+        return -1;
+    }
+    return path_stat.st_size;
+#else
     struct stat path_stat;
     if (stat(path, &path_stat) != 0) {
         return -1;
     }
     return path_stat.st_size;
+#endif
 }
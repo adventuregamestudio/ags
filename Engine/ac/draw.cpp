adventuregamestudio/ags

commit d7c091890d2cde07776cca4ebbed7352d379612d

Engine: distinct overlay types of speech and messagebox
Also moved couple of global variables to GameState struct.

Engine/ac/draw.cpp
@@ -107,7 +107,6 @@ extern int displayed_room;
 extern CharacterExtras *charextra;
 extern CharacterInfo*playerchar;
 extern int eip_guinum;
-extern int is_complete_overlay;
 extern int cur_mode,cur_cursor;
 extern int mouse_frame,mouse_delay;
 extern int lastmx,lastmy;
@@ -2387,7 +2386,7 @@ void construct_game_scene(bool full_redraw)
         play.UpdateRoomCameras();
 
     // Stage: room viewports
-    if (play.screen_is_faded_out == 0 && is_complete_overlay == 0)
+    if (play.screen_is_faded_out == 0 && play.complete_overlay_on == 0)
     {
         if (displayed_room >= 0)
         {

Engine/ac/character.cpp
@@ -74,7 +74,6 @@ extern SpriteCache spriteset;
 extern Bitmap *walkable_areas_temp;
 extern IGraphicsDriver *gfxDriver;
 extern Bitmap **actsps;
-extern int is_text_overlay;
 extern int said_speech_line;
 extern int said_text;
 extern int our_eip;
@@ -2362,7 +2361,7 @@ void _displayspeech(const char*texx, int aschar, int xx, int yy, int widd, int i
     if ((speakingChar->view < 0) || (speakingChar->view >= game.numviews))
         quit("!DisplaySpeech: character has invalid view");
 
-    if (is_text_overlay > 0)
+    if (play.text_overlay_on > 0)
     {
         debug_script_warn("DisplaySpeech: speech was already displayed (nested DisplaySpeech, perhaps room script and global script conflict?)");
         return;

Engine/ac/display.cpp
@@ -164,7 +164,7 @@ int _display_main(int xx, int yy, int wii, const char *text, int disp_type, int 
     int extraHeight = paddingDoubledScaled;
     color_t text_color = MakeColor(15);
     if (disp_type < DISPLAYTEXT_NORMALOVERLAY)
-        remove_screen_overlay(OVER_TEXTMSG); // remove any previous blocking texts
+        remove_screen_overlay(play.text_overlay_on); // remove any previous blocking texts
 
     const int bmp_width = std::max(2, wii);
     const int bmp_height = std::max(2, disp.fulltxtheight + extraHeight);
@@ -243,9 +243,14 @@ int _display_main(int xx, int yy, int wii, const char *text, int disp_type, int 
             wouttext_aligned (text_window_ds, xoffs, yoffs + ee * disp.linespacing, oriwid, usingfont, text_color, Lines[ee].GetCStr(), play.text_align);
     }
 
-    int ovrtype = OVER_TEXTMSG;
-    if (disp_type == DISPLAYTEXT_NORMALOVERLAY) ovrtype=OVER_CUSTOM;
-    else if (disp_type >= OVER_CUSTOM) ovrtype = disp_type;
+    int ovrtype;
+    switch (disp_type)
+    {
+    case DISPLAYTEXT_SPEECH: ovrtype = OVER_TEXTSPEECH; break;
+    case DISPLAYTEXT_MESSAGEBOX: ovrtype = OVER_TEXTMSG; break;
+    case DISPLAYTEXT_NORMALOVERLAY: ovrtype = OVER_CUSTOM; break;
+    default: ovrtype = disp_type; break; // must be precreated overlay id
+    }
 
     int nse = add_screen_overlay(xx, yy, ovrtype, text_window_ds, adjustedXX - xx, adjustedYY - yy, alphaChannel);
     // we should not delete text_window_ds here, because it is now owned by Overlay
@@ -326,7 +331,7 @@ int _display_main(int xx, int yy, int wii, const char *text, int disp_type, int 
         remove_screen_overlay(OVER_TEXTMSG);
         invalidate_screen();
     }
-    else {
+    else { /* DISPLAYTEXT_SPEECH */
         // if the speech does not time out, but we are skipping a cutscene,
         // allow it to time out
         if ((play.messagetime < 0) && (play.fast_forward))

Engine/ac/display.h
@@ -23,8 +23,11 @@
 using AGS::Common::GUIMain;
 
 // options for 'disp_type' parameter
+// blocking speech
 #define DISPLAYTEXT_SPEECH        0
+// super-blocking message box
 #define DISPLAYTEXT_MESSAGEBOX    1
+// regular non-blocking overlay
 #define DISPLAYTEXT_NORMALOVERLAY 2
 // also accepts explicit overlay ID >= OVER_CUSTOM
 

Engine/ac/game.cpp
@@ -1252,8 +1252,8 @@ void start_skipping_cutscene () {
         remove_popup_interface(ifacepopped);
 
     // if a text message is currently displayed, remove it
-    if (is_text_overlay > 0)
-        remove_screen_overlay(OVER_TEXTMSG);
+    if (play.text_overlay_on > 0)
+        remove_screen_overlay(play.text_overlay_on);
 
 }
 

Engine/ac/gamestate.h
@@ -235,6 +235,13 @@ struct GameState {
     // Tells whether character speech stays on screen not animated for additional time
     bool  speech_in_post_state;
 
+    // Special overlays
+    //
+    // Is there a QFG4-style dialog overlay on screen (contains overlay ID)
+    int  complete_overlay_on;
+    // Is there a blocking text overlay on screen (contains overlay ID)
+    int  text_overlay_on;
+
     int shake_screen_yoff; // y offset of the shaking screen
 
 

Engine/ac/gui.cpp
@@ -597,8 +597,6 @@ void recreate_guibg_image(GUIMain *tehgui)
   }
 }
 
-extern int is_complete_overlay;
-
 int gui_get_interactable(int x,int y)
 {
     if ((game.options[OPT_DISABLEOFF]==3) && (all_buttons_disabled > 0))
@@ -620,7 +618,7 @@ int gui_on_mouse_move()
             if (guis[guin].IsInteractableAt(mousex, mousey)) mouse_over_gui=guin;
 
             if (guis[guin].PopupStyle!=kGUIPopupMouseY) continue;
-            if (is_complete_overlay>0) break;  // interfaces disabled
+            if (play.complete_overlay_on > 0) break;  // interfaces disabled
             //    if (play.disabled_user_interface>0) break;
             if (ifacepopped==guin) continue;
             if (!guis[guin].IsVisible()) continue;

Engine/ac/overlay.cpp
@@ -44,7 +44,6 @@ extern IGraphicsDriver *gfxDriver;
 
 ScreenOverlay screenover[MAX_SCREEN_OVERLAYS];
 int numscreenover=0;
-int is_complete_overlay=0,is_text_overlay=0;
 
 void Overlay_Remove(ScriptOverlay *sco) {
     sco->Remove();
@@ -161,8 +160,8 @@ void remove_screen_overlay_index(int over_idx)
 {
     ScreenOverlay &over = screenover[over_idx];
     dispose_overlay(over);
-    if (over.type==OVER_COMPLETE) is_complete_overlay--;
-    if (over.type==OVER_TEXTMSG) is_text_overlay--;
+    if (over.type == play.complete_overlay_on) play.complete_overlay_on = 0;
+    if (over.type == play.text_overlay_on) play.text_overlay_on = 0;
     numscreenover--;
     for (int i = over_idx; i < numscreenover; ++i)
         screenover[i] = screenover[i + 1];
@@ -199,9 +198,9 @@ int add_screen_overlay(int x, int y, int type, Bitmap *piccy, bool alphaChannel)
 
 int add_screen_overlay(int x, int y, int type, Common::Bitmap *piccy, int pic_offx, int pic_offy, bool alphaChannel)
 {
-    if (type==OVER_COMPLETE) is_complete_overlay++;
-    if (type==OVER_TEXTMSG) is_text_overlay++;
-    if (type==OVER_CUSTOM) {
+    if (type == OVER_COMPLETE) play.complete_overlay_on = type;
+    if (type == OVER_TEXTMSG || type == OVER_TEXTSPEECH) play.text_overlay_on = type;
+    if (type == OVER_CUSTOM) {
         // find an unused custom ID; TODO: find a better approach!
         for (int id = OVER_CUSTOM + 1; id < OVER_CUSTOM + 100; ++id) {
             if (find_overlay_of_type(id) == -1) { type=id; break; }

commit d651448d16fd3a72d5ede24ee6ad4c87a7d3d7d1

Engine: fixed speech overlays drawn behind GUI
Was broken by d7c091890d2cde07776cca4ebbed7352d379612d

Engine/ac/draw.cpp
@@ -2209,6 +2209,11 @@ void draw_fps(const Rect &viewport)
     invalidate_sprite_glob(1, yp, ddb);
 }
 
+static inline bool is_over_above_gui(int type)
+{
+    return type == OVER_TEXTMSG || type == OVER_PICTURE || type == OVER_TEXTSPEECH;
+}
+
 // Draw GUI and overlays of all kinds, anything outside the room space
 void draw_gui_and_overlays()
 {
@@ -2220,7 +2225,7 @@ void draw_gui_and_overlays()
         // complete overlay draw in non-transparent mode
         if (over.type == OVER_COMPLETE)
             add_thing_to_draw(over.bmp, over.x, over.y, TRANS_OPAQUE, false);
-        else if (over.type != OVER_TEXTMSG && over.type != OVER_PICTURE) {
+        else if (!is_over_above_gui(over.type)) {
             int tdxp, tdyp;
             get_overlay_position(over, &tdxp, &tdyp);
             add_thing_to_draw(over.bmp, tdxp, tdyp, 0, over.hasAlphaChannel);
@@ -2306,7 +2311,7 @@ void draw_gui_and_overlays()
     // draw speech and portraits (so that they appear over GUIs)
     for (const auto &over : screenover) 
     {
-        if (over.type == OVER_TEXTMSG || over.type == OVER_PICTURE)
+        if (is_over_above_gui(over.type))
         {
             int tdxp, tdyp;
             get_overlay_position(over, &tdxp, &tdyp);
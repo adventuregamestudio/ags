adventuregamestudio/ags

commit 2132ba833b9d4508ed3f81a65f531b88f6087cb5

Engine: simplified use of ReplaceBitmapWithSupportedFormat()
* ReplaceBitmapWithSupportedFormat() no longer deletes old bitmap, leaves that for the caller.
* don't call ReplaceBitmapWithSupportedFormat() right after creating a new bitmap, create bitmap already of a proper format instead.
* simplified PrepareSpriteForUse(), moved most code into AdjustBitmapForUseWithDisplayMode().
* along with above, fixed a potential error in shared_ptr variant of PrepareSpriteForUse(), where it could delete a shared bitmap.
* also use PrepareSpriteForUse() when loading dynamic sprites from file, for consistency.

Engine/ac/draw.cpp
@@ -240,6 +240,12 @@ Bitmap *AdjustBitmapForUseWithDisplayMode(Bitmap* bitmap, bool has_alpha)
     const int bmp_col_depth = bitmap->GetColorDepth();
     const int sys_col_depth = System_GetColorDepth();
     const int game_col_depth = game.GetColorDepth();
+    const int compat_col_depth = gfxDriver->GetCompatibleBitmapFormat(game_col_depth);
+
+    const bool must_switch_palette = bitmap->GetColorDepth() == 8 && game_col_depth > 8;
+    if (must_switch_palette)
+        select_palette(palette);
+
     Bitmap *new_bitmap = bitmap;
 
     //
@@ -270,7 +276,7 @@ Bitmap *AdjustBitmapForUseWithDisplayMode(Bitmap* bitmap, bool has_alpha)
     // In 32-bit game hicolor bitmaps must be converted to the true color
     else if (game_col_depth == 32 && (bmp_col_depth > 8 && bmp_col_depth <= 16))
     {
-        new_bitmap = BitmapHelper::CreateBitmapCopy(bitmap, game_col_depth);
+        new_bitmap = BitmapHelper::CreateBitmapCopy(bitmap, compat_col_depth);
     }
     // In non-32-bit game truecolor bitmaps must be downgraded
     else if (game_col_depth <= 16 && bmp_col_depth > 16)
@@ -278,51 +284,46 @@ Bitmap *AdjustBitmapForUseWithDisplayMode(Bitmap* bitmap, bool has_alpha)
         if (has_alpha) // if has valid alpha channel, convert it to regular transparency mask
             new_bitmap = remove_alpha_channel(bitmap);
         else // else simply convert bitmap
-            new_bitmap = BitmapHelper::CreateBitmapCopy(bitmap, game_col_depth);
+            new_bitmap = BitmapHelper::CreateBitmapCopy(bitmap, compat_col_depth);
     }
     // Special case when we must convert 16-bit RGB to BGR
     else if (convert_16bit_bgr == 1 && bmp_col_depth == 16)
     {
         new_bitmap = convert_16_to_16bgr(bitmap);
     }
+    
+    // Finally, if we did not create a new copy already, - convert to driver compatible format
+    if ((new_bitmap == bitmap) && (bmp_col_depth != compat_col_depth))
+        new_bitmap = GfxUtil::ConvertBitmap(bitmap, compat_col_depth);
+
+    if (must_switch_palette)
+        unselect_palette();
+
     return new_bitmap;
+}
+
+Bitmap *CreateCompatBitmap(int width, int height, int col_depth)
+{
+    return new Bitmap(width, height,
+        gfxDriver->GetCompatibleBitmapFormat(col_depth == 0 ? game.GetColorDepth() : col_depth));
 }
 
 Bitmap *ReplaceBitmapWithSupportedFormat(Bitmap *bitmap)
 {
-    Bitmap *new_bitmap = GfxUtil::ConvertBitmap(bitmap, gfxDriver->GetCompatibleBitmapFormat(bitmap->GetColorDepth()));
-    if (new_bitmap != bitmap)
-        delete bitmap;
-    return new_bitmap;
+    return GfxUtil::ConvertBitmap(bitmap, gfxDriver->GetCompatibleBitmapFormat(bitmap->GetColorDepth()));
 }
 
 Bitmap *PrepareSpriteForUse(Bitmap* bitmap, bool has_alpha)
 {
-    bool must_switch_palette = bitmap->GetColorDepth() == 8 && game.GetColorDepth() > 8;
-    if (must_switch_palette)
-        select_palette(palette);
-
     Bitmap *new_bitmap = AdjustBitmapForUseWithDisplayMode(bitmap, has_alpha);
     if (new_bitmap != bitmap)
         delete bitmap;
-    new_bitmap = ReplaceBitmapWithSupportedFormat(new_bitmap);
-
-    if (must_switch_palette)
-        unselect_palette();
     return new_bitmap;
 }
 
 PBitmap PrepareSpriteForUse(PBitmap bitmap, bool has_alpha)
 {
-    bool must_switch_palette = bitmap->GetColorDepth() == 8 && System_GetColorDepth() > 8;
-    if (must_switch_palette)
-        select_palette(palette);
-
     Bitmap *new_bitmap = AdjustBitmapForUseWithDisplayMode(bitmap.get(), has_alpha);
-    new_bitmap = ReplaceBitmapWithSupportedFormat(new_bitmap);
-
-    if (must_switch_palette)
-        unselect_palette();
     return new_bitmap == bitmap.get() ? bitmap : PBitmap(new_bitmap); // if bitmap is same, don't create new smart ptr!
 }
 
@@ -461,8 +462,7 @@ void create_blank_image(int coldepth)
     // so it's the most likey place for a crash
     try
     {
-        Bitmap *blank = BitmapHelper::CreateBitmap(16, 16, coldepth);
-        blank = ReplaceBitmapWithSupportedFormat(blank);
+        Bitmap *blank = CreateCompatBitmap(16, 16, coldepth);
         blank->Clear();
         blankImage = gfxDriver->CreateDDBFromBitmap(blank, false, true);
         blankSidebarImage = gfxDriver->CreateDDBFromBitmap(blank, false, true);
@@ -1239,8 +1239,7 @@ void recreate_guibg_image(GUIMain *tehgui)
 {
     int ifn = tehgui->ID;
     delete guibg[ifn];
-    guibg[ifn] = BitmapHelper::CreateBitmap(tehgui->Width, tehgui->Height, game.GetColorDepth());
-    guibg[ifn] = ReplaceBitmapWithSupportedFormat(guibg[ifn]);
+    guibg[ifn] = CreateCompatBitmap(tehgui->Width, tehgui->Height);
 
     if (guibgbmp[ifn] != nullptr)
     {
@@ -2175,8 +2174,7 @@ void draw_fps(const Rect &viewport)
     const int font = FONT_NORMAL;
     if (fpsDisplay == nullptr)
     {
-        fpsDisplay = BitmapHelper::CreateBitmap(viewport.GetWidth(), (getfontheight_outlined(font) + get_fixed_pixel_size(5)), game.GetColorDepth());
-        fpsDisplay = ReplaceBitmapWithSupportedFormat(fpsDisplay);
+        fpsDisplay = CreateCompatBitmap(viewport.GetWidth(), (getfontheight_outlined(font) + get_fixed_pixel_size(5)));
     }
     fpsDisplay->ClearTransparent();
     
@@ -2567,8 +2565,7 @@ void construct_engine_overlay()
 
         if (debugConsoleBuffer == nullptr)
         {
-            debugConsoleBuffer = BitmapHelper::CreateBitmap(viewport.GetWidth(), barheight, game.GetColorDepth());
-            debugConsoleBuffer = ReplaceBitmapWithSupportedFormat(debugConsoleBuffer);
+            debugConsoleBuffer = CreateCompatBitmap(viewport.GetWidth(), barheight);
         }
 
         color_t draw_color = debugConsoleBuffer->GetCompatibleColor(15);

Engine/ac/dialog.cpp
@@ -816,8 +816,8 @@ void DialogOptions::Redraw()
 
     update_polled_stuff_if_runtime();
 
-    subBitmap = recycle_bitmap(subBitmap, tempScrn->GetColorDepth(), dirtywidth, dirtyheight);
-    subBitmap = ReplaceBitmapWithSupportedFormat(subBitmap);
+    subBitmap = recycle_bitmap(subBitmap,
+        gfxDriver->GetCompatibleBitmapFormat(tempScrn->GetColorDepth()), dirtywidth, dirtyheight);
 
     update_polled_stuff_if_runtime();
 

Engine/ac/draw.h
@@ -156,11 +156,11 @@ extern AGS_INLINE int game_to_ctx_data_size(int size, bool hires_ctx);
 // This function converts game coordinates coming from script to the actual game resolution.
 extern AGS_INLINE void defgame_to_finalgame_coords(int &x, int &y);
 
-// Checks if the bitmap needs to be converted and **deletes original** if a new bitmap
-// had to be created (by default).
-// TODO: this helper function was meant to remove bitmap deletion from the GraphicsDriver's
-// implementations while keeping code changes to minimum. The proper solution would probably
-// be to use shared pointers when storing Bitmaps, or make Bitmap reference-counted object.
+// Creates bitmap of a format compatible with the gfxdriver;
+// if col_depth is 0, uses game's native color depth.
+Common::Bitmap *CreateCompatBitmap(int width, int height, int col_depth = 0);
+// Checks if the bitmap is compatible with the gfxdriver;
+// returns same bitmap or its copy of a compatible format.
 Common::Bitmap *ReplaceBitmapWithSupportedFormat(Common::Bitmap *bitmap);
 // Checks if the bitmap needs any kind of adjustments before it may be used
 // in AGS sprite operations. Also handles number of certain special cases

Engine/ac/dynamicsprite.cpp
@@ -329,7 +329,7 @@ ScriptDynamicSprite* DynamicSprite_CreateFromScreenShot(int width, int height) {
     update_polled_stuff_if_runtime();
 
     // replace the bitmap in the sprite set
-    add_dynamic_sprite(gotSlot, ReplaceBitmapWithSupportedFormat(newPic));
+    add_dynamic_sprite(gotSlot, newPic);
     ScriptDynamicSprite *new_spr = new ScriptDynamicSprite(gotSlot);
     return new_spr;
 }
@@ -394,14 +394,15 @@ ScriptDynamicSprite* DynamicSprite_Create(int width, int height, int alphaChanne
     if (gotSlot <= 0)
         return nullptr;
 
-    Bitmap *newPic = BitmapHelper::CreateTransparentBitmap(width, height, game.GetColorDepth());
+    Bitmap *newPic = CreateCompatBitmap(width, height);
     if (newPic == nullptr)
         return nullptr;
 
+    newPic->ClearTransparent();
     if ((alphaChannel) && (game.GetColorDepth() < 32))
         alphaChannel = false;
 
-    add_dynamic_sprite(gotSlot, ReplaceBitmapWithSupportedFormat(newPic), alphaChannel != 0);
+    add_dynamic_sprite(gotSlot, newPic, alphaChannel != 0);
     ScriptDynamicSprite *new_spr = new ScriptDynamicSprite(gotSlot);
     return new_spr;
 }

Engine/ac/game.cpp
@@ -1105,7 +1105,7 @@ bool read_savedgame_screenshot(const String &savedgame, int &want_shot)
         if (slot > 0)
         {
             // add it into the sprite set
-            add_dynamic_sprite(slot, ReplaceBitmapWithSupportedFormat(desc.UserImage.release()));
+            add_dynamic_sprite(slot, PrepareSpriteForUse(desc.UserImage.release(), false));
             want_shot = slot;
         }
     }

Engine/ac/global_dynamicsprite.cpp
@@ -55,7 +55,7 @@ int LoadImageFile(const char *filename)
     if (gotSlot <= 0)
         return 0;
 
-    add_dynamic_sprite(gotSlot, ReplaceBitmapWithSupportedFormat(loadedFile));
+    add_dynamic_sprite(gotSlot, PrepareSpriteForUse(loadedFile, false));
 
     return gotSlot;
 }

Engine/ac/screen.cpp
@@ -89,7 +89,6 @@ IDriverDependantBitmap* prepare_screen_for_transition_in()
     if (saved_viewport_bitmap == nullptr)
         quit("Crossfade: buffer is null attempting transition");
 
-    saved_viewport_bitmap = ReplaceBitmapWithSupportedFormat(saved_viewport_bitmap);
     const Rect &viewport = play.GetMainViewport();
     if (saved_viewport_bitmap->GetHeight() < viewport.GetHeight())
     {

Engine/gui/guidialog.cpp
@@ -79,8 +79,7 @@ Bitmap *prepare_gui_screen(int x, int y, int width, int height, bool opaque)
     }
     else
     {
-        windowBuffer = BitmapHelper::CreateBitmap(windowPosWidth, windowPosHeight, game.GetColorDepth());
-        windowBuffer = ReplaceBitmapWithSupportedFormat(windowBuffer);
+        windowBuffer = CreateCompatBitmap(windowPosWidth, windowPosHeight);
     }
     dialogDDB = recycle_ddb_bitmap(dialogDDB, windowBuffer, false, opaque);
     return windowBuffer;

commit 077803120808a9b18a888bb24f7124844f21fa00

Engine: fixed AdjustBitmapForUseWithDisplayMode()
This fixes color conversion for sprites which color depth does not match game's color depth.
Regression introduced by 2132ba833b9d4508ed3f81a65f531b88f6087cb5

Engine/ac/draw.cpp
@@ -252,9 +252,8 @@ Bitmap *convert_32_to_32bgr(Bitmap *tempbl) {
 Bitmap *AdjustBitmapForUseWithDisplayMode(Bitmap* bitmap, bool has_alpha)
 {
     const int bmp_col_depth = bitmap->GetColorDepth();
-    const int sys_col_depth = System_GetColorDepth();
+    const int compat_col_depth = gfxDriver->GetCompatibleBitmapFormat(bmp_col_depth);
     const int game_col_depth = game.GetColorDepth();
-    const int compat_col_depth = gfxDriver->GetCompatibleBitmapFormat(game_col_depth);
 
     const bool must_switch_palette = bitmap->GetColorDepth() == 8 && game_col_depth > 8;
     if (must_switch_palette)
@@ -269,6 +268,7 @@ Bitmap *AdjustBitmapForUseWithDisplayMode(Bitmap* bitmap, bool has_alpha)
     // to match graphics driver expectation about pixel format.
     // TODO: make GetCompatibleBitmapFormat tell this somehow
 #if defined (AGS_INVERTED_COLOR_ORDER)
+    const int sys_col_depth = System_GetColorDepth();
     if (sys_col_depth > 16 && bmp_col_depth == 32)
     {
         // Convert RGB to BGR.
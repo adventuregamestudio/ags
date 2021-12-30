b4fdcdfd86c5e742f21d603fece511e9b040d28d
Ivan Mogilko <ikm_spb@yahoo.com>
2021-12-31 02:15:06 +0300

    Android: search for games using proper engine's algorithm
    Based on the work of @sonneveld (Nick Sonneveld) with minor modifications and fixes; original commit: https://github.com/sonneveld/ags/commit/1b92d9d42d99a401fa02b6cbf0b4849633e581f9
    Adjusted for the new AGS Player in Kotlin.
    
    The old method checked for Windows executable header and retrieved program version info to make sure the file contains Windows engine in certain version range. This made *some* sense many years ago when ports were mostly meant for running older games attached to Windows engine exe. But it no longer does.
    
    The new method calls engine's own function FindGameData() - the one that engine uses itself when scanning directory for a game.

---
+++ Engine/platform/android/helper/jni_helper.cpp
@@ -0,0 +1,46 @@
+//=============================================================================
+//
+// Adventure Game Studio (AGS)
+//
+// Copyright (C) 1999-2011 Chris Jones and 2011-20xx others
+// The full list of copyright holders can be found in the Copyright.txt
+// file, which is part of this source code distribution.
+//
+// The AGS source code is provided under the Artistic License 2.0.
+// A copy of this license can be found in the file License.txt and at
+// http://www.opensource.org/licenses/artistic-license-2.0.php
+//
+//=============================================================================
+#include "core/platform.h"
+
+#if AGS_PLATFORM_OS_ANDROID
+#include <jni.h>
+#include <allegro.h>
+#include "game/main_game_file.h"
+
+using namespace AGS::Common;
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_uk_co_adventuregamestudio_runtime_NativeHelper_findGameDataInDirectory(
+    JNIEnv* env, jobject object, jstring path)
+{
+    auto cpath = env->GetStringUTFChars(path, nullptr);
+    // We have to configure parts of allegro lib because
+    // - we call al_find* during the search
+    // - allegro might not have been initialised yet
+    int tmp_errno = 0;
+    int *orig_allegro_errno = allegro_errno;
+    allegro_errno = &tmp_errno;
+    set_filename_encoding(U_UNICODE);
+
+    auto result_str = FindGameData(cpath);
+
+    allegro_errno = orig_allegro_errno;
+    env->ReleaseStringUTFChars(path, cpath);
+
+    if (result_str.IsEmpty()) return nullptr;
+    return env->NewStringUTF(result_str.GetCStr());
+}
+
+#endif // AGS_PLATFORM_OS_ANDROID

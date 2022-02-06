5916ca26887f0e89390b52499ced1ff7df713de9
e <ericoporto2008@gmail.com>
2022-02-06 13:32:17 -0300

    CI: Android archive single game project with libs instead
    - The libs are significantly tied to the library/runtime project, and we also need the single game project when assembling the android component

--- Android/and-build.sh
+++ Android/and-build.sh
@@ -4,6 +4,39 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 TAR_CMD="bsdtar"
 command -v bsdtar >/dev/null 2>&1 || { TAR_CMD="tar" ; }
 
+function prepare_package {
+  echo "Do this only before any build, or after a full clean."
+  echo "doing preparations..."
+  pushd "${SCRIPT_DIR}"
+  [[ -d package_debug ]] && rm -rf package_debug
+  [[ -d package_release ]] && rm -rf package_release
+  mkdir package_debug
+  mkdir package_release
+  cp -r library package_debug/library
+  cp -r misc package_debug/misc
+  cp -r mygame package_debug/mygame
+  cp -r gradle package_debug/gradle
+  
+  cp -r library package_release/library
+  cp -r misc package_release/misc
+  cp -r mygame package_release/mygame
+  cp -r gradle package_release/gradle
+  
+  mkdir package_release/mygame/gradle
+  mkdir package_release/mygame/gradle/wrapper
+  mkdir package_debug/mygame/gradle
+  mkdir package_debug/mygame/gradle/wrapper
+  
+  cp agsplayer/gradle/wrapper/gradle-wrapper.jar package_release/mygame/gradle/wrapper/
+  cp agsplayer/gradle/wrapper/gradle-wrapper.properties package_release/mygame/gradle/wrapper/
+    
+  cp agsplayer/gradle/wrapper/gradle-wrapper.jar package_debug/mygame/gradle/wrapper/
+  cp agsplayer/gradle/wrapper/gradle-wrapper.properties package_debug/mygame/gradle/wrapper/
+  
+  popd
+  echo "done!"
+}
+
 function build_debug_library_and_runtime {
   echo "building debug library and runtime..."
   pushd "${SCRIPT_DIR}"
@@ -40,12 +73,14 @@ function rename_apks {
   echo "done!"
 }
 
-function create_libs_archive {
+function create_proj_archive {
   echo "creating libs archive..."
   pushd "${SCRIPT_DIR}"
   version=$(awk -F"[ \"]+" '{ if ($1=="#define" && $2=="ACI_VERSION_STR") { print $3; exit } }' ../Common/core/def_version.h)
-  ${TAR_CMD} -f ../AGS-${version}-android-libs-debug.zip -acv --strip-components 8 agsplayer/app/build/intermediates/stripped_native_libs/debug/out/lib
-  ${TAR_CMD} -f ../AGS-${version}-android-libs-release.zip -acv --strip-components 8 agsplayer/app/build/intermediates/stripped_native_libs/release/out/lib
+  cp -rf agsplayer/app/build/intermediates/stripped_native_libs/debug/out/lib/* package_debug/library/runtime/libs
+  cp -rf agsplayer/app/build/intermediates/stripped_native_libs/release/out/lib/* package_release/library/runtime/libs
+  ${TAR_CMD} -f ../AGS-${version}-android-proj-debug.zip -acv --strip-components 1 package_debug
+  ${TAR_CMD} -f ../AGS-${version}-android-proj-release.zip -acv --strip-components 1 package_release
   popd
   echo "done!"
 }
@@ -56,11 +91,12 @@ function usage
    echo
    echo "Syntax: and-build.sh [options]"
    echo "options:"
-   echo "build_debug"
-   echo "build_release"
-   echo "archive_apks"
-   echo "archive_libs"
-   echo "-h --help  prints this message."
+   echo "  prepare"
+   echo "  build_debug"
+   echo "  build_release"
+   echo "  archive_apks"
+   echo "  archive_project"
+   echo "  -h --help  prints this message."
    echo
 }
 
@@ -70,7 +106,10 @@ if [[ $# -eq 0 ]]; then
 fi
 
 while : ; do
-  case "$1" in 
+  case "$1" in
+    prepare)
+       prepare_package
+       shift 1 ;;
     build_debug)
        build_debug_library_and_runtime
        shift 1 ;;
@@ -79,9 +118,9 @@ while : ; do
        shift 1 ;;
     archive_apks)
        rename_apks
-       shift 1 ;;    
-    archive_libs)
-       create_libs_archive
+       shift 1 ;;
+    archive_project)
+       create_proj_archive
        shift 1 ;;
     -h|--help)
        usage

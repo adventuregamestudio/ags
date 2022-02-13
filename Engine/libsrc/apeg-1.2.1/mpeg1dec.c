adventuregamestudio/ags

commit 9be3432e8720a7fd67da231baed6801b00d9ba26

Engine: removed redundant time counting from TheoraPlayer and apeg

Engine/libsrc/apeg-1.2.1/mpeg1dec.c
@@ -177,15 +177,6 @@ static PACKFILE_VTABLE ext_vtable =
 };
 
 
-// The timer.. increments the int pointed to by 'param'
-static Uint32 timer_proc(Uint32 interval, void *param)
-{
-	++(*(volatile int*)param);
-    return interval;
-}
-END_OF_STATIC_FUNCTION(timer_proc);
-
-
 // Gets called at the beginning of every "open" function, to ensure
 // things are initialized
 static void Initialize_Decoder(void)
@@ -194,8 +185,6 @@ static void Initialize_Decoder(void)
 
 	if(!inited)
 	{
-		// Lock the timer function
-		LOCK_FUNCTION(timer_proc);
 		inited = TRUE;
 	}
 }
@@ -401,9 +390,6 @@ static void setup_stream(APEG_LAYER *layer)
 		// Start the timer
 		if(layer->stream.frame_rate <= 0.0)
 			apeg_error_jump(layer, "Illegal frame rate in stream");
-
-		// Reset the timer and return the stream
-		layer->stream.timer = -1;
 	}
 }
 

Engine/media/video/video.cpp
@@ -385,49 +385,34 @@ bool TheoraPlayer::NextFrame()
     const int framedrop = 0;
 
     // reset some data
-    //APEG_LAYER *layer = reinterpret_cast<APEG_LAYER*>(_apegStream);
-    _apegStream->frame = 0;
     _apegStream->frame_updated = -1;
     _apegStream->audio.flushed = FALSE;
 
-    int ret = 0;
     if ((_apegStream->flags & APEG_HAS_AUDIO) && _wantAudio)
     {
         unsigned char *buf = nullptr;
         int count = 0;
-        ret = apeg_get_audio_frame(_apegStream, &buf, &count);
+        if (apeg_get_audio_frame(_apegStream, &buf, &count) == APEG_ERROR)
+            return false;
         _audioFrame = SoundBuffer(buf, count);
-        if ((_apegStream->flags & APEG_HAS_VIDEO))
-        {
-            int pos_ms = GetAudioPos();
-            if (pos_ms >= 0) {
-                double audio_pos_secs = (double)pos_ms / 1000.0;
-                double audio_frames = audio_pos_secs * _apegStream->frame_rate;
-                _apegStream->timer = audio_frames - _apegStream->frame;
-                // could be negative.. so will wait until 0 ?
-            }
-        }
     }
 
     if ((_apegStream->flags & APEG_HAS_VIDEO))
     {
-        ret = apeg_get_video_frame(_apegStream);
+        if (apeg_get_video_frame(_apegStream) == APEG_ERROR)
+            return false;
 
-        if (_apegStream->timer > 0)
-        {
-            // Update frame and timer count
-            ++(_apegStream->frame);
-            --(_apegStream->timer);
+        // Update frame count
+        ++(_apegStream->frame);
 
-            // If we're not behind, update the display frame
-            _apegStream->frame_updated = 0;
-            apeg_display_video_frame(_apegStream);
-        }
+        // Update the display frame
+        _apegStream->frame_updated = 0;
+        apeg_display_video_frame(_apegStream);
     }
 
     _videoFrame->WrapAllegroBitmap(_apegStream->bitmap, true);
 
-    return ret == APEG_OK;
+    return true;
 }
 
 } // namespace Engine

Engine/libsrc/apeg-1.2.1/apeg.h
@@ -63,8 +63,6 @@ typedef struct APEG_STREAM {
 
 	int sequence;
 
-	volatile int timer;
-
 	int flags;
 
 	APEG_AUDIO_INF audio;

commit 1e49d5fad1af43d8731894ee29cb9e31484d509c

Engine: fixed TheoraPlayer not stopping at EOF
Was broken by recent commit 9be3432e8720a7fd67da231baed6801b00d9ba26

Engine/libsrc/apeg-1.2.1/mpeg1dec.c
@@ -601,9 +601,7 @@ int apeg_get_video_frame(APEG_STREAM *stream)
 		else
 			layer->picture = altheora_get_frame(layer);
 
-		if (pack_feof(layer->pf) &&
-			(!(layer->stream.flags&APEG_HAS_AUDIO) ||
-				ret != APEG_OK))
+		if (pack_feof(layer->pf))
 			ret = APEG_EOF;
 	}
 	return ret;

Engine/media/video/video.cpp
@@ -417,9 +417,8 @@ void TheoraPlayer::CloseImpl()
 
 bool TheoraPlayer::NextFrame()
 {
-    const int framedrop = 0;
-
     // reset some data
+    bool has_audio = false, has_video = false;
     _apegStream->frame_updated = -1;
     _apegStream->audio.flushed = FALSE;
 
@@ -427,14 +426,17 @@ bool TheoraPlayer::NextFrame()
     {
         unsigned char *buf = nullptr;
         int count = 0;
-        if (apeg_get_audio_frame(_apegStream, &buf, &count) == APEG_ERROR)
+        int ret = apeg_get_audio_frame(_apegStream, &buf, &count);
+        if (ret == APEG_ERROR)
             return false;
         _audioFrame = SoundBuffer(buf, count);
+        has_audio = ret != APEG_EOF;
     }
 
     if ((_apegStream->flags & APEG_HAS_VIDEO))
     {
-        if (apeg_get_video_frame(_apegStream) == APEG_ERROR)
+        int ret = apeg_get_video_frame(_apegStream);
+        if (ret == APEG_ERROR)
             return false;
 
         // Update frame count
@@ -443,11 +445,11 @@ bool TheoraPlayer::NextFrame()
         // Update the display frame
         _apegStream->frame_updated = 0;
         apeg_display_video_frame(_apegStream);
+        _videoFrame->WrapAllegroBitmap(_apegStream->bitmap, true);
+        has_video = ret != APEG_EOF;
     }
 
-    _videoFrame->WrapAllegroBitmap(_apegStream->bitmap, true);
-
-    return true;
+    return has_audio || has_video;
 }
 
 } // namespace Engine
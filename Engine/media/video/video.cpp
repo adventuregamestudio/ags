adventuregamestudio/ags

commit 6c6507e0427bbb8783616691affea83fdcaf10ef

Engine: further unifying VideoPlayer code in the base class

Engine/media/video/video.cpp
@@ -80,16 +80,30 @@ bool VideoPlayer::Open(const String &name, int flags, VideoSkipType skip)
             _wantAudio = true;
         }
     }
-
-    // TODO: needed for FLIC, but perhaps may be done differently
-    if ((flags & kVideo_ClearScreen) != 0)
+    // Setup video
+    if ((flags & kVideo_EnableVideo) != 0)
     {
-        if (gfxDriver->UsesMemoryBackBuffer())
+        _dstRect = PlaceInRect(play.GetMainViewport(), RectWH(_frameSize), kPlaceStretchProportional);
+        // override the stretch option if necessary
+        if (_frameSize == _dstRect.GetSize())
+            _flags &= ~kVideo_Stretch;
+        else
+            _flags |= kVideo_Stretch;
+
+        if (gfxDriver->HasAcceleratedTransform() || ((_flags & kVideo_Stretch) == 0))
         {
-            Bitmap *screen_bmp = gfxDriver->GetMemoryBackBuffer();
-            screen_bmp->Clear();
+            _videoDDB = gfxDriver->CreateDDB(_frameSize.Width, _frameSize.Height, _frameDepth, true);
         }
-        render_to_screen();
+        else
+        {
+            // For software rendering - create helper bitmaps in case of stretching;
+            // If we are decoding a 8-bit frame in a hi-color game, and stretching,
+            // then create a hi-color buffer, as bitmap lib cannot stretch with depth change
+            if ((_frameDepth == 8) && (game.GetColorDepth() > 8))
+                _hicolBuf.reset(BitmapHelper::CreateBitmap(_frameSize.Width, _frameSize.Height, game.GetColorDepth()));
+            _targetBitmap.reset(BitmapHelper::CreateBitmap(_dstRect.GetWidth(), _dstRect.GetHeight(), game.GetColorDepth()));
+            _videoDDB = gfxDriver->CreateDDB(_dstRect.GetWidth(), _dstRect.GetHeight(), game.GetColorDepth(), true);
+        }
     }
 
     _timerPos = 1;
@@ -121,9 +135,22 @@ void VideoPlayer::Close()
         screen_bmp->Clear();
     }
     render_to_screen();
-
+    invalidate_screen(); // ?
     ags_clear_input_state();
-    invalidate_screen();
+}
+
+void VideoPlayer::Play()
+{
+    // TODO: needed for FLIC, but perhaps may be done differently
+    if ((_flags & kVideo_ClearScreen) != 0)
+    {
+        if (gfxDriver->UsesMemoryBackBuffer())
+        {
+            Bitmap *screen_bmp = gfxDriver->GetMemoryBackBuffer();
+            screen_bmp->Clear();
+        }
+        render_to_screen();
+    }
 }
 
 int VideoPlayer::GetAudioPos()
@@ -133,6 +160,9 @@ int VideoPlayer::GetAudioPos()
 
 bool VideoPlayer::Poll()
 {
+    // Check user input skipping the video
+    if (CheckUserInputSkip())
+        return false;
     // Acquire next video frame
     if (!NextFrame())
         return false;
@@ -140,9 +170,6 @@ bool VideoPlayer::Poll()
     if (_audioFrame && !RenderAudio())
         return false;
     if (_videoFrame && !RenderVideo())
-        return false;
-    // Check user input skipping the video
-    if (CheckUserInputSkip())
         return false;
     // Wait for timer
     _timerPos--;
@@ -189,59 +216,31 @@ bool VideoPlayer::RenderVideo()
     assert(_videoFrame);
     Bitmap *usebuf = _videoFrame.get();
 
-    // FIXME: for FLIC only!! do we need this always, or only when stretched, or at all??
-    // FIXME: test target bitmap, not game's depth?
-    if ((game.color_depth > 1) && (usebuf->GetBPP() == 1))
+    // Use intermediate hi-color buffer if necessary
+    if (_hicolBuf)
     {
         _hicolBuf->Blit(usebuf);
         usebuf = _hicolBuf.get();
     }
 
-    // FIXME: create on Open?
-    if (!_videoDDB)
-    {
-        _videoDDB = gfxDriver->CreateDDBFromBitmap(usebuf, false, true);
-    }
-
-    int drawAtX = 0, drawAtY = 0;
-    const Rect &view = play.GetMainViewport();
     if ((_flags & kVideo_Stretch) != 0)
     {
-        drawAtX = view.GetWidth() / 2 - _targetSize.Width / 2;
-        drawAtY = view.GetHeight() / 2 - _targetSize.Height / 2;
-
-        if (!gfxDriver->HasAcceleratedTransform())
+        if (gfxDriver->HasAcceleratedTransform())
         {
-            _targetBitmap->StretchBlt(usebuf, RectWH(0, 0, usebuf->GetWidth(), usebuf->GetHeight()),
-                RectWH(drawAtX, drawAtY, _targetSize.Width, _targetSize.Height));
-            gfxDriver->UpdateDDBFromBitmap(_videoDDB, _targetBitmap.get(), false);
-            drawAtX = 0;
-            drawAtY = 0;
+            gfxDriver->UpdateDDBFromBitmap(_videoDDB, usebuf, false);
+            _videoDDB->SetStretch(_dstRect.GetWidth(), _dstRect.GetHeight(), false);
         }
         else
         {
-            gfxDriver->UpdateDDBFromBitmap(_videoDDB, usebuf, false);
-            _videoDDB->SetStretch(_targetSize.Width, _targetSize.Height, false);
+            _targetBitmap->StretchBlt(usebuf, RectWH(_dstRect.GetSize()));
+            gfxDriver->UpdateDDBFromBitmap(_videoDDB, _targetBitmap.get(), false);
         }
-
-        /* FROM FLIC:
-        fli_target->Blit(usebuf, 0, 0, view.GetWidth() / 2 - fliwidth / 2, view.GetHeight() / 2 - fliheight / 2, view.GetWidth(), view.GetHeight());
-        */
     }
     else
     {
         gfxDriver->UpdateDDBFromBitmap(_videoDDB, usebuf, false);
-        drawAtX = view.GetWidth() / 2 - usebuf->GetWidth() / 2;
-        drawAtY = view.GetHeight() / 2 - usebuf->GetHeight() / 2;
-        /* FROM FLIC:
-        fli_target->StretchBlt(usebuf, RectWH(0, 0, fliwidth, fliheight), RectWH(0, 0, view.GetWidth(), view.GetHeight()));
-        */
     }
-
-    /* FROM FLIC:
-    gfxDriver->UpdateDDBFromBitmap(fli_ddb, fli_target, false);
-    */
-    gfxDriver->DrawSprite(drawAtX, drawAtY, _videoDDB);
+    gfxDriver->DrawSprite(_dstRect.Left, _dstRect.Top, _videoDDB);
     render_to_screen();
     return true;
 }
@@ -302,21 +301,9 @@ bool FlicPlayer::OpenImpl(const AGS::Common::String &name, int &flags)
 
     get_palette_range(_oldpal, 0, 255);
 
-    if (game.color_depth > 1)
-    {
-        _hicolBuf.reset(BitmapHelper::CreateClearBitmap(fliwidth, fliheight, game.GetColorDepth()));
-    }
-    // override the stretch option if necessary
-    const Rect &view = play.GetMainViewport();
-    if ((fliwidth == view.GetWidth()) && (fliheight == view.GetHeight()))
-        flags &= ~kVideo_Stretch;
-    else if ((fliwidth > view.GetWidth()) || (fliheight >view.GetHeight()))
-        flags |= kVideo_Stretch;
     _videoFrame.reset(BitmapHelper::CreateClearBitmap(fliwidth, fliheight, 8));
-
-    _targetBitmap.reset(BitmapHelper::CreateBitmap(view.GetWidth(), view.GetHeight(), game.GetColorDepth()));
-    _videoDDB = gfxDriver->CreateDDBFromBitmap(_targetBitmap.get(), false, true);
-    _targetSize = view.GetSize();
+    _frameDepth = 8;
+    _frameSize = Size(fliwidth, fliheight);
     _frameTime = fli_speed;
     return true;
 }
@@ -400,32 +387,6 @@ void apeg_stream_skip(int bytes, void *ptr)
 }
 //
 
-
-// TODO: use shared utility function for placing rect in rect
-static void calculate_destination_size_maintain_aspect_ratio(int vidWidth, int vidHeight, int *targetWidth, int *targetHeight)
-{
-    const Rect &viewport = play.GetMainViewport();
-    float aspectRatioVideo = (float)vidWidth / (float)vidHeight;
-    float aspectRatioScreen = (float)viewport.GetWidth() / (float)viewport.GetHeight();
-
-    if (aspectRatioVideo == aspectRatioScreen)
-    {
-        *targetWidth = viewport.GetWidth();
-        *targetHeight = viewport.GetHeight();
-    }
-    else if (aspectRatioVideo > aspectRatioScreen)
-    {
-        *targetWidth = viewport.GetWidth();
-        *targetHeight = (int)(((float)viewport.GetWidth() / aspectRatioVideo) + 0.5f);
-    }
-    else
-    {
-        *targetHeight = viewport.GetHeight();
-        *targetWidth = (float)viewport.GetHeight() * aspectRatioVideo;
-    }
-
-}
-
 bool TheoraPlayer::OpenImpl(const AGS::Common::String &name, int &flags)
 {
     std::unique_ptr<Stream> video_stream(AssetMgr->OpenAsset(name));
@@ -451,37 +412,20 @@ bool TheoraPlayer::OpenImpl(const AGS::Common::String &name, int &flags)
     }
 
     _apegStream = apeg_stream;
-    calculate_destination_size_maintain_aspect_ratio(video_w, video_h, &_targetSize.Width, &_targetSize.Height);
-
-    if ((_targetSize.Width == video_w) && (_targetSize.Height == video_h))
-    {
-        // don't need to stretch after all
-        flags &= ~kVideo_Stretch;
-    }
-
-    if (((flags & kVideo_Stretch) != 0) && (!gfxDriver->HasAcceleratedTransform()))
-    {
-        _targetBitmap.reset(BitmapHelper::CreateClearBitmap(
-            play.GetMainViewport().GetWidth(), play.GetMainViewport().GetHeight(), game.GetColorDepth()));
-        _videoDDB = gfxDriver->CreateDDBFromBitmap(_targetBitmap.get(), false, true);
-    }
-    else
-    {
-        _videoDDB = nullptr;
-    }
 
     if (gfxDriver->UsesMemoryBackBuffer())
         gfxDriver->GetMemoryBackBuffer()->Clear();
 
     // Init APEG
     _dataStream = std::move(video_stream);
+    _frameDepth = game.GetColorDepth();
+    _frameSize = Size(video_w, video_h);
     _frameTime = 1000 / _apegStream->frame_rate;
     _videoFrame.reset(new Bitmap()); // FIXME: use target directly if possible?
     _audioChannels = _apegStream->audio.channels;
     _audioFreq = _apegStream->audio.freq;
     _audioFormat = AUDIO_S16SYS;
     apeg_set_error(_apegStream, NULL);
-
     return true;
 }
 
@@ -552,6 +496,7 @@ bool TheoraPlayer::NextFrame()
 void video_run(std::unique_ptr<VideoPlayer> video)
 {
     gl_Video = std::move(video);
+    gl_Video->Play();
     // Loop until finished or skipped by player
     while (gl_Video->Poll())
     {

Engine/gfx/ali3dsw.cpp
@@ -294,10 +294,14 @@ int SDLRendererGraphicsDriver::GetCompatibleBitmapFormat(int color_depth)
   return color_depth;
 }
 
+IDriverDependantBitmap* SDLRendererGraphicsDriver::CreateDDB(int width, int height, int color_depth, bool opaque)
+{
+  return new ALSoftwareBitmap(width, height, color_depth, opaque);
+}
+
 IDriverDependantBitmap* SDLRendererGraphicsDriver::CreateDDBFromBitmap(Bitmap *bitmap, bool hasAlpha, bool opaque)
 {
-  ALSoftwareBitmap* newBitmap = new ALSoftwareBitmap(bitmap, opaque, hasAlpha);
-  return newBitmap;
+  return new ALSoftwareBitmap(bitmap, opaque, hasAlpha);
 }
 
 void SDLRendererGraphicsDriver::UpdateDDBFromBitmap(IDriverDependantBitmap* bitmapToUpdate, Bitmap *bitmap, bool hasAlpha)

Engine/gfx/ali3dsw.h
@@ -57,12 +57,20 @@ public:
     void SetLightLevel(int lightLevel) override  { }
     void SetTint(int red, int green, int blue, int tintSaturation) override { }
 
-    Bitmap *_bmp;
-    bool _flipped;
-    int _stretchToWidth, _stretchToHeight;
-    bool _opaque; // no mask color
-    bool _hasAlpha;
-    int _transparency;
+    Bitmap *_bmp = nullptr;
+    bool _flipped = false;
+    int _stretchToWidth = 0, _stretchToHeight = 0;
+    bool _opaque = false; // no mask color
+    bool _hasAlpha = false;
+    int _transparency = 0;
+
+    ALSoftwareBitmap(int width, int height, int color_depth, bool opaque)
+    {
+        _width = width;
+        _height = height;
+        _colDepth = color_depth;
+        _opaque = opaque;
+    }
 
     ALSoftwareBitmap(Bitmap *bmp, bool opaque, bool hasAlpha)
     {
@@ -70,10 +78,6 @@ public:
         _width = bmp->GetWidth();
         _height = bmp->GetHeight();
         _colDepth = bmp->GetColorDepth();
-        _flipped = false;
-        _stretchToWidth = 0;
-        _stretchToHeight = 0;
-        _transparency = 0;
         _opaque = opaque;
         _hasAlpha = hasAlpha;
     }
@@ -157,6 +161,7 @@ public:
     // Clears the screen rectangle. The coordinates are expected in the **native game resolution**.
     void ClearRectangle(int x1, int y1, int x2, int y2, RGB *colorToUse) override;
     int  GetCompatibleBitmapFormat(int color_depth) override;
+    IDriverDependantBitmap* CreateDDB(int width, int height, int color_depth, bool opaque) override;
     IDriverDependantBitmap* CreateDDBFromBitmap(Bitmap *bitmap, bool hasAlpha, bool opaque) override;
     void UpdateDDBFromBitmap(IDriverDependantBitmap* bitmapToUpdate, Bitmap *bitmap, bool hasAlpha) override;
     void DestroyDDB(IDriverDependantBitmap* bitmap) override;

Engine/gfx/gfxdriverbase.h
@@ -197,9 +197,6 @@ public:
     IDriverDependantBitmap *CreateDDBFromBitmap(Bitmap *bitmap, bool hasAlpha, bool opaque = false) override;
 
 protected:
-    // Creates a "raw" DDB, without pixel initialization
-    virtual IDriverDependantBitmap *CreateDDB(int width, int height, int color_depth, bool opaque = false) = 0;
-
     // Stage screens are raw bitmap buffers meant to be sent to plugins on demand
     // at certain drawing stages. If used at least once these buffers are then
     // rendered as additional sprites in their respected order.

Engine/gfx/graphicsdriver.h
@@ -116,6 +116,9 @@ public:
   // Gets closest recommended bitmap format (currently - only color depth) for the given original format.
   // Engine needs to have game bitmaps brought to the certain range of formats, easing conversion into the video bitmaps.
   virtual int  GetCompatibleBitmapFormat(int color_depth) = 0;
+  // Creates a "raw" DDB, without pixel initialization
+  virtual IDriverDependantBitmap *CreateDDB(int width, int height, int color_depth, bool opaque = false) = 0;
+  // Creates DDB, initializes from the given bitmap
   virtual IDriverDependantBitmap* CreateDDBFromBitmap(Common::Bitmap *bitmap, bool hasAlpha, bool opaque = false) = 0;
   virtual void UpdateDDBFromBitmap(IDriverDependantBitmap* bitmapToUpdate, Common::Bitmap *bitmap, bool hasAlpha) = 0;
   virtual void DestroyDDB(IDriverDependantBitmap* bitmap) = 0;

Engine/media/video/video.h
@@ -60,6 +60,8 @@ public:
     bool Open(const AGS::Common::String &name, int flags, VideoSkipType skip);
     // Stops the playback, releasing any resources
     void Close();
+    // Begins playback
+    void Play();
     // Restores the video after display switch
     virtual void Restore() {};
 
@@ -86,10 +88,8 @@ protected:
     bool _wantAudio = false;
 
     std::unique_ptr<Bitmap> _videoFrame;
-    std::unique_ptr<Bitmap> _hicolBuf;
-    std::unique_ptr<Bitmap> _targetBitmap;
-    IDriverDependantBitmap *_videoDDB = nullptr;
-    Size _targetSize{};
+    int _frameDepth = 0; // bits per pixel
+    Size _frameSize{};
     uint32_t _frameTime = 0u;
 
 private:
@@ -102,12 +102,20 @@ private:
     // Renders the current video frame
     bool RenderVideo();
 
+    // Parameters
     bool _loop = false;
     int _flags = 0;
     VideoSkipType _skip = VideoSkipNone;
+    // Playback state
     uint32_t _sdlTimer = 0u;
     std::atomic<int> _timerPos{};
+    // Audio
     std::unique_ptr<OpenAlSource> _audioOut;
+    // Video
+    Rect _dstRect{};
+    std::unique_ptr<Bitmap> _hicolBuf;
+    std::unique_ptr<Bitmap> _targetBitmap;
+    IDriverDependantBitmap *_videoDDB = nullptr;
 };
 
 } // namespace Engine

commit 6184b766a1a91dbf18b26af14e6a29faba7d23d7

Engine: fixed TheoraPlayer's non-stretching frame placement
Was broken by 6c6507e0427bbb8783616691affea83fdcaf10ef (probably)

Engine/media/video/video.cpp
@@ -82,7 +82,8 @@ bool VideoPlayer::Open(const String &name, int flags)
     // Setup video
     if ((flags & kVideo_EnableVideo) != 0)
     {
-        _dstRect = PlaceInRect(play.GetMainViewport(), RectWH(_frameSize), kPlaceStretchProportional);
+        _dstRect = PlaceInRect(play.GetMainViewport(), RectWH(_frameSize),
+            ((_flags & kVideo_Stretch) == 0) ? kPlaceCenter : kPlaceStretchProportional);
         // override the stretch option if necessary
         if (_frameSize == _dstRect.GetSize())
             _flags &= ~kVideo_Stretch;
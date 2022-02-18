adventuregamestudio/ags

commit 1483fd631cf79ed6f25a5652d8280ac15333f7c0

Engine: lock background animation depending on loaded room setting

Engine/ac/room.cpp
@@ -297,8 +297,6 @@ void unload_old_room() {
     }
     else croom->tsdatasize=0;
     memset(&play.walkable_areas_on[0],1,MAX_WALK_AREAS+1);
-    play.bg_frame=0;
-    play.bg_frame_locked=0;
     remove_screen_overlay(-1);
     delete raw_saved_screen;
     raw_saved_screen = nullptr;
@@ -927,6 +925,8 @@ void load_new_room(int newnum, CharacterInfo*forchar) {
     new_room_flags=0;
     play.gscript_timer=-1;  // avoid screw-ups with changing screens
     play.player_on_region = 0;
+    play.bg_frame = 0;
+    play.bg_frame_locked = (thisroom.Options.Flags & kRoomFlag_BkgFrameLocked) != 0;
     // trash any input which they might have done while it was loading
     ags_clear_input_buffer();
     // no fade in, so set the palette immediately in case of 256-col sprites

commit 2c1604074da6fc2ccea17a63596b370df0d12147

Engine: fixed bg frame and bg locked flag is reset on save restore
Broken by commit 1483fd631cf79ed6f25a5652d8280ac15333f7c0, placed the variable assignment in the wrong place

Engine/ac/room.cpp
@@ -517,10 +517,6 @@ void load_new_room(int newnum, CharacterInfo*forchar) {
     play.anim_background_speed = thisroom.BgAnimSpeed;
     play.bg_anim_delay = play.anim_background_speed;
 
-    // Fixup the frame index, in case the new room does not have enough background frames
-    if (play.bg_frame < 0 || play.bg_frame >= thisroom.BgFrameCount)
-        play.bg_frame = 0;
-
     // do the palette
     for (cc=0;cc<256;cc++) {
         if (game.paluses[cc]==PAL_BACKGROUND)
@@ -909,8 +905,7 @@ void load_new_room(int newnum, CharacterInfo*forchar) {
     new_room_flags=0;
     play.gscript_timer=-1;  // avoid screw-ups with changing screens
     play.player_on_region = 0;
-    play.bg_frame = 0;
-    play.bg_frame_locked = (thisroom.Options.Flags & kRoomFlag_BkgFrameLocked) != 0;
+
     // trash any input which they might have done while it was loading
     ags_clear_input_buffer();
     // no fade in, so set the palette immediately in case of 256-col sprites
@@ -967,6 +962,10 @@ void new_room(int newnum,CharacterInfo*forchar) {
     update_polled_stuff_if_runtime();
 
     load_new_room(newnum,forchar);
+
+    // Reset background frame state (it's not a part of the RoomStatus currently)
+    play.bg_frame = 0;
+    play.bg_frame_locked = (thisroom.Options.Flags & kRoomFlag_BkgFrameLocked) != 0;
 }
 
 void set_room_placeholder()
@@ -994,8 +993,12 @@ int find_highest_room_entered() {
 
 void first_room_initialization() {
     starting_room = displayed_room;
+    playerchar->prevroom = -1;
     set_loop_counter(0);
     mouse_z_was = sys_mouse_z;
+    // Reset background frame state
+    play.bg_frame = 0;
+    play.bg_frame_locked = (thisroom.Options.Flags & kRoomFlag_BkgFrameLocked) != 0;
 }
 
 void check_new_room() {

Engine/game/savegame.cpp
@@ -552,6 +552,10 @@ HSaveError DoAfterRestore(const PreservedParams &pp, const RestoredData &r_data)
 
     if (displayed_room >= 0)
     {
+        // Fixup the frame index, in case the restored room does not have enough background frames
+        if (play.bg_frame < 0 || play.bg_frame >= thisroom.BgFrameCount)
+            play.bg_frame = 0;
+
         for (int i = 0; i < MAX_ROOM_BGFRAMES; ++i)
         {
             if (r_data.RoomBkgScene[i])
@@ -658,8 +662,6 @@ HSaveError DoAfterRestore(const PreservedParams &pp, const RestoredData &r_data)
     {
         // the restart point, no room was loaded
         load_new_room(playerchar->room, playerchar);
-        playerchar->prevroom = -1;
-
         first_room_initialization();
     }
 

Engine/main/game_start.cpp
@@ -109,8 +109,6 @@ void start_game() {
     if (displayed_room < 0) {
         current_fade_out_effect();
         load_new_room(playerchar->room,playerchar);
-        // load_new_room updates it, but it should be -1 in the first room
-        playerchar->prevroom = -1;
     }
 
     first_room_initialization();
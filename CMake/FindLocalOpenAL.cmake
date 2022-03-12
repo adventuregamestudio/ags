afe3718bb069cdc0a48cd537e0204659ab4fa13e
e <ericoporto2008@gmail.com>
2022-03-12 02:36:14 -0300

    CMake: adjustments on the cmake logic
    - simplifications and homogeinization

--- CMake/FindLocalOpenAL.cmake
+++ CMake/FindLocalOpenAL.cmake
@@ -1,6 +1,14 @@
 # Grab system openAL or use embedded mojoAL
 
 if(WIN32 OR LINUX OR MACOS OR FREEBSD)
+    set(AGS_USE_MOJO_AL TRUE)
+endif()
+
+if(NOT AGS_USE_MOJO_AL)
+    find_package(OpenAL)
+endif()
+
+if(AGS_USE_MOJO_AL OR NOT OPENAL_FOUND)
     add_subdirectory(libsrc/mojoAL EXCLUDE_FROM_ALL)
 
     add_library(openal-interface INTERFACE)
@@ -8,20 +16,9 @@ if(WIN32 OR LINUX OR MACOS OR FREEBSD)
     add_library(External::OpenAL ALIAS openal-interface)
     message("using MojoAL...")
 else()
-    find_package(OpenAL)
-
-    if (OPENAL_FOUND)
-        add_library(openal-interface INTERFACE)
-        target_link_libraries(openal-interface INTERFACE ${OPENAL_LIBRARY})
-        target_include_directories(openal-interface INTERFACE ${OPENAL_INCLUDE_DIR})
-        add_library(External::OpenAL ALIAS openal-interface)
-        message("using OpenAL...")
-    else()
-        add_subdirectory(libsrc/mojoAL EXCLUDE_FROM_ALL)
-
-        add_library(openal-interface INTERFACE)
-        target_link_libraries(openal-interface INTERFACE MojoAL::MojoAL)
-        add_library(External::OpenAL ALIAS openal-interface)
-        message("using MojoAL...")
-    endif()
+    add_library(openal-interface INTERFACE)
+    target_link_libraries(openal-interface INTERFACE ${OPENAL_LIBRARY})
+    target_include_directories(openal-interface INTERFACE ${OPENAL_INCLUDE_DIR})
+    add_library(External::OpenAL ALIAS openal-interface)
+    message("using system OpenAL...")
 endif()

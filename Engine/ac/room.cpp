adventuregamestudio/ags

commit 1483fd631cf79ed6f25a5652d8280ac15333f7c0

Engine: lock background animation depending on loaded room setting

Engine/ac/room.cpp
@@ -297,8 +297,6 @@ void unload_old_room() {
     }
     else croom->tsdatasize=0;
     memset(&play.walkable_areas_on[0],1,MAX_WALK_AREAS+1);
-    play.bg_frame=0;
-    play.bg_frame_locked=0;
     remove_screen_overlay(-1);
     delete raw_saved_screen;
     raw_saved_screen = nullptr;
@@ -927,6 +925,8 @@ void load_new_room(int newnum, CharacterInfo*forchar) {
     new_room_flags=0;
     play.gscript_timer=-1;  // avoid screw-ups with changing screens
     play.player_on_region = 0;
+    play.bg_frame = 0;
+    play.bg_frame_locked = (thisroom.Options.Flags & kRoomFlag_BkgFrameLocked) != 0;
     // trash any input which they might have done while it was loading
     ags_clear_input_buffer();
     // no fade in, so set the palette immediately in case of 256-col sprites

commit e45f87d1196a1c1a05b3e5cc1966c8633314902f

Engine: fixed potential crash with less room bg frames... again
This was first broken in 1483fd631cf79ed6f25a5652d8280ac15333f7c0,
but then incorrectly fixed in 2c1604074da6fc2ccea17a63596b370df0d12147

Engine/ac/room.cpp
@@ -296,6 +296,8 @@ void unload_old_room() {
     }
     else croom->tsdatasize=0;
     memset(&play.walkable_areas_on[0],1,MAX_WALK_AREAS+1);
+    play.bg_frame = 0;
+    play.bg_frame_locked = 0;
     remove_screen_overlay(-1);
     delete raw_saved_screen;
     raw_saved_screen = nullptr;
@@ -515,6 +517,10 @@ void load_new_room(int newnum, CharacterInfo*forchar) {
     play.room_height = thisroom.Height;
     play.anim_background_speed = thisroom.BgAnimSpeed;
     play.bg_anim_delay = play.anim_background_speed;
+
+    // Fixup the frame index, in case the new room does not have enough background frames
+    if (play.bg_frame < 0 || play.bg_frame >= thisroom.BgFrameCount)
+        play.bg_frame = 0;
 
     // do the palette
     for (cc=0;cc<256;cc++) {
@@ -962,7 +968,7 @@ void new_room(int newnum,CharacterInfo*forchar) {
 
     load_new_room(newnum,forchar);
 
-    // Reset background frame state (it's not a part of the RoomStatus currently)
+    // Update background frame state (it's not a part of the RoomStatus currently)
     play.bg_frame = 0;
     play.bg_frame_locked = (thisroom.Options.Flags & kRoomFlag_BkgFrameLocked) != 0;
 }
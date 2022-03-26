adventuregamestudio/ags

commit fd28a2548e39c85478d98e26b51713cd687acada

Engine: share animation cycling algo between Buttons and RoomObjects

Engine/ac/roomobject.cpp
@@ -16,6 +16,7 @@
 #include "ac/common_defines.h"
 #include "ac/gamesetupstruct.h"
 #include "ac/gamestate.h"
+#include "ac/object.h"
 #include "ac/runtime_defines.h"
 #include "ac/viewframe.h"
 #include "debug/debug_log.h"
@@ -76,16 +77,7 @@ void RoomObject::UpdateCyclingView(int ref_id)
     if (view == (uint16_t)-1) return;
     if (wait>0) { wait--; return; }
 
-    if (cycling >= ANIM_BACKWARDS) {
-
-      update_cycle_view_backwards();
-
-    }
-    else {  // Animate forwards
-      
-	  update_cycle_view_forwards();
-
-    }  // end if forwards
+    cycling = CycleViewAnim(view, loop, frame, cycling < ANIM_BACKWARDS, cycling % ANIM_BACKWARDS);
 
     ViewFrame*vfptr=&views[view].loops[loop].frames[frame];
     if (vfptr->pic > UINT16_MAX)
@@ -98,61 +90,6 @@ void RoomObject::UpdateCyclingView(int ref_id)
 
     wait=vfptr->speed+overall_speed;
     CheckViewFrame (view, loop, frame);
-}
-
-
-void RoomObject::update_cycle_view_forwards()
-{
-	frame++;
-      if (frame >= views[view].loops[loop].numFrames) {
-        // go to next loop thing
-        if (views[view].loops[loop].RunNextLoop()) {
-          if (loop+1 >= views[view].numLoops)
-            quit("!Last loop in a view requested to move to next loop");
-          loop++;
-          frame=0;
-        }
-        else if (cycling % ANIM_BACKWARDS == ANIM_ONCE) {
-          // leave it on the last frame
-          cycling=0;
-          frame--;
-          }
-        else {
-          if (play.no_multiloop_repeat == 0) {
-            // multi-loop anims, go back to start of it
-            while ((loop > 0) && 
-              (views[view].loops[loop - 1].RunNextLoop()))
-              loop --;
-          }
-          if (cycling % ANIM_BACKWARDS == ANIM_ONCERESET)
-            cycling=0;
-          frame=0;
-        }
-      }
-}
-
-void RoomObject::update_cycle_view_backwards()
-{
-	// animate backwards
-      if (frame > 0) {
-        frame--;
-      } else {
-        if ((loop > 0) && 
-           (views[view].loops[loop - 1].RunNextLoop())) 
-        {
-          // If it's a Go-to-next-loop on the previous one, then go back
-          loop --;
-          frame = views[view].loops[loop].numFrames - 1;
-        }
-        else if (cycling % ANIM_BACKWARDS == ANIM_ONCE) {
-          // leave it on the first frame
-          cycling = 0;
-          frame = 0;
-        }
-        else { // repeating animation
-          frame = views[view].loops[loop].numFrames - 1;
-        }
-      }
 }
 
 void RoomObject::ReadFromSavegame(Stream *in, int save_ver)

Common/gui/guibutton.h
@@ -131,7 +131,4 @@ private:
 extern std::vector<AGS::Common::GUIButton> guibuts;
 extern int numguibuts;
 
-int UpdateAnimatingButton(int bu);
-void StopButtonAnimation(int idxn);
-
 #endif // __AC_GUIBUTTON_H

Engine/ac/button.cpp
@@ -17,6 +17,7 @@
 #include "ac/view.h"
 #include "ac/gamesetupstruct.h"
 #include "ac/global_translation.h"
+#include "ac/object.h"
 #include "ac/string.h"
 #include "ac/viewframe.h"
 #include "debug/debug_log.h"
@@ -32,6 +33,16 @@ extern std::vector<ViewStruct> views;
 // *** BUTTON FUNCTIONS
 
 std::vector<AnimatingGUIButton> animbuts;
+
+// Update the actual button's image from the current animation frame
+void UpdateButtonState(const AnimatingGUIButton &abtn)
+{
+    guibuts[abtn.buttonid].Image = views[abtn.view].loops[abtn.loop].frames[abtn.frame].pic;
+    guibuts[abtn.buttonid].CurrentImage = guibuts[abtn.buttonid].Image;
+    guibuts[abtn.buttonid].PushedImage = 0;
+    guibuts[abtn.buttonid].MouseOverImage = 0;
+    guibuts[abtn.buttonid].NotifyParentChanged();
+}
 
 void Button_AnimateEx(GUIButton *butt, int view, int loop, int speed, int repeat, int blocking, int direction, int sframe) {
     int guin = butt->ParentId;
@@ -63,27 +74,18 @@ void Button_AnimateEx(GUIButton *butt, int view, int loop, int speed, int repeat
     // if it's already animating, stop it
     FindAndRemoveButtonAnimation(guin, objn);
 
-    // Prepare button
-    int buttonId = guis[guin].GetControlID(objn);
-    guibuts[buttonId].PushedImage = 0;
-    guibuts[buttonId].MouseOverImage = 0;
-
     // reverse animation starts at the *previous frame*
     if (direction)
     {
         if (--sframe < 0)
             sframe = views[view].loops[loop].numFrames - (-sframe);
-        sframe++; // set on next frame, first call to Update will decrement
-    }
-    else
-    {
-        sframe--; // set on prev frame, first call to Update will increment
     }
 
+    int but_id = guis[guin].GetControlID(objn);
     AnimatingGUIButton abtn;
     abtn.ongui = guin;
     abtn.onguibut = objn;
-    abtn.buttonid = buttonId;
+    abtn.buttonid = but_id;
     abtn.view = view;
     abtn.loop = loop;
     abtn.speed = speed;
@@ -91,15 +93,11 @@ void Button_AnimateEx(GUIButton *butt, int view, int loop, int speed, int repeat
     abtn.blocking = blocking;
     abtn.direction = direction;
     abtn.frame = sframe;
-    abtn.wait = 0;
+    abtn.wait = abtn.speed + views[abtn.view].loops[abtn.loop].frames[abtn.frame].speed;
     animbuts.push_back(abtn);
-    // launch into the first frame
-    if (UpdateAnimatingButton(animbuts.size() - 1))
-    {
-        debug_script_warn("AnimateButton: no frames to animate (button: %s, view: %d, loop: %d)",
-            butt->GetScriptName().GetCStr(), view, loop);
-        StopButtonAnimation(animbuts.size() - 1);
-    }
+    // launch into the first frame, and play the first frame's sound
+    UpdateButtonState(abtn);
+    CheckViewFrame(abtn.view, abtn.loop, abtn.frame);
 
     // Blocking animate
     if (blocking)
@@ -244,68 +242,20 @@ void AddButtonAnimation(const AnimatingGUIButton &abtn)
 }
 
 // returns 1 if animation finished
-int UpdateAnimatingButton(int bu) {
+bool UpdateAnimatingButton(int bu)
+{
     AnimatingGUIButton &abtn = animbuts[bu];
-
     if (abtn.wait > 0) {
         abtn.wait--;
-        return 0;
+        return true;
     }
-    ViewStruct *tview = &views[abtn.view];
-
-    if (abtn.direction)
-    { // backwards
-        abtn.frame--;
-        if (abtn.frame < 0)
-        {
-            if ((abtn.loop > 0) && tview->loops[abtn.loop - 1].RunNextLoop()) {
-                // go to next loop
-                abtn.loop--;
-                abtn.frame = tview->loops[abtn.loop].numFrames - 1;
-            }
-            else if (abtn.repeat) {
-                // multi-loop anim, go back
-                while (tview->loops[abtn.loop].RunNextLoop())
-                    abtn.loop++;
-                abtn.frame = tview->loops[abtn.loop].numFrames - 1;
-            }
-            else
-                return 1;
-        }
-    }
-    else
-    { // forwards
-        abtn.frame++;
-        if (abtn.frame >= tview->loops[abtn.loop].numFrames)
-        {
-            if (tview->loops[abtn.loop].RunNextLoop()) {
-                // go to next loop
-                abtn.loop++;
-                abtn.frame = 0;
-            }
-            else if (abtn.repeat) {
-                abtn.frame = 0;
-                // multi-loop anim, go back
-                while ((abtn.loop > 0) &&
-                    (tview->loops[abtn.loop - 1].RunNextLoop()))
-                    abtn.loop--;
-            }
-            else
-                return 1;
-        }
-    }
-
+    if (!CycleViewAnim(abtn.view, abtn.loop, abtn.frame, !abtn.direction,
+            abtn.repeat != 0 ? ANIM_REPEAT : ANIM_ONCE))
+        return false;
     CheckViewFrame(abtn.view, abtn.loop, abtn.frame);
-
-    // update the button's image
-    guibuts[abtn.buttonid].Image = tview->loops[abtn.loop].frames[abtn.frame].pic;
-    guibuts[abtn.buttonid].CurrentImage = guibuts[abtn.buttonid].Image;
-    guibuts[abtn.buttonid].PushedImage = 0;
-    guibuts[abtn.buttonid].MouseOverImage = 0;
-    guibuts[abtn.buttonid].NotifyParentChanged();
-
-    abtn.wait = abtn.speed + tview->loops[abtn.loop].frames[abtn.frame].speed;
-    return 0;
+    abtn.wait = abtn.speed + views[abtn.view].loops[abtn.loop].frames[abtn.frame].speed;
+    UpdateButtonState(abtn);
+    return true;
 }
 
 void StopButtonAnimation(int idxn) {

Engine/ac/button.h
@@ -37,7 +37,8 @@ void		Button_SetPushedGraphic(GUIButton *guil, int slotn);
 int			Button_GetTextColor(GUIButton *butt);
 void		Button_SetTextColor(GUIButton *butt, int newcol);
 
-int			UpdateAnimatingButton(int bu);
+// Update button's animation, returns whether the animation continues
+bool        UpdateAnimatingButton(int bu);
 size_t      GetAnimatingButtonCount();
 AnimatingGUIButton *GetAnimatingButtonByIndex(int idxn);
 void        AddButtonAnimation(const AnimatingGUIButton &abtn);

Engine/ac/object.cpp
@@ -27,6 +27,7 @@
 #include "ac/string.h"
 #include "ac/system.h"
 #include "ac/view.h"
+#include "ac/viewframe.h"
 #include "ac/walkablearea.h"
 #include "debug/debug_log.h"
 #include "gui/guimain.h"
@@ -568,6 +569,91 @@ int check_click_on_object(int roomx, int roomy, int mood)
     if (aa < 0) return 0;
     RunObjectInteraction(aa, mood);
     return 1;
+}
+
+// General view animation algorithm: find next loop and frame, depending on anim settings
+bool CycleViewAnim(int view, uint16_t &o_loop, uint16_t &o_frame, bool forwards, int repeat)
+{
+    // Allow multi-loop repeat: idk why, but original engine behavior
+    // was to only check this for forward animation, not backward
+    const bool multi_loop_repeat = !forwards || (play.no_multiloop_repeat == 0);
+
+    ViewStruct *aview = &views[view];
+    uint16_t loop = o_loop;
+    uint16_t frame = o_frame;
+    bool done = false;
+    
+    if (forwards)
+    {
+        if (frame + 1 >= aview->loops[loop].numFrames)
+        { // Reached the last frame in the loop, find out what to do next
+            if (aview->loops[loop].RunNextLoop())
+            {
+                // go to next loop
+                loop++;
+                frame = 0;
+            }
+            else
+            {
+                // If either ANIM_REPEAT or ANIM_ONCERESET:
+                // reset to the beginning of a multiloop animation
+                if (repeat != ANIM_ONCE)
+                {
+                    frame = 0;
+                    if (multi_loop_repeat)
+                        while ((loop > 0) && (aview->loops[loop - 1].RunNextLoop()))
+                            loop--;
+                }
+                else
+                { // if ANIM_ONCE, stop at the last frame
+                    frame = aview->loops[loop].numFrames - 1;
+                }
+
+                if (repeat != ANIM_REPEAT) // either ANIM_ONCE or ANIM_ONCERESET
+                    done = true; // finished animation
+            }
+        }
+        else
+            frame++;
+    }
+    else // backwards
+    {
+        if (frame == 0)
+        { // Reached the first frame in the loop, find out what to do next
+            if ((loop > 0) && aview->loops[loop - 1].RunNextLoop())
+            {
+                // go to next loop
+                loop--;
+                frame = aview->loops[loop].numFrames - 1;
+            }
+            else
+            {
+                // If either ANIM_REPEAT or ANIM_ONCERESET:
+                // reset to the beginning of a multiloop animation
+                if (repeat != ANIM_ONCE)
+                {
+                    if (multi_loop_repeat)
+                        while (aview->loops[loop].RunNextLoop())
+                            loop++;
+                    frame = aview->loops[loop].numFrames - 1;
+                }
+                else
+                { // if ANIM_ONCE, stop at the first frame
+                    frame = 0;
+                }
+
+                if (repeat != ANIM_REPEAT) // either ANIM_ONCE or ANIM_ONCERESET
+                    done = true; // finished animation
+            }
+        }
+        else
+            frame--;
+    }
+
+    // Update object values
+    o_loop = loop;
+    o_frame = frame;
+    return !done; // have we finished animating?
 }
 
 //=============================================================================

Engine/ac/object.h
@@ -84,6 +84,10 @@ int     is_pos_in_sprite(int xx,int yy,int arx,int ary, Common::Bitmap *sprit, i
 // X and Y co-ordinates must be in native format
 // X and Y are ROOM coordinates
 int     check_click_on_object(int roomx, int roomy, int mood);
+// General view animation algorithm: find next loop and frame, depending on anim settings;
+// loop and frame values are passed by reference and will be updated;
+// returns whether the animation should continue.
+bool    CycleViewAnim(int view, uint16_t &loop, uint16_t &frame, bool forwards, int repeat);
 
 #endif // __AGS_EE_AC__OBJECT_H
 

Engine/ac/roomobject.h
@@ -58,8 +58,6 @@ struct RoomObject {
     inline bool has_explicit_tint()  const { return (flags & OBJF_HASTINT) != 0; }
 
 	void UpdateCyclingView(int ref_id);
-	void update_cycle_view_forwards();
-	void update_cycle_view_backwards();
 
     void ReadFromSavegame(Common::Stream *in, int save_ver);
     void WriteToSavegame(Common::Stream *out) const;

Engine/ac/runtime_defines.h
@@ -81,8 +81,11 @@ const int LegacyRoomVolumeFactor            = 30;
 #define CHANIM_REPEAT    2
 #define CHANIM_BACKWARDS 4
 #define ANIM_BACKWARDS 10
+// Animates once and stops at the *last* frame
 #define ANIM_ONCE      1
+// Animates infinitely until stopped by command
 #define ANIM_REPEAT    2
+// Animates once and stops, resetting to the very first frame
 #define ANIM_ONCERESET 3
 #define FONT_STATUSBAR  0
 #define FONT_NORMAL     play.normal_font

commit a46cef76f8b268c26011965ee6ffcd5b97b7c8ab

Engine: fixed RoomObject animation loosing parameters
Was broken by fd28a2548e39c85478d98e26b51713cd687acada

Engine/ac/roomobject.cpp
@@ -77,7 +77,8 @@ void RoomObject::UpdateCyclingView(int ref_id)
     if (view == RoomObject::NoView) return;
     if (wait>0) { wait--; return; }
 
-    cycling = CycleViewAnim(view, loop, frame, cycling < ANIM_BACKWARDS, cycling % ANIM_BACKWARDS);
+    if (!CycleViewAnim(view, loop, frame, cycling < ANIM_BACKWARDS, cycling % ANIM_BACKWARDS))
+        cycling = 0; // finished animating
 
     ViewFrame*vfptr=&views[view].loops[loop].frames[frame];
     if (vfptr->pic > UINT16_MAX)
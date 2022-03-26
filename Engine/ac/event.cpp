adventuregamestudio/ags

commit a2f9a5ba94377686762b2828df4fccbe661d2b03

Engine: store game events in std::vector, remove event cap

Engine/ac/event.cpp
@@ -50,8 +50,7 @@ extern RGB old_palette[256];
 int in_enters_screen=0,done_es_error = 0;
 int in_leaves_screen = -1;
 
-EventHappened event[MAXEVENTS+1];
-int numevents=0;
+std::vector<EventHappened> events;
 
 const char*evblockbasename;
 int evblocknum;
@@ -130,13 +129,13 @@ void run_event_block_inv(int invNum, int event) {
 
 // event list functions
 void setevent(int evtyp,int ev1,int ev2,int ev3) {
-    event[numevents].type=evtyp;
-    event[numevents].data1=ev1;
-    event[numevents].data2=ev2;
-    event[numevents].data3=ev3;
-    event[numevents].player=game.playercharacter;
-    numevents++;
-    if (numevents>=MAXEVENTS) quit("too many events posted");
+    EventHappened evt;
+    evt.type = evtyp;
+    evt.data1 = ev1;
+    evt.data2 = ev2;
+    evt.data3 = ev3;
+    evt.player = game.playercharacter;
+    events.push_back(evt);
 }
 
 // TODO: this is kind of a hack, which forces event to be processed even if
@@ -150,7 +149,7 @@ void force_event(int evtyp,int ev1,int ev2,int ev3)
         setevent(evtyp, ev1, ev2, ev3);
 }
 
-void process_event(EventHappened*evp) {
+void process_event(const EventHappened *evp) {
     RuntimeScriptValue rval_null;
     if (evp->type==EV_TEXTSCRIPT) {
         ccError=0;
@@ -381,25 +380,22 @@ void runevent_now (int evtyp, int ev1, int ev2, int ev3) {
     process_event(&evh);
 }
 
-void processallevents(int numev,EventHappened*evlist) {
-    int dd;
-
+void processallevents() {
     if (inside_processevent)
         return;
 
     // make a copy of the events - if processing an event includes
     // a blocking function it will continue to the next game loop
     // and wipe out the event pointer we were passed
-    EventHappened copyOfList[MAXEVENTS];
-    memcpy(&copyOfList[0], &evlist[0], sizeof(EventHappened) * numev);
+    std::vector<EventHappened> evtcopy = std::move(events);
 
     int room_was = play.room_changes;
 
     inside_processevent++;
 
-    for (dd=0;dd<numev;dd++) {
+    for (size_t i = 0; i < evtcopy.size(); ++i) {
 
-        process_event(&copyOfList[dd]);
+        process_event(&evtcopy[i]);
 
         if (room_was != play.room_changes)
             break;  // changed room, so discard other events
@@ -408,10 +404,6 @@ void processallevents(int numev,EventHappened*evlist) {
     inside_processevent--;
 }
 
-void update_events() {
-    processallevents(numevents,&event[0]);
-    numevents=0;
-}
 // end event list functions
 
 

Engine/ac/event.h
@@ -32,8 +32,6 @@
 #define GE_LOSE_INV      8
 #define GE_RESTORE_GAME  9
 
-#define MAXEVENTS 15
-
 #define EV_TEXTSCRIPT 1
 #define EV_RUNEVBLOCK 2
 #define EV_FADEIN     3
@@ -46,9 +44,9 @@
 #define EVB_ROOM    2
 
 struct EventHappened {
-    int type;
-    int data1,data2,data3;
-    int player;
+    int type = 0;
+    int data1 = 0, data2 = 0, data3 = 0;
+    int player = -1;
 };
 
 int run_claimable_event(const char *tsname, bool includeRoom, int numParams, const RuntimeScriptValue *params, bool *eventWasClaimed);
@@ -59,18 +57,16 @@ void run_event_block_inv(int invNum, int event);
 // event list functions
 void setevent(int evtyp,int ev1=0,int ev2=-1000,int ev3=0);
 void force_event(int evtyp,int ev1=0,int ev2=-1000,int ev3=0);
-void process_event(EventHappened*evp);
+void process_event(const EventHappened *evp);
 void runevent_now (int evtyp, int ev1, int ev2, int ev3);
-void processallevents(int numev,EventHappened*evlist);
-void update_events();
+void processallevents();
 // end event list functions
 void ClaimEvent();
 
 extern int in_enters_screen,done_es_error;
 extern int in_leaves_screen;
 
-extern EventHappened event[MAXEVENTS+1];
-extern int numevents;
+extern std::vector<EventHappened> events;
 
 extern const char*evblockbasename;
 extern int evblocknum;

Engine/ac/room.cpp
@@ -82,7 +82,6 @@ extern int displayed_room;
 extern RoomObject*objs;
 extern ccInstance *roominst;
 extern AGSPlatformDriver *platform;
-extern int numevents;
 extern CharacterCache *charcache;
 extern CharacterExtras *charextra;
 extern int done_es_error;
@@ -279,7 +278,7 @@ void unload_old_room() {
     }
 
     cancel_all_scripts();
-    numevents = 0;  // cancel any pending room events
+    events.clear();  // cancel any pending room events
 
     if (roomBackgroundBmp != nullptr)
     {

Engine/main/game_run.cpp
@@ -90,7 +90,7 @@ extern char check_dynamic_sprites_at_exit;
 // Checks if user interface should remain disabled for now
 static int ShouldStayInWaitMode();
 
-static int numEventsAtStartOfFunction;
+static size_t numEventsAtStartOfFunction;
 static auto t1 = AGS_Clock::now();  // timer for FPS // ... 't1'... how very appropriate.. :)
 
 #define UNTIL_ANIMEND   1
@@ -192,7 +192,7 @@ static int game_loop_check_ground_level_interactions()
         if ((restrict_until) && (!ShouldStayInWaitMode())) {
             // cancel the Rep Exec and Stands on Hotspot events that
             // we just added -- otherwise the event queue gets huge
-            numevents = numEventsAtStartOfFunction;
+            events.resize(numEventsAtStartOfFunction);
             return 0;
         }
     } // end if checking ground level interactions
@@ -565,7 +565,7 @@ static void check_controls() {
     check_keyboard_controls();
 }
 
-static void check_room_edges(int numevents_was)
+static void check_room_edges(size_t numevents_was)
 {
     if ((IsInterfaceEnabled()) && (IsGamePaused() == 0) &&
         (in_new_room == 0) && (new_room_was == 0)) {
@@ -573,7 +573,7 @@ static void check_room_edges(int numevents_was)
             // if not in Player Enters Screen (allow walking in from off-screen)
             int edgesActivated[4] = {0, 0, 0, 0};
             // Only do it if nothing else has happened (eg. mouseclick)
-            if ((numevents == numevents_was) &&
+            if ((events.size() == numevents_was) &&
                 ((play.ground_level_areas_disabled & GLED_INTERACTION) == 0)) {
 
                     if (playerchar->x <= thisroom.Edges.Left)
@@ -609,7 +609,7 @@ static void game_loop_check_controls(bool checkControls)
     // don't let the player do anything before the screen fades in
     if ((in_new_room == 0) && (checkControls)) {
         int inRoom = displayed_room;
-        int numevents_was = numevents;
+        size_t numevents_was = events.size();
         check_controls();
         check_room_edges(numevents_was);
         // If an inventory interaction changed the room
@@ -684,7 +684,7 @@ static void game_loop_update_events()
     if (in_new_room>0)
         setevent(EV_FADEIN,0,0,0);
     in_new_room=0;
-    update_events();
+    processallevents();
     if ((new_room_was > 0) && (in_new_room == 0)) {
         // if in a new room, and the room wasn't just changed again in update_events,
         // then queue the Enters Screen scripts
@@ -755,7 +755,7 @@ void UpdateGameOnce(bool checkControls, IDriverDependantBitmap *extraBitmap, int
 
     sys_evt_process_pending();
 
-    numEventsAtStartOfFunction = numevents;
+    numEventsAtStartOfFunction = events.size();
 
     if (want_exit) {
         ProperExit();

commit 6a71d858819787001ad50516acae7164537bb02f

Engine: fixed game events not cleared inside blocking action
Was broken by a2f9a5ba94377686762b2828df4fccbe661d2b03

Engine/ac/event.cpp
@@ -384,7 +384,10 @@ void runevent_now (int evtyp, int ev1, int ev2, int ev3) {
 
 void processallevents() {
     if (inside_processevent)
+    {
+        events.clear(); // flush queued events
         return;
+    }
 
     // make a copy of the events - if processing an event includes
     // a blocking function it will continue to the next game loop
adventuregamestudio/ags

commit 9737cabb8e7562122f8c471793bd994685d72bfe

Engine: more config parse helpers: reading boolean ints, min-max range

Engine/platform/windows/setup/winsetup.cpp
@@ -155,7 +155,7 @@ void WinConfig::Load(const ConfigTree &cfg)
     GameResolution.Width = CfgReadInt(cfg, "gameproperties", "resolution_width", GameResolution.Width);
     GameResolution.Height = CfgReadInt(cfg, "gameproperties", "resolution_height", GameResolution.Height);
     GameColourDepth = CfgReadInt(cfg, "gameproperties", "resolution_bpp", GameColourDepth);
-    LetterboxByDesign = CfgReadInt(cfg, "gameproperties", "legacy_letterbox", 0) != 0;
+    LetterboxByDesign = CfgReadBoolInt(cfg, "gameproperties", "legacy_letterbox", false);
 
     GfxDriverId = CfgReadString(cfg, "graphics", "driver", GfxDriverId);
     GfxFilterId = CfgReadString(cfg, "graphics", "filter", GfxFilterId);
@@ -166,8 +166,8 @@ void WinConfig::Load(const ConfigTree &cfg)
     WinGameFrame = parse_scaling_option(CfgReadString(cfg, "graphics", "game_scale_win"), WinGameFrame);
 
     RefreshRate = CfgReadInt(cfg, "graphics", "refresh", RefreshRate);
-    Windowed = CfgReadInt(cfg, "graphics", "windowed", Windowed ? 1 : 0) != 0;
-    VSync = CfgReadInt(cfg, "graphics", "vsync", VSync ? 1 : 0) != 0;
+    Windowed = CfgReadBoolInt(cfg, "graphics", "windowed", Windowed);
+    VSync = CfgReadBoolInt(cfg, "graphics", "vsync", VSync);
     int locked_render_at_screenres = CfgReadInt(cfg, "gameproperties", "render_at_screenres", -1);
     if (locked_render_at_screenres < 0)
         RenderAtScreenRes = CfgReadInt(cfg, "graphics", "render_at_screenres", RenderAtScreenRes ? 1 : 0) != 0;
@@ -176,11 +176,11 @@ void WinConfig::Load(const ConfigTree &cfg)
 
     AntialiasSprites = CfgReadInt(cfg, "misc", "antialias", AntialiasSprites ? 1 : 0) != 0;
 
-    AudioEnabled = CfgReadInt(cfg, "sound", "enabled", AudioEnabled ? 1 : 0) != 0;
+    AudioEnabled = CfgReadBoolInt(cfg, "sound", "enabled", AudioEnabled);
     AudioDriverId = CfgReadString(cfg, "sound", "driver", AudioDriverId);
-    UseVoicePack = CfgReadInt(cfg, "sound", "usespeech", UseVoicePack ? 1 : 0) != 0;
+    UseVoicePack = CfgReadBoolInt(cfg, "sound", "usespeech", UseVoicePack);
 
-    MouseAutoLock = CfgReadInt(cfg, "mouse", "auto_lock", MouseAutoLock ? 1 : 0) != 0;
+    MouseAutoLock = CfgReadBoolInt(cfg, "mouse", "auto_lock", MouseAutoLock);
     MouseSpeed = CfgReadFloat(cfg, "mouse", "speed", 1.f);
     if (MouseSpeed <= 0.f)
         MouseSpeed = 1.f;
@@ -714,11 +714,11 @@ INT_PTR WinSetupDialog::OnInitDialog(HWND hwnd)
     if (!File::TestReadFile("speech.vox"))
         EnableWindow(_hUseVoicePack, FALSE);
 
-    if (CfgReadInt(_cfgIn, "disabled", "speechvox", 0) != 0)
+    if (CfgReadBoolInt(_cfgIn, "disabled", "speechvox"))
         EnableWindow(_hUseVoicePack, FALSE);
-    if (CfgReadInt(_cfgIn, "disabled", "filters", 0) != 0)
+    if (CfgReadBoolInt(_cfgIn, "disabled", "filters"))
         EnableWindow(_hGfxFilterList, FALSE);
-    if (CfgReadInt(_cfgIn, "disabled", "render_at_screenres", 0) != 0 ||
+    if (CfgReadBoolInt(_cfgIn, "disabled", "render_at_screenres") ||
         CfgReadInt(_cfgIn, "gameproperties", "render_at_screenres", -1) >= 0)
         EnableWindow(_hRenderAtScreenRes, FALSE);
 
@@ -1004,7 +1004,7 @@ void WinSetupDialog::FillGfxFilterList()
     for (size_t i = 0; i < _drvDesc->FilterList.size(); ++i)
     {
         const GfxFilterInfo &info = _drvDesc->FilterList[i];
-        if (CfgReadInt(_cfgIn, "disabled", info.Id, 0) == 0)
+        if (CfgReadBoolInt(_cfgIn, "disabled", info.Id))
             AddString(_hGfxFilterList, STR(info.Name), (DWORD_PTR)info.Id.GetCStr());
     }
 

Common/util/ini_util.cpp
@@ -44,27 +44,43 @@ bool CfgReadItem(const ConfigTree &cfg, const String &sectn, const String &item,
     return false;
 }
 
-int CfgReadInt(const ConfigTree &cfg, const String &sectn, const String &item, int def_value)
+int CfgReadInt(const ConfigTree &cfg, const String &sectn, const String &item, int def)
 {
     String str;
     if (!CfgReadItem(cfg, sectn, item, str))
-        return def_value;
-    return StrUtil::StringToInt(str, def_value);
+        return def;
+    return StrUtil::StringToInt(str, def);
 }
 
-float CfgReadFloat(const ConfigTree &cfg, const String &sectn, const String &item, float def_value)
+int CfgReadInt(const ConfigTree &cfg, const String &sectn, const String &item, int min, int max, int def)
 {
-    String str;
-    if (!CfgReadItem(cfg, sectn, item, str))
-        return def_value;
-    return StrUtil::StringToFloat(str, def_value);
+    int val = CfgReadInt(cfg, sectn, item, def);
+    if ((val < min) || (val > max))
+        return def;
+    return val;
 }
 
-String CfgReadString(const ConfigTree &cfg, const String &sectn, const String &item, const String &def_value)
+float CfgReadFloat(const ConfigTree &cfg, const String &sectn, const String &item, float def)
 {
     String str;
     if (!CfgReadItem(cfg, sectn, item, str))
-        return def_value;
+        return def;
+    return StrUtil::StringToFloat(str, def);
+}
+
+float CfgReadFloat(const ConfigTree &cfg, const String &sectn, const String &item, float min, float max, float def)
+{
+    float val = CfgReadFloat(cfg, sectn, item, def);
+    if ((val < min) || (val > max))
+        return def;
+    return val;
+}
+
+String CfgReadString(const ConfigTree &cfg, const String &sectn, const String &item, const String &def)
+{
+    String str;
+    if (!CfgReadItem(cfg, sectn, item, str))
+        return def;
     return str;
 }
 

Common/util/ini_util.h
@@ -33,12 +33,18 @@ typedef std::map<String, StringOrderMap> ConfigTree;
 //
 // Helper functions for parsing values in a ConfigTree
 bool    CfgReadItem(const ConfigTree &cfg, const String &sectn, const String &item, String &value);
-int     CfgReadInt(const ConfigTree &cfg, const String &sectn, const String &item, int def_value = 0);
-float   CfgReadFloat(const ConfigTree &cfg, const String &sectn, const String &item, float def_value = 0.f);
-String  CfgReadString(const ConfigTree &cfg, const String &sectn, const String &item, const String &def_value = "");
+int     CfgReadInt(const ConfigTree &cfg, const String &sectn, const String &item, int def = 0);
+int     CfgReadInt(const ConfigTree &cfg, const String &sectn, const String &item, int min, int max, int def = 0);
+inline bool CfgReadBoolInt(const ConfigTree &cfg, const String &sectn, const String &item, bool def = false)
+            { return CfgReadInt(cfg, sectn, item, 0, 1, def) != 0; }
+float   CfgReadFloat(const ConfigTree &cfg, const String &sectn, const String &item, float def = 0.f);
+float   CfgReadFloat(const ConfigTree &cfg, const String &sectn, const String &item, float min, float max, float def = 0.f);
+String  CfgReadString(const ConfigTree &cfg, const String &sectn, const String &item, const String &def = "");
 //
 // Helper functions for writing values into a ConfigTree
 void    CfgWriteInt(ConfigTree &cfg, const String &sectn, const String &item, int value);
+inline void CfgWriteBoolInt(ConfigTree &cfg, const String &sectn, const String &item, bool value)
+            { CfgWriteInt(cfg, sectn, item, static_cast<int>(value)); }
 void    CfgWriteFloat(ConfigTree &cfg, const String &sectn, const String &item, float value);
 void    CfgWriteFloat(ConfigTree &cfg, const String &sectn, const String &item, float value, unsigned precision);
 void    CfgWriteString(ConfigTree &cfg, const String &sectn, const String &item, const String &value);

Engine/debug/debug.cpp
@@ -276,7 +276,7 @@ void apply_debug_config(const ConfigTree &cfg)
         { DbgGroupOption(kDbgGroup_Main, kDbgMsg_Info),
           DbgGroupOption(kDbgGroup_SDL, kDbgMsg_Info),
         });
-    bool legacy_log_enabled = CfgReadInt(cfg, "misc", "log", 0) != 0;
+    bool legacy_log_enabled = CfgReadBoolInt(cfg, "misc", "log", false);
     apply_log_config(cfg, OutputFileID,
         /* defaults */
         legacy_log_enabled,

Engine/main/config.cpp
@@ -222,7 +222,7 @@ static void read_legacy_graphics_config(const ConfigTree &cfg)
         usetup.override_upscale = true; // run low-res game in high-res mode
     }
 
-    usetup.Screen.Windowed = CfgReadInt(cfg, "misc", "windowed") > 0;
+    usetup.Screen.Windowed = CfgReadBoolInt(cfg, "misc", "windowed");
     usetup.Screen.DriverID = CfgReadString(cfg, "misc", "gfxdriver", usetup.Screen.DriverID);
 
     // Window setup: style and size definition, game frame style
@@ -241,8 +241,8 @@ static void read_legacy_graphics_config(const ConfigTree &cfg)
             if (!usetup.Screen.Windowed)
             {
                 bool allow_borders = 
-                    (CfgReadInt(cfg, "misc", "sideborders") > 0 || CfgReadInt(cfg, "misc", "forceletterbox") > 0 ||
-                     CfgReadInt(cfg, "misc", "prefer_sideborders") > 0 || CfgReadInt(cfg, "misc", "prefer_letterbox") > 0);
+                    (CfgReadBoolInt(cfg, "misc", "sideborders") || CfgReadBoolInt(cfg, "misc", "forceletterbox") ||
+                     CfgReadBoolInt(cfg, "misc", "prefer_sideborders") || CfgReadBoolInt(cfg, "misc", "prefer_letterbox"));
                 usetup.Screen.FsGameFrame = allow_borders ? kFrame_Proportional : kFrame_Stretch;
             }
         }
@@ -258,7 +258,7 @@ static void read_legacy_graphics_config(const ConfigTree &cfg)
         }
 
         // AGS 3.5.* gfx mode with screen definition
-        const bool is_windowed = CfgReadInt(cfg, "graphics", "windowed") != 0;
+        const bool is_windowed = CfgReadBoolInt(cfg, "graphics", "windowed");
         WindowSetup &ws = is_windowed ? usetup.Screen.WinSetup : usetup.Screen.FsSetup;
         const WindowMode wm = is_windowed ? kWnd_Windowed : kWnd_Fullscreen;
         ScreenSizeDefinition scr_def = parse_legacy_screendef(CfgReadString(cfg, "graphics", "screen_def"));
@@ -375,7 +375,7 @@ void override_config_ext(ConfigTree &cfg)
 void apply_config(const ConfigTree &cfg)
 {
     {
-        usetup.audio_enabled = CfgReadInt(cfg, "sound", "enabled", usetup.audio_enabled) != 0;
+        usetup.audio_enabled = CfgReadBoolInt(cfg, "sound", "enabled", usetup.audio_enabled);
         usetup.audio_driver = CfgReadString(cfg, "sound", "driver");
 
         // Legacy graphics settings has to be translated into new options;
@@ -384,7 +384,7 @@ void apply_config(const ConfigTree &cfg)
 
         // Graphics mode
         usetup.Screen.DriverID = CfgReadString(cfg, "graphics", "driver", usetup.Screen.DriverID);
-        usetup.Screen.Windowed = CfgReadInt(cfg, "graphics", "windowed", usetup.Screen.Windowed ? 1 : 0) > 0;
+        usetup.Screen.Windowed = CfgReadBoolInt(cfg, "graphics", "windowed", usetup.Screen.Windowed);
         usetup.Screen.FsSetup =
             parse_window_mode(CfgReadString(cfg, "graphics", "fullscreen", "default"), false, usetup.Screen.FsSetup);
         usetup.Screen.WinSetup =
@@ -402,8 +402,8 @@ void apply_config(const ConfigTree &cfg)
 #endif
 
         usetup.Screen.Params.RefreshRate = CfgReadInt(cfg, "graphics", "refresh");
-        usetup.Screen.Params.VSync = CfgReadInt(cfg, "graphics", "vsync") > 0;
-        usetup.RenderAtScreenRes = CfgReadInt(cfg, "graphics", "render_at_screenres") > 0;
+        usetup.Screen.Params.VSync = CfgReadBoolInt(cfg, "graphics", "vsync");
+        usetup.RenderAtScreenRes = CfgReadBoolInt(cfg, "graphics", "render_at_screenres");
         usetup.Supersampling = CfgReadInt(cfg, "graphics", "supersampling", 1);
         usetup.software_render_driver = CfgReadString(cfg, "graphics", "software_driver");
 
@@ -413,12 +413,12 @@ void apply_config(const ConfigTree &cfg)
                 rotation_str, CstrArr<kNumScreenRotationOptions>{ "unlocked", "portrait", "landscape" },
                 usetup.rotation);
 
-        usetup.enable_antialiasing = CfgReadInt(cfg, "misc", "antialias") > 0;
+        usetup.enable_antialiasing = CfgReadBoolInt(cfg, "misc", "antialias");
 
         // This option is backwards (usevox is 0 if no_speech_pack)
-        usetup.no_speech_pack = CfgReadInt(cfg, "sound", "usespeech", 1) == 0;
+        usetup.no_speech_pack = !CfgReadBoolInt(cfg, "sound", "usespeech", true);
 
-        usetup.clear_cache_on_room_change = CfgReadInt(cfg, "misc", "clear_cache_on_room_change", usetup.clear_cache_on_room_change) != 0;
+        usetup.clear_cache_on_room_change = CfgReadBoolInt(cfg, "misc", "clear_cache_on_room_change", usetup.clear_cache_on_room_change);
         usetup.user_data_dir = CfgReadString(cfg, "misc", "user_data_dir");
         usetup.shared_data_dir = CfgReadString(cfg, "misc", "shared_data_dir");
 
@@ -428,7 +428,7 @@ void apply_config(const ConfigTree &cfg)
         if (cache_size_kb > 0)
             spriteset.SetMaxCacheSize((size_t)cache_size_kb * 1024);
 
-        usetup.mouse_auto_lock = CfgReadInt(cfg, "mouse", "auto_lock") > 0;
+        usetup.mouse_auto_lock = CfgReadBoolInt(cfg, "mouse", "auto_lock");
 
         usetup.mouse_speed = CfgReadFloat(cfg, "mouse", "speed", 1.f);
         if (usetup.mouse_speed <= 0.f)
@@ -437,7 +437,7 @@ void apply_config(const ConfigTree &cfg)
         usetup.mouse_ctrl_when = StrUtil::ParseEnum<MouseControlWhen>(
             mouse_str, CstrArr<kNumMouseCtrlOptions>{ "never", "fullscreen", "always" },
                 usetup.mouse_ctrl_when);
-        usetup.mouse_ctrl_enabled = CfgReadInt(cfg, "mouse", "control_enabled", usetup.mouse_ctrl_enabled) > 0;
+        usetup.mouse_ctrl_enabled = CfgReadBoolInt(cfg, "mouse", "control_enabled", usetup.mouse_ctrl_enabled);
         mouse_str = CfgReadString(cfg, "mouse", "speed_def", "current_display");
         usetup.mouse_speed_def = StrUtil::ParseEnum<MouseSpeedDef>(
             mouse_str, CstrArr<kNumMouseSpeedDefs>{ "absolute", "current_display" }, usetup.mouse_speed_def);
@@ -446,7 +446,7 @@ void apply_config(const ConfigTree &cfg)
         String override_os = CfgReadString(cfg, "override", "os");
         usetup.override_script_os = StrUtil::ParseEnum<eScriptSystemOSID>(override_os,
             CstrArr<eNumOS>{"", "dos", "win", "linux", "mac", "android", "ios", "psp", "web", "freebsd"}, eOS_Unknown);
-        usetup.override_upscale = CfgReadInt(cfg, "override", "upscale", usetup.override_upscale) > 0;
+        usetup.override_upscale = CfgReadBoolInt(cfg, "override", "upscale", usetup.override_upscale);
     }
 
     // Apply logging configuration

Engine/main/engine.cpp
@@ -1051,7 +1051,7 @@ void engine_read_config(ConfigTree &cfg)
     if (usetup.user_conf_dir.IsEmpty())
         usetup.user_conf_dir = CfgReadString(cfg, "misc", "user_conf_dir");
     if (usetup.user_conf_dir.IsEmpty()) // also try deprecated option
-        usetup.user_conf_dir = CfgReadInt(cfg, "misc", "localuserconf") != 0 ? "." : "";
+        usetup.user_conf_dir = CfgReadBoolInt(cfg, "misc", "localuserconf") ? "." : "";
     // Test if the file is writeable, if it is then both engine and setup
     // applications may actually use it fully as a user config, otherwise
     // fallback to default behavior.

commit c13b8ba95135f87f6e81bab68406c5f7f0605b10

WinSetup: fixed condition of disabling gfx filters
Was broken by 9737cabb8e7562122f8c471793bd994685d72bfe

Engine/platform/windows/setup/winsetup.cpp
@@ -1004,7 +1004,7 @@ void WinSetupDialog::FillGfxFilterList()
     for (size_t i = 0; i < _drvDesc->FilterList.size(); ++i)
     {
         const GfxFilterInfo &info = _drvDesc->FilterList[i];
-        if (CfgReadBoolInt(_cfgIn, "disabled", info.Id))
+        if (!CfgReadBoolInt(_cfgIn, "disabled", info.Id))
             AddString(_hGfxFilterList, STR(info.Name), (DWORD_PTR)info.Id.GetCStr());
     }
 
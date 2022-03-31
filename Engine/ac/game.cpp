adventuregamestudio/ags

commit c1a51dd35f8da9e367654c04dfea577af1176462

Android, iOS: reimplemented selectLatestSavegame in the engine

Engine/main/engine.cpp
@@ -99,10 +99,6 @@ extern RGB palette[256];
 extern CharacterExtras *charextra;
 extern CharacterInfo*playerchar;
 
-#if AGS_PLATFORM_OS_ANDROID
-extern "C" void selectLatestSavegame();
-#endif
-
 ResourcePaths ResPaths;
 
 t_engine_pre_init_callback engine_pre_init_callback = nullptr;
@@ -897,9 +893,13 @@ void engine_prepare_to_start_game()
 
     engine_setup_scsystem_auxiliary();
 
-#if AGS_PLATFORM_OS_ANDROID
+#if (AGS_PLATFORM_OS_ANDROID) || (AGS_PLATFORM_OS_IOS)
     if (psp_load_latest_savegame)
-        selectLatestSavegame();
+    {
+        int slot = GetLastSaveSlot();
+        if (slot >= 0)
+            loadSaveGameOnStartup = get_save_game_path(slot);
+    }
 #endif
 }
 
@@ -1327,7 +1327,7 @@ int initialize_engine(const ConfigTree &startup_opts)
 
 	allegro_bitmap_test_init();
 
-    initialize_start_and_play_game(override_start_room, loadSaveGameOnStartup);
+    initialize_start_and_play_game(override_start_room, loadSaveGameOnStartup.GetCStr());
 
     return EXIT_NORMAL;
 }

Engine/ac/global_game.cpp
@@ -191,7 +191,7 @@ int LoadSaveSlotScreenshot(int slnum, int width, int height) {
     return gotSlot;
 }
 
-void FillSaveList(std::vector<SaveListItem> &saves, size_t max_count)
+void FillSaveList(std::vector<SaveListItem> &saves, unsigned top_index, size_t max_count)
 {
     if (max_count == 0)
         return; // duh
@@ -204,7 +204,7 @@ void FillSaveList(std::vector<SaveListItem> &saves, size_t max_count)
     {
         int saveGameSlot = Path::GetFileExtension(ff.Current()).ToInt();
         // only list games .000 to .XXX (to allow higher slots for other perposes)
-        if (saveGameSlot < 0 || saveGameSlot > TOP_LISTEDSAVESLOT)
+        if (saveGameSlot < 0 || saveGameSlot > top_index)
             continue;
         String description;
         GetSaveSlotDescription(saveGameSlot, description);
@@ -212,6 +212,16 @@ void FillSaveList(std::vector<SaveListItem> &saves, size_t max_count)
         if (saves.size() >= max_count)
             break;
     }
+}
+
+int GetLastSaveSlot()
+{
+    std::vector<SaveListItem> saves;
+    FillSaveList(saves, RESTART_POINT_SAVE_GAME_NUMBER - 1);
+    if (saves.size() == 0)
+        return -1;
+    std::sort(saves.rbegin(), saves.rend());
+    return saves[0].Slot;
 }
 
 void SetGlobalInt(int index,int valu) {

Engine/ac/global_game.h
@@ -44,7 +44,9 @@ void RestoreGameSlot(int slnum);
 void DeleteSaveSlot (int slnum);
 int  GetSaveSlotDescription(int slnum,char*desbuf);
 int  LoadSaveSlotScreenshot(int slnum, int width, int height);
-void FillSaveList(std::vector<SaveListItem> &saves, size_t max_count = -1);
+void FillSaveList(std::vector<SaveListItem> &saves, unsigned top_index, size_t max_count = -1);
+// Find the latest save slot, returns the slot index or -1 at failure
+int  GetLastSaveSlot();
 void PauseGame();
 void UnPauseGame();
 int  IsGamePaused();

Engine/ac/listbox.cpp
@@ -96,7 +96,7 @@ int ListBox_GetSaveGameSlots(GUIListBox *listbox, int index) {
 int ListBox_FillSaveGameList(GUIListBox *listbox) {
   // TODO: find out if limiting to MAXSAVEGAMES is still necessary here
   std::vector<SaveListItem> saves;
-  FillSaveList(saves, MAXSAVEGAMES);
+  FillSaveList(saves, TOP_LISTEDSAVESLOT, MAXSAVEGAMES);
   std::sort(saves.rbegin(), saves.rend());
 
   // fill in the list box

Engine/gui/guidialog.cpp
@@ -299,7 +299,7 @@ void preparesavegamelist(int ctrllist)
 {
   // TODO: find out if limiting to MAXSAVEGAMES is still necessary here
   std::vector<SaveListItem> saves;
-  FillSaveList(saves, MAXSAVEGAMES);
+  FillSaveList(saves, TOP_LISTEDSAVESLOT, MAXSAVEGAMES);
   std::sort(saves.rbegin(), saves.rend());
 
   // fill in the list box and global savegameindex[] array for backward compatibilty

Engine/main/main.cpp
@@ -78,7 +78,7 @@ bool justTellInfo = false;
 bool attachToParentConsole = false;
 bool hideMessageBoxes = false;
 std::set<String> tellInfoKeys;
-const char *loadSaveGameOnStartup = nullptr;
+String loadSaveGameOnStartup;
 
 #if ! AGS_PLATFORM_DEFINES_PSP_VARS
 int psp_video_framedrop = 1;

Engine/main/main.h
@@ -42,7 +42,7 @@ extern int override_start_room;
 extern bool justRegisterGame;
 extern bool justUnRegisterGame;
 extern bool justTellInfo;
-extern const char *loadSaveGameOnStartup;
+extern AGS::Common::String loadSaveGameOnStartup;
 
 extern int psp_video_framedrop;
 extern int psp_ignore_acsetup_cfg_file;

Engine/platform/android/acpland.cpp
@@ -66,11 +66,6 @@ String android_save_directory = "";
 char psp_game_file_name[256];
 char* psp_game_file_name_pointer = psp_game_file_name;
 
-bool psp_load_latest_savegame = false;
-extern char saveGameDirectory[260];
-extern const char *loadSaveGameOnStartup;
-char lastSaveGameName[200];
-
 // NOTE: the JVM can't use JNI outside here due to C++ name mangling
 extern "C" 
 {
@@ -324,40 +319,6 @@ JNIEXPORT jint JNICALL
   }
 
   return i;
-}
-
-void selectLatestSavegame()
-{
-  DIR* dir;
-  struct dirent* entry;
-  struct stat statBuffer;
-  char buffer[200];
-  time_t lastTime = 0;
-
-  dir = opendir(saveGameDirectory);
-
-  if (dir)
-  {
-    while ((entry = readdir(dir)) != 0)
-    {
-      if (ags_strnicmp(entry->d_name, "agssave", 7) == 0)
-      {
-        if (ags_stricmp(entry->d_name, "agssave.999") != 0)
-        {
-          strcpy(buffer, saveGameDirectory);
-          strcat(buffer, entry->d_name);
-          stat(buffer, &statBuffer);
-          if (statBuffer.st_mtime > lastTime)
-          {
-            strcpy(lastSaveGameName, buffer);
-            loadSaveGameOnStartup = lastSaveGameName;
-            lastTime = statBuffer.st_mtime;
-          }
-        }
-      }
-    }
-    closedir(dir);
-  }
 }
 
 JNIEXPORT void JNICALL

commit 60fceb04c98a80b79e306f696ca5b3076ef5eaf7

Engine: fixed trying to load save at startup without being asked
Mistake introduced by c1a51dd35f8da9e367654c04dfea577af1176462

Engine/ac/game.cpp
@@ -321,6 +321,13 @@ String get_save_game_path(int slotNum)
     return Path::ConcatPaths(saveGameDirectory, get_save_game_filename(slotNum));
 }
 
+bool get_save_slotnum(const String &filename, int &slot)
+{
+    if (filename.CompareLeftNoCase("agssave.") == 0)
+        return sscanf(filename.GetCStr(), "agssave.%03d", &slot) == 1;
+    return false;
+}
+
 // Convert a path possibly containing path tags into acceptable save path
 // NOTE that the game script may issue an order to change the save directory to
 // a dir of a new name. While we let this work, we also try to keep these

Engine/ac/game.h
@@ -136,6 +136,8 @@ Common::String get_save_game_suffix();
 void set_save_game_suffix(const Common::String &suffix);
 // Returns full path to the save for the given slot number
 Common::String get_save_game_path(int slotNum);
+// Parses filename and retrieves save slot number, if present
+bool get_save_slotnum(const Common::String &filename, int &slot);
 void restore_game_dialog();
 void save_game_dialog();
 void free_do_once_tokens();

Engine/main/engine.cpp
@@ -1327,7 +1327,7 @@ int initialize_engine(const ConfigTree &startup_opts)
 
 	allegro_bitmap_test_init();
 
-    initialize_start_and_play_game(override_start_room, loadSaveGameOnStartup.GetCStr());
+    initialize_start_and_play_game(override_start_room, loadSaveGameOnStartup);
 
     return EXIT_NORMAL;
 }

Engine/main/game_start.cpp
@@ -67,18 +67,14 @@ void start_game_init_editor_debugging()
     }
 }
 
-void start_game_load_savegame_on_startup(const char *load_save)
+void start_game_load_savegame_on_startup(const String &load_save)
 {
-    if (load_save != nullptr)
+    if (!load_save.IsEmpty())
     {
-        int saveGameNumber = 1000;
-        const char *sgName = strstr(load_save, "agssave.");
-        if (sgName != nullptr)
-        {
-            sscanf(sgName, "agssave.%03d", &saveGameNumber);
-        }
+        int slot = 1000;
+        get_save_slotnum(load_save, slot);
         current_fade_out_effect();
-        try_restore_save(load_save, saveGameNumber);
+        try_restore_save(load_save, slot);
     }
 }
 
@@ -109,7 +105,7 @@ void start_game() {
     first_room_initialization();
 }
 
-void initialize_start_and_play_game(int override_start_room, const char *load_save)
+void initialize_start_and_play_game(int override_start_room, const String &load_save)
 {
     try { // BEGIN try for ALI3DEXception
 

Engine/main/game_start.h
@@ -18,7 +18,9 @@
 #ifndef __AGS_EE_MAIN__GAMESTART_H
 #define __AGS_EE_MAIN__GAMESTART_H
 
+#include "util/string.h"
+
 void start_game();
-void initialize_start_and_play_game(int override_start_room, const char *load_save);
+void initialize_start_and_play_game(int override_start_room, const AGS::Common::String &load_save);
 
 #endif // __AGS_EE_MAIN__GAMESTART_H
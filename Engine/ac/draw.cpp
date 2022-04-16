adventuregamestudio/ags

commit bcffc2cdeb24a426e2b048e8a98df9c84cb7c6f2

Engine: implemented gui control transparency in software mode

Engine/ac/draw.cpp
@@ -1215,18 +1215,26 @@ void repair_alpha_channel(Bitmap *dest, Bitmap *bgpic)
 
 
 // used by GUI renderer to draw images
-void draw_gui_sprite(Bitmap *ds, int pic, int x, int y, bool use_alpha, BlendMode blend_mode) 
+// NOTE: use_alpha arg is for backward compatibility (legacy draw modes)
+void draw_gui_sprite(Bitmap *ds, int pic, int x, int y, bool use_alpha, BlendMode blend_mode)
 {
-    Bitmap *sprite = spriteset[pic];
-    const bool ds_has_alpha  = ds->GetColorDepth() == 32;
-    const bool src_has_alpha = (game.SpriteInfos[pic].Flags & SPF_ALPHACHANNEL) != 0;
+    draw_gui_sprite(ds, use_alpha, x, y, spriteset[pic],
+        (game.SpriteInfos[pic].Flags & SPF_ALPHACHANNEL) != 0, blend_mode);
+}
 
-    if (use_alpha && game.options[OPT_NEWGUIALPHA] == kGuiAlphaRender_Proper)
+void draw_gui_sprite(Bitmap *ds, bool use_alpha, int x, int y, Bitmap *sprite, bool src_has_alpha,
+    BlendMode blend_mode, int alpha)
+{
+    if (alpha <= 0)
+        return;
+
+    const bool ds_has_alpha = use_alpha && (ds->GetColorDepth() == 32);
+    if (game.options[OPT_NEWGUIALPHA] == kGuiAlphaRender_Proper)
     {
-        GfxUtil::DrawSpriteBlend(ds, Point(x, y), sprite, blend_mode, ds_has_alpha, src_has_alpha);
+        GfxUtil::DrawSpriteBlend(ds, Point(x, y), sprite, blend_mode, ds_has_alpha, src_has_alpha, alpha);
     }
     // Backwards-compatible drawing
-    else if (use_alpha && ds_has_alpha && game.options[OPT_NEWGUIALPHA] == kGuiAlphaRender_AdditiveAlpha)
+    else if (ds_has_alpha && (game.options[OPT_NEWGUIALPHA] == kGuiAlphaRender_AdditiveAlpha) && (alpha == 0xFF))
     {
         if (src_has_alpha)
             set_additive_alpha_blender();
@@ -1236,7 +1244,7 @@ void draw_gui_sprite(Bitmap *ds, int pic, int x, int y, bool use_alpha, BlendMod
     }
     else
     {
-        GfxUtil::DrawSpriteWithTransparency(ds, sprite, x, y);
+        GfxUtil::DrawSpriteWithTransparency(ds, sprite, x, y, alpha);
     }
 }
 

Common/gui/guimain.cpp
@@ -285,6 +285,7 @@ void GUIMain::DrawWithControls(Bitmap *ds)
     if ((all_buttons_disabled >= 0) && (GUI::Options.DisabledStyle == kGuiDis_Blackout))
         return; // don't draw GUI controls
 
+    Bitmap tempbmp; // in case we need transforms
     for (size_t ctrl_index = 0; ctrl_index < _controls.size(); ++ctrl_index)
     {
         set_eip_guiobj(_ctrlDrawOrder[ctrl_index]);
@@ -296,11 +297,23 @@ void GUIMain::DrawWithControls(Bitmap *ds)
         if (!objToDraw->IsVisible())
             continue;
 
-        if (GUI::Options.ClipControls && objToDraw->IsContentClipped())
-            ds->SetClip(RectWH(objToDraw->X, objToDraw->Y, objToDraw->Width, objToDraw->Height));
+        // Depending on draw properties - draw directly on the gui surface, or use a buffer
+        if (objToDraw->GetTransparency() == 0)
+        {
+            if (GUI::Options.ClipControls && objToDraw->IsContentClipped())
+                ds->SetClip(RectWH(objToDraw->X, objToDraw->Y, objToDraw->Width, objToDraw->Height));
+            else
+                ds->ResetClip();
+            objToDraw->Draw(ds, objToDraw->X, objToDraw->Y);
+        }
         else
-            ds->ResetClip();
-        objToDraw->Draw(ds, objToDraw->X, objToDraw->Y);
+        {
+            const Rect rc = objToDraw->CalcGraphicRect(GUI::Options.ClipControls && objToDraw->IsContentClipped());
+            tempbmp.CreateTransparent(rc.GetWidth(), rc.GetHeight());
+            objToDraw->Draw(&tempbmp, objToDraw->X - rc.Left, objToDraw->Y - rc.Top);
+            draw_gui_sprite(ds, true, objToDraw->X, objToDraw->Y, &tempbmp, objToDraw->HasAlphaChannel(), kBlendMode_Alpha,
+                GfxDef::LegacyTrans255ToAlpha255(objToDraw->GetTransparency()));
+        }
 
         int selectedColour = 14;
 

Common/gui/guimain.h
@@ -256,6 +256,9 @@ extern bool is_sprite_alpha(int spr);
 // This function has distinct implementations in Engine and Editor
 extern void draw_gui_sprite(Common::Bitmap *ds, int spr, int x, int y, bool use_alpha = true,
                             Common::BlendMode blend_mode = Common::kBlendMode_Alpha);
+extern void draw_gui_sprite(Common::Bitmap *ds, bool use_alpha, int x, int y,
+                            Common::Bitmap *image, bool src_has_alpha,
+                            Common::BlendMode blend_mode, int alpha);
 
 extern AGS_INLINE int game_to_data_coord(int coord);
 extern AGS_INLINE int data_to_game_coord(int coord);

Engine/ac/draw.h
@@ -113,6 +113,8 @@ void draw_sprite_slot_support_alpha(Common::Bitmap *ds, bool ds_has_alpha, int x
                                     Common::BlendMode blend_mode = Common::kBlendMode_Alpha, int alpha = 0xFF);
 void draw_gui_sprite(Common::Bitmap *ds, int pic, int x, int y, bool use_alpha, Common::BlendMode blend_mode);
 void draw_gui_sprite_v330(Common::Bitmap *ds, int pic, int x, int y, bool use_alpha = true, Common::BlendMode blend_mode = Common::kBlendMode_Alpha);
+void draw_gui_sprite(Common::Bitmap *ds, bool use_alpha, int xpos, int ypos,
+    Common::Bitmap *image, bool src_has_alpha, Common::BlendMode blend_mode = Common::kBlendMode_Alpha, int alpha = 0xFF);
 // Render game on screen
 void render_to_screen();
 // Callbacks for the graphics driver

commit d3820e0fd369516008633bbc12fc3da359067798

Engine: fixed GUI's background image with alpha channel
Was broken by recent bcffc2cdeb24a426e2b048e8a98df9c84cb7c6f2

Engine/ac/draw.cpp
@@ -1230,13 +1230,13 @@ void draw_gui_sprite(Bitmap *ds, bool use_alpha, int x, int y, Bitmap *sprite, b
     if (alpha <= 0)
         return;
 
-    const bool ds_has_alpha = use_alpha && (ds->GetColorDepth() == 32);
-    if (game.options[OPT_NEWGUIALPHA] == kGuiAlphaRender_Proper)
+    const bool ds_has_alpha = (ds->GetColorDepth() == 32);
+    if (use_alpha && game.options[OPT_NEWGUIALPHA] == kGuiAlphaRender_Proper)
     {
         GfxUtil::DrawSpriteBlend(ds, Point(x, y), sprite, blend_mode, ds_has_alpha, src_has_alpha, alpha);
     }
     // Backwards-compatible drawing
-    else if (ds_has_alpha && (game.options[OPT_NEWGUIALPHA] == kGuiAlphaRender_AdditiveAlpha) && (alpha == 0xFF))
+    else if (use_alpha && ds_has_alpha && (game.options[OPT_NEWGUIALPHA] == kGuiAlphaRender_AdditiveAlpha) && (alpha == 0xFF))
     {
         if (src_has_alpha)
             set_additive_alpha_blender();
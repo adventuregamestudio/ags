adventuregamestudio/ags

commit c9007679f21e8347d34312b0dd078bbbd29f0de5

Engine: unserialize dynamic objects using MemoryStream
Removed its own in-place serialization functions, which practically duplicated streams.

Engine/ac/dynobj/scriptstring.cpp
@@ -49,11 +49,11 @@ void ScriptString::Serialize(const char *address, Stream *out) {
     out->Write(cstr, _len + 1);
 }
 
-void ScriptString::Unserialize(int index, const char *serializedData, int dataSize) {
-    StartUnserialize(serializedData, dataSize);
-    _len = UnserializeInt();
+void ScriptString::Unserialize(int index, Stream *in, size_t data_sz) {
+    _len = in->ReadInt32();
     text = (char*)malloc(_len + 1);
-    strcpy(text, &serializedData[bytesSoFar]);
+    in->Read(text, _len + 1);
+    text[_len] = 0; // for safety
     ccRegisterUnserializedObject(index, text, this);
 }
 

Engine/ac/dynobj/scriptstring.h
@@ -23,7 +23,7 @@ struct ScriptString final : AGSCCDynamicObject, ICCStringClass {
 
     int Dispose(const char *address, bool force) override;
     const char *GetType() override;
-    void Unserialize(int index, const char *serializedData, int dataSize) override;
+    void Unserialize(int index, AGS::Common::Stream *in, size_t data_sz) override;
 
     DynObjectRef CreateString(const char *fromText) override;
 

Engine/ac/dynobj/cc_agsdynamicobject.cpp
@@ -37,30 +37,6 @@ int AGSCCDynamicObject::Serialize(const char *address, char *buffer, int bufsize
     return static_cast<int32_t>(mems.GetPosition());
 }
 
-void AGSCCDynamicObject::StartUnserialize(const char *sbuffer, int pTotalBytes) {
-    bytesSoFar = 0;
-    totalBytes = pTotalBytes;
-    serbuffer = (char*)sbuffer;
-}
-
-int AGSCCDynamicObject::UnserializeInt() {
-    if (bytesSoFar >= totalBytes) // FIXME: don't use quit! return error instead
-        quit("Unserialise: internal error: read past EOF");
-
-    char *chptr = &serbuffer[bytesSoFar];
-    bytesSoFar += 4;
-    return BBOp::Int32FromLE(*((int*)chptr));
-}
-
-float AGSCCDynamicObject::UnserializeFloat() {
-    if (bytesSoFar >= totalBytes) // FIXME: don't use quit! return error instead
-        quit("Unserialise: internal error: read past EOF");
-
-    char *chptr = &serbuffer[bytesSoFar];
-    bytesSoFar += 4;
-    return BBOp::FloatFromLE(*((float*)chptr));
-}
-
 const char* AGSCCDynamicObject::GetFieldPtr(const char *address, intptr_t offset)
 {
     return address + offset;

Engine/ac/dynobj/cc_agsdynamicobject.h
@@ -28,7 +28,8 @@ public:
 
     // TODO: pass savegame format version
     int Serialize(const char *address, char *buffer, int bufsize) override;
-    virtual void Unserialize(int index, const char *serializedData, int dataSize) = 0;
+    // Try unserializing the object from the given input stream
+    virtual void Unserialize(int index, AGS::Common::Stream *in, size_t data_sz) = 0;
 
     // Legacy support for reading and writing object values by their relative offset
     const char* GetFieldPtr(const char *address, intptr_t offset) override;
@@ -45,19 +46,10 @@ public:
 
 protected:
     // Savegame serialization
-    // TODO: reimplement with the proper memory stream?!
-    int bytesSoFar;
-    int totalBytes;
-    char *serbuffer;
-
     // Calculate and return required space for serialization, in bytes
     virtual size_t CalcSerializeSize() = 0;
     // Write object data into the provided stream
     virtual void Serialize(const char *address, AGS::Common::Stream *out) = 0;
-
-    void StartUnserialize(const char *sbuffer, int pTotalBytes);
-    int  UnserializeInt();
-    float UnserializeFloat();
 };
 
 #endif // __AC_CCDYNAMICOBJECT_H

Engine/ac/dynobj/cc_audiochannel.cpp
@@ -34,8 +34,7 @@ void CCAudioChannel::Serialize(const char *address, Stream *out) {
     out->WriteInt32(ach->id);
 }
 
-void CCAudioChannel::Unserialize(int index, const char *serializedData, int dataSize) {
-    StartUnserialize(serializedData, dataSize);
-    int id = UnserializeInt();
+void CCAudioChannel::Unserialize(int index, Stream *in, size_t data_sz) {
+    int id = in->ReadInt32();
     ccRegisterUnserializedObject(index, &scrAudioChannel[id], this);
 }

Engine/ac/dynobj/cc_audiochannel.h
@@ -18,7 +18,7 @@
 
 struct CCAudioChannel final : AGSCCDynamicObject {
     const char *GetType() override;
-    void Unserialize(int index, const char *serializedData, int dataSize) override;
+    void Unserialize(int index, AGS::Common::Stream *in, size_t data_sz) override;
 protected:
     // Calculate and return required space for serialization, in bytes
     size_t CalcSerializeSize() override;

Engine/ac/dynobj/cc_audioclip.cpp
@@ -34,8 +34,7 @@ void CCAudioClip::Serialize(const char *address, Stream *out) {
     out->WriteInt32(ach->id);
 }
 
-void CCAudioClip::Unserialize(int index, const char *serializedData, int dataSize) {
-    StartUnserialize(serializedData, dataSize);
-    int id = UnserializeInt();
+void CCAudioClip::Unserialize(int index, Stream *in, size_t data_sz) {
+    int id = in->ReadInt32();
     ccRegisterUnserializedObject(index, &game.audioClips[id], this);
 }

Engine/ac/dynobj/cc_audioclip.h
@@ -18,7 +18,7 @@
 
 struct CCAudioClip final : AGSCCDynamicObject {
     const char *GetType() override;
-    void Unserialize(int index, const char *serializedData, int dataSize) override;
+    void Unserialize(int index, AGS::Common::Stream *in, size_t data_sz) override;
 protected:
     // Calculate and return required space for serialization, in bytes
     size_t CalcSerializeSize() override;

commit c00be4f9ee9501aebb0abc81ebbe5b4f2ffb0c0c

Engine: fixed ScriptString could be serialized with wrong length
Was probably broken by c9007679f21e8347d34312b0dd078bbbd29f0de5

Engine/ac/dynobj/scriptstring.cpp
@@ -26,9 +26,9 @@ DynObjectRef ScriptString::CreateString(const char *fromText) {
 
 int ScriptString::Dispose(const char* /*address*/, bool /*force*/) {
     // always dispose
-    if (text) {
-        free(text);
-        text = nullptr;
+    if (_text) {
+        free(_text);
+        _text = nullptr;
     }
     delete this;
     return 1;
@@ -44,26 +44,34 @@ size_t ScriptString::CalcSerializeSize()
 }
 
 void ScriptString::Serialize(const char* /*address*/, Stream *out) {
-    const auto *cstr = text ? text : "";
+    const auto *cstr = _text ? _text : "";
     out->WriteInt32(_len);
     out->Write(cstr, _len + 1);
 }
 
 void ScriptString::Unserialize(int index, Stream *in, size_t /*data_sz*/) {
     _len = in->ReadInt32();
-    text = (char*)malloc(_len + 1);
-    in->Read(text, _len + 1);
-    text[_len] = 0; // for safety
-    ccRegisterUnserializedObject(index, text, this);
+    _text = (char*)malloc(_len + 1);
+    in->Read(_text, _len + 1);
+    _text[_len] = 0; // for safety
+    ccRegisterUnserializedObject(index, _text, this);
 }
 
-ScriptString::ScriptString() {
-    text = nullptr;
-    _len = 0;
+ScriptString::ScriptString(const char *text) {
+    _len = strlen(text);
+    _text = (char*)malloc(_len + 1);
+    memcpy(_text, text, _len + 1);
 }
 
-ScriptString::ScriptString(const char *fromText) {
-    _len = strlen(fromText);
-    text = (char*)malloc(_len + 1);
-    memcpy(text, fromText, _len + 1);
+ScriptString::ScriptString(char *text, bool take_ownership) {
+    _len = strlen(text);
+    if (take_ownership)
+    {
+        _text = text;
+    }
+    else
+    {
+        _text = (char*)malloc(_len + 1);
+        memcpy(_text, text, _len + 1);
+    }
 }

Engine/ac/dynobj/scriptstring.h
@@ -17,18 +17,16 @@
 #include "ac/dynobj/cc_agsdynamicobject.h"
 
 struct ScriptString final : AGSCCDynamicObject, ICCStringClass {
-    // TODO: the preallocated text buffer may be assigned externally;
-    // find out if it's possible to refactor while keeping same functionality
-    char *text;
-
     int Dispose(const char *address, bool force) override;
     const char *GetType() override;
     void Unserialize(int index, AGS::Common::Stream *in, size_t data_sz) override;
 
     DynObjectRef CreateString(const char *fromText) override;
 
-    ScriptString();
-    ScriptString(const char *fromText);
+    ScriptString() = default;
+    ScriptString(const char *text);
+    ScriptString(char *text, bool take_ownership);
+    char *GetTextPtr() const { return _text; }
 
 protected:
     // Calculate and return required space for serialization, in bytes
@@ -37,7 +35,10 @@ protected:
     void Serialize(const char *address, AGS::Common::Stream *out) override;
 
 private:
-    size_t _len;
+    // TODO: the preallocated text buffer may be assigned externally;
+    // find out if it's possible to refactor while keeping same functionality
+    char *_text = nullptr;
+    size_t _len = 0;
 };
 
 #endif // __AC_SCRIPTSTRING_H

Engine/ac/string.cpp
@@ -270,11 +270,10 @@ DynObjectRef CreateNewScriptStringObj(const char *fromText, bool reAllocate)
     if (reAllocate) {
         str = new ScriptString(fromText);
     }
-    else {
-        str = new ScriptString();
-        str->text = (char*)fromText;
+    else { // TODO: refactor to avoid const casts!
+        str = new ScriptString((char*)fromText, true);
     }
-    void *obj_ptr = str->text;
+    void *obj_ptr = str->GetTextPtr();
     int32_t handle = ccRegisterManagedObject(obj_ptr, str);
     if (handle == 0)
     {
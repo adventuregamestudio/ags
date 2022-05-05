fe7d11e9cb836cce8886d5e10ea2286babbd9d14
Ivan Mogilko <ikm_spb@yahoo.com>
2022-05-06 00:24:07 +0300

    Engine: fixed header in mytextbox.cpp

--- Engine/gui/mytextbox.cpp
+++ Engine/gui/mytextbox.cpp
@@ -11,6 +11,7 @@
 // http://www.opensource.org/licenses/artistic-license-2.0.php
 //
 //=============================================================================
+#include <stdio.h>
 #include <string.h>
 #include "ac/keycode.h"
 #include "font/fonts.h"

adventuregamestudio/ags

commit 48ef387226e4d420dc62d0a0b204cf4cd89a1139

Grouped ccError variables in a struct

Common/script/cc_common.cpp
@@ -41,19 +41,29 @@ extern std::pair<String, String> cc_error_at_line(const char *error_msg);
 // Returns script error message without location or callstack
 extern String cc_error_without_line(const char *error_msg);
 
-int ccError = 0;
-int ccErrorLine = 0;
-String ccErrorString;
-String ccErrorCallStack;
-bool ccErrorIsUserError = false;
-const char *ccCurScriptName = "";
+ScriptError ccError;
+
+void cc_clear_error()
+{
+    ccError = ScriptError();
+}
+
+bool cc_has_error()
+{
+    return ccError.HasError;
+}
+
+const ScriptError &cc_get_error()
+{
+    return ccError;
+}
 
 void cc_error(const char *descr, ...)
 {
-    ccErrorIsUserError = false;
+    ccError.IsUserError = false;
     if (descr[0] == '!')
     {
-        ccErrorIsUserError = true;
+        ccError.IsUserError = true;
         descr++;
     }
 
@@ -66,15 +76,15 @@ void cc_error(const char *descr, ...)
     {
         // [IKM] Implementation is project-specific
         std::pair<String, String> errinfo = cc_error_at_line(displbuf.GetCStr());
-        ccErrorString = errinfo.first;
-        ccErrorCallStack = errinfo.second;
+        ccError.ErrorString = errinfo.first;
+        ccError.CallStack = errinfo.second;
     }
     else
     {
-        ccErrorString = cc_error_without_line(displbuf.GetCStr());
-        ccErrorCallStack = "";
+        ccError.ErrorString = cc_error_without_line(displbuf.GetCStr());
+        ccError.CallStack = "";
     }
 
-    ccError = 1;
-    ccErrorLine = currentline;
+    ccError.HasError = 1;
+    ccError.Line = currentline;
 }

Common/script/cc_common.h
@@ -35,14 +35,19 @@ extern int ccGetOption(int);
 
 // error reporting
 
-extern void cc_error(const char *, ...);
+struct ScriptError
+{
+    bool HasError = false; // set if error occurs
+    bool IsUserError = false; // marks script use errors
+    AGS::Common::String ErrorString; // description of the error
+    int Line = 0;  // line number of the error
+    AGS::Common::String CallStack; // callstack where error happened
+};
 
-extern int ccError;             // set to non-zero if error occurs
-extern int ccErrorLine;         // line number of the error
-extern AGS::Common::String ccErrorString; // description of the error
-extern AGS::Common::String ccErrorCallStack; // callstack where error happened
-extern bool ccErrorIsUserError;
-extern const char *ccCurScriptName; // name of currently compiling script
+void cc_clear_error();
+bool cc_has_error();
+const ScriptError &cc_get_error();
+void cc_error(const char *, ...);
 
 extern int currentline;
 

Engine/script/script.cpp
@@ -343,8 +343,7 @@ static bool DoRunScriptFuncCantBlock(ccInstance *sci, NonBlockingScriptFunction*
         funcToRun->atLeastOneImplementationExists = true;
     }
     // this might be nested, so don't disrupt blocked scripts
-    ccErrorString = "";
-    ccError = 0;
+    cc_clear_error();
     no_blocking_functions--;
     return(hasTheFunc);
 }
@@ -352,15 +351,15 @@ static bool DoRunScriptFuncCantBlock(ccInstance *sci, NonBlockingScriptFunction*
 char scfunctionname[MAX_FUNCTION_NAME_LEN + 1];
 static int PrepareTextScript(ccInstance *sci, const char**tsname)
 {
-    ccError = 0;
+    cc_clear_error();
     // FIXME: try to make it so this function is not called with NULL sci
     if (sci == nullptr) return -1;
     if (sci->GetSymbolAddress(tsname[0]).IsNull()) {
-        ccErrorString = "no such function in script";
+        cc_error("no such function in script");
         return -2;
     }
     if (sci->IsBeingRun()) {
-        ccErrorString = "script is already in execution";
+        cc_error("script is already in execution");
         return -3;
     }
     scripts[num_scripts].init();
@@ -382,32 +381,20 @@ static int PrepareTextScript(ccInstance *sci, const char**tsname)
     tsname[0] = &scfunctionname[0];
     update_script_mouse_coords();
     inside_script++;
-    //  aborted_ip=0;
-    //  abort_executor=0;
     return 0;
 }
 
 int RunScriptFunction(ccInstance *sci, const char *tsname, size_t numParam, const RuntimeScriptValue *params)
 {
     int oldRestoreCount = gameHasBeenRestored;
-    // First, save the current ccError state
-    // This is necessary because we might be attempting
-    // to run Script B, while Script A is still running in the
-    // background.
-    // If CallInstance here has an error, it would otherwise
-    // also abort Script A because ccError is a global variable.
-    int cachedCcError = ccError;
-    ccError = 0;
 
+    cc_clear_error();
     int toret = PrepareTextScript(sci, &tsname);
     if (toret) {
-        ccError = cachedCcError;
         return -18;
     }
 
-    // Clear the error message
-    ccErrorString = "";
-
+    cc_clear_error();
     toret = curscript->inst->CallScriptFunction(tsname, numParam, params);
 
     // 100 is if Aborted (eg. because we are LoadAGSGame'ing)
@@ -423,9 +410,6 @@ int RunScriptFunction(ccInstance *sci, const char *tsname, size_t numParam, cons
     post_script_cleanup();
 
     post_script_cleanup_stack--;
-
-    // restore cached error state
-    ccError = cachedCcError;
 
     // if the game has been restored, ensure that any further scripts are not run
     if ((oldRestoreCount != gameHasBeenRestored) && (eventClaimed == EVENT_INPROGRESS))
@@ -451,7 +435,7 @@ int RunScriptFunctionInRoom(const char *tsname, size_t param_count, const Runtim
     // If it's a obligatory room event, and return code means missing function - error
     if (strict_room_event && (toret == -18))
         quitprintf("RunScriptFunction: error %d (%s) trying to run '%s'   (Room %d)",
-            toret, ccErrorString.GetCStr(), tsname, displayed_room);
+            toret, cc_get_error().ErrorString.GetCStr(), tsname, displayed_room);
     return toret;
 }
 
@@ -537,7 +521,9 @@ char* make_ts_func_name(const char*base,int iii,int subd) {
 
 void post_script_cleanup() {
     // should do any post-script stuff here, like go to new room
-    if (ccError) quit(ccErrorString);
+    if (cc_has_error())
+        quit(cc_get_error().ErrorString);
+
     ExecutingScript copyof = scripts[num_scripts-1];
     if (scripts[num_scripts-1].forked)
         delete scripts[num_scripts-1].inst;
@@ -630,10 +616,12 @@ void quit_with_script_error(const char *functionName)
     // TODO: clean up the error reporting logic. Now engine will append call
     // stack info in quit_check_for_error_state() but only in case of explicit
     // script error ("!" type), and not in other case.
-    if (ccErrorIsUserError)
-        quitprintf("!Error running function '%s':\n%s", functionName, ccErrorString.GetCStr());
+    const auto &error = cc_get_error();
+    if (error.IsUserError)
+        quitprintf("!Error running function '%s':\n%s", functionName, error.ErrorString.GetCStr());
     else
-        quitprintf("Error running function '%s':\n%s\n\n%s", functionName, ccErrorString.GetCStr(), get_cur_script(5).GetCStr());
+        quitprintf("Error running function '%s':\n%s\n\n%s", functionName,
+            error.ErrorString.GetCStr(), get_cur_script(5).GetCStr());
 }
 
 int get_nivalue (InteractionCommandList *nic, int idx, int parm) {

Common/game/main_game_file.cpp
@@ -224,7 +224,7 @@ HGameFileError ReadDialogScript(PScript &dialog_script, Stream *in, GameDataVers
     {
         dialog_script.reset(ccScript::CreateFromStream(in));
         if (dialog_script == nullptr)
-            return new MainGameFileError(kMGFErr_CreateDialogScriptFailed, ccErrorString);
+            return new MainGameFileError(kMGFErr_CreateDialogScriptFailed, cc_get_error().ErrorString);
     }
     else // 2.x and < 3.1.1 dialog
     {
@@ -243,7 +243,7 @@ HGameFileError ReadScriptModules(std::vector<PScript> &sc_mods, Stream *in, Game
         {
             sc_mods[i].reset(ccScript::CreateFromStream(in));
             if (sc_mods[i] == nullptr)
-                return new MainGameFileError(kMGFErr_CreateScriptModuleFailed, ccErrorString);
+                return new MainGameFileError(kMGFErr_CreateScriptModuleFailed, cc_get_error().ErrorString);
         }
     }
     else
@@ -860,7 +860,7 @@ HGameFileError ReadGameData(LoadedGameEntities &ents, Stream *in, GameDataVersio
     {
         ents.GlobalScript.reset(ccScript::CreateFromStream(in));
         if (!ents.GlobalScript)
-            return new MainGameFileError(kMGFErr_CreateGlobalScriptFailed, ccErrorString);
+            return new MainGameFileError(kMGFErr_CreateGlobalScriptFailed, cc_get_error().ErrorString);
         err = ReadDialogScript(ents.DialogScript, in, data_ver);
         if (!err)
             return err;

Common/game/room_file.cpp
@@ -349,7 +349,7 @@ HError ReadCompSc3Block(RoomStruct *room, Stream *in, RoomFileVersion /*data_ver
 {
     room->CompiledScript.reset(ccScript::CreateFromStream(in));
     if (room->CompiledScript == nullptr)
-        return new RoomFileError(kRoomFileErr_ScriptLoadFailed, ccErrorString);
+        return new RoomFileError(kRoomFileErr_ScriptLoadFailed, cc_get_error().ErrorString);
     return HError::None();
 }
 

Common/script/cc_internal.h
@@ -125,4 +125,6 @@
 extern const char scfilesig[5];
 #define ENDFILESIG 0xbeefcafe
 
+extern const char *ccCurScriptName; // name of currently compiling script
+
 #endif // __CC_INTERNAL_H

Compiler/script/cs_compiler.cpp
@@ -11,6 +11,7 @@
 #include "script/cs_parser.h"
 
 const char *ccSoftwareVersion = "1.0";
+const char *ccCurScriptName = "";
 
 std::vector<const char*> defaultheaders;
 std::vector<const char*> defaultHeaderNames;
@@ -42,8 +43,7 @@ ccScript* ccCompileText(const char *texo, const char *scriptName) {
     if (scriptName == NULL)
         scriptName = "Main script";
 
-    ccError = 0;
-    ccErrorLine = 0;
+    cc_clear_error();
 
     for (size_t t=0;t<defaultheaders.size();t++) {
         if (defaultHeaderNames[t])
@@ -53,16 +53,16 @@ ccScript* ccCompileText(const char *texo, const char *scriptName) {
 
         cctemp->start_new_section(ccCurScriptName);
         cc_compile(defaultheaders[t],cctemp);
-        if (ccError) break;
+        if (cc_has_error()) break;
     }
 
-    if (!ccError) {
+    if (!cc_has_error()) {
         ccCurScriptName = scriptName;
         cctemp->start_new_section(ccCurScriptName);
         cc_compile(texo,cctemp);
     }
 
-    if (ccError) {
+    if (cc_has_error()) {
         cctemp->shutdown();
         delete cctemp;
         return NULL;

Compiler/script/cs_parser.cpp
@@ -3741,7 +3741,7 @@ int __cc_compile_file(const char*inpl,ccCompiledScript*scrip) {
                         }
                         // not found -- a good thing, but find_member_sym will
                         // have errored. Clear the error
-                        ccError = 0;
+                        cc_clear_error();
                     }
 
                     if (isFunction) {
@@ -4308,7 +4308,7 @@ startvarbit:
             if (oldDefinition.stype) {
                 // there was a forward declaration -- check that
                 // the real declaration matches it
-                ccError = 0;
+                cc_clear_error();
                 if (!isglobal)
                     cc_error("Local variable cannot have the same name as an import");
                 else if (oldDefinition.stype != sym.entries[cursym].stype)
@@ -4335,7 +4335,7 @@ startvarbit:
                         }
                     }
                 }
-                if (ccError)
+                if (cc_has_error())
                     return -1;
             }
 

commit 29aaa19d712c3edf5701c648833283011c2f7298

Engine: fixed script errors when non-obligatory callbacks are missing
Was broken by 48ef387226e4d420dc62d0a0b204cf4cd89a1139, now partially reverting the commit.

This is a hotfix, but I think this must be investigated and done differently, to avoid such hacks.

Common/script/cc_common.cpp
@@ -39,7 +39,7 @@ int ccGetOption(int optbit)
 // Returns current running script callstack as a human-readable text
 extern String cc_get_callstack(int max_lines = INT_MAX);
 
-ScriptError ccError;
+static ScriptError ccError;
 
 void cc_clear_error()
 {
@@ -76,4 +76,9 @@ void cc_error(const char *descr, ...)
     ccError.CallStack = cc_get_callstack();
     ccError.HasError = 1;
     ccError.Line = currentline;
+}
+
+void cc_error(const ScriptError &err)
+{
+    ccError = err;
 }

Common/script/cc_common.h
@@ -48,6 +48,7 @@ void cc_clear_error();
 bool cc_has_error();
 const ScriptError &cc_get_error();
 void cc_error(const char *, ...);
+void cc_error(const ScriptError &err);
 // Project-dependent script error formatting
 AGS::Common::String cc_format_error(const AGS::Common::String &message);
 

Engine/script/script.cpp
@@ -386,10 +386,19 @@ static int PrepareTextScript(ccInstance *sci, const char**tsname)
 int RunScriptFunction(ccInstance *sci, const char *tsname, size_t numParam, const RuntimeScriptValue *params)
 {
     int oldRestoreCount = gameHasBeenRestored;
+    // TODO: research why this is really necessary, and refactor to avoid such hacks!
+    // First, save the current ccError state
+    // This is necessary because we might be attempting
+    // to run Script B, while Script A is still running in the
+    // background.
+    // If CallInstance here has an error, it would otherwise
+    // also abort Script A because ccError is a global variable.
+    ScriptError cachedCcError = cc_get_error();
 
     cc_clear_error();
     int toret = PrepareTextScript(sci, &tsname);
     if (toret) {
+        cc_error(cachedCcError);
         return -18;
     }
 
@@ -409,6 +418,9 @@ int RunScriptFunction(ccInstance *sci, const char *tsname, size_t numParam, cons
     post_script_cleanup();
 
     post_script_cleanup_stack--;
+
+    // restore cached error state
+    cc_error(cachedCcError);
 
     // if the game has been restored, ensure that any further scripts are not run
     if ((oldRestoreCount != gameHasBeenRestored) && (eventClaimed == EVENT_INPROGRESS))
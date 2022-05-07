adventuregamestudio/ags

commit a2f3738f892641bfcd7e7ca930808c52f0757e23

Common: apply bitmap clipping in WFN renderer

Common/font/wfnfontrenderer.cpp
@@ -11,12 +11,12 @@
 // http://www.opensource.org/licenses/artistic-license-2.0.php
 //
 //=============================================================================
-
+#include "font/wfnfontrenderer.h"
+#include <algorithm>
 #include "ac/common.h" // our_eip
 #include "core/assetmanager.h"
 #include "debug/out.h"
 #include "font/wfnfont.h"
-#include "font/wfnfontrenderer.h"
 #include "gfx/bitmap.h"
 #include "util/stream.h"
 
@@ -100,28 +100,26 @@ int RenderChar(Bitmap *ds, const int at_x, const int at_y, const WFNChar &wfn_ch
   const unsigned char *actdata = wfn_char.Data;
   const int bytewid = wfn_char.GetRowByteCount();
 
-  int x = at_x;
-  int y = at_y;
-  for (int h = 0; h < height; ++h)
+  // NOTE: allegro's putpixel ignores clipping (optimization),
+  // so we'll have to accomodate for that ourselves
+  Rect clip = ds->GetClip();
+  int sx = std::max(at_x, clip.Left), ex = std::min(at_x + width * scale, clip.Right + 1);
+  int sy = std::max(at_y, clip.Top), ey = std::min(at_y + height * scale, clip.Bottom + 1);
+  for (int h = 0, y = sy; h < height && y < ey; ++h, y += scale)
   {
-    for (int w = 0; w < width; ++w)
+    for (int w = 0, x = sx; w < width && x < ex; ++w, x += scale)
     {
       if (((actdata[h * bytewid + (w / 8)] & (0x80 >> (w % 8))) != 0)) {
         if (scale > 1)
         {
-          ds->FillRect(Rect(x + w, y + h, x + w + (scale - 1),
-              y + h + (scale - 1)), text_color);
+          ds->FillRect(RectWH(x, y, scale - 1, scale - 1), text_color);
         } 
         else
         {
-          ds->PutPixel(x + w, y + h, text_color);
+          ds->PutPixel(x, y, text_color);
         }
       }
-
-      x += scale - 1;
     }
-    y += scale - 1;
-    x = at_x;
   }
   return width * scale;
 }

commit fdff20fd724a377dc06baf04187e52949d21d923

Common: fixed WFN clip causing misplaced letters + small tidyup
Was broken by a2f3738f892641bfcd7e7ca930808c52f0757e23

Common/font/wfnfontrenderer.cpp
@@ -27,8 +27,6 @@ static unsigned char GetCharCode(unsigned char wanted_code, const WFNFont* font)
     return wanted_code < font->GetCharCount() ? wanted_code : '?';
 }
 
-static int RenderChar(Bitmap *ds, const int at_x, const int at_y, const WFNChar &wfn_char, const int scale, const color_t text_color);
-
 void WFNFontRenderer::AdjustYCoordinateForFont(int * /*ycoord*/, int /*fontNumber*/)
 {
   // Do nothing
@@ -77,7 +75,9 @@ int WFNFontRenderer::GetTextHeight(const char *text, int fontNumber)
   return max_height * params.SizeMultiplier;
 }
 
-Bitmap render_wrapper;
+static int RenderChar(Bitmap *ds, const int at_x, const int at_y, Rect clip,
+    const WFNChar &wfn_char, const int scale, const color_t text_color);
+
 void WFNFontRenderer::RenderText(const char *text, int fontNumber, BITMAP *destination, int x, int y, int colour)
 {
   int oldeip = get_our_eip();
@@ -85,31 +85,35 @@ void WFNFontRenderer::RenderText(const char *text, int fontNumber, BITMAP *desti
 
   const WFNFont* font = _fontData[fontNumber].Font;
   const FontRenderParams &params = _fontData[fontNumber].Params;
-  render_wrapper.WrapAllegroBitmap(destination, true);
+  Bitmap ds(destination, true);
 
+  // NOTE: allegro's putpixel ignores clipping (optimization),
+  // so we'll have to accomodate for that ourselves
+  Rect clip = ds.GetClip();
   for (; *text; ++text)
-    x += RenderChar(&render_wrapper, x, y, font->GetChar(GetCharCode(*text, font)), params.SizeMultiplier, colour);
+    x += RenderChar(&ds, x, y, clip, font->GetChar(GetCharCode(*text, font)), params.SizeMultiplier, colour);
 
   set_our_eip(oldeip);
 }
 
-int RenderChar(Bitmap *ds, const int at_x, const int at_y, const WFNChar &wfn_char, const int scale, const color_t text_color)
+static int RenderChar(Bitmap *ds, const int at_x, const int at_y, Rect clip,
+    const WFNChar &wfn_char, const int scale, const color_t text_color)
 {
   const int width = wfn_char.Width;
   const int height = wfn_char.Height;
   const unsigned char *actdata = wfn_char.Data;
   const int bytewid = wfn_char.GetRowByteCount();
 
-  // NOTE: allegro's putpixel ignores clipping (optimization),
-  // so we'll have to accomodate for that ourselves
-  Rect clip = ds->GetClip();
-  int sx = std::max(at_x, clip.Left), ex = std::min(at_x + width * scale, clip.Right + 1);
-  int sy = std::max(at_y, clip.Top), ey = std::min(at_y + height * scale, clip.Bottom + 1);
-  for (int h = 0, y = sy; h < height && y < ey; ++h, y += scale)
+  int sx = std::max(at_x, clip.Left), ex = clip.Right + 1;
+  int sy = std::max(at_y, clip.Top), ey = clip.Bottom + 1;
+  int sw = std::max(0, clip.Left - at_x);
+  int sh = std::max(0, clip.Top - at_y);
+  for (int h = sh, y = sy; h < height && y < ey; ++h, y += scale)
   {
-    for (int w = 0, x = sx; w < width && x < ex; ++w, x += scale)
+    for (int w = sw, x = sx; w < width && x < ex; ++w, x += scale)
     {
-      if (((actdata[h * bytewid + (w / 8)] & (0x80 >> (w % 8))) != 0)) {
+      if (((actdata[h * bytewid + (w / 8)] & (0x80 >> (w % 8))) != 0))
+      {
         if (scale > 1)
         {
           ds->FillRect(RectWH(x, y, scale - 1, scale - 1), text_color);
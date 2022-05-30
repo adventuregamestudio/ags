adventuregamestudio/ags

commit a2f3738f892641bfcd7e7ca930808c52f0757e23

Common: apply bitmap clipping in WFN renderer

Common/font/wfnfontrenderer.cpp
@@ -11,12 +11,12 @@
 // http://www.opensource.org/licenses/artistic-license-2.0.php
 //
 //=============================================================================
-
+#include "font/wfnfontrenderer.h"
+#include <algorithm>
 #include "ac/common.h" // our_eip
 #include "core/assetmanager.h"
 #include "debug/out.h"
 #include "font/wfnfont.h"
-#include "font/wfnfontrenderer.h"
 #include "gfx/bitmap.h"
 #include "util/stream.h"
 
@@ -100,28 +100,26 @@ int RenderChar(Bitmap *ds, const int at_x, const int at_y, const WFNChar &wfn_ch
   const unsigned char *actdata = wfn_char.Data;
   const int bytewid = wfn_char.GetRowByteCount();
 
-  int x = at_x;
-  int y = at_y;
-  for (int h = 0; h < height; ++h)
+  // NOTE: allegro's putpixel ignores clipping (optimization),
+  // so we'll have to accomodate for that ourselves
+  Rect clip = ds->GetClip();
+  int sx = std::max(at_x, clip.Left), ex = std::min(at_x + width * scale, clip.Right + 1);
+  int sy = std::max(at_y, clip.Top), ey = std::min(at_y + height * scale, clip.Bottom + 1);
+  for (int h = 0, y = sy; h < height && y < ey; ++h, y += scale)
   {
-    for (int w = 0; w < width; ++w)
+    for (int w = 0, x = sx; w < width && x < ex; ++w, x += scale)
     {
       if (((actdata[h * bytewid + (w / 8)] & (0x80 >> (w % 8))) != 0)) {
         if (scale > 1)
         {
-          ds->FillRect(Rect(x + w, y + h, x + w + (scale - 1),
-              y + h + (scale - 1)), text_color);
+          ds->FillRect(RectWH(x, y, scale - 1, scale - 1), text_color);
         } 
         else
         {
-          ds->PutPixel(x + w, y + h, text_color);
+          ds->PutPixel(x, y, text_color);
         }
       }
-
-      x += scale - 1;
     }
-    y += scale - 1;
-    x = at_x;
   }
   return width * scale;
 }

commit adc9121f30c028a071783ffb6645bb72aee482ea

Common: fixed rendering scaled up WFN fonts
Was broken by a2f3738f892641bfcd7e7ca930808c52f0757e23

Common/font/wfnfontrenderer.cpp
@@ -116,7 +116,7 @@ static int RenderChar(Bitmap *ds, const int at_x, const int at_y, Rect clip,
       {
         if (scale > 1)
         {
-          ds->FillRect(RectWH(x, y, scale - 1, scale - 1), text_color);
+          ds->FillRect(RectWH(x, y, scale, scale), text_color);
         } 
         else
         {
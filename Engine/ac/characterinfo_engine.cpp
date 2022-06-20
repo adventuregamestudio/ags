adventuregamestudio/ags

commit 2ac1b258db203b62ba0613c979d7a092b51e441f

Engine: fixup character's view loop in update, prevent having no frame

Engine/ac/characterinfo_engine.cpp
@@ -73,10 +73,16 @@ void CharacterInfo::UpdateMoveAndAnim(int &char_index, CharacterExtras *chex, in
 		return;				      //  must be careful not to screw things up
 	}
     
-    // Make sure it doesn't flash up a blue cup
-    if (view < 0) ;
-    else if (loop >= views[view].numLoops)
-      loop = 0;
+    // Fixup character's view when possible
+    if (view >= 0 &&
+        (loop >= views[view].numLoops || frame >= views[view].loops[loop].numFrames))
+    {
+        for (loop = 0;
+            (loop < loop < views[view].numLoops) && (views[view].loops[loop].numFrames == 0);
+            ++loop);
+        if (loop == views[view].numLoops)
+            quitprintf("!Character %s is assigned view %d that has no frames!", name, view);
+    }
 
     int doing_nothing = 1;
 

commit ac73a555d737fb3f759cc7da9eeaf488a3bdcb2f

Engine: fixed character's loop fixup in UpdateMoveAndAnim()
Was broken by 2ac1b258db203b62ba0613c979d7a092b51e441f

This old commit was trying to fix a problem in old game(s), when character was set to a loop with no frames. In such case the old engine would seek for the first loop with frames, starting with 0.
The condition for doing so was made incorrect though: instead of testing simply for a empty loop, it tested for frame number being outside of the current loop's frame range.
This broke games where e.g. some walking view's loops were shorter than the others.

A replacement is a condition that tests exactly for an empty loop instead. The frame exceeding a loop's frame count is fixed later along the way, so it's not a problem here.

Engine/ac/characterinfo_engine.cpp
@@ -75,7 +75,7 @@ void CharacterInfo::UpdateMoveAndAnim(int &char_index, CharacterExtras *chex, st
     
     // Fixup character's view when possible
     if (view >= 0 &&
-        (loop >= views[view].numLoops || frame >= views[view].loops[loop].numFrames))
+        (loop >= views[view].numLoops || views[view].loops[loop].numFrames == 0))
     {
         for (loop = 0;
             (loop < views[view].numLoops) && (views[view].loops[loop].numFrames == 0);
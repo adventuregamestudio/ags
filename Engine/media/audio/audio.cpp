adventuregamestudio/ags

commit 267874c54cde170d9cba9d7dc2c50e44cd0b906c

Engine: make SetChannel() take std unique_ptr, as it passes ownership

Engine/media/audio/audio.cpp
@@ -60,24 +60,22 @@ SOUNDCLIP *AudioChans::GetChannelIfPlaying(int index)
     return (ch != nullptr && ch->is_ready()) ? ch : nullptr;
 }
 
-SOUNDCLIP *AudioChans::SetChannel(int index, SOUNDCLIP* ch)
+SOUNDCLIP *AudioChans::SetChannel(int index, std::unique_ptr<SOUNDCLIP> ch)
 {
-    // TODO: store clips in smart pointers
-    if ((ch != nullptr) && (_channels[index].get() == ch))
+    if ((ch != nullptr) && (_channels[index].get() == ch.get()))
     {
         Debug::Printf(kDbgMsg_Warn, "WARNING: channel %d - same clip assigned", index);
-        return ch;
+        return ch.get();
     }
     if ((ch != nullptr) && (_channels[index] != nullptr))
         Debug::Printf(kDbgMsg_Warn, "WARNING: channel %d - clip overwritten", index);
-    _channels[index].reset(ch);
-    return ch;
+    _channels[index] = std::move(ch);
+    return ch.get();
 }
 
 SOUNDCLIP *AudioChans::MoveChannel(int to, int from)
 {
-    auto from_ch = _channels[from].release();
-    return SetChannel(to, from_ch);
+    return SetChannel(to, std::move(_channels[from]));
 }
 
 void AudioChans::DeleteClipOnChannel(int index)
@@ -419,7 +417,7 @@ ScriptAudioChannel* play_audio_clip_on_channel(int channel, ScriptAudioClip *cli
     if (!play.fast_forward && play.speech_has_voice)
         apply_volume_drop_to_clip(soundfx);
 
-    AudioChans::SetChannel(channel, soundfx);
+    AudioChans::SetChannel(channel, std::unique_ptr<SOUNDCLIP>(soundfx));
     return &scrAudioChannel[channel];
 }
 
@@ -1115,7 +1113,7 @@ static void play_new_music(int mnum, SOUNDCLIP *music)
 
     if (new_clip && new_clip->play())
     {
-        AudioChans::SetChannel(useChannel, new_clip);
+        AudioChans::SetChannel(useChannel, std::unique_ptr<SOUNDCLIP>(new_clip));
         current_music_type = new_clip->get_sound_type();
     }
     else

Engine/ac/global_audio.cpp
@@ -80,7 +80,7 @@ void PlayAmbientSound (int channel, int sndnum, int vol, int x, int y) {
             debug_script_log("Playing ambient sound %d on channel %d", sndnum, channel);
             ambient[channel].channel = channel;
             asound->priority = 15;  // ambient sound higher priority than normal sfx
-            AudioChans::SetChannel(channel, asound);
+            AudioChans::SetChannel(channel, std::unique_ptr<SOUNDCLIP>(asound));
     }
     // calculate the maximum distance away the player can be, using X
     // only (since X centred is still more-or-less total Y)
@@ -155,7 +155,7 @@ int PlaySoundEx(int val1, int channel) {
 
     soundfx->priority = 10;
     soundfx->set_volume255(play.sound_volume);
-    AudioChans::SetChannel(channel,soundfx);
+    AudioChans::SetChannel(channel, std::unique_ptr<SOUNDCLIP>(soundfx));
     return channel;
 }
 
@@ -392,7 +392,7 @@ void PlayMP3File (const char *filename) {
     if (clip) {
         if (clip->play()) {
             clip->set_volume255(150);
-            AudioChans::SetChannel(useChan, clip);
+            AudioChans::SetChannel(useChan, std::unique_ptr<SOUNDCLIP>(clip));
             current_music_type = sound_type;
             play.cur_music_number = 1000;
             // save the filename (if it's not what we were supplied with)
@@ -433,7 +433,7 @@ void PlaySilentMIDI (int mnum) {
         quitprintf("!PlaySilentMIDI: failed to load aMusic%d", mnum);
     }
     if (clip->play()) {
-        AudioChans::SetChannel(play.silent_midi_channel, clip);
+        AudioChans::SetChannel(play.silent_midi_channel, std::unique_ptr<SOUNDCLIP>(clip));
         clip->set_volume100(0);
     } else {
         delete clip;
@@ -534,7 +534,7 @@ static bool play_voice_clip_on_channel(const String &voice_name)
         return false;
     }
 
-    AudioChans::SetChannel(SCHAN_SPEECH,speechmp3);
+    AudioChans::SetChannel(SCHAN_SPEECH, std::unique_ptr<SOUNDCLIP>(speechmp3));
     return true;
 }
 

Engine/media/audio/audio.h
@@ -15,6 +15,7 @@
 #define __AGS_EE_MEDIA__AUDIO_H__
 
 #include <array>
+#include <memory>
 #include "media/audio/audiodefines.h"
 #include "ac/dynobj/scriptaudioclip.h"
 #include "ac/dynobj/scriptaudiochannel.h"
@@ -31,7 +32,7 @@ public:
     // Gets a clip from the channel but only if it's in playback state
     static SOUNDCLIP *GetChannelIfPlaying(int index);
     // Assign new clip to the channel
-    static SOUNDCLIP *SetChannel(int index, SOUNDCLIP *clip);
+    static SOUNDCLIP *SetChannel(int index, std::unique_ptr<SOUNDCLIP> clip);
     // Move clip from one channel to another, clearing the first channel
     static SOUNDCLIP *MoveChannel(int to, int from);
     // Deletes any clip and frees the channel

Engine/plugin/agsplugin.cpp
@@ -588,7 +588,7 @@ void IAGSEngine::PlaySoundChannel (int32 channel, int32 soundType, int32 volume,
     }
 
     newcha->set_volume255(volume);
-    AudioChans::SetChannel(channel, newcha);
+    AudioChans::SetChannel(channel, std::unique_ptr<SOUNDCLIP>(newcha));
 }
 // Engine interface 12 and above are below
 void IAGSEngine::MarkRegionDirty(int32 left, int32 top, int32 right, int32 bottom) {

commit 90105f4af0f52018cd7dc3012bb3ab13c54cc7d1

Engine: fixed AudioChans::SetChannel's return value
Was broken by 267874c54cde170d9cba9d7dc2c50e44cd0b906c

Engine/media/audio/audio.cpp
@@ -62,15 +62,10 @@ SOUNDCLIP *AudioChans::GetChannelIfPlaying(int index)
 
 SOUNDCLIP *AudioChans::SetChannel(int index, std::unique_ptr<SOUNDCLIP> ch)
 {
-    if ((ch != nullptr) && (_channels[index].get() == ch.get()))
-    {
-        Debug::Printf(kDbgMsg_Warn, "WARNING: channel %d - same clip assigned", index);
-        return ch.get();
-    }
     if ((ch != nullptr) && (_channels[index] != nullptr))
         Debug::Printf(kDbgMsg_Warn, "WARNING: channel %d - clip overwritten", index);
     _channels[index] = std::move(ch);
-    return ch.get();
+    return _channels[index].get();
 }
 
 SOUNDCLIP *AudioChans::MoveChannel(int to, int from)
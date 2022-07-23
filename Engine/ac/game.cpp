adventuregamestudio/ags

commit 9dffb0480fd7b8287fead5bc65556b014c01e870

Engine: picked out deleted sprite update code to game_sprite_deleted()

Engine/ac/game.cpp
@@ -54,6 +54,7 @@
 #include "game/savegame.h"
 #include "gfx/bitmap.h"
 #include "gfx/graphicsdriver.h"
+#include "gui/guibutton.h"
 #include "gui/guidialog.h"
 #include "main/engine.h"
 #include "media/audio/audio_system.h"
@@ -1543,7 +1544,7 @@ bool unserialize_audio_script_object(int index, const char *objectType, const ch
 void game_sprite_updated(int sprnum)
 {
     // Check if this sprite is assigned to any game object, and update them if necessary
-    // room object cache
+    // room objects cache
     if (croom != nullptr)
     {
         for (size_t i = 0; i < (size_t)croom->numobj; ++i)
@@ -1561,11 +1562,67 @@ void game_sprite_updated(int sprnum)
     // gui backgrounds
     for (size_t i = 0; i < (size_t)game.numgui; ++i)
     {
-        if ((guis[i].BgImage == sprnum) &&
-            (guis[i].IsDisplayed()))
+        if (guis[i].BgImage == sprnum)
         {
             guis_need_update = 1;
-            break;
+            break; // setting gui update flag once is enough
+        }
+    }
+    // gui buttons
+    for (size_t i = 0; i < (size_t)numguibuts; ++i)
+    {
+        if (guibuts[i].CurrentImage == sprnum)
+        {
+            guis_need_update = 1;
+            break; // setting gui update flag once is enough
+        }
+    }
+}
+
+void game_sprite_deleted(int sprnum)
+{
+    // Check if this sprite is assigned to any game object, and update them if necessary
+    // room objects and their cache
+    if (croom != nullptr)
+    {
+        for (size_t i = 0; i < (size_t)croom->numobj; ++i)
+        {
+            if (objs[i].num == sprnum)
+            {
+                objs[i].num = 0;
+                objcache[i].sppic = -1;
+            }
+        }
+    }
+    // character cache
+    for (size_t i = 0; i < (size_t)game.numcharacters; ++i)
+    {
+        if (charcache[i].sppic == sprnum)
+            charcache[i].sppic = -1;
+    }
+    // gui backgrounds
+    for (size_t i = 0; i < (size_t)game.numgui; ++i)
+    {
+        if (guis[i].BgImage == sprnum)
+        {
+            guis[i].BgImage = 0;
+            guis_need_update = 1;
+        }
+    }
+    // gui buttons
+    for (size_t i = 0; i < (size_t)numguibuts; ++i)
+    {
+        if (guibuts[i].Image == sprnum)
+            guibuts[i].Image = 0;
+        if (guibuts[i].MouseOverImage == sprnum)
+            guibuts[i].MouseOverImage = 0;
+        if (guibuts[i].PushedImage == sprnum)
+            guibuts[i].PushedImage = 0;
+
+        if (guibuts[i].CurrentImage == sprnum)
+        {
+            guibuts[i].CurrentImage = 0;
+            guis_need_update = 1;
         }
     }
 }

Engine/ac/dynamicsprite.cpp
@@ -469,7 +469,6 @@ void add_dynamic_sprite(int gotSlot, Bitmap *redin, bool hasAlpha) {
 }
 
 void free_dynamic_sprite (int gotSlot) {
-  int tt;
 
   if ((gotSlot < 0) || (gotSlot >= spriteset.GetSpriteSlotCount()))
     quit("!FreeDynamicSprite: invalid slot number");
@@ -483,34 +482,7 @@ void free_dynamic_sprite (int gotSlot) {
   game.SpriteInfos[gotSlot].Width = 0;
   game.SpriteInfos[gotSlot].Height = 0;
 
-  // ensure it isn't still on any GUI buttons
-  for (tt = 0; tt < numguibuts; tt++) {
-    if (guibuts[tt].IsDeleted())
-      continue;
-    if (guibuts[tt].Image == gotSlot)
-      guibuts[tt].Image = 0;
-    if (guibuts[tt].CurrentImage == gotSlot)
-      guibuts[tt].CurrentImage = 0;
-    if (guibuts[tt].MouseOverImage == gotSlot)
-      guibuts[tt].MouseOverImage = 0;
-    if (guibuts[tt].PushedImage == gotSlot)
-      guibuts[tt].PushedImage = 0;
-  }
-
-  // force refresh of any object caches using the sprite
-  if (croom != nullptr) 
-  {
-    for (tt = 0; tt < croom->numobj; tt++) 
-    {
-      if (objs[tt].num == gotSlot)
-      {
-        objs[tt].num = 0;
-        objcache[tt].sppic = -1;
-      }
-      else if (objcache[tt].sppic == gotSlot)
-        objcache[tt].sppic = -1;
-    }
-  }
+  game_sprite_deleted(gotSlot);
 }
 
 //=============================================================================

Engine/ac/game.h
@@ -184,6 +184,9 @@ bool unserialize_audio_script_object(int index, const char *objectType, const ch
 // Notifies the game objects that certain sprite was updated.
 // This make them update their render states, caches, and so on.
 void game_sprite_updated(int sprnum);
+// Notifies the game objects that certain sprite was deleted.
+// Those which used that sprite will reset to dummy sprite 0, update their render states and caches.
+void game_sprite_deleted(int sprnum);
 
 extern int in_new_room;
 extern int new_room_pos;

commit 0a98d9ce2b77717c8386b5a7810871826c7e7928

Engine: fixed object cache may sometimes keep an old dynsprite ref
This may result in object texture not updating if the old sprite was deleted but a new dynamic sprite was created right after having same ID.

Was broken by 9dffb0480fd7b8287fead5bc65556b014c01e870

Engine/ac/game.cpp
@@ -1544,7 +1544,7 @@ void game_sprite_updated(int sprnum)
     {
         for (size_t i = 0; i < (size_t)croom->numobj; ++i)
         {
-            if (objs[i].num == sprnum)
+            if (objcache[i].sppic == sprnum)
                 objcache[i].sppic = -1;
         }
     }
@@ -1581,10 +1581,9 @@ void game_sprite_deleted(int sprnum)
         for (size_t i = 0; i < (size_t)croom->numobj; ++i)
         {
             if (objs[i].num == sprnum)
-            {
                 objs[i].num = 0;
+            if (objcache[i].sppic == sprnum)
                 objcache[i].sppic = -1;
-            }
         }
     }
     // character cache
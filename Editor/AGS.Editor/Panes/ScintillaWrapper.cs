adventuregamestudio/ags

commit 2934259cc4d15bf7888852b5ec68b2814059067a

Editor: fixed potential infinite recursion in FindGlobalVariableOrType
The recursion would take place if the variable has name identical to its type.

Editor/AGS.Editor/Panes/ScintillaWrapper.cs
@@ -1306,24 +1306,35 @@ namespace AGS.Editor
             return scriptList;
         }
 
+        private ScriptStruct FindGlobalType(IScript script, string type, ref bool staticAccess)
+        {
+            foreach (ScriptStruct structDef in script.AutoCompleteData.Structs)
+            {
+                if ((structDef.Name == type) && (structDef.FullDefinition))
+                {
+                    staticAccess = true;
+                    return structDef;
+                }
+            }
+            return null;
+        }
+
         private ScriptStruct FindGlobalVariableOrType(string type, ref bool staticAccess)
         {
             foreach (IScript script in GetAutoCompleteScriptList())
             {
-                foreach (ScriptStruct structDef in script.AutoCompleteData.Structs)
-                {
-                    if ((structDef.Name == type) && (structDef.FullDefinition))
-                    {
-                        staticAccess = true;
-                        return structDef;
-                    }
-                }
+                // First try search for a type that has this name
+                var foundType = FindGlobalType(script, type, ref staticAccess);
+                if (foundType != null)
+                    return foundType;
+                // Then, search for a variable that has this name, and try retrieving its type
                 foreach (ScriptVariable varDef in script.AutoCompleteData.Variables)
                 {
                     if (varDef.VariableName == type)
                     {
                         staticAccess = false;
-                        return FindGlobalVariableOrType(varDef.Type);
+                        bool dummy = false;
+                        return FindGlobalType(script, varDef.Type, ref dummy);
                     }
                 }
             }

commit 96770282d619c01fcda25eb8561e8317e3e20379

Editor: fixed FindGlobalVariableOrType again
Some cases were broken by 2934259cc4d15bf7888852b5ec68b2814059067a

Editor/AGS.Editor/Panes/ScintillaWrapper.cs
@@ -1306,13 +1306,16 @@ namespace AGS.Editor
             return scriptList;
         }
 
-        private ScriptStruct FindGlobalType(IScript script, string type)
+        private ScriptStruct FindGlobalType(string type)
         {
-            foreach (ScriptStruct structDef in script.AutoCompleteData.Structs)
+            foreach (IScript script in GetAutoCompleteScriptList())
             {
-                if ((structDef.Name == type) && (structDef.FullDefinition))
+                foreach (ScriptStruct structDef in script.AutoCompleteData.Structs)
                 {
-                    return structDef;
+                    if ((structDef.Name == type) && (structDef.FullDefinition))
+                    {
+                        return structDef;
+                    }
                 }
             }
             return null;
@@ -1320,22 +1323,23 @@ namespace AGS.Editor
 
         private ScriptStruct FindGlobalVariableOrType(string type, ref bool staticAccess)
         {
+            // First try search for a type that has this name
+            var foundType = FindGlobalType(type);
+            if (foundType != null)
+            {
+                staticAccess = true;
+                return foundType;
+            }
+
+            // Then, search for a variable that has this name, and try retrieving its type
             foreach (IScript script in GetAutoCompleteScriptList())
             {
-                // First try search for a type that has this name
-                var foundType = FindGlobalType(script, type);
-                if (foundType != null)
-                {
-                    staticAccess = true;
-                    return foundType;
-                }
-                // Then, search for a variable that has this name, and try retrieving its type
                 foreach (ScriptVariable varDef in script.AutoCompleteData.Variables)
                 {
                     if (varDef.VariableName == type)
                     {
                         staticAccess = false;
-                        return FindGlobalType(script, varDef.Type);
+                        return FindGlobalType(varDef.Type);
                     }
                 }
             }
eb90cfa46251d595f8b8f8c5f29ea720d2833038
Ivan Mogilko <ikm_spb@yahoo.com>
2022-09-21 00:44:13 +0300

    Script API: removed a rule that Viewport and Camera 0 can't be deleted
    This was a silly thing to have, and actually it was not implemented correctly, as a cam or viewport with 0th index is not necessarily the "primary" one (set as Game.Camera and Screen.Viewport). The logic was rather weak there anyway.

--- Engine/ac/gamestate.cpp
+++ Engine/ac/gamestate.cpp
@@ -294,8 +294,7 @@ ScriptViewport *GameState::RegisterRoomViewport(int index, int32_t handle)
 
 void GameState::DeleteRoomViewport(int index)
 {
-    // NOTE: viewport 0 can not be deleted
-    if (index <= 0 || (size_t)index >= _roomViewports.size())
+    if (index < 0 || (size_t)index >= _roomViewports.size())
         return;
     auto handle = _scViewportHandles[index];
     auto scobj = (ScriptViewport*)ccGetObjectAddressFromHandle(handle);
@@ -365,8 +364,7 @@ ScriptCamera *GameState::RegisterRoomCamera(int index, int32_t handle)
 
 void GameState::DeleteRoomCamera(int index)
 {
-    // NOTE: camera 0 can not be deleted
-    if (index <= 0 || (size_t)index >= _roomCameras.size())
+    if (index < 0 || (size_t)index >= _roomCameras.size())
         return;
     auto handle = _scCameraHandles[index];
     auto scobj = (ScriptCamera*)ccGetObjectAddressFromHandle(handle);

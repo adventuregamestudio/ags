a93df971a2c3682b5fa90079caa65b59ac630447
Ivan Mogilko <ikm_spb@yahoo.com>
2022-09-28 02:34:58 +0300

    Editor: refactored dialog script styling, fixing comments and strings

--- Editor/AGS.Editor/Panes/ScintillaWrapper.cs
+++ Editor/AGS.Editor/Panes/ScintillaWrapper.cs
@@ -2514,8 +2514,84 @@ namespace AGS.Editor
             {
                 char c = (char)scintillaControl1.GetCharAt(i);
 
-                if (!Char.IsLetterOrDigit(c) && (stylingMode != Style.Cpp.Comment ||
-                    stylingMode != Style.Cpp.CommentLine || stylingMode != Style.Cpp.String))
+                // Handle end of line
+                if (c == '\n')
+                {
+                    onNewLine = true;
+                    atNewWord = false;
+                    onScriptLine = false;
+                    // The multiline comments and strings should keep their style;
+                    // everything else is cut of at the end of the line
+                    if (stylingMode != Style.Cpp.Comment && stylingMode != Style.Cpp.String)
+                    {
+                        if (laststyle < i)
+                        {
+                            scintillaControl1.SetStyling(i - laststyle, stylingMode);
+                            laststyle = i;
+                        }
+                        stylingMode = Style.Cpp.Default;
+                    }
+                    continue;
+                }
+
+                // Handle new non-empty line
+                if (onNewLine)
+                {
+                    // first whitespace on a new line triggers normal script
+                    if (Char.IsWhiteSpace(c))
+                    {
+                        onScriptLine = true;
+                        onNewLine = false;
+                        atNewWord = true;
+                        continue;
+                    }
+                    // first non-whitespace on a new line: switch to dialog script style
+                    // (unless we are inside a multiline comment or string)
+                    else if (stylingMode != Style.Cpp.CommentLine &&
+                        stylingMode != Style.Cpp.String)
+                    {
+                        scintillaControl1.SetStyling(i - laststyle, stylingMode);
+                        stylingMode = Style.Cpp.Word2;
+                        laststyle = i;
+                    }
+                    onNewLine = false;
+                    atNewWord = true;
+                }
+
+                // Handle inside comments
+                if (stylingMode == Style.Cpp.CommentLine)
+                {
+                    // if inside a one-line comment, then just skip everything (except EOL)
+                    continue;
+                }
+                else if (stylingMode == Style.Cpp.Comment)
+                {
+                    // if inside a multiline comment, then test for the comment end
+                    if (c == '/' && scintillaControl1.GetCharAt(i - 1) == '*')
+                    {
+                        scintillaControl1.SetStyling(i - laststyle + 1, stylingMode);
+                        stylingMode = Style.Cpp.Default;
+                        laststyle = i + 1;
+                        atNewWord = true;
+                    }
+                    continue;
+                }
+
+                // Handle inside strings
+                if (stylingMode == Style.Cpp.String)
+                {
+                    // test for the string end
+                    if (c == '"')
+                    {
+                        scintillaControl1.SetStyling(i - laststyle + 1, stylingMode);
+                        laststyle = i + 1;
+                        stylingMode = Style.Cpp.Default;
+                        atNewWord = true;
+                    }
+                    continue;
+                }
+
+                if (!Char.IsLetterOrDigit(c))
                 {
                     string lastword = previousWordFrom(i);
                     if (lastword.Length != 0)
@@ -2535,85 +2611,38 @@ namespace AGS.Editor
                     }
                 }
 
-                if (c == '\n')
-                {
-                    onNewLine = true;
-                    atNewWord = false;
-                    onScriptLine = false;
-                    if (stylingMode != Style.Cpp.Comment && stylingMode != Style.Cpp.String)
-                    {
-                        if (laststyle < i)
-                        {
-                            scintillaControl1.SetStyling(i - laststyle, stylingMode);
-                            laststyle = i;
-                        }
-                        stylingMode = Style.Cpp.Default;
-                    }
-                    continue;
-                }
-
-                if (onNewLine)
-                {
-                    if (Char.IsWhiteSpace(c))
-                    {
-                        onScriptLine = true;
-                        onNewLine = false;
-                        atNewWord = true;
-                        continue;
-                    }
-                }
-
+                // Handle regular script
                 if (onScriptLine)
                 {
                     if (isOperator(c))
                     {
-                        if (stylingMode != Style.Cpp.String && stylingMode != Style.Cpp.Comment && stylingMode != Style.Cpp.CommentLine)
-                        {
-                            scintillaControl1.SetStyling(i - laststyle, stylingMode);
-                            scintillaControl1.SetStyling(1, Style.Cpp.Operator);
-                            stylingMode = Style.Cpp.Default;
-                            laststyle = i + 1;
-                        }
+                        scintillaControl1.SetStyling(i - laststyle, stylingMode);
+                        scintillaControl1.SetStyling(1, Style.Cpp.Operator);
+                        stylingMode = Style.Cpp.Default;
+                        laststyle = i + 1;
                     }
                     // Style the numbers only when the digit is the first character in a word
                     else if (atNewWord && Char.IsDigit(c))
                     {
-                        if (stylingMode != Style.Cpp.String && stylingMode != Style.Cpp.Comment && stylingMode != Style.Cpp.CommentLine)
-                        {
-                            scintillaControl1.SetStyling(i - laststyle, stylingMode);
-                            stylingMode = Style.Cpp.Number;
-                            laststyle = i;
-                        }
+                        scintillaControl1.SetStyling(i - laststyle, stylingMode);
+                        stylingMode = Style.Cpp.Number;
+                        laststyle = i;
                     }
                     else if (c == '"')
                     {
-                        if (stylingMode == Style.Cpp.String)
-                        {
-                            scintillaControl1.SetStyling(i - laststyle + 1, stylingMode);
-                            laststyle = i + 1;
-                            stylingMode = Style.Cpp.Default;
-                        }
-                        else stylingMode = Style.Cpp.String;
+                        stylingMode = Style.Cpp.String;
                     }
                     else if (Char.IsWhiteSpace(c) || Char.IsPunctuation(c))
                     {
-                        if (stylingMode != Style.Cpp.String && stylingMode != Style.Cpp.Comment && stylingMode != Style.Cpp.CommentLine)
-                        {
-                            scintillaControl1.SetStyling(i - laststyle, stylingMode);
-                            stylingMode = Style.Cpp.Default;
-                            laststyle = i;
-                        }
-                    }
-                }
-                else
-                {
-                    if (onNewLine && stylingMode != Style.Cpp.Comment)
-                    {
                         scintillaControl1.SetStyling(i - laststyle, stylingMode);
-                        stylingMode = Style.Cpp.Word2;
+                        stylingMode = Style.Cpp.Default;
                         laststyle = i;
                     }
-                    if (c == ':' && stylingMode != Style.Cpp.Comment && stylingMode != Style.Cpp.CommentLine)
+                }
+                // Handle dialog script
+                else
+                {
+                    if (c == ':')
                     {
                         scintillaControl1.SetStyling(i - laststyle + 1, stylingMode);
                         laststyle = i + 1;
@@ -2627,20 +2656,17 @@ namespace AGS.Editor
                     }
                 }
 
+                // Handle comment starts
                 if (c == '/')
                 {
-                    if (stylingMode == Style.Cpp.Comment && scintillaControl1.GetCharAt(i - 1) == '*')
-                    {
-                        scintillaControl1.SetStyling(i - laststyle + 1, stylingMode);
-                        stylingMode = Style.Cpp.Default;
-                        laststyle = i + 1;
-                    }
-                    else if (scintillaControl1.GetCharAt(i + 1) == '*' && onScriptLine)
+                    // multiline comment
+                    if (scintillaControl1.GetCharAt(i + 1) == '*' && onScriptLine)
                     {
                         scintillaControl1.SetStyling(i - laststyle, stylingMode);
                         stylingMode = Style.Cpp.Comment;
                         laststyle = i;
                     }
+                    // single-line comment
                     else if (scintillaControl1.GetCharAt(i + 1) == '/')
                     {
                         scintillaControl1.SetStyling(i - laststyle, stylingMode);

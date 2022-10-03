adventuregamestudio/ags

commit c5a99b1f07b51dfc28bb72dd235ce731e6b94dce

Engine: make sure custom overlays connect with script ref properly
This ensures that every overlay that is exposed to script has a proper script handle saved, and then invalidates the script handle when being removed by the engine.

Engine/ac/overlay.cpp
@@ -185,20 +185,14 @@ ScreenOverlay *Overlay_CreateTextCore(int x, int y, int width, int font, int tex
 ScriptOverlay* Overlay_CreateGraphical(int x, int y, int slot, int transparent)
 {
     auto *over = Overlay_CreateGraphicCore(x, y, slot, transparent != 0);
-    ScriptOverlay *sco = new ScriptOverlay();
-    sco->overlayId = over->type;
-    ccRegisterManagedObject(sco, sco);
-    return sco;
+    return over ? create_scriptoverlay(*over) : nullptr;
 }
 
 ScriptOverlay* Overlay_CreateTextual(int x, int y, int width, int font, int colour, const char* text) {
     data_to_game_coords(&x, &y);
     width = data_to_game_coord(width);
     auto *over = Overlay_CreateTextCore(x, y, width, font, colour, text, DISPLAYTEXT_NORMALOVERLAY, 0);
-    ScriptOverlay *sco = new ScriptOverlay();
-    sco->overlayId = over->type;
-    ccRegisterManagedObject(sco, sco);
-    return sco;
+    return over ? create_scriptoverlay(*over) : nullptr;
 }
 
 int Overlay_GetTransparency(ScriptOverlay *scover) {
@@ -239,7 +233,7 @@ void Overlay_SetZOrder(ScriptOverlay *scover, int zorder) {
 
 // Creates and registers a managed script object for existing overlay object;
 // optionally adds an internal engine reference to prevent object's disposal
-ScriptOverlay* create_scriptobj_for_overlay(ScreenOverlay &over, bool internal_ref)
+ScriptOverlay* create_scriptoverlay(ScreenOverlay &over, bool internal_ref)
 {
     ScriptOverlay *scover = new ScriptOverlay();
     scover->overlayId = over.type;
@@ -252,14 +246,21 @@ ScriptOverlay* create_scriptobj_for_overlay(ScreenOverlay &over, bool internal_r
 
 // Invalidates existing script object to let user know that previous overlay is gone,
 // and releases engine's internal reference (script object may exist while there are user refs)
-static void invalidate_and_subref(ScreenOverlay &over, ScriptOverlay *&scover)
+static void invalidate_and_subref(ScreenOverlay &over, ScriptOverlay **scover)
 {
-    scover->overlayId = -1;
-    scover = nullptr;
-    ccReleaseObjectReference(over.associatedOverlayHandle);
+    if (scover && (*scover))
+    {
+        (*scover)->overlayId = -1;
+        *scover = nullptr;
+    }
+    if (over.associatedOverlayHandle > 0)
+    {
+        ccReleaseObjectReference(over.associatedOverlayHandle);
+        over.associatedOverlayHandle = 0;
+    }
 }
 
-// Frees overlay resources and disposes script object if there are no more refs
+// Frees overlay resources and tell to dispose script object if there are no refs left
 static void dispose_overlay(ScreenOverlay &over)
 {
     delete over.pic;
@@ -267,8 +268,13 @@ static void dispose_overlay(ScreenOverlay &over)
     if (over.ddb != nullptr)
         gfxDriver->DestroyDDB(over.ddb);
     over.ddb = nullptr;
-    if (over.associatedOverlayHandle) // dispose script object if there are no more refs
+    // invalidate script object and dispose it if there are no more refs
+    if (over.associatedOverlayHandle > 0)
+    {
+        ScriptOverlay *scover = (ScriptOverlay*)ccGetObjectAddressFromHandle(over.associatedOverlayHandle);
+        if (scover) scover->overlayId = -1;
         ccAttemptDisposeObject(over.associatedOverlayHandle);
+    }
 }
 
 void remove_screen_overlay_index(size_t over_idx)
@@ -280,20 +286,18 @@ void remove_screen_overlay_index(size_t over_idx)
         play.complete_overlay_on = 0;
     }
     else if (over.type == play.text_overlay_on)
-    {
-        if (play.speech_text_scover)
-            invalidate_and_subref(over, play.speech_text_scover);
+    { // release internal ref for speech text
+        invalidate_and_subref(over, &play.speech_text_scover);
         play.text_overlay_on = 0;
     }
     else if (over.type == OVER_PICTURE)
-    {
-        if (play.speech_face_scover)
-            invalidate_and_subref(over, play.speech_face_scover);
+    { // release internal ref for speech face
+        invalidate_and_subref(over, &play.speech_face_scover);
         face_talking = -1;
     }
     else if (over.bgSpeechForChar > 0)
     { // release internal ref for bg speech
-        ccReleaseObjectReference(over.associatedOverlayHandle);
+        invalidate_and_subref(over, nullptr);
     }
     dispose_overlay(over);
     screenover.erase(screenover.begin() + over_idx);
@@ -362,11 +366,11 @@ size_t add_screen_overlay(int x, int y, int type, Bitmap *piccy, int pic_offx, i
         // only make script object for blocking speech now, because messagebox blocks all script
         // and therefore cannot be accessed, so no practical reason for that atm
         if (type == OVER_TEXTSPEECH)
-            play.speech_text_scover = create_scriptobj_for_overlay(over, true);
+            play.speech_text_scover = create_scriptoverlay(over, true);
     }
     else if (type == OVER_PICTURE)
     {
-        play.speech_face_scover = create_scriptobj_for_overlay(over, true);
+        play.speech_face_scover = create_scriptoverlay(over, true);
     }
     over.MarkChanged();
     screenover.push_back(std::move(over));

Engine/ac/character.cpp
@@ -763,7 +763,7 @@ ScriptOverlay* Character_SayBackground(CharacterInfo *chaa, const char *texx) {
     if (ovri<0)
         quit("!SayBackground internal error: no overlay");
     // Create script object with an internal ref, keep at least until internal timeout
-    return create_scriptobj_for_overlay(screenover[ovri], true);
+    return create_scriptoverlay(screenover[ovri], true);
 }
 
 void Character_SetAsPlayer(CharacterInfo *chaa) {

Engine/ac/gamestate.h
@@ -243,6 +243,8 @@ struct GameState {
     int  complete_overlay_on;
     // Is there a blocking text overlay on screen (contains overlay ID)
     int  text_overlay_on;
+    // Script overlay objects, because we must return same pointers
+    // whenever user script queries for them.
     // Blocking speech overlay managed object, for accessing in scripts
     ScriptOverlay *speech_text_scover;
     // Speech portrait overlay managed object

Engine/ac/overlay.h
@@ -47,7 +47,7 @@ size_t add_screen_overlay(int x, int y, int type, Common::Bitmap *piccy, int pic
 void remove_screen_overlay_index(size_t over_idx);
 // Creates and registers a managed script object for existing overlay object;
 // optionally adds an internal engine reference to prevent object's disposal
-ScriptOverlay* create_scriptobj_for_overlay(ScreenOverlay &over, bool internal_ref = false);
+ScriptOverlay* create_scriptoverlay(ScreenOverlay &over, bool internal_ref = false);
 void recreate_overlay_ddbs();
 
 

commit acd7ac23d35aafbceb0115b3c3ae1a946e4de20a

Engine: fixed ScriptOverlay loosing ID when changing text
Apparently broken by c5a99b1f07b51dfc28bb72dd235ce731e6b94dce

Engine/ac/global_overlay.cpp
@@ -45,7 +45,8 @@ int CreateTextOverlay(int xx, int yy, int wii, int fontid, int text_color, const
 void SetTextOverlay(int ovrid, int xx, int yy, int wii, int fontid, int text_color, const char *text) {
     RemoveOverlay(ovrid);
     const int disp_type = ovrid;
-    if (CreateTextOverlay(xx, yy, wii, fontid, text_color, text, disp_type) !=ovrid)
+    int new_ovrid = CreateTextOverlay(xx, yy, wii, fontid, text_color, text, disp_type);
+    if (new_ovrid != ovrid)
         quit("SetTextOverlay internal error: inconsistent type ids");
 }
 

Engine/ac/overlay.cpp
@@ -55,11 +55,18 @@ void Overlay_SetText(ScriptOverlay *scover, int wii, int fontid, int text_color,
     int xx = game_to_data_coord(screenover[ovri].x);
     int yy = game_to_data_coord(screenover[ovri].y);
 
-    RemoveOverlay(scover->overlayId);
+    // FIXME: a refactor needed!
+    // these calls to RemoveOverlay and CreateOverlay below are potentially dangerous,
+    // because they may allocate new script objects too under certain conditions.
+    // This did not happen because users only had access to custom overlay pointers before,
+    // but now they also may access internal overlays (such as Say text and portrait).
     const int disp_type = scover->overlayId;
+    RemoveOverlay(scover->overlayId);
 
-    if (CreateTextOverlay(xx, yy, wii, fontid, text_color, get_translation(text), disp_type) != scover->overlayId)
+    int new_ovrid = CreateTextOverlay(xx, yy, wii, fontid, text_color, get_translation(text), disp_type);
+    if (new_ovrid != disp_type)
         quit("SetTextOverlay internal error: inconsistent type ids");
+    scover->overlayId = new_ovrid;
 }
 
 int Overlay_GetX(ScriptOverlay *scover) {
adventuregamestudio/ags

commit fcc873ef0c26439faf67100f4d70d540434ca9f5

Engine: store gui draw order in a std::vector

Engine/ac/gui.cpp
@@ -11,6 +11,7 @@
 // http://www.opensource.org/licenses/artistic-license-2.0.php
 //
 //=============================================================================
+#include <algorithm>
 #include <cstdio>
 #include "ac/gui.h"
 #include "ac/common.h"
@@ -437,29 +438,15 @@ void replace_macro_tokens(const char *text, String &fixed_text) {
     }
 }
 
-
-void update_gui_zorder() {
-    int numdone = 0, b;
-
-    // for each GUI
-    for (int a = 0; a < game.numgui; a++) {
-        // find the right place in the draw order array
-        int insertAt = numdone;
-        for (b = 0; b < numdone; b++) {
-            if (guis[a].ZOrder < guis[play.gui_draw_order[b]].ZOrder) {
-                insertAt = b;
-                break;
-            }
-        }
-        // insert the new item
-        for (b = numdone - 1; b >= insertAt; b--)
-            play.gui_draw_order[b + 1] = play.gui_draw_order[b];
-        play.gui_draw_order[insertAt] = a;
-        numdone++;
-    }
-
+bool sort_gui_less(const int g1, const int g2)
+{
+    return guis[g1].ZOrder < guis[g2].ZOrder;
 }
 
+void update_gui_zorder()
+{
+    std::sort(play.gui_draw_order.begin(), play.gui_draw_order.end(), sort_gui_less);
+}
 
 void export_gui_controls(int ee)
 {
@@ -568,14 +555,13 @@ int gui_on_mouse_move()
     else {
         // Scan for mouse-y-pos GUIs, and pop one up if appropriate
         // Also work out the mouse-over GUI while we're at it
-        int ll;
-        for (ll = 0; ll < game.numgui;ll++) {
-            const int guin = play.gui_draw_order[ll];
+        // CHECKME: not sure why, but we're testing forward draw order here -
+        // from farthest to nearest (this was in original code?)
+        for (int guin : play.gui_draw_order) {
             if (guis[guin].IsInteractableAt(mousex, mousey)) mouse_over_gui=guin;
 
             if (guis[guin].PopupStyle!=kGUIPopupMouseY) continue;
             if (play.complete_overlay_on > 0) break;  // interfaces disabled
-            //    if (play.disabled_user_interface>0) break;
             if (ifacepopped==guin) continue;
             if (!guis[guin].IsVisible()) continue;
             // Don't allow it to be popped up while skipping cutscene

Engine/game/game_init.cpp
@@ -489,7 +489,9 @@ HGameInitError InitGameState(const LoadedGameEntities &ents, GameDataVersion dat
         // labels are not clickable by default
         guilabels[i].SetClickable(false);
     }
-    play.gui_draw_order = (int*)calloc(game.numgui * sizeof(int), 1);
+    play.gui_draw_order.resize(game.numgui);
+    for (int i = 0; i < game.numgui; ++i)
+        play.gui_draw_order[i] = i;
     update_gui_zorder();
     calculate_reserved_channel_count();
 

Engine/ac/game.cpp
@@ -552,7 +552,7 @@ void unload_game_file()
     ccUnregisterAllObjects();
 
     free_do_once_tokens();
-    free(play.gui_draw_order);
+    play.gui_draw_order.clear();
 
     resetRoomStatuses();
 

Engine/ac/gamestate.h
@@ -213,7 +213,7 @@ struct GameState {
     int   gamma_adjustment;
     short temporarily_turned_off_character;  // Hide Player Charactr ticked
     short inv_backwards_compatibility;
-    int  *gui_draw_order;
+    std::vector<int> gui_draw_order; // used only for hit detection now
     std::vector<AGS::Common::String> do_once_tokens;
     int   text_min_display_time_ms;
     int   ignore_user_input_after_text_timeout_ms;

Engine/ac/global_gui.cpp
@@ -237,12 +237,10 @@ int GetGUIObjectAt (int xx, int yy) {
 
 int GetGUIAt (int xx,int yy) {
     data_to_game_coords(&xx, &yy);
-
-    int aa, ll;
-    for (ll = game.numgui - 1; ll >= 0; ll--) {
-        aa = play.gui_draw_order[ll];
-        if (guis[aa].IsInteractableAt(xx, yy))
-            return aa;
+    // Test in the opposite order (from closer to further)
+    for (auto g = play.gui_draw_order.crbegin(); g < play.gui_draw_order.crend(); ++g) {
+        if (guis[*g].IsInteractableAt(xx, yy))
+            return *g;
     }
     return -1;
 }

Engine/game/savegame_v321.cpp
@@ -173,22 +173,19 @@ static void restore_game_play_ex_data(Stream *in)
         play.do_once_tokens[i] = rbuffer;
     }
 
-    in->ReadArrayOfInt32(&play.gui_draw_order[0], game.numgui);
+    in->Seek(game.numgui * sizeof(int32_t)); // gui_draw_order
 }
 
 static void restore_game_play(Stream *in, RestoredData &r_data)
 {
     int screenfadedout_was = play.screen_is_faded_out;
     int roomchanges_was = play.room_changes;
-    // make sure the pointer is preserved
-    int *gui_draw_order_was = play.gui_draw_order;
 
     ReadGameState_Aligned(in, r_data);
     r_data.Cameras[0].Flags = r_data.Camera0_Flags;
 
     play.screen_is_faded_out = screenfadedout_was;
     play.room_changes = roomchanges_was;
-    play.gui_draw_order = gui_draw_order_was;
 
     restore_game_play_ex_data(in);
 }

commit ff210b2ce50cea0e903cd1d09fa7ad1734a862b0

Engine: fixed GUI sorting for cases when ZOrder is equal
Was broken by fcc873ef0c26439faf67100f4d70d540434ca9f5

Engine/ac/gui.cpp
@@ -439,7 +439,8 @@ void replace_macro_tokens(const char *text, String &fixed_text) {
 
 bool sort_gui_less(const int g1, const int g2)
 {
-    return guis[g1].ZOrder < guis[g2].ZOrder;
+    return (guis[g1].ZOrder < guis[g2].ZOrder) ||
+        (guis[g1].ZOrder == guis[g2].ZOrder) && (g1 < g2);
 }
 
 void update_gui_zorder()

Engine/game/game_init.cpp
@@ -11,6 +11,7 @@
 // http://www.opensource.org/licenses/artistic-license-2.0.php
 //
 //=============================================================================
+#include <numeric>
 #include "ac/character.h"
 #include "ac/dialog.h"
 #include "ac/display.h"
@@ -483,8 +484,7 @@ HGameInitError InitGameState(const LoadedGameEntities &ents, GameDataVersion dat
         label.SetClickable(false);
     }
     play.gui_draw_order.resize(game.numgui);
-    for (int i = 0; i < game.numgui; ++i)
-        play.gui_draw_order[i] = i;
+    std::iota(play.gui_draw_order.begin(), play.gui_draw_order.end(), 0);
     update_gui_zorder();
     calculate_reserved_channel_count();
 
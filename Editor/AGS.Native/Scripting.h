1f38e662fe5aa30a6eda69fd375cc5a359cde23e
Ivan Mogilko <ikm_spb@yahoo.com>
2023-02-04 00:48:07 +0300

    AGS.Native: write scripts using native methods again
    The script writing was reimplemented in the managed code (C++ CLR) in c5c7c8748317ac7ae97c650ce1e6b2548aca2f54 , but this is quite inconvenient, as there's a script serialization code in the native part which is still used by the compiled room serialization code, and thus we'd have to duplicate any additions in native and managed code in parallel.
    
    This change removes managed reimplementation, and makes CompiledScript::Write use native serialization instead.
    
    Because the scripts are written in the middle of the compiled game format, which is serialized by the managed code too, we cannot use same stream object directly.
    There are two options:
    1) Write to a temp file using native stream, then copy file contents to the provided managed stream.
    2) Write to a native membuf, then copy buf's contents to the managed stream (might require conversion, or marshaling).
    
    I implemented this using a temp file for now. If this proves to be slow with large number of scripts, we may switch to using memory buffer (and memory stream) instead.

--- Editor/AGS.Native/Scripting.h
+++ Editor/AGS.Native/Scripting.h
@@ -1,5 +1,7 @@
 #include "script/cc_script.h"
 #include "script/cc_internal.h"
+#include "util/file.h"
+#include "util/stream.h"
 
 namespace AGS
 {
@@ -40,65 +42,20 @@ namespace AGS
                 {
                     throw gcnew AGS::Types::CompileError(gcnew System::String("Script has not been compiled: ") + scriptFileName);
                 }
-                System::IO::BinaryWriter ^writer = gcnew System::IO::BinaryWriter(ostream);
-                for (int i = 0; i < 4; ++i)
-                {
-                    // the BinaryWriter seems to be treating CHAR as a 4-byte type here?
-                    writer->Write((System::Byte)scfilesig[i]);
-                }
-                const ccScript *cs = _compiledScript->get();
-                writer->Write(SCOM_VERSION);
-                writer->Write(cs->globaldatasize);
-                writer->Write(cs->codesize);
-                writer->Write(cs->stringssize);
-                for (int i = 0; i < cs->globaldatasize; ++i)
-                {
-                    writer->Write((System::Byte)cs->globaldata[i]);
-                }
-                for (int i = 0; i < cs->codesize; ++i)
-                {
-                    writer->Write((int)cs->code[i]);
-                }
-                for (int i = 0; i < cs->stringssize; ++i)
-                {
-                    writer->Write((System::Byte)cs->strings[i]);
-                }
-                writer->Write(cs->numfixups);
-                for (int i = 0; i < cs->numfixups; ++i)
-                {
-                    writer->Write((System::Byte)cs->fixuptypes[i]);
-                }
-                for (int i = 0; i < cs->numfixups; ++i)
-                {
-                    writer->Write(cs->fixups[i]);
-                }
-                writer->Write(cs->numimports);
-                for (int i = 0; i < cs->numimports; ++i)
-                {
-                    for (int j = 0, len = strlen(cs->imports[i]); j <= len; ++j)
-                    {
-                        writer->Write((System::Byte)cs->imports[i][j]);
-                    }
-                }
-                writer->Write(cs->numexports);
-                for (int i = 0; i < cs->numexports; ++i)
-                {
-                    for (int j = 0, len = strlen(cs->exports[i]); j <= len; ++j)
-                    {
-                        writer->Write((System::Byte)cs->exports[i][j]);
-                    }
-                    writer->Write(cs->export_addr[i]);
-                }
-                writer->Write(cs->numSections);
-                for (int i = 0; i < cs->numSections; ++i)
-                {
-                    for (int j = 0, len = strlen(cs->sectionNames[i]); j <= len; ++j)
-                    {
-                        writer->Write((System::Byte)cs->sectionNames[i][j]);
-                    }
-                    writer->Write(cs->sectionOffsets[i]);
-                }
-                writer->Write(ENDFILESIG);
+                
+                // NOTE: if using temp files will prove inefficient,
+                // we might switch to writing into a memory buffer instead,
+                // or switching between file and buffer depending on script's size.
+                String ^tempFile = System::IO::Path::GetTempFileName();
+                
+                std::unique_ptr<AGS::Common::Stream> out(
+                    AGS::Common::File::CreateFile(TextHelper::ConvertUTF8(tempFile)));
+                (*_compiledScript)->Write(out.get());
+                out.reset();
+                
+                Utilities::CopyFileContents(tempFile, ostream);
+
+                System::IO::File::Delete(tempFile);
             }
         };
 	}

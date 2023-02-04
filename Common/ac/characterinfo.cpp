adventuregamestudio/ags

commit 0f19fc3d2b9efbc4eb809032bcb7d60b652e1150

Engine: fixed idle_anim_speed after restoring old save in updated game

Common/ac/characterinfo.cpp
@@ -19,7 +19,7 @@
 using AGS::Common::Stream;
 
 
-void CharacterInfo::ReadFromFile(Stream *in)
+void CharacterInfo::ReadFromFile(Stream *in, GameDataVersion data_ver, int save_ver)
 {
     defview = in->ReadInt32();
     talkview = in->ReadInt32();
@@ -68,8 +68,11 @@ void CharacterInfo::ReadFromFile(Stream *in)
     in->Read(scrname, MAX_SCRIPT_NAME_LEN);
     on = in->ReadInt8();
 
-    if (loaded_game_file_version < kGameVersion_360_16)
+    if ((data_ver > kGameVersion_Undefined && data_ver < kGameVersion_360_16) ||
+        (save_ver >= 0 && save_ver < 2))
+    {
         idle_anim_speed = animspeed + 5;
+    }
 }
 
 void CharacterInfo::WriteToFile(Stream *out)

Common/ac/gamesetupstruct.cpp
@@ -11,7 +11,6 @@
 // http://www.opensource.org/licenses/artistic-license-2.0.php
 //
 //=============================================================================
-
 #include "ac/audiocliptype.h"
 #include "ac/gamesetupstruct.h"
 #include "ac/oldgamesetupstruct.h"
@@ -232,7 +231,7 @@ void GameSetupStruct::read_characters(Common::Stream *in)
 {
     chars = new CharacterInfo[numcharacters];
 
-    ReadCharacters_Aligned(in);
+    ReadCharacters_Aligned(in, false);
 }
 
 void GameSetupStruct::read_lipsync(Common::Stream *in, GameDataVersion data_ver)
@@ -269,12 +268,12 @@ void GameSetupStruct::read_messages(Common::Stream *in, GameDataVersion data_ver
     load_messages = nullptr;
 }
 
-void GameSetupStruct::ReadCharacters_Aligned(Stream *in)
+void GameSetupStruct::ReadCharacters_Aligned(Stream *in, bool is_save)
 {
     AlignedStream align_s(in, Common::kAligned_Read);
     for (int iteratorCount = 0; iteratorCount < numcharacters; ++iteratorCount)
     {
-        chars[iteratorCount].ReadFromFile(&align_s);
+        chars[iteratorCount].ReadFromFile(&align_s, is_save ? kGameVersion_Undefined : loaded_game_file_version, 0);
         align_s.Reset();
     }
 }
@@ -412,7 +411,7 @@ void GameSetupStruct::ReadFromSaveGame_v321(Stream *in, char* gswas, ccScript* c
     in->ReadArrayOfInt32(&options[0], OPT_HIGHESTOPTION_321 + 1);
     options[OPT_LIPSYNCTEXT] = in->ReadByte();
 
-    ReadCharacters_Aligned(in);
+    ReadCharacters_Aligned(in, true);
 }
 
 //=============================================================================

Common/ac/characterinfo.h
@@ -17,6 +17,7 @@
 #include <vector>
 #include "core/types.h"
 #include "ac/common_defines.h" // constants
+#include "ac/game_version.h"
 
 namespace AGS { namespace Common { class Stream; } }
 using namespace AGS; // FIXME later
@@ -112,7 +113,7 @@ struct CharacterInfo {
 	void update_character_idle(CharacterExtras *chex, int &doing_nothing);
 	void update_character_follower(int &char_index, std::vector<int> &followingAsSheep, int &doing_nothing);
 
-    void ReadFromFile(Common::Stream *in);
+    void ReadFromFile(Common::Stream *in, GameDataVersion data_ver, int save_ver = -1);
     void WriteToFile(Common::Stream *out);
 };
 

Common/ac/gamesetupstruct.h
@@ -141,7 +141,7 @@ struct GameSetupStruct: public GameSetupStructBase {
     void read_lipsync(Common::Stream *in, GameDataVersion data_ver);
     void read_messages(Common::Stream *in, GameDataVersion data_ver);
 
-    void ReadCharacters_Aligned(Common::Stream *in);
+    void ReadCharacters_Aligned(Common::Stream *in, bool is_save);
     void WriteCharacters_Aligned(Common::Stream *out);
     //------------------------------
     // Part 3

Engine/game/savegame_components.cpp
@@ -555,7 +555,7 @@ HSaveError ReadCharacters(Stream *in, int32_t cmp_ver, const PreservedParams& /*
         return err;
     for (int i = 0; i < game.numcharacters; ++i)
     {
-        game.chars[i].ReadFromFile(in);
+        game.chars[i].ReadFromFile(in, kGameVersion_Undefined, cmp_ver);
         charextra[i].ReadFromSavegame(in, cmp_ver);
         Properties::ReadValues(play.charProps[i], in);
         if (loaded_game_file_version <= kGameVersion_272)

commit 899b6be0ea088737799e075e7906bc3308108699

Engine: fixed idle anim speed always set in backwards compat mode
Was broken by 0f19fc3d2b9efbc4eb809032bcb7d60b652e1150

Common/ac/characterinfo.cpp
@@ -69,7 +69,7 @@ void CharacterInfo::ReadFromFile(Stream *in, GameDataVersion data_ver, int save_
     on = in->ReadInt8();
 
     if ((data_ver > kGameVersion_Undefined && data_ver < kGameVersion_360_16) ||
-        (save_ver >= 0 && save_ver < 2))
+        ((data_ver == kGameVersion_Undefined) && save_ver >= 0 && save_ver < 2))
     {
         idle_anim_speed = animspeed + 5;
     }

Common/ac/gamesetupstruct.cpp
@@ -271,9 +271,11 @@ void GameSetupStruct::read_messages(Common::Stream *in, GameDataVersion data_ver
 void GameSetupStruct::ReadCharacters_Aligned(Stream *in, bool is_save)
 {
     AlignedStream align_s(in, Common::kAligned_Read);
+    const GameDataVersion data_ver = is_save ? kGameVersion_Undefined : loaded_game_file_version;
+    const int save_ver = is_save ? 0 : -1;
     for (int iteratorCount = 0; iteratorCount < numcharacters; ++iteratorCount)
     {
-        chars[iteratorCount].ReadFromFile(&align_s, is_save ? kGameVersion_Undefined : loaded_game_file_version, 0);
+        chars[iteratorCount].ReadFromFile(&align_s, data_ver, save_ver);
         align_s.Reset();
     }
 }
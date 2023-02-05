63fb09a7ab23375bf91267ec1b8822043488f1ca
Ivan Mogilko <ikm_spb@yahoo.com>
2023-02-05 16:04:13 +0300

    Updated build version (3.5.1.25)

--- Common/core/def_version.h
+++ Common/core/def_version.h
@@ -1,9 +1,9 @@
 #ifndef __AGS_CN_CORE__DEFVERSION_H
 #define __AGS_CN_CORE__DEFVERSION_H
 
-#define ACI_VERSION_STR      "3.5.1.24"
+#define ACI_VERSION_STR      "3.5.1.25"
 #if defined (RC_INVOKED) // for MSVC resource compiler
-#define ACI_VERSION_MSRC_DEF  3,5,1,24
+#define ACI_VERSION_MSRC_DEF  3,5,1,25
 #endif
 
 #ifdef NO_MP3_PLAYER
--- Editor/AGS.Types/Properties/AssemblyInfo.cs
+++ Editor/AGS.Types/Properties/AssemblyInfo.cs
@@ -22,9 +22,9 @@ namespace AGS.Types
     public class Version
     {
         public static readonly bool IS_BETA_VERSION = false;
-        public const string AGS_EDITOR_DATE = "January 2023";
+        public const string AGS_EDITOR_DATE = "February 2023";
         public const string AGS_EDITOR_FRIENDLY_VERSION = "3.5.1";
-        public const string AGS_EDITOR_VERSION = "3.5.1.24";
+        public const string AGS_EDITOR_VERSION = "3.5.1.25";
         public const string AGS_EDITOR_COPYRIGHT = "Copyright © 2006-2011 Chris Jones and 2011-2023 others.";
     }
 }

adc57b44fa7f98712df8eafc445b7fd727749739
Ivan Mogilko <ikm_spb@yahoo.com>
2023-02-06 18:31:06 +0300

    Updated pre-alpha build version (3.99.101)

--- Editor/AGS.Types/Properties/AssemblyInfo.cs
+++ Editor/AGS.Types/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ namespace AGS.Types
     {
         public static readonly bool IS_BETA_VERSION = false;
         public const string AGS_EDITOR_DATE = "February 2023";
-        public const string AGS_EDITOR_FRIENDLY_VERSION = "3.99.100";
-        public const string AGS_EDITOR_VERSION = "3.99.100.0";
+        public const string AGS_EDITOR_FRIENDLY_VERSION = "3.99.101";
+        public const string AGS_EDITOR_VERSION = "3.99.101.0";
         public const string AGS_EDITOR_COPYRIGHT = "Copyright © 2006-2011 Chris Jones and 2011-2023 others.";
     }
 }

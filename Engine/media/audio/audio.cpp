adventuregamestudio/ags

commit 86564e983572b4ffb3cd12b00c116ae91c6fcbe4

Engine: allow to place sounds on a crossfade channels for compatibility
The commit b954ad324b6d0bc6b981419093c53062fc11f1f5#gitext://gotocommit/b954ad324b6d0bc6b981419093c53062fc11f1f5 introduced a safety check on audio channel request; but previous versions let to put sound clips on a special crossfade channel that was normally unaccessible from script. Without such allowance some games get errors in script, where it expects a playing sound.

Engine/media/audio/audio.cpp
@@ -171,8 +171,9 @@ static int find_free_audio_channel(ScriptAudioClip *clip, int priority, bool int
     if (!interruptEqualPriority)
         priority--;
 
+    // NOTE: in backward compat mode we allow to place sound on a crossfade channel
     int startAtChannel = reserved_channel_count;
-    int endBeforeChannel = game.numGameChannels;
+    int endBeforeChannel = game.numCompatGameChannels;
 
     if (game.audioClipTypes[clip->type].reservedChannels > 0)
     {
@@ -181,7 +182,7 @@ static int find_free_audio_channel(ScriptAudioClip *clip, int priority, bool int
         {
             startAtChannel += game.audioClipTypes[i].reservedChannels;
         }
-        endBeforeChannel = std::min(game.numGameChannels,
+        endBeforeChannel = std::min(game.numCompatGameChannels,
             startAtChannel + game.audioClipTypes[clip->type].reservedChannels);
     }
 

Common/ac/gamesetupstruct.h
@@ -86,6 +86,7 @@ struct GameSetupStruct: public GameSetupStructBase {
     int               scoreClipID;
     // number of allowed game audio channels (the ones under direct user control)
     int               numGameChannels = 0;
+    int               numCompatGameChannels = 0;
     
     // TODO: I converted original array of sprite infos to vector here, because
     // statistically in most games sprites go in long continious sequences with minimal

Engine/game/game_init.cpp
@@ -138,7 +138,7 @@ String GetGameInitErrorText(GameInitErrorType err)
 // Initializes audio channels and clips and registers them in the script system
 void InitAndRegisterAudioObjects(GameSetupStruct &game)
 {
-    for (int i = 0; i < game.numGameChannels; ++i)
+    for (int i = 0; i < game.numCompatGameChannels; ++i)
     {
         scrAudioChannel[i].id = i;
         ccRegisterManagedObject(&scrAudioChannel[i], &ccDynamicAudio);
@@ -452,9 +452,15 @@ HGameInitError InitGameState(const LoadedGameEntities &ents, GameDataVersion dat
     old_speech_lines = ents.OldSpeechLines;
     // Set number of game channels corresponding to the loaded game version
     if (loaded_game_file_version < kGameVersion_360)
+    {
         game.numGameChannels = MAX_GAME_CHANNELS_v320;
+        game.numCompatGameChannels = TOTAL_AUDIO_CHANNELS_v320;
+    }
     else
+    {
         game.numGameChannels = MAX_GAME_CHANNELS;
+        game.numCompatGameChannels = MAX_GAME_CHANNELS;
+    }
     HError err = InitAndRegisterGameEntities(game);
     if (!err)
         return new GameInitError(kGameInitErr_EntityInitFail, err);

commit b3f4909b1c6a5e507aa1279ae32884b0292fb549

Engine: fixed find_free_audio_channel() incorrect compat fix
This fixes a regression from 86564e983, where the extra channel allowance was added excessively for all cases.

Engine/media/audio/audio.cpp
@@ -166,7 +166,7 @@ static int find_free_audio_channel(ScriptAudioClip *clip, int priority, bool int
 
     // NOTE: in backward compat mode we allow to place sound on a crossfade channel
     int startAtChannel = reserved_channel_count;
-    int endBeforeChannel = game.numCompatGameChannels;
+    int endBeforeChannel = game.numGameChannels;
 
     if (game.audioClipTypes[clip->type].reservedChannels > 0)
     {
@@ -175,6 +175,7 @@ static int find_free_audio_channel(ScriptAudioClip *clip, int priority, bool int
         {
             startAtChannel += game.audioClipTypes[i].reservedChannels;
         }
+        // NOTE: we allow to place sound on a crossfade channel for backward compatibility
         endBeforeChannel = std::min(game.numCompatGameChannels,
             startAtChannel + game.audioClipTypes[clip->type].reservedChannels);
     }
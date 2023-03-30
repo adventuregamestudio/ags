ec8a8e616a4ba231cd7b307017f0a3ee61935bc7
Ivan Mogilko <ikm_spb@yahoo.com>
2023-03-30 03:09:06 +0300

    Updated WIP version to 3.99.102.0

--- Common/core/def_version.h
+++ Common/core/def_version.h
@@ -1,9 +1,9 @@
 #ifndef __AGS_CN_CORE__DEFVERSION_H
 #define __AGS_CN_CORE__DEFVERSION_H
 
-#define ACI_VERSION_STR      "3.99.101.0"
+#define ACI_VERSION_STR      "3.99.102.0"
 #if defined (RC_INVOKED) // for MSVC resource compiler
-#define ACI_VERSION_MSRC_DEF  3,99,101,0
+#define ACI_VERSION_MSRC_DEF  3,99,102,0
 #endif
 
 #define SPECIAL_VERSION ""

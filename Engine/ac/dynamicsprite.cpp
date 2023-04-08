82194b261b4649281742c45498a4c34f452fd350
Ivan Mogilko <ikm_spb@yahoo.com>
2023-04-08 18:50:06 +0300

    Engine: in DynamicSprite.Create() adjust invalid sizes to min valid
    Historically, this function could *happen* to create bitmaps of 0x0 or even -1x-1 sizes and return a valid DynamicSprite object. Try to ensure the image library is passed valid parameters in such case, and keep going, for backwards compatibility.

--- Engine/ac/dynamicsprite.cpp
+++ Engine/ac/dynamicsprite.cpp
@@ -377,6 +377,13 @@ ScriptDynamicSprite* DynamicSprite_CreateFromDrawingSurface(ScriptDrawingSurface
 
 ScriptDynamicSprite* DynamicSprite_Create(int width, int height, int alphaChannel) 
 {
+    if (width <= 0 || height <= 0)
+    {
+        debug_script_warn("WARNING: DynamicSprite.Create: invalid size %d x %d, will adjust", width, height);
+        width = std::max(1, width);
+        height = std::max(1, height);
+    }
+
     data_to_game_coords(&width, &height);
 
     int gotSlot = spriteset.GetFreeIndex();

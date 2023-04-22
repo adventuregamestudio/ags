adventuregamestudio/ags

commit 7ed51861898d864902b0f61b0cb7d0acadef1fe3

Engine: fixed object cache may sometimes keep an old dynsprite ref
This may result in object texture not updating if the old sprite was deleted but a new dynamic sprite was created right after having same ID.

Was broken by 9dffb0480fd7b8287fead5bc65556b014c01e870 in 3.5.1, and also some later changes in 3.6.0.

Engine/ac/draw.cpp
@@ -797,16 +797,19 @@ void mark_object_changed(int objid)
     objcache[objid].y = -9999;
 }
 
-void reset_objcache_for_sprite(int sprnum)
+void reset_objcache_for_sprite(int sprnum, bool deleted)
 {
-    // Check if this sprite is assigned to any game object, and update them if necessary
+    // Check if this sprite is assigned to any game object, and mark these for update;
+    // if the sprite was deleted, also dispose shared textures
     // room objects cache
     if (croom != nullptr)
     {
         for (size_t i = 0; i < (size_t)croom->numobj; ++i)
         {
-            if (objs[i].num == sprnum)
+            if (objcache[i].sppic == sprnum)
                 objcache[i].sppic = -1;
+            if (deleted && (actsps[i].SpriteID == sprnum))
+                actsps[i] = ObjTexture();
         }
     }
     // character cache
@@ -814,6 +817,8 @@ void reset_objcache_for_sprite(int sprnum)
     {
         if (charcache[i].sppic == sprnum)
             charcache[i].sppic = -1;
+        if (deleted && (actsps[ACTSP_OBJSOFF + i].SpriteID == sprnum))
+            actsps[i] = ObjTexture();
     }
 }
 

Engine/ac/draw.h
@@ -68,7 +68,7 @@ void on_roomcamera_changed(Camera *cam);
 // Marks particular object as need to update the texture
 void mark_object_changed(int objid);
 // Resets all object caches which reference this sprite
-void reset_objcache_for_sprite(int sprnum);
+void reset_objcache_for_sprite(int sprnum, bool deleted);
 
 // whether there are currently remnants of a DisplaySpeech
 void mark_screen_dirty();

Engine/ac/game.cpp
@@ -1443,7 +1443,7 @@ void game_sprite_updated(int sprnum)
     gfxDriver->UpdateSharedDDB(sprnum, spriteset[sprnum],
         (game.SpriteInfos[sprnum].Flags & SPF_ALPHACHANNEL) != 0, false);
     // character and object draw caches
-    reset_objcache_for_sprite(sprnum);
+    reset_objcache_for_sprite(sprnum, false);
     // gui backgrounds
     for (auto &gui : guis)
     {
@@ -1481,7 +1481,7 @@ void game_sprite_deleted(int sprnum)
     // clear from texture cache
     gfxDriver->ClearSharedDDB(sprnum);
     // character and object draw caches
-    reset_objcache_for_sprite(sprnum);
+    reset_objcache_for_sprite(sprnum, true);
     // room object graphics
     if (croom != nullptr)
     {

commit 144ac42f99c6be0a1368c74e9a9baaf7440dab70

Engine: fixed reset_objcache_for_sprite() resetting wrong char texture
This mistake was introduced early on in 7ed51861898d864902b0f61b0cb7d0acadef1fe3 and not fixed still in the later fix 38d0dd3d7f0d6a4624d1f908f2e8d3c1abfee52a

Engine/ac/draw.cpp
@@ -159,7 +159,7 @@ struct ObjectCache
 
 // actsps is used for temporary storage of the bitmap and texture
 // of the latest version of the sprite (room objects and characters);
-// objects sprites begin with index 0, characters are after MAX_ROOM_OBJECTS
+// objects sprites begin with index 0, characters are after ACTSP_OBJSOFF
 std::vector<ObjTexture> actsps;
 // Walk-behind textures (3D renderers only)
 std::vector<ObjTexture> walkbehindobj;
@@ -842,7 +842,7 @@ void reset_objcache_for_sprite(int sprnum, bool deleted)
         if (charcache[i].sppic == sprnum)
             charcache[i].sppic = -1;
         if (deleted && (actsps[ACTSP_OBJSOFF + i].SpriteID == sprnum))
-            actsps[i].SpriteID = UINT32_MAX; // invalid sprite ref
+            actsps[ACTSP_OBJSOFF + i].SpriteID = UINT32_MAX; // invalid sprite ref
     }
 }
 
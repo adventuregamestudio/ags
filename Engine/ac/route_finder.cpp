47440b8a84671a145af15f8bb01f3e076f850c99
Ivan Mogilko <ikm_spb@yahoo.com>
2023-05-25 00:48:38 +0300

    Engine: removed legacy pathfinder

--- Engine/ac/route_finder.cpp
+++ Engine/ac/route_finder.cpp
@@ -14,7 +14,6 @@
 #include "ac/route_finder.h"
 #include <memory>
 #include "ac/route_finder_impl.h"
-#include "ac/route_finder_impl_legacy.h"
 #include "debug/out.h"
 
 using AGS::Common::Bitmap;
@@ -71,58 +70,12 @@ public:
     }
 };
 
-class AGSLegacyRouteFinder : public IRouteFinder 
-{
-public:
-    void init_pathfinder() override
-    { 
-        AGS::Engine::RouteFinderLegacy::init_pathfinder(); 
-    }
-    void shutdown_pathfinder() override
-    { 
-        AGS::Engine::RouteFinderLegacy::shutdown_pathfinder(); 
-    }
-    void set_walkablearea(Bitmap *walkablearea) override
-    { 
-        AGS::Engine::RouteFinderLegacy::set_walkablearea(walkablearea); 
-    }
-    int can_see_from(int x1, int y1, int x2, int y2) override
-    { 
-        return AGS::Engine::RouteFinderLegacy::can_see_from(x1, y1, x2, y2); 
-    }
-    void get_lastcpos(int &lastcx, int &lastcy) override
-    { 
-        AGS::Engine::RouteFinderLegacy::get_lastcpos(lastcx, lastcy); 
-    }
-    void set_route_move_speed(int speed_x, int speed_y) override
-    { 
-        AGS::Engine::RouteFinderLegacy::set_route_move_speed(speed_x, speed_y); 
-    }
-    int find_route(short srcx, short srcy, short xx, short yy, Bitmap *onscreen, int movlst, int nocross = 0, int ignore_walls = 0) override
-    { 
-        return AGS::Engine::RouteFinderLegacy::find_route(srcx, srcy, xx, yy, onscreen, movlst, nocross, ignore_walls); 
-    }
-    void calculate_move_stage(MoveList * mlsp, int aaa) override
-    { 
-        AGS::Engine::RouteFinderLegacy::calculate_move_stage(mlsp, aaa); 
-    }
-};
-
 std::unique_ptr<IRouteFinder> route_finder_impl;
 
 void init_pathfinder(GameDataVersion game_file_version)
 {
-    if (game_file_version >= kGameVersion_350) 
-    {
-        AGS::Common::Debug::Printf(AGS::Common::MessageType::kDbgMsg_Info, "Initialize path finder library");
-        route_finder_impl.reset(new AGSRouteFinder());
-    } 
-    else 
-    {
-        AGS::Common::Debug::Printf(AGS::Common::MessageType::kDbgMsg_Info, "Initialize legacy path finder library");
-        route_finder_impl.reset(new AGSLegacyRouteFinder());
-    }
-
+    AGS::Common::Debug::Printf(AGS::Common::MessageType::kDbgMsg_Info, "Initialize path finder library");
+    route_finder_impl.reset(new AGSRouteFinder());
     route_finder_impl->init_pathfinder();
 }
 

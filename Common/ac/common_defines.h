c8ef0ad6f58f68368acdae57bafa8f74e4082282
Ivan Mogilko <ikm_spb@yahoo.com>
2023-05-25 04:08:15 +0300

    Common: removed deprecated constants

--- Common/ac/common_defines.h
+++ Common/ac/common_defines.h
@@ -11,90 +11,21 @@
 // http://www.opensource.org/licenses/artistic-license-2.0.php
 //
 //=============================================================================
-
 #ifndef __AC_DEFINES_H
 #define __AC_DEFINES_H
 
 #include "core/platform.h"
 
+// Some arbitrary return values, should be replaced with either
+// simple boolean, or HError
 #define EXIT_NORMAL 0
 #define EXIT_CRASH  92
 #define EXIT_ERROR  93
 
-
-#if defined (OBSOLETE)
-#define NUM_MISC      20
-#define NUMOTCON      7                 // number of conditions before standing on
-#define NUM_CONDIT    (120 + NUMOTCON)
-#endif
-
-
-
-
+// Script name length limit for some game objects
 #define MAX_SCRIPT_NAME_LEN 20
-
-//const int MISC_COND = MAX_WALK_BEHINDS * 4 + NUMOTCON + MAX_ROOM_OBJECTS * 4;
-
-// NUMCONDIT : whataction[0]:  Char walks off left
-//                       [1]:  Char walks off right
-//                       [2]:  Char walks off bottom
-//                       [3]:  Char walks off top
-//			 [4]:  First enters screen
-//                       [5]:  Every time enters screen
-//                       [6]:  execute every loop
-//                [5]...[19]:  Char stands on lookat type
-//               [20]...[35]:  Look at type
-//               [36]...[49]:  Action on type
-//               [50]...[65]:  Use inv on type
-//               [66]...[75]:  Look at object
-//               [76]...[85]:  Action on object
-//               [86]...[95]:  Speak to object
-//		[96]...[105]:  Use inv on object
-//             [106]...[124]:  Misc conditions 1-20
-
-// game ver     whataction[]=
-// v1.00              0  :  Go to screen
-//                    1  :  Don't do anything
-//                    2  :  Can't walk
-//                    3  :  Man dies
-//                    4  :  Run animation
-//                    5  :  Display message
-//                    6  :  Remove an object (set object.on=0)
-//		                7  :  Remove object & add Val2 to inventory
-//                    8  :  Add Val1 to inventory (Val2=num times)
-//                    9  :  Run a script
-// v1.00 SR-1        10  :  Run graphical script
-// v1.1              11  :  Play sound effect SOUND%d.WAV
-// v1.12             12  :  Play FLI/FLC animation FLIC%d.FLC or FLIC%d.FLI
-//                   13  :  Turn object on
-// v2.00             14  :  Run conversation
-#define NUMRESPONSE   14
-#define NUMCOMMANDS   15
-#define GO_TO_SCREEN  0
-#define NO_ACTION     1
-#define NO_WALK       2
-#define MAN_DIES      3
-#define RUN_ANIMATE   4
-#define SHOW_MESSAGE  5
-#define OBJECT_OFF    6
-#define OBJECT_INV    7
-#define ADD_INV       8
-#define RUNSCRIPT     9
-#define GRAPHSCRIPT   10
-#define PLAY_SOUND    11
-#define PLAY_FLI      12
-#define OBJECT_ON     13
-#define RUN_DIALOG    14
-
 // Number of state-saved rooms
 #define MAX_ROOMS 300
-// Some obsolete room data, likely pre-2.5
-#define MAX_LEGACY_ROOM_FLAGS 15
-
-#define LEGACY_MAXOBJNAMELEN 30
-
-#define LEGACY_MAX_SPRITES_V25  6000
-#define LEGACY_MAX_SPRITES      30000
 
 #if AGS_PLATFORM_OS_WINDOWS
 #define AGS_INLINE inline
@@ -102,9 +33,6 @@
 // the linux compiler won't allow extern inline
 #define AGS_INLINE
 #endif
-
-// The game to screen coordinate conversion multiplier in hi-res type games
-#define HIRES_COORD_MULTIPLIER 2
 
 // object flags (currently only a char)
 #define OBJF_NOINTERACT        1  // not clickable

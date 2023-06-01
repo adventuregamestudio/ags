986ddd2f80a6a465e5e81097099b9223f06e2919
Ivan Mogilko <ikm_spb@yahoo.com>
2023-06-01 13:43:32 +0300

    Marked as a first public AGS 4 early alpha build (3.99.104)

--- Common/core/def_version.h
+++ Common/core/def_version.h
@@ -1,9 +1,9 @@
 #ifndef __AGS_CN_CORE__DEFVERSION_H
 #define __AGS_CN_CORE__DEFVERSION_H
 
-#define ACI_VERSION_STR      "3.99.103.0"
+#define ACI_VERSION_STR      "3.99.104.0"
 #if defined (RC_INVOKED) // for MSVC resource compiler
-#define ACI_VERSION_MSRC_DEF  3,99,103,0
+#define ACI_VERSION_MSRC_DEF  3,99,104,0
 #endif
 
 #define SPECIAL_VERSION ""
--- Editor/AGS.Types/Properties/AssemblyInfo.cs
+++ Editor/AGS.Types/Properties/AssemblyInfo.cs
@@ -22,9 +22,9 @@ namespace AGS.Types
     public class Version
     {
         public static readonly bool IS_BETA_VERSION = true;
-        public const string AGS_EDITOR_DATE = "May 2023";
-        public const string AGS_EDITOR_FRIENDLY_VERSION = "3.99.103";
-        public const string AGS_EDITOR_VERSION = "3.99.103.0";
+        public const string AGS_EDITOR_DATE = "June 2023";
+        public const string AGS_EDITOR_FRIENDLY_VERSION = "3.99.104";
+        public const string AGS_EDITOR_VERSION = "3.99.104.0";
         public const string AGS_EDITOR_COPYRIGHT = "Copyright © 2006-2011 Chris Jones and 2011-2023 others.";
     }
 }

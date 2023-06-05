8997730084cb76e5196af5c6307ced5a70eab4b1
Ivan Mogilko <ikm_spb@yahoo.com>
2023-06-05 21:51:42 +0300

    Updated build version (3.6.1.1)

--- Common/core/def_version.h
+++ Common/core/def_version.h
@@ -1,9 +1,9 @@
 #ifndef __AGS_CN_CORE__DEFVERSION_H
 #define __AGS_CN_CORE__DEFVERSION_H
 
-#define ACI_VERSION_STR      "3.6.1.0"
+#define ACI_VERSION_STR      "3.6.1.1"
 #if defined (RC_INVOKED) // for MSVC resource compiler
-#define ACI_VERSION_MSRC_DEF  3,6,1,0
+#define ACI_VERSION_MSRC_DEF  3,6,1,1
 #endif
 
 #define SPECIAL_VERSION ""

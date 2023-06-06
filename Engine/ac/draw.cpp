adventuregamestudio/ags

commit a9e3a802cc34f72e17b1cad502f9ae949f7a30d7

Engine: a bool cast fix in construct_object_gfx()

Engine/ac/draw.cpp
@@ -1510,7 +1510,7 @@ static bool construct_object_gfx(const ViewFrame *vf, int pic,
     else
     {
         // get the ambient or region tint
-        get_local_tint(objsrc.x, objsrc.y, (tint_flags & OBJF_USEREGIONTINTS),
+        get_local_tint(objsrc.x, objsrc.y, (tint_flags & OBJF_USEREGIONTINTS) == 0,
             &tint_level, &tint_red, &tint_green, &tint_blue,
             &tint_light, &light_level);
     }

commit 8291c23dff6cea60b7e4d2c054b6453efb36b920

Engine: fixed a call to get_local_tint()
Was broken by a9e3a802cc34f72e17b1cad502f9ae949f7a30d7

Engine/ac/draw.cpp
@@ -1510,7 +1510,7 @@ static bool construct_object_gfx(const ViewFrame *vf, int pic,
     else
     {
         // get the ambient or region tint
-        get_local_tint(objsrc.x, objsrc.y, (tint_flags & OBJF_USEREGIONTINTS) == 0,
+        get_local_tint(objsrc.x, objsrc.y, (tint_flags & OBJF_USEREGIONTINTS) != 0,
             &tint_level, &tint_red, &tint_green, &tint_blue,
             &tint_light, &light_level);
     }
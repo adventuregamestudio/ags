adventuregamestudio/ags

commit eb376e63f95b09fd75ca7f96591aca20d0564072

Script API: changed Object.SetView default loop & frame values to 0
The original Object.SetView function had a somewhat unexpected behavior: if no explicit values were provided for loop and frame then it kept the last loop and frame object had from the previously set view.
This change changes default values to 0, which resets loop and frame if they are not provided.

Engine/ac/global_object.cpp
@@ -162,10 +162,18 @@ void SetObjectView(int obn,int vii) {
 void SetObjectFrame(int obn,int viw,int lop,int fra) {
     if (!is_valid_object(obn)) quit("!SetObjectFrame: invalid object number specified");
     viw--;
-    if (viw>=game.numviews) quit("!SetObjectFrame: invalid view number used");
-    if (views[viw].numLoops == 0) quit("!SetObjectFrame: specified view has no loops");
-    if (lop>=views[viw].numLoops) quit("!SetObjectFrame: invalid loop number used");
-
+    if (viw < 0 || viw >= game.numviews) quitprintf("!SetObjectFrame: invalid view number used (%d, range is 0 - %d)", viw, game.numviews - 1);
+    if (lop < 0 || lop >= views[viw].numLoops) quitprintf("!SetObjectFrame: invalid loop number used (%d, range is 0 - %d)", lop, views[viw].numLoops - 1);
+    // AGS < 3.6.0 let user to pass literally any positive invalid frame value by silently reassigning it to zero...
+    if (game.options[OPT_BASESCRIPTAPI] < kScriptAPI_v360)
+    {
+        if (fra >= views[viw].loops[lop].numFrames)
+        {
+            debug_script_warn("SetObjectFrame: frame index out of range (%d, must be 0 - %d), set to 0", fra, views[viw].loops[lop].numFrames - 1);
+            fra = 0;
+        }
+    }
+    if (fra < 0 || fra >= views[viw].loops[lop].numFrames) quitprintf("!SetObjectFrame: invalid frame number used (%d, range is 0 - %d)", fra, views[viw].loops[lop].numFrames - 1);
     if (viw > UINT16_MAX || lop > UINT16_MAX || fra > UINT16_MAX)
     {
         debug_script_warn("Warning: object's (id %d) view/loop/frame (%d/%d/%d) is outside of internal range (%d/%d/%d), reset to no view",
@@ -179,21 +187,18 @@ void SetObjectFrame(int obn,int viw,int lop,int fra) {
         objs[obn].loop = (uint16_t)lop;
     if (fra >= 0)
         objs[obn].frame = (uint16_t)fra;
-    if (objs[obn].loop >= views[viw].numLoops)
-        objs[obn].loop = 0;
-    if (objs[obn].frame >= views[viw].loops[objs[obn].loop].numFrames)
-        objs[obn].frame = 0;
-
     // AGS >= 3.2.0 do not let assign an empty loop
     // NOTE: pre-3.2.0 games are converting views from ViewStruct272 struct, always has at least 1 frame
     if (loaded_game_file_version >= kGameVersion_320)
     {
-        if (views[viw].loops[objs[obn].loop].numFrames == 0) 
+        if (views[viw].loops[lop].numFrames == 0)
             quit("!SetObjectFrame: specified loop has no frames");
     }
-
+    objs[obn].view = viw;
+    objs[obn].loop = lop;
+    objs[obn].frame = fra;
     objs[obn].cycling=0;
-    int pic = views[viw].loops[objs[obn].loop].frames[objs[obn].frame].pic;
+    int pic = views[viw].loops[lop].frames[fra].pic;
     objs[obn].num = Math::InRangeOrDef<uint16_t>(pic, 0);
     if (pic > UINT16_MAX)
         debug_script_warn("Warning: object's (id %d) sprite %d is outside of internal range (%d), reset to 0", obn, pic, UINT16_MAX);

Engine/ac/object.cpp
@@ -26,6 +26,7 @@
 #include "ac/runtime_defines.h"
 #include "ac/string.h"
 #include "ac/system.h"
+#include "ac/view.h"
 #include "ac/walkablearea.h"
 #include "debug/debug_log.h"
 #include "main/game_run.h"
@@ -43,6 +44,7 @@ using namespace AGS::Common;
 extern ScriptObject scrObj[MAX_ROOM_OBJECTS];
 extern RoomStatus*croom;
 extern RoomObject*objs;
+extern ViewStruct*views;
 extern RoomStruct thisroom;
 extern ObjectCache objcache[MAX_ROOM_OBJECTS];
 extern MoveList *mls;
@@ -85,6 +87,16 @@ void Object_RemoveTint(ScriptObject *objj) {
 }
 
 void Object_SetView(ScriptObject *objj, int view, int loop, int frame) {
+    if (game.options[OPT_BASESCRIPTAPI] < kScriptAPI_v360)
+    { // Previous version of SetView had negative loop and frame mean "use latest values"
+        auto &obj = objs[objj->id];
+        if (loop < 0) loop = obj.loop;
+        if (frame < 0) frame = obj.frame;
+        const int vidx = view - 1;
+        if (vidx < 0 || vidx >= game.numviews) quit("!Object_SetView: invalid view number used");
+        loop = Math::Clamp(loop, 0, (int)views[vidx].numLoops - 1);
+        frame = Math::Clamp(frame, 0, (int)views[vidx].loops[loop].numFrames - 1);
+    }
     SetObjectFrame(objj->id, view, loop, frame);
 }
 

Editor/AGS.Editor/Resources/agsdefns.sh
@@ -2180,8 +2180,14 @@ builtin managed struct Object {
   import function RunInteraction(CursorMode);
   /// Instantly moves the object to have its bottom-left at the new co-ordinates.
   import function SetPosition(int x, int y);
+#ifdef SCRIPT_API_v360
+  /// Sets the object to use the specified view, ahead of doing an animation.
+  import function SetView(int view, int loop=0, int frame=0);
+#endif
+#ifndef SCRIPT_API_v360
   /// Sets the object to use the specified view, ahead of doing an animation.
   import function SetView(int view, int loop=-1, int frame=-1);
+#endif
   /// Stops any currently running animation on the object.
   import function StopAnimating();
   /// Stops any currently running move on the object.

commit 66bb18f0224eebfb2a130395e9b18ce59b584e13

Engine: fixed the bw-compat SetObjectFrame() behavior
This was broken by eb376e63f95b09fd75ca7f96591aca20d0564072

1. Treating negative loop and frame as an instruction to keep latest object's loop and frame values was introduced around AGS 2.70 by Object.SetView(), but this behavior also propagated to SetObjectFrame() command.
2. Removed clamping of loop and frame values inside Object.SetView(), as that prevented applying a historical behavior of SetObjectFrame(), where any invalid loop and frame value would be changed to just 0.
3. Don't quit the game in case of invalid loop and frame, but fallback to default 0 value, and print a warning.

Also removed some redundant assignments to RoomObject, possibly a result of a commit merge from another branch.

Engine/ac/global_object.cpp
@@ -161,17 +161,32 @@ void SetObjectFrame(int obn,int viw,int lop,int fra) {
     viw--;
     if (viw < 0 || viw >= game.numviews) quitprintf("!SetObjectFrame: invalid view number used (%d, range is 0 - %d)", viw, game.numviews - 1);
     if (views[viw].numLoops == 0) quitprintf("!SetObjectFrame: view %d has no loops", viw);
-    if (lop < 0 || lop >= views[viw].numLoops) quitprintf("!SetObjectFrame: invalid loop number used for view %d (%d, range is 0 - %d)",
-        viw, lop, views[viw].numLoops - 1);
-    // historically AGS let user to pass literally any positive invalid frame value by silently reassigning it to zero...
+
+    auto &obj = objs[obn];
+
+    // Previous version of Object.SetView had negative loop and frame mean "use latest values",
+    // which also caused SetObjectFrame to act similarly, starting with 2.70.
+    if ((game.options[OPT_BASESCRIPTAPI] < kScriptAPI_v360) &&
+        (loaded_game_file_version >= kGameVersion_270))
+    {
+        if (lop < 0) lop = obj.loop;
+        if (fra < 0) fra = obj.frame;
+    }
+
+    // Fixup invalid loop & frame numbers by using default 0 value
+    if (lop < 0 || lop >= views[viw].numLoops)
+    {
+        debug_script_warn("SetObjectFrame: invalid loop number used for view %d (%d, range is 0 - %d)",
+            viw, lop, views[viw].numLoops - 1);
+        lop = 0;
+    }
     if (fra < 0 || fra >= views[viw].loops[lop].numFrames)
     {
-        if (views[viw].loops[lop].numFrames == 0)
-            debug_script_warn("SetObjectFrame: specified loop %d has no frames, will fallback to dummy frame", lop);
-        else
-            debug_script_warn("SetObjectFrame: frame index out of range (%d, must be 0 - %d), set to 0", fra, views[viw].loops[lop].numFrames - 1);
+        debug_script_warn("SetObjectFrame: frame index out of range (%d, must be 0 - %d)", fra, views[viw].loops[lop].numFrames - 1);
         fra = 0; // NOTE: we have 1 dummy frame allocated for empty loops
     }
+
+    // Current engine's object data limitation by uint16_t
     if (viw > UINT16_MAX || lop > UINT16_MAX || fra > UINT16_MAX)
     {
         debug_script_warn("Warning: object's (id %d) view/loop/frame (%d/%d/%d) is outside of internal range (%d/%d/%d), reset to no view",
@@ -179,21 +194,16 @@ void SetObjectFrame(int obn,int viw,int lop,int fra) {
         SetObjectGraphic(obn, 0);
         return;
     }
-    
-    objs[obn].view = (uint16_t)viw;
-    if (lop >= 0)
-        objs[obn].loop = (uint16_t)lop;
-    if (fra >= 0)
-        objs[obn].frame = (uint16_t)fra;
-    objs[obn].view = viw;
-    objs[obn].loop = lop;
-    objs[obn].frame = fra;
-    objs[obn].cycling=0;
+
+    obj.view = static_cast<uint16_t>(viw);
+    obj.loop = static_cast<uint16_t>(lop);
+    obj.frame = static_cast<uint16_t>(fra);
+    obj.cycling = 0; // reset anim
     int pic = views[viw].loops[lop].frames[fra].pic;
-    objs[obn].num = Math::InRangeOrDef<uint16_t>(pic, 0);
+    obj.num = Math::InRangeOrDef<uint16_t>(pic, 0);
     if (pic > UINT16_MAX)
         debug_script_warn("Warning: object's (id %d) sprite %d is outside of internal range (%d), reset to 0", obn, pic, UINT16_MAX);
-    CheckViewFrame(viw, objs[obn].loop, objs[obn].frame);
+    CheckViewFrame(viw, obj.loop, obj.frame);
 }
 
 // pass trans=0 for fully solid, trans=100 for fully transparent

Engine/ac/object.cpp
@@ -86,16 +86,6 @@ void Object_RemoveTint(ScriptObject *objj) {
 }
 
 void Object_SetView(ScriptObject *objj, int view, int loop, int frame) {
-    if (game.options[OPT_BASESCRIPTAPI] < kScriptAPI_v360)
-    { // Previous version of SetView had negative loop and frame mean "use latest values"
-        auto &obj = objs[objj->id];
-        if (loop < 0) loop = obj.loop;
-        if (frame < 0) frame = obj.frame;
-        const int vidx = view - 1;
-        if (vidx < 0 || vidx >= game.numviews) quit("!Object_SetView: invalid view number used");
-        loop = Math::Clamp(loop, 0, (int)views[vidx].numLoops - 1);
-        frame = Math::Clamp(frame, 0, (int)views[vidx].loops[loop].numFrames - 1);
-    }
     SetObjectFrame(objj->id, view, loop, frame);
 }
 
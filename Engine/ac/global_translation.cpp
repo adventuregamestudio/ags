adventuregamestudio/ags

commit df0151983e077e577eee8691a61d03be44c24cbd

Engine: picked out plugin's save / restore data event runs
This is not precisely a part of a particular save format, but rather a command to plugin callback system.
In theory it might be also possible to implement step by step reading and writing with some engine's action in between.

Engine/ac/global_translation.cpp
@@ -19,7 +19,6 @@
 #include "ac/string.h"
 #include "ac/translation.h"
 #include "platform/base/agsplatformdriver.h"
-#include "plugin/agsplugin.h"
 #include "plugin/plugin_engine.h"
 #include "util/memory.h"
 #include "core/types.h"

Engine/game/savegame.cpp
@@ -788,5 +788,21 @@ void SaveGameState(Stream *out)
     SavegameComponents::WriteAllCommon(out);
 }
 
+void ReadPluginSaveData(Stream *in)
+{
+    auto pluginFileHandle = AGSE_RESTOREGAME;
+    pl_set_file_handle(pluginFileHandle, in);
+    pl_run_plugin_hooks(AGSE_RESTOREGAME, pluginFileHandle);
+    pl_clear_file_handle();
+}
+
+void WritePluginSaveData(Stream *out)
+{
+    auto pluginFileHandle = AGSE_SAVEGAME;
+    pl_set_file_handle(pluginFileHandle, out);
+    pl_run_plugin_hooks(AGSE_SAVEGAME, pluginFileHandle);
+    pl_clear_file_handle();
+}
+
 } // namespace Engine
 } // namespace AGS

Engine/game/savegame_components.cpp
@@ -45,7 +45,6 @@
 #include "gui/guimain.h"
 #include "gui/guislider.h"
 #include "gui/guitextbox.h"
-#include "plugin/agsplugin.h"
 #include "plugin/plugin_engine.h"
 #include "script/cc_common.h"
 #include "script/script.h"
@@ -1126,19 +1125,13 @@ HSaveError ReadManagedPool(Stream *in, int32_t /*cmp_ver*/, const PreservedParam
 
 HSaveError WritePluginData(Stream *out)
 {
-    auto pluginFileHandle = AGSE_SAVEGAME;
-    pl_set_file_handle(pluginFileHandle, out);
-    pl_run_plugin_hooks(AGSE_SAVEGAME, pluginFileHandle);
-    pl_clear_file_handle();
+    WritePluginSaveData(out);
     return HSaveError::None();
 }
 
 HSaveError ReadPluginData(Stream *in, int32_t /*cmp_ver*/, const PreservedParams& /*pp*/, RestoredData& /*r_data*/)
 {
-    auto pluginFileHandle = AGSE_RESTOREGAME;
-    pl_set_file_handle(pluginFileHandle, in);
-    pl_run_plugin_hooks(AGSE_RESTOREGAME, pluginFileHandle);
-    pl_clear_file_handle();
+    ReadPluginSaveData(in);
     return HSaveError::None();
 }
 

Engine/game/savegame_internal.h
@@ -139,6 +139,14 @@ struct RestoredData
     RestoredData();
 };
 
+
+// Runs plugin events, requesting to read save data from the given stream.
+// NOTE: there's no error check in this function, because plugin API currently
+// does not let plugins report any errors when restoring their saved data.
+void ReadPluginSaveData(Stream *in);
+// Runs plugin events, requesting to write save data into the given stream.
+void WritePluginSaveData(Stream *out);
+
 } // namespace Engine
 } // namespace AGS
 

Engine/game/savegame_v321.cpp
@@ -41,7 +41,6 @@
 #include "gui/animatingguibutton.h"
 #include "gui/guimain.h"
 #include "media/audio/audio.h"
-#include "plugin/agsplugin.h"
 #include "plugin/plugin_engine.h"
 #include "script/script.h"
 #include "script/cc_common.h"
@@ -526,10 +525,7 @@ HSaveError restore_save_data_v321(Stream *in, const PreservedParams &pp, Restore
     if (!err)
         return err;
 
-    auto pluginFileHandle = AGSE_RESTOREGAME;
-    pl_set_file_handle(pluginFileHandle, in);
-    pl_run_plugin_hooks(AGSE_RESTOREGAME, pluginFileHandle);
-    pl_clear_file_handle();
+    ReadPluginSaveData(in);
     if (static_cast<uint32_t>(in->ReadInt32()) != MAGICNUMBER)
         return new SavegameError(kSvgErr_InconsistentPlugin);
 

commit c0a2bdea90c0c11b09a3d3e9cc96eb2c7fa76feb

Engine: fixed 64-bit build broken by missing header
Broken by df0151983e077e577eee8691a61d03be44c24cbd

Engine/ac/global_translation.cpp
@@ -18,10 +18,11 @@
 #include "ac/global_translation.h"
 #include "ac/string.h"
 #include "ac/translation.h"
+#include "core/types.h"
 #include "platform/base/agsplatformdriver.h"
+#include "plugin/agsplugin.h"
 #include "plugin/plugin_engine.h"
 #include "util/memory.h"
-#include "core/types.h"
 
 using namespace AGS::Common::Memory;
 
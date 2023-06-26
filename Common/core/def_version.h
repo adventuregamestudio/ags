549b5644954750d1d34eacc4a05e4f2cfaef74cb
Ivan Mogilko <ikm_spb@yahoo.com>
2023-06-26 16:50:47 +0300

    Updated build version (3.6.1.2)

--- Common/core/def_version.h
+++ Common/core/def_version.h
@@ -1,9 +1,9 @@
 #ifndef __AGS_CN_CORE__DEFVERSION_H
 #define __AGS_CN_CORE__DEFVERSION_H
 
-#define ACI_VERSION_STR      "3.6.1.1"
+#define ACI_VERSION_STR      "3.6.1.2"
 #if defined (RC_INVOKED) // for MSVC resource compiler
-#define ACI_VERSION_MSRC_DEF  3,6,1,1
+#define ACI_VERSION_MSRC_DEF  3,6,1,2
 #endif
 
 #define SPECIAL_VERSION ""
--- Editor/AGS.Types/Properties/AssemblyInfo.cs
+++ Editor/AGS.Types/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ namespace AGS.Types
         public static readonly bool IS_BETA_VERSION = true;
         public const string AGS_EDITOR_DATE = "June 2023";
         public const string AGS_EDITOR_FRIENDLY_VERSION = "3.6.1";
-        public const string AGS_EDITOR_VERSION = "3.6.1.1";
+        public const string AGS_EDITOR_VERSION = "3.6.1.2";
         public const string AGS_EDITOR_COPYRIGHT = "Copyright © 2006-2011 Chris Jones and 2011-2023 others.";
     }
 }

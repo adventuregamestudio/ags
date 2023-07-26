adventuregamestudio/ags

commit 310c6f499fa797751ff0dc4d3474f6c0f5b34d37

Editor: delete files using Utilities.TryDeleteFile()
Use uniform way to delete files and handle most common exceptions.
... except for a few specific cases.

Editor/AGS.Editor/AGSEditor.cs
@@ -346,7 +346,7 @@ namespace AGS.Editor
                         "The configuration file will be deleted and user preferences reset to allow Editor start." +
                         "\n\n\nConfiguration file's location: " + filename,
                         MessageBoxIcon.Error);
-                File.Delete(filename);
+                Utilities.TryDeleteFile(filename);
             }
 
             _applicationSettings = new AppSettings();
@@ -571,33 +571,9 @@ namespace AGS.Editor
 
 			foreach (string fileName in fullPathNames)
 			{
-                AttemptToDeleteFileFromDisk(fileName);
+                Utilities.TryDeleteFile(fileName);
 			}
 		}
-
-        public void AttemptToDeleteFileFromDisk(string fileName)
-        {
-            try
-            {
-                try
-                {
-                    File.Delete(fileName);
-                }
-                catch (UnauthorizedAccessException)
-                {
-                    File.SetAttributes(fileName, FileAttributes.Normal);
-                    File.Delete(fileName);
-                }
-            }
-            catch (IOException ex)
-            {
-                throw new CannotDeleteFileException("Unable to delete the file '" + fileName + "'." + Environment.NewLine + ex.Message, ex);
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-                throw new CannotDeleteFileException("Unable to delete the file '" + fileName + "'." + Environment.NewLine + ex.Message, ex);
-            }
-        }
 
         /// <summary>
         /// Attempt to get write access to the specified file. If this fails,
@@ -668,7 +644,7 @@ namespace AGS.Editor
                 {
                     StreamWriter sw = new StreamWriter(fileName);
                     sw.Close();
-					Utilities.DeleteFileIfExists(fileName);
+					Utilities.TryDeleteFile(fileName);
                 }
                 catch (Exception ex)
                 {
@@ -1053,7 +1029,7 @@ namespace AGS.Editor
             string dir = Path.Combine(OUTPUT_DIRECTORY, DATA_OUTPUT_DIRECTORY);
             foreach (string fileName in Utilities.GetDirectoryFileList(dir, this.BaseGameFileName + ".0*"))
             {
-                File.Delete(fileName);
+                Utilities.TryDeleteFile(fileName);
             }
         }
 
@@ -1077,10 +1053,9 @@ namespace AGS.Editor
                 }
             }
 
-            if (File.Exists(audioVox) && 
-                (fileListForVox.Count == 0) || (rebuildVox))
+            if ((fileListForVox.Count == 0) || (rebuildVox))
             {
-                File.Delete(audioVox);
+                Utilities.TryDeleteFile(audioVox);
             }
 
             if ((rebuildVox) && (fileListForVox.Count > 0))
@@ -1738,11 +1713,8 @@ namespace AGS.Editor
 
             try
             {
-                if (File.Exists(gameFile))
-                {
-                    File.Delete(backupFile);
-                    File.Move(gameFile, backupFile);
-                }
+                Utilities.TryDeleteFile(backupFile);
+                File.Move(gameFile, backupFile);
             }
             catch (Exception ex)
             {
@@ -1771,17 +1743,7 @@ namespace AGS.Editor
 
             foreach (string fileName in filesToDelete)
             {
-                if (File.Exists(fileName))
-                {
-                    try
-                    {
-                        File.Delete(fileName);
-                    }
-                    catch (Exception ex)
-                    {
-                        Factory.GUIController.ShowMessage("Unable to remove file '" + fileName + "' because of an error: " + ex.Message, MessageBoxIcon.Warning);
-                    }
-                }
+                Utilities.TryDeleteFile(fileName);
             }
         }
 

Editor/AGS.Editor/BuildTargets/BuildTargetAndroid.cs
@@ -131,7 +131,7 @@ namespace AGS.Editor
                     {
                         string toRemoveRoundPath = Path.Combine(destIconDir, icondir, ICON_ROUND_FILENAME);
                         string icPathPartial = Path.Combine(icondir, ICON_FILENAME);
-                        if (File.Exists(toRemoveRoundPath)) File.Delete(toRemoveRoundPath);
+                        Utilities.TryDeleteFile(toRemoveRoundPath);
                         File.Copy(Path.Combine(projIconDir, icPathPartial), Path.Combine(destIconDir, icPathPartial), overwrite: true);
                     }
                     break;
@@ -165,10 +165,7 @@ namespace AGS.Editor
 
         private void WriteStringToFile(string fileName, string fileText)
         {
-            if (File.Exists(fileName))
-            {
-                File.Delete(fileName);
-            }
+            Utilities.TryDeleteFile(fileName);
 
             FileStream stream = File.Create(fileName);
             byte[] bytes = Encoding.UTF8.GetBytes(fileText);
@@ -298,16 +295,13 @@ namespace AGS.Editor
         {
             string fileName = Path.Combine(dest_dir, "project.properties");
 
-            if(File.Exists(fileName))
+            try
             {
-                try
-                {
-                    File.Delete(fileName);
-                }
-                catch
-                {
-                    StopGradle();
-                }
+                Utilities.TryDeleteFile(fileName);
+            }
+            catch
+            {
+                StopGradle();
             }
 
             Settings gameSettings = Factory.AGSEditor.CurrentGame.Settings;
@@ -329,16 +323,13 @@ namespace AGS.Editor
         {
             string fileName = Path.Combine(dest_dir, "local.static.properties");
 
-            if (File.Exists(fileName))
+            try
             {
-                try
-                {
-                    File.Delete(fileName);
-                }
-                catch
-                {
-                    StopGradle();
-                }
+                Utilities.TryDeleteFile(fileName);
+            }
+            catch
+            {
+                StopGradle();
             }
 
             // this should NOT GET KEYS FROM SETTINGS
@@ -498,7 +489,7 @@ namespace AGS.Editor
         {
             string assetsDir = GetAssetsDir();
             string filename = Path.Combine(assetsDir, name + ".ags");
-            Utilities.DeleteFileIfExists(filename);
+            Utilities.TryDeleteFile(filename);
         }
 
         public override bool Build(CompileMessages errors, bool forceRebuild)

Editor/AGS.Editor/BuildTargets/BuildTargetDataFile.cs
@@ -21,14 +21,14 @@ namespace AGS.Editor
         public override void DeleteMainGameData(string name)
         {
             string filename = Path.Combine(OutputDirectoryFullPath, name + ".ags");
-            Utilities.DeleteFileIfExists(filename);
+            Utilities.TryDeleteFile(filename);
         }
 
         private void DeleteAnyExistingSplitResourceFiles()
         {
             foreach (string fileName in Utilities.GetDirectoryFileList(GetCompiledPath(), Factory.AGSEditor.BaseGameFileName + ".0*"))
             {
-                File.Delete(fileName);
+                Utilities.TryDeleteFile(fileName);
             }
         }
 
@@ -152,10 +152,9 @@ namespace AGS.Editor
                 }
             }
 
-            if (File.Exists(audioVox) &&
-                (fileListForVox.Count == 0) || (rebuildVox))
+            if ((fileListForVox.Count == 0) || (rebuildVox))
             {
-                File.Delete(audioVox);
+                Utilities.TryDeleteFile(audioVox);
             }
 
             if ((rebuildVox) && (fileListForVox.Count > 0))
@@ -180,7 +179,7 @@ namespace AGS.Editor
             {
                 errors.Add(new CompileError(errorMsg));
             }
-            File.Delete(AGSEditor.COMPILED_DTA_FILE_NAME);
+            Utilities.TryDeleteFile(AGSEditor.COMPILED_DTA_FILE_NAME);
             CreateAudioVOXFile(forceRebuild);
             // Update config file with current game parameters
             Factory.AGSEditor.WriteConfigFile(GetCompiledPath());

Editor/AGS.Editor/BuildTargets/BuildTargetDebug.cs
@@ -37,7 +37,7 @@ namespace AGS.Editor
         public override void DeleteMainGameData(string name)
         {
             string filename = Path.Combine(Path.Combine(OutputDirectoryFullPath, DEBUG_DIRECTORY), name + ".exe");
-            Utilities.DeleteFileIfExists(filename);
+            Utilities.TryDeleteFile(filename);
         }
 
         private object CreateDebugFiles(object parameter)
@@ -50,7 +50,7 @@ namespace AGS.Editor
             }
             Factory.NativeProxy.CreateDebugMiniEXE(new string[] { AGSEditor.COMPILED_DTA_FILE_NAME },
                 Factory.AGSEditor.BaseGameFileName + ".exe");
-            File.Delete(AGSEditor.COMPILED_DTA_FILE_NAME);
+            Utilities.TryDeleteFile(AGSEditor.COMPILED_DTA_FILE_NAME);
             return null;
         }
 
@@ -70,15 +70,15 @@ namespace AGS.Editor
                 string compiledEXE = targetWin.GetCompiledPath(exeFileName);
                 string compiledDat = targetWin.GetCompiledPath(baseGameFileName + ".ags");
                 string sourceEXE = Path.Combine(Factory.AGSEditor.EditorDirectory, AGSEditor.ENGINE_EXE_FILE_NAME);
-                Utilities.DeleteFileIfExists(compiledEXE);
-                Utilities.DeleteFileIfExists(compiledDat);
+                Utilities.TryDeleteFile(compiledEXE);
+                Utilities.TryDeleteFile(compiledDat);
                 File.Copy(sourceEXE, exeFileName, true);
                 BusyDialog.Show("Please wait while we prepare to run the game...", new BusyDialog.ProcessingHandler(CreateDebugFiles), errors);
                 if (errors.HasErrors)
                 {
                     return false;
                 }
-                Utilities.DeleteFileIfExists(GetDebugPath(exeFileName));
+                Utilities.TryDeleteFile(GetDebugPath(exeFileName));
                 File.Move(exeFileName, GetDebugPath(exeFileName));
                 // copy configuration from Compiled folder to use with Debugging
                 string cfgFilePath = targetWin.GetCompiledPath(AGSEditor.CONFIG_FILE_NAME);

Editor/AGS.Editor/BuildTargets/BuildTargetLinux.cs
@@ -63,7 +63,7 @@ namespace AGS.Editor
         public override void DeleteMainGameData(string name)
         {
             string filename = Path.Combine(Path.Combine(OutputDirectoryFullPath, LINUX_DATA_DIR), name + ".ags");
-            Utilities.DeleteFileIfExists(filename);
+            Utilities.TryDeleteFile(filename);
         }
 
         private bool CheckPluginsHaveSharedLibraries()

Editor/AGS.Editor/BuildTargets/BuildTargetWeb.cs
@@ -32,7 +32,7 @@ namespace AGS.Editor
         public override void DeleteMainGameData(string name)
         {
             string filename = Path.Combine(Path.Combine(OutputDirectoryFullPath, WEB_DIR), name + ".ags");
-            Utilities.DeleteFileIfExists(filename);
+            Utilities.TryDeleteFile(filename);
         }
 
         public override bool Build(CompileMessages errors, bool forceRebuild)

Editor/AGS.Editor/BuildTargets/BuildTargetWindows.cs
@@ -27,7 +27,7 @@ namespace AGS.Editor
         public override void DeleteMainGameData(string name)
         {
             string filename = Path.Combine(OutputDirectoryFullPath, name + ".exe");
-            Utilities.DeleteFileIfExists(filename);
+            Utilities.TryDeleteFile(filename);
         }
 
         public void CopyPlugins(CompileMessages errors)
@@ -213,7 +213,7 @@ namespace AGS.Editor
                 if (Factory.AGSEditor.CurrentGame.Settings.AttachDataToExe)
                 {
                     AttachDataToEXE(mainGameDataSrc, newExeName);
-                    File.Delete(mainGameDataDst);
+                    Utilities.TryDeleteFile(mainGameDataDst);
                 }
                 else
                 {

Editor/AGS.Editor/Components/AudioComponent.cs
@@ -329,7 +329,7 @@ namespace AGS.Editor.Components
                 newClip.FileType = _fileTypeMappings[fileExtension];
                 newClip.FileLastModifiedDate = File.GetLastWriteTimeUtc(sourceFileName);
                 Utilities.CopyFileAndSetDestinationWritable(sourceFileName, newClip.CacheFileName);
-                Utilities.DeleteFileIfExists(Path.Combine(AGSEditor.OUTPUT_DIRECTORY,
+                Utilities.TryDeleteFile(Path.Combine(AGSEditor.OUTPUT_DIRECTORY,
                     Path.Combine(AGSEditor.DATA_OUTPUT_DIRECTORY, AGSEditor.AUDIO_VOX_FILE_NAME)));
                 _agsEditor.CurrentGame.FilesAddedOrRemoved = true;
                 return newClip;
@@ -780,7 +780,7 @@ namespace AGS.Editor.Components
             }
             _agsEditor.DeleteFileOnDiskAndSourceControl(filesToDelete.ToArray());
             _agsEditor.CurrentGame.FilesAddedOrRemoved = true;
-            Utilities.DeleteFileIfExists(Path.Combine(AGSEditor.OUTPUT_DIRECTORY,
+            Utilities.TryDeleteFile(Path.Combine(AGSEditor.OUTPUT_DIRECTORY,
                 Path.Combine(AGSEditor.DATA_OUTPUT_DIRECTORY, AGSEditor.AUDIO_VOX_FILE_NAME)));
         }
     }

commit 2410787be9c676834e1509b30ef98f824bd3cf73

Editor: fixed Game.agf is not saved after importing an old game
This is broken by 310c6f499fa797751ff0dc4d3474f6c0f5b34d37, where File.Exists condition was removed incorrectly.

Editor/AGS.Editor/AGSEditor.cs
@@ -1735,8 +1735,8 @@ namespace AGS.Editor
 
         private bool WriteMainGameFile(string fileContents)
         {
+            // First save the game into the temp file
             string tempFile = Path.GetTempFileName();
-
             using (StreamWriter fileOutput = new StreamWriter(tempFile, false, _game.TextEncoding))
             {
                 try
@@ -1751,13 +1751,16 @@ namespace AGS.Editor
                 }
             }
 
+            // Create a backup file, by moving previous game file into backup
             string gameFile = Utilities.ResolveSourcePath(GAME_FILE_NAME);
             string backupFile = $"{gameFile}.{BACKUP_EXTENSION}";
-
             try
             {
-                Utilities.TryDeleteFile(backupFile);
-                File.Move(gameFile, backupFile);
+                if (File.Exists(gameFile))
+                {
+                    Utilities.TryDeleteFile(backupFile);
+                    File.Move(gameFile, backupFile);
+                }
             }
             catch (Exception ex)
             {
@@ -1765,6 +1768,7 @@ namespace AGS.Editor
                 return false;
             }
 
+            // Finally try moving the saved temp game file into the project folder
             try
             {
                 File.Move(tempFile, gameFile);
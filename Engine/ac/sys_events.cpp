adventuregamestudio/ags

commit 3a38e69212378a75064265eb14fb158067f9a069

Engine: support multiple mouse clicks per game frame
Implement mouse event buffer similar to key buffer, and process it all in check_controls().
Process Mouse.Click() simulation by sending appropriate events.

Engine/ac/sys_events.cpp
@@ -41,7 +41,7 @@ eAGSKeyCode sdl_key_to_ags_key(const SDL_KeyboardEvent &kbevt, bool old_keyhandl
 int sdl_mod_to_ags_mod(const SDL_KeyboardEvent &kbevt);
 
 // Converts SDL scan and key codes to the ags keycode
-KeyInput ags_keycode_from_sdl(const SDL_Event &event, bool old_keyhandle)
+KeyInput sdl_keyevt_to_ags_key(const SDL_Event &event, bool old_keyhandle)
 {
     KeyInput ki;
     // Normally SDL_TEXTINPUT is meant for handling the printable characters,
@@ -249,10 +249,6 @@ bool ags_key_to_sdl_scan(eAGSKeyCode key, SDL_Scancode(&scan)[3])
 
 
 
-
-
-
-
 // ----------------------------------------------------------------------------
 // KEYBOARD INPUT
 // ----------------------------------------------------------------------------
@@ -346,9 +342,21 @@ enum eAGSMouseButtonMask
     MouseBitX2       = 0x10
 };
 
+eAGSMouseButton sdl_mbut_to_ags_but(int sdl_mbut)
+{
+    switch (sdl_mbut)
+    {
+    case SDL_BUTTON_LEFT: return kMouseLeft;
+    case SDL_BUTTON_RIGHT: return kMouseRight;
+    case SDL_BUTTON_MIDDLE: return kMouseMiddle;
+    default: return kMouseNone;
+    }
+}
+
 static int sdl_button_to_mask(int button)
 {
-    switch (button) {
+    switch (button)
+    {
     case SDL_BUTTON_LEFT: return MouseBitLeft;
     case SDL_BUTTON_RIGHT: return MouseBitRight;
     case SDL_BUTTON_MIDDLE: return MouseBitMiddle;
@@ -358,15 +366,26 @@ static int sdl_button_to_mask(int button)
     return 0;
 }
 
-/* [sonneveld]
-Button tracking:
-On OSX, some tap to click up/down events happen too quickly to be detected on the polled mouse_b global variable.
-Instead we accumulate button presses over a couple of timer loops.
-// TODO: check again when/if we replace polling with different event handling.
-*/
+static int ags_button_to_sdl(eAGSMouseButton but)
+{
+    switch (but)
+    {
+    case kMouseLeft: return SDL_BUTTON_LEFT;
+    case kMouseRight: return SDL_BUTTON_RIGHT;
+    case kMouseMiddle: return SDL_BUTTON_MIDDLE;
+    default: return 0;
+    }
+}
+
+// Convert mouse button id to flags
+const int MouseButton2Bits[kNumMouseButtons] =
+    { 0, MouseBitLeft, MouseBitRight, MouseBitMiddle };
+
+
+// Because our game engine still uses input polling, we have to accumulate
+// mouse button events for our internal use whenever engine have to query button input.
+static std::deque<SDL_Event> g_mouseEvtQueue;
 static int mouse_button_state = 0;
-static int mouse_accum_button_state = 0;
-static auto mouse_clear_at_time = AGS_Clock::now();
 // Accumulated absolute and relative mouse device motion.
 // May be retrieved by calling *acquire_absxy and *acquire_relxy functions,
 // after which these are reset, until next motion event is received.
@@ -377,17 +396,41 @@ volatile int sys_mouse_z = 0; // mouse wheel position
 static int mouse_accum_relx = 0, mouse_accum_rely = 0;
 // ID of a device, which mouse events will be ignored (for easier testing)
 static int disabled_mouse_device = UINT32_MAX - 10;
+// Cached values, remember old mouse state
+static int mouse_z_was = 0;
 
-// Returns accumulated mouse button state and clears internal cache by timer
-static int mouse_button_poll()
+bool ags_mouseevent_ready()
 {
-    auto now = AGS_Clock::now();
-    int result = mouse_button_state | mouse_accum_button_state;
-    if (now >= mouse_clear_at_time) {
-        mouse_accum_button_state = 0;
-        mouse_clear_at_time = now + std::chrono::milliseconds(50);
+    return g_mouseEvtQueue.size() > 0;
+}
+
+SDL_Event ags_get_next_mouseevent()
+{
+    if (g_mouseEvtQueue.size() > 0)
+    {
+        auto evt = g_mouseEvtQueue.front();
+        g_mouseEvtQueue.pop_front();
+        return evt;
     }
-    return result;
+    SDL_Event empty = {};
+    return empty;
+}
+
+bool ags_misbuttondown(eAGSMouseButton but)
+{
+    return (mouse_button_state & MouseButton2Bits[but]) != 0;
+}
+
+void ags_simulate_mouseclick(eAGSMouseButton but)
+{
+    SDL_Event sdlevent = {};
+    sdlevent.type = SDL_MOUSEBUTTONDOWN;
+    sdlevent.button.button = ags_button_to_sdl(but);
+    sdlevent.button.x = sys_mouse_x; // CHECKME later if this is okay...
+    sdlevent.button.y = sys_mouse_y;
+    SDL_PushEvent(&sdlevent);
+    sdlevent.type = SDL_MOUSEBUTTONUP;
+    SDL_PushEvent(&sdlevent);
 }
 
 // Syncs all the emulated mouse devices with the real sys_mouse_* coords
@@ -405,21 +448,21 @@ static void on_sdl_mouse_motion(const SDL_MouseMotionEvent &event)
     sync_sys_mouse_pos();
 }
 
-static void on_sdl_mouse_button(const SDL_MouseButtonEvent &event)
+static void on_sdl_mouse_down(const SDL_Event &event)
 {
-    if (event.which == disabled_mouse_device)
+    if (event.button.which == disabled_mouse_device)
         return;
 
-    sys_mouse_x = event.x;
-    sys_mouse_y = event.y;
+    sys_mouse_x = event.button.x;
+    sys_mouse_y = event.button.y;
+    g_mouseEvtQueue.push_back(event);
+}
 
-    if (event.type == SDL_MOUSEBUTTONDOWN) {
-        mouse_button_state |= sdl_button_to_mask(event.button);
-        mouse_accum_button_state |= sdl_button_to_mask(event.button);
-    }
-    else {
-        mouse_button_state &= ~sdl_button_to_mask(event.button);
-    }
+static void on_sdl_mouse_up(const SDL_Event &event)
+{
+    sys_mouse_x = event.button.x;
+    sys_mouse_y = event.button.y;
+    g_mouseEvtQueue.push_back(event);
 }
 
 static void on_sdl_mouse_wheel(const SDL_MouseWheelEvent &event)
@@ -430,59 +473,25 @@ static void on_sdl_mouse_wheel(const SDL_MouseWheelEvent &event)
     sys_mouse_z += event.y;
 }
 
-int butwas = 0;
-static eAGSMouseButton mgetbutton()
+void ags_mouse_acquire_relxy(int &x, int &y)
 {
-    const int butis = mouse_button_poll();
-    if ((butis > 0) & (butwas > 0))
-        return kMouseNone;  // don't allow holding button down
-
-    butwas = butis;
-    if (butis & MouseBitLeft)
-        return kMouseLeft;
-    else if (butis & MouseBitRight)
-        return kMouseRight;
-    else if (butis & MouseBitMiddle)
-        return kMouseMiddle;
-    return kMouseNone;
-}
-
-extern eAGSMouseButton simulatedClick;
-int mouse_z_was = 0;
-// Convert mouse button id to flags
-const int MouseButton2Bits[kNumMouseButtons] =
-    { 0, MouseBitLeft, MouseBitRight, MouseBitMiddle };
-
-bool ags_misbuttondown(eAGSMouseButton but)
-{
-    return (mouse_button_poll() & MouseButton2Bits[but]) != 0;
-}
-
-eAGSMouseButton ags_mgetbutton()
-{
-    if (simulatedClick > kMouseNone)
-    {
-        eAGSMouseButton mbut = simulatedClick;
-        simulatedClick = kMouseNone;
-        return mbut;
-    }
-    return mgetbutton();
-}
-
-void ags_mouse_acquire_relxy(int &x, int &y) {
     x = mouse_accum_relx;
     y = mouse_accum_rely;
     mouse_accum_relx = 0;
     mouse_accum_rely = 0;
 }
 
-void ags_domouse() {
+void ags_domouse()
+{
     Mouse::Poll();
 }
 
-int ags_check_mouse_wheel() {
-    if (game.options[OPT_MOUSEWHEEL] == 0) { return 0; }
-    if (sys_mouse_z == mouse_z_was) { return 0; }
+int ags_check_mouse_wheel()
+{
+    if (game.options[OPT_MOUSEWHEEL] == 0)
+        return 0;
+    if (sys_mouse_z == mouse_z_was)
+        return 0;
 
     int result = 0;
     if (sys_mouse_z > mouse_z_was)
@@ -900,8 +909,6 @@ void ags_clear_input_state()
     sys_modkeys = 0;
     sys_modkeys_fired = false;
     mouse_button_state = 0;
-    mouse_accum_button_state = 0;
-    mouse_clear_at_time = AGS_Clock::now();
     ags_clear_mouse_movement();
 }
 
@@ -911,8 +918,6 @@ void ags_clear_input_buffer()
     // accumulated mod keys have to be cleared because they depend on key evt queue
     sys_modkeys = 0;
     sys_modkeys_fired = false;
-    // accumulated state only helps to not miss clicks
-    mouse_accum_button_state = 0;
     // forget about accumulated mouse movement too
     ags_clear_mouse_movement();
 }
@@ -1001,8 +1006,10 @@ void sys_evt_process_one(const SDL_Event &event) {
         on_sdl_mouse_motion(event.motion);
         break;
     case SDL_MOUSEBUTTONDOWN:
+        on_sdl_mouse_down(event);
+        break;
     case SDL_MOUSEBUTTONUP:
-        on_sdl_mouse_button(event.button);
+        on_sdl_mouse_up(event);
         break;
     case SDL_MOUSEWHEEL:
         on_sdl_mouse_wheel(event.wheel);

Engine/ac/mouse.cpp
@@ -54,8 +54,6 @@ Bitmap *dotted_mouse_cursor = nullptr;
 IDriverDependantBitmap *mouseCursor = nullptr;
 Bitmap *blank_mouse_cursor = nullptr;
 
-eAGSMouseButton simulatedClick = kMouseNone;
-
 // The Mouse:: functions are static so the script doesn't pass
 // in an object parameter
 void Mouse_SetVisible(int isOn) {
@@ -319,7 +317,7 @@ int IsModeEnabled(int which) {
 }
 
 void SimulateMouseClick(int button_id) {
-    simulatedClick = static_cast<eAGSMouseButton>(button_id);
+    ags_simulate_mouseclick(static_cast<eAGSMouseButton>(button_id));
 }
 
 void Mouse_EnableControl(bool on)

Engine/ac/room.cpp
@@ -93,7 +93,6 @@ extern unsigned int loopcounter;
 extern IDriverDependantBitmap* roomBackgroundBmp;
 extern IGraphicsDriver *gfxDriver;
 extern RGB palette[256];
-extern int mouse_z_was;
 
 extern CCHotspot ccDynamicHotspot;
 extern CCObject ccDynamicObject;
@@ -976,7 +975,6 @@ void first_room_initialization() {
     starting_room = displayed_room;
     playerchar->prevroom = -1;
     set_loop_counter(0);
-    mouse_z_was = sys_mouse_z;
     // Reset background frame state
     play.bg_frame = 0;
     play.bg_frame_locked = (thisroom.Options.Flags & kRoomFlag_BkgFrameLocked) != 0;

Engine/ac/sys_events.h
@@ -28,7 +28,7 @@ union SDL_Event;
 // Converts SDL key data to eAGSKeyCode, which may be also directly used as an ASCII char
 // if it is in proper range, see comments to eAGSKeyCode for details.
 // Optionally works in bacward compatible mode (old_keyhandle)
-KeyInput ags_keycode_from_sdl(const SDL_Event &event, bool old_keyhandle);
+KeyInput sdl_keyevt_to_ags_key(const SDL_Event &event, bool old_keyhandle);
 // Converts eAGSKeyCode to SDL key scans (up to 3 values, because this is not a 1:1 match);
 // NOTE: fails at Ctrl+ or Alt+ AGS keys, or any unknown key codes.
 bool ags_key_to_sdl_scan(eAGSKeyCode key, SDL_Scancode(&scan)[3]);
@@ -86,11 +86,14 @@ extern bool sys_modkeys_fired; // tells whether mod combination had been used fo
 
 
 // Mouse input handling
-//
+// Converts SDL mouse button code to AGS code
+eAGSMouseButton sdl_mbut_to_ags_but(int sdl_mbut);
+// Tells if there are any buffered button events
+bool ags_mouseevent_ready();
+// Queries for the next button event in buffer; returns uninitialized data if none was queued
+SDL_Event ags_get_next_mouseevent();
 // Tells if the mouse button is currently down
 bool ags_misbuttondown(eAGSMouseButton but);
-// Returns last "clicked" mouse button
-eAGSMouseButton ags_mgetbutton();
 // Returns recent relative mouse movement; resets accumulated values
 void ags_mouse_acquire_relxy(int &x, int &y);
 // Updates mouse cursor position in game
@@ -98,6 +101,8 @@ void ags_domouse();
 // Returns -1 for wheel down and +1 for wheel up
 // TODO: introduce constants for this
 int  ags_check_mouse_wheel();
+// Simulates a click with the given mouse button
+void ags_simulate_mouseclick(eAGSMouseButton but);
 
 // TODO: hide these later after refactoring mousew32.cpp
 extern volatile int sys_mouse_x; // mouse x position

Engine/main/game_run.cpp
@@ -228,6 +228,22 @@ static void toggle_mouse_lock()
     }
 }
 
+bool run_service_mb_controls(eAGSMouseButton &out_mbut, int &mwheelz)
+{
+    out_mbut = kMouseNone; // clear the output
+    if (!ags_mouseevent_ready())
+        return false; // there was no mouse event
+
+    const SDL_Event mb_evt = ags_get_next_mouseevent();
+    if (mb_evt.type == SDL_MOUSEBUTTONDOWN)
+    {
+        out_mbut = sdl_mbut_to_ags_but(mb_evt.button.button);
+        lock_mouse_on_click();
+    }
+    mwheelz = ags_check_mouse_wheel();
+    return out_mbut != kMouseNone || mwheelz != 0;
+}
+
 // Runs default mouse button handling
 static void check_mouse_controls()
 {
@@ -365,7 +381,7 @@ bool run_service_key_controls(KeyInput &out_key)
     if (handled || (old_keyhandle && is_only_mod_key))
         return false;
 
-    KeyInput ki = ags_keycode_from_sdl(key_evt, old_keyhandle);
+    KeyInput ki = sdl_keyevt_to_ags_key(key_evt, old_keyhandle);
     if ((ki.Key == eAGSKeyCodeNone) && (ki.UChar == 0))
         return false; // should skip this key event
 
@@ -454,16 +470,6 @@ bool run_service_key_controls(KeyInput &out_key)
 
     // No service operation triggered? return active keypress and mods to caller
     out_key = ki;
-    return true;
-}
-
-bool run_service_mb_controls(eAGSMouseButton &mbut, int &mwheelz)
-{
-    mbut = ags_mgetbutton();
-    mwheelz = ags_check_mouse_wheel();
-    if (mbut == kMouseNone && mwheelz == 0)
-        return false;
-    lock_mouse_on_click();
     return true;
 }
 
@@ -592,7 +598,9 @@ static void check_controls() {
 
     sys_evt_process_pending();
 
-    check_mouse_controls();
+    // Handle all the buffered mouse events
+    while (ags_mouseevent_ready())
+        check_mouse_controls();
     // Handle all the buffered key events
     while (ags_keyevent_ready())
         check_keyboard_controls();

commit dd4220eaaa75a1e2d75fe67b702f132c3916ecb7

Engine: fixed mouse button state
Broken by 3a38e69212378a75064265eb14fb158067f9a069

Engine/ac/sys_events.cpp
@@ -448,6 +448,7 @@ static void on_sdl_mouse_down(const SDL_Event &event)
 
     sys_mouse_x = event.button.x;
     sys_mouse_y = event.button.y;
+    mouse_button_state |= sdl_button_to_mask(event.button.button);
     g_inputEvtQueue.push_back(event);
 }
 
@@ -455,6 +456,7 @@ static void on_sdl_mouse_up(const SDL_Event &event)
 {
     sys_mouse_x = event.button.x;
     sys_mouse_y = event.button.y;
+    mouse_button_state &= ~sdl_button_to_mask(event.button.button);
     // Uncomment when need to handle mouse up event in the game states
     //g_inputEvtQueue.push_back(event);
 }
adventuregamestudio/ags

commit 9658c71528676acb386e2c638e782cc4e7d2afe5

Engine: store ScriptUserObject in memory similar to DynamicArrays
I realized this recently that SUOs were not registered in the script exports properly. In AGS standards the object having a memory buffer should register that buffer's address directly, so that it could be read and written to directly. That is how strings, arrays, and any other structs are exported. This also how the engine plugins can access the data if they use script API to retrieve it.

There have not been any script API working with custom structs earlier, but there is since 3.5.0 (few functions returning managed Point struct). Plugins won't be able to use them correctly without this fix.

Engine/ac/dynobj/scriptuserobject.cpp
@@ -26,120 +26,57 @@ const char *ScriptUserObject::GetType()
     return TypeName;
 }
 
-ScriptUserObject::~ScriptUserObject()
+/* static */ DynObjectRef ScriptUserObject::Create(size_t size)
 {
-    delete [] _data;
-}
-
-/* static */ ScriptUserObject *ScriptUserObject::CreateManaged(size_t size)
-{
-    ScriptUserObject *suo = new ScriptUserObject();
-    suo->Create(nullptr, nullptr, size);
-    ccRegisterManagedObject(suo, suo);
-    return suo;
-}
-
-void ScriptUserObject::Create(const uint8_t *data, Stream *in, size_t size)
-{
-    delete [] _data;
-    _data = nullptr;
-
-    _size = size;
-    if (_size > 0)
+    uint8_t *new_data = new uint8_t[size + MemHeaderSz];
+    memset(new_data, 0, size + MemHeaderSz);
+    Header &hdr = reinterpret_cast<Header&>(*new_data);
+    hdr.Size = size;
+    void *obj_ptr = &new_data[MemHeaderSz];
+    int32_t handle = ccRegisterManagedObject(obj_ptr, &globalDynamicStruct);
+    if (handle == 0)
     {
-        _data = new uint8_t[size];
-        if (data)
-            memcpy(_data, data, _size);
-        else if (in)
-            in->Read(_data, _size);
-        else
-            memset(_data, 0, _size);
+        delete[] new_data;
+        return DynObjectRef();
     }
+    return DynObjectRef(handle, obj_ptr, &globalDynamicStruct);
 }
 
-int ScriptUserObject::Dispose(void* /*address*/, bool /*force*/)
+int ScriptUserObject::Dispose(void *address, bool /*force*/)
 {
-    delete this;
+    delete[] (static_cast<uint8_t*>(address) - MemHeaderSz);
     return 1;
 }
 
-size_t ScriptUserObject::CalcSerializeSize(void* /*address*/)
+size_t ScriptUserObject::CalcSerializeSize(void *address)
 {
-    return _size;
+    const Header &hdr = GetHeader(address);
+    return hdr.Size + FileHeaderSz;
 }
 
-void ScriptUserObject::Serialize(void* /*address*/, AGS::Common::Stream *out)
+void ScriptUserObject::Serialize(void *address, AGS::Common::Stream *out)
 {
-    out->Write(_data, _size);
+    const Header &hdr = GetHeader(address);
+    // NOTE: we only write the data, no header at the moment
+    out->Write(address, hdr.Size);
 }
 
 void ScriptUserObject::Unserialize(int index, Stream *in, size_t data_sz)
 {
-    Create(nullptr, in, data_sz);
-    ccRegisterUnserializedObject(index, this, this);
+    uint8_t *new_data = new uint8_t[(data_sz - FileHeaderSz) + MemHeaderSz];
+    Header &hdr = reinterpret_cast<Header&>(*new_data);
+    in->Read(new_data + MemHeaderSz, data_sz - FileHeaderSz);
+    ccRegisterUnserializedObject(index, &new_data[MemHeaderSz], this);
 }
 
-void* ScriptUserObject::GetFieldPtr(void* /*address*/, intptr_t offset)
-{
-    return _data + offset;
-}
-
-void ScriptUserObject::Read(void* /*address*/, intptr_t offset, uint8_t *dest, size_t size)
-{
-    memcpy(dest, _data + offset, size);
-}
-
-uint8_t ScriptUserObject::ReadInt8(void* /*address*/, intptr_t offset)
-{
-    return *(uint8_t*)(_data + offset);
-}
-
-int16_t ScriptUserObject::ReadInt16(void* /*address*/, intptr_t offset)
-{
-    return *(int16_t*)(_data + offset);
-}
-
-int32_t ScriptUserObject::ReadInt32(void* /*address*/, intptr_t offset)
-{
-    return *(int32_t*)(_data + offset);
-}
-
-float ScriptUserObject::ReadFloat(void* /*address*/, intptr_t offset)
-{
-    return *(float*)(_data + offset);
-}
-
-void ScriptUserObject::Write(void* /*address*/, intptr_t offset, const uint8_t *src, size_t size)
-{
-    memcpy((void*)(_data + offset), src, size);
-}
-
-void ScriptUserObject::WriteInt8(void* /*address*/, intptr_t offset, uint8_t val)
-{
-    *(uint8_t*)(_data + offset) = val;
-}
-
-void ScriptUserObject::WriteInt16(void* /*address*/, intptr_t offset, int16_t val)
-{
-    *(int16_t*)(_data + offset) = val;
-}
-
-void ScriptUserObject::WriteInt32(void* /*address*/, intptr_t offset, int32_t val)
-{
-    *(int32_t*)(_data + offset) = val;
-}
-
-void ScriptUserObject::WriteFloat(void* /*address*/, intptr_t offset, float val)
-{
-    *(float*)(_data + offset) = val;
-}
+ScriptUserObject globalDynamicStruct;
 
 
 // Allocates managed struct containing two ints: X and Y
 ScriptUserObject *ScriptStructHelpers::CreatePoint(int x, int y)
 {
-    ScriptUserObject *suo = ScriptUserObject::CreateManaged(sizeof(int32_t) * 2);
-    suo->WriteInt32(suo, 0, x);
-    suo->WriteInt32(suo, sizeof(int32_t), y);
-    return suo;
+    DynObjectRef ref = ScriptUserObject::Create(sizeof(int32_t) * 2);
+    ref.Mgr->WriteInt32(ref.Obj, 0, x);
+    ref.Mgr->WriteInt32(ref.Obj, sizeof(int32_t), y);
+    return static_cast<ScriptUserObject*>(ref.Obj);
 }

Engine/ac/dynobj/cc_dynamicarray.cpp
@@ -68,7 +68,7 @@ void CCDynamicArray::Serialize(void *address, AGS::Common::Stream *out)
 
 void CCDynamicArray::Unserialize(int index, Stream *in, size_t data_sz)
 {
-    char *new_arr = new char[(data_sz - FileHeaderSz) + MemHeaderSz];
+    uint8_t *new_arr = new uint8_t[(data_sz - FileHeaderSz) + MemHeaderSz];
     Header &hdr = reinterpret_cast<Header&>(*new_arr);
     hdr.ElemCount = in->ReadInt32();
     hdr.TotalSize = in->ReadInt32();
@@ -76,22 +76,21 @@ void CCDynamicArray::Unserialize(int index, Stream *in, size_t data_sz)
     ccRegisterUnserializedObject(index, &new_arr[MemHeaderSz], this);
 }
 
-DynObjectRef CCDynamicArray::Create(int numElements, int elementSize, bool isManagedType)
+/* static */ DynObjectRef CCDynamicArray::Create(int numElements, int elementSize, bool isManagedType)
 {
-    char *new_arr = new char[numElements * elementSize + MemHeaderSz];
+    uint8_t *new_arr = new uint8_t[numElements * elementSize + MemHeaderSz];
     memset(new_arr, 0, numElements * elementSize + MemHeaderSz);
     Header &hdr = reinterpret_cast<Header&>(*new_arr);
     hdr.ElemCount = numElements | (ARRAY_MANAGED_TYPE_FLAG * isManagedType);
     hdr.TotalSize = elementSize * numElements;
     void *obj_ptr = &new_arr[MemHeaderSz];
-    // TODO: investigate if it's possible to register real object ptr directly
-    int32_t handle = ccRegisterManagedObject(obj_ptr, this);
+    int32_t handle = ccRegisterManagedObject(obj_ptr, &globalDynamicArray);
     if (handle == 0)
     {
         delete[] new_arr;
-        return DynObjectRef(0, nullptr);
+        return DynObjectRef();
     }
-    return DynObjectRef(handle, obj_ptr);
+    return DynObjectRef(handle, obj_ptr, &globalDynamicArray);
 }
 
 
@@ -102,17 +101,17 @@ DynObjectRef DynamicArrayHelpers::CreateStringArray(const std::vector<const char
 {
     // NOTE: we need element size of "handle" for array of managed pointers
     DynObjectRef arr = globalDynamicArray.Create(items.size(), sizeof(int32_t), true);
-    if (!arr.second)
+    if (!arr.Obj)
         return arr;
     // Create script strings and put handles into array
-    int32_t *slots = static_cast<int32_t*>(arr.second);
+    int32_t *slots = static_cast<int32_t*>(arr.Obj);
     for (auto s : items)
     {
         DynObjectRef str = stringClassImpl->CreateString(s);
         // We must add reference count, because the string is going to be saved
         // within another object (array), not returned to script directly
-        ccAddObjectReference(str.first);
-        *(slots++) = str.first;
+        ccAddObjectReference(str.Handle);
+        *(slots++) = str.Handle;
     }
     return arr;
 }

Engine/ac/dynobj/cc_dynamicarray.h
@@ -34,17 +34,21 @@ public:
         uint32_t TotalSize = 0u;
     };
 
+    CCDynamicArray() = default;
+    ~CCDynamicArray() = default;
+
     inline static const Header &GetHeader(void *address)
     {
         return reinterpret_cast<const Header&>(*(static_cast<uint8_t*>(address) - MemHeaderSz));
     }
 
+    // Create managed array object and return a pointer to the beginning of a buffer
+    static DynObjectRef Create(int numElements, int elementSize, bool isManagedType);
+
     // return the type name of the object
     const char *GetType() override;
     int Dispose(void *address, bool force) override;
-    void Unserialize(int index, AGS::Common::Stream *in, size_t data_sz);
-    // Create managed array object and return a pointer to the beginning of a buffer
-    DynObjectRef Create(int numElements, int elementSize, bool isManagedType);
+    void Unserialize(int index, AGS::Common::Stream *in, size_t data_sz) override;
 
 private:
     // The size of the array's header in memory, prepended to the element data
@@ -60,6 +64,7 @@ private:
 };
 
 extern CCDynamicArray globalDynamicArray;
+
 
 // Helper functions for setting up dynamic arrays.
 namespace DynamicArrayHelpers

Engine/ac/dynobj/cc_scriptobject.h
@@ -26,8 +26,19 @@
 #include "core/types.h"
 
 
-// A pair of managed handle and abstract object pointer
-typedef std::pair<int32_t, void*> DynObjectRef;
+struct IScriptObject;
+
+// A convenience struct for grouping handle and dynamic object
+struct DynObjectRef
+{
+    const int Handle = 0;
+    void * const Obj = nullptr;
+    IScriptObject * const Mgr = nullptr;
+
+    DynObjectRef() = default;
+    DynObjectRef(int handle, void *obj, IScriptObject *mgr)
+        : Handle(handle), Obj(obj), Mgr(mgr) {}
+};
 
 
 struct IScriptObject

Engine/ac/dynobj/scriptuserobject.h
@@ -12,7 +12,7 @@
 //
 //=============================================================================
 //
-// Managed object, which size and contents are defined by user script
+// ScriptUserObject is a dynamic (managed) struct manager.
 //
 //=============================================================================
 #ifndef __AGS_EE_DYNOBJ__SCRIPTUSERSTRUCT_H
@@ -27,42 +27,38 @@ struct ScriptUserObject final : AGSCCDynamicObject
 public:
     static const char *TypeName;
 
-    ScriptUserObject() = default;
-    
-protected:
-    virtual ~ScriptUserObject();
+    struct Header
+    {
+        uint32_t Size = 0u;
+        // NOTE: we use signed int for Size at the moment, because the managed
+        // object interface's Serialize() function requires the object to return
+        // negative value of size in case the provided buffer was not large
+        // enough. Since this interface is also a part of Plugin API, we would
+        // need more significant change to program before we could use different
+        // approach.
+    };
 
-public:
-    static ScriptUserObject *CreateManaged(size_t size);
-    void Create(const uint8_t *data, AGS::Common::Stream *in, size_t size);
+    ScriptUserObject() = default;
+    ~ScriptUserObject() = default;
+
+    inline static const Header &GetHeader(void *address)
+    {
+        return reinterpret_cast<const Header&>(*(static_cast<uint8_t*>(address) - MemHeaderSz));
+    }
+
+    // Create managed struct object and return a pointer to the beginning of a buffer
+    static DynObjectRef Create(size_t size);
 
     // return the type name of the object
     const char *GetType() override;
     int Dispose(void *address, bool force) override;
     void Unserialize(int index, AGS::Common::Stream *in, size_t data_sz) override;
 
-    // Support for reading and writing object values by their relative offset
-    void   *GetFieldPtr(void *address, intptr_t offset) override;
-    void    Read(void *address, intptr_t offset, uint8_t *dest, size_t size) override;
-    uint8_t ReadInt8(void *address, intptr_t offset) override;
-    int16_t ReadInt16(void *address, intptr_t offset) override;
-    int32_t ReadInt32(void *address, intptr_t offset) override;
-    float   ReadFloat(void *address, intptr_t offset) override;
-    void    Write(void *address, intptr_t offset, const uint8_t *src, size_t size) override;
-    void    WriteInt8(void *address, intptr_t offset, uint8_t val) override;
-    void    WriteInt16(void *address, intptr_t offset, int16_t val) override;
-    void    WriteInt32(void *address, intptr_t offset, int32_t val) override;
-    void    WriteFloat(void *address, intptr_t offset, float val) override;
-
 private:
-    // NOTE: we use signed int for Size at the moment, because the managed
-    // object interface's Serialize() function requires the object to return
-    // negative value of size in case the provided buffer was not large
-    // enough. Since this interface is also a part of Plugin API, we would
-    // need more significant change to program before we could use different
-    // approach.
-    int32_t  _size = 0;
-    uint8_t *_data = nullptr;
+    // The size of the array's header in memory, prepended to the element data
+    static const size_t MemHeaderSz = sizeof(Header);
+    // The size of the serialized header
+    static const size_t FileHeaderSz = sizeof(uint32_t) * 0; // no header serialized
 
     // Savegame serialization
     // Calculate and return required space for serialization, in bytes
@@ -70,6 +66,8 @@ private:
     // Write object data into the provided stream
     void Serialize(void *address, AGS::Common::Stream *out) override;
 };
+
+extern ScriptUserObject globalDynamicStruct;
 
 
 // Helper functions for setting up custom managed structs based on ScriptUserObject.

Engine/ac/scriptcontainers.cpp
@@ -122,7 +122,7 @@ void *Dict_GetKeysAsArray(ScriptDictBase *dic)
     if (items.size() == 0)
         return nullptr;
     DynObjectRef arr = DynamicArrayHelpers::CreateStringArray(items);
-    return arr.second;
+    return arr.Obj;
 }
 
 void *Dict_GetValuesAsArray(ScriptDictBase *dic)
@@ -132,7 +132,7 @@ void *Dict_GetValuesAsArray(ScriptDictBase *dic)
     if (items.size() == 0)
         return nullptr;
     DynObjectRef arr = DynamicArrayHelpers::CreateStringArray(items);
-    return arr.second;
+    return arr.Obj;
 }
 
 RuntimeScriptValue Sc_Dict_Create(const RuntimeScriptValue *params, int32_t param_count)
@@ -277,7 +277,7 @@ void *Set_GetItemsAsArray(ScriptSetBase *set)
     if (items.size() == 0)
         return nullptr;
     DynObjectRef arr = DynamicArrayHelpers::CreateStringArray(items);
-    return arr.second;
+    return arr.Obj;
 }
 
 RuntimeScriptValue Sc_Set_Create(const RuntimeScriptValue *params, int32_t param_count)

Engine/ac/string.cpp
@@ -245,11 +245,11 @@ int StrContains (const char *s1, const char *s2) {
 //=============================================================================
 
 const char *CreateNewScriptString(const String &fromText) {
-    return (const char*)CreateNewScriptStringObj(fromText.GetCStr(), true).second;
+    return (const char*)CreateNewScriptStringObj(fromText.GetCStr(), true).Obj;
 }
 
 const char *CreateNewScriptString(const char *fromText, bool reAllocate) {
-    return (const char*)CreateNewScriptStringObj(fromText, reAllocate).second;
+    return (const char*)CreateNewScriptStringObj(fromText, reAllocate).Obj;
 }
 
 DynObjectRef CreateNewScriptStringObj(const String &fromText) {
@@ -270,9 +270,9 @@ DynObjectRef CreateNewScriptStringObj(const char *fromText, bool reAllocate)
     if (handle == 0)
     {
         delete str;
-        return DynObjectRef(0, nullptr);
+        return DynObjectRef();
     }
-    return DynObjectRef(handle, obj_ptr);
+    return DynObjectRef(handle, obj_ptr, str);
 }
 
 size_t break_up_text_into_lines(const char *todis, SplitLines &lines, int wii, int fonnt, size_t max_lines) {

Engine/script/cc_instance.cpp
@@ -1472,8 +1472,8 @@ int ccInstance::Run(int32_t curpc)
                 cc_error("invalid size for dynamic array; requested: %d, range: 1..%d", numElements, INT32_MAX);
                 return -1;
             }
-            DynObjectRef ref = globalDynamicArray.Create(numElements, arg_elsize, arg_managed);
-            reg1.SetScriptObject(ref.second, &globalDynamicArray);
+            DynObjectRef ref = CCDynamicArray::Create(numElements, arg_elsize, arg_managed);
+            reg1.SetScriptObject(ref.Obj, &globalDynamicArray);
             break;
         }
         case SCMD_NEWUSEROBJECT:
@@ -1485,8 +1485,8 @@ int ccInstance::Run(int32_t curpc)
                 cc_error("Invalid size for user object; requested: %d (or %d), range: 0..%d", arg_size, arg_size, INT_MAX);
                 return -1;
             }
-            ScriptUserObject *suo = ScriptUserObject::CreateManaged(arg_size);
-            reg1.SetScriptObject(suo, suo);
+            DynObjectRef ref = ScriptUserObject::Create(arg_size);
+            reg1.SetScriptObject(ref.Obj, ref.Mgr);
             break;
         }
         case SCMD_FADD:
@@ -1599,7 +1599,7 @@ int ccInstance::Run(int32_t curpc)
             {
                 const char *ptr = reinterpret_cast<const char*>(reg1.GetDirectPtr());
                 reg1.SetScriptObject(
-                    stringClassImpl->CreateString(ptr).second,
+                    stringClassImpl->CreateString(ptr).Obj,
                     &myScriptStringImpl);
             }
             break;

commit f3dff32a20bfd162a7d4f3b8f0ba019512bab473

Engine: fixed ScriptUserObject de-serialization saves garbage into Size
Was broken by an oversight in 9658c71528676acb386e2c638e782cc4e7d2afe5

Engine/ac/dynobj/cc_agsdynamicobject.cpp
@@ -87,6 +87,7 @@ int AGSCCDynamicObject::Serialize(void *address, uint8_t *buffer, int bufsize) {
     // If the required space is larger than the provided buffer,
     // then return negated required space, notifying the caller that a larger buffer is necessary
     size_t req_size = CalcSerializeSize(address);
+    assert(req_size <= INT32_MAX); // dynamic object API does not support size > int32
     if (bufsize < 0 || req_size > static_cast<size_t>(bufsize))
         return -(static_cast<int32_t>(req_size));
 

Engine/ac/dynobj/cc_serializer.cpp
@@ -56,6 +56,7 @@ void AGSDeSerializer::Unserialize(int index, const char *objectType, const char 
     // classes registered by plugin cannot, because streams are not (yet)
     // part of the plugin API.
     size_t data_sz = static_cast<size_t>(dataSize);
+    assert(data_sz <= INT32_MAX); // dynamic object API does not support size > int32
     MemoryStream mems(reinterpret_cast<const uint8_t*>(serializedData), dataSize);
 
     // TODO: consider this: there are object types that are part of the

Engine/ac/dynobj/scriptuserobject.cpp
@@ -65,6 +65,7 @@ void ScriptUserObject::Unserialize(int index, Stream *in, size_t data_sz)
 {
     uint8_t *new_data = new uint8_t[(data_sz - FileHeaderSz) + MemHeaderSz];
     Header &hdr = reinterpret_cast<Header&>(*new_data);
+    hdr.Size = data_sz - FileHeaderSz;
     in->Read(new_data + MemHeaderSz, data_sz - FileHeaderSz);
     ccRegisterUnserializedObject(index, &new_data[MemHeaderSz], this);
 }
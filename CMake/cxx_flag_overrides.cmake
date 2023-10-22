95c8d03c0956109f2428aad80ce8fd54132aa9a8
e <ericoporto2008@gmail.com>
2023-10-21 21:46:08 -0300

    CMake: make sure MinGW builds statically by default
    this is to avoid manually copying mingw dlls, and ensures a portable binary the same as traditional acwin.exe

--- CMake/cxx_flag_overrides.cmake
+++ CMake/cxx_flag_overrides.cmake
@@ -6,3 +6,7 @@ if(MSVC)
     set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "/MT /Zi /O2 /Ob1 /DNDEBUG")
     set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT     "/MT /O1 /Ob1 /DNDEBUG")
 endif()
+
+if(MINGW)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static")
+endif ()

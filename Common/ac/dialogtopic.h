154d3c6f013bf7382b51e5f291161a7a973af4d6
Ivan Mogilko <ikm_spb@yahoo.com>
2023-10-26 03:43:17 +0300

    Engine: remove legacy dialog command constants

--- Common/ac/dialogtopic.h
+++ Common/ac/dialogtopic.h
@@ -12,7 +12,7 @@
 //
 //=============================================================================
 //
-//
+// A Dialog Topic (a set of conversation options).
 //
 //=============================================================================
 #ifndef __AGS_CN_AC__DIALOGTOPIC_H
@@ -21,31 +21,12 @@
 namespace AGS { namespace Common { class Stream; } }
 using namespace AGS; // FIXME later
 
-// [IKM] This is *conversation* dialog, not *gui* dialog, mind you!
-
 #define MAXTOPICOPTIONS     30
 #define DFLG_ON             1  // currently enabled
 #define DFLG_OFFPERM        2  // off forever (can't be trurned on)
 #define DFLG_NOREPEAT       4  // character doesn't repeat it when clicked
 #define DFLG_HASBEENCHOSEN  8  // dialog option is 'read'
 #define DTFLG_SHOWPARSER    1  // show parser in this topic
-#define DCMD_SAY            1
-#define DCMD_OPTOFF         2
-#define DCMD_OPTON          3
-#define DCMD_RETURN         4
-#define DCMD_STOPDIALOG     5
-#define DCMD_OPTOFFFOREVER  6
-#define DCMD_RUNTEXTSCRIPT  7
-#define DCMD_GOTODIALOG     8
-#define DCMD_PLAYSOUND      9
-#define DCMD_ADDINV         10
-#define DCMD_SETSPCHVIEW    11
-#define DCMD_NEWROOM        12
-#define DCMD_SETGLOBALINT   13
-#define DCMD_GIVESCORE      14
-#define DCMD_GOTOPREVIOUS   15
-#define DCMD_LOSEINV        16
-#define DCMD_ENDSCRIPT      0xff
 #define DCHAR_NARRATOR  999
 #define DCHAR_PLAYER    998
 struct DialogTopic {
@@ -62,6 +43,5 @@ struct DialogTopic {
     void ReadFromSavegame(Common::Stream *in);
     void WriteToSavegame(Common::Stream *out) const;
 };
-
 
 #endif // __AGS_CN_AC__DIALOGTOPIC_H

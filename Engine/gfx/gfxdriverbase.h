5c2129b8f176e8d95b0f26132bf1749d7dabdc29
Ivan Mogilko <ikm_spb@yahoo.com>
2023-11-07 17:18:48 +0300

    Engine: separated BitmapToVideoMemLinearImpl for convenience
    Split BitmapToVideoMemImpl into simpler BitmapToVideoMemImpl and BitmapToVideoMemLinearImpl meant for that "linear filter" transparency fix.
    This does not seem to make things noticeably faster with non-linear scaling, but may make it easier to understand the code.
    
    Also in case we'd like to remove/replace this hack someday.

--- Engine/gfx/gfxdriverbase.h
+++ Engine/gfx/gfxdriverbase.h
@@ -362,7 +362,7 @@ private:
     size_t _fxIndex; // next free pool item
 
     // specialized method to convert bitmap to video memory depending on bit depth
-    template <typename T, bool HasAlpha, bool UsingLinearFiltering> void
+    template <typename T, bool HasAlpha> void
     BitmapToVideoMemImpl(
             const Bitmap *bitmap, const TextureTile *tile,
             uint8_t *dst_ptr, const int dst_pitch
@@ -370,6 +370,12 @@ private:
 
     template <typename T> void
     BitmapToVideoMemOpaqueImpl(
+            const Bitmap *bitmap, const TextureTile *tile,
+            uint8_t *dst_ptr, const int dst_pitch
+    );
+
+    template <typename T, bool HasAlpha> void
+    BitmapToVideoMemLinearImpl(
             const Bitmap *bitmap, const TextureTile *tile,
             uint8_t *dst_ptr, const int dst_pitch
     );

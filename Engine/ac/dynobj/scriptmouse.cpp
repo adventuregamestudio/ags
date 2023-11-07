7ed42b394434a67dfe4d4b188c47321b1654c1b7
Ivan Mogilko <ikm_spb@yahoo.com>
2023-11-07 14:45:11 +0300

    Auto update copyright and license link in h/c/cpp files

--- Engine/ac/dynobj/scriptmouse.cpp
+++ Engine/ac/dynobj/scriptmouse.cpp
@@ -2,13 +2,13 @@
 //
 // Adventure Game Studio (AGS)
 //
-// Copyright (C) 1999-2011 Chris Jones and 2011-20xx others
+// Copyright (C) 1999-2011 Chris Jones and 2011-2023 various contributors
 // The full list of copyright holders can be found in the Copyright.txt
 // file, which is part of this source code distribution.
 //
 // The AGS source code is provided under the Artistic License 2.0.
 // A copy of this license can be found in the file License.txt and at
-// http://www.opensource.org/licenses/artistic-license-2.0.php
+// https://opensource.org/license/artistic-2-0/
 //
 //=============================================================================
 #include "ac/dynobj/scriptmouse.h"

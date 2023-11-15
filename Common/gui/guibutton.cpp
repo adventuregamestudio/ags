adventuregamestudio/ags

commit 7989edbb4866af495fb7eb5b0142365f82aefb4b

Common: use GUIButton::UpdateCurrentImage() consistently

Common/gui/guibutton.cpp
@@ -218,7 +218,7 @@ void GUIButton::SetMouseOverImage(int32_t image)
 {
     if (_mouseOverImage == image)
         return;
-    
+
     _mouseOverImage = image;
     UpdateCurrentImage();
 }
@@ -227,7 +227,7 @@ void GUIButton::SetNormalImage(int32_t image)
 {
     if (_image == image)
         return;
-    
+
     _image = image;
     UpdateCurrentImage();
 }
@@ -236,8 +236,16 @@ void GUIButton::SetPushedImage(int32_t image)
 {
     if (_pushedImage == image)
         return;
-    
+
     _pushedImage = image;
+    UpdateCurrentImage();
+}
+
+void GUIButton::SetImages(int32_t normal, int32_t over, int32_t pushed)
+{
+    _image = normal;
+    _mouseOverImage = over;
+    _pushedImage = pushed;
     UpdateCurrentImage();
 }
 
@@ -266,74 +274,61 @@ void GUIButton::SetText(const String &text)
 
 bool GUIButton::OnMouseDown()
 {
-    int new_image = (_pushedImage > 0) ? _pushedImage : _currentImage;
-    if (_currentImage != new_image || !IsImageButton())
-        MarkChanged();
-    _currentImage = new_image;
     IsPushed = true;
+    if (!IsImageButton())
+        MarkChanged();
+    UpdateCurrentImage();
     return false;
 }
 
 void GUIButton::OnMouseEnter()
 {
-    int new_image = (IsPushed && _pushedImage > 0) ? _pushedImage :
-        (_mouseOverImage > 0) ? _mouseOverImage : _image;
-    if ((_currentImage != new_image) || (IsPushed && !IsImageButton()))
-    {
-        _currentImage = new_image;
-        MarkChanged();
-    }
     IsMouseOver = true;
+    if (IsPushed && !IsImageButton())
+        MarkChanged();
+    UpdateCurrentImage();
 }
 
 void GUIButton::OnMouseLeave()
 {
-    if ((_currentImage != _image) || (IsPushed && !IsImageButton()))
-    {
-        _currentImage = _image;
-        MarkChanged();
-    }
     IsMouseOver = false;
+    if (IsPushed && !IsImageButton())
+        MarkChanged();
+    UpdateCurrentImage();
 }
 
 void GUIButton::OnMouseUp()
 {
-    int new_image = _image;
     if (IsMouseOver)
     {
-        if (_mouseOverImage > 0)
-            new_image = _mouseOverImage;
         if (IsGUIEnabled(this) && IsClickable())
             IsActivated = true;
     }
 
-    if ((_currentImage != new_image) || (IsPushed && !IsImageButton()))
-    {
-        _currentImage = new_image;
-        MarkChanged();
-    }
     IsPushed = false;
+    if (IsPushed && !IsImageButton())
+        MarkChanged();
+    UpdateCurrentImage();
 }
 
 void GUIButton::UpdateCurrentImage()
 {
-    int was_image = _currentImage;
+    int new_image = _currentImage;
 
     if (IsPushed && (_pushedImage > 0))
     {
-        _currentImage = _pushedImage;
+        new_image = _pushedImage;
     }
     else if (IsMouseOver && (_mouseOverImage > 0))
     {
-        _currentImage = _mouseOverImage;
+        new_image = _mouseOverImage;
     }
     else
     {
-        _currentImage = _image;
+        new_image = _image;
     }
 
-    if (was_image != _currentImage)
-        MarkChanged();
+    SetCurrentImage(new_image);
 }
 
 void GUIButton::WriteToFile(Stream *out) const

Common/gui/guibutton.h
@@ -86,6 +86,7 @@ public:
     void SetMouseOverImage(int32_t image);
     void SetNormalImage(int32_t image);
     void SetPushedImage(int32_t image);
+    void SetImages(int32_t normal, int32_t over, int32_t pushed);
     void SetText(const String &text);
 
     // Events

Engine/ac/button.cpp
@@ -39,9 +39,9 @@ std::vector<AnimatingGUIButton> animbuts;
 // Update the actual button's image from the current animation frame
 void UpdateButtonState(const AnimatingGUIButton &abtn)
 {
-    guibuts[abtn.buttonid].SetPushedImage(0);
-    guibuts[abtn.buttonid].SetMouseOverImage(0);
-    guibuts[abtn.buttonid].SetNormalImage(views[abtn.view].loops[abtn.loop].frames[abtn.frame].pic);
+    // Assign view frame as normal image and reset all the rest
+    guibuts[abtn.buttonid].SetImages(
+        views[abtn.view].loops[abtn.loop].frames[abtn.frame].pic, 0, 0);
 }
 
 void Button_Animate(GUIButton *butt, int view, int loop, int speed, int repeat,

commit 1de96521f5fece460a96814cd699c51eed49788d

Common: fixed pushed textual button not redrawn after mouse up
Was broken by 7989edbb4866af495fb7eb5b0142365f82aefb4b

Common/gui/guibutton.cpp
@@ -274,26 +274,26 @@ void GUIButton::SetText(const String &text)
 
 bool GUIButton::OnMouseDown()
 {
-    IsPushed = true;
     if (!IsImageButton())
         MarkChanged();
+    IsPushed = true;
     UpdateCurrentImage();
     return false;
 }
 
 void GUIButton::OnMouseEnter()
 {
-    IsMouseOver = true;
     if (IsPushed && !IsImageButton())
         MarkChanged();
+    IsMouseOver = true;
     UpdateCurrentImage();
 }
 
 void GUIButton::OnMouseLeave()
 {
-    IsMouseOver = false;
     if (IsPushed && !IsImageButton())
         MarkChanged();
+    IsMouseOver = false;
     UpdateCurrentImage();
 }
 
@@ -305,9 +305,9 @@ void GUIButton::OnMouseUp()
             IsActivated = true;
     }
 
-    IsPushed = false;
     if (IsPushed && !IsImageButton())
         MarkChanged();
+    IsPushed = false;
     UpdateCurrentImage();
 }
 
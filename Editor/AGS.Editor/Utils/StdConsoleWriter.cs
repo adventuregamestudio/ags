a8d287485a213640a1516d881e23888be3dd6079
e <ericoporto2008@gmail.com>
2023-11-24 00:35:27 -0300

    Editor: fix redirection and first line with compile flag

--- Editor/AGS.Editor/Utils/StdConsoleWriter.cs
+++ Editor/AGS.Editor/Utils/StdConsoleWriter.cs
@@ -7,13 +7,38 @@ namespace AGS.Editor
 
     public sealed class StdConsoleWriter
     {
+        private readonly bool _attached = false;
+        private bool _firstLine = true;
+
         private StdConsoleWriter()
         {
-            AttachConsole(ATTACH_PARENT_PROCESS);
+            // for some reason Console.IsOutputRedirect is not working for me in the old cmd.exe
+            // we only want to attach if output is not being redirected
+            var stdout = Console.OpenStandardOutput();
+            if (stdout == Stream.Null)
+            {
+                _attached = AttachConsole(ATTACH_PARENT_PROCESS);
+            }
         }
+
         private void WriteOutLine(string line)
         {
-            Console.WriteLine("AGS: " + line);
+            const string prefix = "AGS: ";
+            string msg = prefix + line;
+
+            if (_attached && _firstLine)
+            {
+                // AGSEditor is a Windows Application and not a Console Application
+                // so it will release the console before we attach again and print an unused prompt line (in cmd.exe)
+                // the line below erases this line and prints the first message on top of it
+                // this is only necessary once, in the first line printed, and only if not attached to a console
+                ClearPrint(msg);
+            }
+            else 
+            { 
+                Console.WriteLine(msg);
+            }
+            _firstLine = false;
         }
 
         private static bool _on;
@@ -25,6 +50,11 @@ namespace AGS.Editor
                 _instance = new StdConsoleWriter();
             }
             return _instance;
+        }
+
+        private static void ClearPrint(string msg)
+        {
+            Console.WriteLine($"\r{msg}{new String(' ', Console.BufferWidth - msg.Length)}");
         }
 
         [System.Runtime.InteropServices.DllImport("kernel32.dll")]

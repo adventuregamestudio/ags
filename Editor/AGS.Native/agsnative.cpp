adventuregamestudio/ags

commit f2a922a9cefd40337502bde734503605a76550db

AGS.Native: fixed case when imported sprite got flags from placeholder
This could make a new sprite look differently, depending on placeholder's (sprite 0) settings, until the game is saved once.

Editor/AGS.Native/agsnative.cpp
@@ -187,10 +187,10 @@ Common::Bitmap *get_sprite (int spnr) {
   return spriteset[spnr];
 }
 
-void SetNewSprite(int slot, Common::Bitmap *sprit) {
+void SetNewSprite(int slot, Common::Bitmap *sprit, int flags) {
   delete spriteset[slot];
 
-  spriteset.SetSprite(slot, sprit);
+  spriteset.SetSprite(slot, sprit, flags);
   spritesModified = true;
 }
 
@@ -198,12 +198,6 @@ void deleteSprite (int sprslot) {
   spriteset.RemoveSprite(sprslot, true);
   
   spritesModified = true;
-}
-
-void SetNewSpriteFromHBitmap(int slot, int hBmp) {
-  // FIXME later
-  Common::Bitmap *tempsprite = Common::BitmapHelper::CreateRawBitmapOwner(convert_hbitmap_to_bitmap((HBITMAP)hBmp));
-  SetNewSprite(slot, tempsprite);
 }
 
 int GetSpriteAsHBitmap(int slot) {
@@ -264,11 +258,8 @@ void update_sprite_resolution(int spriteNum, bool isVarRes, bool isHighRes)
 
 void change_sprite_number(int oldNumber, int newNumber) {
 
-  spriteset.SetSprite(newNumber, spriteset[oldNumber]);
+  spriteset.SetSprite(newNumber, spriteset[oldNumber], thisgame.SpriteInfos[oldNumber].Flags);
   spriteset.RemoveSprite(oldNumber, false);
-
-  thisgame.SpriteInfos[newNumber].Flags = thisgame.SpriteInfos[oldNumber].Flags;
-  thisgame.SpriteInfos[oldNumber].Flags = 0;
 
   spritesModified = true;
 }
@@ -364,8 +355,8 @@ int crop_sprite_edges(int numSprites, int *sprites, bool symmetric) {
 	Common::Bitmap *newsprit = Common::BitmapHelper::CreateBitmap(newWidth, newHeight, sprit->GetColorDepth());
     newsprit->Blit(sprit, left, top, 0, 0, newWidth, newHeight);
     delete sprit;
-
-    spriteset.SetSprite(sprites[aa], newsprit);
+    // set new image and keep old flags
+    spriteset.SetSprite(sprites[aa], newsprit, thisgame.SpriteInfos[aa].Flags);
   }
 
   spritesModified = true;
@@ -2403,9 +2394,8 @@ AGS::Types::SpriteImportResolution SetNewSpriteFromBitmap(int slot, System::Draw
     int flags;
     Common::Bitmap *tempsprite = CreateNativeBitmap(bmp, spriteImportMethod, remapColours,
         useRoomBackgroundColours, alphaChannel, &flags);
-    thisgame.SpriteInfos[slot].Flags = flags;
 
-	SetNewSprite(slot, tempsprite);
+	SetNewSprite(slot, tempsprite, flags);
 
 	return AGS::Types::SpriteImportResolution::Real;
 }

Common/ac/spritecache.cpp
@@ -103,22 +103,26 @@ void SpriteCache::Reset()
     _lockedSize = 0;
 }
 
-void SpriteCache::SetSprite(sprkey_t index, Bitmap *sprite)
+bool SpriteCache::SetSprite(sprkey_t index, Bitmap *sprite, int flags)
 {
     if (index < 0 || EnlargeTo(index) != index)
     {
         Debug::Printf(kDbgGroup_SprCache, kDbgMsg_Error, "SetSprite: unable to use index %d", index);
-        return;
+        return false;
     }
     if (!sprite)
     {
         Debug::Printf(kDbgGroup_SprCache, kDbgMsg_Error, "SetSprite: attempt to assign nullptr to index %d", index);
-        return;
+        return false;
     }
     _spriteData[index].Image = sprite;
     _spriteData[index].Flags = SPRCACHEFLAG_LOCKED; // NOT from asset file
     _spriteData[index].Size = 0;
+    _sprInfos[index].Flags = flags;
+    _sprInfos[index].Width = sprite->GetWidth();
+    _sprInfos[index].Height = sprite->GetHeight();
     SprCacheLog("SetSprite: (external) %d", index);
+    return true;
 }
 
 void SpriteCache::SetEmptySprite(sprkey_t index, bool as_asset)

Common/ac/spritecache.h
@@ -105,7 +105,8 @@ public:
     // Deletes all data and resets cache to the clear state
     void        Reset();
     // Assigns new sprite for the given index; this sprite won't be auto disposed
-    void        SetSprite(sprkey_t index, Common::Bitmap *);
+    // flags are SPF_* constants that define sprite's behavior in game.
+    bool        SetSprite(sprkey_t index, Common::Bitmap *, int flags = 0);
     // Assigns new sprite for the given index, remapping it to sprite 0;
     // optionally marks it as an asset placeholder
     void        SetEmptySprite(sprkey_t index, bool as_asset);

Editor/AGS.Native/NativeMethods.cpp
@@ -44,7 +44,6 @@ extern void drawSprite(int hdc, int x,int y, int spriteNum, bool flipImage);
 extern void drawSpriteStretch(int hdc, int x,int y, int width, int height, int spriteNum, bool flipImage);
 extern void drawBlockOfColour(int hdc, int x,int y, int width, int height, int colNum);
 extern void drawViewLoop (int hdc, ViewLoop^ loopToDraw, int x, int y, int size, int cursel);
-extern void SetNewSpriteFromHBitmap(int slot, int hBmp);
 extern AGS::Types::SpriteImportResolution SetNewSpriteFromBitmap(int slot, Bitmap^ bmp, int spriteImportMethod, bool remapColours, bool useRoomBackgroundColours, bool alphaChannel);
 extern int GetSpriteAsHBitmap(int spriteSlot);
 extern Bitmap^ getSpriteAsBitmap32bit(int spriteNum, int width, int height);

commit 833e3cb6e0b04200675f2b42087df979dadf88d6

AGS.Native: fixed crop_sprite_edges copies flags from wrong sprite idx
Was broken by f2a922a9cefd40337502bde734503605a76550db

Editor/AGS.Native/agsnative.cpp
@@ -350,12 +350,13 @@ int crop_sprite_edges(int numSprites, int *sprites, bool symmetric) {
   }
 
   for (aa = 0; aa < numSprites; aa++) {
-    AGSBitmap *sprit = get_sprite(sprites[aa]);
+    int sprnum = sprites[aa];
+    AGSBitmap *sprit = get_sprite(sprnum);
     // create a new, smaller sprite and copy across
 	std::unique_ptr<AGSBitmap> newsprit(new AGSBitmap(newWidth, newHeight, sprit->GetColorDepth()));
     newsprit->Blit(sprit, left, top, 0, 0, newWidth, newHeight);
     // set new image and keep old flags
-    spriteset.SetSprite(sprites[aa], std::move(newsprit), thisgame.SpriteInfos[aa].Flags);
+    spriteset.SetSprite(sprnum, std::move(newsprit), thisgame.SpriteInfos[sprnum].Flags);
   }
 
   spritesModified = true;
adventuregamestudio/ags

commit 5929e7ef2a3422b31ed18e56a0ae6c40475b698f

Engine: simplified sound clip loading code

Engine/ac/global_audio.cpp
@@ -374,32 +374,16 @@ void PlayMP3File (const char *filename) {
     int useChan = prepare_for_new_music ();
     bool doLoop = (play.music_repeat > 0);
 
-    SOUNDCLIP *clip = my_load_ogg(asset_name, doLoop);
-    int sound_type = 0;
-    
-    if (!clip) {
-        sound_type = MUS_OGG;
-    }
-
-    if (!clip)
-    {
-        clip = my_load_mp3(asset_name, doLoop);
-        sound_type = MUS_MP3;
-    }
-
+    std::unique_ptr<SOUNDCLIP> clip(load_sound_clip(asset_name, "", doLoop));
     if (clip) {
         if (clip->play()) {
             clip->set_volume255(150);
-            AudioChans::SetChannel(useChan, std::unique_ptr<SOUNDCLIP>(clip));
-            current_music_type = sound_type;
+            AudioChans::SetChannel(useChan, std::move(clip));
+            current_music_type = clip->soundType;
             play.cur_music_number = 1000;
             // save the filename (if it's not what we were supplied with)
             if (filename != &play.playmp3file_name[0])
                 snprintf(play.playmp3file_name, sizeof(play.playmp3file_name), "%s", filename);
-        }
-        else {
-            delete clip;
-            clip = nullptr;
         }
     }
 
@@ -409,7 +393,6 @@ void PlayMP3File (const char *filename) {
     }
 
     post_new_music_check();
-
     update_music_volume();
 }
 
@@ -504,35 +487,37 @@ static bool play_voice_clip_on_channel(const String &voice_name)
 {
     stop_and_destroy_channel(SCHAN_SPEECH);
 
-    String asset_name = voice_name;
-    asset_name.Append(".wav");
-    SOUNDCLIP *speechmp3 = my_load_wave(get_voice_over_assetpath(asset_name), false);
-
-    if (speechmp3 == nullptr) {
-        asset_name.ReplaceMid(asset_name.GetLength() - 3, 3, "ogg");
-        speechmp3 = my_load_ogg(get_voice_over_assetpath(asset_name), false);
+    // TODO: perhaps a better algorithm, allow any extension / sound format?
+    // e.g. make a hashmap matching a voice name to a asset name
+    std::array<const char*, 3> exts = {{ "mp3", "ogg", "wav" }};
+    AssetPath apath = get_voice_over_assetpath(voice_name);
+    bool found = false;
+    for (auto *ext : exts)
+    {
+        apath.Name.Format("%s.%s", voice_name.GetCStr(), ext);
+        found = AssetMgr->DoesAssetExist(apath);
+        if (found)
+            break;
     }
 
-    if (speechmp3 == nullptr) {
-        asset_name.ReplaceMid(asset_name.GetLength() - 3, 3, "mp3");
-        speechmp3 = my_load_mp3(get_voice_over_assetpath(asset_name), false);
+    if (!found) {
+        debug_script_warn("Speech file not found: '%s'", voice_name.GetCStr());
+        return false;
     }
 
-    if (speechmp3 != nullptr) {
-        speechmp3->set_volume255(play.speech_volume);
-        if (!speechmp3->play()) {
-            // not assigned to a channel, so clean up manually.
-            delete speechmp3;
-            speechmp3 = nullptr;
-        }
+    std::unique_ptr<SOUNDCLIP> voice_clip(load_sound_clip(apath, "", false));
+    if (voice_clip != nullptr) {
+        voice_clip->set_volume255(play.speech_volume);
+        if (!voice_clip->play())
+            voice_clip.reset();
     }
 
-    if (speechmp3 == nullptr) {
+    if (!voice_clip) {
         debug_script_warn("Speech load failure: '%s'", voice_name.GetCStr());
         return false;
     }
 
-    AudioChans::SetChannel(SCHAN_SPEECH, std::unique_ptr<SOUNDCLIP>(speechmp3));
+    AudioChans::SetChannel(SCHAN_SPEECH, std::move(voice_clip));
     return true;
 }
 

Engine/media/audio/audio.cpp
@@ -241,27 +241,25 @@ SOUNDCLIP *load_sound_clip(ScriptAudioClip *audioClip, bool repeat)
 
     SOUNDCLIP *soundClip = nullptr;
     AssetPath asset_name = get_audio_clip_assetpath(audioClip->bundlingType, audioClip->fileName);
+    const char *ext = "";
     switch (audioClip->fileType)
     {
     case eAudioFileOGG:
-        soundClip = my_load_ogg(asset_name, repeat);
-        break;
+        ext = "ogg"; break;
     case eAudioFileMP3:
-        soundClip = my_load_mp3(asset_name, repeat);
-        break;
+        ext = "mp3"; break;
     case eAudioFileWAV:
     case eAudioFileVOC:
-        soundClip = my_load_wave(asset_name, repeat);
-        break;
+        ext = "wav"; break;
     case eAudioFileMIDI:
-        soundClip = my_load_midi(asset_name, repeat);
-        break;
+        ext = "mid"; break;
     case eAudioFileMOD:
-        soundClip = my_load_mod(asset_name, repeat);
-        break;
+        ext = "mod"; break;
     default:
         quitprintf("AudioClip.Play: invalid audio file type encountered: %d", audioClip->fileType);
     }
+
+    soundClip = load_sound_clip(asset_name, ext, repeat);
     if (soundClip != nullptr)
     {
         soundClip->set_volume100(audioClip->defaultVolume);

Engine/media/audio/sound.cpp
@@ -97,7 +97,7 @@ void soundcache_clear()
     SndCache.Clear();
 }
 
-static SOUNDCLIP *my_load_clip(const AssetPath &apath, const char *extension_hint, bool loop)
+SOUNDCLIP *load_sound_clip(const AssetPath &apath, const char *extension_hint, bool loop)
 {
     size_t asset_size;
     std::unique_ptr<Stream> s_in;
@@ -146,29 +146,4 @@ static SOUNDCLIP *my_load_clip(const AssetPath &apath, const char *extension_hin
     clip->soundType = sound_type;
     clip->lengthMs = lengthMs;
     return clip;
-}
-
-SOUNDCLIP *my_load_wave(const AssetPath &asset_name, bool loop)
-{
-    return my_load_clip(asset_name, "WAV", loop);
-}
-
-SOUNDCLIP *my_load_mp3(const AssetPath &asset_name, bool loop)
-{
-    return my_load_clip(asset_name, "MP3", loop);
-}
-
-SOUNDCLIP *my_load_ogg(const AssetPath &asset_name, bool loop)
-{
-    return my_load_clip(asset_name, "OGG", loop);
-}
-
-SOUNDCLIP *my_load_midi(const AssetPath &asset_name, bool loop)
-{
-    return my_load_clip(asset_name, "MIDI", loop);
-}
-
-SOUNDCLIP *my_load_mod(const AssetPath &asset_name, bool loop)
-{
-    return my_load_clip(asset_name, "MOD", loop);
 }

Engine/media/audio/sound.h
@@ -31,10 +31,7 @@ const size_t DEFAULT_SOUNDCACHESIZE_KB = 1024u * 32; // 32 MB
 // * max_cachesize - sound cache limit, in bytes
 void soundcache_set_rules(size_t max_loadatonce, size_t max_cachesize);
 void soundcache_clear();
-SOUNDCLIP *my_load_wave(const AssetPath &asset_name, bool loop);
-SOUNDCLIP *my_load_mp3(const AssetPath &asset_name, bool loop);
-SOUNDCLIP *my_load_ogg(const AssetPath &asset_name, bool loop);
-SOUNDCLIP *my_load_midi(const AssetPath &asset_name, bool loop);
-SOUNDCLIP *my_load_mod(const AssetPath &asset_name, bool loop);
+
+SOUNDCLIP *load_sound_clip(const AssetPath &apath, const char *extension_hint, bool loop);
 
 #endif // __AC_SOUND_H

Engine/plugin/agsplugin.cpp
@@ -554,27 +554,35 @@ void IAGSEngine::PlaySoundChannel (int32 channel, int32 soundType, int32 volume,
     // TODO: find out how engine was supposed to decide on where to load the sound from
     AssetPath asset_name(filename, "audio");
 
+    const char *ext = "";
     switch (soundType)
     {
     case PSND_WAVE:
-        newcha = my_load_wave(asset_name, (loop != 0)); break;
+        ext = "wav"; break;
     case PSND_MP3STREAM:
     case PSND_MP3STATIC:
-        newcha = my_load_mp3(asset_name, (loop != 0)); break;
+        ext = "mp3"; break;
     case PSND_OGGSTREAM:
     case PSND_OGGSTATIC:
-        newcha = my_load_ogg(asset_name, (loop != 0)); break;
+        ext = "ogg"; break;
     case PSND_MIDI:
         if (play.silent_midi != 0 || current_music_type == MUS_MIDI)
         {
             debug_script_warn("IAGSEngine::PlaySoundChannel: MIDI already in use");
             return;
         }
-        newcha = my_load_midi(asset_name, (loop != 0)); break;
+        ext = "mid"; break;
     case PSND_MOD:
-        newcha = my_load_mod(asset_name, (loop != 0)); break;
+        ext = "mod"; break;
     default:
         debug_script_warn("IAGSEngine::PlaySoundChannel: unknown sound type %d", soundType);
+        return;
+    }
+
+    newcha = load_sound_clip(asset_name, ext, (loop != 0));
+    if (!newcha)
+    {
+        debug_script_warn("IAGSEngine::PlaySoundChannel: failed to load %s", filename);
         return;
     }
 

commit f1382f646fcd2dabfd7bc94ecc028d932fc46bff

Engine: fixed PlayMP3File() crashing with null ptr access
Was broken by 5929e7ef2a3422b31ed18e56a0ae6c40475b698f

Engine/ac/global_audio.cpp
@@ -368,26 +368,21 @@ void PlayMP3File (const char *filename)
     debug_script_log("PlayMP3File %s", filename);
 
     AssetPath asset_name(filename, "audio");
-
-    int useChan = prepare_for_new_music ();
-    bool doLoop = (play.music_repeat > 0);
-
-    std::unique_ptr<SOUNDCLIP> clip(load_sound_clip(asset_name, "", doLoop));
-    if (clip) {
-        if (clip->play()) {
-            clip->set_volume255(150);
-            AudioChans::SetChannel(useChan, std::move(clip));
-            current_music_type = clip->soundType;
-            play.cur_music_number = 1000;
-            play.playmp3file_name = filename;
-        }
+    const bool do_loop = (play.music_repeat > 0);
+    std::unique_ptr<SOUNDCLIP> clip(load_sound_clip(asset_name, "", do_loop));
+    if (!clip)
+    {
+        debug_script_warn("PlayMP3File: music file '%s' not found or cannot be read", filename);
+        return;
     }
 
-    if (!clip) {
-        AudioChans::SetChannel(useChan, nullptr);
-        debug_script_warn ("PlayMP3File: file '%s' not found or cannot play", filename);
-    }
-
+    const int use_chan = prepare_for_new_music();
+    current_music_type = clip->soundType;
+    play.cur_music_number = 1000;
+    play.playmp3file_name = filename;
+    clip->set_volume255(150); // CHECKME: why 150?...
+    clip->play();
+    AudioChans::SetChannel(use_chan, std::move(clip));
     post_new_music_check();
     update_music_volume();
 }
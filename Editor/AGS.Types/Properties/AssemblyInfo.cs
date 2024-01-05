5f4d95477a8c9dc44665fc23d1403142a93e9c9c
Ivan Mogilko <ikm_spb@yahoo.com>
2024-01-05 16:46:00 +0300

    Updated code headers to year 2024

--- Editor/AGS.Types/Properties/AssemblyInfo.cs
+++ Editor/AGS.Types/Properties/AssemblyInfo.cs
@@ -22,9 +22,9 @@ namespace AGS.Types
     public class Version
     {
         public static readonly bool IS_BETA_VERSION = true;
-        public const string AGS_EDITOR_DATE = "December 2023";
+        public const string AGS_EDITOR_DATE = "January 2024";
         public const string AGS_EDITOR_FRIENDLY_VERSION = "3.6.1";
         public const string AGS_EDITOR_VERSION = "3.6.1.15";
-        public const string AGS_EDITOR_COPYRIGHT = "Copyright © 2006-2011 Chris Jones and 2011-2023 others.";
+        public const string AGS_EDITOR_COPYRIGHT = "Copyright © 2006-2011 Chris Jones and 2011-2024 others.";
     }
 }

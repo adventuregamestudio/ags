adventuregamestudio/ags

commit 33a7a29d35f53bf0d389eb69f85bbb68bd172dd3

Engine: mark transition screenshot screen as opaque texture
This is a minor perf optimization.

+ added couple of TODO comments.

Engine/ac/screen.cpp
@@ -104,8 +104,7 @@ IDriverDependantBitmap* prepare_screen_for_transition_in()
         delete saved_viewport_bitmap;
         saved_viewport_bitmap = clippedBuffer;
     }
-    IDriverDependantBitmap *ddb = gfxDriver->CreateDDBFromBitmap(saved_viewport_bitmap, false);
-    return ddb;
+    return gfxDriver->CreateDDBFromBitmap(saved_viewport_bitmap, false, true);
 }
 
 //=============================================================================

Engine/ac/draw.cpp
@@ -1060,7 +1060,8 @@ IDriverDependantBitmap* recycle_render_target(IDriverDependantBitmap *ddb, int w
     return gfxDriver->CreateRenderTargetDDB(width, height, col_depth, opaque);
 }
 
-void sync_object_texture(ObjTexture &obj, bool has_alpha = false , bool opaque = false)
+// FIXME: make has_alpha and opaque properties of ObjTexture?!
+static void sync_object_texture(ObjTexture &obj, bool has_alpha = false , bool opaque = false)
 {
     Bitmap *use_bmp = obj.Bmp.get() ? obj.Bmp.get() : spriteset[obj.SpriteID];
     obj.Ddb = recycle_ddb_sprite(obj.Ddb, obj.SpriteID, use_bmp, has_alpha, opaque);

Engine/gfx/graphicsdriver.h
@@ -139,6 +139,10 @@ public:
   virtual void DestroyDDB(IDriverDependantBitmap* bitmap) = 0;
 
   // Get shared texture from cache, or create from bitmap and assign ID
+  // FIXME: opaque should be either texture data's flag, - in which case same sprite_id
+  // will be either opaque or not opaque, - or DDB's flag, but in that case it cannot
+  // be applied to the shared texture data. Currently it's possible to share same
+  // texture data, but update it with different "opaque" values, which breaks logic.
   virtual IDriverDependantBitmap *GetSharedDDB(uint32_t sprite_id,
       Common::Bitmap *bitmap = nullptr, bool hasAlpha = true, bool opaque = false) = 0;
   virtual void UpdateSharedDDB(uint32_t sprite_id, Common::Bitmap *bitmap = nullptr, bool hasAlpha = true, bool opaque = false) = 0;

commit 55f1d0376ef583b5f7d767e31c83e190c005246f

Engine: fixed Dissolve transition
Was broken by 33a7a29d35f53bf0d389eb69f85bbb68bd172dd3, because I did not correctly understood uses of this DDB.

Engine/ac/event.cpp
@@ -291,7 +291,10 @@ void process_event(const EventHappened *evp) {
             if (game.color_depth == 1)
                 quit("!Cannot use crossfade screen transition in 256-colour games");
 
-            IDriverDependantBitmap *ddb = prepare_screen_for_transition_in();
+            // TODO: crossfade does not need a screen with transparency, it should be opaque;
+            // but Software renderer cannot alpha-blend non-masked sprite at the moment,
+            // see comment to drawing opaque sprite in SDLRendererGraphicsDriver!
+            IDriverDependantBitmap *ddb = prepare_screen_for_transition_in(false /* transparent */);
             for (int alpha = 254; alpha > 0; alpha -= 16)
             {
                 // do the crossfade
@@ -321,7 +324,7 @@ void process_event(const EventHappened *evp) {
             int aa,bb,cc;
             RGB interpal[256];
 
-            IDriverDependantBitmap *ddb = prepare_screen_for_transition_in();
+            IDriverDependantBitmap *ddb = prepare_screen_for_transition_in(false /* transparent */);
             for (aa=0;aa<16;aa++) {
                 // merge the palette while dithering
                 if (game.color_depth == 1) 

Engine/ac/screen.cpp
@@ -87,7 +87,7 @@ void current_fade_out_effect () {
     }
 }
 
-IDriverDependantBitmap* prepare_screen_for_transition_in()
+IDriverDependantBitmap* prepare_screen_for_transition_in(bool opaque)
 {
     if (saved_viewport_bitmap == nullptr)
         quit("Crossfade: buffer is null attempting transition");
@@ -107,7 +107,7 @@ IDriverDependantBitmap* prepare_screen_for_transition_in()
         delete saved_viewport_bitmap;
         saved_viewport_bitmap = clippedBuffer;
     }
-    return gfxDriver->CreateDDBFromBitmap(saved_viewport_bitmap, false, true);
+    return gfxDriver->CreateDDBFromBitmap(saved_viewport_bitmap, false, opaque);
 }
 
 //=============================================================================

Engine/ac/screen.h
@@ -24,7 +24,7 @@ namespace AGS { namespace Engine { class IDriverDependantBitmap; } }
 void fadein_impl(PALETTE p, int speed);
 void fadeout_impl(int spdd);
 void current_fade_out_effect ();
-AGS::Engine::IDriverDependantBitmap* prepare_screen_for_transition_in();
+AGS::Engine::IDriverDependantBitmap* prepare_screen_for_transition_in(bool opaque);
 
 // Screenshot made in the last room, used during some of the transition effects
 extern AGS::Common::Bitmap *saved_viewport_bitmap;
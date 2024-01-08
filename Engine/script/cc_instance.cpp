adventuregamestudio/ags

commit 973845a15ed9726956895161c8847bc668c3e787

Engine: safety check for fixups with out of range code reference

Engine/script/cc_instance.cpp
@@ -1834,14 +1834,20 @@ bool ccInstance::CreateRuntimeCodeFixups(const ccScript *scri)
     memset(code_fixups, 0, scri->codesize);
     for (int i = 0; i < scri->numfixups; ++i)
     {
+        const int32_t fixup = scri->fixups[i];
+        if (fixup < 0 || fixup >= scri->codesize)
+        {
+            cc_error_fixups(scri, SIZE_MAX, "bad fixup at %d (fixup type %d, bytecode pos %d, bytecode range is 0..%d)",
+                i,  scri->fixuptypes[i], fixup, scri->codesize);
+            return false;
+        }
+
         if (scri->fixuptypes[i] == FIXUP_DATADATA)
         {
             continue;
         }
 
-        int32_t fixup = scri->fixups[i];
         code_fixups[fixup] = scri->fixuptypes[i];
-
         switch (scri->fixuptypes[i])
         {
         case FIXUP_GLOBALDATA:

commit 9d86d5ab1a6e84115c33ffbfc8a96185e6b9fd63

Engine: fixed bytecode fixup assertion done for unapplicable case
Was broken by 973845a15ed9726956895161c8847bc668c3e787

Engine/script/cc_instance.cpp
@@ -1834,17 +1834,17 @@ bool ccInstance::CreateRuntimeCodeFixups(const ccScript *scri)
     memset(code_fixups, 0, scri->codesize);
     for (int i = 0; i < scri->numfixups; ++i)
     {
+        if (scri->fixuptypes[i] == FIXUP_DATADATA)
+        {
+            continue;
+        }
+
         const int32_t fixup = scri->fixups[i];
         if (fixup < 0 || fixup >= scri->codesize)
         {
             cc_error_fixups(scri, SIZE_MAX, "bad fixup at %d (fixup type %d, bytecode pos %d, bytecode range is 0..%d)",
                 i,  scri->fixuptypes[i], fixup, scri->codesize);
             return false;
-        }
-
-        if (scri->fixuptypes[i] == FIXUP_DATADATA)
-        {
-            continue;
         }
 
         code_fixups[fixup] = scri->fixuptypes[i];
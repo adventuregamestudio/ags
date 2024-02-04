6ad3e6ef63a4ef8995a0fd0e678eda629b52c635
Ivan Mogilko <ikm_spb@yahoo.com>
2024-02-04 17:26:04 +0300

    Engine: restored proper character name formatting (broken by merge)

--- Engine/ac/global_character.cpp
+++ Engine/ac/global_character.cpp
@@ -77,7 +77,7 @@ int GetCharacterWidth(int ww) {
             (char1->frame >= views[char1->view].loops[char1->loop].numFrames))
         {
             debug_script_warn("GetCharacterWidth: Character %s has invalid frame: view %d, loop %d, frame %d",
-                char1->scrname, char1->view + 1, char1->loop, char1->frame);
+                char1->scrname.GetCStr(), char1->view + 1, char1->loop, char1->frame);
             return 4;
         }
 
@@ -97,7 +97,7 @@ int GetCharacterHeight(int charid) {
             (char1->frame >= views[char1->view].loops[char1->loop].numFrames))
         {
             debug_script_warn("GetCharacterHeight: Character %s has invalid frame: view %d, loop %d, frame %d",
-                char1->scrname, char1->view + 1, char1->loop, char1->frame);
+                char1->scrname.GetCStr(), char1->view + 1, char1->loop, char1->frame);
             return 2;
         }
 

adventuregamestudio/ags

commit 40faa6ce746c77ab6fb4599460dc8eea0726562d

AGS.Native: removed cstretch.cpp
It looks like there has been a duplicate of Allegro's stretch blits for the purpose of making them work in the older versions of MSVS (at least according to the CJ's comments found in the code).

Editor/AGS.Native/agsnative.cpp
@@ -64,20 +64,6 @@ typedef AGS::Common::PBitmap PBitmap;
 typedef AGS::Common::Error AGSError;
 typedef AGS::Common::HError HAGSError;
 
-// TODO: do something with this later
-// (those are from 'cstretch' unit)
-extern void Cstretch_blit(BITMAP *src, BITMAP *dst, int sx, int sy, int sw, int sh, int dx, int dy, int dw, int dh);
-extern void Cstretch_sprite(BITMAP *dst, BITMAP *src, int x, int y, int w, int h);
-
-inline void Cstretch_blit(Common::Bitmap *src, Common::Bitmap *dst, int sx, int sy, int sw, int sh, int dx, int dy, int dw, int dh)
-{
-	Cstretch_blit(src->GetAllegroBitmap(), dst->GetAllegroBitmap(), sx, sy, sw, sh, dx, dy, dw, dh);
-}
-inline void Cstretch_sprite(Common::Bitmap *dst, Common::Bitmap *src, int x, int y, int w, int h)
-{
-	Cstretch_sprite(dst->GetAllegroBitmap(), src->GetAllegroBitmap(), x, y, w, h);
-}
-
 void save_room_file(RoomStruct &rs, const AGSString &path);
 
 int mousex = 0, mousey = 0;
@@ -804,7 +790,7 @@ void draw_area_mask(RoomStruct *roomptr, Common::Bitmap *ds, RoomAreaMask maskTy
 	}
 	else
 	{
-		Cstretch_sprite(ds, source, 0, 0, dest_width, dest_height);
+        ds->StretchBlt(source, RectWH(0, 0, dest_width, dest_height));
 	}
 }
 
@@ -863,9 +849,9 @@ void draw_room_background(void *roomvoidptr, int hdc, int x, int y, int bgnum, f
         srcHeight = roomBkgBuffer->GetHeight() - srcY;
       }
     }
-
-		Cstretch_blit(roomBkgBuffer.get(), drawBuffer.get(), srcX, srcY, srcWidth, srcHeight,
-            x, y, (int)(srcWidth * scaleFactor), (int)(srcHeight * scaleFactor));
+        drawBuffer->StretchBlt(roomBkgBuffer.get(),
+            RectWH(srcX, srcY, srcWidth, srcHeight),
+            RectWH(x, y, (int)(srcWidth * scaleFactor), (int)(srcHeight * scaleFactor)));
 	}
 	else {
 		drawBlockScaledAt(hdc, srcBlock.get(), x, y, scaleFactor);
@@ -1057,8 +1043,7 @@ static void doDrawViewLoop (int hdc, int numFrames, ViewFrame *frames, int x, in
       toblt = flipped;
       freeBlock = true;
     }
-    //->StretchBlt(toblt, todraw, 0, 0, toblt->GetWidth(), toblt->GetHeight(), size*i, 0, neww, newh);
-	Cstretch_sprite(todraw, toblt, size*i, 0, neww, newh);
+    todraw->StretchBlt(toblt, RectWH(size*i, 0, neww, newh));
     if (freeBlock)
       delete toblt;
     if (i < numFrames-1) {
@@ -1732,7 +1717,7 @@ void DrawSpriteToBuffer(int sprNum, int x, int y, float scale) {
 	}
 	else
 	{
-		Cstretch_sprite(drawBuffer.get(), imageToDraw, x, y, drawWidth, drawHeight);
+        drawBuffer->StretchBlt(imageToDraw, RectWH(x, y, drawWidth, drawHeight));
 	}
 
 	if (imageToDraw != todraw)
@@ -2309,7 +2294,9 @@ void import_area_mask(void *roomptr, int maskType, System::Drawing::Bitmap ^bmp)
 	if (mask->GetWidth() != importedImage->GetWidth())
 	{
 		// allow them to import a double-size or half-size mask, adjust it as appropriate
-		Cstretch_blit(importedImage, mask, 0, 0, importedImage->GetWidth(), importedImage->GetHeight(), 0, 0, mask->GetWidth(), mask->GetHeight());
+        mask->StretchBlt(importedImage,
+            RectWH(0, 0, importedImage->GetWidth(), importedImage->GetHeight()),
+            RectWH(0, 0, mask->GetWidth(), mask->GetHeight()));
 	}
 	else
 	{
@@ -2449,7 +2436,9 @@ System::Drawing::Bitmap^ ConvertBlockToBitmap32(Common::Bitmap *todraw, int widt
           delete tempBlock;
           return nullptr; // out of mem?
       }
-	  Cstretch_blit(tempBlock, newBlock, 0, 0, todraw->GetWidth(), todraw->GetHeight(), 0, 0, width, height);
+      newBlock->StretchBlt(tempBlock,
+          RectWH(0, 0, todraw->GetWidth(), todraw->GetHeight()),
+          RectWH(0, 0, width, height));
 	  delete tempBlock;
 	  tempBlock = newBlock;
   }

Editor/AGS.Native/NativeDLL.vcxproj
@@ -60,7 +60,7 @@
   <ItemDefinitionGroup>
     <Link>
       <AdditionalLibraryDirectories>$(SolutionDir)\.lib\$(Configuration)_MD;$(SolutionDir)\..\Windows\lib;%(AdditionalLibraryDirectories)</AdditionalLibraryDirectories>
-    </Link>  
+    </Link>
   </ItemDefinitionGroup>
   <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
     <PreBuildEvent>
@@ -238,7 +238,6 @@
     <ClCompile Include="..\scintilla\win32\ScintillaWin.cxx" />
     <ClCompile Include="agsicons.cpp" />
     <ClCompile Include="agsnative.cpp" />
-    <ClCompile Include="cstretch.cpp" />
     <ClCompile Include="MSSCCI.cpp" />
     <ClCompile Include="NativeMethods.cpp" />
     <ClCompile Include="ScriptCompiler.cpp" />

Editor/AGS.Native/NativeDLL.vcxproj.filters
@@ -55,9 +55,6 @@
     <ClCompile Include="..\Native\color_native.cpp">
       <Filter>Source Files</Filter>
     </ClCompile>
-    <ClCompile Include="cstretch.cpp">
-      <Filter>Source Files</Filter>
-    </ClCompile>
     <ClCompile Include="MSSCCI.cpp">
       <Filter>Source Files</Filter>
     </ClCompile>

Editor/AGS.Native/cstretch.cpp
@@ -1,465 +1,0 @@
-#pragma unmanaged
-/*         ______   ___    ___
- *        /\  _  \ /\_ \  /\_ \
- *        \ \ \L\ \\//\ \ \//\ \      __     __   _ __   ___
- *         \ \  __ \ \ \ \  \ \ \   /'__`\ /'_ `\/\`'__\/ __`\
- *          \ \ \/\ \ \_\ \_ \_\ \_/\  __//\ \L\ \ \ \//\ \L\ \
- *           \ \_\ \_\/\____\/\____\ \____\ \____ \ \_\\ \____/
- *            \/_/\/_/\/____/\/____/\/____/\/___L\ \/_/ \/___/
- *                                           /\____/
- *                                           \_/__/
- *
- *      Bitmap stretching functions.
- *
- *      By Michael Bukin.
- *      Fixed and improved in 2007 by Trent Gamblin.
- *
- *      See readme.txt for copyright information.
- */
-
-
-#include "allegro.h"
-
-
-
-/* Information for stretching line */
-static struct {
-   int xcstart; /* x counter start */
-   int sxinc; /* amount to increment src x every time */
-   int xcdec; /* amount to deccrement counter by, increase sptr when this reaches 0 */
-   int xcinc; /* amount to increment counter by when it reaches 0 */
-   int linesize; /* size of a whole row of pixels */
-} _al_stretch;
-
-
-
-/* Stretcher macros */
-#define DECLARE_STRETCHER(type, size, put, get) \
-   int xc = _al_stretch.xcstart; \
-   uintptr_t dend = dptr + _al_stretch.linesize; \
-   ASSERT(dptr); \
-   ASSERT(sptr); \
-   for (; dptr < dend; dptr += size, sptr += _al_stretch.sxinc) { \
-      put(dptr, get((type*)sptr)); \
-      if (xc <= 0) { \
-	 sptr += size; \
-	 xc += _al_stretch.xcinc; \
-      } \
-      else \
-	 xc -= _al_stretch.xcdec; \
-   }
-
-
-
-#define DECLARE_MASKED_STRETCHER(type, size, put, get, mask) \
-   int xc = _al_stretch.xcstart; \
-   uintptr_t dend = dptr + _al_stretch.linesize; \
-   ASSERT(dptr); \
-   ASSERT(sptr); \
-   for (; dptr < dend; dptr += size, sptr += _al_stretch.sxinc) { \
-      int color = get((type*)sptr); \
-      if (color != mask) \
-	 put(dptr, get((type*)sptr)); \
-      if (xc <= 0) { \
-	 sptr += size; \
-	 xc += _al_stretch.xcinc; \
-      } \
-      else \
-	 xc -= _al_stretch.xcdec; \
-   }
-
-
-
-#ifdef ALLEGRO_GFX_HAS_VGA
-/*
- * Mode-X line stretcher.
- */
-static void stretch_linex(uintptr_t dptr, unsigned char *sptr)
-{
-   int plane;
-   int first_xc = _al_stretch.xcstart;
-   int dw = _al_stretch.linesize;
-
-   ASSERT(dptr);
-   ASSERT(sptr);
-
-   for (plane = 0; plane < 4; plane++) {
-      int xc = first_xc;
-      unsigned char *s = sptr;
-      uintptr_t d = dptr / 4;
-      uintptr_t dend = (dptr + dw) / 4;
-
-      outportw(0x3C4, (0x100 << (dptr & 3)) | 2);
-
-      for (; d < dend; d++, s += 4 * _al_stretch.sxinc) {
-	 bmp_write8(d, *s);
-	 if (xc <= 0) s++, xc += _al_stretch.xcinc;
-	 else xc -= _al_stretch.xcdec;
-	 if (xc <= 0) s++, xc += _al_stretch.xcinc;
-	 else xc -= _al_stretch.xcdec;
-	 if (xc <= 0) s++, xc += _al_stretch.xcinc;
-	 else xc -= _al_stretch.xcdec;
-	 if (xc <= 0) s++, xc += _al_stretch.xcinc;
-	 else xc -= _al_stretch.xcdec;
-      }
-
-      /* Move to the beginning of next plane.  */
-      if (first_xc <= 0) {
-	  sptr++;
-	  first_xc += _al_stretch.xcinc;
-      }
-      else
-	 first_xc -= _al_stretch.xcdec;
-
-      dptr++;
-      sptr += _al_stretch.sxinc;
-      dw--;
-   }
-}
-
-/*
- * Mode-X masked line stretcher.
- */
-static void stretch_masked_linex(uintptr_t dptr, unsigned char *sptr)
-{
-   int plane;
-   int dw = _al_stretch.linesize;
-   int first_xc = _al_stretch.xcstart;
-
-   ASSERT(dptr);
-   ASSERT(sptr);
-
-   for (plane = 0; plane < 4; plane++) {
-      int xc = first_xc;
-      unsigned char *s = sptr;
-      uintptr_t d = dptr / 4;
-      uintptr_t dend = (dptr + dw) / 4;
-
-      outportw(0x3C4, (0x100 << (dptr & 3)) | 2);
-
-      for (; d < dend; d++, s += 4 * _al_stretch.sxinc) {
-	 unsigned long color = *s;
-	 if (color != 0)
-	    bmp_write8(d, color);
-	 if (xc <= 0) s++, xc += _al_stretch.xcinc;
-	 else xc -= _al_stretch.xcdec;
-	 if (xc <= 0) s++, xc += _al_stretch.xcinc;
-	 else xc -= _al_stretch.xcdec;
-	 if (xc <= 0) s++, xc += _al_stretch.xcinc;
-	 else xc -= _al_stretch.xcdec;
-	 if (xc <= 0) s++, xc += _al_stretch.xcinc;
-	 else xc -= _al_stretch.xcdec;
-      }
-
-      /* Move to the beginning of next plane.  */
-      if (first_xc <= 0) {
-	 sptr++;
-	 first_xc += _al_stretch.xcinc;
-      }
-      else
-	 first_xc -= _al_stretch.xcdec;
-
-      dptr++;
-      sptr += _al_stretch.sxinc;
-      dw--;
-   }
-}
-#endif
-
-
-
-#ifdef ALLEGRO_COLOR8
-static void stretch_line8(uintptr_t dptr, unsigned char *sptr)
-{
-   DECLARE_STRETCHER(unsigned char, 1, bmp_write8, *);
-}
-
-static void stretch_masked_line8(uintptr_t dptr, unsigned char *sptr)
-{
-   DECLARE_MASKED_STRETCHER(unsigned char, 1, bmp_write8, *, 0);
-}
-#endif
-
-
-
-#ifdef ALLEGRO_COLOR16
-static void stretch_line15(uintptr_t dptr, unsigned char* sptr)
-{
-   DECLARE_STRETCHER(unsigned short, 2, bmp_write15, *);
-}
-
-static void stretch_line16(uintptr_t dptr, unsigned char* sptr)
-{
-   DECLARE_STRETCHER(unsigned short, 2, bmp_write16, *);
-}
-
-static void stretch_masked_line15(uintptr_t dptr, unsigned char* sptr)
-{
-   DECLARE_MASKED_STRETCHER(unsigned short, 2, bmp_write15, *, MASK_COLOR_15);
-}
-
-static void stretch_masked_line16(uintptr_t dptr, unsigned char* sptr)
-{
-   DECLARE_MASKED_STRETCHER(unsigned short, 2, bmp_write16, *, MASK_COLOR_16);
-}
-#endif
-
-
-
-#ifdef ALLEGRO_COLOR24
-static void stretch_line24(uintptr_t dptr, unsigned char* sptr)
-{
-   DECLARE_STRETCHER(unsigned char, 3, bmp_write24, READ3BYTES);
-}
-
-static void stretch_masked_line24(uintptr_t dptr, unsigned char* sptr)
-{
-   DECLARE_MASKED_STRETCHER(unsigned char, 3, bmp_write24, READ3BYTES, MASK_COLOR_24);
-}
-#endif
-
-
-
-#ifdef ALLEGRO_COLOR32
-static void stretch_line32(uintptr_t dptr, unsigned char* sptr)
-{
-   DECLARE_STRETCHER(uint32_t, 4, bmp_write32, *);
-}
-
-static void stretch_masked_line32(uintptr_t dptr, unsigned char* sptr)
-{
-   DECLARE_MASKED_STRETCHER(uint32_t, 4, bmp_write32, *, MASK_COLOR_32);
-}
-#endif
-
-
-
-/*
- * Stretch blit work-horse.
- */
-static void _al_stretch_blit(BITMAP *src, BITMAP *dst,
-    int sx, int sy, int sw, int sh, int dx, int dy, int dw, int dh,
-   int masked)
-{
-   int y; /* current dst y */
-   int yc; /* y counter */
-   int sxofs, dxofs; /* start offsets */
-   int syinc; /* amount to increment src y each time */
-   int ycdec; /* amount to deccrement counter by, increase sy when this reaches 0 */
-   int ycinc; /* amount to increment counter by when it reaches 0 */
-   int size = 0; /* pixel size */
-   int dxbeg, dxend; /* clipping information */
-   int dybeg, dyend;
-   int i;
-
-   void (*stretch_line)(uintptr_t, unsigned char*) = 0;
-
-   ASSERT(src);
-   ASSERT(dst);
-
-   /* vtable hook; not called if dest is a memory surface */   
-   if (src->vtable->do_stretch_blit && !is_memory_bitmap(dst)) {
-      src->vtable->do_stretch_blit(src, dst, sx, sy, sw, sh, dx, dy, dw, dh, masked);
-      return;
-   }
-
-   if ((sw <= 0) || (sh <= 0) || (dw <= 0) || (dh <= 0))
-      return;
-
-   /* Find out which stretcher should be used */
-   if (masked) {
-      switch (bitmap_color_depth(dst)) {
-#ifdef ALLEGRO_COLOR8
-	 case 8:
-	    if (is_linear_bitmap(dst))
-	       stretch_line = stretch_masked_line8;
-#ifdef ALLEGRO_GFX_HAS_VGA
-	    else
-	       stretch_line = stretch_masked_linex;
-#endif
-	    size = 1;
-	    break;
-#endif
-#ifdef ALLEGRO_COLOR16
-	 case 15:
-	    stretch_line = stretch_masked_line15;
-	    size = 2;
-	    break;
-	 case 16:
-	    stretch_line = stretch_masked_line16;
-	    size = 2;
-	    break;
-#endif
-#ifdef ALLEGRO_COLOR24
-	 case 24:
-	    stretch_line = stretch_masked_line24;
-	    size = 3;
-	    break;
-#endif
-#ifdef ALLEGRO_COLOR32
-	 case 32:
-	    stretch_line = stretch_masked_line32;
-	    size = 4;
-	    break;
-#endif
-      }
-   }
-   else {
-      switch (bitmap_color_depth(dst)) {
-#ifdef ALLEGRO_COLOR8
-	 case 8:
-	    if (is_linear_bitmap(dst))
-	       stretch_line = stretch_line8;
-#ifdef ALLEGRO_GFX_HAS_VGA
-	    else
-	       stretch_line = stretch_linex;
-#endif
-	    size = 1;
-	    break;
-#endif
-#ifdef ALLEGRO_COLOR16
-	 case 15:
-	    stretch_line = stretch_line15;
-	    size = 2;
-	    break;
-	 case 16:
-	    stretch_line = stretch_line16;
-	    size = 2;
-	    break;
-#endif
-#ifdef ALLEGRO_COLOR24
-	 case 24:
-	    stretch_line = stretch_line24;
-	    size = 3;
-	    break;
-#endif
-#ifdef ALLEGRO_COLOR32
-	 case 32:
-	    stretch_line = stretch_line32;
-	    size = 4;
-	    break;
-#endif
-      }
-   }
-
-   ASSERT(stretch_line);
-
-   if (dst->clip) {
-      dybeg = ((dy > dst->ct) ? dy : dst->ct);
-      dyend = (((dy + dh) < dst->cb) ? (dy + dh) : dst->cb);
-      if (dybeg >= dyend)
-	 return;
-
-      dxbeg = ((dx > dst->cl) ? dx : dst->cl);
-      dxend = (((dx + dw) < dst->cr) ? (dx + dw) : dst->cr);
-      if (dxbeg >= dxend)
-	 return;
-   }
-   else {
-      dxbeg = dx;
-      dxend = dx + dw;
-      dybeg = dy;
-      dyend = dy + dh;
-   }
-
-   syinc = sh / dh;
-   ycdec = sh - (syinc*dh);
-   ycinc = dh - ycdec;
-   yc = ycinc;
-   sxofs = sx * size;
-   dxofs = dx * size;
-
-   _al_stretch.sxinc = sw / dw * size;
-   _al_stretch.xcdec = sw - ((sw/dw)*dw);
-   _al_stretch.xcinc = dw - _al_stretch.xcdec;
-   _al_stretch.linesize = (dxend-dxbeg)*size;
-
-   /* get start state (clip) */
-   _al_stretch.xcstart = _al_stretch.xcinc;
-   for (i = 0; i < dxbeg-dx; i++, sxofs += _al_stretch.sxinc) {
-      if (_al_stretch.xcstart <= 0) {
-	 _al_stretch.xcstart += _al_stretch.xcinc;
-	 sxofs += size;
-      }
-      else
-	 _al_stretch.xcstart -= _al_stretch.xcdec;
-   }
-
-   dxofs += i * size;
-
-   /* skip clipped lines */
-   for (y = dy; y < dybeg; y++, sy += syinc) {
-      if (yc <= 0) {
-	 sy++;
-	 yc += ycinc;
-      }
-      else
-	    yc -= ycdec;
-   }
-
-   /* Stretch it */
-
-   bmp_select(dst);
-
-   for (; y < dyend; y++, sy += syinc) {
-      (*stretch_line)(bmp_write_line(dst, y) + dxofs, src->line[sy] + sxofs);
-      if (yc <= 0) {
-	 sy++;
-	 yc += ycinc;
-      }
-      else
-	    yc -= ycdec;
-   }
-
-}
-
-
-
-/* stretch_blit:
- *  Opaque bitmap scaling function.
- *
- *  NOTE: If this is crashing on VS2008, I have changed al386vc.h to replace
- *  the contents of bmp_write_line and bmp_read_line with this:
- *  return (uintptr_t)&bmp->line[lyne][0];
- *  which fixes the problem. Some sort of ASM issue by the looks of it.
- */
-void Cstretch_blit(BITMAP *src, BITMAP *dst, int sx, int sy, int sw, int sh,
-		  int dx, int dy, int dw, int dh)
-{
-   ASSERT(src);
-   ASSERT(dst);
-
-   #ifdef ALLEGRO_MPW
-      if (is_system_bitmap(src) && is_system_bitmap(dst))
-         system_stretch_blit(src, dst, sx, sy, sw, sh, dx, dy, dw, dh);
-      else
-   #endif
-         _al_stretch_blit(src, dst, sx, sy, sw, sh, dx, dy, dw, dh, 0);
-}
-
-
-
-/* masked_stretch_blit:
- *  Masked bitmap scaling function.
- */
-void Cmasked_stretch_blit(BITMAP *src, BITMAP *dst, int sx, int sy, int sw, int sh,
-                         int dx, int dy, int dw, int dh)
-{
-   ASSERT(src);
-   ASSERT(dst);
-
-   _al_stretch_blit(src, dst, sx, sy, sw, sh, dx, dy, dw, dh, 1);
-}
-
-
-
-/* stretch_sprite:
- *  Masked version of stretch_blit().
- */
-void Cstretch_sprite(BITMAP *dst, BITMAP *src, int x, int y, int w, int h)
-{
-   ASSERT(src);
-   ASSERT(dst);
-
-   _al_stretch_blit(src, dst, 0, 0, src->w, src->h, x, y, w, h, 1);
-}

commit 86ee891a256fa152fa8fafdf8b0e9ee382f75468

Editor: fixed room sprites drawn without transparency
Was broken by 40faa6ce746c77ab6fb4599460dc8eea0726562d

Editor/AGS.Native/agsnative.cpp
@@ -1060,7 +1060,7 @@ static void doDrawViewLoop(int hdc, const std::vector<::ViewFrame> &frames,
             bmpbuf.reset(flipped);
             toblt = flipped;
         }
-        todraw->StretchBlt(toblt, RectWH(size*i, 0, neww, newh));
+        todraw->StretchBlt(toblt, RectWH(size*i, 0, neww, newh), Common::kBitmap_Transparency);
         bmpbuf.reset();
         if (i < frames.size() - 1)
         {
@@ -1730,7 +1730,7 @@ void DrawSpriteToBuffer(int sprNum, int x, int y, float scale) {
 	}
 	else
 	{
-        drawBuffer->StretchBlt(imageToDraw, RectWH(x, y, drawWidth, drawHeight));
+        drawBuffer->StretchBlt(imageToDraw, RectWH(x, y, drawWidth, drawHeight), Common::kBitmap_Transparency);
 	}
 
 	if (imageToDraw != todraw)
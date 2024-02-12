93ae9a95685600e708c078d970ec14f6e461b01f
Ivan Mogilko <ikm_spb@yahoo.com>
2024-02-12 23:28:48 +0300

    Added more AGS code headers with license where missing

--- Compiler/script2/cs_parser.h
+++ Compiler/script2/cs_parser.h
@@ -1,5 +1,18 @@
+//=============================================================================
+//
+// Adventure Game Studio (AGS)
+//
+// Copyright (C) 1999-2011 Chris Jones and 2011-2024 various contributors
+// The full list of copyright holders can be found in the Copyright.txt
+// file, which is part of this source code distribution.
+//
+// The AGS source code is provided under the Artistic License 2.0.
+// A copy of this license can be found in the file License.txt and at
+// https://opensource.org/license/artistic-2-0/
+//
+//=============================================================================
 /*
-'C'-style script compiler development file. (c) 2000,2001 Chris Jones
+
 SCOM is a script compiler for the 'C' language.
 
 BIRD'S EYE OVERVIEW - INTERFACE AND HIGH-LEVEL STRUCTURE

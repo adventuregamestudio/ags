16c6eee33837d32cbb2fbfdf0664a05982068a79
Ivan Mogilko <ikm_spb@yahoo.com>
2024-03-15 04:44:05 +0300

    Editor: display a correct reason for rebuilding a room on "Rebuild All"

--- Editor/AGS.Editor/Components/RoomsComponent.cs
+++ Editor/AGS.Editor/Components/RoomsComponent.cs
@@ -1429,7 +1429,9 @@ namespace AGS.Editor.Components
 				return false;
 			}
 
-			foreach (UnloadedRoom unloadedRoom in roomsToRebuild)
+            string rebuildReason = rebuildAll ? "because the full rebuild was ordered" : "because a script has changed";
+
+            foreach (UnloadedRoom unloadedRoom in roomsToRebuild)
 			{
 				Room room;
 				if ((_loadedRoom == null) || (_loadedRoom.Number != unloadedRoom.Number))
@@ -1443,7 +1445,7 @@ namespace AGS.Editor.Components
 				}
 
 				CompileMessages roomErrors = new CompileMessages();
-				SaveRoomButDoNotShowAnyErrors(room, roomErrors, "Rebuilding room " + room.Number + " because a script has changed...");
+				SaveRoomButDoNotShowAnyErrors(room, roomErrors, $"Rebuilding room {room.Number} {rebuildReason}...");
 
 				if (roomErrors.Count > 0)
 				{

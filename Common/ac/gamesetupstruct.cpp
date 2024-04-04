adventuregamestudio/ags

commit 28693377db7ae242782b313717ec9c476ecaff0f

Engine: fix some minor warnings

Common/ac/gamesetupstruct.cpp
@@ -140,17 +140,17 @@ void GameSetupStruct::read_font_infos(Common::Stream *in, GameDataVersion data_v
 
 void GameSetupStruct::ReadInvInfo(Stream *in)
 {
-    for (int iteratorCount = 0; iteratorCount < numinvitems; ++iteratorCount)
+    for (int i = 0; i < numinvitems; ++i)
     {
-        invinfo[iteratorCount].ReadFromFile(in);
+        invinfo[i].ReadFromFile(in);
     }
 }
 
 void GameSetupStruct::WriteInvInfo(Stream *out)
 {
-    for (int iteratorCount = 0; iteratorCount < numinvitems; ++iteratorCount)
+    for (int i = 0; i < numinvitems; ++i)
     {
-        invinfo[iteratorCount].WriteToFile(out);
+        invinfo[i].WriteToFile(out);
     }
 }
 
@@ -197,17 +197,17 @@ void GameSetupStruct::read_words_dictionary(Common::Stream *in)
 
 void GameSetupStruct::ReadMouseCursors(Stream *in)
 {
-    for (int iteratorCount = 0; iteratorCount < numcursors; ++iteratorCount)
+    for (int i = 0; i < numcursors; ++i)
     {
-        mcurs[iteratorCount].ReadFromFile(in);
+        mcurs[i].ReadFromFile(in);
     }
 }
 
 void GameSetupStruct::WriteMouseCursors(Stream *out)
 {
-    for (int iteratorCount = 0; iteratorCount < numcursors; ++iteratorCount)
+    for (int i = 0; i < numcursors; ++i)
     {
-        mcurs[iteratorCount].WriteToFile(out);
+        mcurs[i].WriteToFile(out);
     }
 }
 
@@ -256,17 +256,17 @@ void GameSetupStruct::read_messages(Common::Stream *in,
 
 void GameSetupStruct::ReadCharacters(Stream *in)
 {
-    for (int iteratorCount = 0; iteratorCount < numcharacters; ++iteratorCount)
+    for (int i = 0; i < numcharacters; ++i)
     {
-        chars[iteratorCount].ReadFromFile(in, loaded_game_file_version);
+        chars[i].ReadFromFile(in, loaded_game_file_version);
     }
 }
 
 void GameSetupStruct::WriteCharacters(Stream *out)
 {
-    for (int iteratorCount = 0; iteratorCount < numcharacters; ++iteratorCount)
+    for (int i = 0; i < numcharacters; ++i)
     {
-        chars[iteratorCount].WriteToFile(out);
+        chars[i].WriteToFile(out);
     }
 }
 

Common/ac/gamesetupstructbase.h
@@ -42,19 +42,21 @@ struct GameSetupStructBase
 
     Common::String    gamename;
     int               options[MAX_OPTIONS];
-    unsigned char     paluses[256];
+    uint8_t           paluses[256];
     RGB               defpal[256];
     int               numviews;
     int               numcharacters;
     int               playercharacter;
     int               totalscore;
-    short             numinvitems;
-    int               numdialog, numdlgmessage;
+    int               numinvitems;
+    int               numdialog;
+    int               numdlgmessage; // [DEPRECATED]
     int               numfonts;
     int               color_depth;          // in bytes per pixel (ie. 1 or 2)
     int               target_win;
     int               dialog_bullet;        // 0 for none, otherwise slot num of bullet point
-    unsigned short    hotdot, hotdotouter;  // inv cursor hotspot dot color
+    int               hotdot; // inv cursor hotspot dot color
+    int               hotdotouter;
     int               uniqueid;    // random key identifying the game
     int               numgui;
     int               numcursors;

Engine/ac/button.cpp
@@ -67,7 +67,7 @@ void Button_Animate(GUIButton *butt, int view, int loop, int speed, int repeat,
     abtn.view = view;
     abtn.loop = loop;
     abtn.speed = speed;
-    abtn.repeat = static_cast<bool>(repeat) ? ANIM_REPEAT : ANIM_ONCE; // for now, clamp to supported modes
+    abtn.repeat = (repeat != 0) ? ANIM_REPEAT : ANIM_ONCE; // for now, clamp to supported modes
     abtn.blocking = blocking;
     abtn.direction = direction;
     abtn.frame = SetFirstAnimFrame(view, loop, sframe, direction);

Engine/ac/draw.cpp
@@ -997,7 +997,7 @@ void reset_drawobj_for_overlay(int objnum)
 
 void notify_sprite_changed(int sprnum, bool deleted)
 {
-    assert(sprnum >= 0 && sprnum < game.SpriteInfos.size());
+    assert(sprnum >= 0 && static_cast<uint32_t>(sprnum) < game.SpriteInfos.size());
     // Update texture cache (regen texture or clear from cache)
     if (deleted)
         clear_shared_texture(sprnum);
@@ -1010,7 +1010,7 @@ void notify_sprite_changed(int sprnum, bool deleted)
     // reference this sprite.
     if (drawstate.SoftwareRender)
     {
-        assert(sprnum < play.spritemodified.size());
+        assert(static_cast<uint32_t>(sprnum) < play.spritemodified.size());
         play.spritemodified[sprnum] = true;
         play.spritemodifiedlist.push_back(sprnum);
     }
@@ -1158,7 +1158,7 @@ void render_to_screen()
     else
     {
         bool new_vsync = gfxDriver->SetVsync(scsystem.vsync > 0);
-        if (new_vsync != scsystem.vsync)
+        if (new_vsync != (scsystem.vsync != 0))
             System_SetVSyncInternal(new_vsync);
     }
 
@@ -2076,7 +2076,7 @@ void prepare_characters_for_drawing()
     const bool hw_accel = !drawstate.SoftwareRender;
 
     // draw characters
-    for (uint32_t charid = 0; charid < game.numcharacters; ++charid)
+    for (int charid = 0; charid < game.numcharacters; ++charid)
     {
         const CharacterInfo &chin = game.chars[charid];
         if (chin.on == 0)

Engine/main/config.cpp
@@ -287,7 +287,7 @@ static void read_legacy_graphics_config(const ConfigTree &cfg)
     }
 
     usetup.Screen.Params.RefreshRate = CfgReadInt(cfg, "misc", "refresh");
-    usetup.enable_antialiasing = CfgReadInt(cfg, "misc", "antialias");
+    usetup.enable_antialiasing = CfgReadBoolInt(cfg, "misc", "antialias");
 }
 
 static void read_legacy_config(const ConfigTree &cfg)

Engine/main/game_run.cpp
@@ -696,7 +696,7 @@ static void update_objects_scale()
         update_object_scale(objid);
     }
 
-    for (uint32_t charid = 0; charid < game.numcharacters; ++charid)
+    for (int charid = 0; charid < game.numcharacters; ++charid)
     {
         update_character_scale(charid);
     }

commit 4de9f8b1064823130945d780e7ec16104c678e00

Common: fixed item crosshair colors read with mistake
Was broken by 28693377db7ae242782b313717ec9c476ecaff0f

Common/ac/gamesetupstruct.cpp
@@ -445,8 +445,8 @@ void GameSetupStruct::ReadFromSavegame(Stream *in)
     // of GameSetupStructBase
     playercharacter = in->ReadInt32();
     dialog_bullet = in->ReadInt32();
-    hotdot = in->ReadInt16();
-    hotdotouter = in->ReadInt16();
+    hotdot = static_cast<uint16_t>(in->ReadInt16());
+    hotdotouter = static_cast<uint16_t>(in->ReadInt16());
     invhotdotsprite = in->ReadInt32();
     default_lipsync_frame = in->ReadInt32();
 }
@@ -459,8 +459,8 @@ void GameSetupStruct::WriteForSavegame(Stream *out)
     // of GameSetupStructBase
     out->WriteInt32(playercharacter);
     out->WriteInt32(dialog_bullet);
-    out->WriteInt16(hotdot);
-    out->WriteInt16(hotdotouter);
+    out->WriteInt16(static_cast<uint16_t>(hotdot));
+    out->WriteInt16(static_cast<uint16_t>(hotdotouter));
     out->WriteInt32(invhotdotsprite);
     out->WriteInt32(default_lipsync_frame);
 }

Common/ac/gamesetupstructbase.cpp
@@ -162,8 +162,8 @@ void GameSetupStructBase::ReadFromFile(Stream *in, GameDataVersion game_ver, Ser
     color_depth = in->ReadInt32();
     target_win = in->ReadInt32();
     dialog_bullet = in->ReadInt32();
-    hotdot = in->ReadInt16();
-    hotdotouter = in->ReadInt16();
+    hotdot = static_cast<uint16_t>(in->ReadInt16());
+    hotdotouter = static_cast<uint16_t>(in->ReadInt16());
     uniqueid = in->ReadInt32();
     numgui = in->ReadInt32();
     numcursors = in->ReadInt32();
@@ -212,8 +212,8 @@ void GameSetupStructBase::WriteToFile(Stream *out, const SerializeInfo &info) co
     out->WriteInt32(color_depth);
     out->WriteInt32(target_win);
     out->WriteInt32(dialog_bullet);
-    out->WriteInt16(hotdot);
-    out->WriteInt16(hotdotouter);
+    out->WriteInt16(static_cast<uint16_t>(hotdot));
+    out->WriteInt16(static_cast<uint16_t>(hotdotouter));
     out->WriteInt32(uniqueid);
     out->WriteInt32(numgui);
     out->WriteInt32(numcursors);
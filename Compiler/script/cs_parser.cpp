4da3c09a3647445938c111affc6176432b21fb21
Ivan Mogilko <ikm_spb@yahoo.com>
2024-05-17 02:32:46 +0300

    Compiler: fixed compilation in case of user-defined variadic function
    User-defined variadic functions are not properly usable at the moment, but this fixes a potential bad memory access.

--- Compiler/script/cs_parser.cpp
+++ Compiler/script/cs_parser.cpp
@@ -3450,7 +3450,7 @@ int __cc_compile_file(const char*inpl,ccCompiledScript*scrip) {
 
                 // loop through all parameters and check if they are pointers
                 // the first entry is the return value
-                for (int pa = 1; pa <= sym.entries[inFuncSym].sscope; pa++) {
+                for (int pa = 1; pa <= sym.entries[inFuncSym].get_num_args(); pa++) {
                     if (sym.entries[inFuncSym].funcparamtypes[pa] & (STYPE_POINTER | STYPE_DYNARRAY)) {
                         // pointers are passed in on the stack with the real
                         // memory address -- convert this to the mem handle

8e0fb5f2b99a6f2337c4676183d0eb497a408bb0
Érico Porto <ericoporto2008@gmail.com>
2024-06-16 12:24:41 -0300

    Engine: add to macOS new apple free space check

--- Engine/platform/osx/alplmac.mm
+++ Engine/platform/osx/alplmac.mm
@@ -32,9 +32,22 @@ int AGSMacGetFreeSpaceInMB(const char path[PATH_MAX])
   @autoreleasepool {
   NSString *dirpath = @(path);
   NSError *error;
-  NSDictionary* fileAttributes = [[NSFileManager defaultManager] attributesOfFileSystemForPath:dirpath
-                                                                                       error:&error];
-  unsigned long long freeSpace = [[fileAttributes objectForKey:NSFileSystemFreeSize] unsignedLongLongValue];
+  unsigned long long freeSpace = 0;
+
+  if (@available(macOS 10.13, iOS 11.0, *)) {
+      NSURL *fileURL = [[NSURL alloc] initFileURLWithPath:dirpath];
+      NSDictionary *results = [fileURL resourceValuesForKeys:@[NSURLVolumeAvailableCapacityForImportantUsageKey] error:&error];
+      if(results) {
+          NSNumber *availableImportantSpace  = results[NSURLVolumeAvailableCapacityForImportantUsageKey];
+          freeSpace = availableImportantSpace.longLongValue;
+      }
+  }
+
+  if(freeSpace == 0) {
+      NSDictionary *fileAttributes = [[NSFileManager defaultManager] attributesOfFileSystemForPath:dirpath
+                                                                                             error:&error];
+      freeSpace = [[fileAttributes objectForKey:NSFileSystemFreeSize] longLongValue];
+  }
   return (int)(freeSpace / (1024*1024));
   }
 }

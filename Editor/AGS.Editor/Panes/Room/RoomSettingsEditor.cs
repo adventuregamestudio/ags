e9f1ba5ca41a96cf8ee134ae70e12eaca919dff8
Ivan Mogilko <ikm_spb@yahoo.com>
2024-07-14 01:36:28 +0300

    Editor: clamp room edges after room bg size changes

--- Editor/AGS.Editor/Panes/Room/RoomSettingsEditor.cs
+++ Editor/AGS.Editor/Panes/Room/RoomSettingsEditor.cs
@@ -541,6 +541,7 @@ namespace AGS.Editor
                     bmp = ExtendBitmapIfSmallerThanScreen(bmp);
                     bool doImport = true;
                     bool deleteExtraFrames = false;
+                    bool roomSizeChanged = false;
 					RoomResolution newResolution;
 					if (bgIndex > 0)
 					{
@@ -572,6 +573,7 @@ namespace AGS.Editor
                             }
                             else
                             {
+                                roomSizeChanged = true;
                                 deleteExtraFrames = true;
                             }
                         }
@@ -580,6 +582,10 @@ namespace AGS.Editor
                             if (Factory.GUIController.ShowQuestion("The new background is a different size to the old one. If you import it, all your regions, hotspots and walkable areas will be cleared. Do you want to proceed?") != DialogResult.Yes)
                             {
                                 doImport = false;
+                            }
+                            else
+                            {
+                                roomSizeChanged = true;
                             }
                         }
                     }
@@ -600,6 +606,11 @@ namespace AGS.Editor
                             }
                         }
 
+                        if (roomSizeChanged)
+                        {
+                            OnRoomSizeChanged();
+                        }
+
                         // TODO: choose default zoom based on the room size vs window size?
                         SetZoomSliderToMultiplier(_room.Width <= 320 ? 2 : 1);
                     }
@@ -614,6 +625,15 @@ namespace AGS.Editor
                         bmp.Dispose();
                 }
             }
+        }
+
+        private void OnRoomSizeChanged()
+        {
+            // Clamp room edges to the new size
+            _room.LeftEdgeX = MathExtra.Clamp(_room.LeftEdgeX, 0, _room.Width - 2);
+            _room.RightEdgeX = MathExtra.Clamp(_room.RightEdgeX, _room.LeftEdgeX + 1, _room.Width - 1);
+            _room.TopEdgeY = MathExtra.Clamp(_room.TopEdgeY, 0, _room.Height - 2);
+            _room.BottomEdgeY = MathExtra.Clamp(_room.BottomEdgeY, _room.TopEdgeY + 1, _room.Height - 1);
         }
 
         private void btnChangeImage_Click(object sender, EventArgs e)

adventuregamestudio/ags

commit 9e76800d668d8d0af7d47d815e6d1b781c7601f6

Engine: cc_error returns stack from all script threads

Common/script/cc_common.cpp
@@ -36,10 +36,8 @@ int ccGetOption(int optbit)
     return 0;
 }
 
-// Returns full script error message and callstack (if possible)
-extern std::pair<String, String> cc_error_at_line(const char *error_msg);
-// Returns script error message without location or callstack
-extern String cc_error_without_line(const char *error_msg);
+// Returns current running script callstack as a human-readable text
+extern String cc_get_callstack(int max_lines = INT_MAX);
 
 ScriptError ccError;
 
@@ -72,19 +70,12 @@ void cc_error(const char *descr, ...)
     String displbuf = String::FromFormatV(descr, ap);
     va_end(ap);
 
-    if (currentline > 0)
-    {
-        // [IKM] Implementation is project-specific
-        std::pair<String, String> errinfo = cc_error_at_line(displbuf.GetCStr());
-        ccError.ErrorString = errinfo.first;
-        ccError.CallStack = errinfo.second;
-    }
+    String callstack = cc_get_callstack();
+    if ((currentline > 0) && callstack.IsEmpty())
+        ccError.ErrorString = String::FromFormat("Error (line %d): %s", currentline, displbuf.GetCStr());
     else
-    {
-        ccError.ErrorString = cc_error_without_line(displbuf.GetCStr());
-        ccError.CallStack = "";
-    }
-
+        ccError.ErrorString = String::FromFormat("Error: %s", displbuf.GetCStr());
+    ccError.CallStack = callstack;
     ccError.HasError = 1;
     ccError.Line = currentline;
 }

Engine/main/quit.cpp
@@ -43,6 +43,7 @@
 #include "platform/base/agsplatformdriver.h"
 #include "platform/base/sys_main.h"
 #include "plugin/plugin_engine.h"
+#include "script/cc_common.h"
 #include "media/audio/audio_system.h"
 #include "media/video/video.h"
 
@@ -138,7 +139,7 @@ QuitReason quit_check_for_error_state(const char *&qmsg, String &alertis)
                 "(ACI version %s)\n\n", EngineVersion.LongString.GetCStr());
         }
 
-        alertis.Append(get_cur_script(5));
+        alertis.Append(cc_get_error().CallStack);
 
         if (qreason != kQuit_UserAbort)
             alertis.Append("\nError: ");
@@ -151,7 +152,7 @@ QuitReason quit_check_for_error_state(const char *&qmsg, String &alertis)
         qmsg++;
         alertis.Format("A warning has been generated. This is not normally fatal, but you have selected "
             "to treat warnings as errors.\n"
-            "(ACI version %s)\n\n%s\n", EngineVersion.LongString.GetCStr(), get_cur_script(5).GetCStr());
+            "(ACI version %s)\n\n%s\n", EngineVersion.LongString.GetCStr(), cc_get_error().CallStack.GetCStr());
         return kQuit_GameWarning;
     }
     else

Editor/AGS.Native/NativeDLL.vcxproj
@@ -188,7 +188,6 @@
     <ClCompile Include="..\Native\ac\actiontype.cpp" />
     <ClCompile Include="..\Native\ac\scriptmodule.cpp" />
     <ClCompile Include="..\Native\color_native.cpp" />
-    <ClCompile Include="..\Native\script_agsnative.cpp" />
     <ClCompile Include="..\Native\sprcache_agsnative.cpp" />
     <ClCompile Include="..\scintilla\lexers\LexCPP.cxx" />
     <ClCompile Include="..\scintilla\lexlib\Accessor.cxx" />

Editor/AGS.Native/NativeDLL.vcxproj.filters
@@ -64,9 +64,6 @@
     <ClCompile Include="NativeMethods.cpp">
       <Filter>Source Files</Filter>
     </ClCompile>
-    <ClCompile Include="..\Native\script_agsnative.cpp">
-      <Filter>Source Files</Filter>
-    </ClCompile>
     <ClCompile Include="ScriptCompiler.cpp">
       <Filter>Source Files</Filter>
     </ClCompile>

Editor/AGS.Native/agsnative.cpp
@@ -3968,6 +3968,12 @@ void save_room_file(RoomStruct &rs, const AGSString &path)
 }
 
 
+// Dummy placeholders to avoid unresolved functions in Common
+AGSString cc_get_callstack(int max_lines)
+{
+    return "";
+}
+
 void quit(const char * message) 
 {
 	ThrowManagedException((const char*)message);

Editor/Native/script_agsnative.cpp
@@ -1,20 +1,0 @@
-//
-// Implementation for script specific to AGS.Native library
-//
-
-#include <utility>
-#include "util/string.h"
-
-using namespace AGS::Common;
-
-extern int currentline; // in script/script_common
-
-std::pair<String, String> cc_error_at_line(const char *error_msg)
-{
-    return std::make_pair(String::FromFormat("Error (line %d): %s", currentline, error_msg), String());
-}
-
-String cc_error_without_line(const char *error_msg)
-{
-    return String::FromFormat("Error (line unknown): %s", error_msg);
-}

Engine/CMakeLists.txt
@@ -437,7 +437,6 @@ target_sources(engine
     script/script.h
     script/script_api.cpp
     script/script_api.h
-    script/script_engine.cpp
     script/script_runtime.cpp
     script/script_runtime.h
     script/systemimports.cpp

Engine/debug/debug.cpp
@@ -390,17 +390,6 @@ void debug_script_log(const char *msg, ...)
 }
 
 
-String get_cur_script(int numberOfLinesOfCallStack)
-{
-    String callstack;
-    ccInstance *sci = ccInstance::GetCurrentInstance();
-    if (sci)
-        callstack = sci->GetCallStack(numberOfLinesOfCallStack);
-    if (callstack.IsEmpty())
-        callstack = cc_get_error().CallStack;
-    return callstack;
-}
-
 struct Breakpoint
 {
     char scriptName[80];
@@ -412,7 +401,7 @@ int numBreakpoints = 0;
 
 bool send_message_to_editor(const char *msg, const char *errorMsg) 
 {
-    String callStack = get_cur_script(25);
+    String callStack = cc_get_error().CallStack;
     if (callStack.IsEmpty())
         return false;
 

commit 716a36aa0252a7481d299d4e409eed2d56239bb3

Engine: fixed callstack not reported in case of script api errors
This was broken by 9e76800d668d8d0af7d47d815e6d1b781c7601f6

Common/script/cc_common.cpp
@@ -56,6 +56,11 @@ const ScriptError &cc_get_error()
     return ccError;
 }
 
+String cc_get_err_callstack(int max_lines)
+{
+    return cc_has_error() ? ccError.CallStack : cc_get_callstack(max_lines);
+}
+
 void cc_error(const char *descr, ...)
 {
     ccError.IsUserError = false;

Common/script/cc_common.h
@@ -47,6 +47,9 @@ struct ScriptError
 void cc_clear_error();
 bool cc_has_error();
 const ScriptError &cc_get_error();
+// Returns callstack of the last recorded script error, or a callstack
+// of a current execution point, if no script error is currently saved in memory.
+AGS::Common::String cc_get_err_callstack(int max_lines = INT_MAX);
 void cc_error(const char *, ...);
 void cc_error(const ScriptError &err);
 // Project-dependent script error formatting

Engine/main/quit.cpp
@@ -141,7 +141,7 @@ QuitReason quit_check_for_error_state(const char *qmsg, String &errmsg, String &
                 "(Engine version %s)\n\n", EngineVersion.LongString.GetCStr());
         }
 
-        alertis.Append(cc_get_error().CallStack);
+        alertis.Append(cc_get_err_callstack());
 
         if (qreason != kQuit_UserAbort)
         {
@@ -155,7 +155,7 @@ QuitReason quit_check_for_error_state(const char *qmsg, String &errmsg, String &
         qmsg++;
         alertis.Format("A warning has been generated. This is not normally fatal, but you have selected "
             "to treat warnings as errors.\n"
-            "(Engine version %s)\n\n%s\n%s", EngineVersion.LongString.GetCStr(), cc_get_error().CallStack.GetCStr(),
+            "(Engine version %s)\n\n%s\n%s", EngineVersion.LongString.GetCStr(), cc_get_err_callstack().GetCStr(),
             qmsg);
         errmsg = qmsg;
         return kQuit_GameWarning;
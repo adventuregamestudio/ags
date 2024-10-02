adventuregamestudio/ags

commit 45e50484f4240d6d1144384162c65b8b129ba0aa

Engine: handle "run script function" error codes as enums

Engine/ac/event.cpp
@@ -71,7 +71,8 @@ ScriptEventCallback ScriptEventCb[kTS_Num] = {
 };
 
 
-int run_claimable_event(const String &tsname, bool includeRoom, int numParams, const RuntimeScriptValue *params, bool *eventWasClaimed) {
+void run_claimable_event(const String &tsname, bool includeRoom, int numParams, const RuntimeScriptValue *params, bool *eventWasClaimed)
+{
     *eventWasClaimed = true;
     // Run the room script function, and if it is not claimed,
     // then run the main one
@@ -79,30 +80,28 @@ int run_claimable_event(const String &tsname, bool includeRoom, int numParams, c
     // this is a nested event
     int eventClaimedOldValue = eventClaimed;
     eventClaimed = EVENT_INPROGRESS;
-    int toret;
 
-    if (includeRoom && roominst) {
-        toret = RunScriptFunction(roominst.get(), tsname, numParams, params);
-
-        if (eventClaimed == EVENT_CLAIMED) {
+    if (includeRoom && roominst)
+    {
+        RunScriptFunction(roominst.get(), tsname, numParams, params);
+        if (eventClaimed == EVENT_CLAIMED)
+        {
             eventClaimed = eventClaimedOldValue;
-            return toret;
         }
     }
 
     // run script modules
-    for (auto &module_inst : moduleInst) {
-        toret = RunScriptFunction(module_inst.get(), tsname, numParams, params);
-
-        if (eventClaimed == EVENT_CLAIMED) {
+    for (auto &module_inst : moduleInst)
+    {
+        RunScriptFunction(module_inst.get(), tsname, numParams, params);
+        if (eventClaimed == EVENT_CLAIMED)
+        {
             eventClaimed = eventClaimedOldValue;
-            return toret;
         }
     }
 
     eventClaimed = eventClaimedOldValue;
     *eventWasClaimed = false;
-    return 0;
 }
 
 // runs the global script on_event function

Engine/script/script.cpp
@@ -330,29 +330,33 @@ static bool DoRunScriptFuncCantBlock(ccInstance *sci, NonBlockingScriptFunction*
     return(hasTheFunc);
 }
 
-static int PrepareTextScript(ccInstance *sci, const String &tsname)
+static RunScFuncResult PrepareTextScript(ccInstance *sci, const String &tsname)
 {
+    assert(sci);
     cc_clear_error();
-    // FIXME: try to make it so this function is not called with NULL sci
-    if (sci == nullptr) return -1;
-    if (sci->GetSymbolAddress(tsname.GetCStr()).IsNull()) {
+    if (sci->GetSymbolAddress(tsname.GetCStr()).IsNull())
+    {
         cc_error("no such function in script");
-        return -2;
+        return kScFnRes_NotFound;
     }
-    if (sci->IsBeingRun()) {
+    if (sci->IsBeingRun())
+    {
         cc_error("script is already in execution");
-        return -3;
+        return kScFnRes_ScriptBusy;
     }
     ExecutingScript exscript;
     // CHECKME: this conditional block will never run, because
     // function would have quit earlier (deprecated functionality?)
-    if (sci->IsBeingRun()) {
+    if (sci->IsBeingRun())
+    {
         auto fork = sci->Fork();
         if (!fork)
             quit("unable to fork instance for secondary script");
         exscript.ForkedInst.reset(fork);
         exscript.Inst = fork;
-    } else {
+    }
+    else
+    {
         exscript.Inst = sci;
     }
     scripts[num_scripts] = std::move(exscript);
@@ -362,11 +366,12 @@ static int PrepareTextScript(ccInstance *sci, const String &tsname)
         quit("too many nested text script instances created");
     update_script_mouse_coords();
     inside_script++;
-    return 0;
+    return kScFnRes_Done;
 }
 
-int RunScriptFunction(ccInstance *sci, const String &tsname, size_t numParam, const RuntimeScriptValue *params)
+RunScFuncResult RunScriptFunction(ccInstance *sci, const String &tsname, size_t numParam, const RuntimeScriptValue *params)
 {
+    assert(sci);
     int oldRestoreCount = gameHasBeenRestored;
     // TODO: research why this is really necessary, and refactor to avoid such hacks!
     // First, save the current ccError state
@@ -378,17 +383,18 @@ int RunScriptFunction(ccInstance *sci, const String &tsname, size_t numParam, co
     ScriptError cachedCcError = cc_get_error();
 
     cc_clear_error();
-    int toret = PrepareTextScript(sci, tsname);
-    if (toret) {
+    const RunScFuncResult res = PrepareTextScript(sci, tsname);
+    if (res != kScFnRes_Done)
+    {
         cc_error(cachedCcError);
-        return -18;
+        return res;
     }
 
     cc_clear_error();
-    toret = curscript->Inst->CallScriptFunction(tsname.GetCStr(), numParam, params);
+    const ccInstError inst_ret = curscript->Inst->CallScriptFunction(tsname.GetCStr(), numParam, params);
 
-    // 100 is if Aborted (eg. because we are LoadAGSGame'ing)
-    if ((toret != 0) && (toret != -2) && (toret != 100)) {
+    if ((inst_ret != kInstErr_None) && (inst_ret != kInstErr_FuncNotFound) && (inst_ret != kInstErr_Aborted))
+    {
         quit_with_script_error(tsname);
     }
 
@@ -408,7 +414,11 @@ int RunScriptFunction(ccInstance *sci, const String &tsname, size_t numParam, co
     if ((oldRestoreCount != gameHasBeenRestored) && (eventClaimed == EVENT_INPROGRESS))
         eventClaimed = EVENT_CLAIMED;
 
-    return toret;
+    // Convert any instance exec error into RunScriptFunction result;
+    // NOTE: only kInstErr_FuncNotFound and kInstErr_Aborted can reach here
+    if (inst_ret == kInstErr_FuncNotFound)
+        return kScFnRes_NotFound;
+    return kScFnRes_Done;
 }
 
 void RunScriptFunctionInModules(const String &tsname, size_t param_count, const RuntimeScriptValue *params)
@@ -418,22 +428,21 @@ void RunScriptFunctionInModules(const String &tsname, size_t param_count, const 
     RunScriptFunction(gameinst.get(), tsname, param_count, params);
 }
 
-int RunScriptFunctionInRoom(const String &tsname, size_t param_count, const RuntimeScriptValue *params)
+void RunScriptFunctionInRoom(const String &tsname, size_t param_count, const RuntimeScriptValue *params)
 {
     // Some room callbacks are considered to be obligatory; for historical reasons these are
     // identified by having no parameters;
-    // TODO: this is a hack, this should be defined either by function type, or as an arg
+    // FIXME: this is a hack, this should be defined either by function type, or as an arg
     const bool strict_room_event = (param_count == 0);
-    int toret = RunScriptFunction(roominst.get(), tsname, param_count, params);
+    const RunScFuncResult res = RunScriptFunction(roominst.get(), tsname, param_count, params);
     // If it's a obligatory room event, and return code means missing function - error
-    if (strict_room_event && (toret == -18))
+    if (strict_room_event && (res != kScFnRes_Done))
         quitprintf("RunScriptFunction: error %d (%s) trying to run '%s'   (Room %d)",
-            toret, cc_get_error().ErrorString.GetCStr(), tsname.GetCStr(), displayed_room);
-    return toret;
+            res, cc_get_error().ErrorString.GetCStr(), tsname.GetCStr(), displayed_room);
 }
 
 // Run non-claimable event in all script modules, except room, break if certain events occured
-static int RunUnclaimableEvent(const String &tsname)
+static void RunUnclaimableEvent(const String &tsname)
 {
     const int room_changes_was = play.room_changes;
     const int restore_game_count_was = gameHasBeenRestored;
@@ -444,32 +453,36 @@ static int RunUnclaimableEvent(const String &tsname)
         // Break on room change or save restoration
         if ((room_changes_was != play.room_changes) ||
             (restore_game_count_was != gameHasBeenRestored))
-            return 0;
+            return;
     }
-    return RunScriptFunction(gameinst.get(), tsname);
+    RunScriptFunction(gameinst.get(), tsname);
 }
 
-static int RunClaimableEvent(const String &tsname, size_t param_count, const RuntimeScriptValue *params)
+static void RunClaimableEvent(const String &tsname, size_t param_count, const RuntimeScriptValue *params)
 {
     // Run claimable event chain in script modules and room script
     bool eventWasClaimed;
-    int toret = run_claimable_event(tsname, true, param_count, params, &eventWasClaimed);
+    run_claimable_event(tsname, true, param_count, params, &eventWasClaimed);
     // Break on event claim
     if (eventWasClaimed)
-        return toret;
-    return RunScriptFunction(gameinst.get(), tsname, param_count, params);
+        return;
+    RunScriptFunction(gameinst.get(), tsname, param_count, params);
 }
 
-int RunScriptFunctionAuto(ScriptType sc_type, const String &tsname, size_t param_count, const RuntimeScriptValue *params)
+void RunScriptFunctionAuto(ScriptType sc_type, const String &tsname, size_t param_count, const RuntimeScriptValue *params)
 {
     // If told to use a room instance, then run only there
     if (sc_type == kScTypeRoom)
-        return RunScriptFunctionInRoom(tsname, param_count, params);
+    {
+        RunScriptFunctionInRoom(tsname, param_count, params);
+        return;
+    }
     // Rep-exec is only run in script modules, but not room script
     // (because room script has its own callback, attached to event slot)
     if (strcmp(tsname.GetCStr(), REP_EXEC_NAME) == 0)
     {
-        return RunUnclaimableEvent(REP_EXEC_NAME);
+        RunUnclaimableEvent(REP_EXEC_NAME);
+        return;
     }
     // Claimable event is run in all the script modules and room script,
     // before running in the globalscript instance
@@ -477,13 +490,14 @@ int RunScriptFunctionAuto(ScriptType sc_type, const String &tsname, size_t param
     if ((strcmp(tsname.GetCStr(), ScriptEventCb[kTS_KeyPress].FnName) == 0) || (strcmp(tsname.GetCStr(), ScriptEventCb[kTS_MouseClick].FnName) == 0) ||
         (strcmp(tsname.GetCStr(), ScriptEventCb[kTS_TextInput].FnName) == 0) || (strcmp(tsname.GetCStr(), "on_event") == 0))
     {
-        return RunClaimableEvent(tsname, param_count, params);
+        RunClaimableEvent(tsname, param_count, params);
+        return;
     }
     // Else run on the single chosen script instance
     ccInstance *sci = GetScriptInstanceByType(sc_type);
     if (!sci)
-        return 0;
-    return RunScriptFunction(sci, tsname, param_count, params);
+        return;
+    RunScriptFunction(sci, tsname, param_count, params);
 }
 
 void AllocScriptModules()

Engine/ac/event.h
@@ -199,7 +199,7 @@ struct AGSEvent
         : Type(kAGSEvent_NewRoom), Data(evt) {}
 };
 
-int run_claimable_event(const AGS::Common::String &tsname, bool includeRoom, int numParams, const RuntimeScriptValue *params, bool *eventWasClaimed);
+void run_claimable_event(const AGS::Common::String &tsname, bool includeRoom, int numParams, const RuntimeScriptValue *params, bool *eventWasClaimed);
 // runs the global script on_event fnuction
 void run_on_event (int evtype, RuntimeScriptValue &wparam);
 void run_room_event(int id);

Engine/script/cc_instance.cpp
@@ -314,7 +314,7 @@ void ccInstance::AbortAndDestroy()
     if (COND) \
     { \
         cc_error(ERROR, ##__VA_ARGS__); \
-        return -1; \
+        return kInstErr_Generic; \
     }
 
 #define CC_ERROR_IF_RETVAL(COND, T, ERROR, ...) \
@@ -327,7 +327,7 @@ void ccInstance::AbortAndDestroy()
 #define ASSERT_CC_ERROR() \
     if (cc_has_error()) \
     { \
-        return -1; \
+        return kInstErr_Generic; \
     }
 
 #else
@@ -347,7 +347,7 @@ void ccInstance::AbortAndDestroy()
         (stackdata_ptr + N_BYTES - stackdata) >= CC_STACK_DATA_SIZE) \
     { \
         cc_error("stack overflow, attempted to grow from %d by %d bytes", (stackdata_ptr - stackdata), N_BYTES); \
-        return -1; \
+        return kInstErr_Generic; \
     }
 
 // ASSERT_STACK_SPACE_BYTES tests that we do not exceed stack limit
@@ -363,7 +363,7 @@ void ccInstance::AbortAndDestroy()
     if (registers[SREG_SP].RValue - N < &stack[0]) \
     { \
         cc_error("stack underflow"); \
-        return -1; \
+        return kInstErr_Generic; \
     }
 
 // ASSERT_STACK_UNWINDED tests that the stack pointer is at the expected position
@@ -372,11 +372,11 @@ void ccInstance::AbortAndDestroy()
         (stackdata_ptr > DATA_PTR)) \
     { \
         cc_error("stack is not unwinded after function call, %d bytes remain", (stackdata_ptr - DATA_PTR)); \
-        return -1; \
+        return kInstErr_Generic; \
     }
 
 
-int ccInstance::CallScriptFunction(const char *funcname, int32_t numargs, const RuntimeScriptValue *params)
+ccInstError ccInstance::CallScriptFunction(const char *funcname, int32_t numargs, const RuntimeScriptValue *params)
 {
     cc_clear_error();
     currentline = 0;
@@ -384,17 +384,19 @@ int ccInstance::CallScriptFunction(const char *funcname, int32_t numargs, const 
     if (numargs > 0 && !params)
     {
         cc_error("internal error in ccInstance::CallScriptFunction");
-        return -1; // TODO: correct error value
+        return kInstErr_Generic;
     }
 
-    if ((numargs >= MAX_FUNCTION_PARAMS) || (numargs < 0)) {
-        cc_error("too many arguments to function");
-        return -3;
+    if ((numargs >= MAX_FUNCTION_PARAMS) || (numargs < 0))
+    {
+        cc_error("invalid number of function arguments %d, supported range is %d - %d", numargs, 0, MAX_FUNCTION_PARAMS - 1);
+        return kInstErr_InvalidArgNum;
     }
 
-    if (pc != 0) {
+    if (pc != 0)
+    {
         cc_error("instance already being executed");
-        return -4;
+        return kInstErr_Busy;
     }
 
     // NOTE: passing more parameters than expected by the function is fine:
@@ -417,7 +419,7 @@ int ccInstance::CallScriptFunction(const char *funcname, int32_t numargs, const 
             if (export_args > numargs) {
                 cc_error("Not enough parameters to exported function '%s' (expected %d, supplied %d)",
                     funcname, export_args, numargs);
-                return -1;
+                return kInstErr_Generic;
             }
             match = true;
         }
@@ -425,17 +427,18 @@ int ccInstance::CallScriptFunction(const char *funcname, int32_t numargs, const 
         if (match || (strcmp(thisExportName, funcname) == 0)) {
             const int32_t etype = (instanceof->export_addr[k] >> 24L) & 0x000ff;
             if (etype != EXPORT_FUNCTION) {
-                cc_error("symbol is not a function");
-                return -1;
+                cc_error("symbol '%s' is not a function", funcname);
+                return kInstErr_FuncNotFound;
             }
             startat = (instanceof->export_addr[k] & 0x00ffffff);
             break;
         }
     }
 
-    if (startat < 0) {
+    if (startat < 0)
+    {
         cc_error("function '%s' not found", funcname);
-        return -2;
+        return kInstErr_FuncNotFound;
     }
 
     // Prepare instance for run
@@ -457,14 +460,14 @@ int ccInstance::CallScriptFunction(const char *funcname, int32_t numargs, const 
 
     InstThreads.push_back(this); // push instance thread
     runningInst = this;
-    const int reterr = Run(startat);
+    const ccInstError reterr = Run(startat);
     // Cleanup before returning, even if error
     ASSERT_STACK_SIZE(numargs);
     PopValuesFromStack(numargs);
     pc = 0;
     currentline = 0;
     InstThreads.pop_back(); // pop instance thread
-    if (reterr != 0)
+    if (reterr != kInstErr_None)
         return reterr;
 
     // NOTE that if proper multithreading is added this will need
@@ -481,18 +484,18 @@ int ccInstance::CallScriptFunction(const char *funcname, int32_t numargs, const 
 
         if (flags & INSTF_FREE)
             Free();
-        return 100;
+        return kInstErr_Aborted;
     }
 
     ASSERT_STACK_UNWINDED(registers[SREG_SP], stackdata);
-    return cc_has_error();
+    return cc_has_error() ? kInstErr_Generic : kInstErr_None;
 }
 
 // Macros to maintain the call stack
 #define PUSH_CALL_STACK \
     if (callStackSize >= MAX_CALL_STACK) { \
         cc_error("CallScriptFunction stack overflow (recursive call error?)"); \
-        return -1; \
+        return kInstErr_Generic; \
     } \
     callStackLineNumber[callStackSize] = line_number;  \
     callStackCodeInst[callStackSize] = runningInst;  \
@@ -502,7 +505,7 @@ int ccInstance::CallScriptFunction(const char *funcname, int32_t numargs, const 
 #define POP_CALL_STACK \
     if (callStackSize < 1) { \
         cc_error("CallScriptFunction stack underflow -- internal error"); \
-        return -1; \
+        return kInstErr_Generic; \
     } \
     callStackSize--;\
     line_number = callStackLineNumber[callStackSize];\
@@ -580,7 +583,7 @@ inline bool FixupArgument(RuntimeScriptValue &arg, const int fixup, const uintpt
 
 
 #define MAXNEST 50  // number of recursive function calls allowed
-int ccInstance::Run(int32_t curpc)
+ccInstError ccInstance::Run(int32_t curpc)
 {
     pc = curpc;
     returnValue = -1;
@@ -588,7 +591,7 @@ int ccInstance::Run(int32_t curpc)
     if ((curpc < 0) || (curpc >= runningInst->codesize))
     {
         cc_error("specified code offset is not valid");
-        return -1;
+        return kInstErr_Generic;
     }
 
     int32_t thisbase[MAXNEST], funcstart[MAXNEST];
@@ -782,7 +785,7 @@ int ccInstance::Run(int32_t curpc)
             if (pc == 0)
             {
                 returnValue = registers[SREG_AX].IValue;
-                return 0;
+                return kInstErr_None;
             }
             POP_CALL_STACK;
             continue; // continue so that the PC doesn't get overwritten
@@ -831,7 +834,7 @@ int ccInstance::Run(int32_t curpc)
             if (reg2.IValue == 0)
             {
                 cc_error("!Integer divide by zero");
-                return -1;
+                return kInstErr_Generic;
             }
             reg1.SetInt32(reg1.IValue / reg2.IValue);
             break;
@@ -936,7 +939,7 @@ int ccInstance::Run(int32_t curpc)
             if (reg2.IValue == 0)
             {
                 cc_error("!Integer divide by zero");
-                return -1;
+                return kInstErr_Generic;
             }
             reg1.SetInt32(reg1.IValue % reg2.IValue);
             break;
@@ -954,7 +957,7 @@ int ccInstance::Run(int32_t curpc)
             if (curnest >= MAXNEST - 1)
             {
                 cc_error("!call stack overflow, recursive call problem?");
-                return -1;
+                return kInstErr_Generic;
             }
 
             PUSH_CALL_STACK;
@@ -1056,7 +1059,7 @@ int ccInstance::Run(int32_t curpc)
                     (++loopCheckIterations > _maxWhileLoops))
                 {
                     cc_error("!Script appears to be hung (a while loop ran %d times). The problem may be in a calling function; check the call stack.", loopCheckIterations);
-                    return -1;
+                    return kInstErr_Generic;
                 }
                 else if ((loopIterations & 0x3FF) == 0 && // test each 1024 loops (arbitrary)
                     (std::chrono::duration_cast<std::chrono::milliseconds>(
@@ -1085,7 +1088,7 @@ int ccInstance::Run(int32_t curpc)
                 (reg1.IValue >= arg_lit))
             {
                 cc_error("!Array index out of bounds (index: %d, bounds: 0..%d)", reg1.IValue, arg_lit - 1);
-                return -1;
+                return kInstErr_Generic;
             }
             break;
         }
@@ -1110,7 +1113,7 @@ int ccInstance::Run(int32_t curpc)
                     int elementSize = (hdr.TotalSize / elem_count);
                     cc_error("!Array index out of bounds (index: %d, bounds: 0..%d)", reg1.IValue / elementSize, elem_count - 1);
                 }
-                return -1;
+                return kInstErr_Generic;
             }
             break;
         }
@@ -1157,7 +1160,7 @@ int ccInstance::Run(int32_t curpc)
 
             int32_t newHandle = ccGetObjectHandleFromAddress(address);
             if (newHandle == -1)
-                return -1;
+                return kInstErr_Generic;
 
             if (handle != newHandle)
             {
@@ -1198,7 +1201,7 @@ int ccInstance::Run(int32_t curpc)
             // like memwriteptr, but doesn't attempt to free the old one
             int32_t newHandle = ccGetObjectHandleFromAddress(address);
             if (newHandle == -1)
-                return -1;
+                return kInstErr_Generic;
 
             ccAddObjectReference(newHandle);
             registers[SREG_MAR].WriteInt32(newHandle);
@@ -1230,7 +1233,7 @@ int ccInstance::Run(int32_t curpc)
             if (registers[SREG_MAR].IsNull())
             {
                 cc_error("!Null pointer referenced");
-                return -1;
+                return kInstErr_Generic;
             }
             break;
         case SCMD_CHECKNULLREG:
@@ -1239,7 +1242,7 @@ int ccInstance::Run(int32_t curpc)
             if (reg1.IsNull())
             {
                 cc_error("!Null string referenced");
-                return -1;
+                return kInstErr_Generic;
             }
             break;
         }
@@ -1286,12 +1289,12 @@ int ccInstance::Run(int32_t curpc)
             if (callAddr % sizeof(uintptr_t) != 0)
             {
                 cc_error("call address not aligned");
-                return -1;
+                return kInstErr_Generic;
             }
             callAddr /= sizeof(uintptr_t); // size of ccScript::code elements
 
             if (Run(static_cast<int32_t>(callAddr)))
-                return -1;
+                return kInstErr_Generic;
 
             runningInst = wasRunning;
 
@@ -1379,7 +1382,7 @@ int ccInstance::Run(int32_t curpc)
 
             if (cc_has_error())
             {
-                return -1;
+                return kInstErr_Generic;
             }
 
             registers[SREG_AX] = return_value;
@@ -1412,7 +1415,7 @@ int ccInstance::Run(int32_t curpc)
             if (reg1.IsNull())
             {
                 cc_error("!Null pointer referenced");
-                return -1;
+                return kInstErr_Generic;
             }
             switch (reg1.Type)
             {
@@ -1436,7 +1439,7 @@ int ccInstance::Run(int32_t curpc)
                 break;
             default:
                 cc_error("internal error: SCMD_CALLOBJ argument is not an object of built-in or user-defined type");
-                return -1;
+                return kInstErr_Generic;
             }
             next_call_needs_object = 1;
             break;
@@ -1470,7 +1473,7 @@ int ccInstance::Run(int32_t curpc)
             if (numElements < 1)
             {
                 cc_error("invalid size for dynamic array; requested: %d, range: 1..%d", numElements, INT32_MAX);
-                return -1;
+                return kInstErr_Generic;
             }
             DynObjectRef ref = CCDynamicArray::Create(numElements, arg_elsize, arg_managed);
             reg1.SetScriptObject(ref.Obj, &globalDynamicArray);
@@ -1483,7 +1486,7 @@ int ccInstance::Run(int32_t curpc)
             if (arg_size < 0)
             {
                 cc_error("Invalid size for user object; requested: %d (or %d), range: 0..%d", arg_size, arg_size, INT_MAX);
-                return -1;
+                return kInstErr_Generic;
             }
             DynObjectRef ref = ScriptUserObject::Create(arg_size);
             reg1.SetScriptObject(ref.Obj, ref.Mgr);
@@ -1517,7 +1520,7 @@ int ccInstance::Run(int32_t curpc)
             if (reg2.FValue == 0.0)
             {
                 cc_error("!Floating point divide by zero");
-                return -1;
+                return kInstErr_Generic;
             }
             reg1.SetFloat(reg1.FValue / reg2.FValue);
             break;
@@ -1581,7 +1584,7 @@ int ccInstance::Run(int32_t curpc)
             {
                 cc_error("internal error: stack tail address expected on SCMD_ZEROMEMORY instruction, reg[MAR] type is %d",
                     registers[SREG_MAR].Type);
-                return -1;
+                return kInstErr_Generic;
             }
             break;
         }
@@ -1600,7 +1603,7 @@ int ccInstance::Run(int32_t curpc)
             if ((reg1.IsNull()) || (reg2.IsNull()))
             {
                 cc_error("!Null pointer referenced");
-                return -1;
+                return kInstErr_Generic;
             }
             else
             {
@@ -1617,7 +1620,7 @@ int ccInstance::Run(int32_t curpc)
             if ((reg1.IsNull()) || (reg2.IsNull()))
             {
                 cc_error("!Null pointer referenced");
-                return -1;
+                return kInstErr_Generic;
             }
             else
             {
@@ -1633,14 +1636,14 @@ int ccInstance::Run(int32_t curpc)
             break;
         default:
             cc_error("instruction %d is not implemented", codeOp.Instruction.Code);
-            return -1;
+            return kInstErr_Generic;
         }
         /* End perform operation */
         //=====================================================================
 
         pc += codeOp.ArgCount + 1;
     }
-    return 0;
+    return kInstErr_None;
 }
 
 String ccInstance::GetCallStack(const int maxLines) const

Engine/script/cc_instance.h
@@ -112,6 +112,15 @@ struct ScriptPosition
     int32_t         Line;
 };
 
+enum ccInstError
+{
+    kInstErr_None = 0, // ok
+    kInstErr_Aborted = 100, // aborted by request
+    kInstErr_Generic = -1, // any generic exec error; use cc_get_error()
+    kInstErr_FuncNotFound = -2, // requested function is not found in script
+    kInstErr_InvalidArgNum = -3, // invalid number of args (not in supported range)
+    kInstErr_Busy = -4, // instance is busy executing script
+};
 
 // Running instance of the script
 struct ccInstance
@@ -181,7 +190,7 @@ public:
     void    AbortAndDestroy();
     
     // Call an exported function in the script
-    int     CallScriptFunction(const char *funcname, int32_t num_params, const RuntimeScriptValue *params);
+    ccInstError CallScriptFunction(const char *funcname, int32_t num_params, const RuntimeScriptValue *params);
     
     // Get the script's execution position and callstack as human-readable text
     Common::String GetCallStack(int max_lines = INT_MAX) const;
@@ -213,7 +222,7 @@ private:
     bool    CreateRuntimeCodeFixups(const ccScript *scri);
 
     // Begin executing script starting from the given bytecode index
-    int     Run(int32_t curpc);
+    ccInstError Run(int32_t curpc);
 
     // Stack processing
     // Push writes new value and increments stack ptr;

Engine/script/script.h
@@ -95,23 +95,31 @@ void    run_unhandled_event(const ObjectEvent &obj_evt, int evnt);
 int     create_global_script();
 void    cancel_all_scripts();
 
+enum RunScFuncResult
+{
+    kScFnRes_Done = 0,
+    kScFnRes_GenericInstError = -1, // running instance generic failure
+    kScFnRes_NotFound = -2,         // function not found
+    kScFnRes_ScriptBusy = -3,       // script is already being executed
+};
+
 ccInstance *GetScriptInstanceByType(ScriptType sc_type);
 // Queues a script function to be run either called by the engine or from another script
 void    QueueScriptFunction(ScriptType sc_type, const String &fn_name, size_t param_count = 0,
     const RuntimeScriptValue *params = nullptr);
 // Try to run a script function on a given script instance
-int     RunScriptFunction(ccInstance *sci, const String &tsname, size_t param_count = 0,
+RunScFuncResult RunScriptFunction(ccInstance *sci, const String &tsname, size_t param_count = 0,
     const RuntimeScriptValue *params = nullptr);
 // Run a script function in all the regular script modules, in order, where available
 // includes globalscript, but not the current room script.
 void    RunScriptFunctionInModules(const String &tsname, size_t param_count = 0,
     const RuntimeScriptValue *params = nullptr);
 // Run an obligatory script function in the current room script
-int     RunScriptFunctionInRoom(const String &tsname, size_t param_count = 0,
+void    RunScriptFunctionInRoom(const String &tsname, size_t param_count = 0,
     const RuntimeScriptValue *params = nullptr);
 // Try to run a script function, guessing the behavior by its name and script instance type;
 // depending on the type may run a claimable callback chain
-int     RunScriptFunctionAuto(ScriptType sc_type, const String &fn_name, size_t param_count = 0,
+void   RunScriptFunctionAuto(ScriptType sc_type, const String &fn_name, size_t param_count = 0,
     const RuntimeScriptValue *params = nullptr);
 
 // Preallocates script module instances

commit 9ec173608eb76ef814bd62b37240075b3fafefc2

Engine: fixed running claimable events, and "on_call" in "game_start"
Was broken by 45e50484f4240d6d1144384162c65b8b129ba0aa

Engine/ac/event.cpp
@@ -87,6 +87,7 @@ void run_claimable_event(const String &tsname, bool includeRoom, int numParams, 
         if (eventClaimed == EVENT_CLAIMED)
         {
             eventClaimed = eventClaimedOldValue;
+            return;
         }
     }
 
@@ -97,6 +98,7 @@ void run_claimable_event(const String &tsname, bool includeRoom, int numParams, 
         if (eventClaimed == EVENT_CLAIMED)
         {
             eventClaimed = eventClaimedOldValue;
+            return;
         }
     }
 

Engine/script/script.cpp
@@ -450,6 +450,8 @@ void RunScriptFunctionInModules(const String &tsname, size_t param_count, const 
 
 void RunScriptFunctionInRoom(const String &tsname, size_t param_count, const RuntimeScriptValue *params)
 {
+    if (!roominst)
+        return; // room is not loaded yet
     // Some room callbacks are considered to be obligatory; for historical reasons these are
     // identified by having no parameters;
     // FIXME: this is a hack, this should be defined either by function type, or as an arg
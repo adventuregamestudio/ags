ivan-mogilko/ags-refactoring

Script API: implemented a number of global events for Dialogs
* eEventDialogStart
* eEventDialogStop
* eEventDialogRun
* eEventDialogOptionsOpen
* eEventDialogOptionsClose

Engine/ac/event.cpp
@@ -106,10 +106,10 @@ int run_claimable_event(const char *tsname, bool includeRoom, int numParams, con
 }
 
 // runs the global script on_event function
-void run_on_event(int evtype, RuntimeScriptValue &wparam)
+void run_on_event(AGSScriptEventType evtype, const RuntimeScriptValue &data1, const RuntimeScriptValue &data2)
 {
-    RuntimeScriptValue params[]{ evtype , wparam };
-    QueueScriptFunction(kScInstGame, "on_event", 2, params);
+    RuntimeScriptValue params[]{ evtype , data1, data2 };
+    QueueScriptFunction(kScInstGame, "on_event", 3, params);
 }
 
 void run_room_event(int id) {

adventuregamestudio/ags

Engine/ac/event.cpp
@@ -107,10 +107,10 @@ void run_claimable_event(const String &tsname, bool includeRoom, int numParams, 
 }
 
 // runs the global script on_event function
-void run_on_event(int evtype, RuntimeScriptValue &wparam)
+void run_on_event(AGSScriptEventType evtype, const RuntimeScriptValue &data1, const RuntimeScriptValue &data2)
 {
-    RuntimeScriptValue params[]{ evtype , wparam };
-    QueueScriptFunction(kScTypeGame, "on_event", 2, params);
+    RuntimeScriptValue params[]{ evtype , data1, data2 };
+    QueueScriptFunction(kScTypeGame, "on_event", 3, params);
 }
 
 void run_room_event(int id) {
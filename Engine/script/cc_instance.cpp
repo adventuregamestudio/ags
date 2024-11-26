adventuregamestudio/ags

commit 1f6abf4b678f77825c710d4ae18b57ff8f882c93

Engine: store game's ccInstances as unique_ptrs instead
The script handling part of the engine appeared to be too fragile and dependent on a precise timing of **deleting** a ccInstance.
For example, restoring a saved game may execute in the midst of the call to RunScriptFunction(), at the "post script cleanup" stage, while there are still references to ccInstance on the engine's call stack (and few other places). If anything prevents ccInstance's disposal at this moment, that would in turn lead to logical errors due to the game scripts "instances" counting. The most outstanding consequence is that the script exports are kept pointing into the deleted memory and not re-registered after the save is restored.

Overall, the script handling part of the engine would benefit from a major refactor, with a goal to make it safe and consistent, but this is not something I might do at the moment.

Engine/script/cc_instance.cpp
@@ -219,15 +219,15 @@ ccInstance *ccInstance::GetCurrentInstance()
     return InstThreads.size() > 0 ? InstThreads.back() : nullptr;
 }
 
-PInstance ccInstance::CreateFromScript(PScript scri)
+ccInstance *ccInstance::CreateFromScript(PScript scri)
 {
     return CreateEx(scri, nullptr);
 }
 
-PInstance ccInstance::CreateEx(PScript scri, ccInstance * joined)
+ccInstance *ccInstance::CreateEx(PScript scri, ccInstance *joined)
 {
     // allocate and copy all the memory with data, code and strings across
-    std::shared_ptr<ccInstance> cinst(new ccInstance());
+    ccInstance *cinst = new ccInstance();
     if (!cinst->_Create(scri, joined))
     {
         return nullptr;
@@ -278,7 +278,7 @@ ccInstance::~ccInstance()
     Free();
 }
 
-PInstance ccInstance::Fork()
+ccInstance *ccInstance::Fork()
 {
     return CreateEx(instanceof, this);
 }
@@ -1926,6 +1926,8 @@ bool ccInstance::_Create(PScript scri, ccInstance * joined)
 
 void ccInstance::Free()
 {
+    // When the base script has no more "instances",
+    // remove all script exports
     if (instanceof != nullptr) {
         instanceof->instances--;
         if (instanceof->instances == 0)

Engine/ac/dialog.cpp
@@ -195,7 +195,7 @@ int run_dialog_script(int dialogID, int offse, int optionIndex) {
     char func_name[100];
     snprintf(func_name, sizeof(func_name), "_run_dialog%d", dialogID);
     RuntimeScriptValue params[]{ optionIndex };
-    RunScriptFunction(dialogScriptsInst, func_name, 1, params);
+    RunScriptFunction(dialogScriptsInst.get(), func_name, 1, params);
     result = dialogScriptsInst->returnValue;
   }
   else

Engine/ac/event.cpp
@@ -75,7 +75,7 @@ int run_claimable_event(const char *tsname, bool includeRoom, int numParams, con
     int toret;
 
     if (includeRoom && roominst) {
-        toret = RunScriptFunction(roominst, tsname, numParams, params);
+        toret = RunScriptFunction(roominst.get(), tsname, numParams, params);
 
         if (eventClaimed == EVENT_CLAIMED) {
             eventClaimed = eventClaimedOldValue;
@@ -85,7 +85,7 @@ int run_claimable_event(const char *tsname, bool includeRoom, int numParams, con
 
     // run script modules
     for (auto &module_inst : moduleInst) {
-        toret = RunScriptFunction(module_inst, tsname, numParams, params);
+        toret = RunScriptFunction(module_inst.get(), tsname, numParams, params);
 
         if (eventClaimed == EVENT_CLAIMED) {
             eventClaimed = eventClaimedOldValue;

Engine/ac/room.cpp
@@ -1005,7 +1005,7 @@ void check_new_room() {
 void compile_room_script() {
     cc_clear_error();
 
-    roominst = ccInstance::CreateFromScript(thisroom.CompiledScript);
+    roominst.reset(ccInstance::CreateFromScript(thisroom.CompiledScript));
     if ((cc_has_error()) || (roominst==nullptr)) {
         quitprintf("Unable to create local script:\n%s", cc_get_error().ErrorString.GetCStr());
     }
@@ -1016,7 +1016,7 @@ void compile_room_script() {
     if (!roominst->ResolveImportFixups(roominst->instanceof.get()))
         quitprintf("Unable to resolve import fixups in room script:\n%s", cc_get_error().ErrorString.GetCStr());
 
-    roominstFork = roominst->Fork();
+    roominstFork.reset(roominst->Fork());
     if (roominstFork == nullptr)
         quitprintf("Unable to create forked room instance:\n%s", cc_get_error().ErrorString.GetCStr());
 

Engine/game/game_init.cpp
@@ -74,10 +74,6 @@ extern ScriptAudioChannel scrAudioChannel[MAX_GAME_CHANNELS];
 extern ScriptDialogOptionsRendering ccDialogOptionsRendering;
 extern ScriptDrawingSurface* dialogOptionsRenderingSurface;
 
-extern std::vector<PInstance> moduleInst;
-extern std::vector<PInstance> moduleInstFork;
-extern std::vector<RuntimeScriptValue> moduleRepExecAddr;
-
 // Old dialog support (defined in ac/dialog)
 extern std::vector<std::vector<uint8_t>> old_dialog_scripts;
 extern std::vector<String> old_speech_lines;

Engine/plugin/agsplugin.cpp
@@ -657,16 +657,18 @@ int IAGSEngine::IsSpriteAlphaBlended(int32 slot) {
 void IAGSEngine::DisableSound() {
     shutdown_sound();
 }
+
 int IAGSEngine::CanRunScriptFunctionNow() {
     if (inside_script)
         return 0;
     return 1;
 }
+
 int IAGSEngine::CallGameScriptFunction(const char *name, int32 globalScript, int32 numArgs, long arg1, long arg2, long arg3) {
     if (inside_script)
         return -300;
 
-    PInstance toRun = GetScriptInstanceByType(globalScript ? kScInstGame : kScInstRoom);
+    ccInstance *toRun = GetScriptInstanceByType(globalScript ? kScInstGame : kScInstRoom);
 
     RuntimeScriptValue params[]{
         RuntimeScriptValue().SetPluginArgument(arg1),

Engine/script/cc_instance.h
@@ -113,9 +113,6 @@ struct ScriptPosition
 };
 
 
-struct ccInstance;
-typedef std::shared_ptr<ccInstance> PInstance;
-
 // Running instance of the script
 struct ccInstance
 {
@@ -165,14 +162,14 @@ public:
     // returns the currently executing instance, or NULL if none
     static ccInstance *GetCurrentInstance(void);
     // create a runnable instance of the supplied script
-    static PInstance CreateFromScript(PScript script);
-    static PInstance CreateEx(PScript scri, ccInstance * joined);
+    static ccInstance *CreateFromScript(PScript script);
+    static ccInstance *CreateEx(PScript scri, ccInstance * joined);
     static void SetExecTimeout(unsigned sys_poll_ms, unsigned abort_ms, unsigned abort_loops);
 
     ccInstance();
     ~ccInstance();
     // Create a runnable instance of the same script, sharing global memory
-    PInstance Fork();
+    ccInstance *Fork();
     // Specifies that when the current function returns to the script, it
     // will stop and return from CallInstance
     void    Abort();

Engine/script/executingscript.cpp
@@ -68,19 +68,3 @@ void ExecutingScript::run_another(const char *namm, ScriptInstType scinst, size_
     for (size_t p = 0; p < MAX_QUEUED_PARAMS && p < param_count; ++p)
         script.Params[p] = params[p];
 }
-
-void ExecutingScript::init() {
-    inst = nullptr;
-    forked = false;
-    numanother = 0;
-    numPostScriptActions = 0;
-
-    memset(postScriptActions, 0, sizeof(postScriptActions));
-    memset(postScriptActionNames, 0, sizeof(postScriptActionNames));
-    memset(postScriptSaveSlotDescription, 0, sizeof(postScriptSaveSlotDescription));
-    memset(postScriptActionData, 0, sizeof(postScriptActionData));
-}
-
-ExecutingScript::ExecutingScript() {
-    init();
-}

commit 34dcd62023d4bc6d64bcdfc24f7a7a2d6cc56d4c

Engine: Fix memory leak in ccInstance::CreateEx when it fails
This is a regression from commit 1f6abf4. Before that commit it
used a std::shared_ptr which means the pointer was automatically
deleted.

Engine/script/cc_instance.cpp
@@ -234,6 +234,7 @@ ccInstance *ccInstance::CreateEx(PScript scri, const ccInstance *joined)
     ccInstance *cinst = new ccInstance();
     if (!cinst->_Create(scri, joined))
     {
+        delete cinst;
         return nullptr;
     }
     return cinst;
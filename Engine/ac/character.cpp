adventuregamestudio/ags

commit db71d928a6f290172975209d36ba7b7e22881f57

Engine: picked out view-loop-frame Animate params check, fixup sframe

Engine/ac/character.cpp
@@ -182,6 +182,7 @@ void Character_AddWaypoint(CharacterInfo *chaa, int x, int y) {
 void Character_Animate(CharacterInfo *chaa, int loop, int delay, int repeat,
     int blocking, int direction, int sframe, int volume)
 {
+    ValidateViewAnimVLF("Character.Animate", chaa->view, loop, sframe);
     ValidateViewAnimParams("Character.Animate", repeat, blocking, direction);
 
     animate_character(chaa, loop, delay, repeat, 0, direction, sframe, volume);
@@ -2122,39 +2123,37 @@ void setup_player_character(int charid) {
     }
 }
 
+// Animate character internal implementation;
+// this function may be called by the game logic too, so we assume
+// the arguments must be correct, and do not fix them up as we do for API functions.
 void animate_character(CharacterInfo *chap, int loopn, int sppd, int rept,
     int noidleoverride, int direction, int sframe, int volume)
 {
-    if ((chap->view < 0) || (chap->view > game.numviews)) {
-        quitprintf("!AnimateCharacter: you need to set the view number first\n"
-            "(trying to animate '%s' using loop %d. View is currently %d).",chap->name,loopn,chap->view+1);
-    }
-    debug_script_log("%s: Start anim view %d loop %d, spd %d, repeat %d, frame: %d",
-        chap->scrname, chap->view+1, loopn, sppd, rept, sframe);
-    if ((chap->idleleft < 0) && (noidleoverride == 0)) {
-        // if idle view in progress for the character (and this is not the
-        // "start idle animation" animate_character call), stop the idle anim
+    // If idle view in progress for the character (and this is not the
+    // "start idle animation" animate_character call), stop the idle anim
+    if ((chap->idleleft < 0) && (noidleoverride == 0))
+    {
         Character_UnlockView(chap);
         chap->idleleft=chap->idletime;
     }
-    if ((loopn < 0) || (loopn >= views[chap->view].numLoops)) {
-        quitprintf("!AnimateCharacter: invalid loop number\n"
-            "(trying to animate '%s' using loop %d. View is currently %d).",chap->name,loopn,chap->view+1);
+
+    if ((chap->view < 0) || (chap->view > game.numviews) ||
+        (loopn < 0) || (loopn >= views[chap->view].numLoops))
+    {
+        quitprintf("!AnimateCharacter: invalid view and/or loop\n"
+            "(trying to animate '%s' using view %d (range is 1..%d) and loop %d (view has %d loops)).",
+            chap->name, chap->view + 1, game.numviews, loopn, views[chap->view].numLoops);
     }
-    if ((sframe < 0) || (sframe >= views[chap->view].loops[loopn].numFrames))
-        quit("!AnimateCharacter: invalid starting frame number specified");
+    // NOTE: there's always frame 0 allocated for safety
+    sframe = std::max(0, std::min(sframe, views[chap->view].loops[loopn].numFrames - 1));
+    debug_script_log("%s: Start anim view %d loop %d, spd %d, repeat %d, frame: %d",
+        chap->scrname, chap->view+1, loopn, sppd, rept, sframe);
+
     Character_StopMoving(chap);
 
     chap->set_animating(rept != 0, direction == 0, sppd);
-
     chap->loop=loopn;
-    // reverse animation starts at the *previous frame*
-    if (direction) {
-        sframe--;
-        if (sframe < 0)
-            sframe = views[chap->view].loops[loopn].numFrames - (-sframe);
-    }
-    chap->frame = sframe;
+    chap->frame = SetFirstAnimFrame(chap->view, loopn, sframe, direction);
     chap->wait = sppd + views[chap->view].loops[loopn].frames[chap->frame].speed;
     charextra[chap->index_id].cur_anim_volume = Math::Clamp(volume, 0, 100);
 

Engine/ac/button.cpp
@@ -55,27 +55,14 @@ void Button_Animate(GUIButton *butt, int view, int loop, int speed, int repeat,
     int guin = butt->ParentId;
     int objn = butt->Id;
 
-    if ((view < 1) || (view > game.numviews))
-        quit("!AnimateButton: invalid view specified");
-    view--;
-    if ((loop < 0) || (loop >= views[view].numLoops))
-        quit("!AnimateButton: invalid loop specified for view");
-    if (sframe < 0 || sframe >= views[view].loops[loop].numFrames)
-        quit("!AnimateButton: invalid starting frame number specified");
-
+    view--; // convert to 0-based view ID
+    ValidateViewAnimVLF("Button.Animate", view, loop, sframe);
     ValidateViewAnimParams("Button.Animate", repeat, blocking, direction);
 
     volume = Math::Clamp(volume, 0, 100);
 
     // if it's already animating, stop it
     FindAndRemoveButtonAnimation(guin, objn);
-
-    // reverse animation starts at the *previous frame*
-    if (direction)
-    {
-        if (--sframe < 0)
-            sframe = views[view].loops[loop].numFrames - (-sframe);
-    }
 
     int but_id = guis[guin].GetControlID(objn);
     AnimatingGUIButton abtn;
@@ -88,7 +75,7 @@ void Button_Animate(GUIButton *butt, int view, int loop, int speed, int repeat,
     abtn.repeat = static_cast<bool>(repeat) ? ANIM_REPEAT : ANIM_ONCE; // for now, clamp to supported modes
     abtn.blocking = blocking;
     abtn.direction = direction;
-    abtn.frame = sframe;
+    abtn.frame = SetFirstAnimFrame(view, loop, sframe, direction);
     abtn.wait = abtn.speed + views[abtn.view].loops[abtn.loop].frames[abtn.frame].speed;
     abtn.volume = volume;
     animbuts.push_back(abtn);

Engine/ac/global_object.cpp
@@ -234,21 +234,9 @@ void AnimateObjectImpl(int obn, int loopn, int spdd, int rept, int direction, in
 
     if (obj.view == RoomObject::NoView)
         quit("!AnimateObject: object has not been assigned a view");
-    if (loopn < 0 || loopn >= views[obj.view].numLoops)
-        quit("!AnimateObject: invalid loop number specified");
-    if (views[obj.view].loops[loopn].numFrames < 1)
-        quit("!AnimateObject: no frames in the specified view loop");
-    if (sframe < 0 || sframe >= views[obj.view].loops[loopn].numFrames)
-        quit("!AnimateObject: invalid starting frame number specified"); 
 
+    ValidateViewAnimVLF("Object.Animate", obj.view, loopn, sframe);
     ValidateViewAnimParams("Object.Animate", rept, blocking, direction);
-
-    // reverse animation starts at the *previous frame*
-    if (direction) {
-        sframe--;
-        if (sframe < 0)
-            sframe = views[obj.view].loops[loopn].numFrames - (-sframe);
-    }
 
     if (loopn > UINT16_MAX || sframe > UINT16_MAX)
     {
@@ -257,11 +245,12 @@ void AnimateObjectImpl(int obn, int loopn, int spdd, int rept, int direction, in
         return;
     }
 
-    debug_script_log("Obj %d start anim view %d loop %d, speed %d, repeat %d, frame %d", obn, obj.view +1, loopn, spdd, rept, sframe);
+    debug_script_log("Obj %d start anim view %d loop %d, speed %d, repeat %d, frame %d",
+        obn, obj.view + 1, loopn, spdd, rept, sframe);
 
     obj.set_animating(rept, direction == 0, spdd);
     obj.loop = (uint16_t)loopn;
-    obj.frame = (uint16_t)sframe;
+    obj.frame = (uint16_t)SetFirstAnimFrame(obj.view, loopn, sframe, direction);
     obj.wait = spdd+views[obj.view].loops[loopn].frames[obj.frame].speed;
     int pic = views[obj.view].loops[loopn].frames[obj.frame].pic;
     obj.num = Math::InRangeOrDef<uint16_t>(pic, 0);

Engine/ac/object.cpp
@@ -584,6 +584,39 @@ void ValidateViewAnimParams(const char *apiname, int &repeat, int &blocking, int
     }
 }
 
+void ValidateViewAnimVLF(const char *apiname, int view, int loop, int &sframe)
+{
+    if ((view < 1) || (view > game.numviews))
+        quitprintf("!%s: invalid view (range is 1..%d.", apiname, view + 1, game.numviews);
+    if (views[view].numLoops == 0)
+        quitprintf("!%s: view %d does not have any loops.", apiname, view + 1);
+    if (loop < 0 || loop >= views[view].numLoops)
+        quitprintf("!%s: invalid loop number for view %d (range is 0..%d).", apiname, view + 1, views[view].numLoops);
+
+    if (views[view].loops[loop].numFrames < 1)
+        debug_script_warn("%s: view %d loop %d does not have any frames, will use a frame placeholder.",
+            apiname, view + 1, loop);
+    else if (sframe < 0 || sframe >= views[view].loops[loop].numFrames)
+        debug_script_warn("%s: invalid starting frame number for view %d loop %d (range is 0..%d)",
+            view + 1, loop, views[view].loops[loop].numFrames);
+    // NOTE: there's always frame 0 allocated for safety
+    sframe = std::max(0, std::min(sframe, views[view].loops[loop].numFrames - 1));
+}
+
+int SetFirstAnimFrame(int view, int loop, int sframe, int direction)
+{
+    if (views[view].loops[loop].numFrames <= 1)
+        return 0;
+    // reverse animation starts at the *previous frame*
+    if (direction != 0)
+    {
+        sframe--;
+        if (sframe < 0)
+            sframe = views[view].loops[loop].numFrames - (-sframe);
+    }
+    return sframe;
+}
+
 // General view animation algorithm: find next loop and frame, depending on anim settings
 bool CycleViewAnim(int view, uint16_t &o_loop, uint16_t &o_frame, bool forwards, int repeat)
 {

Engine/ac/object.h
@@ -85,10 +85,18 @@ int     is_pos_in_sprite(int xx,int yy,int arx,int ary, Common::Bitmap *sprit, i
 // X and Y co-ordinates must be in native format
 // X and Y are ROOM coordinates
 int     check_click_on_object(int roomx, int roomy, int mood);
+
+// Shared functions that prepare or advance the view animation;
+// used by characters, room objects and buttons.
 // TODO: pick out some kind of "animation" struct
 // Tests if the standard animate parameters are in valid range, if not then clamps them and
 // reports a script warning.
-void   ValidateViewAnimParams(const char *apiname, int &repeat, int &blocking, int &direction);
+void    ValidateViewAnimParams(const char *apiname, int &repeat, int &blocking, int &direction);
+// Tests if the view, loop, frame animate params are in valid range,
+// errors in case of out-of-range view or loop, but clamps a frame to a range.
+void    ValidateViewAnimVLF(const char *apiname, int view, int loop, int &sframe);
+// Calculates the first shown frame for a view animation, depending on parameters.
+int     SetFirstAnimFrame(int view, int loop, int sframe, int direction);
 // General view animation algorithm: find next loop and frame, depending on anim settings;
 // loop and frame values are passed by reference and will be updated;
 // returns whether the animation should continue.

commit b63ae88ee064ca14dfd0a50cb5e7be6d40a391ad

Engine: fix Character.Animate in case called during idling
This fixes a regression after db71d928a6f290172975209d36ba7b7e22881f57
in case Character.Animate() called during idling, and Loop number assertion is done prior to view changing back to normal view, could cause game to error.

Pick out "stop_character_idling" function that checks idle state and releases the locked idle view.

Engine/ac/character.cpp
@@ -111,6 +111,16 @@ bool is_valid_character(int char_id)
     return ((char_id >= 0) && (char_id < game.numcharacters));
 }
 
+// Checks if character is currently playing idle anim, and reset it
+static void stop_character_idling(CharacterInfo *chi)
+{
+    if (chi->idleleft < 0)
+    {
+        Character_UnlockView(chi);
+        chi->idleleft = chi->idletime;
+    }
+}
+
 bool AssertCharacter(const char *apiname, int char_id)
 {
     if ((char_id >= 0) && (char_id < game.numcharacters))
@@ -208,10 +218,14 @@ void Character_AddWaypoint(CharacterInfo *chaa, int x, int y) {
 void Character_Animate(CharacterInfo *chaa, int loop, int delay, int repeat,
     int blocking, int direction, int sframe, int volume)
 {
+    // If idle view in progress for the character, stop the idle anim;
+    // do this prior to the loop check, as the view may switch back to defview here
+    stop_character_idling(chaa);
+
     ValidateViewAnimVLF("Character.Animate", chaa->view, loop, sframe);
     ValidateViewAnimParams("Character.Animate", repeat, blocking, direction);
 
-    animate_character(chaa, loop, delay, repeat, 0, direction, sframe, volume);
+    animate_character(chaa, loop, delay, repeat, direction, sframe, volume);
 
     if (blocking != 0)
         GameLoopUntilValueIsZero(&chaa->animating);
@@ -302,10 +316,7 @@ void Character_ChangeView(CharacterInfo *chap, int vii) {
         debug_script_warn("Warning: ChangeCharacterView was used while the view was fixed - call ReleaseCharView first");
 
     // if the idle animation is playing we should release the view
-    if ( chap->idleleft < 0) {
-      Character_UnlockView(chap);
-      chap->idleleft = chap->idletime;
-    }
+    stop_character_idling(chap);
 
     debug_script_log("%s: Change view to %d", chap->scrname, vii+1);
     chap->defview = vii;
@@ -624,11 +635,7 @@ void Character_LockViewEx(CharacterInfo *chap, int vii, int stopMoving) {
     vii--; // convert to 0-based
     AssertView("SetCharacterView", vii);
 
-    debug_script_log("%s: View locked to %d", chap->scrname, vii+1);
-    if (chap->idleleft < 0) {
-        Character_UnlockView(chap);
-        chap->idleleft = chap->idletime;
-    }
+    stop_character_idling(chap);
     if (stopMoving != KEEP_MOVING)
     {
         Character_StopMoving(chap);
@@ -641,6 +648,7 @@ void Character_LockViewEx(CharacterInfo *chap, int vii, int stopMoving) {
     chap->flags|=CHF_FIXVIEW;
     chap->pic_xoffs = 0;
     chap->pic_yoffs = 0;
+    debug_script_log("%s: View locked to %d", chap->scrname, vii+1);
 }
 
 void Character_LockViewAligned_Old(CharacterInfo *chap, int vii, int loop, int align) {
@@ -840,8 +848,7 @@ void Character_SetIdleView(CharacterInfo *chaa, int iview, int itime) {
         quit("!SetCharacterIdle: view 1 cannot be used as an idle view, sorry.");
 
     // if an idle anim is currently playing, release it
-    if (chaa->idleleft < 0)
-        Character_UnlockView(chaa);
+    stop_character_idling(chaa);
 
     chaa->idleview = iview - 1;
     // make sure they don't appear idle while idle anim is disabled
@@ -1725,11 +1732,8 @@ void walk_character(int chac,int tox,int toy,int ignwal, bool autoWalkAnims) {
 
     if ((chin->animating) && (autoWalkAnims))
         stop_character_anim(chin);
-
-    if (chin->idleleft < 0) {
-        ReleaseCharacterView(chac);
-        chin->idleleft=chin->idletime;
-    }
+    // Stop idling anim
+    stop_character_idling(chin);
     // stop them to make sure they're on a walkable area
     // but save their frame first so that if they're already
     // moving it looks smoother
@@ -2207,16 +2211,8 @@ void setup_player_character(int charid) {
 // this function may be called by the game logic too, so we assume
 // the arguments must be correct, and do not fix them up as we do for API functions.
 void animate_character(CharacterInfo *chap, int loopn, int sppd, int rept,
-    int noidleoverride, int direction, int sframe, int volume)
+    int direction, int sframe, int volume)
 {
-    // If idle view in progress for the character (and this is not the
-    // "start idle animation" animate_character call), stop the idle anim
-    if ((chap->idleleft < 0) && (noidleoverride == 0))
-    {
-        Character_UnlockView(chap);
-        chap->idleleft=chap->idletime;
-    }
-
     if ((chap->view < 0) || (chap->view > game.numviews) ||
         (loopn < 0) || (loopn >= views[chap->view].numLoops))
     {
@@ -2579,8 +2575,9 @@ void _displayspeech(const char*texx, int aschar, int xx, int yy, int widd, int i
         allowShrink = 1;
 
     // If has a valid speech view, and idle anim in progress for the character, then stop it
-    if (useview >= 0 && speakingChar->idleleft < 0)  {
-        ReleaseCharacterView(aschar);
+    if (useview >= 0)
+    {
+        stop_character_idling(speakingChar);
     }
 
     int tdxp = xx,tdyp = yy;

Engine/ac/character.h
@@ -170,7 +170,7 @@ using namespace AGS; // FIXME later
 
 // Configures and starts character animation.
 void animate_character(CharacterInfo *chap, int loopn, int sppd, int rept,
-    int noidleoverride = 0, int direction = 0, int sframe = 0, int volume = 100);
+    int direction = 0, int sframe = 0, int volume = 100);
 // Clears up animation parameters
 void stop_character_anim(CharacterInfo *chap);
 void walk_character(int chac,int tox,int toy,int ignwal, bool autoWalkAnims);

Engine/ac/characterinfo_engine.cpp
@@ -479,7 +479,7 @@ void UpdateCharacterIdle(CharacterInfo *chi, CharacterExtras *chex, int &doing_n
         else if (useloop >= maxLoops)
           useloop = 0;
 
-        animate_character(chi, useloop, chi->idle_anim_speed, (chi->idletime == 0) ? 1 : 0, 1);
+        animate_character(chi, useloop, chi->idle_anim_speed, (chi->idletime == 0) ? 1 : 0 /* repeat */);
 
         // don't set Animating while the idle anim plays (TODO: investigate why?)
         chi->animating = 0;
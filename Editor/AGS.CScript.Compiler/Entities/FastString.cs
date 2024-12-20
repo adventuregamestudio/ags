adventuregamestudio/ags

commit cf66b8fdf2298d9cbb501204e1f3bcfb25b8a7dc

Editor: support autocomplete dynamic arrays in function parameters

Editor/AGS.Editor/AutoComplete.cs
@@ -760,28 +760,37 @@ namespace AGS.Editor
             string[] parameters = func.ParamList.Split(',');
             foreach (string thisParam in parameters)
             {
-                string param = thisParam.Trim();
+                FastString param = thisParam.Trim();
                 if (param.StartsWith("optional "))
                 {
                     param = param.Substring(9).Trim();
                 }
                 int index = param.Length - 1;
+
+                bool isDynamicArray = false;
+                if (index >= 0 && param[index] == ']')
+                {
+                    isDynamicArray = true;
+                    while (index >= 0 && param[index--] != '[');
+                    param = param.Substring(0, index + 1).Trim();
+                }
+
                 while ((index >= 0) &&
                        (Char.IsLetterOrDigit(param[index]) || param[index] == '_'))
                 {
                     index--;
                 }
-                string paramName = param.Substring(index + 1);
-                string paramType = param.Substring(0, index + 1).Trim();
+                FastString paramName = param.Substring(index + 1);
+                FastString paramType = param.Substring(0, index + 1).Trim();
                 bool isPointer = false;
-                if (paramType.EndsWith("*"))
+                if (paramType[paramType.Length - 1] == '*')
                 {
                     isPointer = true;
                     paramType = paramType.Substring(0, paramType.Length - 1).Trim();
                 }
                 if ((paramName.Length > 0) && (paramType.Length > 0))
                 {
-                    variables.Add(new ScriptVariable(paramName, paramType, false, false, isPointer, null, null, false, false, false, false, func.StartsAtCharacterIndex));
+                    variables.Add(new ScriptVariable(paramName.ToString(), paramType.ToString(), false, isDynamicArray, isPointer, null, null, false, false, false, false, func.StartsAtCharacterIndex));
                 }
             }
             return;

commit adaebbbde3e2bd7e74af939499930a85c7c501ce

Editor: hotfix possible "index out of range" in AutoComplete
bug introduced by cf66b8fdf2298d9cbb501204e1f3bcfb25b8a7dc

Editor/AGS.CScript.Compiler/Entities/FastString.cs
@@ -6,7 +6,9 @@ namespace AGS.CScript.Compiler
 {
     /// <summary>
     /// Optimised class to allow Substrings to be done without creating new
-    /// copies of the string data in memory
+    /// copies of the string data in memory.
+    /// TODO: implement Length, to be able to work with any range inside parent string.
+    /// TODO: implement EndsWith.
     /// </summary>
     public class FastString
     {
@@ -51,6 +53,7 @@ namespace AGS.CScript.Compiler
 			{
 				return false;
 			}
+            // FIXME: compare Char by Char instead? calling String.Substring here defeats the purpose of FastString!
             return (_data[_offset] == text[0]) && (_data.Substring(_offset).StartsWith(text, _useComparison));
         }
 

Editor/AGS.Editor/AutoComplete.cs
@@ -788,7 +788,8 @@ namespace AGS.Editor
                 FastString paramName = param.Substring(index + 1);
                 FastString paramType = param.Substring(0, index + 1).Trim();
                 bool isPointer = false;
-                if (paramType[paramType.Length - 1] == '*')
+                // FIXME: implement FastString EndsWith
+                if ((paramType.Length > 0) && paramType[paramType.Length - 1] == '*')
                 {
                     isPointer = true;
                     paramType = paramType.Substring(0, paramType.Length - 1).Trim();
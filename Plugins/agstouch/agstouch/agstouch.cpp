6f729271255b7a28d02fd950b133ea50802cf629
Ivan Mogilko <ikm_spb@yahoo.com>
2024-12-27 02:12:17 +0300

    agstouch: reorganized project files, create MSVC project
    * Create MSVC solution and project;
    * Don't include any headers from Common or Engine dirs, make plugin self-sustained.
    
    NOTE:
    * does not build with MSVC project atm, need to clarify dependencies, add SDL or SDL2.

--- Plugins/agstouch/agstouch/agstouch.cpp
+++ Plugins/agstouch/agstouch/agstouch.cpp
@@ -4,9 +4,9 @@ Helper functions for touch devices
 
 */
 
-#include "core/platform.h"
+#include "platform.h"
 
-#if AGS_PLATFORM_OS_WINDOWS
+#if (AGS_PLATFORM_OS_WINDOWS)
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #pragma warning(disable : 4244)
@@ -16,7 +16,7 @@ Helper functions for touch devices
 #define THIS_IS_THE_PLUGIN
 #endif
 
-#include "plugin/agsplugin.h"
+#include "agsplugin.h"
 
 #include <SDL.h>
 
@@ -89,7 +89,7 @@ void AGS_EngineInitGfx(const char *driverID, void *data)
 
 
 
-#if AGS_PLATFORM_OS_WINDOWS && !defined(BUILTIN_PLUGINS)
+#if (AGS_PLATFORM_OS_WINDOWS) && !defined(BUILTIN_PLUGINS)
 
 // ********************************************
 // ***********  Editor Interface  *************

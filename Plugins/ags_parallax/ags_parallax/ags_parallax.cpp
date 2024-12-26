baa14a715471e5903099afe2919cc316efe8fc80
Ivan Mogilko <ikm_spb@yahoo.com>
2024-12-27 01:03:51 +0300

    ags_parallax: reorganized project files, adjust build settings
    * Move MSVC project into a distinct dir;
    * Add x64 builds;
    * Don't include any headers from Common or Engine dirs, make plugin self-sustained.
    * Remove old-style X_VERSION compile flags from makefile.

--- Plugins/ags_parallax/ags_parallax/ags_parallax.cpp
+++ Plugins/ags_parallax/ags_parallax/ags_parallax.cpp
@@ -5,9 +5,9 @@ but a workalike plugin created by JJS for the AGS engine ports.
 
 */
 
-#include "core/platform.h"
+#include "platform.h"
 
-#if AGS_PLATFORM_OS_WINDOWS
+#if (AGS_PLATFORM_OS_WINDOWS)
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #pragma warning(disable : 4244)
@@ -22,7 +22,7 @@ but a workalike plugin created by JJS for the AGS engine ports.
 #include <string.h>
 #include <math.h>
 
-#include "plugin/agsplugin.h"
+#include "agsplugin.h"
 
 #if defined(BUILTIN_PLUGINS)
 namespace ags_parallax {
@@ -258,7 +258,7 @@ void AGS_EngineInitGfx(const char *driverID, void *data)
 
 
 
-#if AGS_PLATFORM_OS_WINDOWS && !defined(BUILTIN_PLUGINS)
+#if (AGS_PLATFORM_OS_WINDOWS) && !defined(BUILTIN_PLUGINS)
 
 // ********************************************
 // ***********  Editor Interface  *************

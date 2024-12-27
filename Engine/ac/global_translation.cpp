adventuregamestudio/ags

commit 0c238a2fc93a33b2ea34bf2c9d71851a69291382

Platform fixups
- introduce core/platform.h
- use new AGS_PLATFORM defines for OS detection and debugging
- all cpp files can now be included in all projects, they will guard themselves if not for the right platform.

Engine/ac/global_translation.cpp
@@ -38,7 +38,7 @@ const char *get_translation (const char *text) {
 
     source_text_length = GetTextDisplayLength(text);
 
-#ifndef AGS_64BIT
+#if AGS_PLATFORM_64BIT
     // check if a plugin wants to translate it - if so, return that
     // TODO: plugin API is currently strictly 32-bit, so this may break on 64-bit systems
     char *plResult = Int32ToPtr<char>(pl_run_plugin_hooks(AGSE_TRANSLATETEXT, PtrToInt32(text)));

commit 441a14d92447724470b85f8f7ce9560fd1a452ad

Engine: fix reverse condition for running "translate" plugin hook
Was broken by 0c238a2fc93a33b2ea34bf2c9d71851a69291382

Engine/ac/global_translation.cpp
@@ -33,9 +33,9 @@ const char *get_translation (const char *text) {
 
     source_text_length = GetTextDisplayLength(text);
 
-#if AGS_PLATFORM_64BIT
+#if !(AGS_PLATFORM_64BIT)
     // check if a plugin wants to translate it - if so, return that
-    // TODO: plugin API is currently strictly 32-bit, so this may break on 64-bit systems
+    // FIXME: plugin API is currently strictly 32-bit, so this may break on 64-bit systems
     char *plResult = Int32ToPtr<char>(pl_run_plugin_hooks(kPluginEvt_TranslateText, PtrToInt32(text)));
     if (plResult) {
         return plResult;
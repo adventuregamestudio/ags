88d11d61a9665d1d5d670aa71960b6c4802691ea
Ivan Mogilko <ikm_spb@yahoo.com>
2025-02-27 16:41:21 +0300

    Engine: fix DynamicSprite's invalidation on restoring a save
    I made a mistake by assigning slot -1, as DynamicSprite is using slot id 0 for marking itself "deleted", and all existing safety checks in code use comparison with 0.

--- Engine/ac/dynamicsprite.cpp
+++ Engine/ac/dynamicsprite.cpp
@@ -52,7 +52,7 @@ char check_dynamic_sprites_at_exit = 1;
 void DynamicSprite_Delete(ScriptDynamicSprite *sds) {
     if (sds->slot) {
         free_dynamic_sprite(sds->slot);
-        sds->slot = 0;
+        sds->slot = 0; // 0 is a "safety placeholder" sprite id
     }
 }
 

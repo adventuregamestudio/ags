adventuregamestudio/ags

commit 8766c6813f34f59fbd2027225d006e91f5ee0004

Editor: fix SelectObjectInPropertyList() may cause recursion in foreach

Editor/AGS.Editor/GUI/frmMain.cs
@@ -370,22 +370,30 @@ namespace AGS.Editor
         public void SetPropertyObjects(object[] propertiesObjects)
         {
             propertiesPanel.SelectedObjects = propertiesObjects;
-
             propertiesPanel.propertyObjectCombo.SelectedIndex = -1;
         }
 
         private void SelectObjectInPropertyList(object propertiesObject)
         {
-            if (_propertyObjectList != null)
+            try
             {
-                foreach (string name in _propertyObjectList.Keys)
+                _ignorePropertyListChange = true;
+                if (_propertyObjectList != null)
                 {
-                    if (_propertyObjectList[name] == propertiesObject)
+                    foreach (string name in _propertyObjectList.Keys)
                     {
-                        propertiesPanel.propertyObjectCombo.SelectedIndex =
-                            propertiesPanel.propertyObjectCombo.Items.IndexOf(name);
+                        if (_propertyObjectList[name] == propertiesObject)
+                        {
+                            propertiesPanel.propertyObjectCombo.SelectedIndex =
+                                propertiesPanel.propertyObjectCombo.Items.IndexOf(name);
+                            break;
+                        }
                     }
                 }
+            }
+            finally
+            {
+                _ignorePropertyListChange = false;
             }
         }
 

commit 4f107d1a255d320e3ed20a33cdf3656b744355f7

Editor: fix bug with redrawing room mask after changing selection
Something was broken after 8766c6813f34f59fbd2027225d006e91f5ee0004

Editor/AGS.Editor/GUI/frmMain.cs
@@ -30,7 +30,8 @@ namespace AGS.Editor
 
         private Dictionary<string, object> _propertyObjectList = null;
         private bool _ignorePropertyListChange = false;
-		private bool _suspendDrawing = false;
+        private bool _ignoreSelectObjectInList = false;
+        private bool _suspendDrawing = false;
         private List<DockContent> _dockPanes = new List<DockContent>();
         private WindowsLayoutManager _layoutManager;
 
@@ -375,9 +376,13 @@ namespace AGS.Editor
 
         private void SelectObjectInPropertyList(object propertiesObject)
         {
+            // Prevent occasional recursion (code logic around PropertyGrid is quite confusing)
+            if (_ignoreSelectObjectInList)
+                return;
+
             try
             {
-                _ignorePropertyListChange = true;
+                _ignoreSelectObjectInList = true;
                 if (_propertyObjectList != null)
                 {
                     foreach (string name in _propertyObjectList.Keys)
@@ -393,7 +398,7 @@ namespace AGS.Editor
             }
             finally
             {
-                _ignorePropertyListChange = false;
+                _ignoreSelectObjectInList = false;
             }
         }
 
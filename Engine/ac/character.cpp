adventuregamestudio/ags

commit caf4752037a625506d3dc0cc9468e60aa2504a52

Engine: tidy Character_StopMoving(), bail out early if not moving

Engine/ac/character.cpp
@@ -957,37 +957,50 @@ void Character_SetSpeed(CharacterInfo *chaa, int xspeed, int yspeed) {
     }
 }
 
-void Character_StopMoving(CharacterInfo *charp) {
+void Character_StopMoving(CharacterInfo *chi)
+{
+    // If not moving, then clear the move-related flags (for safety) and bail out
+    // NOTE: I recall there was a potential case when this flag could remain after Move...
+    if (chi->walking == 0)
+    {
+        chi->flags &= ~CHF_MOVENOTWALK;
+        return;
+    }
 
-    int chaa = charp->index_id;
-    if (chaa == play.skip_until_char_stops)
+    int chid = chi->index_id;
+    if (chid == play.skip_until_char_stops)
         EndSkippingUntilCharStops();
 
-    if (charextra[chaa].xwas != INVALID_X) {
-        charp->x = charextra[chaa].xwas;
-        charp->y = charextra[chaa].ywas;
-        charextra[chaa].xwas = INVALID_X;
-    }
-    if ((charp->walking > 0) && (charp->walking < TURNING_AROUND)) {
-        // if it's not a MoveCharDirect, make sure they end up on a walkable area
-        if ((mls[charp->walking].direct == 0) && (charp->room == displayed_room))
-            Character_PlaceOnWalkableArea(charp);
-
-        debug_script_log("%s: stop moving", charp->scrname);
-
-        charp->idleleft = charp->idletime;
-        // restart the idle animation straight away
-        charextra[chaa].process_idle_this_time = 1;
-    }
-    if (charp->walking)
+    CharacterExtras &chex = charextra[chid];
+    // Fixup position to the last saved one (CHECKME: find out what this means)
+    if (chex.xwas != INVALID_X)
     {
-        // If the character is *currently* moving, stop them and reset their frame
-        charp->walking = 0;
-        if ((charp->flags & CHF_MOVENOTWALK) == 0)
-            charp->frame = 0;
+        chi->x = chex.xwas;
+        chi->y = chex.ywas;
+        chex.xwas = INVALID_X;
     }
-    // Reset any moving-related flags
-    charp->flags &= ~CHF_MOVENOTWALK;
+
+    // If it is in walking state, and is *not* during turning around,
+    // then validate the character position, ensuring that it does not
+    // end on a non-walkable area and gets stuck.
+    if ((chi->walking < TURNING_AROUND) && (chi->room == displayed_room))
+    {
+        if ((mls[chi->walking].direct == 0))
+            Character_PlaceOnWalkableArea(chi);
+    }
+
+    debug_script_log("%s: stop moving", chi->scrname);
+
+    // Switch character state from walking to standing
+    chi->walking = 0;
+    // If the character was walking, then reset their frame
+    if ((chi->flags & CHF_MOVENOTWALK) == 0)
+        chi->frame = 0;
+    chi->flags &= ~CHF_MOVENOTWALK;
+
+    // Restart idle timer and mark to process right away (in case its persistent idling)
+    chi->idleleft = chi->idletime;
+    chex.process_idle_this_time = 1;
 }
 
 void Character_Tint(CharacterInfo *chaa, int red, int green, int blue, int opacity, int luminance) {

Engine/ac/characterextras.h
@@ -44,7 +44,7 @@ struct CharacterExtras
     short width = 0;
     short height = 0;
     short zoom = 100;
-    short xwas = 0;
+    short xwas = 0; // TODO: figure out how these xwas,ywas are being used and comment them
     short ywas = 0;
     short tint_r = 0;
     short tint_g = 0;

Engine/ac/movelist.h
@@ -57,7 +57,7 @@ public:
     // made a fractional value to let recalculate movelist dynamically
     float   onpart = 0.f;
     uint8_t doneflag = 0u;
-    uint8_t direct = 0;  // MoveCharDirect was used or not
+    uint8_t direct = 0;  // ignoring walkable areas (yes = 1, no = 0)
 
     // Dynamic fixups, not serialized
     // Final section move speed and steps, used when an object

commit 3f405d3fb14870861905d8b829876af6e6dbe238

Engine: refactor StopMoving() again, restoring a missed position fixup
This (hopefully) fixes a regression introduced by caf4752037a625506d3dc0cc9468e60aa2504a52
where one important final position fixup was excluded in case this function is called right after character finished walking the path.

Engine/ac/character.cpp
@@ -974,18 +974,6 @@ void Character_StopMoving(CharacterInfo *chi)
 
 void Character_StopMovingEx(CharacterInfo *chi, bool force_walkable_area)
 {
-    // If not moving, then clear the move-related flags (for safety) and bail out
-    // NOTE: I recall there was a potential case when this flag could remain after Move...
-    if (chi->walking == 0)
-    {
-        chi->flags &= ~CHF_MOVENOTWALK;
-        if (force_walkable_area && (chi->room == displayed_room))
-        {
-            Character_PlaceOnWalkableArea(chi);
-        }
-        return;
-    }
-
     int chid = chi->index_id;
     if (chid == play.skip_until_char_stops)
         EndSkippingUntilCharStops();
@@ -999,13 +987,18 @@ void Character_StopMovingEx(CharacterInfo *chi, bool force_walkable_area)
         chex.xwas = INVALID_X;
     }
 
-    // If it is in walking state, and is *not* during turning around,
-    // then validate the character position, ensuring that it does not
-    // end on a non-walkable area and gets stuck.
-    if (force_walkable_area && (chi->walking < TURNING_AROUND) && (chi->room == displayed_room))
+    // If requested by a caller, then validate the character position,
+    // ensuring that it does not end on a non-walkable area and gets stuck.
+    // Because the pathfinder is not ideal, and there are various sources of
+    // inaccuracy in the walking logic, in rare cases the character might
+    // pass through or even finish on a non-walkable pixel.
+    if (force_walkable_area && (chi->room == displayed_room))
     {
         // TODO: don't use PlaceOnWalkable, as that may result in moving character
-        // into the random direction. Instead, consider writing a "backtracing"
+        // into the random direction.
+        // For finishing a walk clamp to a precalculated final destination instead
+        // (CHECKME: is it guaranteed valid?).
+        // For when interrupting a move, consider writing a "backtracing"
         // function that runs along MoveList's current stage back, looking for
         // the first pixel on a walkable area.
         Character_PlaceOnWalkableArea(chi);
@@ -1013,16 +1006,22 @@ void Character_StopMovingEx(CharacterInfo *chi, bool force_walkable_area)
 
     debug_script_log("%s: stop moving", chi->scrname);
 
-    // Switch character state from walking to standing
-    chi->walking = 0;
-    // If the character was walking, then reset their frame
-    if ((chi->flags & CHF_MOVENOTWALK) == 0)
-        chi->frame = 0;
-    chi->flags &= ~CHF_MOVENOTWALK;
+    // If the character is currently moving, stop them and reset their state
+    if (chi->walking > 0)
+    {
+        // Switch character state from walking to standing
+        chi->walking = 0;
+        // If the character was animating a walk, then reset their frame to standing
+        if ((chi->flags & CHF_MOVENOTWALK) == 0)
+            chi->frame = 0;
+        // Restart idle timer and mark to process right away (in case its persistent idling)
+        chi->idleleft = chi->idletime;
+        chex.process_idle_this_time = 1;
+    }
 
-    // Restart idle timer and mark to process right away (in case its persistent idling)
-    chi->idleleft = chi->idletime;
-    chex.process_idle_this_time = 1;
+    // Always clear the move-related flags (for safety)
+    // NOTE: I recall there was a potential case when this flag could remain after Move...
+    chi->flags &= ~CHF_MOVENOTWALK;
 }
 
 void Character_Tint(CharacterInfo *chaa, int red, int green, int blue, int opacity, int luminance) {
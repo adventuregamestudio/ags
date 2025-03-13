adventuregamestudio/ags

commit fcdb84a81e0e0e3b53e222c7d1f1a33cc4b81ca9

Engine: support restoring a GUIObject managed object from old saves
The idea here is that we find out the actual control's type, and re-register using a proper manager.

Engine/ac/dynobj/cc_guicontrol.cpp
@@ -12,33 +12,26 @@
 //
 //=============================================================================
 #include "ac/dynobj/cc_guicontrol.h"
-#include "ac/dynobj/scriptobjects.h"
-#include "ac/dynobj/dynobj_manager.h"
-#include "ac/gui.h"
-#include "util/stream.h"
 
 using namespace AGS::Common;
-extern std::vector<std::vector<int>> StaticGUIControlsHandles;
 
 // return the type name of the object
-const char *CCGUIControl::GetType() {
+const char *CCGUIControl::GetType()
+{
     return "GUIControl";
 }
 
 size_t CCGUIControl::CalcSerializeSize(const void* /*address*/)
 {
-    return sizeof(int32_t) * 2;
+    return 0;
 }
 
-void CCGUIControl::Serialize(const void *address, Stream *out) {
-    const GUIObject *guio = static_cast<const GUIObject*>(address);
-    out->WriteInt32(guio->ParentId);
-    out->WriteInt32(guio->Id);
+void CCGUIControl::Serialize(const void* /*address*/, Stream* /*out*/)
+{
+    // no longer supported
 }
 
-void CCGUIControl::Unserialize(int index, Stream *in, size_t /*data_sz*/) {
-    int guinum = in->ReadInt32();
-    int objnum = in->ReadInt32();
-    int handle = ccRegisterUnserializedPersistentObject(index, guis[guinum].GetControl(objnum), this);
-    (StaticGUIControlsHandles[guinum])[objnum] = handle;
+void CCGUIControl::Unserialize(int /*index*/, Stream* /*in*/, size_t /*data_sz*/)
+{
+    // no longer supported
 }

Engine/ac/dynobj/cc_serializer.cpp
@@ -23,6 +23,7 @@
 #include "ac/dynobj/scriptfile.h"
 #include "ac/dynobj/scriptviewport.h"
 #include "ac/game.h"
+#include "ac/gui.h"
 #include "debug/debug_log.h"
 #include "plugin/plugin_engine.h"
 #include "util/memory_compat.h"
@@ -53,6 +54,7 @@ extern CCDialog    ccDynamicDialog;
 extern ScriptDrawingSurface* dialogOptionsRenderingSurface;
 extern ScriptDialogOptionsRendering ccDialogOptionsRendering;
 extern std::vector<PluginObjectReader> pluginReaders;
+extern std::vector<std::vector<int>> StaticGUIControlsHandles;
 
 // *** De-serialization of script objects
 
@@ -61,7 +63,7 @@ void AGSDeSerializer::Unserialize(int index, const char *objectType, const char 
     if (dataSize < 0)
     {
         quitprintf("Unserialise: invalid data size (%d) for object type '%s'", dataSize, objectType);
-        return; // TODO: don't quit, return error
+        return; // FIXME: don't quit, return error
     }
     // Note that while our builtin classes may accept Stream object,
     // classes registered by plugin cannot, because streams are not (yet)
@@ -86,7 +88,7 @@ void AGSDeSerializer::Unserialize(int index, const char *objectType, const char 
     }
     else if (strcmp(objectType, "GUIObject") == 0 || // old historical name
              strcmp(objectType, "GUIControl") == 0) {
-        ccDynamicGUIControl.Unserialize(index, &mems, data_sz);
+        UnserializeGUIControl(index, &mems, data_sz);
     }
     else if (strcmp(objectType, "Button") == 0) {
         ccDynamicGUIButton.Unserialize(index, &mems, data_sz);
@@ -218,6 +220,30 @@ void AGSDeSerializer::Unserialize(int index, const char *objectType, const char 
                 return;
             }
         }
+        // FIXME: find a way to return error back to caller instead
         quitprintf("Unserialise: unknown object type: '%s'", objectType);
     }
+}
+
+int AGSDeSerializer::RegisterGUIControl(int index, int guinum, int objnum)
+{
+    GUIObject *obj = guis[guinum].GetControl(objnum);
+    switch (guis[guinum].GetControlType(objnum))
+    {
+    case kGUIButton: return ccRegisterUnserializedPersistentObject(index, obj, &ccDynamicGUIButton);
+    case kGUILabel: return ccRegisterUnserializedPersistentObject(index, obj, &ccDynamicGUILabel);
+    case kGUIInvWindow: return ccRegisterUnserializedPersistentObject(index, obj, &ccDynamicGUIInvWindow);
+    case kGUISlider: return ccRegisterUnserializedPersistentObject(index, obj, &ccDynamicGUISlider);
+    case kGUITextBox: return ccRegisterUnserializedPersistentObject(index, obj, &ccDynamicGUITextBox);
+    case kGUIListBox: return ccRegisterUnserializedPersistentObject(index, obj, &ccDynamicGUIListBox);
+    default: assert(false); /* should not happen */ return 0;
+    }
+}
+
+void AGSDeSerializer::UnserializeGUIControl(int index, AGS::Common::Stream *in, size_t data_sz)
+{
+    int guinum = in->ReadInt32();
+    int objnum = in->ReadInt32();
+    int handle = RegisterGUIControl(index, guinum, objnum);
+    (StaticGUIControlsHandles[guinum])[objnum] = handle;
 }

Engine/ac/dynobj/cc_serializer.h
@@ -15,11 +15,17 @@
 #define __AC_SERIALIZER_H
 
 #include "ac/dynobj/cc_scriptobject.h"
+#include "util/stream.h"
 
 struct AGSDeSerializer : ICCObjectCollectionReader
 {
 public:
     void Unserialize(int index, const char *objectType, const char *serializedData, int dataSize) override;
+
+private:
+    // Unserializes an old GUIControl record
+    void UnserializeGUIControl(int index, AGS::Common::Stream *in, size_t data_sz);
+    int  RegisterGUIControl(int index, int guinum, int objnum);
 };
 
 #endif // __AC_SERIALIZER_H

commit 46cb50ca783436011dad45cc92c1f6b18820fe43

Engine: restored proper save of GUI Controls
Was broken by mistake in fcdb84a81e0e0e3b53e222c7d1f1a33cc4b81ca9

Engine/ac/dynobj/cc_guicontrol.cpp
@@ -12,8 +12,13 @@
 //
 //=============================================================================
 #include "ac/dynobj/cc_guicontrol.h"
+#include "ac/gui.h"
+#include "ac/dynobj/dynobj_manager.h"
+#include "util/stream.h"
 
 using namespace AGS::Common;
+
+extern std::vector<std::vector<int>> StaticGUIControlsHandles;
 
 // return the type name of the object
 const char *CCGUIControl::GetType()
@@ -23,15 +28,20 @@ const char *CCGUIControl::GetType()
 
 size_t CCGUIControl::CalcSerializeSize(const void* /*address*/)
 {
-    return 0;
+    return sizeof(int32_t) * 2;
 }
 
-void CCGUIControl::Serialize(const void* /*address*/, Stream* /*out*/)
+void CCGUIControl::Serialize(const void* address, Stream* out)
 {
-    // no longer supported
+    const GUIObject *guio = static_cast<const GUIObject *>(address);
+    out->WriteInt32(guio->ParentId);
+    out->WriteInt32(guio->Id);
 }
 
-void CCGUIControl::Unserialize(int /*index*/, Stream* /*in*/, size_t /*data_sz*/)
+void CCGUIControl::Unserialize(int index, Stream* in, size_t /*data_sz*/)
 {
-    // no longer supported
+    int guinum = in->ReadInt32();
+    int objnum = in->ReadInt32();
+    int handle = ccRegisterUnserializedPersistentObject(index, guis[guinum].GetControl(objnum), this);
+    (StaticGUIControlsHandles[guinum])[objnum] = handle;
 }
8179aa46abdc2ba8937f1ba118d3715b2b955927
Ivan Mogilko <ikm_spb@yahoo.com>
2025-03-26 10:23:20 +0300

    Editor: added "Collapse All" and "Expand All" commands in script editor

--- Editor/AGS.Editor/Panes/ScintillaWrapper.cs
+++ Editor/AGS.Editor/Panes/ScintillaWrapper.cs
@@ -629,6 +629,16 @@ namespace AGS.Editor
             scintillaControl1.EndUndoAction();
         }
 
+        public void CollapseAllRegions()
+        {
+            scintillaControl1.FoldAll(FoldAction.Contract);
+        }
+
+        public void ExpandAllRegions()
+        {
+            scintillaControl1.FoldAll(FoldAction.Expand);
+        }
+
         public void GoToPosition(int newPos)
         {
             int lineNum = scintillaControl1.LineFromPosition(newPos);

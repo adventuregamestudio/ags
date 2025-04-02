5357e34a721f07f17cb31539e14bbfcf257fb180
Ivan Mogilko <ikm_spb@yahoo.com>
2025-04-02 03:12:32 +0300

    Engine: fix default dialog options highlight color

--- Engine/main/engine.cpp
+++ Engine/main/engine.cpp
@@ -788,7 +788,7 @@ void engine_init_game_settings()
     play.speech_portrait_x = 0;
     play.speech_portrait_y = 0;
     play.speech_display_post_time_ms = 0;
-    play.dialog_options_highlight_color = DIALOG_OPTIONS_HIGHLIGHT_COLOR_DEFAULT;
+    play.dialog_options_highlight_color = GUI::GetStandardColor(DIALOG_OPTIONS_HIGHLIGHT_COLOR_DEFAULT);
     play.speech_has_voice = false;
     play.speech_voice_blocking = false;
     play.speech_in_post_state = false;

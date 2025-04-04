46d66344a75ccc31772d384d02e48453efb079ff
Ivan Mogilko <ikm_spb@yahoo.com>
2025-04-05 00:33:28 +0300

    Common: fix Bitmap::RotateBlt
    Was broken by incomplete backport from ags4: bb5b3f39bbc01cbde737b250d2be877d3e1472cd

--- Common/gfx/allegrobitmap.cpp
+++ Common/gfx/allegrobitmap.cpp
@@ -379,16 +379,16 @@ void Bitmap::RotateBlt(Bitmap *src, int dst_x, int dst_y, fixed_t angle)
 {
     // convert to allegro angle
     fixed_t al_angle = itofix((angle * 256) / 360);
-	BITMAP *al_src_bmp = src->_alBitmap;
-	rotate_sprite(_alBitmap, al_src_bmp, dst_x, dst_y, angle);
+    BITMAP *al_src_bmp = src->_alBitmap;
+    rotate_sprite(_alBitmap, al_src_bmp, dst_x, dst_y, al_angle);
 }
 
 void Bitmap::RotateBlt(Bitmap *src, int dst_x, int dst_y, int pivot_x, int pivot_y, fixed_t angle)
 {
     // convert to allegro angle
     fixed_t al_angle = itofix((angle * 256) / 360);
-	BITMAP *al_src_bmp = src->_alBitmap;
-	pivot_sprite(_alBitmap, al_src_bmp, dst_x, dst_y, pivot_x, pivot_y, angle);
+    BITMAP *al_src_bmp = src->_alBitmap;
+    pivot_sprite(_alBitmap, al_src_bmp, dst_x, dst_y, pivot_x, pivot_y, al_angle);
 }
 
 //=============================================================================

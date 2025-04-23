f7b08a80a9642d16757293122f11f8f1a5c0940a
Érico Porto <ericoporto2008@gmail.com>
2025-04-23 19:08:12 -0300

    CMake: populate with a single argument is deprecated
    we are still using populate so we can do the file copy to do CMake replacement, which is not possible when using MakeAvailable

--- CMake/FetchVorbis.cmake
+++ CMake/FetchVorbis.cmake
@@ -1,13 +1,10 @@
-FetchContent_Declare(
-  vorbis_content
-  DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-  URL https://github.com/xiph/vorbis/releases/download/v1.3.7/libvorbis-1.3.7.tar.xz
-  URL_HASH MD5=50902641d358135f06a8392e61c9ac77
-)
-
 FetchContent_GetProperties(vorbis_content)
 if(NOT vorbis_content_POPULATED)
-  FetchContent_Populate(vorbis_content)
+  FetchContent_Populate(vorbis_content
+          DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+          URL https://github.com/xiph/vorbis/releases/download/v1.3.7/libvorbis-1.3.7.tar.xz
+          URL_HASH MD5=50902641d358135f06a8392e61c9ac77
+  )
   file(COPY Engine/libsrc/vorbis/CMakeLists.txt DESTINATION ${vorbis_content_SOURCE_DIR})
   add_subdirectory(${vorbis_content_SOURCE_DIR} ${vorbis_content_BINARY_DIR} EXCLUDE_FROM_ALL)
 endif()

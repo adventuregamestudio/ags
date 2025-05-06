e4101836c2d3f8358774823b4518d5d08d0a526d
Ivan Mogilko <ikm_spb@yahoo.com>
2025-05-07 01:16:10 +0300

    Engine: fixed setting null ShaderInstance pointer to game objects

--- Engine/ac/gui.cpp
+++ Engine/ac/gui.cpp
@@ -433,7 +433,7 @@ void GUI_SetShader(ScriptGUI *gui, ScriptShaderInstance *shader_inst)
         ccReleaseObjectReference(guim.GetShaderHandle());
 
     ccAddObjectReference(new_inst_ref);
-    guim.SetShader(shader_inst->GetID(), new_inst_ref);
+    guim.SetShader(shader_inst ? shader_inst->GetID() : ScriptShaderInstance::NullInstanceID, new_inst_ref);
 }
 
 float GUI_GetRotation(ScriptGUI *gui) {

adventuregamestudio/ags

commit fe13f614c8e8cbe63b24602b0a781b7341e643a0

Engine: hardcode alignment padding in RoomStruct's legacy save

Engine/ac/roomstatus.cpp
@@ -18,7 +18,6 @@
 #include "ac/roomstatus.h"
 #include "game/customproperties.h"
 #include "game/savegame_components.h"
-#include "util/alignedstream.h"
 #include "util/string_utils.h"
 
 using namespace AGS::Common;
@@ -73,21 +72,23 @@ void RoomStatus::FreeProperties()
     objProps.clear();
 }
 
-void RoomStatus::ReadFromFile_v321(Stream *in, GameDataVersion data_ver)
+void RoomStatus::ReadFromSavegame_v321(Stream *in, GameDataVersion data_ver)
 {
     FreeScriptData();
     FreeProperties();
 
     contentFormat = kRoomStatSvgVersion_Initial;
-    beenhere = in->ReadInt32();
-    numobj = in->ReadInt32();
     obj.resize(MAX_ROOM_OBJECTS_v300);
     objProps.resize(MAX_ROOM_OBJECTS_v300);
     intrObject.resize(MAX_ROOM_OBJECTS_v300);
+
+    beenhere = in->ReadInt32();
+    numobj = in->ReadInt32();
     ReadRoomObjects_Aligned(in);
 
     int16_t dummy[MAX_LEGACY_ROOM_FLAGS]; // cannot seek with AlignedStream
     in->ReadArrayOfInt16(dummy, MAX_LEGACY_ROOM_FLAGS); // flagstates (OBSOLETE)
+    in->ReadInt16(); // alignment padding to int32
     tsdatasize = static_cast<uint32_t>(in->ReadInt32());
     in->ReadInt32(); // tsdata
     for (int i = 0; i < MAX_ROOM_HOTSPOTS; ++i)
@@ -107,6 +108,7 @@ void RoomStatus::ReadFromFile_v321(Stream *in, GameDataVersion data_ver)
         hotspot[i].Enabled = in->ReadInt8() != 0;
     in->ReadArrayOfInt8((int8_t*)region_enabled, MAX_ROOM_REGIONS);
     in->ReadArrayOfInt16(walkbehind_base, MAX_WALK_BEHINDS);
+    in->ReadInt16(); // alignment padding to int32 (66 int8 + 16 int16 = 49 int16 -> 50)
     in->ReadArrayOfInt32(interactionVariableValues, MAX_GLOBAL_VARIABLES);
 
     if (data_ver >= kGameVersion_340_4)
@@ -125,11 +127,9 @@ void RoomStatus::ReadFromFile_v321(Stream *in, GameDataVersion data_ver)
 
 void RoomStatus::ReadRoomObjects_Aligned(Common::Stream *in)
 {
-    AlignedStream align_s(in, Common::kAligned_Read);
     for (auto &o : obj)
     {
-        o.ReadFromSavegame(&align_s, 0);
-        align_s.Reset();
+        o.ReadFromSavegame(in, -1 /* legacy save with padding */);
     }
 }
 

Common/game/interactions.cpp
@@ -319,8 +319,8 @@ void Interaction::ReadFromSavedgame_v321(Stream *in)
     {
         Events[i].Type = in->ReadInt32();
     }
-    const size_t padding = (MAX_NEWINTERACTION_EVENTS - evt_count);
-    for (size_t i = 0; i < padding; ++i)
+    const size_t dummy_count = (MAX_NEWINTERACTION_EVENTS - evt_count);
+    for (size_t i = 0; i < dummy_count; ++i)
         in->ReadInt32(); // cannot skip when reading aligned structs
     ReadTimesRunFromSave_v321(in);
 

Engine/ac/roomobject.cpp
@@ -109,6 +109,7 @@ void RoomObject::CheckViewFrame()
 
 void RoomObject::ReadFromSavegame(Stream *in, int save_ver)
 {
+    const bool do_align_pad = save_ver < 0;
     x = in->ReadInt32();
     y = in->ReadInt32();
     transparent = in->ReadInt32();
@@ -133,6 +134,9 @@ void RoomObject::ReadFromSavegame(Stream *in, int save_ver)
     flags = in->ReadInt8();
     blocking_width = in->ReadInt16();
     blocking_height = in->ReadInt16();
+    if (do_align_pad)
+        in->ReadInt16(); // int16 padding to int32 (max)
+
     if (save_ver >= kRoomStatSvgVersion_36016)
     {
         name = StrUtil::ReadString(in);

Engine/ac/roomstatus.h
@@ -88,7 +88,7 @@ struct RoomStatus
     void FreeScriptData();
     void FreeProperties();
 
-    void ReadFromFile_v321(Common::Stream *in, GameDataVersion data_ver);
+    void ReadFromSavegame_v321(Common::Stream *in, GameDataVersion data_ver);
     void ReadRoomObjects_Aligned(Common::Stream *in);
     void ReadFromSavegame(Common::Stream *in, GameDataVersion data_ver, RoomStatSvgVersion save_ver);
     void WriteToSavegame(Common::Stream *out, GameDataVersion data_ver) const;

Engine/game/savegame_v321.cpp
@@ -159,8 +159,7 @@ static HSaveError restore_game_scripts(Stream *in, const PreservedParams &pp, Re
 
 static void ReadRoomStatus_Aligned(RoomStatus *roomstat, Stream *in, GameDataVersion data_ver)
 {
-    AlignedStream align_s(in, Common::kAligned_Read);
-    roomstat->ReadFromFile_v321(&align_s, data_ver);
+    roomstat->ReadFromSavegame_v321(in, data_ver);
 }
 
 static void restore_game_room_state(Stream *in, GameDataVersion data_ver)

commit 2260770dfd1872e9bf879f2fe999ddb172b4d871

Engine: fixed loading 3.4 - 3.4.1 saves with custom properties
Was broken since commit fe13f614c8e8cbe63b24602b0a781b7341e643a0

Common/game/customproperties.cpp
@@ -89,7 +89,7 @@ void WriteSchema(const PropertySchema &schema, Stream *out)
     }
 }
 
-PropertyError ReadValues(StringIMap &map, Stream *in)
+PropertyError ReadValues(StringIMap &map, Stream *in, bool aligned)
 {
     PropertyVersion version = (PropertyVersion)in->ReadInt32();
     if (version < kPropertyVersion_Initial ||
@@ -109,10 +109,21 @@ PropertyError ReadValues(StringIMap &map, Stream *in)
     }
     else
     {
-        for (int i = 0; i < count; ++i)
+        if (aligned)
         {
-            String name  = StrUtil::ReadString(in);
-            map[name] = StrUtil::ReadString(in);
+            for (int i = 0; i < count; ++i)
+            {
+                String name = StrUtil::ReadStringAligned(in);
+                map[name] = StrUtil::ReadStringAligned(in);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < count; ++i)
+            {
+                String name = StrUtil::ReadString(in);
+                map[name] = StrUtil::ReadString(in);
+            }
         }
     }
     return kPropertyErr_NoError;

Common/game/customproperties.h
@@ -90,7 +90,8 @@ namespace Properties
 
     // Reads property values from the stream and assign them to map.
     // The non-matching existing map items, if any, are NOT erased.
-    PropertyError ReadValues(StringIMap &map, Stream *in);
+    // NOTE: "aligned" parameter is for legacy saves support only.
+    PropertyError ReadValues(StringIMap &map, Stream *in, bool aligned = false);
     // Writes property values chunk to the stream
     void          WriteValues(const StringIMap &map, Stream *out);
 

Common/util/string_utils.cpp
@@ -157,6 +157,19 @@ void StrUtil::ReadString(char **cstr, Stream *in)
     (*cstr)[len] = 0;
 }
 
+String StrUtil::ReadStringAligned(Stream *in)
+{
+    String s = ReadString(in);
+    size_t rem = s.GetLength() % sizeof(int32_t);
+    if (rem > 0)
+    {
+        size_t pad = sizeof(int32_t) - rem;
+        for (size_t i = 0; i < pad; ++i)
+            in->ReadByte();
+    }
+    return s;
+}
+
 void StrUtil::SkipString(Stream *in)
 {
     size_t len = in->ReadInt32();

Common/util/string_utils.h
@@ -61,6 +61,9 @@ namespace StrUtil
     void            ReadString(char *cstr, Stream *in, size_t buf_limit);
     void            ReadString(char **cstr, Stream *in);
     void            ReadString(String &s, Stream *in);
+    // Read a string and trailing padding, aligning total read data to int32;
+    // this is a special case used strictly for legacy save format
+    String          ReadStringAligned(Stream *in);
     void            SkipString(Stream *in);
     void            WriteString(const String &s, Stream *out);
     void            WriteString(const char *cstr, Stream *out);

Engine/ac/roomstatus.cpp
@@ -116,14 +116,14 @@ void RoomStatus::ReadFromSavegame_v321(Stream *in, GameDataVersion data_ver)
 
     if (data_ver >= kGameVersion_340_4)
     {
-        Properties::ReadValues(roomProps, in);
+        Properties::ReadValues(roomProps, in, true /* aligned */);
         for (int i = 0; i < MAX_ROOM_HOTSPOTS; ++i)
         {
-            Properties::ReadValues(hsProps[i], in);
+            Properties::ReadValues(hsProps[i], in, true /* aligned */);
         }
         for (auto &props : objProps)
         {
-            Properties::ReadValues(props, in);
+            Properties::ReadValues(props, in, true /* aligned */);
         }
     }
 }
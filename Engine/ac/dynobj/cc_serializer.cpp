41692911f4e69d190a0cdd3dee7147526372c106
Ivan Mogilko <ikm_spb@yahoo.com>
2025-06-04 21:53:46 +0300

    Engine: fix a typo in AGSDeSerializer::Unserialize()

--- Engine/ac/dynobj/cc_serializer.cpp
+++ Engine/ac/dynobj/cc_serializer.cpp
@@ -88,7 +88,7 @@ void AGSDeSerializer::Unserialize(int index, const char *objectType, const char 
         ScriptUserObject *suo = new ScriptUserObject();
         suo->Unserialize(index, &mems, data_sz);
     }
-    else if (strcmp(objectType, "GUIControl") == 0 || // old historical name
+    else if (strcmp(objectType, "GUIObject") == 0 || // old historical name
              strcmp(objectType, "GUIControl") == 0) {
         UnserializeGUIControl(index, &mems, data_sz);
     }

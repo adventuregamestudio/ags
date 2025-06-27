260de08ed4e616fbb15149c50d3ff7b8465a5145
Ivan Mogilko <ikm_spb@yahoo.com>
2025-06-27 18:07:58 +0300

    Editor: move CommandLineOptions to ApplicationController
    1. Move command opts handling outside of CommandLineOptions class. Leave strictly command-line parsing and options assignment.
    IMO the CommandLineOptions should only parse args and declare options. It's up to the application (GUIController or ApplicationController in this case) to decide how to handle them.
    2. Do the initial options parsing in ApplicationController, and pass CommandLineOptions object to GUIController.
    3. Enable StdConsoleWriter in ApplicationController, in order to let application print unhandled exceptions to the stdout if it's launched from the command line.
    Report unknown parameters also in ApplicationController.
    (The rest is handled by GUIController, as before.)
    4. Do all the bad parameters situation handling in GUIController.

--- Editor/AGS.Editor/Program.cs
+++ Editor/AGS.Editor/Program.cs
@@ -14,8 +14,8 @@ namespace AGS.Editor
 {
     static class Program
     {
+        private static string[] _commandLineArgs;
         private static ApplicationController _application;
-
         private static int _exitCode = 0;
 
         public static void SetExitCode(int exit_code)
@@ -36,8 +36,8 @@ namespace AGS.Editor
         {
 #if !DEBUG
             try
-            {
 #endif
+            {
                 Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
                 Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
                 AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
@@ -46,6 +46,7 @@ namespace AGS.Editor
                 string filePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
                 Directory.SetCurrentDirectory(Path.GetDirectoryName(filePath));
 
+                _commandLineArgs = args;
                 SplashScreen splash = new SplashScreen();
                 splash.Load += new EventHandler(splash_Load);
                 Application.Run(splash);
@@ -61,12 +62,12 @@ namespace AGS.Editor
                 Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
                 AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
 
-                _application.StartGUI(args);
-#if !DEBUG
+                _application.StartGUI();
             }
+#if !DEBUG
             catch (Exception ex)
             {
-                MessageBox.Show("An unexpected error occurred. See below for details." + Environment.NewLine + ex.ToString(), "Application Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                ApplicationController.ShowMessage("An unexpected error occurred. See below for details." + Environment.NewLine + ex.ToString(), "Application Error", MessageBoxIcon.Stop);
             }
 #endif
         }
@@ -88,7 +89,7 @@ namespace AGS.Editor
             ((Timer)sender).Dispose();
 
             // This line does all the work loading the app
-            _application = new ApplicationController();
+            _application = new ApplicationController(_commandLineArgs);
 
             // Close the splash screen; control will return to
             // the RunApplication function where it will start
@@ -147,7 +148,7 @@ namespace AGS.Editor
                     error = string.Format("\nStack Trace SEHException: HResult Error Code: {0}, Exception: {1}",
                         sehException.ErrorCode, error);
                 }
-                MessageBox.Show("An unexpected error occurred trying to start up the AGS Editor. Please consult the details below and post the error to the AGS Technical Forum.\n\n" + error, "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                ApplicationController.ShowMessage("An unexpected error occurred trying to start up the AGS Editor. Please consult the details below and post the error to the AGS Technical Forum.\n\n" + error, "Error", MessageBoxIcon.Stop);
 
                 // Close the Splash Screen if it was open while the exception occured
                 if (Application.OpenForms.Count > 0)
@@ -156,6 +157,5 @@ namespace AGS.Editor
                 }
             }
 		}
-
     }
 }

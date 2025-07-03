adventuregamestudio/ags

commit 3c26a93cbe462b067fb4cec53d9f52197a4ae3e7

Engine: tidy up GetLocationName(), use named constants for saved loc

Engine/ac/global_game.cpp
@@ -725,83 +725,94 @@ void SaveCursorForLocationChange() {
     }
 }
 
-const char *GetLocationName(int x, int y)
+// Finds out what is located under the cursor;
+// returns location's name and "location index" using respective SavedLocationType index base.
+static const char *GetLocationNameAndIndex(int x, int y, int &loc_index)
 {
     if (displayed_room < 0)
-        return ""; // no room loaded yet
-
-    const char *out_name = "";
-    if (GetGUIAt(x, y) >= 0)
     {
-        int mover = GetInvAt(x, y);
-        if (mover > 0)
-        {
-            if (play.get_loc_name_last_time != 1000 + mover)
-                GUIE::MarkSpecialLabelsForUpdate(kLabelMacro_Overhotspot);
-            play.get_loc_name_last_time = 1000 + mover;
-            out_name = get_translation(game.invinfo[mover].name.GetCStr());
-        }
-        else if ((play.get_loc_name_last_time > 1000) && (play.get_loc_name_last_time < 1000 + MAX_INV)) {
-            // no longer selecting an item
-            GUIE::MarkSpecialLabelsForUpdate(kLabelMacro_Overhotspot);
-            play.get_loc_name_last_time = -1;
-        }
-        return out_name;
+        loc_index = kSavedLocType_Undefined;
+        return ""; // no room loaded yet
     }
 
-    int loctype = GetLocationType(x, y); // GetLocationType takes screen coords
-    VpPoint vpt = play.ScreenToRoomDivDown(x, y);
-    if (vpt.second < 0)
-        return "";
+    if (GetGUIAt(x, y) >= 0)
+    {
+        // On GUI, test if we're above an inventory item
+        int invitem = GetInvAt(x, y);
+        if (invitem > 0)
+        {
+            loc_index = kSavedLocType_InvItem + invitem;
+            return get_translation(game.invinfo[invitem].name.GetCStr());
+        }
+        else
+        {
+            loc_index = kSavedLocType_Undefined;
+            return "";
+        }
+    }
+
+    // Find out if we're inside the room viewport
+    const VpPoint vpt = play.ScreenToRoomDivDown(x, y);
     x = vpt.first.X;
     y = vpt.first.Y;
-    if ((x >= thisroom.Width) || (x < 0) || (y < 0) || (y >= thisroom.Height))
+    const int view_index = vpt.second;
+    if ((view_index < 0) || (x < 0) || (y < 0) || (x >= thisroom.Width) || (y >= thisroom.Height))
+    {
+        loc_index = kSavedLocType_Undefined;
         return "";
+    }
 
-    int onhs,aa;
+    // Get if we're above any interactable location
+    const int loctype = GetLocationType(x, y); // GetLocationType takes screen coords
+    const int onhs = getloctype_index; // FIXME: stop using global variable
     if (loctype == 0)
     {
-        if (play.get_loc_name_last_time != 0)
-        {
-            play.get_loc_name_last_time = 0;
-            GUIE::MarkSpecialLabelsForUpdate(kLabelMacro_Overhotspot);
-        }
+        loc_index = kSavedLocType_NoHotspot;
         return "";
     }
 
     // on character
     if (loctype == LOCTYPE_CHAR)
     {
-        onhs = getloctype_index;
-        out_name = get_translation(game.chars2[onhs].name_new.GetCStr());
-        if (play.get_loc_name_last_time != 2000+onhs)
-            GUIE::MarkSpecialLabelsForUpdate(kLabelMacro_Overhotspot);
-        play.get_loc_name_last_time = 2000+onhs;
-        return out_name;
+        loc_index = kSavedLocType_Character + onhs;
+        return get_translation(game.chars2[onhs].name_new.GetCStr());
     }
     // on object
     if (loctype == LOCTYPE_OBJ)
     {
-        aa = getloctype_index;
-        out_name = get_translation(croom->obj[aa].name.GetCStr());
+        const char *out_name = get_translation(croom->obj[onhs].name.GetCStr());
         // Compatibility: < 3.1.1 games returned space for nameless object
         // (presumably was a bug, but fixing it affected certain games behavior)
         if (loaded_game_file_version < kGameVersion_311 && out_name[0] == 0)
         {
             out_name = " ";
         }
-        if (play.get_loc_name_last_time != 3000+aa)
-            GUIE::MarkSpecialLabelsForUpdate(kLabelMacro_Overhotspot);
-        play.get_loc_name_last_time = 3000+aa;
+        loc_index = kSavedLocType_Object + onhs;
         return out_name;
     }
-    onhs = getloctype_index;
-    if (onhs>0)
-        out_name = get_translation(croom->hotspot[onhs].Name.GetCStr());
-    if (play.get_loc_name_last_time != onhs)
+    // on hotspot
+    if (onhs > 0)
+    {
+        loc_index = kSavedLocType_Hotspot + onhs;
+        return get_translation(croom->hotspot[onhs].Name.GetCStr());
+    }
+    return "";
+}
+
+const char *GetLocationName(int x, int y)
+{
+    int loc_index;
+    const char *loc_name = GetLocationNameAndIndex(x, y, loc_index);
+
+    // If it's a new location, different from the last time we checked,
+    // then update "@OVERHOTSPOT@" label(s), and save the last index
+    if (play.get_loc_name_last_time != loc_index)
+    {
         GUIE::MarkSpecialLabelsForUpdate(kLabelMacro_Overhotspot);
-    play.get_loc_name_last_time = onhs;
-    return out_name;
+        play.get_loc_name_last_time = loc_index;
+    }
+
+    return loc_name;
 }
 
 // GetLocationNameInBuf assumes a string buffer of MAX_MAXSTRLEN

Engine/ac/gamestate.h
@@ -64,6 +64,26 @@ enum GameStateSvgVersion
     kGSSvgVersion_361_14    = 4,
 };
 
+// SavedLocationType defines the type of location which
+// was last hovered by the mouse cursor.
+// This value is used as a index base for the respective kind of entity.
+// See GamePlayState::get_loc_name_last_time and get_loc_name_save_cursor
+enum SavedLocationType
+{
+    // not over a room
+    kSavedLocType_Undefined = -1,
+    // over a empty place in a room
+    kSavedLocType_NoHotspot = 0,
+    // hotspot base index
+    kSavedLocType_Hotspot   = 0,
+    // inv item base index
+    kSavedLocType_InvItem   = 1000,
+    // character base index
+    kSavedLocType_Character = 2000,
+    // room object base index
+    kSavedLocType_Object    = 3000,
+};
+
 
 // A GameState is a parent class for the game states.
 class GameState
@@ -250,8 +270,8 @@ struct GamePlayState
     bool  rtint_enabled = false;
     int   end_cutscene_music = 0;
     int   skip_until_char_stops = 0;
-    int   get_loc_name_last_time = 0;
-    int   get_loc_name_save_cursor = 0;
+    int   get_loc_name_last_time = kSavedLocType_Undefined;
+    int   get_loc_name_save_cursor = kSavedLocType_Undefined;
     int   restore_cursor_mode_to = 0;
     int   restore_cursor_image_to = 0;
     short music_queue_size = 0;

Engine/ac/gui.cpp
@@ -487,7 +487,7 @@ void process_interface_click(int ifce, int btn, int mbut) {
     }
 }
 
-
+// FIXME: rewrite this awful code, use ready GUILabelMacro (?)
 void replace_macro_tokens(const char *text, String &fixed_text) {
     const char*curptr=&text[0];
     char tmpm[3];

Engine/main/engine.cpp
@@ -734,8 +734,8 @@ void engine_init_game_settings()
     play.silent_midi = 0;
     play.current_music_repeating = 0;
     play.skip_until_char_stops = -1;
-    play.get_loc_name_last_time = -1;
-    play.get_loc_name_save_cursor = -1;
+    play.get_loc_name_last_time = kSavedLocType_Undefined;
+    play.get_loc_name_save_cursor = kSavedLocType_Undefined;
     play.restore_cursor_mode_to = -1;
     play.restore_cursor_image_to = -1;
     play.ground_level_areas_disabled = 0;

Engine/main/game_run.cpp
@@ -1104,7 +1104,7 @@ static void UpdateMouseOverLocation()
         (mouse_on_iface < 0) && (ifacepopped < 0)) {
             // we have saved the cursor, but the mouse location has changed
             // and it's time to restore it
-            play.get_loc_name_save_cursor = -1;
+            play.get_loc_name_save_cursor = kSavedLocType_Undefined;
             set_cursor_mode(play.restore_cursor_mode_to);
 
             if (cur_mode == play.restore_cursor_mode_to)

commit 9602b22611294e9127e3ac14c10e8081407cc353

Engine: fix GetLocationName in a scrolling room
Was broken by the recent 3c26a93cbe462b067fb4cec53d9f52197a4ae3e7

Engine/ac/global_game.cpp
@@ -751,19 +751,18 @@ static const char *GetLocationNameAndIndex(int x, int y, int &loc_index)
         }
     }
 
+    const int loctype = GetLocationType(x, y); // GetLocationType takes screen coords
     // Find out if we're inside the room viewport
     const VpPoint vpt = play.ScreenToRoomDivDown(x, y);
-    x = vpt.first.X;
-    y = vpt.first.Y;
+    const Point room_pt = vpt.first;
     const int view_index = vpt.second;
-    if ((view_index < 0) || (x < 0) || (y < 0) || (x >= thisroom.Width) || (y >= thisroom.Height))
+    if ((view_index < 0) || (!RectWH(0, 0, thisroom.Width, thisroom.Height).IsInside(room_pt)))
     {
         loc_index = kSavedLocType_Undefined;
         return "";
     }
 
     // Get if we're above any interactable location
-    const int loctype = GetLocationType(x, y); // GetLocationType takes screen coords
     const int onhs = getloctype_index; // FIXME: stop using global variable
     if (loctype == 0)
     {
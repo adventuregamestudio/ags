64f1b410fe0ed78b92b30931ea1d1a5e4c9379cd
Ivan Mogilko <ikm_spb@yahoo.com>
2025-08-03 12:38:25 +0300

    Engine: fix DialogOptions::CalcOptionsHeight() accumulating font's ext
    This fixes total options height calculation, where the font's graphical extension height would be added per *each* option instead of only last option.
    For a proper calculation, it is the font's linespacing should be added between options, and extension applied after the last option only, and only if there's no parser input field.
    
    This cuts extra blank space for both old and new games.

--- Engine/ac/dialog.cpp
+++ Engine/ac/dialog.cpp
@@ -420,7 +420,7 @@ int write_dialog_options(Bitmap *ds, bool ds_has_alpha, int dlgxp, int curyp, in
     }
     for (size_t cc=0;cc<Lines.Count();cc++) {
       wouttext_outline(ds, dlgxp+((cc==0) ? 0 : 9)+bullet_wid, curyp, usingfont, text_color, Lines[cc].GetCStr());
-      curyp+=linespacing;
+      curyp += linespacing;
     }
     if (ww < numdisp-1)
       curyp += data_to_game_coord(game.options[OPT_DIALOGGAP]);
@@ -547,16 +547,31 @@ private:
 
 void DialogOptions::CalcOptionsHeight()
 {
+    // According to the options drawing logic:
+    // * the distance between LINES in a multiline option is font linespacing;
+    // * the distance between OPTIONS is font linespacing + OPT_DIALOGGAP;
+    // * IF there's a parser input, we also add another linespacing + OPT_DIALOGGAP
+    //   after the last option;
+    // * IF there's no parser, then we add *font's graphical height* + OPT_DIALOGGAP,
+    //   to ensure that the text is not going to be cut off visually.
     needheight = 0;
+    int total_lines = 0;
+    // TODO: cache breaking text into lines, don't repeat the process in Draw
     for (int i = 0; i < numdisp; ++i)
     {
         const char *draw_text = skip_voiceover_token(get_translation(dtop->optionnames[disporder[i]]));
         break_up_text_into_lines(draw_text, Lines, areawid-(2*padding+2+bullet_wid), usingfont);
-        needheight += get_text_lines_surf_height(usingfont, Lines.Count()) + data_to_game_coord(game.options[OPT_DIALOGGAP]);
+        total_lines += Lines.Count();
     }
+    needheight = linespacing * (total_lines - 1) + data_to_game_coord(game.options[OPT_DIALOGGAP]) * (numdisp - 1);
     if (parserInput)
     {
+        needheight += linespacing + data_to_game_coord(game.options[OPT_DIALOGGAP]);
         needheight += parserInput->GetHeight() + data_to_game_coord(game.options[OPT_DIALOGGAP]);
+    }
+    else
+    {
+        needheight += get_text_lines_surf_height(usingfont, 1) + data_to_game_coord(game.options[OPT_DIALOGGAP]);
     }
 }
 

adventuregamestudio/ags

commit 3994575fb2c2fdb37b1a0cc0d752ca5150cbe7ce

Engine: fixed @OVERHOTSPOT@ labels update after a blocking action ends

Engine/ac/dialog.cpp
@@ -922,9 +922,11 @@ bool DialogOptions::Run()
     // Optionally run full game update, otherwise only minimal auto & overlay update
     if (runGameLoopsInBackground)
     {
-        play.disabled_user_interface++;
+        // CHECKME: why do we disable user interface here, but not when doing
+        // render_graphics() in another branch below?
+        DisableInterfaceEx(false /* don't change cursor */);
         UpdateGameOnce(false, ddb, dirtyx, dirtyy);
-        play.disabled_user_interface--;
+        EnableInterfaceEx(false /* don't change cursor */);
 
         // Stop the dialog options if requested from script
         if (doStop)

Engine/ac/display.cpp
@@ -426,8 +426,10 @@ ScreenOverlay *display_main(int xx, int yy, int wii, const char *text,
     if ((look.Style == kDisplayTextStyle_PlainText || look.Style == kDisplayTextStyle_Overchar)
         && (disp_type < kDisplayText_NormalOverlay))
     {
-        // update the all_buttons_disabled variable in advance
-        // of the adjust_x/y_for_guis calls
+        // Update the GUI disabled state in advance of the adjust_x/y_for_guis calls;
+        // this is done to avoid display box moved away from GUIs that will be hidden
+        // FIXME: this is a misleading hack, find a way to do this without tweaking
+        // play.disabled_user_interface.
         play.disabled_user_interface++;
         update_gui_disabled_status();
         play.disabled_user_interface--;

Engine/ac/global_gui.cpp
@@ -189,24 +189,53 @@ void SetGUIBackgroundPic (int guin, int slotn) {
   GUI_SetBackgroundGraphic(&scrGui[guin], slotn);
 }
 
-void DisableInterface() {
-  // If GUI looks change when disabled, then mark all of them for redraw
-  bool redraw_gui = (play.disabled_user_interface == 0) && // only if was enabled before
-      (GUI::Options.DisabledStyle != kGuiDis_Unchanged);
-  GUIE::MarkAllGUIForUpdate(redraw_gui, true);
-  play.disabled_user_interface++;
-  set_mouse_cursor(CURS_WAIT);
+void DisableInterfaceEx(bool update_cursor)
+{
+    play.disabled_user_interface++;
+    if (play.disabled_user_interface == 1) // if just switched to disabled
+    {
+        // If GUI looks change when disabled, then mark all of them for redraw
+        GUIE::MarkAllGUIForUpdate(GUI::Options.DisabledStyle != kGuiDis_Unchanged, true);
+        // Also mark "overhotspot" labels for update, as their text gets reset
+        // to empty string when the interface is disabled
+        GUIE::MarkSpecialLabelsForUpdate(kLabelMacro_Overhotspot);
+    }
+    // We update cursor even if interface was already disabled prior,
+    // because disabled status may had been toggled by internal game logic too
+    if (update_cursor)
+    {
+        set_mouse_cursor(CURS_WAIT);
+    }
 }
 
-void EnableInterface() {
-  play.disabled_user_interface--;
-  if (play.disabled_user_interface<1) {
-    play.disabled_user_interface=0;
-    set_default_cursor();
-    // If GUI looks change when disabled, then mark all of them for redraw
-    GUIE::MarkAllGUIForUpdate(GUI::Options.DisabledStyle != kGuiDis_Unchanged, true);
-  }
+void DisableInterface()
+{
+    DisableInterfaceEx(true);
 }
+
+void EnableInterfaceEx(bool update_cursor)
+{
+    play.disabled_user_interface--;
+    if (play.disabled_user_interface < 1) // if just switched to enabled
+    {
+        play.disabled_user_interface = 0; // clamp to 0
+        // If GUI looks change when disabled, then mark all of them for redraw
+        GUIE::MarkAllGUIForUpdate(GUI::Options.DisabledStyle != kGuiDis_Unchanged, true);
+        // Also mark "overhotspot" labels for update, as their text gets reset
+        // to empty string when the interface is disabled
+        GUIE::MarkSpecialLabelsForUpdate(kLabelMacro_Overhotspot);
+        if (update_cursor)
+        {
+            set_default_cursor();
+        }
+    }
+}
+
+void EnableInterface()
+{
+    EnableInterfaceEx(true);
+}
+
 // Returns 1 if user interface is enabled, 0 if disabled
 int IsInterfaceEnabled() {
   return (play.disabled_user_interface > 0) ? 0 : 1;

Engine/ac/global_gui.h
@@ -36,6 +36,8 @@ int  GetFontLineSpacing(int fontnum);
 void SetGUIBackgroundPic (int guin, int slotn);
 void DisableInterface();
 void EnableInterface();
+void DisableInterfaceEx(bool update_cursor);
+void EnableInterfaceEx(bool update_cursor);
 // Returns 1 if user interface is enabled, 0 if disabled
 int  IsInterfaceEnabled();
 // pass trans=0 for fully solid, trans=100 for fully transparent

Engine/main/game_run.cpp
@@ -136,26 +136,18 @@ public:
     void Begin() override
     {
         assert(_disabledFor == FOR_EXITLOOP);
-        play.disabled_user_interface++;
-        // If GUI looks change when disabled, then mark all of them for redraw
-        GUIE::MarkAllGUIForUpdate(GUI::Options.DisabledStyle != kGuiDis_Unchanged, true);
-
         // Only change the mouse cursor if it hasn't been specifically changed first
         // (or if it's speech, always change it)
-        if (((cur_cursor == cur_mode) || (_untilType == UNTIL_NOOVERLAY)) &&
-            (cur_mode != CURS_WAIT))
-        {
-            set_mouse_cursor(CURS_WAIT);
-        }
+        bool should_update_cursor =
+            ((cur_cursor == cur_mode) || (_untilType == UNTIL_NOOVERLAY)) &&
+            (cur_mode != CURS_WAIT);
+        DisableInterfaceEx(should_update_cursor);
     }
     // End the state, release all resources
     void End() override
     {
         set_our_eip(77);
-        set_default_cursor();
-        // If GUI looks change when disabled, then mark all of them for redraw
-        GUIE::MarkAllGUIForUpdate(GUI::Options.DisabledStyle != kGuiDis_Unchanged, true);
-        play.disabled_user_interface--;
+        EnableInterfaceEx(true /* update cursor */);
 
         switch (_disabledFor)
         {

commit 5d0b5af996fb770e8dd6522e7f37c8c11d726071

Engine: fixed severe performance drop when dialog options are displayed
This was caused by certain changes in commits 3994575fb2c2fdb37b1a0cc0d752ca5150cbe7ce and cd78d94f82912d69de08bcb2a4ee99bb5a8bd5dc, which caused all the gui textures reupdate continuously while the dialog options are shown on screen.
Instead of disabling/reenabling interface each time a game is redrawn/updated during dialog options, disable when dialog options are brought to screen and enable when they are removed from screen.

Engine/ac/dialog.cpp
@@ -367,6 +367,8 @@ int run_dialog_script(int dialogID, int offse, int optionIndex)
     // the line below fixes the problem with the close-up face remaining on the
     // screen after they finish talking; however, it makes the dialog options
     // area flicker when going between topics.
+    // FIXME: find a better solution rather than disable/enable iface?
+    // this causes all gui texture reupdate twice, and other things...
     DisableInterface();
     UpdateGameOnce(); // redraw the screen to make sure it looks right
     EnableInterface();
@@ -725,6 +727,10 @@ void DialogOptions::Begin()
     needRedraw = false;
     wantRefresh = false;
     mouseison=-10;
+
+    // Disable rest of the game interface while the dialog options are displayed;
+    // note that this also disables "overhotspot" labels update
+    DisableInterfaceEx(false /* don't change cursor */);
 }
 
 void DialogOptions::Draw()
@@ -926,9 +932,7 @@ void DialogOptions::Draw()
 
     if (runGameLoopsInBackground)
     {
-        DisableInterfaceEx(false /* don't change cursor */);
         render_graphics(ddb, dirtyx, dirtyy);
-        EnableInterfaceEx(false /* don't change cursor */);
     }
 }
 
@@ -937,9 +941,6 @@ bool DialogOptions::Run()
     // Run() can be called in a loop, so keep events going.
     sys_evt_process_pending();
 
-    // Disable interface prior to updating & rendering the game in background;
-    // not that this also disables "overhotspot" labels
-    DisableInterfaceEx(false /* don't change cursor */);
     // Optionally run full game update, otherwise only minimal auto & overlay update
     if (runGameLoopsInBackground)
     {
@@ -951,7 +952,6 @@ bool DialogOptions::Run()
         UpdateCursorAndDrawables();
         render_graphics(ddb, dirtyx, dirtyy);
     }
-    EnableInterfaceEx(false /* don't change cursor */);
 
     // Stop the dialog options if wsa requested from script
     if (doStop)
@@ -1291,6 +1291,9 @@ void DialogOptions::End()
   // In case it's the QFG4 style dialog, remove the black screen
   play.in_conversation--;
   remove_screen_overlay(OVER_COMPLETE);
+
+  // Re-enabled the game GUI
+  EnableInterfaceEx(false /* don't change cursor */);
 }
 
 int run_dialog_entry(int dlgnum)
@@ -1498,6 +1501,8 @@ void DialogExec::Run()
             if (said_speech_line > 0)
             {
                 // fix the problem with the close-up face remaining on screen
+                // FIXME: find a better solution rather than disable/enable iface?
+                // this causes all gui texture reupdate twice, and other things...
                 DisableInterface();
                 UpdateGameOnce(); // redraw the screen to make sure it looks right
                 EnableInterface();
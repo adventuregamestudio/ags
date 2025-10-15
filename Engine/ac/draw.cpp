adventuregamestudio/ags

commit 54946962e721c3b1d1e727af24e1ac91aecda7dc

Engine: fixed overlay's draw index designation
The previous method was assigning draw index following the order of overlay's storage index again, but we require them to follow the order of overlay's creation.

Engine/ac/draw.cpp
@@ -1065,6 +1065,18 @@ void reset_drawobj_dynamic_index()
     drawstate.NextDrawIndex = drawstate.FixedDrawIndexBase;
 }
 
+void add_drawobj_for_overlay(int objnum)
+{
+    if (objnum > 0)
+    {
+        if (static_cast<uint32_t>(objnum) >= overtxs.size())
+            overtxs.resize(static_cast<uint32_t>(objnum) + 1);
+
+        overtxs[objnum].DrawIndex = drawstate.NextDrawIndex;
+        drawstate.NextDrawIndex = (drawstate.NextDrawIndex == UINT32_MAX) ? drawstate.FixedDrawIndexBase : drawstate.NextDrawIndex + 1;
+    }
+}
+
 void reset_drawobj_for_overlay(int objnum)
 {
     if (objnum > 0 && static_cast<size_t>(objnum) < overtxs.size())
@@ -2737,12 +2749,6 @@ static void construct_overlays()
         if (over.GetTransparency() == 255) continue; // skip fully transparent
 
         auto &overtx = overtxs[i];
-        if (overtxs[i].DrawIndex == 0u)
-        {
-            overtxs[i].DrawIndex = drawstate.NextDrawIndex;
-            drawstate.NextDrawIndex = (drawstate.NextDrawIndex == UINT32_MAX) ? drawstate.FixedDrawIndexBase : drawstate.NextDrawIndex + 1;
-        }
-
         bool has_changed = over.HasChanged();
         // If walk behinds are drawn over the cached object sprite, then check if positions were updated
         if (crop_walkbehinds && over.IsRoomLayer())

Engine/ac/draw.h
@@ -84,6 +84,8 @@ void mark_object_changed(int objid);
 // allocates a drawable for itself, and disposes one if being removed.
 // Resets drawing index for dynamic objects
 void reset_drawobj_dynamic_index();
+// Assigns drawable object index for this overlay
+void add_drawobj_for_overlay(int objnum);
 // Resets drawable object for this overlay
 void reset_drawobj_for_overlay(int objnum);
 // Marks all game objects which reference this sprite for redraw

Engine/ac/overlay.cpp
@@ -458,6 +458,7 @@ size_t add_screen_overlay_impl(bool roomlayer, int x, int y, int type, int sprnu
     over.MarkChanged();
     screenover[type] = std::move(over);
     play.overlay_count++;
+    add_drawobj_for_overlay(type);
     return type;
 }
 

commit cca3d1c8a8c8b45f2d71c88a42b221cb9e68b26c

Engine: fix overlay caching in software render mode
Was broken by the recent commit 54946962e721c3b1d1e727af24e1ac91aecda7dc

Engine/ac/draw.cpp
@@ -1070,7 +1070,11 @@ void add_drawobj_for_overlay(int objnum)
     if (objnum > 0)
     {
         if (static_cast<uint32_t>(objnum) >= overtxs.size())
+        {
             overtxs.resize(static_cast<uint32_t>(objnum) + 1);
+            if (drawstate.SoftwareRender)
+                overcache.resize(static_cast<uint32_t>(objnum) + 1, Point(INT32_MIN, INT32_MIN));
+        }
 
         overtxs[objnum].DrawIndex = drawstate.NextDrawIndex;
         drawstate.NextDrawIndex = (drawstate.NextDrawIndex == UINT32_MAX) ? drawstate.FixedDrawIndexBase : drawstate.NextDrawIndex + 1;
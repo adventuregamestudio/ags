92f4296ecac8a93bdb7ed414e3df89291a1b54cb
Ivan Mogilko <ikm_spb@yahoo.com>
2025-11-21 19:05:44 +0300

    Editor: in AutoComplete list display parent extenders for child types

--- Editor/AGS.Types/EditorFeatures/AutoComplete/ScriptStruct.cs
+++ Editor/AGS.Types/EditorFeatures/AutoComplete/ScriptStruct.cs
@@ -6,18 +6,20 @@ namespace AGS.Types.AutoComplete
 {
     public class ScriptStruct : ScriptToken
     {
-        public ScriptStruct(string name, string ifDefOnly, string ifNDefOnly, int scriptCharacterIndex)
+        public ScriptStruct(string name, string parentType, string ifDefOnly, string ifNDefOnly, int scriptCharacterIndex)
         {
             Name = name;
+            ParentType = parentType;
             IfDefOnly = ifDefOnly;
             IfNDefOnly = ifNDefOnly;
             FullDefinition = true;
             StartsAtCharacterIndex = scriptCharacterIndex;
         }
 
-        public ScriptStruct(string name, string baseType, string ifDefOnly, string ifNDefOnly, int scriptCharacterIndex)
+        public ScriptStruct(string name, string parentType, string baseType, string ifDefOnly, string ifNDefOnly, int scriptCharacterIndex)
         {
             Name = name;
+            ParentType = parentType;
             BaseType = baseType;
             IfDefOnly = ifDefOnly;
             IfNDefOnly = ifNDefOnly;
@@ -56,8 +58,12 @@ namespace AGS.Types.AutoComplete
         }
 
         public string Name;
-        // Base type of the complex type, e.g. type of elements for array type
+        /// Parent type (inherited type)
+        public string ParentType;
+        /// Base type of the complex type, e.g. type of elements for multi-dimensional dynamic array,
+        /// or pointer "type" of another type
         public string BaseType;
+        /// Tells if this is a fully defined struct, or a temporary struct draft
         public bool FullDefinition;
         public List<ScriptVariable> Variables = new List<ScriptVariable>();
         public List<ScriptFunction> Functions = new List<ScriptFunction>();

33bf03f7dd3f82380f662ee823f819b2aa6f254c
Ivan Mogilko <ikm_spb@yahoo.com>
2025-12-13 17:05:06 +0300

    Editor: actually set ColorMode for Color prop editor and converter

--- Editor/AGS.Editor/Components/SettingsComponent.cs
+++ Editor/AGS.Editor/Components/SettingsComponent.cs
@@ -21,6 +21,9 @@ namespace AGS.Editor.Components
             RecreateDocument();
             _guiController.RegisterIcon(ICON_KEY, Resources.ResourceManager.GetIcon("iconsett.ico"));
             _guiController.ProjectTree.AddTreeRoot(this, "GeneralSettings", "General Settings", ICON_KEY);
+
+            Factory.Events.GamePostLoad += Events_GamePostLoad;
+            Factory.Events.GameSettingsChanged += Events_GameSettingsChanged;
         }
 
         private void RecreateDocument()
@@ -95,6 +98,18 @@ namespace AGS.Editor.Components
             _agsEditor.CurrentGame.Settings.DialogOptionsGUI = UpdatePropertyOnGuiMove(_agsEditor.CurrentGame.Settings.DialogOptionsGUI, oldID, gui.ID, false);
             _agsEditor.CurrentGame.Settings.ThoughtGUI = UpdatePropertyOnGuiMove(_agsEditor.CurrentGame.Settings.ThoughtGUI, oldID, gui.ID, false);
             _settingsPane.RefreshData();
+        }
+
+        private void Events_GamePostLoad(Game game)
+        {
+            ColorUIEditor.ColorMode = game.Settings.ColorDepth;
+            CustomColorConverter.ColorMode = game.Settings.ColorDepth;
+        }
+
+        private void Events_GameSettingsChanged()
+        {
+            ColorUIEditor.ColorMode = _agsEditor.CurrentGame.Settings.ColorDepth;
+            CustomColorConverter.ColorMode = _agsEditor.CurrentGame.Settings.ColorDepth;
         }
     }
 }

6333efc8e87309b3f85c5ee36265239f962c93d4
Johan Wigert <45201794+jwigert@users.noreply.github.com>
2025-12-31 18:29:00 +0100

    Update mask bitmap error message

--- Editor/AGS.Editor/Panes/Room/RoomEditFilters/BaseAreasEditorFilter.cs
+++ Editor/AGS.Editor/Panes/Room/RoomEditFilters/BaseAreasEditorFilter.cs
@@ -524,7 +524,7 @@ namespace AGS.Editor
 
                 if (!bmp.IsIndexed())
                 {
-                    Factory.GUIController.ShowMessage("This is not a valid mask bitmap. Masks must be an indexed colour images (1, 4 or 8-bit), using the first colours in the palette to draw the room areas.", MessageBoxIcon.Warning);
+                    Factory.GUIController.ShowMessage("This is not a valid mask bitmap. Masks must be indexed colour images (1-, 4-, or 8-bit) that use the first colours in the palette to draw the room areas.", MessageBoxIcon.Warning);
                     bmp.Dispose();
                     return;
                 }
@@ -709,3 +709,4 @@ namespace AGS.Editor
         }
     }
 }
+

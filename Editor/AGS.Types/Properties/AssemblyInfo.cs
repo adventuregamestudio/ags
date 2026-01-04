2c233c04c602ff17df83890b60ba2d51de48f237
Ivan Mogilko <ikm_spb@yahoo.com>
2026-01-05 01:18:52 +0300

    Updated build version (3.6.2.16 P6)

--- Editor/AGS.Types/Properties/AssemblyInfo.cs
+++ Editor/AGS.Types/Properties/AssemblyInfo.cs
@@ -23,10 +23,10 @@ namespace AGS.Types
     public class Version
     {
         public static readonly bool IS_BETA_VERSION = false;
-        public const string AGS_EDITOR_DATE = "November 2025";
+        public const string AGS_EDITOR_DATE = "January 2026";
         public const string AGS_EDITOR_FRIENDLY_VERSION = "3.6.2";
-        public const string AGS_EDITOR_VERSION = "3.6.2.15";
-        public const string AGS_EDITOR_COPYRIGHT = "Copyright © 2006-2011 Chris Jones and 2011-2025 others.";
+        public const string AGS_EDITOR_VERSION = "3.6.2.16";
+        public const string AGS_EDITOR_COPYRIGHT = "Copyright © 2006-2011 Chris Jones and 2011-2026 others.";
         public static readonly string AGS_EDITOR_TARGETNAME =
             IntPtr.Size > 4 ? "64-bit" : "32-bit";
     }

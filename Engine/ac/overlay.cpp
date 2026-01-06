adventuregamestudio/ags

commit 4caf0765ef36b4ebc2fb609f99a36a56d0736255

Engine: perform SetTextOverlay through new Overlay_SetText algorithm
The implementation of Overlay_SetText() was rewritten in aaf6144e5123d36e4f04fcc6592b02ec18f869e0 , with a purpose of replacing only the internal image of overlay, but not going all the way of removing/creating a new overlay into the same slot.

In addition to the initial reasons, it became even more essential after the overlay storage optimization (see merge commit 77b8264da4b602d717953f6da79eaa1c3d70dc91), as overlay system now records free ids on overlay removal. As recreating overlay with exact same id would now require extra hacks into overlay logic.

Unfortunately, an old SetTextOverlay() function was forgotten about, and it still doing this remove/create way of resetting a text. This results in engine wrongly considering certain ids to be free, while they are already taken.

This commit fixes this by making SetTextOverlay delegate to the new Overlay_SetText() algorithm instead.

Engine/ac/overlay.cpp
@@ -58,9 +58,12 @@ void Overlay_SetText(ScriptOverlay *scover, int width, int fontid, int text_colo
     auto *over = get_overlay(scover->overlayId);
     if (!over)
         quit("!Overlay.SetText: invalid overlay ID specified");
-    const int x = over->x;
-    const int y = over->y;
 
+    Overlay_SetText(*over, over->x, over->y, width, fontid, text_color, text);
+}
+
+void Overlay_SetText(ScreenOverlay &over, int x, int y, int width, int fontid, int text_color, const char *text)
+{
     // TODO: find a nice way to refactor and share these code pieces
     // from CreateTextOverlay
     width = data_to_game_coord(width);
@@ -84,7 +87,7 @@ void Overlay_SetText(ScriptOverlay *scover, int width, int fontid, int text_colo
         width, fontid, allow_shrink, has_alpha);
 
     // Update overlay properties
-    over->SetImage(std::unique_ptr<Bitmap>(image), has_alpha, adj_x - dummy_x, adj_y - dummy_y);
+    over.SetImage(std::unique_ptr<Bitmap>(image), has_alpha, adj_x - dummy_x, adj_y - dummy_y);
 }
 
 int Overlay_GetX(ScriptOverlay *scover) {

Engine/ac/global_overlay.cpp
@@ -42,15 +42,16 @@ int CreateTextOverlay(int xx, int yy, int wii, int fontid, int text_color, const
         allowShrink = 1;
 
     auto *over = Overlay_CreateTextCore(false, xx, yy, wii, fontid, text_color, text, disp_type, allowShrink);
+    assert((disp_type < OVER_FIRSTFREE) || (disp_type == over->type));
     return over ? over->type : 0;
 }
 
 void SetTextOverlay(int ovrid, int xx, int yy, int wii, int fontid, int text_color, const char *text) {
-    RemoveOverlay(ovrid);
-    const int disp_type = ovrid;
-    int new_ovrid = CreateTextOverlay(xx, yy, wii, fontid, text_color, text, disp_type);
-    if (new_ovrid != ovrid)
-        quit("SetTextOverlay internal error: inconsistent type ids");
+    auto *over = get_overlay(ovrid);
+    if (!over)
+        quit("!SetTextOverlay: invalid overlay ID specified");
+
+    Overlay_SetText(*over, xx, yy, wii, fontid, text_color, text);
 }
 
 void MoveOverlay(int ovrid, int newx,int newy) {

Engine/ac/overlay.h
@@ -26,7 +26,8 @@ namespace AGS { namespace Common { class Bitmap; } }
 using namespace AGS; // FIXME later
 
 void Overlay_Remove(ScriptOverlay *sco);
-void Overlay_SetText(ScriptOverlay *scover, int wii, int fontid, int clr, const char*text);
+void Overlay_SetText(ScriptOverlay *scover, int width, int fontid, int text_color, const char *text);
+void Overlay_SetText(ScreenOverlay &over, int x, int y, int width, int fontid, int text_color, const char *text);
 int  Overlay_GetX(ScriptOverlay *scover);
 void Overlay_SetX(ScriptOverlay *scover, int newx);
 int  Overlay_GetY(ScriptOverlay *scover);

commit 78175ddfdf850b3164a9172b946c410ad679156c

Engine: fix SetTextOverlay() ignoring new position parameters
Was broken by commit 4caf0765ef36b4ebc2fb609f99a36a56d0736255

Engine/ac/overlay.cpp
@@ -79,15 +79,17 @@ void Overlay_SetText(ScreenOverlay &over, int x, int y, int width, int fontid, i
     draw_text = skip_voiceover_token(draw_text);
 
     // Recreate overlay image
-    int dummy_x = x, dummy_y = y, adj_x = x, adj_y = y;
+    int adj_x = x, adj_y = y;
     bool has_alpha = false;
     // NOTE: we pass text_color negated to let optionally use textwindow (if applicable)
     // this is a generic ugliness of _display_main args, need to refactor later.
-    Bitmap *image = create_textual_image(draw_text, -text_color, 0, dummy_x, dummy_y, adj_x, adj_y,
+    Bitmap *image = create_textual_image(draw_text, -text_color, 0, x, y, adj_x, adj_y,
         width, fontid, allow_shrink, has_alpha);
 
     // Update overlay properties
-    over.SetImage(std::unique_ptr<Bitmap>(image), has_alpha, adj_x - dummy_x, adj_y - dummy_y);
+    over.x = x;
+    over.y = y;
+    over.SetImage(std::unique_ptr<Bitmap>(image), has_alpha, adj_x - x, adj_y - y);
 }
 
 int Overlay_GetX(ScriptOverlay *scover) {
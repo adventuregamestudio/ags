e57d010458a87e08325dfc19c1b3897ecdbc5155
Ivan Mogilko <ikm_spb@yahoo.com>
2026-02-05 17:26:52 +0300

    Editor: implement column sort in Global Variables

--- Editor/AGS.Editor/Panes/GlobalVariablesEditor.Designer.cs
+++ Editor/AGS.Editor/Panes/GlobalVariablesEditor.Designer.cs
@@ -29,17 +29,17 @@ namespace AGS.Editor
         private void InitializeComponent()
         {
             this.mainFrame = new System.Windows.Forms.GroupBox();
-            this.lvwWords = new System.Windows.Forms.ListView();
-            this.clmName = new System.Windows.Forms.ColumnHeader();
-            this.clmType = new System.Windows.Forms.ColumnHeader();
-            this.clmDefaultValue = new System.Windows.Forms.ColumnHeader();
+            this.lvwVars = new AGS.Editor.SortableListView();
+            this.clmName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmDefaultValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.label1 = new System.Windows.Forms.Label();
             this.mainFrame.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainFrame
             // 
-            this.mainFrame.Controls.Add(this.lvwWords);
+            this.mainFrame.Controls.Add(this.lvwVars);
             this.mainFrame.Controls.Add(this.label1);
             this.mainFrame.Location = new System.Drawing.Point(5, 4);
             this.mainFrame.Name = "mainFrame";
@@ -48,24 +48,24 @@ namespace AGS.Editor
             this.mainFrame.TabStop = false;
             this.mainFrame.Text = "Global Variables";
             // 
-            // lvwWords
+            // lvwVars
             // 
-            this.lvwWords.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lvwVars.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.clmName,
             this.clmType,
             this.clmDefaultValue});
-            this.lvwWords.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lvwWords.FullRowSelect = true;
-            this.lvwWords.HideSelection = false;
-            this.lvwWords.Location = new System.Drawing.Point(3, 75);
-            this.lvwWords.MultiSelect = false;
-            this.lvwWords.Name = "lvwWords";
-            this.lvwWords.Size = new System.Drawing.Size(504, 436);
-            this.lvwWords.TabIndex = 1;
-            this.lvwWords.UseCompatibleStateImageBehavior = false;
-            this.lvwWords.View = System.Windows.Forms.View.Details;
-            this.lvwWords.ItemActivate += new System.EventHandler(this.lvwWords_ItemActivate);
-            this.lvwWords.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lvwWords_MouseUp);
+            this.lvwVars.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvwVars.FullRowSelect = true;
+            this.lvwVars.HideSelection = false;
+            this.lvwVars.Location = new System.Drawing.Point(3, 64);
+            this.lvwVars.MultiSelect = false;
+            this.lvwVars.Name = "lvwVars";
+            this.lvwVars.Size = new System.Drawing.Size(504, 447);
+            this.lvwVars.TabIndex = 1;
+            this.lvwVars.UseCompatibleStateImageBehavior = false;
+            this.lvwVars.View = System.Windows.Forms.View.Details;
+            this.lvwVars.ItemActivate += new System.EventHandler(this.lvwVars_ItemActivate);
+            this.lvwVars.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lvwVars_MouseUp);
             // 
             // clmName
             // 
@@ -86,18 +86,18 @@ namespace AGS.Editor
             // 
             this.label1.AutoSize = true;
             this.label1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label1.Location = new System.Drawing.Point(3, 20);
+            this.label1.Location = new System.Drawing.Point(3, 17);
             this.label1.MaximumSize = new System.Drawing.Size(502, 0);
             this.label1.Name = "label1";
             this.label1.Padding = new System.Windows.Forms.Padding(3, 9, 3, 12);
-            this.label1.Size = new System.Drawing.Size(500, 55);
+            this.label1.Size = new System.Drawing.Size(501, 47);
             this.label1.TabIndex = 0;
             this.label1.Text = "Global variables allow you to create variables which you can access from all your" +
     " scripts. Right-click in the list to add, modify and delete variables.";
             // 
             // GlobalVariablesEditor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.mainFrame);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -115,7 +115,7 @@ namespace AGS.Editor
         #endregion
 
         private System.Windows.Forms.GroupBox mainFrame;
-        private System.Windows.Forms.ListView lvwWords;
+        private AGS.Editor.SortableListView lvwVars;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ColumnHeader clmName;
         private System.Windows.Forms.ColumnHeader clmType;

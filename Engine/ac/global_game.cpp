adventuregamestudio/ags

commit df399aaea6b2f73be5d5e4755f209e3b5bfb0363

Script API: support passing arbitrary sprite to SaveGameSlot()

Engine/ac/global_game.cpp
@@ -17,7 +17,6 @@
 #include "core/platform.h"
 #include "ac/audiocliptype.h"
 #include "ac/common.h"
-#include "ac/view.h"
 #include "ac/character.h"
 #include "ac/draw.h"
 #include "ac/dynamicsprite.h"
@@ -27,6 +26,7 @@
 #include "ac/gamesetupstruct.h"
 #include "ac/gamestate.h"
 #include "ac/global_character.h"
+#include "ac/global_display.h"
 #include "ac/global_gui.h"
 #include "ac/global_hotspot.h"
 #include "ac/global_inventoryitem.h"
@@ -42,6 +42,7 @@
 #include "ac/roomstatus.h"
 #include "ac/string.h"
 #include "ac/system.h"
+#include "ac/view.h"
 #include "debug/debugger.h"
 #include "debug/debug_log.h"
 #include "font/fonts.h"
@@ -62,6 +63,7 @@
 #include "util/path.h"
 #include "util/string_utils.h"
 #include "media/audio/audio_system.h"
+#include "platform/base/agsplatformdriver.h"
 #include "platform/base/sys_main.h"
 
 using namespace AGS::Common;
@@ -118,6 +120,39 @@ void RestoreGameSlot(int slnum) {
         return;
     }
     try_restore_save(slnum);
+}
+
+void SaveGameSlot(int slotn, const char *descript, int spritenum)
+{
+    VALIDATE_STRING(descript);
+
+    if (platform->GetDiskFreeSpaceMB(get_save_game_directory()) < 2)
+    {
+        Display("ERROR: There is not enough disk space free to save the game. Clear some disk space and try again.");
+        return;
+    }
+
+    // dont allow save in rep_exec_always, because we dont save
+    // the state of blocked scripts
+    can_run_delayed_command();
+
+    // Make a sprite copy, as save process may be scheduled and asynchronous (in theory)
+    std::unique_ptr<Bitmap> image;
+    if (spritenum >= 0)
+        image.reset(BitmapHelper::CreateBitmapCopy(spriteset[spritenum]));
+
+    if (inside_script)
+    {
+        curscript->QueueAction(PostScriptAction(ePSASaveGame, slotn, "SaveGameSlot", descript, std::move(image)));
+        return;
+    }
+
+    save_game(slotn, descript, std::move(image));
+}
+
+void SaveGameSlot2(int slnum, const char *descript)
+{
+    SaveGameSlot(slnum, descript, -1);
 }
 
 void DeleteSaveSlot (int slnum) {

Editor/AGS.Editor/Resources/agsdefns.sh
@@ -810,7 +810,7 @@ import void SaveGameDialog();
 /// Restarts the game from the restart point.
 import void RestartGame();
 /// Saves the current game position to the specified slot.
-import void SaveGameSlot(int slot, const string description);
+import void SaveGameSlot(int slot, const string description, int sprite = -1);
 /// Restores the game saved to the specified game slot.
 import void RestoreGameSlot(int slot);
 /// Deletes the specified save game.

Engine/ac/game.cpp
@@ -946,30 +946,13 @@ Bitmap *create_savegame_screenshot()
     return CopyScreenIntoBitmap(usewid, usehit, &viewport);
 }
 
-void save_game(int slotn, const char*descript)
+void save_game(int slotn, const String &descript, std::unique_ptr<Bitmap> &&image)
 {
-    VALIDATE_STRING(descript);
-
-    // dont allow save in rep_exec_always, because we dont save
-    // the state of blocked scripts
-    can_run_delayed_command();
-
-    if (inside_script) {
-        get_executingscript()->QueueAction(PostScriptAction(ePSASaveGame, slotn, "SaveGameSlot", descript));
-        return;
-    }
-
-    if (platform->GetDiskFreeSpaceMB(get_save_game_directory()) < 2) {
-        Display("ERROR: There is not enough disk space free to save the game. Clear some disk space and try again.");
-        return;
-    }
-
     String nametouse = get_save_game_path(slotn);
-    std::unique_ptr<Bitmap> screenShot;
-    if (game.options[OPT_SAVESCREENSHOT] != 0)
-        screenShot.reset(create_savegame_screenshot());
+    if (!image && (game.options[OPT_SAVESCREENSHOT] != 0))
+        image.reset(create_savegame_screenshot());
 
-    std::unique_ptr<Stream> out(StartSavegame(nametouse, descript, screenShot.get()));
+    std::unique_ptr<Stream> out(StartSavegame(nametouse, descript, image.get()));
     if (out == nullptr)
     {
         Display("ERROR: Unable to open savegame file for writing!");

Engine/ac/game.h
@@ -20,11 +20,12 @@
 
 #include <memory>
 #include "ac/dynobj/scriptviewframe.h"
+#include "gfx/bitmap.h"
 #include "main/game_file.h"
 #include "util/string.h"
 
 // Forward declaration
-namespace AGS { namespace Common { class AssetManager; class Bitmap; class Stream; } }
+namespace AGS { namespace Common { class AssetManager; class Stream; } }
 using namespace AGS; // FIXME later
 
 #define RAGMODE_PRESERVEGLOBALINT 1
@@ -161,7 +162,7 @@ bool do_save_game_dialog();
 void free_do_once_tokens();
 // Free all the memory associated with the game
 void unload_game();
-void save_game(int slotn, const char*descript);
+void save_game(int slotn, const Common::String &descript, std::unique_ptr<Common::Bitmap> &&image = nullptr);
 bool read_savedgame_description(const Common::String &savedgame, Common::String &description);
 std::unique_ptr<Common::Bitmap> read_savedgame_screenshot(const Common::String &savedgame);
 // Tries to restore saved game and displays an error on failure; if the error occured

Engine/ac/global_api.cpp
@@ -25,6 +25,7 @@
 #include "ac/dynamicsprite.h"
 #include "ac/event.h"
 #include "ac/game.h"
+#include "ac/gamestructdefines.h"
 #include "ac/global_audio.h"
 #include "ac/global_button.h"
 #include "ac/global_character.h"
@@ -1473,10 +1474,14 @@ RuntimeScriptValue Sc_save_game_dialog(const RuntimeScriptValue *params, int32_t
     API_SCALL_VOID(save_game_dialog);
 }
 
-// void (int slotn, const char*descript)
-RuntimeScriptValue Sc_save_game(const RuntimeScriptValue *params, int32_t param_count)
+RuntimeScriptValue Sc_SaveGameSlot(const RuntimeScriptValue *params, int32_t param_count)
 {
-    API_SCALL_VOID_PINT_POBJ(save_game, const char);
+    API_SCALL_VOID_PINT_POBJ_PINT(SaveGameSlot, const char);
+}
+
+RuntimeScriptValue Sc_SaveGameSlot2(const RuntimeScriptValue *params, int32_t param_count)
+{
+    API_SCALL_VOID_PINT_POBJ(SaveGameSlot2, const char);
 }
 
 // int (char*namm)
@@ -2304,7 +2309,7 @@ void ScPl_sc_sprintf(char *destt, const char *texx, ...)
 }
 
 
-void RegisterGlobalAPI()
+void RegisterGlobalAPI(ScriptAPIVersion base_api, ScriptAPIVersion /*compat_api*/)
 {
     ScFnRegister global_api[] = {
         { "AbortGame",                Sc_sc_AbortGame, ScPl_sc_AbortGame },
@@ -2551,7 +2556,6 @@ void RegisterGlobalAPI()
         { "SaidUnknownWord",          API_FN_PAIR(SaidUnknownWord) },
         { "SaveCursorForLocationChange", API_FN_PAIR(SaveCursorForLocationChange) },
         { "SaveGameDialog",           API_FN_PAIR(save_game_dialog) },
-        { "SaveGameSlot",             API_FN_PAIR(save_game) },
         { "SaveScreenShot",           API_FN_PAIR(SaveScreenShot) },
         { "SeekMIDIPosition",         API_FN_PAIR(SeekMIDIPosition) },
         { "SeekMODPattern",           API_FN_PAIR(SeekMODPattern) },
@@ -2681,6 +2685,13 @@ void RegisterGlobalAPI()
         { "WaitInput",                API_FN_PAIR(WaitInput) },
         { "SkipWait",                 API_FN_PAIR(SkipWait) },
     };
+
+    // Few functions have to be selected based on API level,
+    // because historically AGS compiler did not generate "number of args" in the import name.
+    if (base_api < kScriptAPI_v362)
+        ccAddExternalStaticFunction("SaveGameSlot", API_FN_PAIR(SaveGameSlot2));
+    else
+        ccAddExternalStaticFunction("SaveGameSlot", API_FN_PAIR(SaveGameSlot));
 
     ccAddExternalFunctions(global_api);
 }

Engine/ac/global_game.h
@@ -43,6 +43,8 @@ void AbortGame();
 void GiveScore(int amnt);
 void restart_game();
 void RestoreGameSlot(int slnum);
+void SaveGameSlot(int slnum, const char *descript, int spritenum);
+void SaveGameSlot2(int slnum, const char *descript);
 void DeleteSaveSlot (int slnum);
 int  GetSaveSlotDescription(int slnum,char*desbuf);
 int  LoadSaveSlotScreenshot(int slnum, int width, int height);

Engine/script/cc_instance.cpp
@@ -11,16 +11,18 @@
 // https://opensource.org/license/artistic-2-0/
 //
 //=============================================================================
+#include "script/cc_instance.h"
 #include <cstdio>
 #include <deque>
 #include <string.h>
 #include "ac/common.h"
-#include "ac/dynobj/cc_dynamicarray.h"
-#include "ac/dynobj/managedobjectpool.h"
-#include "ac/dynobj/dynobj_manager.h"
 #include "ac/sys_events.h"
+#include "ac/dynobj/cc_dynamicarray.h"
+#include "ac/dynobj/dynobj_manager.h"
+#include "ac/dynobj/managedobjectpool.h"
+#include "ac/dynobj/scriptstring.h"
+#include "ac/dynobj/scriptuserobject.h"
 #include "gui/guidefines.h"
-#include "script/cc_instance.h"
 #include "debug/debug_log.h"
 #include "debug/out.h"
 #include "script/cc_common.h"
@@ -30,8 +32,6 @@
 #include "util/bbop.h"
 #include "util/stream.h"
 #include "util/textstreamwriter.h"
-#include "ac/dynobj/scriptstring.h"
-#include "ac/dynobj/scriptuserobject.h"
 #include "util/file.h"
 #include "util/memory.h"
 #include "util/string_utils.h" // linux strnicmp definition

Engine/script/executingscript.cpp
@@ -12,9 +12,9 @@
 //
 //=============================================================================
 #include <string.h>
-#include "executingscript.h"
-#include "debug/debug_log.h"
 #include "ac/game_version.h"
+#include "debug/debug_log.h"
+#include "script/executingscript.h"
 #include "script/script.h"
 
 using namespace AGS::Common;
@@ -25,7 +25,7 @@ QueuedScript::QueuedScript()
 {
 }
 
-void ExecutingScript::QueueAction(const PostScriptAction &act)
+void ExecutingScript::QueueAction(PostScriptAction &&act)
 {
     // A strange behavior in pre-2.7.0 games allowed to call NewRoom right after
     // RestartGame, cancelling RestartGame. Probably an unintended effect.
@@ -58,9 +58,9 @@ void ExecutingScript::QueueAction(const PostScriptAction &act)
         }
     }
 
-    PostScriptAction act_pos = act;
+    PostScriptAction act_pos = std::move(act);
     get_script_position(act_pos.Position);
-    PostScriptActions.push_back(act_pos);
+    PostScriptActions.push_back(std::move(act_pos));
 }
 
 void ExecutingScript::RunAnother(const char *namm, ScriptInstType scinst, size_t param_count, const RuntimeScriptValue *params)

commit 809d42542224f107629fa36580fc39a078317ee7

Engine: fix SetRestartPoint does not schedule save until after script
This logic was broken by refactor in df399aaea6b2f73be5d5e4755f209e3b5bfb0363

Engine/ac/global_game.cpp
@@ -526,14 +526,10 @@ void QuitGame(int dialog) {
     quit("|You have exited.");
 }
 
-
-
-
-void SetRestartPoint() {
-    save_game(RESTART_POINT_SAVE_GAME_NUMBER, "Restart Game Auto-Save");
+void SetRestartPoint()
+{
+    SaveGameSlot2(RESTART_POINT_SAVE_GAME_NUMBER, "Restart Game Auto-Save");
 }
-
-
 
 void SetGameSpeed(int newspd)
 {
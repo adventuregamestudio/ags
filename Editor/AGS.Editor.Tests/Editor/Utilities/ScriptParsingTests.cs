841c16d0b87725067d727b369f225e5f16f3877b
Ivan Mogilko <ikm_spb@yahoo.com>
2026-02-08 22:41:23 +0300

    Editor.Tests: use uniform linebreaks in parser tests

--- Editor/AGS.Editor.Tests/Editor/Utilities/ScriptParsingTests.cs
+++ Editor/AGS.Editor.Tests/Editor/Utilities/ScriptParsingTests.cs
@@ -42,70 +42,83 @@ function f()
 }
 ";
 
+        /// <summary>
+        /// Unify line endings in the input string, in case they depend on a enviroment.
+        /// </summary>
+        string UniformTestString(string s)
+        {
+            return s.Replace("\r\n", "\n");
+        }
+
         [Test]
         public void FillCommentSections_Test()
         {
-            ScriptParsing.ParserState state = new ScriptParsing.ParserState(TestScript);
+            // Unify line endings in the input string, in case they depend on a enviroment.
+            string script = UniformTestString(TestScript);
+
+            ScriptParsing.ParserState state = new ScriptParsing.ParserState(script);
             ScriptParsing.FillCommentSections(state);
 
             var commentsAsDict = state.Comments.ToDictionary(c => c.Item1, c => new Tuple<int, int>(c.Item1, c.Item2));
 
             Assert.AreEqual(9, commentsAsDict.Count);
-            Assert.That(commentsAsDict.ContainsKey(23));
-            Assert.AreEqual(23, commentsAsDict[23].Item1);
-            Assert.AreEqual(33, commentsAsDict[23].Item2);
-            Assert.That(commentsAsDict.ContainsKey(35));
-            Assert.AreEqual(35, commentsAsDict[35].Item1);
-            Assert.AreEqual(48, commentsAsDict[35].Item2);
-            Assert.That(commentsAsDict.ContainsKey(50));
-            Assert.AreEqual(50, commentsAsDict[50].Item1);
-            Assert.AreEqual(80, commentsAsDict[50].Item2);
-            Assert.That(commentsAsDict.ContainsKey(151));
-            Assert.AreEqual(151, commentsAsDict[151].Item1);
-            Assert.AreEqual(161, commentsAsDict[151].Item2);
-            Assert.That(commentsAsDict.ContainsKey(167));
-            Assert.AreEqual(167, commentsAsDict[167].Item1);
-            Assert.AreEqual(180, commentsAsDict[167].Item2);
-            Assert.That(commentsAsDict.ContainsKey(186));
-            Assert.AreEqual(186, commentsAsDict[186].Item1);
-            Assert.AreEqual(208, commentsAsDict[186].Item2);
-            Assert.That(commentsAsDict.ContainsKey(295));
-            Assert.AreEqual(295, commentsAsDict[295].Item1);
-            Assert.AreEqual(308, commentsAsDict[295].Item2);
-            Assert.That(commentsAsDict.ContainsKey(321));
-            Assert.AreEqual(321, commentsAsDict[321].Item1);
-            Assert.AreEqual(331, commentsAsDict[321].Item2);
-            Assert.That(commentsAsDict.ContainsKey(341));
-            Assert.AreEqual(341, commentsAsDict[341].Item1);
-            Assert.AreEqual(371, commentsAsDict[341].Item2);
+            Assert.That(commentsAsDict.ContainsKey(21));
+            Assert.AreEqual(21, commentsAsDict[21].Item1);
+            Assert.AreEqual(31, commentsAsDict[21].Item2);
+            Assert.That(commentsAsDict.ContainsKey(32));
+            Assert.AreEqual(32, commentsAsDict[32].Item1);
+            Assert.AreEqual(45, commentsAsDict[32].Item2);
+            Assert.That(commentsAsDict.ContainsKey(46));
+            Assert.AreEqual(46, commentsAsDict[46].Item1);
+            Assert.AreEqual(73, commentsAsDict[46].Item2);
+            Assert.That(commentsAsDict.ContainsKey(139));
+            Assert.AreEqual(139, commentsAsDict[139].Item1);
+            Assert.AreEqual(149, commentsAsDict[139].Item2);
+            Assert.That(commentsAsDict.ContainsKey(154));
+            Assert.AreEqual(154, commentsAsDict[154].Item1);
+            Assert.AreEqual(167, commentsAsDict[154].Item2);
+            Assert.That(commentsAsDict.ContainsKey(172));
+            Assert.AreEqual(172, commentsAsDict[172].Item1);
+            Assert.AreEqual(193, commentsAsDict[172].Item2);
+            Assert.That(commentsAsDict.ContainsKey(278));
+            Assert.AreEqual(278, commentsAsDict[278].Item1);
+            Assert.AreEqual(291, commentsAsDict[278].Item2);
+            Assert.That(commentsAsDict.ContainsKey(303));
+            Assert.AreEqual(303, commentsAsDict[303].Item1);
+            Assert.AreEqual(313, commentsAsDict[303].Item2);
+            Assert.That(commentsAsDict.ContainsKey(322));
+            Assert.AreEqual(322, commentsAsDict[322].Item1);
+            Assert.AreEqual(349, commentsAsDict[322].Item2);
         }
 
         [Test]
         public void GetCurrentFunctionCall_Test()
         {
-            ScriptParsing.ParserState state = new ScriptParsing.ParserState(TestScript);
+            string script = UniformTestString(TestScript);
+
+            ScriptParsing.ParserState state = new ScriptParsing.ParserState(script);
             ScriptParsing.FillCommentSections(state);
 
-            Assert.AreEqual(string.Empty, ScriptParsing.GetCurrentFunctionCall(state, 115));
-            Assert.AreEqual(string.Empty, ScriptParsing.GetCurrentFunctionCall(state, 209));
-            Assert.AreEqual("FuncWithStringArg(", ScriptParsing.GetCurrentFunctionCall(state, 245));
-            Assert.AreEqual("FuncWithStringArg(0, ", ScriptParsing.GetCurrentFunctionCall(state, 248));
-            Assert.AreEqual(@"FuncWithStringArg( /* comment */
+            Assert.AreEqual(string.Empty, ScriptParsing.GetCurrentFunctionCall(state, 105));
+            Assert.AreEqual(string.Empty, ScriptParsing.GetCurrentFunctionCall(state, 194));
+            Assert.AreEqual("FuncWithStringArg(", ScriptParsing.GetCurrentFunctionCall(state, 229));
+            Assert.AreEqual("FuncWithStringArg(0, ", ScriptParsing.GetCurrentFunctionCall(state, 232));
+            Assert.AreEqual(UniformTestString(@"FuncWithStringArg( /* comment */
         1, // comment
         /* multiline comment
 *
 *
 */
-        ",
-                ScriptParsing.GetCurrentFunctionCall(state, 381));
-            Assert.AreEqual("InnerCall(  3, 4, ", ScriptParsing.GetCurrentFunctionCall(state, 451));
-            Assert.AreEqual("Outer_Call     (1, 2, InnerCall(  3, 4, ", ScriptParsing.GetCurrentFunctionCall(state, 451, true));
-            Assert.AreEqual("Object_ . Call (Constant, ", ScriptParsing.GetCurrentFunctionCall(state, 496));
-            Assert.AreEqual(@"Object_
+        "),
+                ScriptParsing.GetCurrentFunctionCall(state, 358));
+            Assert.AreEqual("InnerCall(  3, 4, ", ScriptParsing.GetCurrentFunctionCall(state, 426));
+            Assert.AreEqual("Outer_Call     (1, 2, InnerCall(  3, 4, ", ScriptParsing.GetCurrentFunctionCall(state, 426, true));
+            Assert.AreEqual("Object_ . Call (Constant, ", ScriptParsing.GetCurrentFunctionCall(state, 470));
+            Assert.AreEqual(UniformTestString(@"Object_
         .
         Call
         (Constant,
-        ", ScriptParsing.GetCurrentFunctionCall(state, 577));
+        "), ScriptParsing.GetCurrentFunctionCall(state, 546));
         }
     }
 }

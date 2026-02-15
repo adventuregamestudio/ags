c9e8d2b886e54a4332e8f27b3b23101746f6b7d3
Ivan Mogilko <ikm_spb@yahoo.com>
2026-02-16 01:19:53 +0300

    Editor: add PropertyGrid ctx menu with Copy Property Name and Value

--- Editor/AGS.Editor/GUI/PropertiesPanel.Designer.cs
+++ Editor/AGS.Editor/GUI/PropertiesPanel.Designer.cs
@@ -28,18 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PropertiesPanel));
             this.propertiesGrid = new System.Windows.Forms.PropertyGrid();
             this.propertyObjectCombo = new System.Windows.Forms.ComboBox();
+            this.ctxmenuPropertyGrid = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyPropertyNameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyPropertyValueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ctxmenuPropertyGrid.SuspendLayout();
             this.SuspendLayout();
             // 
             // propertiesGrid
             // 
+            this.propertiesGrid.ContextMenuStrip = this.ctxmenuPropertyGrid;
             this.propertiesGrid.Dock = System.Windows.Forms.DockStyle.Fill;
             this.propertiesGrid.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.propertiesGrid.Location = new System.Drawing.Point(1, 44);
+            this.propertiesGrid.Location = new System.Drawing.Point(0, 21);
             this.propertiesGrid.Name = "propertiesGrid";
-            this.propertiesGrid.Size = new System.Drawing.Size(290, 215);
+            this.propertiesGrid.Size = new System.Drawing.Size(292, 239);
             this.propertiesGrid.TabIndex = 12;
             // 
             // propertyObjectCombo
@@ -47,14 +53,36 @@
             this.propertyObjectCombo.Dock = System.Windows.Forms.DockStyle.Top;
             this.propertyObjectCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.propertyObjectCombo.FormattingEnabled = true;
-            this.propertyObjectCombo.Location = new System.Drawing.Point(1, 20);
+            this.propertyObjectCombo.Location = new System.Drawing.Point(0, 0);
             this.propertyObjectCombo.Name = "propertyObjectCombo";
-            this.propertyObjectCombo.Size = new System.Drawing.Size(290, 24);
+            this.propertyObjectCombo.Size = new System.Drawing.Size(292, 21);
             this.propertyObjectCombo.TabIndex = 11;
+            // 
+            // ctxmenuPropertyGrid
+            // 
+            this.ctxmenuPropertyGrid.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyPropertyNameToolStripMenuItem,
+            this.copyPropertyValueToolStripMenuItem});
+            this.ctxmenuPropertyGrid.Name = "ctxmenuPropertyGrid";
+            this.ctxmenuPropertyGrid.Size = new System.Drawing.Size(175, 48);
+            // 
+            // copyPropertyNameToolStripMenuItem
+            // 
+            this.copyPropertyNameToolStripMenuItem.Name = "copyPropertyNameToolStripMenuItem";
+            this.copyPropertyNameToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.copyPropertyNameToolStripMenuItem.Text = "Copy Property Name";
+            this.copyPropertyNameToolStripMenuItem.Click += new System.EventHandler(this.copyPropertyNameToolStripMenuItem_Click);
+            // 
+            // copyPropertyValueToolStripMenuItem
+            // 
+            this.copyPropertyValueToolStripMenuItem.Name = "copyPropertyValueToolStripMenuItem";
+            this.copyPropertyValueToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.copyPropertyValueToolStripMenuItem.Text = "Copy Property Value";
+            this.copyPropertyValueToolStripMenuItem.Click += new System.EventHandler(this.copyPropertyValueToolStripMenuItem_Click);
             // 
             // PropertiesPanel
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 260);
             this.Controls.Add(this.propertiesGrid);
@@ -64,6 +92,7 @@
             this.Name = "PropertiesPanel";
             this.Load += new System.EventHandler(this.PropertiesPanel_Load);
             this.VisibleChanged += new System.EventHandler(this.PropertiesPanel_VisibleChanged);
+            this.ctxmenuPropertyGrid.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -72,5 +101,8 @@
 
         private System.Windows.Forms.PropertyGrid propertiesGrid;
         internal System.Windows.Forms.ComboBox propertyObjectCombo;
+        private System.Windows.Forms.ContextMenuStrip ctxmenuPropertyGrid;
+        private System.Windows.Forms.ToolStripMenuItem copyPropertyNameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyPropertyValueToolStripMenuItem;
     }
 }

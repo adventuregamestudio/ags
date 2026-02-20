adventuregamestudio/ags

commit 0fa69f53aaff0d6726966f3eebdc64b2dea0df5c

Engine: removed hard-limit of script Timers

Engine/ac/gamestate.cpp
@@ -473,6 +473,51 @@ bool GamePlayState::ShouldPlayVoiceSpeechNonBlocking() const
     return !play.fast_forward && (play.voice_avail);
 }
 
+void GamePlayState::StartScriptTimer(int timer_id, int timeout)
+{
+    assert(timer_id >= 0);
+    if (timer_id < 0 || timeout <= 0)
+        return;
+
+    _scriptTimers[timer_id] = timeout;
+}
+
+int GamePlayState::GetScriptTimerPos(int timer_id)
+{
+    auto it = _scriptTimers.find(timer_id);
+    if (it != _scriptTimers.end())
+        return it->second;
+    return 0;
+}
+
+bool GamePlayState::CheckScriptTimer(int timer_id)
+{
+    // The timer logic is that it counts down to 1, where stays
+    // *until* user checks for its expiration in script.
+    // Only then the timer is removed.
+    auto it = _scriptTimers.find(timer_id);
+    if (it != _scriptTimers.end())
+    {
+        if (it->second == 1)
+        {
+            _scriptTimers.erase(it);
+            return true;
+        }
+    }
+    return false;
+}
+
+void GamePlayState::UpdateScriptTimers()
+{
+    // The timer logic is that it counts down to 1, where stays
+    // *until* user checks for its expiration in script.
+    for (auto &timer : _scriptTimers)
+    {
+        if (timer.second > 1)
+            timer.second--;
+    }
+}
+
 void GamePlayState::ReadFromSavegame(Stream *in, GameDataVersion data_ver, GameStateSvgVersion svg_ver, RestoredData &r_data)
 {
     score = in->ReadInt32();
@@ -592,7 +637,17 @@ void GamePlayState::ReadFromSavegame(Stream *in, GameDataVersion data_ver, GameS
     entered_edge = in->ReadInt32();
     speech_mode = (SpeechMode)in->ReadInt32();
     speech_skip_style = in->ReadInt32();
-    in->ReadArrayOfInt32(script_timers, MAX_TIMERS);
+    // Old-style timers, limited to 20, with 1-based ID
+    if (svg_ver < kGSSvgVersion_363_04)
+    {
+        int old_script_timers[LEGACY_MAX_TIMERS];
+        in->ReadArrayOfInt32(old_script_timers, LEGACY_MAX_TIMERS);
+        for (int timer_id = 1; timer_id < LEGACY_MAX_TIMERS; ++timer_id)
+        {
+            if (old_script_timers[timer_id] > 0)
+                _scriptTimers[timer_id] = old_script_timers[timer_id];
+        }
+    }
     sound_volume = in->ReadInt32();
     speech_volume = in->ReadInt32();
     normal_font = in->ReadInt32();
@@ -696,6 +751,20 @@ void GamePlayState::ReadFromSavegame(Stream *in, GameDataVersion data_ver, GameS
     else
     {
         dialog_options_zorder = INT32_MAX;
+    }
+
+    // New-style timers
+    if (svg_ver >= kGSSvgVersion_363_04)
+    {
+        uint32_t timer_count = in->ReadInt32();
+        _scriptTimers.clear();
+        for (uint32_t i = 0; i < timer_count; ++i)
+        {
+            int timer_id = in->ReadInt32();
+            int timer_time = in->ReadInt32();
+            if (timer_id >= 0 && timer_time > 0)
+                _scriptTimers[timer_id] = timer_time;
+        }
     }
 }
 
@@ -809,7 +878,6 @@ void GamePlayState::WriteForSavegame(Stream *out) const
     out->WriteInt32( entered_edge);
     out->WriteInt32( speech_mode);
     out->WriteInt32( speech_skip_style);
-    out->WriteArrayOfInt32(script_timers, MAX_TIMERS);
     out->WriteInt32( sound_volume);
     out->WriteInt32( speech_volume);
     out->WriteInt32( normal_font);
@@ -892,6 +960,14 @@ void GamePlayState::WriteForSavegame(Stream *out) const
     out->WriteInt32(0); // reserved up to 4 ints
     out->WriteInt32(0);
     out->WriteInt32(0);
+
+    // kGSSvgVersion_363_04
+    out->WriteInt32(_scriptTimers.size());
+    for (const auto &timer : _scriptTimers)
+    {
+        out->WriteInt32(timer.first);
+        out->WriteInt32(timer.second);
+    }
 }
 
 void GamePlayState::FreeProperties()

Engine/ac/gamestate.h
@@ -16,6 +16,7 @@
 
 #include <memory>
 #include <vector>
+#include <unordered_map>
 #include <unordered_set>
 #include "ac/characterinfo.h"
 #include "ac/characterextras.h"
@@ -63,7 +64,8 @@ enum GameStateSvgVersion
     kGSSvgVersion_350_10    = 3,
     kGSSvgVersion_361_14    = 4,
     kGSSvgVersion_363       = 3060300,
-    kGSSvgVersion_363_02    = 3060302
+    kGSSvgVersion_363_02    = 3060302,
+    kGSSvgVersion_363_04    = 3060304,
 };
 
 // SavedLocationType defines the type of location which
@@ -119,7 +121,7 @@ struct GamePlayState
     int  score = 0;             // player's current score
     int  usedmode = 0;          // set by ProcessClick to last cursor mode used
     int  disabled_user_interface = 0; // >0 while in cutscene/etc
-    int  gscript_timer = 0;     // obsolete
+    int  gscript_timer = 0;     // deprecated "graphical script" timer (use unknown)
     int  debug_mode = 0;        // whether we're in debug mode
     int  globalvars[MAXGLOBALVARS]{}; // obsolete
     int  messagetime = 0;       // time left for auto-remove messages
@@ -237,7 +239,6 @@ struct GamePlayState
     bool  voice_avail = false; // whether voice-over is available
     SpeechMode speech_mode = kSpeech_TextOnly; // speech mode (text, voice, or both)
     int   speech_skip_style = 0; // stores SKIP_* flags
-    int   script_timers[MAX_TIMERS]{};
     int   sound_volume = 0;
     int   speech_volume = 0;
     int   normal_font = 0;
@@ -474,6 +475,14 @@ struct GamePlayState
     bool ShouldPlayVoiceSpeechNonBlocking() const;
 
     //
+    // Script Timers
+    //
+    void StartScriptTimer(int timer_id, int timeout);
+    int  GetScriptTimerPos(int timer_id);
+    bool CheckScriptTimer(int timer_id);
+    void UpdateScriptTimers();
+
+    //
     // Serialization
     //
     void ReadFromSavegame(Common::Stream *in, GameDataVersion data_ver, GameStateSvgVersion svg_ver, AGS::Engine::RestoredData &r_data);
@@ -512,7 +521,11 @@ private:
     // Tells that room viewports need z-order resort
     bool  _roomViewportZOrderChanged = false;
 
+    // A time point until which the user input will be ignored
     AGS::Engine::Clock::time_point _ignoreUserInputUntilTime{};
+
+    // Script Timers: simple game tick counters with arbitrary ID
+    std::unordered_map<int32_t, int32_t> _scriptTimers;
 };
 
 // Converts legacy alignment type used in script API

Engine/ac/global_timer.cpp
@@ -15,29 +15,43 @@
 #include "ac/runtime_defines.h"
 #include "ac/common.h"
 #include "ac/gamestate.h"
+#include "debug/debug_log.h"
 
-
-void SetTimer(int tnum,int timeout)
+bool AssertTimerID(const char *api_name, int tnum)
 {
-    if ((tnum < 1) || (tnum >= MAX_TIMERS))
-        quit("!StartTimer: invalid timer number");
-    play.script_timers[tnum] = timeout;
+    if (tnum < 0)
+    {
+        debug_script_warn("%s: invalid timer id %d, must be a number >= 0", api_name, tnum);
+        return false;
+    }
+    return true;
+}
+
+void SetTimer(int tnum, int timeout)
+{
+    if (!AssertTimerID("SetTimer", tnum))
+        return;
+    if (timeout <= 0)
+    {
+        debug_script_warn("SetTimer: invalid timeout %d, must be a positive number", timeout);
+        return;
+    }
+
+    play.StartScriptTimer(tnum, timeout);
 }
 
 int GetTimerPos(int tnum)
 {
-    if ((tnum < 1) || (tnum >= MAX_TIMERS))
-        quit("!IsTimerExpired: invalid timer number");
-    return play.script_timers[tnum];
+    if (!AssertTimerID("GetTimerPos", tnum))
+        return -1;
+
+    return play.GetScriptTimerPos(tnum);
 }
 
 int IsTimerExpired(int tnum)
 {
-    if ((tnum < 1) || (tnum >= MAX_TIMERS))
-        quit("!IsTimerExpired: invalid timer number");
-    if (play.script_timers[tnum] == 1) {
-        play.script_timers[tnum] = 0;
-        return 1;
-    }
-    return 0;
+    if (!AssertTimerID("IsTimerExpired", tnum))
+        return 0;
+
+    return play.CheckScriptTimer(tnum);
 }

Engine/ac/runtime_defines.h
@@ -24,7 +24,7 @@
 #define MAXGSVALUES 500
 #define MAXGLOBALSTRINGS 51
 #define MAX_INVORDER 500
-#define MAX_TIMERS       21
+#define LEGACY_MAX_TIMERS 21
 #define MAX_PARSED_WORDS 15
 
 #define MAX_QUEUED_MUSIC 10

Engine/game/savegame_components.cpp
@@ -1776,7 +1776,7 @@ ComponentHandler ComponentHandlers[] =
     },
     {
         "Game State",
-        kGSSvgVersion_363_02,
+        kGSSvgVersion_363_04,
         kGSSvgVersion_Initial,
         kSaveCmp_GameState,
         WriteGameState,

Engine/main/engine.cpp
@@ -846,7 +846,6 @@ void engine_init_game_settings()
     GUIE::MarkForFontUpdate(-1);
 
     memset(&play.walkable_areas_on[0],1,MAX_WALK_AREAS);
-    memset(&play.script_timers[0],0,MAX_TIMERS * sizeof(int));
     play.default_audio_type_volumes.resize(game.audioClipTypes.size(), -1);
 
     if (!usetup.Translation.IsEmpty())

Engine/main/update.cpp
@@ -248,10 +248,11 @@ void restore_movelists()
 
 void update_script_timers()
 {
-  if (play.gscript_timer > 0) play.gscript_timer--;
-  for (int aa=0;aa<MAX_TIMERS;aa++) {
-    if (play.script_timers[aa] > 1) play.script_timers[aa]--;
-    }
+    // Deprecated "graphical script" timer (use unknown)
+    if (play.gscript_timer > 0)
+        play.gscript_timer--;
+
+    play.UpdateScriptTimers();
 }
 
 void update_cycling_views()

commit 1a6958370e027fba4213a8aee7db09d0f62f2e6f

Engine: fixed SetTimer not allowing to stop the active timer
Was broken by 0fa69f53aaff0d6726966f3eebdc64b2dea0df5c

Engine/ac/gamestate.cpp
@@ -475,11 +475,19 @@ bool GamePlayState::ShouldPlayVoiceSpeechNonBlocking() const
 
 void GamePlayState::StartScriptTimer(int timer_id, int timeout)
 {
-    assert(timer_id >= 0);
-    if (timer_id < 0 || timeout <= 0)
-        return;
+    assert(timer_id >= 0 && timeout > 0);
+    if (timer_id >= 0 && timeout > 0)
+    {
+        _scriptTimers[timer_id] = timeout;
+    }
+}
 
-    _scriptTimers[timer_id] = timeout;
+void GamePlayState::StopScriptTimer(int timer_id)
+{
+    assert(timer_id >= 0);
+    auto it = _scriptTimers.find(timer_id);
+    if (it != _scriptTimers.end())
+        _scriptTimers.erase(it);
 }
 
 int GamePlayState::GetScriptTimerPos(int timer_id)

Engine/ac/gamestate.h
@@ -478,6 +478,7 @@ struct GamePlayState
     // Script Timers
     //
     void StartScriptTimer(int timer_id, int timeout);
+    void StopScriptTimer(int timer_id);
     int  GetScriptTimerPos(int timer_id);
     bool CheckScriptTimer(int timer_id);
     void UpdateScriptTimers();

Engine/ac/global_timer.cpp
@@ -31,13 +31,19 @@ void SetTimer(int tnum, int timeout)
 {
     if (!AssertTimerID("SetTimer", tnum))
         return;
-    if (timeout <= 0)
-    {
-        debug_script_warn("SetTimer: invalid timeout %d, must be a positive number", timeout);
-        return;
-    }
 
-    play.StartScriptTimer(tnum, timeout);
+    if (timeout > 0)
+    {
+        play.StartScriptTimer(tnum, timeout);
+    }
+    else if (timeout == 0)
+    {
+        play.StopScriptTimer(tnum);
+    }
+    else
+    {
+        debug_script_warn("SetTimer: invalid timeout %d for timer id %d, timeout must be a positive number to start the timer, or 0 to stop the timer", timeout, tnum);
+    }
 }
 
 int GetTimerPos(int tnum)
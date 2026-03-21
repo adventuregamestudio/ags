adventuregamestudio/ags

commit 720114e8a873dc69bf54de4148d2da77ae6561c6

Script API: implemented File.ResolvePath()
File.ResolvePath resolves a script path into the filesystem path, in exact same way as the engine does it when searching for a file.

Common/util/file.cpp
@@ -252,23 +252,12 @@ Stream *File::OpenStderr()
 String File::FindFileCI(const String &base_dir, const String &file_name,
     bool is_dir, String *most_found, String *not_found)
 {
-#if !defined (AGS_CASE_SENSITIVE_FILESYSTEM)
-    // Simply try a match
-    String path = Path::ConcatPaths(base_dir, file_name);
-    if ((is_dir && ags_directory_exists(path.GetCStr())) ||
-        (!is_dir && ags_file_exists(path.GetCStr())))
-        return path; // success
-    // TODO: this is never used, but for a formal consistency might
-    // support searching for an existing part of the path, and fill
-    // most_found & not_found
-    return {}; // fail
-#else
+    // Case insensitive file find - on case sensitive filesystems
     if (most_found)
         *most_found = base_dir;
     if (not_found)
         *not_found = "";
 
-    // Case insensitive file find - on case sensitive filesystems
     if (file_name.IsEmpty())
         return {}; // fail, no filename provided
 
@@ -299,6 +288,13 @@ String File::FindFileCI(const String &base_dir, const String &file_name,
         (!is_dir && ags_file_exists(test.GetCStr())))
         return test; // success
 
+#if !defined (AGS_CASE_SENSITIVE_FILESYSTEM)
+    // TODO: most_found & not_found are never used in practice with CI fs;
+    // but if becomes necessary, we may reuse most of the CS code below
+    // for this case too, and uncommenting the following condition -
+    /* if (!most_found && !not_found) */
+    return {}; // fail
+#else
     // Begin splitting filename into path sections,
     // for each section open previous dir and search for any case-insensitive
     // match for the next section.

Engine/ac/file.cpp
@@ -76,6 +76,14 @@ void *sc_OpenFile(const char *fnmm, int mode) {
   return scf;
 }
 
+const char *File_ResolvePath(const char *fnmm)
+{
+    ResolvedPath rp = ResolveScriptPathAndFindFile(fnmm, true, true);
+    // Make path pretty -
+    String path = Path::MakeAbsolutePath(rp.FullPath);
+    return CreateNewScriptString(path.GetCStr());
+}
+
 void File_Close(sc_File *fil) {
   fil->Close();
 }
@@ -446,7 +454,7 @@ bool ResolveScriptPath(const String &orig_sc_path, bool read_only, ResolvedPath 
     return true;
 }
 
-ResolvedPath ResolveScriptPathAndFindFile(const String &sc_path, bool read_only)
+ResolvedPath ResolveScriptPathAndFindFile(const String &sc_path, bool read_only, bool ignore_find_result)
 {
     ResolvedPath rp, alt_rp;
     if (!ResolveScriptPath(sc_path, read_only, rp, alt_rp))
@@ -459,14 +467,24 @@ ResolvedPath ResolveScriptPathAndFindFile(const String &sc_path, bool read_only)
         return rp; // we don't test AssetMgr here
 
     ResolvedPath final_rp = rp;
-    String found_file = File::FindFileCI(rp.Loc.BaseDir, rp.SubPath);
+    String most_found, missing_path;
+    String found_file = File::FindFileCI(rp.Loc.BaseDir, rp.SubPath, false, &most_found, &missing_path);
     if (found_file.IsEmpty() && alt_rp)
     {
         final_rp = alt_rp;
-        found_file = File::FindFileCI(alt_rp.Loc.BaseDir, alt_rp.SubPath);
+        found_file = File::FindFileCI(alt_rp.Loc.BaseDir, alt_rp.SubPath, false, &most_found, &missing_path);
     }
     if (found_file.IsEmpty())
     {
+        if (ignore_find_result)
+        {
+#if !defined (AGS_CASE_SENSITIVE_FILESYSTEM)
+            return final_rp; // if we want a case-precise result, need to adjust FindFileCI, see comment inside
+#else
+            return ResolvedPath(most_found, missing_path);
+#endif
+        }
+
         debug_script_warn("ResolveScriptPath: failed to find a match for: %s\n\ttried: %s\n\talt try: %s",
             sc_path.GetCStr(), rp.FullPath.GetCStr(), alt_rp.FullPath.GetCStr());
         return {}; // nothing matching found
@@ -720,6 +738,11 @@ RuntimeScriptValue Sc_sc_OpenFile(const RuntimeScriptValue *params, int32_t para
     API_SCALL_OBJAUTO_POBJ_PINT(sc_File, sc_OpenFile, const char);
 }
 
+RuntimeScriptValue Sc_File_ResolvePath(const RuntimeScriptValue *params, int32_t param_count)
+{
+    API_SCALL_OBJ_POBJ(const char, myScriptStringImpl, File_ResolvePath, const char);
+}
+
 // void (sc_File *fil)
 RuntimeScriptValue Sc_File_Close(void *self, const RuntimeScriptValue *params, int32_t param_count)
 {
@@ -826,6 +849,7 @@ void RegisterFileAPI()
         { "File::Delete^1",           API_FN_PAIR(File_Delete) },
         { "File::Exists^1",           API_FN_PAIR(File_Exists) },
         { "File::Open^2",             API_FN_PAIR(sc_OpenFile) },
+        { "File::ResolvePath^1",      API_FN_PAIR(File_ResolvePath) },
 
         { "File::Close^0",            API_FN_PAIR(File_Close) },
         { "File::ReadInt^0",          API_FN_PAIR(File_ReadInt) },

Editor/AGS.Editor/Resources/agsdefns.sh
@@ -1260,6 +1260,10 @@ builtin managed struct File {
   /// Writes a raw 32-bit int to the file.
   import void WriteRawInt(int value);
 #endif
+#ifdef SCRIPT_API_v361
+  /// Resolves the script path into the system filepath; for diagnostic purposes only.
+  import static String ResolvePath(const string filename);   // $AUTOCOMPLETESTATICONLY$
+#endif
   int reserved[2];   // $AUTOCOMPLETEIGNORE$
 };
 

Engine/ac/path_helper.h
@@ -109,7 +109,7 @@ bool ResolveScriptPath(const String &sc_path, bool read_only, ResolvedPath &rp, 
 // if necessary. Fills in the successful ResolvedPath variant.
 // Fails if path could not be resolved, or no matching file was found.
 // WARNING: AssetMgr path is not tested.
-ResolvedPath ResolveScriptPathAndFindFile(const String &sc_path, bool read_only);
+ResolvedPath ResolveScriptPathAndFindFile(const String &sc_path, bool read_only, bool ignore_find_result = false);
 // Resolves a user file path for writing, and makes sure all the sub-directories are
 // created along the actual path.
 // Returns 'true' on success, and 'false' if either path is impossible to resolve,

commit a2aa97b795fd6fa428dd8a70baff36ae9938eb6a

Common: fix File::FindFileCI() for the directory match case
This was broken by 720114e8a873dc69bf54de4148d2da77ae6561c6
As a result, script commands such as ListBox.FillDirList("*.tra") will always fail.
I have no idea how no one noticed this until now.

Common/util/file.cpp
@@ -287,7 +287,10 @@ String File::FindFileCI(const String &base_dir, const String &file_name,
     if (not_found)
         *not_found = "";
 
-    if (file_name.IsEmpty())
+    // If file_name is empty, and we are looking for the file, then fail right away.
+    // If we are looking for the dir, then further code always succeeds at the
+    // very first match test, so long as base_dir exists.
+    if (!is_dir && file_name.IsEmpty())
         return {}; // fail, no filename provided
 
     String directory;

Common/util/path.cpp
@@ -99,6 +99,10 @@ String GetDirectoryPath(const String &path)
 
 bool IsRelativePath(const String &path)
 {
+    // Consider empty path relative
+    if (path.IsEmpty())
+        return true;
+
     // All filenames that start with a '.' are relative.
     if (path[0] == '.' )
         return true;
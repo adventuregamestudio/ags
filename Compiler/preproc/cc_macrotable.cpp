738620f062dae9dad1e26914cd181cf6015d7f82
Érico Porto <ericoporto2008@gmail.com>
2026-05-26 23:04:15 -0300

    Compiler: more clang-tidy fixes
    plus formating

--- Compiler/preproc/cc_macrotable.cpp
+++ Compiler/preproc/cc_macrotable.cpp
@@ -18,34 +18,47 @@
 using namespace AGS::Common;
 
 
-void MacroTable::merge(MacroTable &others) {
+void MacroTable::merge(MacroTable &others)
+{
     _macro_table.insert(others._macro_table.begin(), others._macro_table.end());
 }
-bool MacroTable::contains(const String &name) {
+
+bool MacroTable::contains(const String &name) const
+{
     return _macro_table.count(name) > 0;
 }
-String MacroTable::get_macro(const String &name) {
-    if(_macro_table.count(name)) {
+
+String MacroTable::get_macro(const String &name)
+{
+    if(_macro_table.count(name))
+    {
         return _macro_table[name];
     }
     return nullptr;
 }
-void MacroTable::add(const String &macroname, const String &value) {
-    if (this->contains(macroname)) {
+
+void MacroTable::add(const String &macroname, const String &value)
+{
+    if (this->contains(macroname))
+    {
         cc_error("macro '%s' already defined",macroname.GetCStr());
         return;
     }
 
     _macro_table[macroname] = value;
 }
-void MacroTable::remove(String &macroname) {
-    if (!this->contains(macroname)) {
+
+void MacroTable::remove(const String &macroname)
+{
+    if (!this->contains(macroname))
+    {
         cc_error("MacroTable::Remove: macro '%s' not found", macroname.GetCStr());
         return;
     }
     _macro_table.erase(macroname);
 }
 
-void MacroTable::clear() {
+void MacroTable::clear()
+{
     _macro_table.clear();
 }

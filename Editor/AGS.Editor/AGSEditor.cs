fb264af7edcaa2d13fad1a25eea6c09ece6c36f1
Ivan Mogilko <ikm_spb@yahoo.com>
2026-06-05 13:24:38 +0300

    Editor: up project version, sync with 3.6.3.10

--- Editor/AGS.Editor/AGSEditor.cs
+++ Editor/AGS.Editor/AGSEditor.cs
@@ -146,8 +146,9 @@ namespace AGS.Editor
          * 4.00.00.26     - sync with colored gui controls (3.6.3),
          *                  and other properties (BlockingRectangle etc)
          * 4.00.00.27     - Character.GraphicAnchor/Offset.
+         * 4.00.00.29     - Sync with 3.6.3.10.
         */
-        public const string LATEST_XML_VERSION = "4.0.0.27";
+        public const string LATEST_XML_VERSION = "4.0.0.29";
 
         /*
          * FIRST_XML_VERSION is the very first version of the Editor using XML project file.

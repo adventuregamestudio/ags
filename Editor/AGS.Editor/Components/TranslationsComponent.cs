46aa96c0bc7912e0d236532983f33297bb173dd7
Ivan Mogilko <ikm_spb@yahoo.com>
2026-06-26 19:36:50 +0300

    Editor: open Translations in external editor when double clicked

--- Editor/AGS.Editor/Components/TranslationsComponent.cs
+++ Editor/AGS.Editor/Components/TranslationsComponent.cs
@@ -643,11 +643,6 @@ namespace AGS.Editor.Components
                 if (_guiController.ShowQuestion("Are you sure you want to delete this translation?") == DialogResult.Yes)
                 {
                     string removing = _itemRightClicked.FileName;
-                    /*                    if (_documents.ContainsKey(_itemRightClicked))
-                                        {
-                                            _guiController.RemovePaneIfExists(_documents[_itemRightClicked]);
-                                            _documents.Remove(_itemRightClicked);
-                                        }*/
                     _agsEditor.CurrentGame.Translations.Remove(_itemRightClicked);
 					_agsEditor.CurrentGame.FilesAddedOrRemoved = true;
 
@@ -664,16 +659,7 @@ namespace AGS.Editor.Components
                 if (controlID != TOP_LEVEL_COMMAND_ID)
                 {
                     Translation translation = _agsEditor.CurrentGame.Translations[Convert.ToInt32(controlID.Substring(3))];
-                    _guiController.ShowMessage("Currently you cannot edit translations within the editor. Load the file " + translation.FileName + " into your favourite text editor to do your translation work.", MessageBoxIcon.Information);
-                    /*                    if (!_documents.ContainsKey(chosenFont))
-                                        {
-                                            Dictionary<string, object> list = new Dictionary<string, object>();
-                                            list.Add(chosenFont.Name + " (Font " + chosenFont.ID + ")", chosenFont);
-
-                                            _documents.Add(chosenFont, new ContentDocument(new FontEditor(chosenFont), chosenFont.WindowTitle, this, list));
-                                            _documents[chosenFont].SelectedPropertyGridObject = chosenFont;
-                                        }
-                                        _guiController.AddOrShowPane(_documents[chosenFont]);*/
+                    Utilities.OpenFileInExternalProgram(Path.Combine(_agsEditor.CurrentGame.DirectoryPath, translation.FileName));
                 }
             }
         }
@@ -683,11 +669,6 @@ namespace AGS.Editor.Components
             if (propertyName == "Name")
             {
                 RePopulateTreeView();
-                /*
-                foreach (ContentDocument doc in _documents.Values)
-                {
-                    doc.Name = ((FontEditor)doc.Control).ItemToEdit.WindowTitle;
-                }*/
             }
         }
 
@@ -726,14 +707,7 @@ namespace AGS.Editor.Components
         }
 
         public override void RefreshDataFromGame()
-        {/*
-            foreach (ContentDocument doc in _documents.Values)
-            {
-                _guiController.RemovePaneIfExists(doc);
-                doc.Dispose();
-            }
-            _documents.Clear();
-            */
+        {
             RePopulateTreeView();
             TranslationListTypeConverter.SetTranslationsList(_agsEditor.CurrentGame.Translations);
         }
@@ -804,13 +778,7 @@ namespace AGS.Editor.Components
                 AddTreeLeafForTranslation("Trl" + index, item);
                 index++;
             }
-            /*
-            if (_documents.ContainsValue(_guiController.ActivePane))
-            {
-                FontEditor editor = (FontEditor)_guiController.ActivePane.Control;
-                _guiController.ProjectTree.SelectNode(this, "Trl" + editor.ItemToEdit.ID);
-            }
-            else*/ if (_agsEditor.CurrentGame.Translations.Count > 0)
+            if (_agsEditor.CurrentGame.Translations.Count > 0)
             {
                 _guiController.ProjectTree.SelectNode(this, "Trl0");
             }

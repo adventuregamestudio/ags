adventuregamestudio/ags

commit 0c6f8b0b1c5da06757fd6db78e27b60c88a29b51

Engine: carry "unused" part of the move to the next move segment
When the move segments are too short, the character may "stumble" on each segment's end, slowing its walking down. This becomes a problem when the pathfinder have returned a path consisting of multiple small segments with approximately same general direction.
The change in this commit makes it so that the movelist carries over "unused" progress from the previous segment to the next one.

Such behavior is only executed when the character is NOT turning before entering next move segment.

Engine/ac/character.cpp
@@ -1993,7 +1993,8 @@ void start_character_turning (CharacterInfo *chinf, int useloop, bool use_diaglo
 
 }
 
-void fix_player_sprite(MoveList*cmls,CharacterInfo*chinf) {
+void fix_player_sprite(const MoveList *cmls, CharacterInfo *chinf)
+{
     const fixed xpmove = cmls->permove[cmls->onstage].X;
     const fixed ypmove = cmls->permove[cmls->onstage].Y;
 
@@ -2051,17 +2052,27 @@ int has_hit_another_character(int sourceChar) {
 // Does the next move from the character's movelist.
 // Returns 1 if they are now waiting for another char to move,
 // otherwise returns 0
-int doNextCharMoveStep(CharacterInfo *chi, CharacterExtras *chex) {
-    int ntf=0, xwas = chi->x, ywas = chi->y;
-
-    if (do_movelist_move(chi->walking, chi->x, chi->y) == kMoveResult_NextStage) 
+int doNextCharMoveStep(CharacterInfo *chi, CharacterExtras *chex)
+{
+    int xwas = chi->x, ywas = chi->y;
+    // If we support smooth walk, then keep moving even across multiple stages,
+    // until all the "current move" is not depleted OR until we have to turn.
+    while (do_movelist_move(chi->walking, chi->x, chi->y, play.ShouldSmoothWalk()) == kMoveResult_NextStage)
     {
+        // Character just switched to the next path segment
+        auto &mlist = mls[chi->get_movelist_id()];
         if (chi->is_moving_walkanim())
-            fix_player_sprite(&mls[chi->get_movelist_id()], chi);
+            fix_player_sprite(&mlist, chi);
+        if (chi->is_turning() || !play.ShouldSmoothWalk() || mlist.onpart <= 0.f)
+        {
+            mlist.onpart = 0.f;
+            break;
+        }
     }
 
-    ntf = has_hit_another_character(chi->index_id);
-    if (ntf >= 0) {
+    int ntf = has_hit_another_character(chi->index_id);
+    if (ntf >= 0)
+    {
         chi->walkwait = 30;
         if (game.chars[ntf].walkspeed < 5)
             chi->walkwait += (5 - game.chars[ntf].walkspeed) * 5;
@@ -2076,8 +2087,9 @@ int doNextCharMoveStep(CharacterInfo *chi, CharacterExtras *chex) {
             chex->animwait = chi->walkwait;
         }
 
-        if ((chi->walking < 1) || (chi->walking >= TURNING_AROUND)) ;
-        else if (mls[chi->get_movelist_id()].onpart > 0.f) {
+        if ((chi->walking < 1) || (chi->walking >= TURNING_AROUND)) { /* skip */ }
+        else if (mls[chi->get_movelist_id()].onpart > 0.f)
+        {
             mls[chi->get_movelist_id()].onpart -= 1.f;
             chi->x = xwas;
             chi->y = ywas;

Engine/main/update.cpp
@@ -152,9 +152,9 @@ static bool movelist_handle_donemove(const uint8_t testflag, const fixed xpermov
     return (doneflag & testflag) != 0;
 }
 
-MoveResult do_movelist_move(short &mslot, int &pos_x, int &pos_y)
+MoveResult do_movelist_move(short &mslot, int &pos_x, int &pos_y, bool smooth_move)
 {
-    // TODO: find out why movelist 0 is not being used
+    // NOTE: mslot 0 is not used, because id 0 is treated as "no move"
     assert(mslot >= 1);
     if (mslot < 1)
         return kMoveResult_Abort;
@@ -166,10 +166,9 @@ MoveResult do_movelist_move(short &mslot, int &pos_x, int &pos_y)
     const float main_onpart = (cmls.fin_from_part > 0.f) ? cmls.fin_from_part : cmls.onpart;
     const float fin_onpart = cmls.onpart - main_onpart;
     Point target = cmls.pos[cmls.onstage + 1];
-    int xps = pos_x, yps = pos_y;
 
     // Old-style optional move target fixup
-    if (loaded_game_file_version < kGameVersion_361)
+    if (!smooth_move)
     {
         if ((ypermove != 0) && (cmls.doneflag & kMoveListDone_X) != 0)
         { // X-move has finished, handle the Y-move remainer
@@ -181,20 +180,24 @@ MoveResult do_movelist_move(short &mslot, int &pos_x, int &pos_y)
         }
     }
 
-    // Calculate next positions, as required
+    // Calculate next positions, as required, save them in max_* positions;
+    // these positions are saved for the reference, but the actual pos may
+    // be further adjusted by clamping to the move target pos.
+    int max_xps = pos_x, max_yps = pos_y;
     if ((cmls.doneflag & kMoveListDone_X) == 0)
     {
-        xps = cmls.from.X + (int)(fixtof(xpermove) * main_onpart) +
+        max_xps = cmls.from.X + (int)(fixtof(xpermove) * main_onpart) +
           (int)(fixtof(fin_move) * fin_onpart);
     }
     if ((cmls.doneflag & kMoveListDone_Y) == 0)
     {
-        yps = cmls.from.Y + (int)(fixtof(ypermove) * main_onpart) +
+        max_yps = cmls.from.Y + (int)(fixtof(ypermove) * main_onpart) +
           (int)(fixtof(fin_move) * fin_onpart);
     }
 
     // Check if finished either horizontal or vertical movement;
     // if any was finished just now, then also handle remainer fixup
+    int xps = max_xps, yps = max_yps;
     bool done_now = movelist_handle_donemove(kMoveListDone_X, xpermove, target.X, cmls.doneflag, xps);
     done_now     |= movelist_handle_donemove(kMoveListDone_Y, ypermove, target.Y, cmls.doneflag, yps);
     if (done_now)
@@ -202,35 +205,47 @@ MoveResult do_movelist_move(short &mslot, int &pos_x, int &pos_y)
 
     // Handle end of move stage
     MoveResult move_result = kMoveResult_Continue;
-    if ((cmls.doneflag & kMoveListDone_XY) == kMoveListDone_XY)
+    if ((cmls.doneflag & kMoveListDone_XY) != kMoveListDone_XY)
     {
-        // this stage is done, go on to the next stage
-        cmls.from = cmls.pos[cmls.onstage + 1];
-        cmls.onstage++;
-        cmls.onpart = -1.f;
-        cmls.fin_from_part = 0.f;
-        cmls.fin_move = 0;
-        cmls.doneflag = 0;
-        if (cmls.onstage < cmls.GetNumStages())
+        // Make a step along the current vector and return
+        cmls.onpart += 1.f;
+    }
+    else
+    {
+        // End of stage (or end of path)
+        const uint32_t next_stage = cmls.onstage + 1;
+        if (next_stage < cmls.GetNumStages())
         {
-            xps = cmls.from.X;
-            yps = cmls.from.Y;
+            xps = cmls.pos[next_stage].X;
+            yps = cmls.pos[next_stage].Y;
         }
 
-        if (cmls.onstage >= cmls.GetNumStages() - 1)
-        {  // last stage is just dest pos
+        if (next_stage >= cmls.GetNumStages() - 1)
+        {
+            // Last stage is just dest pos, so finish the move
             cmls = {};
             mslot = 0;
             move_result = kMoveResult_Done;
         }
         else
         {
+            // Switch to the next stage
+            cmls.onstage = next_stage;
+            cmls.from = cmls.pos[cmls.onstage];
+            cmls.onpart = 0.f;
+            cmls.fin_from_part = 0.f;
+            cmls.fin_move = 0;
+            cmls.doneflag = 0;
+
+            // Smooth out next stage transition by carrying over remaining % of the previous move
+            const float frac_complete_x = static_cast<float>(xps - pos_x) / static_cast<float>(max_xps - pos_x);
+            const float frac_complete_y = static_cast<float>(yps - pos_y) / static_cast<float>(max_yps - pos_y);
+            const float lost_frac = std::max(1.f - frac_complete_x, 1.f - frac_complete_y);
+            cmls.onpart += lost_frac;
             move_result = kMoveResult_NextStage;
         }
     }
 
-    // Make a step along the current vector and return
-    cmls.onpart += 1.f;
     pos_x = xps;
     pos_y = yps;
     return move_result;

Engine/ac/character.h
@@ -178,7 +178,7 @@ bool should_use_diagloops(CharacterInfo *char1, bool require_animation);
 // returns 1 normally, or 0 if they only have horizontal animations
 int  hasUpDownLoops(CharacterInfo *char1);
 void start_character_turning(CharacterInfo *chinf, int useloop, bool use_diagloops);
-void fix_player_sprite(MoveList*cmls,CharacterInfo*chinf);
+void fix_player_sprite(const MoveList *cmls, CharacterInfo *chinf);
 // Check whether two characters have walked into each other
 int  has_hit_another_character(int sourceChar);
 int  doNextCharMoveStep(CharacterInfo *chi, CharacterExtras *chex);

Engine/ac/gamestate.h
@@ -423,6 +423,9 @@ struct GamePlayState
 
     // Tells if engine should apply AA (linear) scaling to the game sprites
     bool ShouldAASprites() const { return enable_antialiasing && (disable_antialiasing == 0); }
+    // Tells if the smooth walk mode is enabled for characters
+    // (this means - smooth transition between two separate walks, without stopping)
+    bool ShouldSmoothWalk() const { return loaded_game_file_version >= kGameVersion_361; }
 
     //
     // User input management

Engine/ac/roomobject.cpp
@@ -71,9 +71,14 @@ int RoomObject::get_baseline() const {
 void RoomObject::UpdateCyclingView(int ref_id)
 {
 	if (on != OBJ_STATE_ENABLED) return;
-    if (moving>0) {
-      do_movelist_move(moving, x, y);
-      }
+    if (moving > 0)
+    {
+        // If we support smooth walk, then keep moving even across multiple stages,
+        // until all the "current move" is not depleted
+        MoveResult last_move_result;
+        while (((last_move_result = do_movelist_move(moving, x, y, play.ShouldSmoothWalk())) == kMoveResult_NextStage)
+            && play.ShouldSmoothWalk() && mls[moving].onpart > 0.f);
+    }
     if (cycling==0) return;
     if (view == RoomObject::NoView) return;
     if (wait>0) { wait--; return; }

Engine/main/update.h
@@ -33,7 +33,7 @@ enum MoveResult
 // returns "need_to_fix_sprite" value, which may be 0,1,2;
 // TODO: find out what this return value means, and refactor.
 // TODO: do not reset mslot in this function, reset externally instead.
-MoveResult do_movelist_move(short &mslot, int &pos_x, int &pos_y);
+MoveResult do_movelist_move(short &mslot, int &pos_x, int &pos_y, bool smooth_move);
 // Recalculate derived (non-serialized) values in movelists
 void restore_movelists();
 // Update various things on the game frame (historical code mess...)

commit 9970d78240799508de3ca56c621babe8bcac413b

Engine: safety fixes vs div on zero in character movement code
Fixes a infinite walk bug introduced by 0c6f8b0b1c5da06757fd6db78e27b60c88a29b51

Engine/ac/character.cpp
@@ -2087,13 +2087,19 @@ int doNextCharMoveStep(CharacterInfo *chi, CharacterExtras *chex)
             chex->animwait = chi->walkwait;
         }
 
-        if ((chi->walking < 1) || (chi->walking >= TURNING_AROUND)) { /* skip */ }
-        else if (mls[chi->get_movelist_id()].onpart > 0.f)
+        if ((chi->walking < 1) || (chi->walking >= TURNING_AROUND))
+        { /* skip */ }
+        else
         {
-            mls[chi->get_movelist_id()].onpart -= 1.f;
-            chi->x = xwas;
-            chi->y = ywas;
+            auto &mlist = mls[chi->get_movelist_id()];
+            if (mlist.onpart > 0.f)
+            {
+                mlist.onpart = std::max(0.f, mlist.onpart - 1.f);
+                chi->x = xwas;
+                chi->y = ywas;
+            }
         }
+
         debug_script_log("%s: Bumped into %s, waiting for them to move",
             chi->scrname, game.chars[ntf].scrname);
         return 1;

Engine/ac/route_finder.cpp
@@ -263,8 +263,7 @@ void RecalculateMoveSpeeds(MoveList &mls, int old_speed_x, int old_speed_y, int 
     const fixed new_movspeed_x = InputSpeedToFixed(new_speed_x);
     const fixed new_movspeed_y = InputSpeedToFixed(new_speed_y);
     // save current stage's step lengths, for later onpart's update
-    const fixed old_stage_xpermove = mls.permove[mls.onstage].X;
-    const fixed old_stage_ypermove = mls.permove[mls.onstage].Y;
+    const Point old_stage_permove = mls.permove[mls.onstage];
 
     for (uint32_t i = 0; (i < mls.GetNumStages()) && ((mls.permove[i].X != 0.f) || (mls.permove[i].Y != 0.f)); ++i)
     {
@@ -298,10 +297,13 @@ void RecalculateMoveSpeeds(MoveList &mls, int old_speed_x, int old_speed_y, int 
     // now adjust current passed stage fraction
     if (mls.onpart >= 0.f)
     {
-        if (old_stage_xpermove != 0)
-            mls.onpart = (mls.onpart * fixtof(old_stage_xpermove)) / fixtof(mls.permove[mls.onstage].X);
+        const auto &new_stage_permove = mls.permove[mls.onstage];
+        if ((old_stage_permove.X != 0) && (new_stage_permove.X != 0))
+            mls.onpart = (mls.onpart * fixtof(old_stage_permove.X)) / fixtof(new_stage_permove.X);
+        else if ((old_stage_permove.Y != 0) && (new_stage_permove.Y != 0))
+            mls.onpart = (mls.onpart * fixtof(old_stage_permove.Y)) / fixtof(new_stage_permove.Y);
         else
-            mls.onpart = (mls.onpart * fixtof(old_stage_ypermove)) / fixtof(mls.permove[mls.onstage].Y);
+            assert(false); // should never happen
     }
 }
 

Engine/main/update.cpp
@@ -160,6 +160,10 @@ MoveResult do_movelist_move(short &mslot, int &pos_x, int &pos_y, bool smooth_mo
         return kMoveResult_Abort;
 
     MoveList &cmls = mls[mslot];
+    // Safety fix, in case onpart became invalid
+    if (std::isnan(cmls.onpart) || cmls.onpart < 0.f)
+        cmls.onpart = 0.f;
+
     const fixed xpermove = cmls.permove[cmls.onstage].X;
     const fixed ypermove = cmls.permove[cmls.onstage].Y;
     const fixed fin_move = cmls.fin_move;
@@ -238,8 +242,8 @@ MoveResult do_movelist_move(short &mslot, int &pos_x, int &pos_y, bool smooth_mo
             cmls.doneflag = 0;
 
             // Smooth out next stage transition by carrying over remaining % of the previous move
-            const float frac_complete_x = static_cast<float>(xps - pos_x) / static_cast<float>(max_xps - pos_x);
-            const float frac_complete_y = static_cast<float>(yps - pos_y) / static_cast<float>(max_yps - pos_y);
+            const float frac_complete_x = (max_xps != pos_x) ? static_cast<float>(xps - pos_x) / static_cast<float>(max_xps - pos_x) : 0.f;
+            const float frac_complete_y = (max_yps != pos_y) ? static_cast<float>(yps - pos_y) / static_cast<float>(max_yps - pos_y) : 0.f;
             const float lost_frac = std::max(1.f - frac_complete_x, 1.f - frac_complete_y);
             cmls.onpart += lost_frac;
             move_result = kMoveResult_NextStage;
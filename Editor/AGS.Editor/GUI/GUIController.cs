6859f0895d405fc4824c4df41f776c3943284c34
Ivan Mogilko <ikm_spb@yahoo.com>
2026-07-08 19:02:52 +0300

    Editor: fix exit code if /compile resulted in only warnings
    + output all compilation messages to the console if console mode is enabled.

--- Editor/AGS.Editor/GUI/GUIController.cs
+++ Editor/AGS.Editor/GUI/GUIController.cs
@@ -151,6 +151,8 @@ namespace AGS.Editor
 
         public Dictionary<string, string> InstalledFonts { get; private set; }
 
+        public bool IsConsoleMode { get { return StdConsoleWriter.IsEnabled; } }
+
         public void ShowMessage(string message, MessageBoxIconType icon)
 		{
 			MessageBoxIcon windowsFormsIcon = MessageBoxIcon.Information;
@@ -1089,7 +1091,7 @@ namespace AGS.Editor
                     _agsEditor.SaveUserDataFile(); // in case pending config is applied
 
                 _batchProcessShutdown = true;
-                if (messages.Count == 0)
+                if (!messages.HasErrors)
                 {
                     BuildCommandsComponent.ShowCompileSuccessMessage();
                 }
@@ -1098,7 +1100,8 @@ namespace AGS.Editor
                     error = true;
                 }
             }
-            if(error) Program.SetExitCode(1);
+            if (error)
+                Program.SetExitCode(1);
             this.ExitApplication();
         }
 

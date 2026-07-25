df4d70ba2dfaf8f3dcc7fd4061858bbb9fe57456
Ivan Mogilko <ikm_spb@yahoo.com>
2026-07-25 18:59:41 +0300

    Common: merged path_ex.cpp back into path.cpp
    As a consequence this requires Tools to link few Allegro sources:
    - allegro.c - contains std lib implementations (alloc etc)
    - file.c
    - wfile.c (on Windows)
    - ufile.c (elsewhere
    - unicode.c
    
    All above are added to TOOLS_MINIMAL_ALLEGRO_SOURCES in CMakeLists.
    
    MSVS specific: Allegro's file.c must be configured to output as al_file.obj because Common lib already has a file.cpp, which output obj will conflict with allegro's.

--- Common/util/path.cpp
+++ Common/util/path.cpp
@@ -11,12 +11,13 @@
 // https://opensource.org/license/artistic-2-0/
 //
 //=============================================================================
+#include "util/path.h"
+#include <allegro/file.h>
 #include "platform/platform.h"
 #if AGS_PLATFORM_OS_WINDOWS
 #include "platform/windows/windows.h"
 #endif
 #include "util/file.h"
-#include "util/path.h"
 #include "util/stdio_compat.h"
 
 namespace AGS
@@ -97,6 +98,24 @@ String ReplaceExtension(const String &filename, const String &ext)
     return String::FromFormat("%s.%s", noext.GetCStr(), ext.GetCStr());
 }
 
+int ComparePaths(const String &path1, const String &path2)
+{
+    // Make minimal absolute paths
+    String fixed_path1 = MakeAbsolutePath(path1);
+    String fixed_path2 = MakeAbsolutePath(path2);
+
+    fixed_path1.TrimRight('/');
+    fixed_path2.TrimRight('/');
+
+    int cmp_result =
+#if defined AGS_CASE_SENSITIVE_FILESYSTEM
+        fixed_path1.Compare(fixed_path2);
+#else
+        fixed_path1.CompareUtf8NoCase(fixed_path2);
+#endif // AGS_CASE_SENSITIVE_FILESYSTEM
+    return cmp_result;
+}
+
 String GetDirectoryPath(const String &path)
 {
     if (File::IsDirectory(path))
@@ -111,6 +130,28 @@ String GetDirectoryPath(const String &path)
         return dir;
     }
     return "./";
+}
+
+bool IsSameOrSubDir(const String &parent, const String &path)
+{
+    char can_parent[MAX_PATH_SZ];
+    char can_path[MAX_PATH_SZ];
+    char relative[MAX_PATH_SZ];
+    // canonicalize_filename treats "." as "./." (file in working dir)
+    const char *use_parent = parent == "." ? "./" : parent.GetCStr();
+    const char *use_path   = path   == "." ? "./" : path.GetCStr();
+    canonicalize_filename(can_parent, use_parent, MAX_PATH_SZ);
+    canonicalize_filename(can_path, use_path, MAX_PATH_SZ);
+    const char *pstr = make_relative_filename(relative, can_parent, can_path, MAX_PATH_SZ);
+    if (!pstr)
+        return false;
+    for (pstr = strstr(pstr, ".."); pstr && *pstr; pstr = strstr(pstr, ".."))
+    {
+        pstr += 2;
+        if (*pstr == '/' || *pstr == '\\' || *pstr == 0)
+            return false;
+    }
+    return true;
 }
 
 bool IsRelativePath(const String &path)
@@ -176,6 +217,36 @@ String MakeTrailingSlash(const String &path)
     String dir_path = String::FromFormat("%s/", path.GetCStr());
     FixupPath(dir_path);
     return dir_path;
+}
+
+String MakeAbsolutePath(const String &path)
+{
+    if (path.IsEmpty())
+    {
+        return "";
+    }
+    // canonicalize_filename treats "." as "./." (file in working dir)
+    String abs_path = path == "." ? "./" : path;
+    char buf[MAX_PATH_SZ];
+    canonicalize_filename(buf, abs_path.GetCStr(), MAX_PATH_SZ);
+    abs_path = buf;
+    FixupPath(abs_path);
+    return abs_path;
+}
+
+String MakeRelativePath(const String &base, const String &path)
+{
+    char can_parent[MAX_PATH_SZ];
+    char can_path[MAX_PATH_SZ];
+    char relative[MAX_PATH_SZ];
+    // canonicalize_filename treats "." as "./." (file in working dir)
+    const char *use_parent = base == "." ? "./" : base.GetCStr();
+    const char *use_path = path == "." ? "./" : path.GetCStr(); // FIXME?
+    canonicalize_filename(can_parent, use_parent, MAX_PATH_SZ);
+    canonicalize_filename(can_path, use_path, MAX_PATH_SZ);
+    String rel_path = make_relative_filename(relative, can_parent, can_path, MAX_PATH_SZ);
+    FixupPath(rel_path);
+    return rel_path;
 }
 
 String &AppendPath(String &path, const String &child)

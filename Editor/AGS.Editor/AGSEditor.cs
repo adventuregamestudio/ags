7ab210e9535fe98da3f3a4bbfe72ffe776802f81
Ivan Mogilko <ikm_spb@yahoo.com>
2026-09-09 16:18:28 +0300

    Editor: fix upgrading games from earlier versions to 3.6.3
    1. Buttons with no graphic and no text are supposed to be not visible (in earlier versions), we emulate that by turning their background and border off.
    2. InventoryItems with cursor hotspot 0,0 are supposed to have hotspot centered in their graphic.

--- Editor/AGS.Editor/AGSEditor.cs
+++ Editor/AGS.Editor/AGSEditor.cs
@@ -121,7 +121,8 @@ namespace AGS.Editor
          * 3.6.3.6        - Character.BlockingRectangle, RoomObject.BlockingRectangle.
          *                  expose Character.Transparency, Baseline, RoomObject.Transparency.
          * 3.6.3.8        - Font Overrides in Translation, TextOutlineColor in GUI controls.
-         * 3.6.3.10       - Settings.GameTextLanguage, TranslateTextParser, DisplaySingleDialogOption
+         * 3.6.3.10       - Settings.GameTextLanguage, TranslateTextParser, DisplaySingleDialogOption;
+         *                  InventoryItem.Hotspot now treats value 0,0 exactly, and not "centered in graphic"
          * 3.6.3.12       - Settings.TurnOrderPriority, TextBoxKeyClaimStyle.
         */
         public const int    LATEST_XML_VERSION_INDEX = 3060312;

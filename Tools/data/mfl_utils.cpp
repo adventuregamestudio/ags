dc7df8976aeb4e9a49181aacfd8f9e738befc899
Érico Porto <ericoporto2008@gmail.com>
2026-09-13 20:58:37 -0300

    Tools: fix mfl_utils format string use in errors

--- Tools/data/mfl_utils.cpp
+++ Tools/data/mfl_utils.cpp
@@ -288,7 +288,7 @@ HError TestLibraryFile(const String &lib_file, const AssetLibInfo *compare_lib)
     if (compare_lib)
     {
         if (lib.BaseFileOffset != compare_lib->BaseFileOffset)
-            return new Error(String::FromFormat("Base library offset does not match: %lld vs %lld", lib.BaseFileOffset, compare_lib->BaseFileOffset));
+            return new Error(String::FromFormat("Base library offset does not match: %" PRId64 " vs %" PRId64, lib.BaseFileOffset, compare_lib->BaseFileOffset));
         if (lib.AssetInfos.size() != compare_lib->AssetInfos.size())
             return new Error(String::FromFormat("Number of assets does not match: %zu vs %zu", lib.AssetInfos.size(), compare_lib->AssetInfos.size()));
         for (size_t i = 0; i < lib.AssetInfos.size(); ++i)
@@ -298,9 +298,9 @@ HError TestLibraryFile(const String &lib_file, const AssetLibInfo *compare_lib)
             if (asset1.FileName.CompareNoCase(asset2.FileName) != 0)
                 return new Error(String::FromFormat("Asset %zu does not match filename: %s vs %s", i, asset1.FileName.GetCStr(), asset2.FileName.GetCStr()));
             if (asset1.Size != asset2.Size)
-                return new Error(String::FromFormat("Asset %zu does not match size: %lld vs %lld", i, asset1.Size, asset2.Size));
+                return new Error(String::FromFormat("Asset %zu does not match size: %" PRId64 " vs %" PRId64, i, asset1.Size, asset2.Size));
             if (asset1.Offset - lib.BaseFileOffset != asset2.Offset)
-                return new Error(String::FromFormat("Asset %zu does not match offset: %lld vs %lld", i, asset1.Offset - lib.BaseFileOffset, asset2.Offset));
+                return new Error(String::FromFormat("Asset %zu does not match offset: %" PRId64 " vs %" PRId64, i, asset1.Offset - lib.BaseFileOffset, asset2.Offset));
         }
     }
 
